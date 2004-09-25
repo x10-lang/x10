8388a6a4cbd5a5f1ed8d1532a94f75e475f1bbec
Christian Grothoff <grothoff@domain>
2004-09-25 20:19:28 +0000

    Init.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@5 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ x10.compiler/src/polyglot/ext/x10/types/X10TypeSystem.java
@@ -0,0 +1,7 @@
+package polyglot.ext.x10.types;
+
+import polyglot.types.*;
+
+public interface X10TypeSystem extends TypeSystem {
+    // TODO: declare any new methods needed
+}
---
+++ x10.compiler/src/polyglot/ext/x10/types/X10TypeSystem_c.java
@@ -0,0 +1,9 @@
+package polyglot.ext.x10.types;
+
+import polyglot.types.*;
+import polyglot.ext.jl.types.TypeSystem_c;
+
+public class X10TypeSystem_c extends TypeSystem_c implements X10TypeSystem {
+    // TODO: implement new methods in X10TypeSystem.
+    // TODO: override methods as needed from TypeSystem_c.
+}
