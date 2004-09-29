6b41c4a8adfe1b03ff3322201cf03b631517bfdc
Christian Grothoff <grothoff@domain>
2004-09-29 19:03:56 +0000

    Import cleanup.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@14 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.compiler/src/polyglot/ext/x10/ast/X10NodeFactory_c.java
+++ x10.compiler/src/polyglot/ext/x10/ast/X10NodeFactory_c.java
@@ -1,13 +1,6 @@
 package polyglot.ext.x10.ast;
 
-import polyglot.ast.*;
-import polyglot.ext.jl.ast.*;
-import polyglot.types.Flags;
-import polyglot.types.Package;
-import polyglot.types.Type;
-import polyglot.types.Qualifier;
-import polyglot.util.*;
-import java.util.*;
+import polyglot.ext.jl.ast.NodeFactory_c;
 
 /**
  * NodeFactory for x10 extension.
