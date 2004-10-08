3cd0c72420e525fdd36cb974945fc31f4aebf5ee
Christian Grothoff <grothoff@domain>
2004-10-08 02:43:44 +0000

    Refactoring of the template code to use the Runabout and the Polyglot Extension mechanism.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@33 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.compiler/src/polyglot/ext/x10/ast/X10NodeFactory_c.java
+++ x10.compiler/src/polyglot/ext/x10/ast/X10NodeFactory_c.java
@@ -1,12 +1,12 @@
 package polyglot.ext.x10.ast;
 
+import polyglot.ast.Block;
 import polyglot.ast.Expr;
 import polyglot.ast.ExtFactory;
 import polyglot.ast.Instanceof;
 import polyglot.ast.TypeNode;
 import polyglot.ext.jl.ast.Instanceof_c;
 import polyglot.ext.jl.ast.NodeFactory_c;
-import polyglot.ext.x10.ast.X10ExtFactory_c;
 import polyglot.ext.x10.extension.X10InstanceofDel_c;
 import polyglot.util.Position;
 
@@ -19,7 +19,8 @@ public class X10NodeFactory_c extends NodeFactory_c implements X10NodeFactory {
     // TODO:  Override factory methods for AST nodes with new extension nodes.
 	
     public X10NodeFactory_c() {
-        super(new X10ExtFactory_c());
+        super(new X10ExtFactory_c(),
+              new X10DelFactory_c());
     }
     protected X10NodeFactory_c(ExtFactory extFact) {
         super(extFact);
@@ -30,4 +31,17 @@ public class X10NodeFactory_c extends NodeFactory_c implements X10NodeFactory {
         n = (Instanceof)n.ext(extFactory().extInstanceof());
         return (Instanceof)n.del(new X10InstanceofDel_c());
     }
+    
+    public Async Async(Position pos, Expr place, Block body) {
+        Async a = new Async_c(pos, place, body);
+        a = (Async) a.ext(extFactory().extExpr());
+        return (Async) a.del(delFactory().delExpr());
+    }
+    
+    public Future Future(Position pos, Expr place, Expr body) {
+        Future f = new Future_c(pos, place, body);
+        f = (Future) f.ext(extFactory().extStmt());
+        return (Future) f.del(delFactory().delStmt());
+    }
+    
 }
