7ebf9870582469312df125a41e8f469359626863
Christoph von Praun <user@domain>
2004-11-03 13:54:52 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@94 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ x10.runtime/src/x10/array/sharedmemory/Place_c.java
@@ -0,0 +1,14 @@
+/*
+ * Created on Nov 2, 2004
+ */
+package x10.array.sharedmemory;
+
+import x10.array.Place;
+
+/**
+ * Abstraction for processor in the x10.array.sharedmemory 
+ * package.
+ * 
+ * @author Christoph von Praun
+ */
+public class Place_c implements Place {}
