e23f05fc15f61a83ba3e92adac8cc550fb073bd7
Christoph von Praun <user@domain>
2004-11-15 00:44:34 +0000

    Added comment.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@118 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.runtime/src/x10/base/UnsafeContainer.java
+++ x10.runtime/src/x10/base/UnsafeContainer.java
@@ -5,6 +5,9 @@
 package x10.base;
 
 /**
+ * This interface has to be implemented by any data structure that 
+ * is passed to a native method (structs and arrays).
+ * 
  * @author Christoph von Praun
  */
 public interface UnsafeContainer {
