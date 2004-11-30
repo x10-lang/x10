a141481c353fd60429e74325dc8249082418c273
Christoph von Praun <user@domain>
2004-11-30 22:54:01 +0000

    Added support for primitive types.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@148 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.runtime/src/x10/lang/Future.java
+++ trunk/x10.runtime/src/x10/lang/Future.java
@@ -5,12 +5,30 @@ package x10.lang;
 
 /**
  * @author Christian Grothoff
+ * @author Christoph von Praun
  */
-public abstract class Future extends X10Object {
+public abstract class Future extends Object {
 
 	/**    
 	 * Wait for the completion of this activity and return the
 	 * return value.   
 	 */    
-	public abstract X10Object force();    
+	public abstract Object force();
+	
+	public abstract boolean forceBoolean();
+	
+	public abstract byte forceByte();
+	
+	public abstract char forceChar();
+	
+	public abstract short forceShort();
+	
+	public abstract int forceInt();
+	
+	public abstract long forceLong();
+	
+	public abstract float forceFloat();
+	
+	public abstract double forceDouble(); 	
+	
 }
