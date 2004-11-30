07c3181c54a6cdd5a48304ba72b4bd5268874fb5
Christoph von Praun <user@domain>
2004-11-30 22:54:01 +0000

    Added support for primitive types.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@148 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.runtime/src/x10/runtime/Future_c.java
+++ x10.runtime/src/x10/runtime/Future_c.java
@@ -4,7 +4,7 @@
 package x10.runtime;
 
 import x10.lang.Future;
-import x10.lang.X10Object;
+import x10.lang.Object;
 
 /**
  * This class encapsulates the return value of a local async
@@ -12,22 +12,54 @@ import x10.lang.X10Object;
  * async call (force future).
  * 
  * @author Christian Grothoff
+ * @author Christoph von PRaun
  */
 final class Future_c extends Future {
 
     private boolean haveResult_;
     
-    private X10Object result_;    
+    private Object result_;
+    private char resultUnsigned_;
+    private boolean resultBoolean_;
+    private long resultSigned_;
+    private double resultDouble_;
     
     /**
      * Set the result value returned by this async call.
      * 
      * @param result
      */
-    public synchronized void setResult(X10Object result) {
+    public synchronized void setResult(Object result) {
         assert (! haveResult_); 
-		
         this.result_ = result;
+        this.haveResult_ = true;
+        this.notifyAll(); // wake up 'force' if waiting
+    }
+    
+    public synchronized void setResult(boolean result) {
+        assert (! haveResult_); 
+        this.resultBoolean_ = result;
+        this.haveResult_ = true;
+        this.notifyAll(); // wake up 'force' if waiting
+    }
+    
+    public synchronized void setResult(long result) {
+        assert (! haveResult_); 
+        this.resultSigned_ = result;
+        this.haveResult_ = true;
+        this.notifyAll(); // wake up 'force' if waiting
+    }
+    
+    public synchronized void setResult(char result) {
+        assert (! haveResult_); 
+        this.resultUnsigned_ = result;
+        this.haveResult_ = true;
+        this.notifyAll(); // wake up 'force' if waiting
+    }
+    
+    public synchronized void setResult(double result) {
+        assert (! haveResult_); 
+        this.resultDouble_ = result;
         this.haveResult_ = true;
         this.notifyAll(); // wake up 'force' if waiting
     }
@@ -35,7 +67,7 @@ final class Future_c extends Future {
     /*
      * @see x10.runtime.Activity.Result#force()
      */
-    public synchronized X10Object force() {
+    public synchronized Object force() {
         while (! haveResult_) {
             try {
                 LoadMonitored.blocked();
@@ -48,5 +80,37 @@ final class Future_c extends Future {
         }
         return result_;
     }
+	
+	public boolean forceBoolean() {
+	    return resultBoolean_;
+	}
+	
+	public byte forceByte() {
+	    return (byte) resultSigned_;
+	}
+	
+	public char forceChar() {
+	    return (char) resultUnsigned_;
+	}
+	
+	public short forceShort() {
+	    return (short) resultSigned_;
+	}
+	
+	public int forceInt() {
+	    return (int) resultSigned_;
+	}
+	
+	public long forceLong() {
+	    return resultUnsigned_;
+	}
+	
+	public float forceFloat() {
+	    return (float) resultDouble_;
+	}
+	
+	public double forceDouble() {
+	    return resultDouble_;
+	}
 
 }
