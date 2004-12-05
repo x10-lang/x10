d10eaa81d6b8342a23ca8fa6e5204ec26039a62a
Vijay Saraswat <vsaraswa@us.ibm.com>
2004-12-05 22:46:30 +0000

    Code now uses X10Object() on X10TypeSystem to get to the object representing x10.lang.Object (rather than Object(), which will give you java.lang.Object).
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@166 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.compiler/src/polyglot/ext/x10/types/X10PrimitiveType_c.java
+++ x10.compiler/src/polyglot/ext/x10/types/X10PrimitiveType_c.java
@@ -33,18 +33,21 @@ public class X10PrimitiveType_c extends PrimitiveType_c implements X10PrimitiveT
      *
      */
     public boolean descendsFromImpl(Type ancestor) {
-        return ts.equals(ancestor, ts.Object());
+    	X10TypeSystem xts = (X10TypeSystem) ts;
+        return ts.equals(ancestor, xts.X10Object());
     }
 
     /** Return true if this type can be assigned to <code>toType</code>. */
     public boolean isImplicitCastValidImpl(Type toType) {
-        return ts.equals(toType, ts.Object()) ||
+    	X10TypeSystem xts = (X10TypeSystem) ts;
+        return ts.equals(toType, xts.X10Object()) ||
                super.isImplicitCastValidImpl(toType);
     }
 
     /** Returns true iff a cast from this to <code>toType</code> is valid. */
     public boolean isCastValidImpl(Type toType) {
-        return ts.equals(toType, ts.Object()) || super.isCastValidImpl(toType);
+    	X10TypeSystem xts = (X10TypeSystem) ts;
+        return ts.equals(toType, xts.Object()) || super.isCastValidImpl(toType);
     }
     
 //	 ----------------------------- begin manual mixin code from X10Type_c
