e1acbf6c1662bf0731abf1ced082c50ae3c14adc
Christoph von Praun <user@domain>
2004-12-05 21:16:25 +0000

    Removed unnecessary methods.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@164 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.runtime/src/x10/runtime/Future_c.java
+++ trunk/x10.runtime/src/x10/runtime/Future_c.java
@@ -19,10 +19,6 @@ final class Future_c extends Future {
     private boolean haveResult_;
     
     private Object result_;
-    private char resultUnsigned_;
-    private boolean resultBoolean_;
-    private long resultSigned_;
-    private double resultDouble_;
     
     /**
      * Set the result value returned by this async call.
@@ -32,34 +28,6 @@ final class Future_c extends Future {
     public synchronized void setResult(Object result) {
         assert (! haveResult_); 
         this.result_ = result;
-        this.haveResult_ = true;
-        this.notifyAll(); // wake up 'force' if waiting
-    }
-    
-    public synchronized void setResult(boolean result) {
-        assert (! haveResult_); 
-        this.resultBoolean_ = result;
-        this.haveResult_ = true;
-        this.notifyAll(); // wake up 'force' if waiting
-    }
-    
-    public synchronized void setResult(long result) {
-        assert (! haveResult_); 
-        this.resultSigned_ = result;
-        this.haveResult_ = true;
-        this.notifyAll(); // wake up 'force' if waiting
-    }
-    
-    public synchronized void setResult(char result) {
-        assert (! haveResult_); 
-        this.resultUnsigned_ = result;
-        this.haveResult_ = true;
-        this.notifyAll(); // wake up 'force' if waiting
-    }
-    
-    public synchronized void setResult(double result) {
-        assert (! haveResult_); 
-        this.resultDouble_ = result;
         this.haveResult_ = true;
         this.notifyAll(); // wake up 'force' if waiting
     }
@@ -80,37 +48,4 @@ final class Future_c extends Future {
         }
         return result_;
     }
-	
-	public boolean forceBoolean() {
-	    return resultBoolean_;
-	}
-	
-	public byte forceByte() {
-	    return (byte) resultSigned_;
-	}
-	
-	public char forceChar() {
-	    return (char) resultUnsigned_;
-	}
-	
-	public short forceShort() {
-	    return (short) resultSigned_;
-	}
-	
-	public int forceInt() {
-	    return (int) resultSigned_;
-	}
-	
-	public long forceLong() {
-	    return resultUnsigned_;
-	}
-	
-	public float forceFloat() {
-	    return (float) resultDouble_;
-	}
-	
-	public double forceDouble() {
-	    return resultDouble_;
-	}
-
 }
