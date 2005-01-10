1f692262261f65581b544f6ef834206cf4f0b36e
Vijay Saraswat <vsaraswa@us.ibm.com>
2005-01-10 01:37:22 +0000

    Fixed off by 1 error in place.factory.places
    git-svn-id: svn://svn.code.sf.net/p/x10/code@271 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.runtime/src/x10/lang/place.java
+++ trunk/x10.runtime/src/x10/lang/place.java
@@ -23,81 +23,82 @@ package x10.lang;
 import /*x10*/java.util.Set;
 
 public /*value*/ class place /*(nat i : i =< MAX_PLACES)*/ {
-    public final /*nat*/int id;
-
-    /** The number of places in this run of the system. Set on
-     * initialization, through the command line/init parameters file.
-     * is initialized by a specific runtime, e.g. x10.runtime.DefaultRuntime_c
-     */
-    /*config*/ public static /*final*/ /*nat*/ int MAX_PLACES = 10;
-   
-
-    protected place( /*nat*/ int id) {
-     this.id = id;
-    }
-
-    public static abstract /*value*/ class factory {
-	/**  Return the place numbered i, using modulo MAX_PLACES
-	 *  arithmetic.
+	public final /*nat*/int id;
+	
+	/** The number of places in this run of the system. Set on
+	 * initialization, through the command line/init parameters file.
+	 * is initialized by a specific runtime, e.g. x10.runtime.DefaultRuntime_c
 	 */
-	abstract public place/*(i % MAX_PLACES)*/ place( final /*nat*/int i );
-
-	/** Return the set of places from 0 to last. */
-	abstract public Set/*<place>*/ places( /*nat*/int last);
-
-	/** Return the place of the current activity.
+	/*config*/ public static /*final*/ /*nat*/ int MAX_PLACES = 10;
+	
+	
+	protected place( /*nat*/ int id) {
+		this.id = id;
+	}
+	
+	public static abstract /*value*/ class factory {
+		/**  Return the place numbered i, using modulo MAX_PLACES
+		 *  arithmetic.
+		 */
+		abstract public place/*(i % MAX_PLACES)*/ place( final /*nat*/int i );
+		
+		/** Return the set of places from 0 to last. */
+		abstract public Set/*<place>*/ places( /*nat*/int last);
+		
+		/** Return the place of the current activity.
+		 */
+		abstract public place here();
+	}
+	public static final factory factory = Runtime.factory.getPlaceFactory();
+	
+	/** The last place in this program execution.
 	 */
-	abstract public place here();
-    }
-   public static final factory factory = Runtime.factory.getPlaceFactory();
-
-    /** The last place in this program execution.
-     */
-    public static final place/*(MAX_PLACES)*/ LAST_PLACE = factory.place(MAX_PLACES);
-
-    /** The first place in this program execution.
-     */
-    public static final place/*(0)*/ FIRST_PLACE = factory.place(0);
-    public static final Set/*<place>*/ places = factory.places( MAX_PLACES );	
-
-
-    /** Returns the next place, using modular arithmetic. Thus the
-     * next place for the last place is the first place. 
-     */
-    public place/*(id+1 % MAX_PLACES)*/ next()  { 
-    	final place result = next(1);
-    	assert result.id == (id+1 % MAX_PLACES);
-    	return result; 
-    }
-
-    /** Returns the previous place, using modular arithmetic. Thus the
-     * previous place for the first place is the last place. 
-     */
-    public place/*(i-1 % MAX_PLACES)*/ prev()  { 
-    	final place result = next( -1 );
-    	assert result.id == (id-1 % MAX_PLACES);
-    	return result; 
-    }
-
-
-    /** Returns the k'th next place, using modular arithmetic. k may
-     * be negative.
-     */
-    public place/*(i+k % MAX_PLACES)*/ next( final int k ) {
-	final place result = factory.place( (id + k) % MAX_PLACES);
-	assert result.id == (id+k % MAX_PLACES);
-	return result; 
-    }
-
-    /**  Is this the first place?
-     */
-    public boolean isFirst() { 
-	return id==0;
-    }
-
-    /** Is this the last place?
-     */
-    public boolean isLast() { 
-	return id==MAX_PLACES -1; 
-    }
+	public static final place/*(MAX_PLACES)*/ LAST_PLACE = factory.place(MAX_PLACES);
+	
+	/** The first place in this program execution.
+	 */
+	public static final place/*(0)*/ FIRST_PLACE = factory.place(0);
+	
+	public static final Set/*<place>*/ places = factory.places( MAX_PLACES-1 );	
+	
+	
+	/** Returns the next place, using modular arithmetic. Thus the
+	 * next place for the last place is the first place. 
+	 */
+	public place/*(id+1 % MAX_PLACES)*/ next()  { 
+		final place result = next(1);
+		assert result.id == (id+1 % MAX_PLACES);
+		return result; 
+	}
+	
+	/** Returns the previous place, using modular arithmetic. Thus the
+	 * previous place for the first place is the last place. 
+	 */
+	public place/*(i-1 % MAX_PLACES)*/ prev()  { 
+		final place result = next( -1 );
+		assert result.id == (id-1 % MAX_PLACES);
+		return result; 
+	}
+	
+	
+	/** Returns the k'th next place, using modular arithmetic. k may
+	 * be negative.
+	 */
+	public place/*(i+k % MAX_PLACES)*/ next( final int k ) {
+		final place result = factory.place( (id + k) % MAX_PLACES);
+		assert result.id == (id+k % MAX_PLACES);
+		return result; 
+	}
+	
+	/**  Is this the first place?
+	 */
+	public boolean isFirst() { 
+		return id==0;
+	}
+	
+	/** Is this the last place?
+	 */
+	public boolean isLast() { 
+		return id==MAX_PLACES -1; 
+	}
 }
