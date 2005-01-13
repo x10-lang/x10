1235de687b287d21e69b8fe7044c99581902e6c2
Christoph von Praun <user@domain>
2005-01-13 14:35:11 +0000

    Fixed unit-testing for arrays package.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@298 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.runtime/src/x10/array/TestIntArray.java
+++ x10.runtime/src/x10/array/TestIntArray.java
@@ -4,16 +4,52 @@
 package x10.array;
 
 import junit.framework.TestCase;
+import x10.lang.Activity;
 import x10.lang.Runtime;
 import x10.lang.region;
 import x10.lang.distribution;
-import x10.lang.place;
+import x10.runtime.DefaultRuntime_c;
+import x10.runtime.Place;
+import x10.runtime.ThreadRegistry;
 
 /**
  * @author Christoph von Praun
  */
 public class TestIntArray extends TestCase {
-
+    
+    private final Activity a
+    = new Activity() { public void run() {} }; // dummy
+    
+    /**
+     * Junit may use additional threads to run the testcases
+     * (other than the main one used to initialize the
+     * Runtime class).  Hence we need a litte hack to register
+     * the thread used to run the testcase as a 'local' thread
+     * with the Runtime.
+     */
+    public void setUp() {
+        DefaultRuntime_c r = (DefaultRuntime_c) Runtime.runtime;
+        Place[] pls = Place.places();
+        if (r instanceof ThreadRegistry) {
+            Thread t = Thread.currentThread();
+            ThreadRegistry tr = (ThreadRegistry) r;
+            tr.registerThread(t, pls[0]);
+            tr.registerActivityStart(t, a, null);
+        }
+    }
+    
+    /**
+     * Clean-up effects from setUp().
+     */
+    public void tearDown() {
+        DefaultRuntime_c r = (DefaultRuntime_c) Runtime.runtime;
+        if (r instanceof ThreadRegistry) {
+            Thread t = Thread.currentThread();
+            ThreadRegistry tr = (ThreadRegistry) r;
+            tr.registerActivityStop(t, a);
+        }
+    }
+    
     public void testToJava() {
         // ArrayFactory.init(ArrayRuntime.getRuntime());
         try {
