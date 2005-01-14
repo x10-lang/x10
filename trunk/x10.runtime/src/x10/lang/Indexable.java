83609e4f9e9b221eac9ca8102c77dd9830459b35
Vijay Saraswat <vsaraswa@us.ibm.com>
2005-01-14 14:21:07 +0000

    Supporting X10Arrays -- the X10 array types are now directly defined by the (Java) classes in x10.lang.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@327 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ trunk/x10.runtime/src/x10/lang/Indexable.java
@@ -0,0 +1,14 @@
+/*
+ * Created by vj on Jan 13, 2005
+ *
+ * 
+ */
+package x10.lang;
+
+/** A package protected interface implemented by x10.lang classes which can be accessed as arrays.
+ * @author vj Jan 13, 2005
+ * 
+ */
+interface Indexable {
+
+}
