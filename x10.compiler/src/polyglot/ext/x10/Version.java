e2398a05f97e711170ca88ed2bfe53b306492b37
Chris Donawa <donawa@domain>
2005-03-12 01:42:03 +0000

    Update version number
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@522 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.compiler/src/polyglot/ext/x10/Version.java
+++ x10.compiler/src/polyglot/ext/x10/Version.java
@@ -6,8 +6,7 @@ package polyglot.ext.x10;
 public class Version extends polyglot.main.Version {
     public String name() { return "x10"; }
 
-    // TODO: define a version number, the default (below) is 0.1.0
     public int major() { return 0; }
-    public int minor() { return 1; }
+    public int minor() { return 41; }
     public int patch_level() { return 0; }
 }
