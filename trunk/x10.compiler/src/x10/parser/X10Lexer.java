4c991b51c4baa23197a214cc26b931d15c439549
Philippe Charles <pcharles@domain>
2005-04-08 22:36:13 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/x10/code@582 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.compiler/src/x10/parser/X10Lexer.java
+++ trunk/x10.compiler/src/x10/parser/X10Lexer.java
@@ -3,20 +3,28 @@
 
 package x10.parser;
 
-import java.util.*;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.io.File;
+
 import com.ibm.lpg.*;
 
 public class X10Lexer extends LpgLexStream implements RuleAction, X10Parsersym, X10Lexersym
 {
     private PrsStream prsStream;
-    private ParseTable prs;
-    private LexParser lexParser;
+    private ParseTable prs = new X10Lexerprs();
+    private LexParser lexParser = new LexParser(this, prs, this);
+
+    public X10Lexer(String filename, int tab) throws java.io.IOException 
+    {
+        super(filename, tab);
+    }
 
     public X10Lexer(char[] input_chars, String filename, int tab)
     {
         super(input_chars, filename, tab);
-        prs = new X10Lexerprs();
-        lexParser = new LexParser(this, prs, this);
     }
 
     public X10Lexer(char[] input_chars, String filename)
@@ -54,6 +62,12 @@ public class X10Lexer extends LpgLexStream implements RuleAction, X10Parsersym, 
     boolean printTokens;
     private final static int ECLIPSE_TAB_VALUE = 4;
 
+    public X10Lexer(String filename) throws java.io.IOException
+    {
+        this(filename, ECLIPSE_TAB_VALUE);
+        kwLexer = new X10KWLexer(getInputChars(), TK_IDENTIFIER);
+    }
+    
     public X10Lexer(Option option)
     {
         this(option.getInputChars(), option.getFileName(), ECLIPSE_TAB_VALUE);
@@ -97,6 +111,140 @@ public class X10Lexer extends LpgLexStream implements RuleAction, X10Parsersym, 
         System.out.print(s);
     }
 
+    //
+    //
+    //
+    private static boolean differ_tokens = false;
+    private static int changeCount = 0,
+                       insertCount = 0,
+                       deleteCount = 0,
+                       replaceCount = 0,
+                       moveCount = 0;
+
+    private static void compareFiles(String old_file, String new_file)
+    {
+        try
+        {
+            X10Lexer old_lexer = new X10Lexer(old_file),
+                                      new_lexer = new X10Lexer(new_file);
+
+            PrsStream old_stream = new PrsStream(old_lexer);
+            old_lexer.lexer(old_stream);
+
+            PrsStream new_stream = new PrsStream(new_lexer);
+            new_lexer.lexer(new_stream);
+
+            Differ diff = (differ_tokens ? (Differ) new DifferTokens(old_stream, new_stream)
+                                         : (Differ) new DifferLines(old_stream, new_stream));
+            diff.compare();
+
+            if (diff.getChangeCount() > 0)
+            {
+                diff.outputChanges();
+
+                changeCount += diff.getChangeCount();
+                insertCount += diff.getInsertCount();
+                deleteCount += diff.getDeleteCount();
+                replaceCount += diff.getReplaceCount();
+                moveCount += diff.getMoveCount();
+            }
+        }
+        catch (Exception e)
+        {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+
+    private static void compareDirectories(File old_dir, File new_dir)
+    {
+        try
+        {
+            File old_file[] = old_dir.listFiles(),
+                 new_file[] = new_dir.listFiles();
+            HashMap old_map = new HashMap();
+            for (int i = 0; i < old_file.length; i++)
+                old_map.put(old_file[i].getName(), old_file[i]);
+
+            for (int i = 0; i < new_file.length; i++)
+            {
+                File file = (File) old_map.get(new_file[i].getName());
+                if (file != null)
+                {
+                    old_map.remove(new_file[i].getName());
+
+                    if (file.isDirectory() && new_file[i].isDirectory())
+                         compareDirectories(file, new_file[i]);
+                    else compareFiles(file.getPath(), new_file[i].getPath());
+                }
+                else ; /* TODO: file is a new file */ 
+            }
+
+            for (Iterator i = old_map.entrySet().iterator(); i.hasNext(); )
+            {
+                Map.Entry e = (Map.Entry) i.next();
+                File file = (File) e.getValue();
+                // TODO: file was deleted
+            }
+        }
+        catch (Exception e)
+        {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        String new_file = "new file",
+               old_file = "old file";
+
+        int i;
+        for (i = 0; i < args.length; i++)
+        {
+            if (args[i].charAt(0) == '-')
+            {
+                if (args[i].equals("-t"))
+                    differ_tokens = true;
+                else if (args[i].equals("-l"))
+                    differ_tokens = false;
+            }
+            else break;
+        }
+        if (i < args.length) 
+            new_file = args[i++];
+        if (i < args.length) 
+            old_file = args[i++];
+        for (; i < args.length; i++)
+            System.err.println("Invalid argument: " + args[i]);
+
+        File old_dir = new File(old_file),
+             new_dir = new File(new_file);
+        if (old_dir.isDirectory() && new_dir.isDirectory())
+             compareDirectories(old_dir, new_dir);
+        else compareFiles(old_file, new_file);
+
+        if (changeCount == 0)
+            System.out.println("***** No difference *****");
+        else
+        {
+            System.out.println("***** " +
+                               changeCount +
+                               " different " +
+                               (changeCount == 1 ? "section" : "sections") + " *****");
+            System.out.println("    " + insertCount  + (differ_tokens ? " tokens" : " lines") + " inserted");
+            System.out.println("    " + deleteCount  + (differ_tokens ? " tokens" : " lines") + " deleted");
+            System.out.println("    " + replaceCount + (differ_tokens ? " tokens" : " lines") + " replaced");
+            System.out.println("    " + moveCount    + (differ_tokens ? " tokens" : " lines") + " moved");
+        }
+
+        return;
+    }
+
+    //
+    //
+    //
     public final static int tokenKind[] =
     {
         Char_CtlCharNotWS,
