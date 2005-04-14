dc65d21987e28175d16d66989c5e17de427995d1
Chris Donawa <donawa@domain>
2005-04-14 20:45:54 +0000

    Do not use points for get/set.  Note: had to adjust indicies by region(x).low, which adds additional cost
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@790 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.runtime/src/x10/array/sharedmemory/FloatArray_c.java
+++ x10.runtime/src/x10/array/sharedmemory/FloatArray_c.java
@@ -232,40 +232,48 @@ public class FloatArray_c extends FloatArray implements UnsafeContainer, Cloneab
         return arr_.setFloat(v, (int) distribution.region.ordinal(pos));
     }
     
+    public float setOrdinal(float v, int rawIndex) {
+    	
+    	return arr_.setFloat(v,rawIndex);
+    }
     
     public float set(float v, int d0) {
     	assert this.region.rank == 1;
-        int[] pos = {d0};
-    	final point p = Runtime.factory.getPointFactory().point(this.region, pos);
-        return set(v, p);
+    	d0 -= region.rank(0).low();
+    	return arr_.setFloat(v,d0);
     }
-    
+     
     public float set(float v, int d0, int d1) {
     	assert this.region.rank == 2;
-    	if(false){
-    		int[] pos = {d0, d1};
-    		final point p = Runtime.factory.getPointFactory().point(this.region, pos);
-    		return set(v, p);
-    	}else {
-    		int theIndex= d1 + (d0 *region.rank(1).size());
-    		return arr_.setFloat(v,theIndex);
-    	}
+    	d0 -= region.rank(0).low();
+    	d1 -= region.rank(1).low();
+    	int theIndex= d1 + (d0 *region.rank(1).size());
+    	return arr_.setFloat(v,theIndex);
     }
     
     public float set(float v, int d0, int d1, int d2) {
     	assert this.region.rank == 3;
-        int[] pos = {d0, d1, d2};
-        final point p = Runtime.factory.getPointFactory().point(this.region, pos);
-        return set(v, p);
+    	d0 -= region.rank(0).low();
+    	d1 -= region.rank(1).low();
+    	d2 -= region.rank(2).low();
+    	int theIndex= d2 + (d1 * region.rank(2).size()) + (d0 * (region.rank(2).size()*region.rank(1).size())) ;
+    	return arr_.setFloat(v,theIndex);
     }
     
     public float set(float v, int d0, int d1, int d2, int d3) {
     	assert this.region.rank == 4;
-        int[] pos = {d0, d1, d2, d3};
-        final point p = Runtime.factory.getPointFactory().point(this.region, pos);
-        return set(v, p);
-        
+    	d0 -= region.rank(0).low();
+    	d1 -= region.rank(1).low();
+    	d2 -= region.rank(2).low();
+    	d3 -= region.rank(3).low();
+    	
+    	int theIndex= d3+ (d2 * region.rank(3).size()) + 
+		(d1 * region.rank(2).size()* region.rank(3).size()) + 
+		(d0 *region.rank(1).size()* region.rank(2).size()* region.rank(3).size()) ;
+    	return arr_.setFloat(v,theIndex);
+    	
     }
+
 
     /* (non-Javadoc)
      * @see x10.lang.FloatArray#get(int[])
@@ -275,38 +283,52 @@ public class FloatArray_c extends FloatArray implements UnsafeContainer, Cloneab
         return arr_.getFloat((int) distribution.region.ordinal(pos));
     }
     
+    
+    public float getOrdinal(int rawIndex) {
+    	
+    	return arr_.getFloat(rawIndex);
+    }
+    
     public float get(int d0) {
     	assert this.region.rank == 1;
-        int[] pos = {d0};
-        final point p = Runtime.factory.getPointFactory().point(this.region, pos);
-    	return get(p);
+    	d0 -= region.rank(0).low();
+    	return arr_.getFloat(d0);
     }
     public float get(int d0, int d1) {
     	assert this.region.rank == 2;
-    	if(false){
-    		int[] pos = {d0, d1};
-    		final point p = Runtime.factory.getPointFactory().point(this.region, pos);
-    		return get(p);
-    	}else {
-    		int theIndex= d1 + (d0 *region.rank(1).size());
-    		
-    		return arr_.getFloat(theIndex);
-    	}
+    	d0 -= region.rank(0).low();
+    	d1 -= region.rank(1).low();
+    	int theIndex= d1 + (d0 *region.rank(1).size());
+    	
+    	return arr_.getFloat(theIndex);
     }
     
     public float get(int d0, int d1, int d2) {
     	assert this.region.rank == 3;
-        int[] pos = {d0, d1, d2};
-        final point p = Runtime.factory.getPointFactory().point(this.region, pos);
-    	return get(p);
-    }
+    	d0 -= region.rank(0).low();
+    	d1 -= region.rank(1).low();
+    	d2 -= region.rank(2).low();
+    	
+    	int theIndex= d2 + (d1 *region.rank(2).size()) +
+		(d0 *region.rank(1).size()*region.rank(2).size());
+    	return arr_.getFloat(theIndex);  	
+    } 
     
     public float get(int d0, int d1, int d2, int d3) {
     	assert this.region.rank == 4;
-        int[] pos = {d0, d1, d2, d3};
-        final point p = Runtime.factory.getPointFactory().point(this.region, pos);
-    	return get(p);
+    	d0 -= region.rank(0).low();
+    	d1 -= region.rank(1).low();
+    	d2 -= region.rank(2).low();
+    	d3 -= region.rank(3).low();
+    	
+    	int theIndex= d3 + (d2*region.rank(3).size()) + 
+		(d1 *region.rank(2).size()*region.rank(3).size()) + 
+		(d0 *region.rank(1).size()*region.rank(2).size()*region.rank(3).size());
+    	
+    	return arr_.getFloat(theIndex);
+    	
     }
+    
     public float get(int[] pos) {
         final point p = Runtime.factory.getPointFactory().point(this.region, pos);
     	return get(p);
--- x10.runtime/src/x10/array/sharedmemory/IntArray_c.java
+++ x10.runtime/src/x10/array/sharedmemory/IntArray_c.java
@@ -232,33 +232,48 @@ public class IntArray_c extends IntArray implements UnsafeContainer {
     public int setOrdinal(int v, int d0) {
     	return arr_.setInt(v, d0);
     }
+  
+    
+  
+    
     public int set(int v, int d0) {
     	assert this.region.rank == 1;
-        int[] pos = {d0};
-    	final point p = Runtime.factory.getPointFactory().point(this.region, pos);
-        return set(v, p);
+    	d0 -= region.rank(0).low();
+    	return arr_.setInt(v,d0);
     }
+    
+    
+    
     
     public int set(int v, int d0, int d1) {
     	assert this.region.rank == 2;
-        int[] pos = {d0, d1};
-        final point p = Runtime.factory.getPointFactory().point(this.region, pos);
-        return set(v, p);
+    	d0 -= region.rank(0).low();
+    	d1 -= region.rank(1).low();
+    	int theIndex= d1 + (d0 *region.rank(1).size());
+    	return arr_.setInt(v,theIndex);
     }
     
     public int set(int v, int d0, int d1, int d2) {
     	assert this.region.rank == 3;
-        int[] pos = {d0, d1, d2};
-        final point p = Runtime.factory.getPointFactory().point(this.region, pos);
-        return set(v, p);
+    	d0 -= region.rank(0).low();
+    	d1 -= region.rank(1).low();
+    	d2 -= region.rank(2).low();
+    	int theIndex= d2 + (d1 * region.rank(2).size()) + (d0 * (region.rank(2).size()*region.rank(1).size())) ;
+    	return arr_.setInt(v,theIndex);
     }
     
     public int set(int v, int d0, int d1, int d2, int d3) {
     	assert this.region.rank == 4;
-        int[] pos = {d0, d1, d2, d3};
-        final point p = Runtime.factory.getPointFactory().point(this.region, pos);
-        return set(v, p);
-        
+    	d0 -= region.rank(0).low();
+    	d1 -= region.rank(1).low();
+    	d2 -= region.rank(2).low();
+    	d3 -= region.rank(3).low();
+    	
+    	int theIndex= d3+ (d2 * region.rank(3).size()) + 
+		(d1 * region.rank(2).size()* region.rank(3).size()) + 
+		(d0 *region.rank(1).size()* region.rank(2).size()* region.rank(3).size()) ;
+    	return arr_.setInt(v,theIndex);
+    	
     }
 
     /* (non-Javadoc)
@@ -276,32 +291,47 @@ public class IntArray_c extends IntArray implements UnsafeContainer {
     	
     	return arr_.getInt(rawIndex);
     }
+    
+    
+    
     public int get(int d0) {
     	assert this.region.rank == 1;
-        int[] pos = {d0};
-        final point p = Runtime.factory.getPointFactory().point(this.region, pos);
-    	return get(p);
+    	d0 -= region.rank(0).low();
+    	return arr_.getInt(d0);
     }
-    
     public int get(int d0, int d1) {
     	assert this.region.rank == 2;
-        int[] pos = {d0, d1};
-        final point p = Runtime.factory.getPointFactory().point(this.region, pos);
-    	return get(p);
+    	d0 -= region.rank(0).low();
+    	d1 -= region.rank(1).low();
+    	int theIndex= d1 + (d0 *region.rank(1).size());
+    	
+    	return arr_.getInt(theIndex);
     }
     
     public int get(int d0, int d1, int d2) {
     	assert this.region.rank == 3;
-        int[] pos = {d0, d1, d2};
-        final point p = Runtime.factory.getPointFactory().point(this.region, pos);
-    	return get(p);
-    }
+    	d0 -= region.rank(0).low();
+    	d1 -= region.rank(1).low();
+    	d2 -= region.rank(2).low();
+    	
+    	int theIndex= d2 + (d1 *region.rank(2).size()) +
+		(d0 *region.rank(1).size()*region.rank(2).size());
+    	return arr_.getInt(theIndex);  	
+    } 
     
     public int get(int d0, int d1, int d2, int d3) {
     	assert this.region.rank == 4;
-        int[] pos = {d0, d1, d2, d3};
-        final point p = Runtime.factory.getPointFactory().point(this.region, pos);
-    	return get(p);
+    	d0 -= region.rank(0).low();
+    	d1 -= region.rank(1).low();
+    	d2 -= region.rank(2).low();
+    	d3 -= region.rank(3).low();
+    	
+    	int theIndex= d3 + (d2*region.rank(3).size()) + 
+		(d1 *region.rank(2).size()*region.rank(3).size()) + 
+		(d0 *region.rank(1).size()*region.rank(2).size()*region.rank(3).size());
+    	
+    	return arr_.getInt(theIndex);
+    	
     }
     public int get(int[] pos) {
         final point p = Runtime.factory.getPointFactory().point(this.region, pos);
