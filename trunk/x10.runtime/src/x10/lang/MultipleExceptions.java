34b35106dec76ac3f80c39f5964ca4d816fb0052
Christoph von Praun <user@domain>
2005-05-10 13:05:39 +0000

    Fixed bug that could cause StackOverflow in toString method.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@963 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.runtime/src/x10/lang/MultipleExceptions.java
+++ trunk/x10.runtime/src/x10/lang/MultipleExceptions.java
@@ -8,12 +8,27 @@ import java.util.Stack;
  */
 public class MultipleExceptions extends x10.lang.Exception {
     public final Stack exceptions; // <Throwable>
+    
     public MultipleExceptions(Stack s) {
-        this.exceptions = s;
+        Stack s_actual = new Stack();
+        // cvp: make sure that there are no MultipleExceptions in the stack -- 
+        // otherwise toString might result in a stack-overflow.
+        for (Iterator it = s.iterator(); it.hasNext(); ) {
+            Throwable t = (Throwable) it.next();
+            if (t instanceof MultipleExceptions) {
+                MultipleExceptions me = (MultipleExceptions) t;
+                s_actual.addAll(me.exceptions);
+            } else {
+                s_actual.add(t);
+            }
+        }
+        exceptions = s_actual;
     }
+    
     public String toString() {
         return exceptions.toString();
     }
+    
     public void printStackTrace() {
         super.printStackTrace();
         for (Iterator it = exceptions.iterator();it.hasNext();)
