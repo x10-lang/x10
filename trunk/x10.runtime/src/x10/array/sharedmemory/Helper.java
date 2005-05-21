f79891893b03f2f90e3a2a10dbaf1706bf60a67c
Chris Donawa <donawa@domain>
2005-05-21 02:26:08 +0000

    specialize check for array out of bounds check
    git-svn-id: svn://svn.code.sf.net/p/x10/code@1046 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.runtime/src/x10/array/sharedmemory/Helper.java
+++ trunk/x10.runtime/src/x10/array/sharedmemory/Helper.java
@@ -10,24 +10,53 @@ package x10.array.sharedmemory;
  * dereferencing. 
  */
 
+import x10.array.MultiDimRegion;
 import x10.lang.region;
 
 
 public class Helper {
 	final public static int ordinal(region r,int d0){
 		assert (r.rank == 1);
-		final int index[] = {d0};
-		if(!r.contains(index)) throw new ArrayIndexOutOfBoundsException();
-
+		if(r instanceof MultiDimRegion){
+			MultiDimRegion mdr = (MultiDimRegion)r;
+			int lowd0 = mdr.rank(0).low();		
+			int highd0 = mdr.rank(0).high();				
+			if(d0 < lowd0 || d0 > highd0){
+				throw new ArrayIndexOutOfBoundsException();
+			}
+		}
+		else
+		{
+			final int index[] = {d0};
+			if(!r.contains(index)) throw new ArrayIndexOutOfBoundsException();
+		}
 		d0 -= r.rank(0).low();
 		return d0;
 	}
 	
 	final public static int ordinal(region r,int d0,int d1){
 		assert (r.rank == 2);
+		if(r instanceof MultiDimRegion){
+			MultiDimRegion mdr = (MultiDimRegion)r;
+			int lowd0 = mdr.rank(0).low();
+			int lowd1 = mdr.rank(1).low();
+			
+			int highd0 = mdr.rank(0).high();
+			int highd1 = mdr.rank(1).high();
+		
+			
+			if(d0 < lowd0 || d0 > highd0 ||
+				d1 < lowd1 || d1 > highd1){
+				throw new ArrayIndexOutOfBoundsException();
+			}
+		}
+		else
+		{
 		final int index[] = {d0,d1};
+	
 		if(!r.contains(index)) 
 			throw new ArrayIndexOutOfBoundsException();
+		}
 		
 		d0 -= r.rank(0).low();
 		d1 -= r.rank(1).low();
@@ -35,9 +64,28 @@ public class Helper {
 	}
 	final public static int ordinal(region r,int d0,int d1, int d2){
 		assert (r.rank == 3);
-		final int index[] = {d0,d1,d2};
-		if(!r.contains(index)) 
-			throw new ArrayIndexOutOfBoundsException();
+			
+		if(r instanceof MultiDimRegion){
+			MultiDimRegion mdr = (MultiDimRegion)r;
+			int lowd0 = mdr.rank(0).low();
+			int lowd1 = mdr.rank(1).low();
+			int lowd2 = mdr.rank(2).low();
+			int highd0 = mdr.rank(0).high();
+			int highd1 = mdr.rank(1).high();
+			int highd2 = mdr.rank(2).high();
+			
+			if(d0 < lowd0 || d0 > highd0 ||
+				d1 < lowd1 || d1 > highd1 ||
+				d2 < lowd2 || d2 > highd2){
+				throw new ArrayIndexOutOfBoundsException();
+			}
+		}
+		else
+		{
+			final int index[] = {d0,d1,d2};
+			if(!r.contains(index)) 
+				throw new ArrayIndexOutOfBoundsException();
+		}
 		
 		d0 -= r.rank(0).low();
 		d1 -= r.rank(1).low();
@@ -45,13 +93,35 @@ public class Helper {
 		int d1Size=r.rank(2).size();
 		int d0Size=d1Size * r.rank(1).size();
 		return d2+ d1*d1Size+ d0*d0Size;
+		
 	}
 	final public static int ordinal(region r,int d0,int d1, int d2,int d3){
 		assert (r.rank == 4);
+		
+		if(r instanceof MultiDimRegion){
+			MultiDimRegion mdr = (MultiDimRegion)r;
+			int lowd0 = mdr.rank(0).low();
+			int lowd1 = mdr.rank(1).low();
+			int lowd2 = mdr.rank(2).low();
+			int lowd3 = mdr.rank(3).low();
+			int highd0 = mdr.rank(0).high();
+			int highd1 = mdr.rank(1).high();
+			int highd2 = mdr.rank(2).high();
+			int highd3 = mdr.rank(3).high();
+			if(d0 < lowd0 || d0 > highd0 ||
+				d1 < lowd1 || d1 > highd1 ||
+				d2 < lowd2 || d2 > highd2 ||
+				d3 < lowd3 || d3 > highd3){
+				throw new ArrayIndexOutOfBoundsException();
+			}
+		}
+		else
+		{
 		final int index[] = {d0,d1,d2,d3};
 		if(!r.contains(index)) 
 			throw new ArrayIndexOutOfBoundsException();
-			
+		}
+		
 		d0 -= r.rank(0).low();
 		d1 -= r.rank(1).low();
 		d2 -= r.rank(2).low();
