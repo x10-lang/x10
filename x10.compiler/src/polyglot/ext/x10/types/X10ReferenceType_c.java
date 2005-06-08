3f574aceeb51db062393c244228de476a1940914
Christoph von Praun <user@domain>
2005-06-08 23:14:32 +0000

    Removed unnecessary imports.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@1166 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.compiler/src/polyglot/ext/x10/types/X10ReferenceType_c.java
+++ x10.compiler/src/polyglot/ext/x10/types/X10ReferenceType_c.java
@@ -3,12 +3,8 @@
  */
 package polyglot.ext.x10.types;
 
-import java.util.List;
-
 import polyglot.ext.jl.types.ReferenceType_c;
 import polyglot.main.Report;
-import polyglot.types.FieldInstance;
-import polyglot.types.Resolver;
 import polyglot.types.Type;
 import polyglot.types.TypeSystem;
 import polyglot.util.Position;
