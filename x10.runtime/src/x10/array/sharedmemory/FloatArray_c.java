3442391011ee998dc25579d3c511945eccb108a3
Chris Donawa <donawa@domain>
2005-06-25 00:58:40 +0000

    Add prototype code to support remote constructors
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@1248 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.runtime/src/x10/array/sharedmemory/FloatArray_c.java
+++ x10.runtime/src/x10/array/sharedmemory/FloatArray_c.java
@@ -11,11 +11,15 @@ import x10.base.MemoryBlock;
 import x10.base.UnsafeContainer;
 import x10.lang.Indexable;
 import x10.lang.Runtime;
+import x10.runtime.RemotePlace;
+import x10.lang.place;
 import x10.lang.point;
 import x10.lang.dist;
 import x10.lang.region;
 import x10.lang.FloatReferenceArray;
 import x10.runtime.Configuration;
+import x10.runtime.FatPointer;
+import x10.runtime.ElementType;
 
 
 /**
@@ -97,11 +101,47 @@ public class FloatArray_c extends FloatArray implements UnsafeContainer, Cloneab
     public FloatArray_c( dist d, FloatArray.pointwiseOp f, boolean safe) {
     	this(d, f, safe, true);
     }
-    public FloatArray_c( dist d, FloatArray.pointwiseOp f, boolean safe, boolean mutable) {
+    
+    public FloatArray_c(/*TODO: add FatPointer containingObject*/
+    		dist d, FloatArray.pointwiseOp f, boolean safe, boolean mutable) {
     	super(d);
     	this.mutable_ = mutable;
     	int count =  d.region.size();
         this.safe_ = safe;
+        
+        if(Configuration.isMultiNodeVM()){
+        	System.out.println("In float constructor C");
+        	System.out.println("distribution:"+d);
+        	System.out.println("region:"+d.region);
+        	// find the region applicable to this place
+        	place myPlace = Runtime.here();
+        	int currentPlaceId = myPlace.id;
+        	System.out.println("current place:"+currentPlaceId);
+        	region theRegions[] = ((Distribution_c)d).getAllocatedRegions();
+        	for(int i=0;i <theRegions.length;++i){
+        		System.out.println("place:"+i+"::"+theRegions[i]+" size:"+theRegions[i].size());
+        	}
+        	place placeCursor = place.FIRST_PLACE;
+        	do {
+        		if(placeCursor.id != myPlace.id){ 
+        			// FIXME RemotePlace remoteP = (RemotePlace)placeCursor;
+        			
+        			FatPointer tempHandle = new FatPointer(); // TODO remove once it's a parm
+        			tempHandle.setObject((Object)this);// bogus
+        			
+        			RemotePlace.runArrayConstructor(tempHandle,ElementType.FLOAT,4,d,safe,mutable,placeCursor.id);
+        		}
+        		else {// could do this but perhaps better to optimize and call directly
+        			// TODO: create array locally
+        	    	System.out.println("Need to create local storage (place="+myPlace.id+") size:4*"
+        	    			+theRegions[currentPlaceId].size()+"("+theRegions[currentPlaceId]+")");
+        	        
+        		}
+        		placeCursor = placeCursor.next();
+        	}while (placeCursor != place.FIRST_PLACE);
+        
+        }
+        
         if (!safe) {
             int rank = d.region.rank;
             int ranks[] = new int[rank];
