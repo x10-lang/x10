976d3dc50043d50ac816782b777241a99f7beeaa
Christoph von Praun <user@domain>
2005-07-04 17:05:08 +0000

    Improved reporting for BadPlaceException.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@1284 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.runtime/src/x10/lang/BadPlaceException.java
+++ x10.runtime/src/x10/lang/BadPlaceException.java
@@ -5,23 +5,26 @@ package x10.lang;
  */
 public class BadPlaceException extends x10.lang.Exception {
     private final x10.lang.Object o_;
-    private final x10.lang.place p_;
+    private final x10.lang.place pAccess_;
+    private final x10.lang.place pVar_;
 
-    public BadPlaceException(x10.lang.Object o) {
+    public BadPlaceException(x10.lang.Object o, x10.lang.place p_access) {
         o_ = o;
-        p_ = null;
+        pAccess_ = p_access;
+        pVar_ = null;
     }
     
-    public BadPlaceException(x10.lang.place p) {
+    public BadPlaceException(x10.lang.place p_var, x10.lang.place p_access) {
         o_ = null;
-        p_ = p;
+        pAccess_ = p_access;
+        pVar_ = p_var;
     }
     public String toString() {
         String ret;
         if (o_ == null) 
-            ret = "BadPlaceException(dist. array, " + "var location=" + p_ + " access at place=" + Runtime.here() + ")";
+            ret = "BadPlaceException(dist. array, " + "var location=" + pVar_ + " access at place=" + pAccess_ + ")";
         else
-            ret = "BadPlaceException(obj=" + o_ + " o.location=" + o_.getLocation() + "access at place=" + Runtime.here() + ")";
+            ret = "BadPlaceException(obj=" + o_ + " o.location=" + o_.getLocation() + "access at place=" + pAccess_ + ")";
         return ret;
     }
 }
