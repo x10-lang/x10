39e480e56bf2454807071fc62a665df324e97e57
Robert Fuhrer <rfuhrer@us.ibm.com>
2005-08-18 21:18:26 +0000

    Made Cast_c as it originally was in polyglot 1.3 by moving stuff between its typeCheck() and that of the derived class X10Cast_c.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@1376 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.compiler/src/polyglot/ext/x10/ast/X10Cast_c.java
+++ x10.compiler/src/polyglot/ext/x10/ast/X10Cast_c.java
@@ -18,18 +18,14 @@ public class X10Cast_c extends Cast_c {
         }
 
         public Node typeCheck(TypeChecker tc) throws SemanticException {
-            TypeSystem ts = tc.typeSystem();
-
-            if (! ts.isCastValid(expr.type(), castType.type())) {
-                throw new SemanticException("Cannot cast the expression of type \"" 
-                                            + expr.type() + "\" to type \"" 
-                                            + castType.type() + "\".",
-                                            position());
-            }
             Type type = castType.type();
            if (Report.should_report("debug", 5)) {
                     Report.report(5, "[Cast_c] |" + this + "|.typeCheck(...):");
                     Report.report(5, "[Cast_c] ...type=|" +  type+"|.");
+           }
+           Expr result = type(castType.type());
+           if (Report.should_report("debug", 5)) {
+           	Report.report(5, "[Cast_c] ...returning=|" +  result+"| of type=|" + result.type() + "|.");
            }
            return super.typeCheck(tc);
         }
