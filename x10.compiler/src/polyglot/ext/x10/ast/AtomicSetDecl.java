869965f8f9d68b8cd630b95a03845b99ef60e47b
Juemin Zhang <juemin@gmail.com>
2005-08-30 20:11:52 +0000

    (interface) atomic set declaration node
    git-svn-id: svn://svn.code.sf.net/p/x10/code/branches/Atomic_Location_in_X10@1420 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ x10.compiler/src/polyglot/ext/x10/ast/AtomicSetDecl.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package polyglot.ext.x10.ast;
+
+import polyglot.ast.FieldDecl;
+
+/**
+ * @author zhangy
+ *
+ */
+public interface AtomicSetDecl extends FieldDecl {
+	public String getlabel();
+}
