863c75efaf06953f8bc346315e18723198002027
Allan Kielstra <user@domain>
2005-10-05 20:33:44 +0000

    we may need to allow the creation of a new thread in order to allow the program to make progress
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@1498 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.runtime/src/x10/runtime/LocalPlace_c.java
+++ x10.runtime/src/x10/runtime/LocalPlace_c.java
@@ -298,53 +298,55 @@ public class LocalPlace_c extends Place {
 	 * @throws InterruptedException
 	 */
 	protected void execute(Runnable r) {
-		PoolRunner t;
-		synchronized(this) {
+            PoolRunner t;
+            synchronized(this) {
             
-            // if threadQueue_ is empty, we do create a new thread right away if 
-            // the more than MAX_NUM_THREADS_PER_PLACE are already created 
-            // and not all threads are blocked! 
+                // if threadQueue_ is empty, we do create a new thread right away if 
+                // the more than MAX_NUM_THREADS_PER_PLACE are already created 
+                // and not all threads are blocked! 
             
-            // The following numbers I chose are 'magic', 
-            // i.e. they seem to work resonably well on my thinkpad.
-            long time_to_wait = 20 +  
-                  ((threadInPool_ > 50) ? 1000 : 
-                  (long) Math.pow(1.15, (double) threadInPool_));
-            long start = -1;
-            long next_turn = time_to_wait;
+                // The following numbers I chose are 'magic', 
+                // i.e. they seem to work resonably well on my thinkpad.
+                long time_to_wait = 20 +  
+                    ((threadInPool_ > 50) ? 1000 : 
+                     (long) Math.pow(1.15, (double) threadInPool_));
+                long start = -1;
+                long next_turn = time_to_wait;
+                int cc = 0;
             
-            while ( threadQueue_ == null &&
-                    threadInPool_ >= Configuration.NUMBER_OF_ACTIVITIES_PER_PLACE) {
-                try {
-                    if (start == -1)                         
-                        start = System.currentTimeMillis();                    
-                    wait(next_turn);
-                } catch (Exception e) {
-                    // should not happen 
-                    assert (false);
+                while ( threadQueue_ == null &&
+                        threadInPool_ >= Configuration.NUMBER_OF_ACTIVITIES_PER_PLACE) {
+                    try {
+                        if (start == -1)                         
+                            start = System.currentTimeMillis();                    
+                        wait(next_turn);
+                    } catch (Exception e) {
+                        // should not happen 
+                        assert (false);
+                    }
+                    next_turn = (start + time_to_wait) - System.currentTimeMillis();
+                    if (next_turn < 0)                     
+                        break;
+                    if (++cc > 10) break;       // we really can't wait forever!
                 }
-                next_turn = (start + time_to_wait) - System.currentTimeMillis();
-                if (next_turn < 0)                     
-                    break;               
-            }
             
-			if (threadQueue_ == null) {
-                threadInPool_ ++;
-				t = new PoolRunner(this);               
-				t.setDaemon(false);
-				t.start();
-				if (Report.should_report("activity", 5)) {
-					Report.report(5, PoolRunner.logString() + "LocalPlace starts " 
-							+ (t.isDaemon() ? "" : "non") + "daemon thread " + t 
-							+ "in group " + Thread.currentThread().getThreadGroup() 
-							+ ".");
-				}				
-			} else {
-				t = threadQueue_;
-				threadQueue_ = t.next;
-			}
-        }
-		t.run(r);
+                if (threadQueue_ == null) {
+                    threadInPool_ ++;
+                    t = new PoolRunner(this);               
+                    t.setDaemon(false);
+                    t.start();
+                    if (Report.should_report("activity", 5)) {
+                        Report.report(5, PoolRunner.logString() + "LocalPlace starts " 
+                                      + (t.isDaemon() ? "" : "non") + "daemon thread " + t 
+                                      + "in group " + Thread.currentThread().getThreadGroup() 
+                                      + ".");
+                    }				
+                } else {
+                    t = threadQueue_;
+                    threadQueue_ = t.next;
+                }
+            }
+            t.run(r);
 	}
 	
 	/**
