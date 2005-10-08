88f993134b64e2bf4dfe03cd9eebfa90387c90cd
Chris Donawa <donawa@domain>
2005-10-08 02:39:31 +0000

    add placeowner members
    git-svn-id: svn://svn.code.sf.net/p/x10/code@1504 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.runtime/src/x10/lang/x10Array.java
+++ trunk/x10.runtime/src/x10/lang/x10Array.java
@@ -1,7 +1,16 @@
 package x10.lang;
+import x10.lang.place;
 
 /* root for all array classes */
 abstract public class x10Array implements Indexable, Unsafe {
+
+	// used to associate a partitioned global array with a
+	// particular place
+        private x10.lang.place _owningPlace; 
+	public place getOwningPlace() {assert (_owningPlace != null);return _owningPlace;}
+	public void setOwningPlace(place id) { _owningPlace = id;}
+
 	// copy src onto dest over regions on the this place
 	abstract public dist getDistribution();
+
 }
