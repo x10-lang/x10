03f10ca8c412010fcf7aace63cbe35385129133e
Robert Fuhrer <rfuhrer@us.ibm.com>
2005-11-29 22:13:31 +0000

    Now dispatches to right method for "enhanced for loops".
    git-svn-id: svn://svn.code.sf.net/p/x10/code@1629 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/com.ibm.wala.cast.x10/src/com/ibm/wala/cast/x10/translator/polyglot/X10ASTTraverser.java
+++ trunk/com.ibm.wala.cast.x10/src/com/ibm/wala/cast/x10/translator/polyglot/X10ASTTraverser.java
@@ -6,6 +6,7 @@ package com.ibm.domo.ast.x10.translator.polyglot;
 import polyglot.ast.Node;
 import polyglot.ext.x10.ast.Async;
 import polyglot.ext.x10.ast.Finish;
+import polyglot.ext.x10.ast.ForLoop_c;
 import polyglot.ext.x10.ast.Future;
 import polyglot.ext.x10.ast.Here;
 
@@ -25,6 +26,8 @@ public class X10ASTTraverser extends ASTTraverser {
 	    return xtv.visit((Future) n, wc);
 	else if (n instanceof Finish)
 	    return xtv.visit((Finish) n, wc);
+	else if (n instanceof ForLoop_c)
+	    return xtv.visit((ForLoop_c) n, wc);
 	else
 	    return ASTTraverser.visit(n, xtv, wc);
     }
