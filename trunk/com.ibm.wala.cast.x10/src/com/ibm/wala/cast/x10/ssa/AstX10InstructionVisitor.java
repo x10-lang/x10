d2b5615171fa3bd5b3f39a0b305337491df6d749
Robert Fuhrer <rfuhrer@us.ibm.com>
2005-11-29 22:11:04 +0000

    Added visitor methods for new IR instruction types.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@1624 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/com.ibm.wala.cast.x10/src/com/ibm/wala/cast/x10/ssa/AstX10InstructionVisitor.java
+++ trunk/com.ibm.wala.cast.x10/src/com/ibm/wala/cast/x10/ssa/AstX10InstructionVisitor.java
@@ -11,4 +11,10 @@ public interface AstX10InstructionVisitor extends AstJavaInstructionVisitor {
     void visitFinish(SSAFinishInstruction instruction);
 
     void visitForce(SSAForceInstruction instruction);
+
+    void visitRegionIterInit(SSARegionIterInitInstruction instruction);
+
+    void visitRegionIterHasNext(SSARegionIterHasNextInstruction instruction);
+
+    void visitRegionIterNext(SSARegionIterNextInstruction instruction);
 }
