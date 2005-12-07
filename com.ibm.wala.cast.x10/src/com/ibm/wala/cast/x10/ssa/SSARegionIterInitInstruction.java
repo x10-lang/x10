1eb6f6296ec1cff9f800efa554fbd2941dec87bd
Julian Dolby <dolby@us.ibm.com>
2005-12-07 15:35:24 +0000

    refactor to use new abstract unary operation stuff
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@1660 a326200e-df2c-42e0-81fa-597e909af41d

--- com.ibm.wala.cast.x10/src/com/ibm/wala/cast/x10/ssa/SSARegionIterInitInstruction.java
+++ com.ibm.wala.cast.x10/src/com/ibm/wala/cast/x10/ssa/SSARegionIterInitInstruction.java
@@ -6,17 +6,14 @@ import com.ibm.domo.ssa.SymbolTable;
 import com.ibm.domo.ssa.ValueDecorator;
 import com.ibm.domo.types.TypeReference;
 
-public class SSARegionIterInitInstruction extends SSAInstruction {
-    private final int regionVal;
-    private final int iterVal;
+public class SSARegionIterInitInstruction extends SSAAbstractUnaryInstruction {
 
     public SSARegionIterInitInstruction(int iterVal, int regionVal) {
-	this.regionVal= regionVal;
-	this.iterVal= iterVal;
+      super(iterVal, regionVal);
     }
 
     public SSAInstruction copyForSSA(int[] defs, int[] uses) {
-	return new SSARegionIterInitInstruction((defs != null ? defs[0] : iterVal), (uses != null ? uses[0] : regionVal));
+	return new SSARegionIterInitInstruction((defs != null ? defs[0] : getDef(0)), (uses != null ? uses[0] : getUse(0)));
     }
 
     public String toString(SymbolTable symbolTable, ValueDecorator d) {
@@ -27,41 +24,7 @@ public class SSARegionIterInitInstruction extends SSAInstruction {
 	((AstX10InstructionVisitor) v).visitRegionIterInit(this);
     }
 
-    public int hashCode() {
-	return (49 + (iterVal * 13)) * 11 + regionVal;
-    }
-
     public TypeReference[] getExceptionTypes() {
 	return null;
-    }
-
-    public boolean isFallThrough() {
-	return true;
-    }
-
-    public int getDef() {
-        return iterVal;
-    }
-
-    public int getDef(int i) {
-	Assertions._assert(i == 0, "invalid def index for regionIterator instruction");
-        return iterVal;
-    }
-
-    public int getNumberOfDefs() {
-        return 1;
-    }
-
-    public boolean hasDef() {
-        return true;
-    }
-
-    public int getUse(int j) {
-	Assertions._assert(j == 0, "invalid use index for regionIterator instruction");
-	return regionVal;
-    }
-
-    public int getNumberOfUses() {
-        return 1;
     }
 }
--- com.ibm.wala.cast.x10/src/com/ibm/wala/cast/x10/ssa/SSARegionIterNextInstruction.java
+++ com.ibm.wala.cast.x10/src/com/ibm/wala/cast/x10/ssa/SSARegionIterNextInstruction.java
@@ -6,18 +6,14 @@ import com.ibm.domo.ssa.SymbolTable;
 import com.ibm.domo.ssa.ValueDecorator;
 import com.ibm.domo.types.TypeReference;
 
-public class SSARegionIterNextInstruction extends SSAInstruction {
+public class SSARegionIterNextInstruction extends SSAAbstractUnaryInstruction {
 
-    private final int regionIter;
-    private final int nextValue;
-
-    public SSARegionIterNextInstruction(int hasNextValue, int regionIter) {
-	this.regionIter= regionIter;
-	this.nextValue= hasNextValue;
+    public SSARegionIterNextInstruction(int nextValue, int regionIter) {
+	super(nextValue, regionIter);
     }
 
     public SSAInstruction copyForSSA(int[] defs, int[] uses) {
-	return new SSARegionIterNextInstruction((defs != null ? defs[0] : nextValue), (uses != null ? uses[0] : regionIter));
+	return new SSARegionIterNextInstruction((defs != null ? defs[0] : getDef(0)), (uses != null ? uses[0] : getUse(0)));
     }
 
     public String toString(SymbolTable symbolTable, ValueDecorator d) {
@@ -28,41 +24,7 @@ public class SSARegionIterNextInstruction extends SSAInstruction {
 	((AstX10InstructionVisitor) v).visitRegionIterNext(this);
     }
 
-    public int hashCode() {
-	return (49 + (nextValue * 13)) * 11 + regionIter;
-    }
-
     public TypeReference[] getExceptionTypes() {
 	return null;
-    }
-
-    public boolean isFallThrough() {
-	return true;
-    }
-
-    public int getDef() {
-        return nextValue;
-    }
-
-    public int getDef(int i) {
-	Assertions._assert(i == 0, "invalid def index for regionIteratorNext instruction");
-        return nextValue;
-    }
-
-    public int getNumberOfDefs() {
-        return 1;
-    }
-
-    public boolean hasDef() {
-        return true;
-    }
-
-    public int getUse(int j) {
-	Assertions._assert(j == 0, "invalid use index for regionIteratorNext instruction");
-	return regionIter;
-    }
-
-    public int getNumberOfUses() {
-        return 1;
     }
 }
