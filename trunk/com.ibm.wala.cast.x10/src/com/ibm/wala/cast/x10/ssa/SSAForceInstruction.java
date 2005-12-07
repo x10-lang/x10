1e0970fff5fbecb3322f86ee3fdfed7da208c117
Julian Dolby <dolby@us.ibm.com>
2005-12-07 15:35:24 +0000

    refactor to use new abstract unary operation stuff
    git-svn-id: svn://svn.code.sf.net/p/x10/code@1660 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/com.ibm.wala.cast.x10/src/com/ibm/wala/cast/x10/ssa/SSAForceInstruction.java
+++ trunk/com.ibm.wala.cast.x10/src/com/ibm/wala/cast/x10/ssa/SSAForceInstruction.java
@@ -6,20 +6,17 @@ import com.ibm.domo.ssa.SymbolTable;
 import com.ibm.domo.ssa.ValueDecorator;
 import com.ibm.domo.types.TypeReference;
 
-public class SSAForceInstruction extends SSAInstruction {
+public class SSAForceInstruction extends SSAAbstractUnaryInstruction {
 
-    private final int retValue;
-    private final int targetValue;
     private final TypeReference valueType;
 
     public SSAForceInstruction(int retValue, int targetValue, TypeReference valueType) {
-	this.retValue= retValue;
-	this.targetValue= targetValue;
+	suer(retValue, targetValue);
 	this.valueType= valueType;
     }
 
     public SSAInstruction copyForSSA(int[] defs, int[] uses) {
-	return new SSAForceInstruction((defs != null) ? defs[0] : retValue, (uses != null) ? uses[0] : targetValue, valueType);
+	return new SSAForceInstruction((defs != null) ? defs[0] : getDef(0), (uses != null) ? uses[0] : getUse(0), valueType);
     }
 
     public String toString(SymbolTable symbolTable, ValueDecorator d) {
@@ -30,45 +27,11 @@ public class SSAForceInstruction extends SSAInstruction {
 	((AstX10InstructionVisitor) v).visitForce(this);
     }
 
-    public int hashCode() {
-	return (49 + (retValue * 13)) * 11 + targetValue ;
-    }
-
     public TypeReference[] getExceptionTypes() {
 	return null;
     }
 
-    public boolean isFallThrough() {
-	return true;
-    }
-
     public TypeReference getValueType() {
         return valueType;
-    }
-
-    public int getDef() {
-        return retValue;
-    }
-
-    public int getDef(int i) {
-	Assertions._assert(i == 0, "invalid def index for force instruction");
-        return retValue;
-    }
-
-    public boolean hasDef() {
-        return true;
-    }
-
-    public int getNumberOfDefs() {
-        return 1;
-    }
-
-    public int getUse(int j) {
-	Assertions._assert(j == 0, "invalid use index for force instruction");
-	return targetValue;
-    }
-
-    public int getNumberOfUses() {
-        return 1;
     }
 }
--- trunk/com.ibm.wala.cast.x10/src/com/ibm/wala/cast/x10/ssa/SSARegionIterHasNextInstruction.java
+++ trunk/com.ibm.wala.cast.x10/src/com/ibm/wala/cast/x10/ssa/SSARegionIterHasNextInstruction.java
@@ -1,67 +1,28 @@
 package com.ibm.domo.ast.x10.ssa;
 
 import com.ibm.capa.impl.debug.Assertions;
-import com.ibm.domo.ssa.SSAInstruction;
-import com.ibm.domo.ssa.SymbolTable;
-import com.ibm.domo.ssa.ValueDecorator;
+import com.ibm.domo.ssa.*;
 import com.ibm.domo.types.TypeReference;
 
-public class SSARegionIterHasNextInstruction extends SSAInstruction {
-    private final int regionIter;
-    private final int hasNextValue;
+public class SSARegionIterHasNextInstruction extends SSAAbstractUnaryInstruction {
 
     public SSARegionIterHasNextInstruction(int hasNextValue, int regionIter) {
-	this.regionIter= regionIter;
-	this.hasNextValue= hasNextValue;
+	super(hasNextValue, regionIter);
     }
 
     public SSAInstruction copyForSSA(int[] defs, int[] uses) {
-	return new SSARegionIterHasNextInstruction((defs != null ? defs[0] : hasNextValue), (uses != null ? uses[0] : regionIter));
+	return new SSARegionIterHasNextInstruction((defs != null ? defs[0] : getDef(0)), (uses != null ? uses[0] : getUse(0)));
     }
 
     public String toString(SymbolTable symbolTable, ValueDecorator d) {
-	return getValueString(symbolTable, d, hasNextValue) + " = regionIterHasNext(" + getValueString(symbolTable, d, regionIter) + ")";
+	return getValueString(symbolTable, d, getDef(0)) + " = regionIterHasNext(" + getValueString(symbolTable, d, getUse(0)) + ")";
     }
 
     public void visit(Visitor v) {
 	((AstX10InstructionVisitor) v).visitRegionIterHasNext(this);
     }
 
-    public int hashCode() {
-	return (49 + (hasNextValue * 13)) * 11 + regionIter;
-    }
-
     public TypeReference[] getExceptionTypes() {
 	return null;
-    }
-
-    public boolean isFallThrough() {
-	return true;
-    }
-
-    public int getDef() {
-        return hasNextValue;
-    }
-
-    public int getDef(int i) {
-	Assertions._assert(i == 0, "invalid def index for regionIteratorHasNext instruction");
-        return hasNextValue;
-    }
-
-    public int getNumberOfDefs() {
-        return 1;
-    }
-
-    public boolean hasDef() {
-        return true;
-    }
-
-    public int getUse(int j) {
-	Assertions._assert(j == 0, "invalid use index for regionIteratorHasNext instruction");
-	return regionIter;
-    }
-
-    public int getNumberOfUses() {
-        return 1;
     }
 }
