95efb9ab6d561680e3b0453bd8f00e2ce3ff45cb
Robert Fuhrer <rfuhrer@us.ibm.com>
2005-12-08 17:07:04 +0000

    Added static TypeReference/TypeName fields for x10.lang.place.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@1673 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/com.ibm.wala.cast.x10/src/com/ibm/wala/cast/x10/types/X10TypeReference.java
+++ trunk/com.ibm.wala.cast.x10/src/com/ibm/wala/cast/x10/types/X10TypeReference.java
@@ -8,4 +8,8 @@ public class X10TypeReference {
     private final static TypeName x10LangPointName = TypeName.string2TypeName("Lx10/lang/point");
     public final static TypeReference x10LangPoint =
       TypeReference.findOrCreate(ClassLoaderReference.Primordial, x10LangPointName);
+
+    private final static TypeName x10LangPlaceName = TypeName.string2TypeName("Lx10/lang/place");
+    public final static TypeReference x10LangPlace =
+	      TypeReference.findOrCreate(ClassLoaderReference.Primordial, x10LangPlaceName);
 }
