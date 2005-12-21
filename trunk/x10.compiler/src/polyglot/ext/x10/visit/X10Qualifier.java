0f56e4c77b9c858628a5557afb3f40c841b11211
Igor Peshansky <igorp@us.ibm.com>
2005-12-21 19:45:14 +0000

    Only qualify "this" when templates wrap code into inner classes
    git-svn-id: svn://svn.code.sf.net/p/x10/code@1700 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.compiler/src/polyglot/ext/x10/visit/X10Qualifier.java
+++ trunk/x10.compiler/src/polyglot/ext/x10/visit/X10Qualifier.java
@@ -2,33 +2,55 @@ package polyglot.ext.x10.visit;
 
 import polyglot.ast.Special;
 import polyglot.ast.Expr;
+import polyglot.ast.Node;
 import polyglot.ast.NodeFactory;
 import polyglot.frontend.Job;
 import polyglot.types.Type;
 import polyglot.types.TypeSystem;
 import polyglot.visit.AscriptionVisitor;
+import polyglot.visit.ContextVisitor;
+import polyglot.visit.NodeVisitor;
 import polyglot.util.Position;
+import polyglot.ext.x10.ast.Async;
+import polyglot.ext.x10.ast.ForEach;
+import polyglot.ext.x10.ast.Future;
 
 
 /**
- * Visitor that inserts boxing and unboxing code into the AST.
+ * Visitor that qualifies uses of 'this' and 'super' inside Async statements.
  */
-public class X10Qualifier extends AscriptionVisitor
+public class X10Qualifier extends ContextVisitor
 {
     public X10Qualifier(Job job, TypeSystem ts, NodeFactory nf) {
         super(job, ts, nf);
     }
 
-    public Expr ascribe(Expr e, Type toType) {
-        if (e instanceof Special) {
-            Position p = e.position();
-            Special sc = (Special) e;
-            if (sc.qualifier() == null /* && sc.type() != toType */) {
-                Special ret = sc.qualifier(nf.CanonicalTypeNode(p, sc.type()));
-                return ret;
-            }
+    public static class InnerWrapperQualifier extends AscriptionVisitor {
+        public InnerWrapperQualifier(Job job, TypeSystem ts, NodeFactory nf) {
+            super(job, ts, nf);
         }
-  
-        return e;
+
+        public Expr ascribe(Expr e, Type toType) {
+            if (e instanceof Special) {
+                Position p = e.position();
+                Special sc = (Special) e;
+                if (sc.qualifier() == null /* && sc.type() != toType */) {
+                    //System.err.println("Setting qualifier on 'this'");
+                    Special ret = sc.qualifier(nf.CanonicalTypeNode(p, sc.type()));
+                    return ret;
+                }
+            }
+
+            return e;
+        }
+    }
+
+    protected NodeVisitor enterCall(Node parent, Node n) {
+        if (parent instanceof Async || parent instanceof Future ||
+	    parent instanceof ForEach)
+	{
+            return new InnerWrapperQualifier(job, ts, nf).context(context);
+        }
+        return this;
     }
 }
