8444708dfaad82b0d53dc8bea15cb8a385705f2f
Igor Peshansky <igorp@us.ibm.com>
2005-12-24 02:03:47 +0000

    Change X10Qualifier.InnerWrapperQualifier to a ContextVisitor
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@1714 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.compiler/src/polyglot/ext/x10/visit/X10Qualifier.java
+++ x10.compiler/src/polyglot/ext/x10/visit/X10Qualifier.java
@@ -5,6 +5,7 @@ import polyglot.ast.Expr;
 import polyglot.ast.Node;
 import polyglot.ast.NodeFactory;
 import polyglot.frontend.Job;
+import polyglot.types.SemanticException;
 import polyglot.types.Type;
 import polyglot.types.TypeSystem;
 import polyglot.visit.AscriptionVisitor;
@@ -25,30 +26,31 @@ public class X10Qualifier extends ContextVisitor
         super(job, ts, nf);
     }
 
-    public static class InnerWrapperQualifier extends AscriptionVisitor {
+    public static class InnerWrapperQualifier extends ContextVisitor {
         public InnerWrapperQualifier(Job job, TypeSystem ts, NodeFactory nf) {
             super(job, ts, nf);
         }
 
-        public Expr ascribe(Expr e, Type toType) {
-            if (e instanceof Special) {
-                Position p = e.position();
-                Special sc = (Special) e;
-                if (sc.qualifier() == null /* && sc.type() != toType */) {
+        public Node leaveCall(Node old, Node n, NodeVisitor v)
+            throws SemanticException
+        {
+            if (n instanceof Special) {
+                Position p = n.position();
+                Special sc = (Special) n;
+                if (sc.qualifier() == null) {
                     //System.err.println("Setting qualifier on 'this'");
                     Special ret = sc.qualifier(nf.CanonicalTypeNode(p, sc.type()));
                     return ret;
                 }
             }
-
-            return e;
+            return n;
         }
     }
 
     protected NodeVisitor enterCall(Node parent, Node n) {
         if (parent instanceof Async || parent instanceof Future ||
-	    parent instanceof ForEach)
-	{
+            parent instanceof ForEach)
+        {
             return new InnerWrapperQualifier(job, ts, nf).context(context);
         }
         return this;
