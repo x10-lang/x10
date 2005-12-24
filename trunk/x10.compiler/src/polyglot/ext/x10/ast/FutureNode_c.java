721bca083266ceda102459db7a72811d80ba19fa
Igor Peshansky <igorp@us.ibm.com>
2005-12-24 02:03:37 +0000

    Assign initial type
    git-svn-id: svn://svn.code.sf.net/p/x10/code@1709 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.compiler/src/polyglot/ext/x10/ast/FutureNode_c.java
+++ trunk/x10.compiler/src/polyglot/ext/x10/ast/FutureNode_c.java
@@ -1,7 +1,7 @@
 /*
  * Created on Nov 26, 2004
  *
- * 
+ *
  */
 package polyglot.ext.x10.ast;
 
@@ -27,12 +27,8 @@ import polyglot.visit.PrettyPrinter;
 import polyglot.main.Report;
 
 
-
-
 /** A FutureNode is an TypeNode that has been marked with a future
  * qualifier.  Note that the base TypeNode may be ambiguous and may need to be resolved.
- * TODO: Check if the buildTypes phase should be used on this node.
- * TODO: Find a better way of handling the constructin of FutureType.
  * @author vj
  *
  */
@@ -40,99 +36,94 @@ public class FutureNode_c extends TypeNode_c implements FutureNode {
 
 	// Recall the field this.type is defined at the supertype.
 	// The Typenode representing the argument type X.
-    protected TypeNode base;
+	protected TypeNode base;
 
-    public FutureNode_c(Position pos, TypeNode base) {
-    	super( pos );
-    	this.base = base;
-    }
+	public FutureNode_c(Position pos, TypeNode base) {
+		super( pos );
+		this.base = base;
+	}
 
+	public TypeNode base() {
+		return this.base;
+	}
 
-    public TypeNode base() {
-    	return this.base;
-    }
-    public FutureNode_c base( TypeNode base ) {
-    	FutureNode_c n = (FutureNode_c) copy();
-    	n.base = base;
-    	return n;
-    }
+	public FutureNode_c base( TypeNode base ) {
+		FutureNode_c n = (FutureNode_c) copy();
+		n.base = base;
+		return n;
+	}
 
-  protected FutureNode_c reconstruct( TypeNode base ) {
-      return (base != this.base) ? base( base ) :  this;
-  }
+	protected FutureNode_c reconstruct( TypeNode base ) {
+		return (base != this.base) ? base( base ) :  this;
+	}
 
-    // Fri Nov 26 22:31:31 2004 vj I dont understand yet what this does...
-    public Node buildTypes(TypeBuilder tb) throws SemanticException {
-    	/*
-    	Report.report(5,"[FutureNode_c] Building type |" + this + "|:");
-    	// new SemanticException("", position()).printStackTrace( System.out);
-    	Type myType = tb.typeSystem().unknownType(position());
-		Report.report(5,"[FutureNode_c] ... yields type |" + myType + "|(hashcode:"+ myType.hashCode()+ ").");
-    	Node result = type( myType );
-    	*/ 
-    	return this;
-    }
+	public Node buildTypes(TypeBuilder tb) throws SemanticException {
+		X10TypeSystem ts = (X10TypeSystem) tb.typeSystem();
+		return type(ts.unknownType(position()));
+	}
 
+	public Node visitChildren( NodeVisitor v ) {
+		TypeNode base = (TypeNode) visitChild(this.base, v);
+		return reconstruct( base );
+	}
 
-    public Node visitChildren( NodeVisitor v ) {
-    	TypeNode base = (TypeNode) visitChild( this.base, v);
-    	return reconstruct( base );
-    }
+	public Node disambiguate(AmbiguityRemover sc) throws SemanticException {
+		// Report.report(5,"[FutureNode_c] Disambiguating |" + this + "|(#" + this.hashCode() +"):");
+		TypeNode newType = (TypeNode) base.disambiguate( sc );
+		// Report.report(5,"[FutureNode_c] ... yields type |" + type + "|.");
+		Type baseType = newType.type();
 
-    public Node disambiguate(AmbiguityRemover sc) throws SemanticException {
-    	// Report.report(5,"[FutureNode_c] Disambiguating |" + this + "|(#" + this.hashCode() +"):");
-    	TypeNode newType = (TypeNode) base.disambiguate( sc );
-    	// Report.report(5,"[FutureNode_c] ... yields type |" + type + "|.");
-    	Type baseType = newType.type();
+		// RMF 11/2/2005 - Don't proceed further if the base type hasn't yet been disambiguated...
+		if (!baseType.isCanonical())
+			return this;
 
-    	// RMF 11/2/2005 - Don't proceed further if the base type hasn't yet been disambiguated...
-    	if (!baseType.isCanonical())
-    	    return this;
-
-    	if (null == baseType ) {
-    		throw new SemanticException("The type constructor future cannot be applied to a <null> type", 
-        			position());
-    	}
-	X10TypeSystem ts = (X10TypeSystem) baseType.typeSystem();
-	this.type = ts.createFutureType( position(), baseType );
-    	Node result = reconstruct( newType );
-    	// Report.report(5,"[FutureNode_c] ... returns |" + result +"|(#" + result.hashCode() +").");
-    	return result; 
-       }
+		if (null == baseType) {
+			throw new SemanticException("The type constructor future cannot be applied to a <null> type",
+					position());
+		}
+		X10TypeSystem ts = (X10TypeSystem) baseType.typeSystem();
+		this.type = ts.createFutureType( position(), baseType );
+		Node result = reconstruct( newType );
+		// Report.report(5,"[FutureNode_c] ... returns |" + result +"|(#" + result.hashCode() +").");
+		return result;
+	}
 
     /**
-     * Typecheck the type-argument. 
+     * Typecheck the type-argument.
      */
-    public Node typeCheck( TypeChecker tc) throws SemanticException {
-    	// Report.report(5,"[FutureNode_c] Type checking |" + this +"|:");
-    	Node n = base.typeCheck( tc );
-    	// Report.report(5,"[FutureNode_c] ... yields node |" + n +"|.");
-    	if (n instanceof TypeNode) {
-    	 TypeNode arg = (TypeNode) n;
-    	 Type argType = arg.type();
+	public Node typeCheck( TypeChecker tc) throws SemanticException {
+		// Report.report(5,"[FutureNode_c] Type checking |" + this +"|:");
+		Node n = base.typeCheck( tc );
+		// Report.report(5,"[FutureNode_c] ... yields node |" + n +"|.");
+		if (n instanceof TypeNode) {
+			TypeNode arg = (TypeNode) n;
+			Type argType = arg.type();
 
-	 X10TypeSystem ts = (X10TypeSystem) argType.typeSystem();
-	 this.type = ts.createFutureType( position(), argType );
+			X10TypeSystem ts = (X10TypeSystem) argType.typeSystem();
+			this.type = ts.createFutureType( position(), argType );
 
-    	 // this.base = null?
-		 // Report.report(5, "[FutureNode_c] ... sets type to |" + this.type + "|.");
-		 // Report.report(5, "[FutureNode_c] ... returns |" + this + "|.");
-    	 return this;
-    	}
-    	throw new SemanticException("Argument to future type-constructor does not type-check" + position());
-    }
+			// this.base = null?
+			// Report.report(5, "[FutureNode_c] ... sets type to |" + this.type + "|.");
+			// Report.report(5, "[FutureNode_c] ... returns |" + this + "|.");
+			return this;
+		}
+		throw new SemanticException("Argument to future type-constructor does not type-check" + position());
+	}
 
-    public String toString() {
-    	return  this.type.toString();
-  }
-    /**
-     * Write out Java code for this node.
-     */
-    public void prettyPrint(CodeWriter w, PrettyPrinter ignore) {             
-        w.write(this.type.toString());
-      } 
+	public String toString() {
+		if (this.type == null)
+			return "unknown";
+		return this.type.toString();
+	}
 
-    // translate??
-    // prettyPrint?
-    // dump?
+	/**
+	 * Write out Java code for this node.
+	 */
+	public void prettyPrint(CodeWriter w, PrettyPrinter ignore) {
+		w.write(this.type.toString());
+	}
+
+	// translate??
+	// prettyPrint?
+	// dump?
 }
