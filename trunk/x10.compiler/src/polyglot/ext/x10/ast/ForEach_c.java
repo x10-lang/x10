a7fcb2e59e6177a3c6b067d360b3b6f2170d753a
Igor Peshansky <igorp@us.ibm.com>
2005-12-25 19:15:57 +0000

    Copy the clock list Visit the clock list in visitChildren
    git-svn-id: svn://svn.code.sf.net/p/x10/code@1720 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.compiler/src/polyglot/ext/x10/ast/ForEach_c.java
+++ trunk/x10.compiler/src/polyglot/ext/x10/ast/ForEach_c.java
@@ -1,11 +1,12 @@
 /*
  * Created by vj on Dec 9, 2004
  *
- * 
+ *
  */
 package polyglot.ext.x10.ast;
 
 import java.util.List;
+import java.util.Collections;
 
 import polyglot.ast.Expr;
 import polyglot.ast.Node;
@@ -13,6 +14,7 @@ import polyglot.ast.Stmt;
 import polyglot.ast.Formal;
 import polyglot.ext.jl.ast.Field_c;
 import polyglot.util.Position;
+import polyglot.util.TypedList;
 
 import polyglot.util.CodeWriter;
 import polyglot.visit.CFGBuilder;
@@ -24,21 +26,21 @@ import polyglot.types.SemanticException;
 import polyglot.types.TypeSystem;
 import polyglot.types.Type;
 
-/** An immutable representation of the X10 statement: foreach (i : D) S 
+/** An immutable representation of the X10 statement: foreach (i : D) S
  * @author vj Dec 9, 2004
  * @author Christian Grothoff
  */
 public class ForEach_c extends X10Loop_c implements ForEach, Clocked {
-	
-    protected List clocks;
-        
+
+	protected List clocks;
+
 	/**
 	 * @param pos
 	 */
 	public ForEach_c(Position pos) {
 		super(pos);
 	}
-	
+
 	/**
 	 * @param pos
 	 * @param formal
@@ -47,26 +49,26 @@ public class ForEach_c extends X10Loop_c implements ForEach, Clocked {
 	 */
 	public ForEach_c(Position pos, Formal formal, Expr domain, List clocks, Stmt body) {
 		super(pos, formal, domain, body);
-		this.clocks = clocks;
+		this.clocks = TypedList.copyAndCheck(clocks, Expr.class, true);
 	}
-	
-    /** Expression */
-    public List clocks() {
-        return this.clocks;
-    }
-    
-    /** clock */
-    public Clocked expr(List clocks) {
-        ForEach_c n = (ForEach_c) copy();
-        n.clocks = clocks;
-        return n;
-    }
-    
-	
+
+	/** Expression */
+	public List clocks() {
+		return Collections.unmodifiableList(this.clocks);
+	}
+
+	/** clock */
+	public Clocked expr(List clocks) {
+		ForEach_c n = (ForEach_c) copy();
+		n.clocks = TypedList.copyAndCheck(clocks, Expr.class, true);
+		return n;
+	}
+
+
 	public String toString() {
 		return "foreach (" + formal + ":" + domain + ")" + body;
 	}
-	
+
 	public void prettyPrint( CodeWriter w, PrettyPrinter tr ) {
 		w.write("foreach(");
 		printBlock(formal, w, tr);
@@ -75,10 +77,11 @@ public class ForEach_c extends X10Loop_c implements ForEach, Clocked {
 		w.write(") ");
 		printSubStmt(body, w, tr);
 	}
-	public Node visitChildren( NodeVisitor v) {
+	public Node visitChildren(NodeVisitor v) {
 		Formal formal = (Formal) visitChild(this.formal, v);
 		Expr domain = (Expr) visitChild(this.domain, v);
+		List clocks = visitList(this.clocks, v);
 		Stmt body = (Stmt) visitChild(this.body, v);
-		return reconstruct( formal, domain, body);
+		return ((Clocked) reconstruct(formal, domain, body)).expr(clocks);
 	}
 }
