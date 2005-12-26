d06c6a4d171e0a2fae9017e95e19e126023bf29f
Igor Peshansky <igorp@us.ibm.com>
2005-12-26 00:23:57 +0000

    Make async a separate scope
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@1723 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.compiler/src/polyglot/ext/x10/ast/Async_c.java
+++ x10.compiler/src/polyglot/ext/x10/ast/Async_c.java
@@ -18,14 +18,18 @@ import polyglot.visit.NodeVisitor;
 import polyglot.visit.TypeChecker;
 import polyglot.visit.AscriptionVisitor;
 import polyglot.visit.PrettyPrinter;
+import polyglot.types.Context;
 import polyglot.types.SemanticException;
 import polyglot.types.TypeSystem;
 import polyglot.types.Type;
 import java.util.LinkedList;
 import polyglot.util.TypedList;
 import java.util.ArrayList;
+import polyglot.main.Report;
+import java.util.Collection;
+import polyglot.util.CollectionUtil;
 
-/*
+/**
  * Created on Oct 5, 2004
 
  * @author Christian Grothoff
@@ -33,62 +37,62 @@ import java.util.ArrayList;
  * @author vj
  */
 
-public class Async_c extends Stmt_c 
+public class Async_c extends Stmt_c
       implements Async, Clocked {
-	
-	public Expr place; 
+
+	public Expr place;
 	public Stmt body;
     protected List clocks;
-	
+
 	public Async_c( Position pos, Expr place, List clocks, Stmt body ) {
 		super( pos );
 		this.place = place;
 		this.clocks = clocks;
 		this.body = body;
 	}
-	
+
 	public Async_c(Position p) {
 		super(p);
 	}
-	
+
 	/* (non-Javadoc)
 	 * @see polyglot.ext.x10.ast.Future#body()
 	 */
 	public Stmt body() {
 		return body;
 	}
-	
+
     /** Expression */
     public List clocks() {
         return this.clocks;
     }
-    
+
     /** clock */
     public Clocked expr(List clocks) {
         Async_c n = (Async_c) copy();
         n.clocks = clocks;
         return n;
     }
-    
-	/** Set the body of the statement. 
+
+	/** Set the body of the statement.
 	 */
 	public Async body(Stmt body) {
 		Async_c n = (Async_c) copy();
 		n.body = body;
 		return n;
 	}
-	
+
 	/** Get the RemoteActivity's place. */
 	public Expr place() {
 		return place;
 	}
-	
+
 	/** Set the RemoteActivity's place. */
 	public RemoteActivityInvocation place(Expr place) {
 		this.place = place;
 		return this;
 	}
-	
+
 	/** Reconstruct the statement. */
 	protected Async reconstruct( Expr place, Stmt body ) {
 		if ( place != this.place || body != this.body ) {
@@ -97,17 +101,25 @@ public class Async_c extends Stmt_c
 			n.body = body;
 			return n;
 		}
-		
+
 		return this;
 	}
-	
+
 	/** Visit the children of the statement. */
 	public Node visitChildren( NodeVisitor v ) {
 		Expr place = (Expr) visitChild(this.place, v);
 		Stmt body = (Stmt) visitChild(this.body, v);
 		return reconstruct(place, body);
 	}
-	
+
+	public Context enterScope(Context c) {
+		if (Report.should_report(TOPICS, 5))
+			Report.report(5, "enter async scope");
+		X10TypeSystem ts = (X10TypeSystem) c.typeSystem();
+		c = c.pushCode(ts.asyncCodeInstance());
+		return c;
+	}
+
 	/** Type check the statement. */
 	public Node typeCheck(TypeChecker tc) throws SemanticException {
 		X10TypeSystem ts = (X10TypeSystem) tc.typeSystem();
@@ -117,27 +129,27 @@ public class Async_c extends Stmt_c
 		if ( ! placeIsPlace ) {
 			newPlace = (Expr) new X10Field_c(position(), place, "location").typeCheck(tc);
 		}
-    	
+
        	return (Async_c) place(newPlace);
-		
-		
+
+
 	}
-	
+
 	// not sure how this works.. vj. Copied from Synchronized_c.
 	public Type childExpectedType(Expr child, AscriptionVisitor av) {
 		TypeSystem ts = av.typeSystem();
-		
+
 		if (child == place) {
 			return ts.Object();
 		}
-		
+
 		return child.type();
 	}
-	
+
 	public String toString() {
 		return "async (" + place + ") { ... }";
 	}
-	
+
 	/** Write the statement to an output file. */
 	public void prettyPrint(CodeWriter w, PrettyPrinter tr) {
 		w.write("async (");
@@ -145,7 +157,7 @@ public class Async_c extends Stmt_c
 		w.write(") ");
 		printSubStmt(body, w, tr);
 	}
-	
+
 	/**
 	 * Return the first (sub)term performed when evaluating this
 	 * term.
@@ -153,7 +165,7 @@ public class Async_c extends Stmt_c
 	public Term entry() {
 		return (place != null ? place.entry() : this);
 	}
-	
+
 	/**
 	 * Visit this term in evaluation order.
 	 */
@@ -162,4 +174,7 @@ public class Async_c extends Stmt_c
 		v.visitCFG(body, this);
 		return succs;
 	}
+
+	private static final Collection TOPICS =
+		CollectionUtil.list(Report.types, Report.context);
 }
