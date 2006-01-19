7d74f961d237a79a110d4abceea3219540c48e85
Igor Peshansky <igorp@us.ibm.com>
2006-01-19 05:42:17 +0000

    Move explode() methods to X10Formal_c
    git-svn-id: svn://svn.code.sf.net/p/x10/code@1778 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.compiler/src/x10/parser/X10VarDeclarator.java
+++ trunk/x10.compiler/src/x10/parser/X10VarDeclarator.java
@@ -1,99 +1,51 @@
 /*
  * Created by vj on Jan 23, 2005
- *
- * 
  */
 package x10.parser;
 
-import java.util.LinkedList;
 import java.util.List;
+import java.util.Iterator;
 
-import polyglot.ast.Expr;
-import polyglot.ast.IntLit;
-import polyglot.ast.LocalDecl;
-import polyglot.ast.Stmt;
-import polyglot.ast.TypeNode;
+import polyglot.ast.AmbExpr;
 import polyglot.ext.jl.parse.Name;
 import polyglot.parse.VarDeclarator;
 import polyglot.types.Flags;
 import polyglot.util.Position;
-import polyglot.ext.x10.ast.X10NodeFactory;
-import polyglot.ext.x10.ast.X10NodeFactory_c;
-import polyglot.ext.x10.types.X10TypeSystem;
-import polyglot.ext.x10.types.X10TypeSystem_c;
-
 
 /**
  * @author vj Jan 23, 2005
- * 
+ * @author igor Jan 13, 2006
  */
 public class X10VarDeclarator extends VarDeclarator {
-	public final List/*<Name>*/ paramList;
+	private final AmbExpr[] vars;
 	public Flags flags;
-	X10NodeFactory nf = X10NodeFactory_c.getFactory();
-	X10TypeSystem ts = X10TypeSystem_c.getFactory();
-	
-	public X10VarDeclarator(Position pos, String name ) {
+
+	public X10VarDeclarator(Position pos, String name) {
 		this(pos, name, null);
 	}
+
 	public X10VarDeclarator(Position pos, String name, List/*<Name>*/ paramList) {
 		super(pos, name);
-		this.paramList = paramList;
+		if (paramList != null) {
+			this.vars = new AmbExpr[paramList.size()];
+			for (int i = 0; i < this.vars.length; i++)
+				this.vars[i] = (AmbExpr) ((Name) paramList.get(i)).toExpr();
+		} else
+			vars = null;
 	}
+
 	public void setFlag(Flags flags) {
-		
 		boolean allCapitals = name.equals(name.toUpperCase());
 		// vj: disable until we have more support for declarative programming in X10.
-		this.flags = (false && ( allCapitals || hasExplodedVars())) ? flags.set(Flags.FINAL) : flags;
+		this.flags = (false && (allCapitals || hasExplodedVars())) ? flags.set(Flags.FINAL) : flags;
 	}
+
 	public boolean hasExplodedVars() {
-	 return paramList != null;
+		return vars != null;
 	}
-	/** Create a local variable declaration for an exploded var,
-	 *  at the given type, name and with the given initializer. The exploded variable is implicitly final.
-	 * @param flags
-	 * @param type
-	 * @param name
-	 * @param init
-	 * @return
-	 */
-	protected LocalDecl makeLocalDecl( TypeNode type, String name, Expr init ) {
-	
-		/* boolean allCapitals = name.equals(name.toUpperCase());
-		// vj: disable until we have more support for declarative programming in X10.
-		Flags f = (false || allCapitals ? flags.set(Flags.FINAL) : flags);
-		*/
-		return nf.LocalDecl(pos, flags.set(Flags.FINAL), nf.array(type, pos, dims), name, init);
-	}
-	/**
-	 * Return the initialization statements for the exploding variables.
-	 * @return
-	 */
-	public List/*<Stmt>*/ explode() {
-	    if (paramList == null) return null;
-		List stmt = new LinkedList();
-		Expr arrayBase = new Name(nf, ts, pos, name).toExpr();
-		TypeNode intType = nf.CanonicalTypeNode(pos, ts.Int());
-		for (int i=0; i < paramList.size(); i++) {
-			// int arglist(i) = name[i];
-			Name varName = (Name) paramList.get(i);
-			Expr index = nf.IntLit(varName.pos, IntLit.INT, i);
-			Expr init = nf.X10ArrayAccess1(varName.pos, arrayBase, index);
-			Stmt d = makeLocalDecl( intType, varName.name, init);
-			stmt.add(d);
-		}
-		return stmt;
-	}
-	public List/*<Stmt>*/ explode( Stmt s ) {
-		List/*<Stmt>*/ init = explode();
-		if (s != null )
-			init.add( s );
-		return init;
-	}
-	public List/*<Stmt>*/ explode( List/*<Stmt>*/ s ) {
-		List/*<Stmt>*/ init = explode();
-		if (s != null )
-			init.addAll( s );
-		return init;
+
+	public AmbExpr[] names() {
+		return vars;
 	}
 }
+
