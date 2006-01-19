6e2bec89412a1c36a8a3e3d15f2bc3202610a0a1
Igor Peshansky <igorp@us.ibm.com>
2006-01-19 05:42:03 +0000

    Change signature of explode()
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@1773 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.compiler/src/polyglot/ext/x10/ast/X10Formal.java
+++ x10.compiler/src/polyglot/ext/x10/ast/X10Formal.java
@@ -8,6 +8,8 @@ package polyglot.ext.x10.ast;
 import polyglot.ast.Formal;
 import java.util.List;
 import polyglot.ast.Stmt;
+import polyglot.ast.NodeFactory;
+import polyglot.types.TypeSystem;
 
 /**
  * @author vj Jan 23, 2005
@@ -15,12 +17,14 @@ import polyglot.ast.Stmt;
  */
 public interface X10Formal extends Formal {
    boolean hasExplodedVars();
-   /**Return a list of statements containing the initializations for the exploded vars,
+
+   /**
+    * Return a list of statements containing the initializations for the exploded vars,
     * with s appended.
     * 
     * @param s -- the list of statements to be appended.
     * @return
     */
-   List/*<Stmt>*/ explode( Stmt s );
-   List/*<Stmt>*/ explode();
+   List/*<Stmt>*/ explode(NodeFactory nf, TypeSystem ts, Stmt s);
+   List/*<Stmt>*/ explode(NodeFactory nf, TypeSystem ts);
 }
