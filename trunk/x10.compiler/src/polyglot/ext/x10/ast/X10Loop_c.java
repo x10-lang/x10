ec7a71c36bcc2838200ea9b7afa49e7a9cabd388
Igor Peshansky <igorp@us.ibm.com>
2006-01-24 00:15:54 +0000

    Make setter methods part of the interface Reformat
    git-svn-id: svn://svn.code.sf.net/p/x10/code@1798 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.compiler/src/polyglot/ext/x10/ast/X10Loop_c.java
+++ trunk/x10.compiler/src/polyglot/ext/x10/ast/X10Loop_c.java
@@ -1,7 +1,7 @@
 /*
  * Created by vj on Dec 9, 2004
  *
- * 
+ *
  */
 package polyglot.ext.x10.ast;
 
@@ -24,15 +24,15 @@ import polyglot.types.SemanticException;
 import polyglot.visit.NodeVisitor;
 import polyglot.visit.TypeChecker;
 
-
-/** Captures the commonality of for, foreach and ateach loops in X10.
+/**
+ * Captures the commonality of for, foreach and ateach loops in X10.
  * TODO:
  * (1) formal must be a variable whose type will be that of the region underlying domain.
- * (2) domain must be an array, distribution or region. If it is an array or distribution a, 
+ * (2) domain must be an array, distribution or region. If it is an array or distribution a,
  *     the system must behave as if the user typed a.region.
- * (3) Perhaps we can allow continue statements within a for loop, but not within 
+ * (3) Perhaps we can allow continue statements within a for loop, but not within
  *     a foreach or an ateach.
- * 
+ *
  * COMMENTS / TODO (added by Christian)
  * (1) for now, continue/break should work as usual
  *      for foreach;
@@ -43,32 +43,30 @@ import polyglot.visit.TypeChecker;
  *      probably ok, for now the XCD files expect to see
  *      ateach(i:D) { S } and type 'i' as 'int[]' for multi-dimensional
  *      arrays, and as 'int' for single-dimensional arrays.
- * 
+ *
  * @author vj Dec 9, 2004
- * 
  */
 public abstract class X10Loop_c extends Stmt_c implements X10Loop {
-    protected Formal formal;
-    protected Expr domain;
-    protected Stmt body;
+	protected Formal formal;
+	protected Expr domain;
+	protected Stmt body;
 
-    /**
-     * @param pos
-     */
-    protected X10Loop_c(Position pos) {
-	super(pos);
-    }
+	/**
+	 * @param pos
+	 */
+	protected X10Loop_c(Position pos) {
+		super(pos);
+	}
 
-    protected X10Loop_c( Position pos, Formal formal, Expr domain, Stmt body) {
-    	super( pos );
-    	this.formal = formal;
-    	this.domain = domain;
-    	this.body = body;
-    	
-    }
-    
+	protected X10Loop_c(Position pos, Formal formal, Expr domain, Stmt body) {
+		super(pos);
+		this.formal = formal;
+		this.domain = domain;
+		this.body = body;
+	}
+
 	/** Reconstruct the expression. */
-	protected X10Loop_c reconstruct( Formal formal, Expr domain, Stmt body ) {
+	protected X10Loop_c reconstruct(Formal formal, Expr domain, Stmt body) {
 		if (formal != this.formal || domain != this.domain || body != this.body) {
 			X10Loop_c n = (X10Loop_c) copy();
 			n.formal = formal;
@@ -78,88 +76,98 @@ public abstract class X10Loop_c extends Stmt_c implements X10Loop {
 		}
 		return this;
 	}
-    
-    /** Type check the statement. */
-    public Node typeCheck(TypeChecker tc) throws SemanticException {
-    	X10TypeSystem ts = (X10TypeSystem) tc.typeSystem();
-		
+
+	/** Type check the statement. */
+	public Node typeCheck(TypeChecker tc) throws SemanticException {
+		X10TypeSystem ts = (X10TypeSystem) tc.typeSystem();
+
 		Expr newDomain = domain;
 		X10Type domainType = (X10Type) domain.type();
 		if (domainType.isX10Array())
-			newDomain = (Expr) getDomain( domain ).typeCheck( tc );
-		
-		return domain( newDomain);
-    }
-    
-    public Expr getDomain( Expr d ) {
+			newDomain = (Expr) getDomain(domain).typeCheck(tc);
+
+		return domain(newDomain);
+	}
+
+	public Expr getDomain(Expr d) {
 		return new Field_c(position(), d, "region");
 	}
-    /* (non-Javadoc)
-     * @see polyglot.ast.Term#entry()
-     */
-    public Term entry() {
-    	return formal.entry();
-    }
-    
-    /* (non-Javadoc)
-     * @see polyglot.ast.Term#acceptCFG(polyglot.visit.CFGBuilder, java.util.List)
-     */
-    public List acceptCFG(CFGBuilder v, List succs) {
-    	v.visitCFG( formal, domain.entry());
-    	v.visitCFG( domain, body.entry());
-    	v.visitCFG( body, this);
-    	return succs;
-    }
-    
-    public Context enterScope(Context c) {
-    	return c.pushBlock();
-        }
 
-    /** Visit the children of the expression. */
+	/* (non-Javadoc)
+	 * @see polyglot.ast.Term#entry()
+	 */
+	public Term entry() {
+		return formal.entry();
+	}
+
+	/* (non-Javadoc)
+	 * @see polyglot.ast.Term#acceptCFG(polyglot.visit.CFGBuilder, java.util.List)
+	 */
+	public List acceptCFG(CFGBuilder v, List succs) {
+		v.visitCFG(formal, domain.entry());
+		v.visitCFG(domain, body.entry());
+		v.visitCFG(body, this);
+		return succs;
+	}
+
+	public Context enterScope(Context c) {
+		return c.pushBlock();
+	}
+
+	/** Visit the children of the expression. */
 	public Node visitChildren(NodeVisitor v) {
 		Formal formal = (Formal) visitChild(this.formal, v);
 		Expr domain = (Expr) visitChild(this.domain, v);
 		Stmt body = (Stmt) visitChild(this.body, v);
-		return reconstruct( formal, domain, body );
+		return reconstruct(formal, domain, body);
 	}
-    /* (non-Javadoc)
-     * @see polyglot.ext.x10.ast.X10Loop#body()
-     */
-    public Stmt body() {
-    	return this.body;
-    }
-    
-    /* (non-Javadoc)
-     * @see polyglot.ext.x10.ast.X10Loop#formal()
-     */
-    public Formal formal() {
-    	return this.formal;
-    }
-    
-    /* (non-Javadoc)
-     * @see polyglot.ext.x10.ast.X10Loop#domain()
-     */
-    public Expr domain() {
-    	return this.domain;
-    }
-    public X10Loop_c body(Stmt body) {
+
+	/* (non-Javadoc)
+	 * @see polyglot.ext.x10.ast.X10Loop#body()
+	 */
+	public Stmt body() {
+		return this.body;
+	}
+
+	/* (non-Javadoc)
+	 * @see polyglot.ext.x10.ast.X10Loop#formal()
+	 */
+	public Formal formal() {
+		return this.formal;
+	}
+
+	/* (non-Javadoc)
+	 * @see polyglot.ext.x10.ast.X10Loop#domain()
+	 */
+	public Expr domain() {
+		return this.domain;
+	}
+
+	/* (non-Javadoc)
+	 * @see polyglot.ext.x10.ast.X10Loop#body(polyglot.ast.Stmt)
+	 */
+	public X10Loop body(Stmt body) {
 		X10Loop_c n = (X10Loop_c) copy();
 		n.body = body;
 		return n;
 	}
-	
-	public X10Loop_c formal(Formal formal) {
+
+	/* (non-Javadoc)
+	 * @see polyglot.ext.x10.ast.X10Loop#formal(polyglot.ast.Formal)
+	 */
+	public X10Loop formal(Formal formal) {
 		X10Loop_c n = (X10Loop_c) copy();
 		n.formal = formal;
 		return n;
 	}
-	
-	public X10Loop_c domain(Expr domain) {
+
+	/* (non-Javadoc)
+	 * @see polyglot.ext.x10.ast.X10Loop#domain(polyglot.ast.Expr)
+	 */
+	public X10Loop domain(Expr domain) {
 		X10Loop_c n = (X10Loop_c) copy();
 		n.domain = domain;
 		return n;
 	}
-	
-    
-    
 }
+
