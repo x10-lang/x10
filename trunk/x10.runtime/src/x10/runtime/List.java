f726b06772876aa100a689437ba4603c476eb6d8
Igor Peshansky <igorp@us.ibm.com>
2006-01-26 00:12:54 +0000

    Make an abstract class instead of an interface Reformat
    git-svn-id: svn://svn.code.sf.net/p/x10/code@1814 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.runtime/src/x10/runtime/List.java
+++ trunk/x10.runtime/src/x10/runtime/List.java
@@ -1,17 +1,13 @@
 /*
  * Created by vj on May 17, 2005
- *
- * 
  */
 package x10.runtime;
 
 
-/** Needed for simplicity of code generation.
+/**
+ * Needed for simplicity of code generation.
  * @author vj May 17, 2005
- * 
  */
-public interface List {
-	
-	java.util.List run();
-
+public abstract class List {
+	public abstract java.util.List run();
 }
