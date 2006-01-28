56aea60b14c7f3a00919daaee19526f94930d5b9
Igor Peshansky <igorp@us.ibm.com>
2006-01-28 04:00:38 +0000

    Implement Loop (since Polyglot requires it) Remove unused imports Build a proper CFG Reindent
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@1827 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.compiler/src/polyglot/ext/x10/ast/ForLoop_c.java
+++ x10.compiler/src/polyglot/ext/x10/ast/ForLoop_c.java
@@ -1,45 +1,31 @@
 /*
  * Created by vj on Dec 9, 2004
- *
- * 
  */
 package polyglot.ext.x10.ast;
 
-import polyglot.ast.Expr;
-import polyglot.ast.Stmt;
-import polyglot.ast.Variable;
-import polyglot.util.Position;
+import java.util.List;
 
 import polyglot.ast.Expr;
+import polyglot.ast.Loop;
 import polyglot.ast.Stmt;
+import polyglot.ast.Term;
 import polyglot.ast.Formal;
-import polyglot.ext.jl.ast.Field_c;
 import polyglot.util.Position;
-
-import polyglot.util.CodeWriter;
 import polyglot.visit.CFGBuilder;
-import polyglot.visit.NodeVisitor;
-import polyglot.visit.TypeChecker;
-import polyglot.visit.AscriptionVisitor;
-import polyglot.visit.PrettyPrinter;
-import polyglot.types.SemanticException;
-import polyglot.types.TypeSystem;
-import polyglot.types.Type;
+import polyglot.visit.FlowGraph;
 
-
-/** An immutable representation of an X10 for loop: for (i : D) S
+/**
+ * An immutable representation of an X10 for loop: for (i : D) S
+ *
  * @author vj Dec 9, 2004
- * 
  */
-
-public class ForLoop_c extends X10Loop_c {
+public class ForLoop_c extends X10Loop_c implements Loop {
 
 	/**
 	 * @param pos
 	 */
 	public ForLoop_c(Position pos) {
 		super(pos);
-		
 	}
 
 	/**
@@ -50,8 +36,25 @@ public class ForLoop_c extends X10Loop_c {
 	 */
 	public ForLoop_c(Position pos, Formal formal, Expr domain, Stmt body) {
 		super(pos, formal, domain, body);
-		
 	}
-	
 
+	/* (non-Javadoc)
+	 * @see polyglot.ast.Term#acceptCFG(polyglot.visit.CFGBuilder, java.util.List)
+	 */
+	public List acceptCFG(CFGBuilder v, List succs) {
+		v.visitCFG(formal, domain.entry());
+		v.visitCFG(domain, FlowGraph.EDGE_KEY_TRUE, body.entry(), 
+						   FlowGraph.EDGE_KEY_FALSE, this);
+		v.push(this).visitCFG(body, continueTarget());
+		return succs;
+	}
+
+	public Term continueTarget() {
+		return formal.entry();
+	}
+
+	public Expr cond() { return null; }
+	public boolean condIsConstant() { return false; }
+	public boolean condIsConstantTrue() { return false; }
 }
+
