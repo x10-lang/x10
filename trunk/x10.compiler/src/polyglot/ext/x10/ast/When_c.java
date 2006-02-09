cdd37e0e7a77b9b438c1ddb58abd2f4adb598db7
Igor Peshansky <igorp@us.ibm.com>
2006-02-09 00:59:25 +0000

    Remove the Branch_c class Store positions, expressions, and statements in parallel lists Rename branches() to reconstruct() Do not reconstruct if using same values Rename add() to addBranch() Change control flow of the when statement Reindent
    git-svn-id: svn://svn.code.sf.net/p/x10/code@1884 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.compiler/src/polyglot/ext/x10/ast/When_c.java
+++ trunk/x10.compiler/src/polyglot/ext/x10/ast/When_c.java
@@ -26,6 +26,7 @@ import polyglot.util.Position;
 import polyglot.util.CodeWriter;
 
 import polyglot.visit.CFGBuilder;
+import polyglot.visit.FlowGraph;
 import polyglot.visit.NodeVisitor;
 import polyglot.visit.TypeChecker;
 import polyglot.visit.AscriptionVisitor;
@@ -37,167 +38,143 @@ import polyglot.visit.PrettyPrinter;
  */
 public class When_c extends Stmt_c implements CompoundStmt, When {
 
-    public static class Branch_c extends Stmt_c implements Branch {
-    	public Expr expr;
-    	public Stmt stmt;
-    	public Expr expr() { return this.expr; }
-    	public Stmt stmt() { return this.stmt; }
+	protected List/*<Position>*/ positions;
+	protected List/*<Expr>*/ exprs;
+	protected List/*<Stmt>*/ stmts;
 
-        public Branch branch(Expr expr, Stmt stmt) {
-    		if (expr != this.expr || stmt != this.stmt) {
-    			return new Branch_c(position, expr, stmt);
-    		}
-    		return this;
-    	}
+	protected Expr expr;
+	protected Stmt stmt;
 
-        public Branch_c(Position p, Expr expr, Stmt stmt) {
-    		super(p);
-            this.expr = expr;
-    		this.stmt = stmt;
-    	}
+	public Expr expr() { return this.expr; }
+	public Stmt stmt() { return this.stmt; }
 
-//      TODO: cvp -> vj implement this
-        public Node typeCheck(TypeChecker tc) throws SemanticException {
-    	    TypeSystem ts = tc.typeSystem();
-    	    return super.typeCheck(tc);
-    	}
+	public When_c(Position p, Expr expr, Stmt stmt) {
+		super(p);
+		this.expr = expr;
+		this.stmt = stmt;
+		this.positions = new TypedList(new LinkedList(), Position.class, false);
+		this.exprs = new TypedList(new LinkedList(), Expr.class, false);
+		this.stmts = new TypedList(new LinkedList(), Stmt.class, false);
+	}
 
-    	/** Visit the children of the statement. */
-    	public Node visitChildren( NodeVisitor v ) {
-    	    Expr e = (Expr) visitChild(expr, v);
-    	    Stmt s = (Stmt) visitChild(stmt, v);
-    	    return reconstruct(e, s);
-    	}
+	public void addBranch(Position p, Expr e, Stmt s) {
+		this.positions.add(p);
+		this.exprs.add(e);
+		this.stmts.add(s);
+	}
 
-    	/** Reconstruct the statement. */
-    	protected Node reconstruct( Expr e, Stmt s) {
-    	    if ( e != this.expr || s != this.stmt ) {
-    	        Branch_c n = (Branch_c) copy();
-    	        n.expr = e;
-    	        n.stmt = s;
-    	        return n;
-    	    }
-    	    return this;
-    	}
+	public List exprs() {
+		return exprs;
+	}
 
-        /**
-         * Return the first (sub)term performed when evaluating this
-         * term.
-         */
-        public Term entry() {
-            return expr.entry();
+	public List stmts() {
+		return stmts;
+	}
+
+	private boolean isSame(List a, List b) {
+		if (a.size() != b.size()) return false;
+		Iterator i = a.iterator();
+		Iterator j = b.iterator();
+		while (i.hasNext())
+			if (i.next() != j.next()) return false;
+		return true;
+	}
+
+	/** Reconstruct the statement. */
+	public When reconstruct(Expr expr, Stmt stmt, List exprs, List stmts) {
+		if (expr == this.expr && stmt == this.stmt &&
+			isSame(exprs, this.exprs) && isSame(stmts, this.stmts))
+		{
+			return this;
+		}
+		When_c n = (When_c) copy();
+		n.expr = expr;
+		n.stmt = stmt;
+		n.exprs = TypedList.copyAndCheck(exprs, Expr.class, true);
+		n.stmts = TypedList.copyAndCheck(stmts, Stmt.class, true);
+		return n;
+	}
+
+	/** Visit the children of the statement. */
+	public Node visitChildren(NodeVisitor v) {
+		Expr e = (Expr) visitChild(expr, v);
+		Stmt s = (Stmt) visitChild(stmt, v);
+		List es = visitList(exprs, v);
+		List ss = visitList(stmts, v);
+		return reconstruct(e, s, es, ss);
+	}
+
+	/** Type check the statement. */
+	// TODO: cvp -> vj implement this
+	public Node typeCheck(TypeChecker tc) throws SemanticException {
+		TypeSystem ts = tc.typeSystem();
+
+		/*
+		if (!ts.isSubtype(expr.type(), ts.Object())) {
+			throw new SemanticException(
+					"Cannot synchronize on an expression of type \"" +
+					expr.type() + "\".", expr.position());
+		}
+		*/
+		return super.typeCheck(tc);// this;
+	}
+
+	public String toString() {
+		return "when (" + expr + ")" + stmt + (exprs.size() > 0 ? "..." : "");
+	}
+
+	/** Write the statement to an output file. */
+	public void prettyPrint(CodeWriter w, PrettyPrinter tr) {
+		w.write("when (");
+		printBlock(expr, w, tr);
+		w.write(") ");
+		printSubStmt(stmt, w, tr);
+		if (exprs.size() > 0) {
+			Iterator es = exprs.iterator();
+			Iterator ss = stmts.iterator();
+			while (es.hasNext()) {
+				Expr e = (Expr) es.next();
+				Stmt s = (Stmt) ss.next();
+				w.write("or (");
+				printBlock(e, w, tr);
+				w.write(") ");
+				printSubStmt(s, w, tr);
+			}
+		}
+	}
+
+	/**
+	 * Return the first (sub)term performed when evaluating this
+	 * term.
+	 */
+	public Term entry() {
+		return expr.entry();
+	}
+
+	/**
+	 * Visit this term in evaluation order.
+	 * Each expression goes to the corresponding statement if true or to the
+	 * next expression if false.  The last expression wraps around to the
+	 * first.  Each statement goes to this "when".
+	 */
+	public List acceptCFG(CFGBuilder v, List succs) {
+		Expr e = expr;
+		Stmt s = stmt;
+		Expr ne = null;
+		Iterator es = exprs.iterator();
+		Iterator ss = stmts.iterator();
+        while (es.hasNext()) {
+			ne = (Expr) es.next();
+			v.visitCFG(e, FlowGraph.EDGE_KEY_TRUE, s.entry(),
+						  FlowGraph.EDGE_KEY_FALSE, ne);
+			v.visitCFG(s, this);
+			e = ne;
+			s = (Stmt) ss.next();
         }
-
-        /**
-         * Visit this term in evaluation order.
-         */
-        public List acceptCFG(CFGBuilder v, List succs) {
-            v.visitCFG(expr, stmt.entry());
-            v.visitCFG(stmt, this);
-            return succs;
-        }
-    }
-
-    // Optimize the implementation of single-armed whens.
-    Expr expr;
-    Stmt stmt;
-
-    public Expr expr() { return this.expr; }
-
-    public Stmt stmt() { return this.stmt; }
-
-	public /*final*/ List/*<Branches>*/ rest;
-
-
-    public When_c(Position p, Expr expr, Stmt stmt) {
-    	super(p);
-    	this.expr = expr;
-    	this.stmt = stmt;
-    	this.rest = new TypedList(new LinkedList(), Branch_c.class, false);
-    }
-
-
-    public void add( Branch b) {
-    	this.rest.add( b );
-    }
-
-    public List branches() {
-    	List result = new LinkedList();
-    	result.add(new Branch_c(position, expr, stmt));
-    	result.addAll( rest);
-    	return result;
-    }
-
-    /** Set the branches of the statement. */
-    public When branches( Expr expr, Stmt stmt, List branches ) {
-    	When_c n = (When_c) copy();
-    	n.expr = expr;
-    	n.stmt = stmt;
-    	n.rest = TypedList.copyAndCheck(branches, Branch_c.class, true);
-    	return n;
-    }
-
-    /** Visit the children of the statement. */
-    public Node visitChildren( NodeVisitor v ) {
-    	Expr e = (Expr) visitChild(expr, v);
-    	Stmt s = (Stmt) visitChild(stmt, v);
-    	List result = visitList(rest, v);
-    	return branches( e, s, result);
-    }
-
-    /** Type check the statement. */
-//  TODO: cvp -> vj implement this
-    public Node typeCheck(TypeChecker tc) throws SemanticException {
-    	TypeSystem ts = tc.typeSystem();
-
-    	/*
-    	 if (! ts.isSubtype(expr.type(), ts.Object()) ) {
-    	 throw new SemanticException(
-    	 "Cannot synchronize on an expression of type \"" +
-    	 expr.type() + "\".", expr.position());
-    	 }
-    	 */
-    	return super.typeCheck(tc);// this;
-    }
-
-
-    public String toString() {
-    	return "when (" + expr + ")" + stmt
-		+ (rest.size() > 0 ? "..." : "");
-    }
-
-    /** Write the statement to an output file. */
-    public void prettyPrint(CodeWriter w, PrettyPrinter tr) {
-    	w.write("when (");
-    	printBlock(expr, w, tr);
-    	w.write(") ");
-    	printSubStmt(stmt, w, tr);
-    	if (rest.size() > 0)
-    		for (Iterator it = rest.iterator(); it.hasNext();) {
-    			Branch_c branch = (Branch_c) it.next();
-    			w.write("or (" );
-    			printBlock( branch.expr, w, tr);
-    			w.write(") ");
-    			printSubStmt( branch.stmt, w, tr);
-    		}
-    }
-
-    /**
-     * Return the first (sub)term performed when evaluating this
-     * term.
-     */
-    public Term entry() {
-    	return expr.entry();
-    }
-
-    /**
-     * Visit this term in evaluation order.
-     */
-    public List acceptCFG(CFGBuilder v, List succs) {
-    	v.visitCFG(expr, stmt.entry());
-    	v.visitCFG(stmt, listEntry(rest, this));
-    	v.visitCFGList(rest, this);
-    	return succs;
-    }
+		v.visitCFG(e, FlowGraph.EDGE_KEY_TRUE, s.entry(),
+					  FlowGraph.EDGE_KEY_FALSE, expr);
+		v.visitCFG(s, this);
+		return succs;
+	}
 }
 
