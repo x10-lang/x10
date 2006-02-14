4585279567245cd64492c5436b5f0e05270df00d
Igor Peshansky <igorp@us.ibm.com>
2006-02-14 00:06:43 +0000

    Add base() setter Reindent
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@1892 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.compiler/src/polyglot/ext/x10/ast/NullableNode.java
+++ x10.compiler/src/polyglot/ext/x10/ast/NullableNode.java
@@ -1,19 +1,17 @@
 /*
  * Created on Nov 26, 2004
- *
- *
  */
 package polyglot.ext.x10.ast;
 
 import polyglot.ast.TypeNode;
 
-/** The AST node created for the X10 type constructor: nullable X.
- * 
- * @author vj
+/**
+ * The AST node created for the X10 type constructor: nullable X.
  *
- * 
+ * @author vj
  */
 public interface NullableNode extends TypeNode {
 	TypeNode base();
-
+	NullableNode base(TypeNode base);
 }
+
