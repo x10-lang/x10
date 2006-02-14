b5bcba3a804ce71398219e23f6159a8eaa563b6e
Igor Peshansky <igorp@us.ibm.com>
2006-02-14 00:06:45 +0000

    Allow non-reference nullable types Properly pretty-print base type node Change toString() form Reindent
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@1893 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.compiler/src/polyglot/ext/x10/ast/NullableNode_c.java
+++ x10.compiler/src/polyglot/ext/x10/ast/NullableNode_c.java
@@ -1,11 +1,10 @@
 /*
  * Created on Nov 26, 2004
- *
- * 
  */
 package polyglot.ext.x10.ast;
 
 import polyglot.ext.jl.ast.TypeNode_c;
+import polyglot.ext.x10.types.X10Type;
 import polyglot.ext.x10.types.X10TypeSystem;
 
 import polyglot.util.Position;
@@ -16,7 +15,7 @@ import polyglot.ast.Node;
 
 import polyglot.types.SemanticException;
 import polyglot.types.Type;
-import polyglot.types.ReferenceType;
+//import polyglot.types.ReferenceType;
 
 import polyglot.visit.TypeChecker;
 import polyglot.visit.NodeVisitor;
@@ -26,141 +25,146 @@ import polyglot.visit.PrettyPrinter;
 
 import polyglot.main.Report;
 
-
-
-
-/** A NullableNode is an TypeNode that has been marked with a nullable
- * qualifier.  Note that the base TypeNode may be ambiguous and may need to be resolved.
- * TODO: Find a better way of dealing with the construction of the Nullable type. In the code below,
- * this might be done twice. That doesnt seem right.
- * @author vj
+/**
+ * A NullableNode is an TypeNode that has been marked with a nullable
+ * qualifier.  Note that the base TypeNode may be ambiguous and may need to
+ * be resolved.
+ * TODO: Find a better way of dealing with the construction of the Nullable
+ * type. In the code below, this might be done twice. That doesnt seem right.
  *
+ * @author vj
  */
-public class NullableNode_c extends TypeNode_c  implements NullableNode {
+public class NullableNode_c extends TypeNode_c implements NullableNode {
 
 	// Recall the field this.type is defined at the supertype.
 	// The Typenode representing the argument type X.
-    protected TypeNode base;
+	protected TypeNode base;
 
-    public NullableNode_c(Position pos, TypeNode base) {
-    	super( pos );
-    	this.base = base;
-    }
-
-
-    public TypeNode base() {
-    	return this.base;
-    }
-    public NullableNode_c base( TypeNode base ) {
-    	NullableNode_c n = (NullableNode_c) copy();
-    	n.base = base;
-    	return n;
-    }
-
-  protected NullableNode_c reconstruct( TypeNode base ) {
-      return (base != this.base) ? base( base ) :  this;
-  }
-
-   
-    public Node visitChildren( NodeVisitor v ) {
-    	TypeNode base = (TypeNode) visitChild( this.base, v);
-    	return reconstruct( base );
-    }
-
-    /**
-     * Disambiguate the base node. Ensure that it is unambiguous and a
-     * ReferenceType. Create a NullableType_c and store it in
-     * this.type.
-     */
-    public Node disambiguate(AmbiguityRemover sc) throws SemanticException {
-	if (Report.should_report("debug", 5)) {
-	   Report.report(5,"[NullableNode_c] Disambiguating |" + this + "|(#" 
-			 + this.hashCode() +") with base=|" + base + "|:");
+	public NullableNode_c(Position pos, TypeNode base) {
+		super(pos);
+		this.base = base;
 	}
 
-    	TypeNode newType = (TypeNode) base.disambiguate( sc );
-
-    	// RMF 11/2/2005 - Don't throw a SemanticException if all that's wrong is
-    	// that disambiguation still needs to be done on the type argument
-    	if (!newType.type().isCanonical())
-    	    return this;
-
-    	if (Report.should_report("debug", 5)) {
-	    Report.report(5,"[NullableNode_c] ... yields type |" + newType + "|.");
+	public TypeNode base() {
+		return this.base;
 	}
 
-    	Type baseType = newType.type();
-    	if (null == baseType || ! (baseType instanceof ReferenceType) ) {
-    		throw new SemanticException("The type constructor nullable cannot be applied to a <null> type",
-        			position());
-    	}
-    	
-    	X10TypeSystem ts = (X10TypeSystem) baseType.typeSystem();
-    	this.type = ts.createNullableType( position(), (ReferenceType) baseType);
-
-    	Node result = reconstruct( newType );
-
-	if (Report.should_report("debug", 5)) {
-	     Report.report(5,"[NullableNode_c] ... returns |" + result +"|(#" 
-			   + result.hashCode() +").");
+	public NullableNode base(TypeNode base) {
+		NullableNode_c n = (NullableNode_c) copy();
+		n.base = base;
+		return n;
 	}
 
-    	return result; 
-       }
-
-    /**
-     * Typecheck the type-argument (in this.base). If it typechecks
-     * (e.g. passes visibility constraints), and the result is a
-     * ReferenceType, update type.this if necessary. Otherwise throw a
-     * semantic exception.  TODO: Ensure that visibility of this node
-     * is the same as that of the type argument.
-     */
-
-    public Node typeCheck( TypeChecker tc) throws SemanticException {
-
-	if (Report.should_report("debug", 5)) {
-	    Report.report(5,"[NullableNode_c] Type checking |" + this +"|:");
+	protected NullableNode reconstruct(TypeNode base) {
+		return (base != this.base) ? base(base) : this;
 	}
 
-    	Node n = base.typeCheck( tc );
-
-	if (Report.should_report("debug", 5)) {
-	    Report.report(5,"[NullableNode_c] ... yields node |" + n +"|.");
+	public Node visitChildren(NodeVisitor v) {
+		TypeNode base = (TypeNode) visitChild(this.base, v);
+		return reconstruct(base);
 	}
 
-    	if (! (n instanceof TypeNode)) 
-    		throw new SemanticException("Argument to nullable type-constructor does not type-check" + position());
-    	if (n == base )
-    		return this;
-    	TypeNode arg = (TypeNode) n;
-    	Type argType = arg.type();
-    	if ( ! (argType instanceof ReferenceType ))
-    		throw new SemanticException("Argument to nullable type-constructor must be a reference type" + position());
-	X10TypeSystem ts = (X10TypeSystem) argType.typeSystem();
-    	this.type = ts.createNullableType( position(), (ReferenceType) argType);
+	/**
+	 * Disambiguate the base node. Ensure that it is unambiguous.
+	 * Create a NullableType_c and store it in this.type.
+	 */
+	public Node disambiguate(AmbiguityRemover sc) throws SemanticException {
+		if (Report.should_report("debug", 5)) {
+			Report.report(5,"[NullableNode_c] Disambiguating |" + this + "|(#"
+					+ this.hashCode() +") with base=|" + base + "|:");
+		}
 
-	if (Report.should_report("debug", 5)) {
-	   Report.report(5, "[NullableNode_c] ... sets type to |" + this.type + "|.");
-	   Report.report(5, "[NullableNode_c] ... returns |" + this + "|."); 
+		TypeNode newType = (TypeNode) base.disambiguate(sc);
+
+		// RMF 11/2/2005 - Don't throw a SemanticException if all that's wrong is
+		// that disambiguation still needs to be done on the type argument
+		if (!newType.type().isCanonical())
+			return this;
+
+		if (Report.should_report("debug", 5)) {
+			Report.report(5,"[NullableNode_c] ... yields type |" + newType + "|.");
+		}
+
+		X10Type baseType = (X10Type) newType.type();
+		if (null == baseType) {
+			throw new SemanticException("The type constructor nullable cannot be applied to a <null> type",
+					position());
+		}
+		// [IP] This is incorrect -- non-reference types can also be nullable
+//		if (!(baseType instanceof ReferenceType))
+//			throw new SemanticException("Argument to nullable type-constructor must be a reference type",
+//					position());
+
+		X10TypeSystem ts = (X10TypeSystem) baseType.typeSystem();
+		// [IP] FIXME: Why are we modifying this in-place?
+		this.type = ts.createNullableType(position(), baseType);
+
+		Node result = reconstruct(newType);
+
+		if (Report.should_report("debug", 5)) {
+			Report.report(5,"[NullableNode_c] ... returns |" + result +"|(#"
+					+ result.hashCode() +").");
+		}
+
+		return result;
 	}
 
-    	return this;
-   	
-    }
+	/**
+	 * Typecheck the type-argument (in this.base). If it typechecks
+	 * (e.g. passes visibility constraints), update type.this if necessary.
+	 * Otherwise throw a semantic exception.  TODO: Ensure that visibility of
+	 * this node is the same as that of the type argument.
+	 */
+	public Node typeCheck(TypeChecker tc) throws SemanticException {
 
-    public String toString() {
-    	return "/*nullable*/" + base.toString();
-  }
-    /**
-     * Write out Java code for this node.
-     * Hmmm.. TODO: this will need to output the BoxedType for the primitive types, 
-     * and just the reference type (unchanged) otherwise.
-     */
-    public void prettyPrint(CodeWriter w, PrettyPrinter ignore) {             
-        w.write( "/*nullable*/" + base.toString());
-      } 
+		if (Report.should_report("debug", 5)) {
+			Report.report(5,"[NullableNode_c] Type checking |" + this +"|:");
+		}
 
-    // translate??
-    // prettyPrint?
-    // dump?
+		Node n = base.typeCheck(tc);
+
+		if (Report.should_report("debug", 5)) {
+			Report.report(5,"[NullableNode_c] ... yields node |" + n +"|.");
+		}
+
+		if (!(n instanceof TypeNode))
+			throw new SemanticException("Argument to nullable type-constructor does not type-check",
+					position());
+		if (n == base)
+			return this;
+		TypeNode arg = (TypeNode) n;
+		X10Type argType = (X10Type) arg.type();
+		// [IP] This is incorrect -- non-reference types can also be nullable
+//		if (!(argType instanceof ReferenceType))
+//			throw new SemanticException("Argument to nullable type-constructor must be a reference type",
+//					position());
+		X10TypeSystem ts = (X10TypeSystem) argType.typeSystem();
+		// [IP] FIXME: Why are we modifying this in-place?
+		this.type = ts.createNullableType(position(), argType);
+
+		if (Report.should_report("debug", 5)) {
+			Report.report(5, "[NullableNode_c] ... sets type to |" + this.type + "|.");
+			Report.report(5, "[NullableNode_c] ... returns |" + this + "|.");
+		}
+
+		return this;
+	}
+
+	public String toString() {
+		return "nullable<" + base.toString() + ">";
+	}
+
+	/**
+	 * Write out Java code for this node.
+	 * Hmmm.. TODO: this will need to output the BoxedType for the primitive types,
+	 * and just the reference type (unchanged) otherwise.
+	 */
+	public void prettyPrint(CodeWriter w, PrettyPrinter pp) {
+		w.write("/*nullable*/");
+		base.del().prettyPrint(w, pp);
+	}
+
+	// translate??
+	// dump?
 }
+
