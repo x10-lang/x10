2dacbd0135127cf6b08b6f02031d607a2a3493bc
Igor Peshansky <igorp@us.ibm.com>
2006-02-16 03:23:30 +0000

    Add arithmetic operations Reindent
    git-svn-id: svn://svn.code.sf.net/p/x10/code@1926 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.runtime/src/x10/lang/point.java
+++ trunk/x10.runtime/src/x10/lang/point.java
@@ -1,11 +1,11 @@
 package x10.lang;
 
-abstract public /*value*/ class point/*( region region )*/ extends Object 
-implements Indexable, ValueType {
+abstract public /*value*/ class point/*(region region)*/ extends Object
+	implements Indexable, ValueType
+{
 	public final region region;
 	/*parameter*/ public final /*nat*/int rank;
-	
-	
+
 	abstract public static /*value*/ class factory implements ValueType {
 		/** Create a point with the given coordinates in the given region.
 		 * Throws an error if the point is not in the region.
@@ -14,61 +14,71 @@ implements Indexable, ValueType {
 		 * @return
 		 */
 		abstract public point/*(region)*/ point(region r, int[/*rank*/] val);
-		
+
 		/** Return a point with the given coordinates, created in the smallest region
 		 * that can enclose it.
-		 * 
+		 *
 		 */
 		abstract public point point(int[/*rank*/] val);
 		/** Return a point of rank 1 with the given coordinates, created in the smallest region
 		 * that can enclose it.
-		 * 
+		 *
 		 */
 		abstract public point point(int v1);
 		/** Return a point of rank 2 with the given coordinates, created in the smallest region
 		 * that can enclose it.
-		 * 
+		 *
 		 */
 		abstract public point point(int v1, int v2);
 		/** Return a point of rank 3 with the given coordinates, created in the smallest region
 		 * that can enclose it.
-		 * 
+		 *
 		 */
 		abstract public point point(int v1, int v2, int v3);
 		/** Return a point of rank 4 with the given coordinates, created in the smallest region
 		 * that can enclose it.
-		 * 
+		 *
 		 */
 		abstract public point point(int v1, int v2, int v3, int v4);
 		/** Return a point of rank 5 with the given coordinates, created in the smallest region
 		 * that can enclose it.
-		 * 
+		 *
 		 */
 		abstract public point point(int v1, int v2, int v3, int v4, int v5);
 	}
 	public static final factory factory = Runtime.factory.getPointFactory();
-	
-	protected point( region region) {
+
+	protected point(region region) {
 		this.region = region;
 		this.rank = region.rank;
 	}
 	/** Return the value of this point on the i'th dimension.
-	 */    
-	abstract public int get( /*nat*/ int i );
-	
+	 */
+	abstract public int get(/*nat*/ int i);
+
 	/** Return true iff the point is on the upper boundary of the i'th
 	 * dimension.
 	 */
-	abstract public boolean onUpperBoundary( /*nat*/ int i );
-	
+	abstract public boolean onUpperBoundary(/*nat*/ int i);
+
 	/** Return true iff the point is on the lower boundary of the i'th
 	 * dimension.
 	 */
-	abstract public boolean onLowerBoundary( /*nat*/ int i );
+	abstract public boolean onLowerBoundary(/*nat*/ int i);
 
-    abstract public boolean gt ( point/*(region)*/ p);
-    abstract public boolean lt ( point/*(region)*/ p);
-    abstract public boolean ge ( point/*(region)*/ p);
-    abstract public boolean le ( point/*(region)*/ p);
- 
+	abstract public boolean gt(point/*(region)*/ p);
+	abstract public boolean lt(point/*(region)*/ p);
+	abstract public boolean ge(point/*(region)*/ p);
+	abstract public boolean le(point/*(region)*/ p);
+
+	abstract public point/*(region)*/ neg();
+	abstract public point/*(region)*/ add(point/*(region)*/ p);
+	abstract public point/*(region)*/ sub(point/*(region)*/ p);
+	abstract public point/*(region)*/ mul(point/*(region)*/ p);
+	abstract public point/*(region)*/ div(point/*(region)*/ p);
+	abstract public point/*(region)*/ add(int c);
+	public point/*(region)*/ sub(int c) { return add(-c); }
+	abstract public point/*(region)*/ mul(int c);
+	abstract public point/*(region)*/ div(int c);
+
 }
