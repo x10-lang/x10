61a45277603baca05d31405a21eb273c6b36dfb8
Igor Peshansky <igorp@us.ibm.com>
2006-02-17 00:13:44 +0000

    Add (constant x point) operations Provide default implementation for sub(p) and all constant ops
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@1934 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.runtime/src/x10/lang/point.java
+++ x10.runtime/src/x10/lang/point.java
@@ -7,7 +7,8 @@ abstract public /*value*/ class point/*(region region)*/ extends Object
 	/*parameter*/ public final /*nat*/int rank;
 
 	abstract public static /*value*/ class factory implements ValueType {
-		/** Create a point with the given coordinates in the given region.
+		/**
+		 * Create a point with the given coordinates in the given region.
 		 * Throws an error if the point is not in the region.
 		 * @param r
 		 * @param val
@@ -15,34 +16,34 @@ abstract public /*value*/ class point/*(region region)*/ extends Object
 		 */
 		abstract public point/*(region)*/ point(region r, int[/*rank*/] val);
 
-		/** Return a point with the given coordinates, created in the smallest region
+		/**
+		 * Return a point with the given coordinates, created in the smallest region
 		 * that can enclose it.
-		 *
 		 */
 		abstract public point point(int[/*rank*/] val);
-		/** Return a point of rank 1 with the given coordinates, created in the smallest region
+		/**
+		 * Return a point of rank 1 with the given coordinates, created in the smallest region
 		 * that can enclose it.
-		 *
 		 */
 		abstract public point point(int v1);
-		/** Return a point of rank 2 with the given coordinates, created in the smallest region
+		/**
+		 * Return a point of rank 2 with the given coordinates, created in the smallest region
 		 * that can enclose it.
-		 *
 		 */
 		abstract public point point(int v1, int v2);
-		/** Return a point of rank 3 with the given coordinates, created in the smallest region
+		/**
+		 * Return a point of rank 3 with the given coordinates, created in the smallest region
 		 * that can enclose it.
-		 *
 		 */
 		abstract public point point(int v1, int v2, int v3);
-		/** Return a point of rank 4 with the given coordinates, created in the smallest region
+		/**
+		 * Return a point of rank 4 with the given coordinates, created in the smallest region
 		 * that can enclose it.
-		 *
 		 */
 		abstract public point point(int v1, int v2, int v3, int v4);
-		/** Return a point of rank 5 with the given coordinates, created in the smallest region
+		/**
+		 * Return a point of rank 5 with the given coordinates, created in the smallest region
 		 * that can enclose it.
-		 *
 		 */
 		abstract public point point(int v1, int v2, int v3, int v4, int v5);
 	}
@@ -52,16 +53,19 @@ abstract public /*value*/ class point/*(region region)*/ extends Object
 		this.region = region;
 		this.rank = region.rank;
 	}
-	/** Return the value of this point on the i'th dimension.
+	/**
+	 * Return the value of this point on the i'th dimension.
 	 */
 	abstract public int get(/*nat*/ int i);
 
-	/** Return true iff the point is on the upper boundary of the i'th
+	/**
+	 * Return true iff the point is on the upper boundary of the i'th
 	 * dimension.
 	 */
 	abstract public boolean onUpperBoundary(/*nat*/ int i);
 
-	/** Return true iff the point is on the lower boundary of the i'th
+	/**
+	 * Return true iff the point is on the lower boundary of the i'th
 	 * dimension.
 	 */
 	abstract public boolean onLowerBoundary(/*nat*/ int i);
@@ -73,12 +77,21 @@ abstract public /*value*/ class point/*(region region)*/ extends Object
 
 	abstract public point/*(region)*/ neg();
 	abstract public point/*(region)*/ add(point/*(region)*/ p);
-	abstract public point/*(region)*/ sub(point/*(region)*/ p);
+	public point/*(region)*/ sub(point/*(region)*/ p) { return add(p.neg()); }
 	abstract public point/*(region)*/ mul(point/*(region)*/ p);
 	abstract public point/*(region)*/ div(point/*(region)*/ p);
-	abstract public point/*(region)*/ add(int c);
+	private static point/*(region)*/ toPoint(int rank, int c) {
+		int[] a = new int[rank];
+		for (int i = 0; i < rank; i++) a[i] = c;
+		return factory.point(a);
+	}
+	public point/*(region)*/ add(int c) { return add(toPoint(rank, c)); }
 	public point/*(region)*/ sub(int c) { return add(-c); }
-	abstract public point/*(region)*/ mul(int c);
-	abstract public point/*(region)*/ div(int c);
-
+	public point/*(region)*/ mul(int c) { return mul(toPoint(rank, c)); }
+	public point/*(region)*/ div(int c) { return div(toPoint(rank, c)); }
+	public point/*(region)*/ invadd(int c) { return add(c); }
+	public point/*(region)*/ invsub(int c) { return add(-c).neg(); }
+	public point/*(region)*/ invmul(int c) { return mul(c); }
+	public point/*(region)*/ invdiv(int c) { return toPoint(rank, c).div(this); }
 }
+
