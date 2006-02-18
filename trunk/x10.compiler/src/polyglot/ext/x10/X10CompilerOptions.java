8e40aa876d9331ba4989a9e7810a17a48724fbc9
Igor Peshansky <igorp@us.ibm.com>
2006-02-18 01:30:49 +0000

    Enable parsing of configuration options Fix usage to actually print out all command-line and configuration options Reindent
    git-svn-id: svn://svn.code.sf.net/p/x10/code@1944 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.compiler/src/polyglot/ext/x10/X10CompilerOptions.java
+++ trunk/x10.compiler/src/polyglot/ext/x10/X10CompilerOptions.java
@@ -3,19 +3,36 @@
 package polyglot.ext.x10;
 
 import java.io.PrintStream;
+import java.util.Set;
 import polyglot.frontend.ExtensionInfo;
+import polyglot.main.Main;
+import polyglot.main.UsageError;
 
 public class X10CompilerOptions extends polyglot.main.Options {
 
-    public X10CompilerOptions(ExtensionInfo extension) {
-        super(extension);
-    }
+	public X10CompilerOptions(ExtensionInfo extension) {
+		super(extension);
+	}
 
-    /**
-     * Print usage information
-     */
-    public void usage(PrintStream out) {
-        out.println(extension.compilerName() + ": missing or illegal argument(s).");
-        out.println("Try `" + extension.compilerName() + " -help' for more information.");
-    }
+	protected int parseCommand(String args[], int index, Set source) 
+		throws UsageError, Main.TerminationException
+	{
+		int i = super.parseCommand(args, index, source);
+		if (i != index) return i;
+
+		if (Configuration.parseArgument(args[index]))
+			return ++index;
+		return index;
+	}
+
+	/**
+	 * Print usage information
+	 */
+	public void usage(PrintStream out) {
+		super.usage(out);
+		String[][] options = Configuration.options();
+		for (int i = 0; i < options.length; i++) {
+			usageForFlag(out, options[i][0], options[i][1]);
+		}
+	}
 }
