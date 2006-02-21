1900d8c912310073c7545ceec770f4278a7e3084
Robert Fuhrer <rfuhrer@us.ibm.com>
2006-02-21 18:25:17 +0000

    Now properly uses the "default context interpreter" which understands source IMethod's, as does the Java ZeroXCFABuilder.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@1948 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/com.ibm.wala.cast.x10/src/com/ibm/wala/cast/x10/ipa/callgraph/X10ZeroXCFABuilder.java
+++ trunk/com.ibm.wala.cast.x10/src/com/ibm/wala/cast/x10/ipa/callgraph/X10ZeroXCFABuilder.java
@@ -1,6 +1,5 @@
 package com.ibm.domo.ast.x10.ipa.callgraph;
 
-import com.ibm.domo.analysis.reflection.FactoryBypassInterpreter;
 import com.ibm.domo.ast.java.ipa.callgraph.JavaScopeMappingInstanceKeys;
 import com.ibm.domo.ipa.callgraph.AnalysisOptions;
 import com.ibm.domo.ipa.callgraph.ContextSelector;
@@ -9,8 +8,6 @@ import com.ibm.domo.ipa.callgraph.impl.DefaultContextSelector;
 import com.ibm.domo.ipa.callgraph.impl.DelegatingContextSelector;
 import com.ibm.domo.ipa.callgraph.propagation.DefaultPropagationContextSelector;
 import com.ibm.domo.ipa.callgraph.propagation.SSAContextInterpreter;
-import com.ibm.domo.ipa.callgraph.propagation.cfa.DefaultSSAInterpreter;
-import com.ibm.domo.ipa.callgraph.propagation.cfa.DelegatingSSAContextInterpreter;
 import com.ibm.domo.ipa.callgraph.propagation.cfa.ZeroXInstanceKeys;
 import com.ibm.domo.ipa.cha.ClassHierarchy;
 import com.ibm.domo.util.warnings.WarningSet;
@@ -33,9 +30,8 @@ public class X10ZeroXCFABuilder extends X10CFABuilder {
   			   int instancePolicy) {
       super(cha, warnings, options);
 
-      SSAContextInterpreter c = new DefaultSSAInterpreter(options, cha, warnings);
-      c = new DelegatingSSAContextInterpreter(new FactoryBypassInterpreter(options, cha, reflect, warnings), c);
-      SSAContextInterpreter contextInterpreter = new DelegatingSSAContextInterpreter(appContextInterpreter, c);
+      SSAContextInterpreter contextInterpreter = 
+	      makeDefaultContextInterpreters(appContextInterpreter, options, cha, reflect, warnings);
       setContextInterpreter(contextInterpreter);
 
       ContextSelector def = new DefaultContextSelector(cha, options.getMethodTargetSelector());
