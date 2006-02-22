8e3c6ac68ac822239a1f7cefb9a65403b1494e26
Igor Peshansky <igorp@us.ibm.com>
2006-02-22 07:08:01 +0000

    Allow regions and distributions as arguments
    git-svn-id: svn://svn.code.sf.net/p/x10/code@1963 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.runtime/src/x10/lang/RankMismatchException.java
+++ trunk/x10.runtime/src/x10/lang/RankMismatchException.java
@@ -5,15 +5,37 @@ package x10.lang;
  */
 public class RankMismatchException extends x10.lang.Exception {
 	private final x10.lang.point p_;
-	private final int r_;
+	private final x10.lang.region r_;
+	private final x10.lang.dist d_;
+	private final int n_;
 
-	public RankMismatchException(x10.lang.point p, int r) {
+	public RankMismatchException(x10.lang.point p, int n) {
 		p_ = p;
+		r_ = null;
+		d_ = null;
+		n_ = n;
+	}
+
+	public RankMismatchException(x10.lang.region r, int n) {
+		p_ = null;
 		r_ = r;
+		d_ = null;
+		n_ = n;
+	}
+
+	public RankMismatchException(x10.lang.dist d, int n) {
+		p_ = null;
+		r_ = null;
+		d_ = d;
+		n_ = n;
 	}
 
 	public String toString() {
-		return "RankMismatchException(point " + p_ + " accessed as rank " + r_ + ")";
+		return "RankMismatchException(" +
+			(p_ != null ? "point " + p_ :
+			 r_ != null ? "region " + r_ :
+			 "distribution " + d_) +
+			" accessed as rank " + n_ + ")";
 	}
 }
 
