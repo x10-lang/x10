c8046ab3c547a3c52c0a6854b993f4e583a75007
Vivek Sarkar <vsarkar@rice.edu>
2006-03-15 20:19:44 +0000

    Significant rework to migrate to new runtime based on JCU.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@2037 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.runtime/src/x10/runtime/PoolRunner.java
+++ x10.runtime/src/x10/runtime/PoolRunner.java
@@ -1,3 +1,10 @@
+package x10.runtime;
+
+import java.util.concurrent.*;
+import java.util.*;
+import java.lang.*;
+import x10.lang.place;
+
 
 /**
 * Thread in the thread pool that can be used to run multiple
@@ -8,127 +15,87 @@
 *
 * @author Christian Grothoff
 * @author vj
+* 
+* @author Raj Barik, Vivek Sarkar
+* 3/6/2006: replaced original PoolRunner by JCU implementation
 */
-package x10.runtime;
 
-public final  class PoolRunner extends Thread 
-	implements ActivityRunner {
-   /**
-    * For building a linked list of these.
-    */
-   PoolRunner next;
-   private boolean active = true;
-   private Runnable job;
-   private Activity act;
-
-   
-   /* the pace where this runner currently executed - can be different from 
-    *  homePlace, e.g inside an array initiizer, see also 
-    *  DefaultRuntime_c.setCurrentPlace(place p) */
-   public LocalPlace_c place; 
-   
-   /* the pace to which this worker thread belongs */
-   public final LocalPlace_c homePlace;
-   
-   PoolRunner(LocalPlace_c p) {
-       homePlace = p;
-	   place = p;
-       p.addThread( this );
-   }
-   public Activity getActivity() {
-   	return act;
-   }
-   public void setActivity( Activity a) {
-   	this.act = a;
-   }
-
-   
-   synchronized void shutdown() {
-       active = false;
-       this.notifyAll();
-   }
-   /**
-    * Assign a new job to this runner and notify it of the change so it can start running again.
-    * @param r
-    */
-   synchronized void run(Runnable r) {
-       assert job == null;
-       job = r;       
-       this.notifyAll();
-   }
-   /**
-    * Change the 'running' status of a thread.
-    * @param delta +1 for thread starts to run (unblocked), -1 for thread is blocked
-    */
-   public void changeRunningStatus(int delta) {
-	   homePlace.changeRunningStatus(delta);
-   }
-   
-   /**
-    * Run jobs until shutdown is called.
-    */
-   public synchronized void run() {
-       while (active) {
-           if (Report.should_report("activity", 5)) {
-               Report.report(5, logString() + " waiting for a job.");
-           }
-           while (active && job == null) {
-               try {
-                   wait();
-               } catch (InterruptedException ie) {
-                   throw new Error(ie);
-               }
-               if (Report.should_report("activity", 5)) {
-                   Report.report(5, logString() +" awakes.");
-               }
-           }
-           if (job != null) {
-               Runnable j = job;
-               job = null;
-               try {
-                   changeRunningStatus(1);
-                   if (Report.should_report("activity", 5)) {
-                       Report.report(5, logString() + " starts running " + j +".");
-                   }
-                   j.run();
-                   if (Report.should_report("activity", 5)) {
-                       Report.report(5, logString() + " finished running " + j +".");
-                   }
-                   
-               } finally {
-                   changeRunningStatus(-1);
-                   // act.finalizeTermination();
-               }
-               // notify the LocalPlace_c that we're again available
-               // for more work!
-               synchronized (homePlace) {
-                   if (homePlace.isShutdown()) {
-                       if (Report.should_report("activity", 5)) {
-                           Report.report(5, logString() + " shuts down.");
-                       }                       
-                       return;
-                   }
-                   if (homePlace.shouldRunnerTerminate())
-                       active = false;
-                   else
-                	   homePlace.repool(this);
-               }
-           }
-       }
-       if (Report.should_report("activity", 5)) {
-           Report.report(5, logString() + " shuts down.");
-       }
-   }
-   
-   public String thisThreadString() {
-   	return Thread.currentThread() +  "@" + homePlace + ":" + System.currentTimeMillis();
-   }
-   public static String logString() {
-   	return ((PoolRunner) Thread.currentThread()).thisThreadString();
-   }
-   
-   public String toString() {
-   	return "<PoolRunner " + hashCode() + ">";
-   			
-   }
-} // end of LocalPlace_c.PoolRunner
+public class PoolRunner extends Thread implements ActivityRunner{
+    
+	/**
+	 * Current activity being run by this thread 
+	 */
+	private Activity act;
+	
+	/** the pace where this runner currently executed - can be different from 
+	 *  homePlace, e.g inside an array initiizer, see also 
+	 *  DefaultRuntime_c.setCurrentPlace(place p) */
+	private Place place;
+	
+	
+    PoolRunner(ThreadGroup group, Runnable r, String namePrefix) {
+    	super(group, r, namePrefix, 0);
+    }
+    
+    /**
+     * Set place
+     */
+    public void setPlace(Place p) {
+    	place = p;
+    }
+    
+    /**
+     * get place
+     */
+    public Place getPlace(){
+    	return place;
+    }
+    
+    /**
+     * Set Activity
+     */
+    public void setActivity(Activity a) {
+    	act = a;
+    }
+    
+    /**
+     * Get Activity
+     */
+    public Activity getActivity() {
+    	return act;
+    }
+      
+    
+    /**
+     * This thread adds to pool dynamically if  
+     */
+    public void addPoolQueuePositive() {
+		if(!act.getAddPoolCalled()) {
+			int ret=place.getThreadPool().addPoolIfQueuePositive();
+			if (ret > 0) act.setAddPoolCalled();
+		}
+	}
+    
+    /**
+     * Add to pool only if number of threads waiting is greater or equal to corepoolsize
+     *
+     */
+    public void addPoolNew() {
+    	place.incNumBlocked();
+    	if(place.getNumBlocked() >= place.getThreadPool().getCorePoolSize()) 
+    		place.getThreadPool().addPool();
+    }
+    
+    public String thisThreadString() {
+       	return Thread.currentThread() +  "@" + place + ":" + System.currentTimeMillis();
+    }
+    
+    public static String logString() {
+       	return ( (PoolRunner)Thread.currentThread()).thisThreadString();
+    }
+       
+    public String toString() {
+       	return "<PoolRunner " + hashCode() + ">";
+       			
+    }
+}
