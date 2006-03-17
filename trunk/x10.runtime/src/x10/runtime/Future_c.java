8370b873a99d8bef87b4dac3e72ea593e3c5569d
Vivek Sarkar <vsarkar@rice.edu>
2006-03-17 07:30:38 +0000

    Guard calls to cdl.countDown() with the predicate, cdl.getCount() > 0.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@2043 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.runtime/src/x10/runtime/Future_c.java
+++ trunk/x10.runtime/src/x10/runtime/Future_c.java
@@ -23,74 +23,68 @@ import x10.lang.place;
  */
 public final class Future_c extends Future {
 
-	/**
-	 * Set if the activity terminated with an exception.
-	 * Can only be of type Error or RuntimeException
-	 * (since X10 only has unchecked exceptions).
-	 */
-	private Throwable exception_;
+    /**
+     * Set if the activity terminated with an exception.
+     * Can only be of type Error or RuntimeException
+     * (since X10 only has unchecked exceptions).
+     */
+    private Throwable exception_;
 
-	private Object result_;
+    private Object result_;
 
-	/**
-	 * CountDownLatch for signalling and wait -- can be replaced with a boolean latch
-	 */
-	private ModCountDownLatch cdl = new ModCountDownLatch(1);
+    /**
+     * CountDownLatch for signalling and wait -- can be replaced by a boolean latch
+     */
+    private ModCountDownLatch cdl = new ModCountDownLatch(1);
 
-	public Future_c() {
+    public Future_c() {
+    }
+
+    /**
+     * Set the result value returned by this async call.
+     * 
+     * @param result
+     */
+    public void setResult(Object result) {
+	this.result_ = result;
+	if (cdl.getCount() > 0) cdl.countDown();
+    }
+
+    /**
+     * Set the result value returned by this async call. 
+     * Caller must ensure that this is called only after the activity
+     * associated with the future has finish'ed.
+     * 
+     * @param result
+     */
+    public void setException(Throwable t) {
+	this.exception_ = t;
+	if (cdl.getCount() > 0) cdl.countDown();
+    }
+
+    /*
+     * @see x10.runtime.Activity.Result#force()
+     */
+    public Object force() {
+	Thread t = Thread.currentThread();
+	if (cdl.getCount() > 0) {
+	    ((PoolRunner) t).addPoolNew();
+	    try {
+		cdl.await();
+	    } catch (InterruptedException ie) {
+		System.err.println("Future_c::force - unexpected exception e" + ie);
+		throw new Error(ie); // this should never happen...
+	    }
+	    ((PoolRunner) t).getPlace().decNumBlocked();
 	}
-
-	/**
-	 * Set the result value returned by this async call.
-	 * 
-	 * @param result
-	 */
-	public void setResult(Object result) {
-		this.result_ = result;
-		cdl.countDown();
+	if (exception_ != null) {
+	    if (exception_ instanceof Error)
+		throw (Error) exception_;
+	    if (exception_ instanceof RuntimeException)
+		throw (RuntimeException) exception_;
+	    assert false;
 	}
-
-	/**
-	 * Set the result value returned by this async call. 
-	 * Caller must ensure that this is called only after the activity
-	 * associated with the future has finish'ed.
-	 * 
-	 * @param result
-	 */
-	public void setException(Throwable t) {
-		this.exception_ = t;
-		cdl.countDown();
-	}
-
-	/*
-	 * @see x10.runtime.Activity.Result#force()
-	 */
-	public Object force() {
-		Thread t = Thread.currentThread();
-		boolean incCount = false;
-		if (cdl.getCount() != 0) {
-			((PoolRunner) t).addPoolNew();
-			incCount = true;
-		}
-
-		try {
-			cdl.await();
-		} catch (InterruptedException ie) {
-			System.err.println("Future_c::force - unexpected exception e" + ie);
-			throw new Error(ie); // this should never happen...
-		}
-
-		if (incCount)
-			((PoolRunner) t).getPlace().decNumBlocked();
-
-		if (exception_ != null) {
-			if (exception_ instanceof Error)
-				throw (Error) exception_;
-			if (exception_ instanceof RuntimeException)
-				throw (RuntimeException) exception_;
-			assert false;
-		}
 			
-		return result_;
-	}
+	return result_;
+    }
 }
