691b6186472d67f3394345180ba4690de7d3e2ba
Igor Peshansky <igorp@us.ibm.com>
2006-03-27 21:35:06 +0000

    Add localInstances() methods
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@2051 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.compiler/src/polyglot/ext/x10/ast/X10Formal.java
+++ x10.compiler/src/polyglot/ext/x10/ast/X10Formal.java
@@ -7,6 +7,7 @@ import polyglot.ast.Formal;
 import java.util.List;
 import polyglot.ast.Stmt;
 import polyglot.ast.NodeFactory;
+import polyglot.types.LocalInstance;
 import polyglot.types.TypeSystem;
 
 /**
@@ -14,6 +15,12 @@ import polyglot.types.TypeSystem;
  */
 public interface X10Formal extends Formal {
    boolean hasExplodedVars();
+
+   /** Get the local instances of the bound variables. */
+   public LocalInstance[] localInstances();
+
+   /** Set the local instances of the bound variables. */
+   public X10Formal localInstances(LocalInstance[] lis);
 
    /**
     * Return a list of statements containing the initializations for the exploded vars,
