08fe8a29533a2f8d3afb7b56e900609a44c8622e
Chris Donawa <donawa@domain>
2006-03-28 01:38:58 +0000

    increase starting core size
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@2055 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.runtime/src/x10/runtime/X10ThreadPoolExecutor.java
+++ x10.runtime/src/x10/runtime/X10ThreadPoolExecutor.java
@@ -15,7 +15,7 @@ public class X10ThreadPoolExecutor extends ThreadPoolExecutor {
 
 	/** Fix nThreads to 1 for best results */
 	public X10ThreadPoolExecutor ( int nThreads ) { 
-		super ( 2*nThreads,2*nThreads, 
+		super ( 3*nThreads,3*nThreads, 
 				0L, TimeUnit.MILLISECONDS,
 				new LinkedBlockingQueue (),
 				new X10ThreadFactory ()); 
