b6ea5b7405724107cd731cd00f3ca5f7f60a4dfb
Igor Peshansky <igorp@us.ibm.com>
2006-03-29 15:46:19 +0000

    Make into a DelegatingCAstVisitor Add translate() methods
    git-svn-id: svn://svn.code.sf.net/p/x10/code@2058 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/com.ibm.wala.cast.x10/src/com/ibm/wala/cast/x10/translator/polyglot/X10CAst2IRTranslator.java
+++ trunk/com.ibm.wala.cast.x10/src/com/ibm/wala/cast/x10/translator/polyglot/X10CAst2IRTranslator.java
@@ -6,6 +6,7 @@ package com.ibm.domo.ast.x10.translator.polyglot;
 import com.ibm.capa.ast.CAstEntity;
 import com.ibm.capa.ast.CAstNode;
 import com.ibm.capa.ast.CAstType;
+import com.ibm.capa.ast.visit.*;
 import com.ibm.domo.ast.java.loader.JavaSourceLoaderImpl;
 import com.ibm.domo.ast.java.translator.JavaCAst2IRTranslator;
 import com.ibm.domo.ast.java.types.JavaPrimitiveTypeMap;
@@ -19,22 +20,35 @@ import com.ibm.domo.ast.x10.ssa.SSARegionIterInitInstruction;
 import com.ibm.domo.ast.x10.ssa.SSARegionIterNextInstruction;
 import com.ibm.domo.ast.x10.translator.X10CAstEntity;
 import com.ibm.domo.ast.x10.translator.X10CastNode;
+import com.ibm.domo.ast.x10.visit.X10CAstVisitor;
 import com.ibm.domo.ssa.SSAInstructionFactory;
 import com.ibm.domo.types.Descriptor;
 import com.ibm.domo.types.MethodReference;
 import com.ibm.domo.types.TypeName;
 import com.ibm.domo.types.TypeReference;
 import com.ibm.domo.util.Atom;
+import com.ibm.capa.util.debug.Trace;
 
-public class X10CAst2IRTranslator extends JavaCAst2IRTranslator {
+import com.ibm.domo.ast.translator.AstTranslator.WalkContext;
+import com.ibm.domo.ast.translator.AstTranslator.DefaultContext;
+
+public class X10CAst2IRTranslator extends X10CAstVisitor {
     public X10CAst2IRTranslator(CAstEntity sourceFileEntity, JavaSourceLoaderImpl loader) {
-	super(sourceFileEntity, loader);
+	this(new JavaCAst2IRTranslator(sourceFileEntity, loader));
+    }
+
+    private final JavaCAst2IRTranslator translator;
+
+    private X10CAst2IRTranslator(JavaCAst2IRTranslator translator) {
+	super(translator);
+	this.translator = translator;
     }
 
     public MethodReference asyncEntityToMethodReference(CAstEntity asyncEntity) {
 	CAstType.Method bodyType= (CAstType.Method) asyncEntity.getType();
 	CAstType retType= bodyType.getReturnType();
 	CAstType owningType= bodyType.getDeclaringType();
+	JavaSourceLoaderImpl fLoader = translator.loader();
 
 	Atom asyncName= Atom.findOrCreateUnicodeAtom(asyncEntity.getName());
 	Descriptor asyncDesc= Descriptor.findOrCreate(null, TypeName.string2TypeName(retType.getName()));
@@ -43,80 +57,8 @@ public class X10CAst2IRTranslator extends JavaCAst2IRTranslator {
 	return MethodReference.findOrCreate(owningTypeRef, asyncName, asyncDesc);
     }
 
-    protected boolean doVisit(CAstNode n, Context context) {
-	switch (n.getKind()) {
-	    case X10CastNode.ASYNC_INVOKE: {
-		if (visitAsyncInvoke(n, context))
-		    break;
-		leaveAsyncInvoke(n, context);
-		break;
-	    }
-	    case X10CastNode.ATOMIC_ENTER: {
-		if (visitAtomicEnter(n, context))
-		    break;
-		leaveAtomicEnter(n, context);
-		break;
-	    }
-	    case X10CastNode.ATOMIC_EXIT: {
-		if (visitAtomicExit(n, context))
-		    break;
-		leaveAtomicExit(n, context);
-		break;
-	    }
-	    case X10CastNode.FINISH_ENTER: {
-		if (visitFinishEnter(n, context))
-		    break;
-		leaveFinishEnter(n, context);
-		break;
-	    }
-	    case X10CastNode.FINISH_EXIT: {
-		if (visitFinishExit(n, context))
-		    break;
-		leaveFinishExit(n, context);
-		break;
-	    }
-	    case X10CastNode.FORCE: {
-		if (visitForce(n, context))
-		    break;
-		visit(n.getChild(0), context);
-		leaveForce(n, context);
-		break;
-	    }
-	    case X10CastNode.REGION_ITER_INIT: {
-		if (visitRegionIterInit(n, context))
-		    break;
-		visit(n.getChild(0), context);
-		leaveRegionIterInit(n, context);
-		break;
-	    }
-	    case X10CastNode.REGION_ITER_HASNEXT: {
-		if (visitRegionIterHasNext(n, context))
-		    break;
-		visit(n.getChild(0), context);
-		leaveRegionIterHasNext(n, context);
-		break;
-	    }
-	    case X10CastNode.REGION_ITER_NEXT: {
-		if (visitRegionIterNext(n, context))
-		    break;
-		visit(n.getChild(0), context);
-		leaveRegionIterNext(n, context);
-		break;
-	    }
-	    case X10CastNode.HERE: {
-		if (visitHere(n, context))
-		    break;
-		leaveHere(n, context);
-		break;
-	    }
-	    default:
-		return super.doVisit(n, context);
-	}
-	return true;
-    }
-
-    protected boolean visitAsyncInvoke(CAstNode n, Context c) { /* empty */ return false; }
-    protected void leaveAsyncInvoke(CAstNode n, Context c) {
+    protected boolean visitAsyncInvoke(CAstNode n, Context c, CAstVisitor visitor) { /* empty */ return false; }
+    protected void leaveAsyncInvoke(CAstNode n, Context c, CAstVisitor visitor) {
 	WalkContext context = (WalkContext)c;
 	CAstEntity bodyEntity = (CAstEntity) n.getChild(1).getValue();
 	// Figure out whether this is a future or an async
@@ -129,97 +71,87 @@ public class X10CAst2IRTranslator extends JavaCAst2IRTranslator {
 	    int retValue = context.scope().allocateTempValue();
 
 	    context.cfg().addInstruction(SSAInstructionFactory.InvokeInstruction(retValue, new int[0], exceptValue, acsr));
-	    setValue(n, retValue);
+	    translator.setValue(n, retValue);
 	}
     }
-    protected boolean visitAtomicEnter(CAstNode n, Context c) { /* empty */ return false; }
-    protected void leaveAtomicEnter(CAstNode n, Context c) {
+    protected boolean visitAtomicEnter(CAstNode n, Context c, CAstVisitor visitor) { /* empty */ return false; }
+    protected void leaveAtomicEnter(CAstNode n, Context c, CAstVisitor visitor) {
 	WalkContext context = (WalkContext)c;
 	context.cfg().addInstruction(new SSAAtomicInstruction(true));
     }
-    protected boolean visitAtomicExit(CAstNode n, Context c) { /* empty */ return false; }
-    protected void leaveAtomicExit(CAstNode n, Context c) {
+    protected boolean visitAtomicExit(CAstNode n, Context c, CAstVisitor visitor) { /* empty */ return false; }
+    protected void leaveAtomicExit(CAstNode n, Context c, CAstVisitor visitor) {
 	WalkContext context = (WalkContext)c;
 	context.cfg().addInstruction(new SSAAtomicInstruction(false));
     }
-    protected boolean visitFinishEnter(CAstNode n, Context c) { /* empty */ return false; }
-    protected void leaveFinishEnter(CAstNode n, Context c) {
+    protected boolean visitFinishEnter(CAstNode n, Context c, CAstVisitor visitor) { /* empty */ return false; }
+    protected void leaveFinishEnter(CAstNode n, Context c, CAstVisitor visitor) {
 	WalkContext context = (WalkContext)c;
 	context.cfg().addInstruction(new SSAFinishInstruction(true));
     }
-    protected boolean visitFinishExit(CAstNode n, Context c) { /* empty */ return false; }
-    protected void leaveFinishExit(CAstNode n, Context c) {
+    protected boolean visitFinishExit(CAstNode n, Context c, CAstVisitor visitor) { /* empty */ return false; }
+    protected void leaveFinishExit(CAstNode n, Context c, CAstVisitor visitor) {
 	WalkContext context = (WalkContext)c;
 	context.cfg().addInstruction(new SSAFinishInstruction(false));
     }
-    protected boolean visitForce(CAstNode n, Context c) { /* empty */ return false; }
-    protected void leaveForce(CAstNode n, Context c) {
+    protected boolean visitForce(CAstNode n, Context c, CAstVisitor visitor) { /* empty */ return false; }
+    protected void leaveForce(CAstNode n, Context c, CAstVisitor visitor) {
 	WalkContext context = (WalkContext)c;
-	int targetValue = getValue(n.getChild(0));
+	int targetValue = translator.getValue(n.getChild(0));
 	int retValue = context.scope().allocateTempValue();
 	context.cfg().addInstruction(new SSAForceInstruction(retValue, targetValue, (TypeReference) n.getChild(1).getValue()));
-	setValue(n, retValue);
+	translator.setValue(n, retValue);
     }
-    protected boolean visitRegionIterInit(CAstNode n, Context c) { /* empty */ return false; }
-    protected void leaveRegionIterInit(CAstNode n, Context c) {
+    protected boolean visitRegionIterInit(CAstNode n, Context c, CAstVisitor visitor) { /* empty */ return false; }
+    protected void leaveRegionIterInit(CAstNode n, Context c, CAstVisitor visitor) {
 	WalkContext context = (WalkContext)c;
-	int targetValue = getValue(n.getChild(0));
+	int targetValue = translator.getValue(n.getChild(0));
 	int retValue = context.scope().allocateTempValue();
 	context.cfg().addInstruction(new SSARegionIterInitInstruction(retValue, targetValue));
-	setValue(n, retValue);
+	translator.setValue(n, retValue);
     }
-    protected boolean visitRegionIterHasNext(CAstNode n, Context c) { /* empty */ return false; }
-    protected void leaveRegionIterHasNext(CAstNode n, Context c) {
+    protected boolean visitRegionIterHasNext(CAstNode n, Context c, CAstVisitor visitor) { /* empty */ return false; }
+    protected void leaveRegionIterHasNext(CAstNode n, Context c, CAstVisitor visitor) {
 	WalkContext context = (WalkContext)c;
-	int targetValue = getValue(n.getChild(0));
+	int targetValue = translator.getValue(n.getChild(0));
 	int retValue = context.scope().allocateTempValue();
 	context.cfg().addInstruction(new SSARegionIterHasNextInstruction(retValue, targetValue));
-	setValue(n, retValue);
+	translator.setValue(n, retValue);
     }
-    protected boolean visitRegionIterNext(CAstNode n, Context c) { /* empty */ return false; }
-    protected void leaveRegionIterNext(CAstNode n, Context c) {
+    protected boolean visitRegionIterNext(CAstNode n, Context c, CAstVisitor visitor) { /* empty */ return false; }
+    protected void leaveRegionIterNext(CAstNode n, Context c, CAstVisitor visitor) {
 	WalkContext context = (WalkContext)c;
-	int targetValue = getValue(n.getChild(0));
+	int targetValue = translator.getValue(n.getChild(0));
 	int retValue = context.scope().allocateTempValue();
 	context.cfg().addInstruction(new SSARegionIterNextInstruction(retValue, targetValue));
-	setValue(n, retValue);
+	translator.setValue(n, retValue);
     }
-    protected boolean visitHere(CAstNode n, Context c) { /* empty */ return false; }
-    protected void leaveHere(CAstNode n, Context c) {
+    protected boolean visitHere(CAstNode n, Context c, CAstVisitor visitor) { /* empty */ return false; }
+    protected void leaveHere(CAstNode n, Context c, CAstVisitor visitor) {
 	WalkContext context = (WalkContext)c;
 	int retValue = context.scope().allocateTempValue();
 	context.cfg().addInstruction(new SSAHereInstruction(retValue));
-	setValue(n, retValue);
+	translator.setValue(n, retValue);
     }
 
-    protected boolean isFunctionEntity(CAstEntity n) {
-	return n.getKind() == X10CAstEntity.ASYNC_BODY || super.isFunctionEntity(n);
-    }
-
-    protected boolean doVisitEntity(CAstEntity n, Context context) {
-	switch (n.getKind()) {
-	    case X10CAstEntity.ASYNC_BODY: {
-		Context codeContext = makeCodeContext(context, n);
-		visitAsyncBodyEntity(n, context, codeContext);
-		// visit the AST if any
-		if (n.getAST() != null)
-		    visit(n.getAST(), codeContext);
-		// process any remaining scoped children
-		visitScopedEntities(n, n.getScopedEntities(null), codeContext);
-		leaveAsyncBodyEntity(n, context, codeContext);
-		break;
-	    }
-	    default:
-		return super.doVisitEntity(n, context);
-	}
-	return true;
-    }
-
-    protected boolean visitAsyncBodyEntity(CAstEntity n, Context context, Context codeContext) {
-	initFunctionEntity(n, (WalkContext)context, (WalkContext)codeContext);
+    protected boolean visitAsyncBodyEntity(CAstEntity n, Context context, Context codeContext, CAstVisitor visitor) {
+	translator.initFunctionEntity(n, (WalkContext)context, (WalkContext)codeContext);
 	return false;
     }
-    protected void leaveAsyncBodyEntity(CAstEntity n, Context context, Context codeContext) {
-	closeFunctionEntity(n, (WalkContext)context, (WalkContext)codeContext);
+    protected void leaveAsyncBodyEntity(CAstEntity n, Context context, Context codeContext, CAstVisitor visitor) {
+	translator.closeFunctionEntity(n, (WalkContext)context, (WalkContext)codeContext);
+    }
+
+    private void translate(final CAstEntity N, final String nm) {
+	if (translator.DEBUG_TOP)
+	    Trace.println("translating " + nm);
+	visitEntities(N, new DefaultContext(translator, N, nm), this);
+    }
+
+    /* UGH! */
+    public void translate() {
+	CAstEntity fSourceEntity = translator.sourceFileEntity();
+	translate(fSourceEntity, fSourceEntity.getName());
     }
 }
+
