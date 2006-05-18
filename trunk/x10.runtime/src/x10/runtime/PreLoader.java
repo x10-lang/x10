d48736c2fe2479988cd64b10334d6329e82e65f7
Igor Peshansky <igorp@us.ibm.com>
2006-05-18 14:38:37 +0000

    Minor private API changes Add Javadoc Better error reporting Skip parsing of array classes Fix bug in parsing long and double constant pool entries Fix bug in reading UTF-8 strings Add functionality for printing constant pool entries
    git-svn-id: svn://svn.code.sf.net/p/x10/code@2206 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.runtime/src/x10/runtime/PreLoader.java
+++ trunk/x10.runtime/src/x10/runtime/PreLoader.java
@@ -15,12 +15,12 @@ import java.util.*;
 public class PreLoader {
 	public static void main(String[] args) {
 //		try {
-//			String name = getClassName(PreLoader.class);
+//			String name = getClassFile(PreLoader.class);
 //			if (args.length > 0)
 //				name = args[0];
 //			InputStream in = getClassAsStream(name, PreLoader.class);
 //			byte[][] cp = getConstantPool(in);
-//			String[] ref_classes = getReferencedClasses(cp);
+//			String[] ref_classes = getReferencedClasses(name, cp);
 //			for (int i = 0; i < ref_classes.length; i++)
 //				System.out.println(i+": "+ref_classes[i]);
 //		} catch (IOException e) { e.printStackTrace(); }
@@ -32,39 +32,55 @@ public class PreLoader {
 	private static final String TRUE = "true";
 	private static final Map inited = new HashMap();
 	private static final ClassLoader bootstrap = Object.class.getClassLoader();
+	/**
+	 * Recursively pre-load the given class and all the classes it statically
+	 * references.
+	 * @param c the class to pre-load
+	 */
 	public static void preLoad(Class c) {
-		preLoad(getClassName(c), c);
+		preLoad(getClassFile(c), c);
 	}
+	/**
+	 * Recursively pre-load the given class and all the classes it statically
+	 * references, optionally interning string constants.
+	 * @param c the class to pre-load
+	 * @param intern whether to intern string constants
+	 */
 	public static void preLoad(Class c, boolean intern) {
-		preLoad(getClassName(c), c, intern);
+		preLoad(getClassFile(c), c, intern);
 	}
-	public static void preLoad(String name, Class c) {
+	private static void preLoad(String name, Class c) {
 		preLoad(name, c, true);
 	}
-	public static void preLoad(String name, Class c, boolean intern) {
+	private static void preLoad(String name, Class c, boolean intern) {
 		if (inited.get(name) != null) return;
 		inited.put(name, TRUE);
 //		System.out.println("Pre-loading '"+name+"'");
 		try {
 			InputStream in = getClassAsStream(name, c);
-			byte[][] cp = getConstantPool(in);
+			byte[][] cp = getConstantPool(name, in);
+//			for (int j = 1; j < cp.length; j++)
+//				printConstantPoolEntry(cp, j, System.out);
 			if (intern) internStrings(cp);
-			String[] ref_classes = getReferencedClasses(cp);
+			String[] ref_classes = getReferencedClasses(name, cp);
 			for (int i = 0; i < ref_classes.length; i++) {
 				String nm = toClassName(ref_classes[i]);
 				try {
 					Class u = Class.forName(nm);
 //					System.out.println(i+": "+nm+" -> "+u);
+					// Skip arrays
+					if (nm.charAt(0) == '[') continue;
+					// Skip system classes
 					if (u.getClassLoader() == bootstrap) continue;
 					preLoad(toFileName(nm), c, intern);
 				} catch (ClassNotFoundException e) {
 //					System.out.println(i+": "+nm+" not found");
 				}
 			}
-		} catch (IOException e) { assert false; }
+		} catch (IOException e) { e.printStackTrace(System.out); assert false; }
 	}
 	private static InputStream getClassAsStream(Class c) throws IOException {
-		return getClassAsStream(getClassName(c), c);
+		return getClassAsStream(getClassFile(c), c);
 	}
 	private static InputStream getClassAsStream(String name, Class c) throws IOException {
 		InputStream cin = c.getClassLoader().getResourceAsStream(name);
@@ -78,7 +94,7 @@ public class PreLoader {
 	private static String toFileName(String n) {
 		return n.replace('.','/')+".class";
 	}
-	private static String getClassName(Class c) {
+	private static String getClassFile(Class c) {
 		return toFileName(c.getName());
 	}
 	private static byte[] getClassBytes(String name, Class c) throws IOException {
@@ -98,7 +114,7 @@ public class PreLoader {
 			} catch (UnsupportedEncodingException e) { }
 		}
 	}
-	private static String[] getReferencedClasses(byte[][] cp) {
+	private static String[] getReferencedClasses(String name, byte[][] cp) {
 		int num_classes = 0;
 		for (int i = 1; i < cp.length; i++)
 			if (cp[i][0] == CONSTANT_Class)
@@ -109,7 +125,7 @@ public class PreLoader {
 			if (cp[i][0] != CONSTANT_Class) continue;
 			int name_idx = (cp[i][1] & 0xFF) << 8 | (cp[i][2] & 0xFF);
 			if (cp[name_idx][0] != CONSTANT_Utf8)
-				throw new Error("Class entry "+i+" refers to non-utf entry "+name_idx);
+				throw new Error("Class entry "+i+" refers to non-utf ("+cp[name_idx][0]+") entry "+name_idx+" in "+name);
 			int name_len = (cp[name_idx][1] & 0xFF) << 8 | (cp[name_idx][2] & 0xFF);
 			try {
 				ref_classes[j++] = new String(cp[name_idx], 3, name_len, "UTF-8").intern();
@@ -117,6 +133,14 @@ public class PreLoader {
 		}
 		return ref_classes;
 	}
+	private static byte[][] getConstantPool(String name, byte[] classbytes) {
+		try {
+			return getConstantPool(name, new ByteArrayInputStream(classbytes));
+		} catch (IOException e) {
+			throw new Error("Unable to read from an array of bytes");
+		}
+	}
+	private static final int CONSTANT_Empty = 0;
 	private static final int CONSTANT_Class = 7;
 	private static final int CONSTANT_Fieldref = 9;
 	private static final int CONSTANT_Methodref = 10;
@@ -128,10 +152,12 @@ public class PreLoader {
 	private static final int CONSTANT_Double = 6;
 	private static final int CONSTANT_NameAndType = 12;
 	private static final int CONSTANT_Utf8 = 1;
-	private static byte[][] getConstantPool(InputStream in) throws IOException {
+	private static final byte[] EMPTY = new byte[] { CONSTANT_Empty };
+	private static byte[][] getConstantPool(String name, InputStream in) throws IOException {
 		for (int i = 0; i < 8; i++) in.read();
 		int cp_size = ((in.read() & 0xFF) << 8 | (in.read() & 0xFF));
 		byte[][] cp = new byte[cp_size][];
+		cp[0] = EMPTY;
 		for (int i = 1; i < cp_size; i++) {
 			byte tag = (byte)in.read();
 			switch (tag) {
@@ -156,6 +182,7 @@ public class PreLoader {
 											  (byte)in.read(), (byte)in.read(),
 											  (byte)in.read(), (byte)in.read(),
 											  (byte)in.read(), (byte)in.read() };
+					cp[++i] = EMPTY;
 					break;
 				case CONSTANT_NameAndType:
 					cp[i] = new byte[] { tag, (byte)in.read(), (byte)in.read(),
@@ -168,21 +195,101 @@ public class PreLoader {
 						int len = (l_h & 0xFF) << 8 | (l_l & 0xFF);
 						cp[i] = new byte[len+3];
 						cp[i][0] = tag; cp[i][1] = l_h; cp[i][2] = l_l;
-						in.read(cp[i], 3, len);
+						int off = 3;
+						int n;
+						while ((n = in.read(cp[i], off, len)) != len) {
+							len -= n;
+							off += n;
+						}
 					}
 					break;
 				default:
-					throw new Error("Unknown constant pool tag: "+tag);
+					throw new Error("Unknown constant pool tag ("+tag+") at entry "+i+" in "+name);
 			}
+//			printConstantPoolEntry(cp, i-1, System.out);
+//			printConstantPoolEntry(cp, i, System.out);
 		}
 		return cp;
 	}
-	private static byte[][] getConstantPool(byte[] classbytes) {
-		try {
-			return getConstantPool(new ByteArrayInputStream(classbytes));
-		} catch (IOException e) {
-			throw new Error("Unable to read from an array of bytes");
+	private static final String[] TAGS = { "EMPTY", "UTF8", null, "INTEGER",
+		"FLOAT", "LONG", "DOUBLE", "CLASS", "STRING", "FIELDREF", "METHODREF",
+		"IMETHOD", "NAME/TYPE" };
+	private static void printConstantPoolEntry(byte[][] cp, int i, PrintStream out) {
+		byte[] entry = cp[i];
+		byte tag = entry[0];
+		int tmp1, tmp2;
+		out.print(i+": "+TAGS[tag]);
+		switch (tag) {
+			case CONSTANT_Empty:
+				out.println();
+				break;
+			case CONSTANT_Class:
+			case CONSTANT_String:
+				tmp1 = (entry[1] & 0xFF) << 8 | (entry[2] & 0xFF);
+				out.println("["+tmp1+"]");
+//				out.print(" -> ");
+//				printConstantPoolEntry(cp, tmp1, out);
+				break;
+			case CONSTANT_Fieldref:
+			case CONSTANT_Methodref:
+			case CONSTANT_InterfaceMethodref:
+				tmp1 = (entry[1] & 0xFF) << 8 | (entry[2] & 0xFF);
+				tmp2 = (entry[3] & 0xFF) << 8 | (entry[4] & 0xFF);
+				out.println("["+tmp1+","+tmp2+"]");
+//				out.print(" -> ");
+//				printConstantPoolEntry(cp, tmp1, out);
+//				out.print(" -> ");
+//				printConstantPoolEntry(cp, tmp2, out);
+				break;
+			case CONSTANT_Integer:
+			case CONSTANT_Float:
+				tmp1 = ((entry[1] & 0xFF) << 8 | (entry[2] & 0xFF)) << 16 |
+					   ((entry[3] & 0xFF) << 8 | (entry[4] & 0xFF));
+				if (tag == CONSTANT_Float)
+					out.println(" = "+toFloat(tmp1));
+				else
+					out.println(" = "+tmp1);
+				break;
+			case CONSTANT_Long:
+			case CONSTANT_Double:
+				tmp1 = ((entry[1] & 0xFF) << 8 | (entry[2] & 0xFF)) << 16 |
+					   ((entry[3] & 0xFF) << 8 | (entry[4] & 0xFF));
+				tmp2 = ((entry[5] & 0xFF) << 8 | (entry[6] & 0xFF)) << 16 |
+					   ((entry[7] & 0xFF) << 8 | (entry[8] & 0xFF));
+				if (tag == CONSTANT_Double)
+					out.println(" = "+toDouble(tmp1,tmp2));
+				else
+					out.println(" = "+toLong(tmp1,tmp2));
+				break;
+			case CONSTANT_NameAndType:
+				tmp1 = (entry[1] & 0xFF) << 8 | (entry[2] & 0xFF);
+				tmp2 = (entry[3] & 0xFF) << 8 | (entry[4] & 0xFF);
+				out.println("["+tmp1+","+tmp2+"]");
+//				out.print(" -> ");
+//				printConstantPoolEntry(cp, tmp1, out);
+//				out.print(" -> ");
+//				printConstantPoolEntry(cp, tmp2, out);
+				break;
+			case CONSTANT_Utf8:
+				tmp1 = (entry[1] & 0xFF) << 8 | (entry[2] & 0xFF);
+				try {
+					out.println(" = \""+new String(entry, 3, tmp1, "UTF-8")+"\"");
+				} catch (UnsupportedEncodingException e) { }
+				break;
+			default:
+				throw new Error("Unknown constant pool tag ("+tag+") at entry "+i);
 		}
+	}
+	private static float toFloat(int v) {
+		return Float.intBitsToFloat(v);
+	}
+	private static long toLong(int h, int l) {
+		long r = h;
+		return r << 32 + l;
+	}
+	private static double toDouble(int h, int l) {
+		long r = h;
+		return Double.longBitsToDouble(r << 32 + l);
 	}
 }
 
