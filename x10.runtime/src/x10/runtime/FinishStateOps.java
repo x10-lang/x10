5838a5a36607eb188c48809c5c116863db0126f0
xinb <user@domain>
2006-05-24 23:38:08 +0000

    Improve the 'finish' implementation to reduce finished-related message sent at runtime.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/branches/CLUSTER@2226 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.runtime/src/x10/runtime/FinishStateOps.java
+++ x10.runtime/src/x10/runtime/FinishStateOps.java
@@ -3,22 +3,22 @@
  */
 package x10.runtime;
 
-import java.rmi.Remote;
-import java.rmi.RemoteException;
 import java.util.Stack;
 
 /**
  * @author xinb
  *
  */
-public interface FinishStateOps /*extends Remote*/ {
+public interface FinishStateOps {
 	//called remotedly from child activity
-	public void notifySubActivityTermination() throws RemoteException;
-    public void notifySubActivityTermination(Throwable t) throws RemoteException;
+	public void notifySubActivityTermination();
+    public void notifySubActivityTermination(Throwable t);
+    public void notifySubActivitySpawn();
     
     //called locally only?
-    public void notifySubActivitySpawn() throws RemoteException;
-    public Stack exceptions() throws RemoteException;
-    public void waitForFinish() throws RemoteException;
-    public void pushException( Throwable t) throws RemoteException;
+    public Stack exceptions();
+    public void waitForFinish();
+    public void pushException( Throwable t);
+    public void notifySubActivitySpawnAtChild(boolean fromRoot);
+    public boolean notShadow();
 }
