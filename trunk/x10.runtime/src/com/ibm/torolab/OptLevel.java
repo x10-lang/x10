09356558f375796aca105f54ca71f181d3b53b9a
Chris Donawa <donawa@domain>
2006-06-17 01:56:22 +0000

    for annotation
    git-svn-id: svn://svn.code.sf.net/p/x10/code@2263 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ trunk/x10.runtime/src/com/ibm/torolab/OptLevel.java
@@ -0,0 +1,15 @@
+package com.ibm.torolab;
+
+/* enumerations for each of TR's optimization levels
+ */
+
+public enum OptLevel {
+   NOOPT,
+   COLD,
+   WARM,
+   HOT,
+   VERYHOT,
+   SCORCHING,
+   AOT
+};
+
