9caafe5aeec9c6f61f94b063a62be5d49a378c21
Igor Peshansky <igorp@us.ibm.com>
2006-06-24 04:28:13 +0000

    Update header comment Include sys/types.h Include generated stubs and remove stub code Rename C to BasicExternTest1$C and update function names
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@2292 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.common/examples/Constructs/Extern/BasicExternTest1Impl.c
+++ x10.common/examples/Constructs/Extern/BasicExternTest1Impl.c
@@ -1,121 +1,109 @@
 /*
- * Filename:BasicExternTest1_Impl.c,
- * taken from generated stub
- * compile this into a library Extern1 e.g. Extern1.dll on windows, libExtern1.so on AIX etc
- * for windows, I use the command:
+ * Filename:BasicExternTest1Impl.c
+ * implements the methods in the generated stub.
+ *
+ * Compile this into a library BasicExtern1 e.g. BasicExtern1.dll on windows,
+ * libBasicExtern1.so on AIX, etc.
+ *
+ * [CMD] For windows, I use the command:
 
-   cl "-Ic:/Program Files/IBM/Java142/include" 
-       "-Ic:/Program Files/IBM/Java142/include/win32" -LD BasicExternTest1Impl.c -FeExtern1.dll
- where my java is installed in c:/Program Files/IBM/Java142
+   cl "-Ic:/Program Files/IBM/Java142/include" \
+      "-Ic:/Program Files/IBM/Java142/include/win32" \
+      -LD BasicExternTest1Impl.c -FeBasicExtern1.dll
 
+ * where my java is installed in c:/Program Files/IBM/Java142
+ *
+ * [IP] In Cygwin, I used
+
+   gcc -mno-cygwin -I"$JAVA_HOME/include" -I"$JAVA_HOME/include/win32" \
+       -shared -o /dev/null -Wl,--output-def=BasicExtern1.din \
+       BasicExternTest1Impl.c
+   sed -e '/Java_/ {; h; s|Java_|_Java_|; G; s| @[0-9]\+\n *| = |' \
+       -e 's| @[0-9]\+$||; H; s|00024||; x; G }' < BasicExtern1.din \
+       > BasicExtern1.def
+   dllwrap -mno-cygwin -I"$JAVA_HOME/include" -I"$JAVA_HOME/include/win32" \
+       -shared -o BasicExtern1.dll --def BasicExtern1.def \
+       BasicExternTest1Impl.c
+
+ * where JAVA_HOME was set to my java installation directory:
+ * "/cygdrive/c/Program Files/IBM/Java50"
+ *
+ * [IP] In Linux/AIX, a simple
+
+   gcc -I"$JAVA_HOME/include" -I"$JAVA_HOME/include/win32" \
+       -shared -o libBasicExtern1.so BasicExternTest1Impl.c
+
+ * worked fine (again, JAVA_HOME being the java install directory)
+ *
+ * The Makefile in this directory will take care of the latter two
+ * cases, using GNU make and gcc/binutils.
+ *
+ * [AHK] On AIX, I used
+   xlc_r -I $JAVA_HOME/include -g -qnolm -bM:SHR -bnoentry -bexpall \
+         -o libBasicExtern1.a BasicExternTest1Impl.c
+ * [CMD] On AIX, use
+   xlC -I/usr/java131/include -qmkshrobj -o libBasicExtern1.so \
+       BasicExternTest1Impl.c
  */
-
+#include <sys/types.h>
 #include <jni.h>
+
+#include "BasicExternTest1_x10stub.c"
+#include "BasicExternTest1_00024C_x10stub.c"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-/* * * * * * * */
-  signed int C_doit(){ return 100;}
-JNIEXPORT jint JNICALL
-Java_C_C_1doit(JNIEnv *env, jobject obj){
-   return C_doit();
-}
-/* * * * * * * */
-  signed int BasicExternTest1_doit(){ return 12;}
-JNIEXPORT jint JNICALL
-Java_BasicExternTest1_BasicExternTest1_1doit(JNIEnv *env, jobject obj){
-   return BasicExternTest1_doit();
-}
+  signed int BasicExternTest1_00024C_doit() { return 100; }
 
-/* * * * * * * */
-  signed int BasicExternTest1_overload__F(float f){
-    if(12 == f) return 100;
+  signed int BasicExternTest1_doit() { return 12; }
+
+  signed int BasicExternTest1_overload__F(float f) {
+    if (12 == f) return 100;
     return 2;
   }
 
-JNIEXPORT jint JNICALL
-Java_BasicExternTest1_BasicExternTest1_1overload__F(JNIEnv *env, jobject obj, jfloat f){
-   return BasicExternTest1_overload__F( f);
-}
-
-/* * * * * * * */
-  signed int BasicExternTest1_overload__J(jlong l){
-    if(12 == l) return ++l;
+  signed int BasicExternTest1_overload__J(jlong l) {
+    if (12 == l) return ++l;
     else return 44;
   }
-JNIEXPORT jint JNICALL
-Java_BasicExternTest1_BasicExternTest1_1overload__J(JNIEnv *env, jobject obj, jlong l){
-   return BasicExternTest1_overload__J( l);
-}
 
-/* * * * * * * */
-  signed int BasicExternTest1_alltypes(unsigned char bool, signed char bt, signed short ch, signed short sh, signed int i, jlong l, float f, double d){
-    if(!bool) return 9;
-    if(bt != 1) return 10; 
-    if(ch != 2) return 11; 
-    if(sh != 3) return 12; 
-    if(i != 4) return 13; 
-    if(l != 5) return 14; 
-    if(f != (float) 12) return 15; 
-    if(d - (double)1.2 > 0.0000001)  return 16;
+  signed int BasicExternTest1_alltypes(unsigned char bool, signed char bt, signed short ch, signed short sh, signed int i, jlong l, float f, double d) {
+    if (!bool) return 9;
+    if (bt != 1) return 10; 
+    if (ch != 2) return 11; 
+    if (sh != 3) return 12; 
+    if (i != 4) return 13; 
+    if (l != 5) return 14; 
+    if (f != (float) 12) return 15; 
+    if (d - (double)1.2 > 0.0000001) return 16;
     return 101;
   }
-JNIEXPORT jint JNICALL
-Java_BasicExternTest1_BasicExternTest1_1alltypes(JNIEnv *env, jobject obj, jboolean bool, jbyte bt, jchar ch, jshort sh, jint i, jlong l, jfloat f, jdouble d){
-   return BasicExternTest1_alltypes( bool,  bt,  ch,  sh,  i,  l,  f,  d);
-}
 
-/* * * * * * * */
-  unsigned char BasicExternTest1_returnBoolean(signed int x){
-    return ( x == 11);
+  unsigned char BasicExternTest1_returnBoolean(signed int x) {
+    return (x == 11);
   }
-JNIEXPORT jboolean JNICALL
-Java_BasicExternTest1_BasicExternTest1_1returnBoolean(JNIEnv *env, jobject obj, jint x){
-   return BasicExternTest1_returnBoolean( x);
-}
 
-/* * * * * * * */
-  signed char BasicExternTest1_returnByte(signed int x){
-    if(x == 12) return 12;
+  signed char BasicExternTest1_returnByte(signed int x) {
+    if (x == 12) return 12;
     return 33;
   }
-JNIEXPORT jbyte JNICALL
-Java_BasicExternTest1_BasicExternTest1_1returnByte(JNIEnv *env, jobject obj, jint x){
-   return BasicExternTest1_returnByte( x);
-}
 
-
-/* * * * * * * */
-  jlong BasicExternTest1_returnLong(signed int x){
-  
-    if(x == 101) return 55;
+  jlong BasicExternTest1_returnLong(signed int x) {
+    if (x == 101) return 55;
     return 66;
   }
-JNIEXPORT jlong JNICALL
-Java_BasicExternTest1_BasicExternTest1_1returnLong(JNIEnv *env, jobject obj, jint x){
-   return BasicExternTest1_returnLong( x);
-}
 
-/* * * * * * * */
- float BasicExternTest1_returnFloat(signed int x){
-    if(x == 122) return 1007;
+  float BasicExternTest1_returnFloat(signed int x) {
+    if (x == 122) return 1007;
     return -1;
   }
-JNIEXPORT jfloat JNICALL
-Java_BasicExternTest1_BasicExternTest1_1returnFloat(JNIEnv *env, jobject obj, jint x){
-   return BasicExternTest1_returnFloat( x);
-}
 
-/* * * * * * * */
-  double BasicExternTest1_returnDouble(signed int x){
-    if(x == 99) return 100.3;
+  double BasicExternTest1_returnDouble(signed int x) {
+    if (x == 99) return 100.3;
     return 10.1;
   }
-JNIEXPORT jdouble JNICALL
-Java_BasicExternTest1_BasicExternTest1_1returnDouble(JNIEnv *env, jobject obj, jint x){
-   return BasicExternTest1_returnDouble( x);
-}
 
 #ifdef __cplusplus
 }
