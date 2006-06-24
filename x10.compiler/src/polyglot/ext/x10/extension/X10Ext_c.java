296500402f0794e2729400ee31342f030aaf5b3c
Igor Peshansky <igorp@us.ibm.com>
2006-06-24 04:28:07 +0000

    Pass ExtensionInfo to rewrite()
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@2289 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.compiler/src/polyglot/ext/x10/extension/X10Ext_c.java
+++ x10.compiler/src/polyglot/ext/x10/extension/X10Ext_c.java
@@ -2,11 +2,12 @@ package polyglot.ext.x10.extension;
 
 import polyglot.ast.Node;
 import polyglot.ast.NodeFactory;
+import polyglot.frontend.ExtensionInfo;
 import polyglot.ext.jl.ast.Ext_c;
 import polyglot.ext.x10.types.X10TypeSystem;
 
 public class X10Ext_c extends Ext_c implements X10Ext {
-    public Node rewrite(X10TypeSystem ts, NodeFactory nf) {
+    public Node rewrite(X10TypeSystem ts, NodeFactory nf, ExtensionInfo info) {
         return node();
     }
 }
