367e04e29a7b6766bb881d16a1fd40b7162de266
Igor Peshansky <igorp@us.ibm.com>
2006-06-24 04:28:15 +0000

    Include sys/types.h and stdio.h Include generated stubs and remove stub code
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@2293 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.common/examples/Constructs/Extern/IntArrayExternUnsafeImpl.c
+++ x10.common/examples/Constructs/Extern/IntArrayExternUnsafeImpl.c
@@ -5,38 +5,35 @@
 cl "-Ic:/Program Files/IBM/Java142/include" "-Ic:/Program Files/IBM/Java142/include/win32" 
    -LD TestExternUnsafe1Impl.c -FeTestExternUnsafe1.dll
 */
+#include <sys/types.h>
 #include <jni.h>
+#include <stdio.h>
+
+#include "IntArrayExternUnsafe_x10stub.c"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-/* * * * * * * */
-extern void IntArrayExternUnsafe_howdy(int yy[], int yy_x10DeScRiPtOr[]);
-JNIEXPORT void JNICALL
-Java_IntArrayExternUnsafe_IntArrayExternUnsafe_1howdy(JNIEnv *env, jobject obj, jlong yy, jlong yy_x10DeScRiPtOr ){
-   IntArrayExternUnsafe_howdy( (int *)yy, (int *)yy_x10DeScRiPtOr);
-}
+  void IntArrayExternUnsafe_howdy(int array[], int descriptor[]) {
+    int i;	
+    int rank = descriptor[0];
+    int size;
+    if(rank != 1) {
+      printf("Error: expected rank of 1, not %d\n",rank);
+      return;
+    }	
 
+    size = descriptor[1];
+    printf("Extern call: passed in array address %p, size %d\n",array,size);
+    fflush(stdout);
 
-
- void IntArrayExternUnsafe_howdy(int array[], int descriptor[]){
-  int i;	
-  int rank = descriptor[0];
-  int size;
-  if(rank != 1) {
-    printf("Error: expected rank of 1, not %d\n",rank);
-    return;
-  }	
-     
-  size = descriptor[1];
-  printf("Extern call: passed in array address %p, size %d\n",array,size);
-  fflush(stdout);
-
-  for(i = 0;i < size;++i){
-    array[i] += 100;
-    //printf("[%d]:%d (%p=%d)\n",i,array[i],&(array[i]),&(array[i]));
+    for(i = 0; i < size; ++i) {
+      array[i] += 100;
+      //printf("[%d]:%d (%p=%d)\n",i,array[i],&(array[i]),&(array[i]));
+    }
   }
-}
+
 #ifdef __cplusplus
 }
 #endif
