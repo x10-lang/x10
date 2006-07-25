32154e7ecef73486208a59fc4427c96e5e208413
Philippe Charles <pcharles@domain>
2006-07-25 22:26:21 +0000

    output for new x10.g
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@2388 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.compiler/src/x10/parser/X10KWLexer.java
+++ x10.compiler/src/x10/parser/X10KWLexer.java
@@ -1,12 +1,15 @@
+
 //
 // Licensed Material 
 // (C) Copyright IBM Corp, 2006
 //
+
 package x10.parser;
 
 import lpg.lpgjavaruntime.*;
 import java.util.*;
 import java.util.*;
+
 public class X10KWLexer extends X10KWLexerprs implements X10Parsersym
 {
     private char[] inputChars;
@@ -39,6 +42,7 @@ public class X10KWLexer extends X10KWLexerprs implements X10Parsersym
     }
 
     public void setInputChars(char[] inputChars) { this.inputChars = inputChars; }
+
 
     final static int tokenKind[] = new int[128];
     static
@@ -75,6 +79,7 @@ public class X10KWLexer extends X10KWLexerprs implements X10Parsersym
     {
         return (c < 128 ? tokenKind[c] : 0);
     }
+
 
     public X10KWLexer(char[] inputChars, int identifierKind)
     {
@@ -555,6 +560,7 @@ public class X10KWLexer extends X10KWLexerprs implements X10Parsersym
                 keywordKind[79] = (TK_when);
       
     
+
         for (int i = 0; i < keywordKind.length; i++)
         {
             if (keywordKind[i] == 0)
