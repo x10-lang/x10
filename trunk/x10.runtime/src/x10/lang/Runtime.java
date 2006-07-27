472041b4ef4ed7764412d31a9b67ed67d3857a23
Igor Peshansky <igorp@us.ibm.com>
2006-07-27 23:13:05 +0000

    Add usage information
    git-svn-id: svn://svn.code.sf.net/p/x10/code@2395 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.runtime/src/x10/lang/Runtime.java
+++ trunk/x10.runtime/src/x10/lang/Runtime.java
@@ -2,6 +2,8 @@ package x10.lang;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
+import java.io.PrintStream;
+import java.util.StringTokenizer;
 
 import x10.runtime.Activity;
 import x10.runtime.Configuration;
@@ -9,6 +11,9 @@ import x10.runtime.DefaultRuntime_c;
 import x10.runtime.Place;
 import x10.runtime.PoolRunner;
 import x10.runtime.Report;
+
+import x10.runtime.util.ConfigurationError;
+import x10.runtime.util.ShowUsageNotification;
 
 /**
  * This is the central entrypoint to the X10 Runtime for the
@@ -86,12 +91,117 @@ public abstract class Runtime {
 			String[] args_stripped = Configuration.parseCommandLine(args);
 			init();
 			runtime.run(args_stripped);
+		} catch (ShowUsageNotification e) {
+			usage(System.out, null);
+		} catch (ConfigurationError e) {
+			usage(System.err, e);
 		} catch (Exception e) {
 			Runtime.java.error("Unexpected Exception in X10 Runtime.", e);
 		}
 		// vj: The return from this method does not signal termination of the VM
 		// because a separate non-daemon thread has been spawned to execute Boot Activity.
 	}
+
+	/**
+	 * The name of the language this runtime represents.
+	 */
+	public static final String LANGUAGE = "x10";
+
+	/**
+	 * Print usage information
+	 */
+	public static void usage(PrintStream out, ConfigurationError err) {
+		if (err != null)
+			out.println("Error: "+err.getMessage());
+        out.println("Usage: " + LANGUAGE + " [options] " +
+                           "<main-class> [arg0 arg1 ...]");
+        out.println("where [options] includes:");
+		String[][] options = Configuration.options();
+		for (int i = 0; i < options.length; i++) {
+			usageForFlag(out, options[i][0], options[i][1]);
+		}
+	}
+
+	/* The following code was borrowed from Polyglot's Options.java */
+    /**
+     * The maximum width of a line when printing usage information. Used
+     * by <code>usageForFlag</code> and <code>usageSubsection</code>.
+     */
+    protected static final int USAGE_SCREEN_WIDTH = 76;
+
+    /**
+     * The number of spaces from the left that the descriptions for flags will
+	 * be displayed. Used by <code>usageForFlag</code>.
+     */
+    private static final int USAGE_FLAG_WIDTH = 34;
+
+    /**
+     * The number of spaces from the left that the flag names will be
+	 * indented. Used by <code>usageForFlag</code>.
+     */
+    private static final int USAGE_FLAG_INDENT = 4;
+
+    /**
+     * Utility method to print a number of spaces to a PrintStream.
+     * @param out output PrintStream
+     * @param n number of spaces to print.
+     */
+    protected static void printSpaces(PrintStream out, int n) {
+        while (n-- > 0) {
+            out.print(' ');
+        }
+    }
+
+    /**
+     * Output a flag and a description of its usage in a nice format.
+     *
+     * @param out output PrintStream
+     * @param flag
+     * @param description description of the flag.
+     */
+    protected static void usageForFlag(PrintStream out, String flag, String description) {
+		printSpaces(out, USAGE_FLAG_INDENT);
+        out.print(flag);
+        // cur is where the cursor is on the screen.
+        int cur = flag.length() + USAGE_FLAG_INDENT;
+
+        // print space to get up to indentation level
+        if (cur < USAGE_FLAG_WIDTH) {
+            printSpaces(out, USAGE_FLAG_WIDTH - cur);
+        }
+        else {
+            // the flag is long. Get a new line before printing the
+            // description.
+            out.println();
+            printSpaces(out, USAGE_FLAG_WIDTH);
+        }
+        cur = USAGE_FLAG_WIDTH;
+
+        // break up the description.
+        StringTokenizer st = new StringTokenizer(description);
+        while (st.hasMoreTokens()) {
+            String s = st.nextToken();
+            if (cur + s.length() > USAGE_SCREEN_WIDTH) {
+                out.println();
+                printSpaces(out, USAGE_FLAG_WIDTH);
+                cur = USAGE_FLAG_WIDTH;
+            }
+            out.print(s);
+            cur += s.length();
+            if (st.hasMoreTokens()) {
+                if (cur + 1 > USAGE_SCREEN_WIDTH) {
+                    out.println();
+                    printSpaces(out, USAGE_FLAG_WIDTH);
+                    cur = USAGE_FLAG_WIDTH;
+                }
+                else {
+                    out.print(" ");
+                    cur++;
+                }
+            }
+        }
+        out.println();
+    }
 
 	static int exitCode;
 
