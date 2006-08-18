d361d63ebe2700f65a1fd6d4d3aea49d659ba623
Vincent Cave <user@domain>
2006-08-18 18:38:24 +0000

    Abstract Metrics has been introduced to avoid creation of unnecessary class member in activiy.java and also to separate this aspect from the activity code
    git-svn-id: svn://svn.code.sf.net/p/x10/code@2466 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ trunk/x10.runtime/src/x10/runtime/abstractmetrics/AbstractMetrics.java
@@ -0,0 +1,29 @@
+package x10.runtime.abstractmetrics;
+
+public interface AbstractMetrics {
+
+	public abstract long getTotalOps();
+
+	public abstract long getCritPathOps();
+
+	public abstract void addLocalOps(long n);
+
+	public abstract void maxCritPathOps(long n);
+
+	public abstract long getTotalUnblockedTime();
+
+	public abstract long getCritPathTime();
+
+	public abstract void maxCritPathTime(long t);
+
+	public abstract long getResumeTime();
+
+	public abstract void setResumeTime();
+
+	public abstract void updateIdealTime();
+
+	public abstract long getCurrentTime();
+
+	public abstract void addUnblockedTime(long t);
+
+}
