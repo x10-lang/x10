7695adb5180e22f838e3df81c103f663ec23c995
Igor Peshansky <igorp@us.ibm.com>
2006-08-21 04:40:12 +0000

    Pull up "distribution", "rank", and "region" fields and accessors into x10Array
    git-svn-id: svn://svn.code.sf.net/p/x10/code@2478 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.runtime/src/x10/lang/doubleArray.java
+++ trunk/x10.runtime/src/x10/lang/doubleArray.java
@@ -9,20 +9,11 @@ package x10.lang;
  * @author vj 12/24/2004
  */
 
-import java.util.Iterator;
 
 abstract public class doubleArray extends x10Array{
 
-	public final dist distribution;
-	/*parameter*/ public final /*nat*/int rank /*= distribution.rank*/;
-	/*parameter*/ public final region/*(rank)*/ region /*= distribution.region*/;
-
-	public dist getDistribution() { return distribution;}
-	
 	protected doubleArray( dist D) {
-		this.distribution = D;
-		this.region = D.region;
-		this.rank = D.rank;
+		super(D);
 	}
 	
 	public static interface binaryOp {
@@ -304,11 +295,4 @@ abstract public class doubleArray extends x10Array{
 	 * @return an immutable version of this array.
 	 */
 	abstract public doubleArray toValueArray();
-	
-	public Iterator iterator() {
-	 	return region.iterator();
-	 }
-	public dist toDistribution() {
-		return distribution;
-	}
 }
--- trunk/x10.runtime/src/x10/lang/genericArray.java
+++ trunk/x10.runtime/src/x10/lang/genericArray.java
@@ -8,7 +8,6 @@ package x10.lang;
  * @author vj 12/24/2004
  */
 
-import java.util.Iterator;
 
 import x10.compilergenerated.Parameter1;
 
@@ -16,15 +15,8 @@ abstract public class genericArray /*( distribution distribution )*/
 /*implements Cloneable, Serializable */
 extends x10Array {
 
-	public final dist distribution;
-	/*parameter*/ public final /*nat*/int rank /*= distribution.rank*/;
-	/*parameter*/ public final region/*(rank)*/ region /*= distribution.region*/;
-	
-	public dist getDistribution() { return distribution;}
 	protected genericArray( dist D) {
-		this.distribution = D;
-		this.region = D.region;
-		this.rank = D.rank;
+		super(D);
 	}
 	
 	public static interface binaryOp {
@@ -191,11 +183,4 @@ extends x10Array {
 	 * @return an immutable version of this array.
 	 */
 	abstract public genericArray toValueArray();
-	
-	public Iterator iterator() {
-	 	return region.iterator();
-	 }
-	public dist toDistribution() {
-		return distribution;
-	}
 }
