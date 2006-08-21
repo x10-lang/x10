64dacacc55bf6f64a38319e8492b62b7f6165716
Igor Peshansky <igorp@us.ibm.com>
2006-08-21 04:40:06 +0000

    Get rid of owningPlace Pull up "distribution", "rank", and "region" fields and accessors Pull up toJava()
    git-svn-id: svn://svn.code.sf.net/p/x10/code@2476 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.runtime/src/x10/lang/x10Array.java
+++ trunk/x10.runtime/src/x10/lang/x10Array.java
@@ -1,21 +1,36 @@
 package x10.lang;
+import java.util.Iterator;
+
 import x10.lang.place;
 
 import x10.compilergenerated.Parameter1;
 
 /* root for all array classes */
 abstract public class x10Array extends x10.lang.Object implements Indexable, Unsafe, Parameter1, ValueType  {
-
-	// used to associate a partitioned global array with a
-	// particular place
-        private x10.lang.place _owningPlace; 
-	public place getOwningPlace() {assert (_owningPlace != null);return _owningPlace;}
-	public void setOwningPlace(place id) { _owningPlace = id;}
-
-	// copy src onto dest over regions on the this place
-	abstract public dist getDistribution();
-
+	public final dist distribution;
+	/*parameter*/ public final /*nat*/int rank /*= distribution.rank*/;
+	/*parameter*/ public final region/*(rank)*/ region /*= distribution.region*/;
+	protected x10Array(dist d) {
+		distribution = d;
+		rank = d.rank;
+		region = d.region;
+	}
 	
+	// TODO: Remove me
+	public final dist getDistribution() { return distribution; }
 
-       
+	/**
+	 * Return an iterator over the array's region.
+	 */
+	public final Iterator iterator() { return region.iterator(); }
+
+	/**
+	 * Return the array's distribution.
+	 */
+	public final dist toDistribution() { return distribution; }
+
+	/**
+	 * Create a multi-dimensional Java array.
+	 */
+	public abstract java.lang.Object toJava();
 }
