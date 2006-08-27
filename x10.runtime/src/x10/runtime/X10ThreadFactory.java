26343959c45e57009504354369e0c0fbac6d150e
Igor Peshansky <igorp@us.ibm.com>
2006-08-27 16:56:30 +0000

    Make thread and pool numbers 0-based
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@2508 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.runtime/src/x10/runtime/X10ThreadFactory.java
+++ x10.runtime/src/x10/runtime/X10ThreadFactory.java
@@ -5,32 +5,31 @@ import java.util.*;
 import java.lang.*;
 import java.util.concurrent.atomic.*;
 
-    /**
-	 * @author Raj Barik, Vivek Sarkar 
-	 * 3/6/2006: X10 thread factory for JCU implementation
-	 */
-
+/**
+ * X10 thread factory for JCU implementation.
+ * @author Raj Barik, Vivek Sarkar -- 3/6/2006
+ */
 public class X10ThreadFactory implements ThreadFactory {
-    static final AtomicInteger poolNumber = new AtomicInteger(1);
-    final ThreadGroup group;
-    final AtomicInteger threadNumber = new AtomicInteger(1);
-    final String namePrefix;
+	static final AtomicInteger poolNumber = new AtomicInteger(0);
+	final ThreadGroup group;
+	final AtomicInteger threadNumber = new AtomicInteger(0);
+	final String namePrefix;
 
-    public X10ThreadFactory() {
-        SecurityManager s = System.getSecurityManager();
-        group = (s != null)? s.getThreadGroup() :
-                             Thread.currentThread().getThreadGroup();
-        namePrefix = "pool-" +
-                      poolNumber.getAndIncrement() +
-                     "-thread-";
-    }
+	public X10ThreadFactory() {
+		SecurityManager s = System.getSecurityManager();
+		group = (s != null)? s.getThreadGroup() :
+							 Thread.currentThread().getThreadGroup();
+		namePrefix = "pool-" +
+					 poolNumber.getAndIncrement() +
+					 "-thread-";
+	}
 
-    public Thread newThread(Runnable r) {
-        Thread t = new PoolRunner(group, r, namePrefix + threadNumber.getAndIncrement());
-        if (t.isDaemon())
-            t.setDaemon(false);
-        if (t.getPriority() != Thread.NORM_PRIORITY)
-            t.setPriority(Thread.NORM_PRIORITY);
-        return t;
-    }
+	public Thread newThread(Runnable r) {
+		Thread t = new PoolRunner(group, r, namePrefix + threadNumber.getAndIncrement());
+		if (t.isDaemon())
+			t.setDaemon(false);
+		if (t.getPriority() != Thread.NORM_PRIORITY)
+			t.setPriority(Thread.NORM_PRIORITY);
+		return t;
+	}
 }
