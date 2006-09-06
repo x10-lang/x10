64cba4684362fa2905a4177349c8e5cc2156c696
Robert Fuhrer <rfuhrer@us.ibm.com>
2006-09-06 15:38:48 +0000

    New class to represent the function corresponding to an X10 async body.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@2539 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ trunk/com.ibm.wala.cast.x10/src/com/ibm/wala/cast/x10/translator/polyglot/X10AsyncObject.java
@@ -0,0 +1,31 @@
+/*
+ * Created on Apr 28, 2006
+ */
+package com.ibm.domo.ast.x10.translator.polyglot;
+
+import com.ibm.domo.ast.loader.AstFunctionClass;
+import com.ibm.domo.classLoader.IClassLoader;
+import com.ibm.domo.classLoader.IMethod;
+import com.ibm.domo.types.TypeReference;
+
+/**
+ * Represents the synthesized type that contains a single synthesized method that houses the async code body
+ * @author rfuhrer
+ */
+public class X10AsyncObject extends AstFunctionClass {
+    public X10AsyncObject(TypeReference reference, TypeReference superReference, IClassLoader loader, String fileName) {
+	super(reference, superReference, loader, fileName);
+    }
+
+    public X10AsyncObject(TypeReference reference, IClassLoader loader, String fileName) {
+	super(reference, loader, fileName);
+    }
+
+    public void setCodeBody(IMethod method) {
+	functionBody= method;
+    }
+
+    public String toString() {
+        return "Async" + super.toString();
+    }
+}
