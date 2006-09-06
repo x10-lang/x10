8b9b804cf10cc12223b2ccc3e91108a0d5a41488
Robert Fuhrer <rfuhrer@us.ibm.com>
2006-09-06 15:37:39 +0000

    Changed ctor signature to take the more specific X10SourceLoaderImpl to force clients to supply same (needed by translate() method).
    git-svn-id: svn://svn.code.sf.net/p/x10/code@2537 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/com.ibm.wala.cast.x10/src/com/ibm/wala/cast/x10/translator/X10ToIRTranslator.java
+++ trunk/com.ibm.wala.cast.x10/src/com/ibm/wala/cast/x10/translator/X10ToIRTranslator.java
@@ -6,14 +6,14 @@ package com.ibm.domo.ast.x10.translator;
 import java.io.PrintWriter;
 
 import com.ibm.capa.ast.CAstEntity;
-import com.ibm.domo.ast.java.loader.JavaSourceLoaderImpl;
 import com.ibm.domo.ast.java.translator.Java2IRTranslator;
 import com.ibm.domo.ast.java.translator.TranslatorToCAst;
 import com.ibm.domo.ast.x10.translator.polyglot.X10CAst2IRTranslator;
+import com.ibm.domo.ast.x10.translator.polyglot.X10SourceLoaderImpl;
 
 public class X10ToIRTranslator extends Java2IRTranslator {
     public X10ToIRTranslator(TranslatorToCAst sourceTranslator,
-			     JavaSourceLoaderImpl srcLoader) {
+			     X10SourceLoaderImpl srcLoader) {
 	super(sourceTranslator, srcLoader);
     }
 
@@ -24,6 +24,6 @@ public class X10ToIRTranslator extends Java2IRTranslator {
       X10CAstPrinter.printTo(ce, printWriter);
       printWriter.flush();
 
-      new X10CAst2IRTranslator(ce, fLoader).translate();
+      new X10CAst2IRTranslator(ce, (X10SourceLoaderImpl) fLoader).translate();
     }
 }
