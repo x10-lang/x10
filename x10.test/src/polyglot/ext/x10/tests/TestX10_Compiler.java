1a6ea2938bd80dc14d182a8d312337db644d998d
Robert Fuhrer <rfuhrer@us.ibm.com>
2006-09-14 16:52:50 +0000

    Changes to mostly fix the JUnit harness (at long last), in part by making use of new Runtime API that supports a client-supplied boot activity.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@2571 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.test/src/polyglot/ext/x10/tests/TestX10_Compiler.java
+++ x10.test/src/polyglot/ext/x10/tests/TestX10_Compiler.java
@@ -1,7 +1,5 @@
 /*
  * Created by vj on Jan 11, 2005
- *
- * 
  */
 package polyglot.ext.x10.tests;
 
@@ -10,13 +8,12 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
-
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 import x10.lang.Runtime;
 import x10.runtime.Activity;
 import x10.runtime.Configuration;
-import x10.runtime.Place;
+import x10.runtime.util.ConfigurationError;
 
 /**
  * JUnit testing harness for the X10 compiler.
@@ -54,135 +51,111 @@ import x10.runtime.Place;
  * @author vj -- Refactored to pull out the core code.
  * @author kemal -- supporting new test hierarchy
  */
-
 /**
  * @author vj Jan 11, 2005
- * 
+ * @author rfuhrer,igor Aug 3, 2006
  */
 public class TestX10_Compiler extends TestCase {
-
     static {
-        Configuration.parseCommandLine(null);
-        Runtime.init();
+	try {
+	    Configuration.parseCommandLine(null);
+	} catch (ConfigurationError e) {
+	    System.err.println(e.getMessage());
+	    e.printStackTrace();
+	}
+	Runtime.init();
     }
-    
-	/**
-	 * 
-	 */
-	public TestX10_Compiler() {
-		super();
-	}
-	/**
-     * Constructor for TestX10_CompilerPrettyPrinterVisitor.
-     * @param name
-     */
-	public TestX10_Compiler(String name ) {
-		super( name );
-	}
 
-	    
-	public static void Main(Class k) {
-		junit.textui.TestRunner.run(k);
-	}
-	
-	public static TestSuite suite(Class k ) {
-		return new TestSuite(k);
-	}
-	
-	protected final Activity a = new Activity() { public void run() {} }; // dummy
-	
-	/**
-	 * Junit may use additional threads to run the testcases
-	 * (other than the main one used to initialize the
-	 * Runtime class).  Hence we need a litte hack to register
-	 * the thread used to run the testcase as a 'local' thread
-	 * with the Runtime.
-	 */
-	public void setUp() {
-		/*
-        DefaultRuntime_c r = (DefaultRuntime_c) Runtime.runtime;
-		Place[] pls = Place.places();
-		Thread t = Thread.currentThread();
-		r.registerThread(t, pls[0]);
-		r.registerActivityStart(t, a, null);
-		*/
-	}
-	
-	
-	protected void compile(String dir, String file) {
-		String[] poargs = new String[] { "-sourcepath",dir,"-d",dir,"-ext", "x10", dir + file.substring(1) };
-		polyglot.main.Main.main( poargs ); // run compiler!
-	}
-	
-	protected void run(final String file, final String main, final String dir) {
-		Place.places()[0].runAsync(new Activity() {
-			public void run() {
-				try {
-					compile(dir, file);
-					String cwd=System.getProperty("user.dir")+
-					dir.substring(1);
-					ClassLoader loader = new URLClassLoader
-					(new URL[] {new URL("file://"+cwd+"/")}); 
-					Class c = loader.loadClass(main);
-					Object inst = c.newInstance();
-					Method m = c.getMethod("run", new Class[0]);
-					Boolean ret = (Boolean) m.invoke(inst, null);
-					assertTrue(ret.booleanValue());
-					System.out.println("Test "+main+" completed.");
-				} catch (IOException io) {
-					fail(io.toString());
-				} catch (InstantiationException ie) {
-					fail(ie.toString());
-				} catch (NoSuchMethodException nmse) {
-					fail(nmse.toString());
-				} catch (InvocationTargetException ite) {
-					fail(ite.getCause().getMessage());
-				} catch (ClassNotFoundException cnfe) { fail(cnfe.toString());
-				} catch (IllegalArgumentException iae) {
-					fail(iae.toString());
-				} catch (ClassFormatError cfe) {
-					fail(cfe.toString());
-				} catch (IllegalAccessException iae) {
-					iae.printStackTrace();
-					fail(iae.toString());
-				}
-			}
-		});
-	}
-	
-// runMain is no longer supported
-/*
-	protected void runMain(String file, String main) {
-		runMain(file, main, null);
-	}
-	
-	protected void runMain(String file, String main, String dir, String[] args) {
-		compile(file);
+    private boolean fTimedout;
+
+    public TestX10_Compiler(String name) {
+	super(name);
+	Activity a= new Activity() { public void run() {} }; // dummy
+    }
+
+    public static void Main(Class k) {
+	junit.textui.TestRunner.run(k);
+    }
+
+    public static TestSuite suite(Class k) {
+	return new TestSuite(k);
+    }
+
+    protected void compile(String dir, String file) {
+	dir= "../x10.common/examples/" + dir;
+	String srcPath= dir + ";" + "../x10.common/examples/x10lib";
+	String[] poargs= new String[] { "-sourcepath", srcPath, "-d", dir, "-ext", "x10", dir + file.substring(1) };
+	polyglot.main.Main.main(poargs); // run compiler!
+    }
+
+    protected void runHelper() {
+	String name= getName().substring(5);
+	int pathEnd= name.lastIndexOf('_');
+	if (name.substring(pathEnd+1).startsWith("MustFail"))
+	    pathEnd= name.lastIndexOf('_', pathEnd-1);
+	String path= "./" + name.substring(0, pathEnd).replace('_', '/');
+	String shortName= name.substring(pathEnd+1).replace('$', '_');
+	String x10SrcName= "./" + shortName + ".x10";
+
+	run(x10SrcName, shortName, path);
+    }
+
+    protected void run(final String file, final String main, final String dir) {
+	final Activity testActivity= new Activity() {
+	    public void run() {
 		try {
-			ClassLoader loader 
-			= new URLClassLoader(new URL[] { new URL("file://" + System.getProperty("user.dir") + "/") }); 
-			Class c
-			= loader.loadClass(main);
-			Method m
-			= c.getMethod("main", new Class[] { String[].class });
-			m.invoke(null, new Object[] {args});        
+		    compile(dir, file);
+		    String cwd= System.getProperty("user.dir") + "/../x10.common/examples/" + dir;
+		    ClassLoader loader= new URLClassLoader(new URL[] { new URL("file://" + cwd + "/") });
+		    Class c= loader.loadClass(main);
+		    Object inst= c.newInstance();
+		    Method m= c.getMethod("executeAsync", new Class[0]);
+		    Thread timer= startTimeoutTimer(10);
+		    Boolean ret= (Boolean) m.invoke(inst, null);
+		    assertFalse("Timeout exceeded.", fTimedout);
+		    assertTrue(ret.booleanValue());
+		    System.out.println("Test " + main + " completed.");
+		    timer.interrupt();
 		} catch (IOException io) {
-			fail(io.toString());
+		    fail(io.toString());
+		} catch (InstantiationException ie) {
+		    fail(ie.toString());
 		} catch (NoSuchMethodException nmse) {
-			fail(nmse.toString());
+		    fail(nmse.toString());
 		} catch (InvocationTargetException ite) {
-			fail(ite.toString());
+		    fail(ite.getCause().getMessage());
 		} catch (ClassNotFoundException cnfe) {
-			fail(cnfe.toString());
+		    fail(cnfe.toString());
 		} catch (IllegalArgumentException iae) {
-			fail(iae.toString());
+		    fail(iae.toString());
 		} catch (ClassFormatError cfe) {
-			fail(cfe.toString());
+		    fail(cfe.toString());
 		} catch (IllegalAccessException iae) {
-			fail(iae.toString());
+		    iae.printStackTrace();
+		    fail(iae.toString());
 		}
-	}
-*/
-	
+	    }
+	    /**
+	     * Start the timeout timer for the number of seconds specified in the
+	     * "x10test.timeout" system property (default is 300).
+	     * @return the timer thread.
+	     */
+	    private Thread startTimeoutTimer(final int seconds) {
+		Thread timer = new Thread(new Runnable() {
+		    public void run() {
+			if (Runtime.sleep(seconds*1000)) {
+			    fTimedout= true;
+			    Runtime.runtime.shutdown();
+			}
+		    }
+		});
+		timer.start();
+		return timer;
+	    }
+	};
+	Runtime.runtime.prepareForBoot();
+	Runtime.runtime.run(testActivity);
+	if (!fTimedout)
+	    Runtime.runtime.shutdown();
+    }
 }
-
