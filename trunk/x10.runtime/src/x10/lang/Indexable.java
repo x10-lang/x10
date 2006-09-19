6db065beeb934d61f66c34d7f69ac248dcb82695
Igor Peshansky <igorp@us.ibm.com>
2006-09-19 16:35:32 +0000

    Fix comment
    git-svn-id: svn://svn.code.sf.net/p/x10/code@2611 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.runtime/src/x10/lang/Indexable.java
+++ trunk/x10.runtime/src/x10/lang/Indexable.java
@@ -5,9 +5,9 @@
  */
 package x10.lang;
 
-/** A package protected interface implemented by x10.lang classes which can be accessed as arrays.
+/**
+ * An interface implemented by x10.lang classes which can be accessed as arrays.
  * @author vj Jan 13, 2005
- * 
  */
 public interface Indexable extends IndexableGet, IndexableSet {
     /**
