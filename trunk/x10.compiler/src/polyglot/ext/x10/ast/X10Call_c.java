1e22c28d7d8d9f7fe4f579ebe98e152b8ae61a67
Igor Peshansky <igorp@us.ibm.com>
2006-09-19 16:34:22 +0000

    Dispatch to static methods for some calls
    git-svn-id: svn://svn.code.sf.net/p/x10/code@2587 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.compiler/src/polyglot/ext/x10/ast/X10Call_c.java
+++ trunk/x10.compiler/src/polyglot/ext/x10/ast/X10Call_c.java
@@ -2,15 +2,22 @@ package polyglot.ext.x10.ast;
 
 import java.util.List;
 
+import polyglot.ast.Expr;
 import polyglot.ast.Node;
 import polyglot.ast.Receiver;
+import polyglot.ast.TypeNode;
 import polyglot.ext.jl.ast.Call_c;
+import polyglot.ext.x10.types.X10TypeSystem;
+import polyglot.types.NoMemberException;
 import polyglot.types.SemanticException;
+import polyglot.types.Type;
 import polyglot.util.Position;
+import polyglot.util.TypedList;
 import polyglot.visit.TypeChecker;
 
 /**
- * A method call wrapper to rewrite getLocation() calls on primitives.
+ * A method call wrapper to rewrite getLocation() calls on primitives
+ * and array operator calls.
  * @author Igor
  */
 public class X10Call_c extends Call_c {
@@ -20,17 +27,68 @@ public class X10Call_c extends Call_c {
     }
 
     /**
-     * Rewrite getLocation() to Here for value types, otherwise leave alone.
+     * Rewrite getLocation() to Here for value types and operator calls for
+     * array types, otherwise leave alone.
      */
     public Node typeCheck(TypeChecker tc) throws SemanticException {
+        X10NodeFactory xnf = (X10NodeFactory) tc.nodeFactory();
+        X10TypeSystem xts = (X10TypeSystem) tc.typeSystem();
         if (this.target != null && this.target.type().isPrimitive() &&
                 name().equals("getLocation") && arguments().isEmpty())
         {
-            X10NodeFactory xnf = (X10NodeFactory) tc.nodeFactory();
             return xnf.Here(position()).typeCheck(tc);
         }
 
+        try {
+            return super.typeCheck(tc);
+        } catch (NoMemberException e) {
+            if (e.getKind() != NoMemberException.METHOD || this.target == null)
+                throw e;
+            Type type = target.type();
+            if (!xts.isX10Array(type))
+                throw e;
+            // Special methods on arrays
+            String name = name();
+            List arguments = arguments();
+            Type elem = xts.baseType(type);
+            //reduce(), scan(), restriction(), union(), overlay(), update(), and lift()
+            if ((name.equals("reduce") && arguments.size() == 2 &&
+                    xts.isSubtype(((Expr)arguments.get(0)).type(), xts.OperatorBinary()) &&
+                    xts.isSubtype(((Expr)arguments.get(1)).type(), elem)) ||
+                (name.equals("scan") && arguments.size() == 2 &&
+                    xts.isSubtype(((Expr)arguments.get(0)).type(), xts.OperatorBinary()) &&
+                    xts.isSubtype(((Expr)arguments.get(1)).type(), elem)) ||
+                (name.equals("restriction") && arguments.size() == 1 &&
+                    isRestrictionArgType(((Expr)arguments.get(0)).type(), xts)) ||
+                (name.equals("union") && arguments.size() == 1 &&
+                    xts.isSubtype(type, ((Expr)arguments.get(0)).type())) ||
+                (name.equals("overlay") && arguments.size() == 1 &&
+                    xts.isSubtype(type, ((Expr)arguments.get(0)).type())) ||
+                (name.equals("update") && arguments.size() == 1 &&
+                    xts.isSubtype(type, ((Expr)arguments.get(0)).type())) ||
+                (name.equals("lift") &&
+                    ((arguments.size() == 2 &&
+                      xts.isSubtype(((Expr)arguments.get(0)).type(), xts.OperatorBinary()) &&
+                      xts.isSubtype(type, ((Expr)arguments.get(1)).type())) ||
+                     (arguments.size() == 1 &&
+                      xts.isSubtype(((Expr)arguments.get(0)).type(), xts.OperatorUnary()))))
+               )
+            {
+                TypeNode t = xnf.CanonicalTypeNode(position(), xts.ArrayOperations());
+                List newargs = TypedList.copy(arguments, Expr.class, false);
+                newargs.add(0, this.target);
+                return ((X10Call_c)this.target(t).arguments(newargs)).superTypeCheck(tc);
+            }
+            throw e;
+        }
+    }
+
+	private Node superTypeCheck(TypeChecker tc) throws SemanticException {
         return super.typeCheck(tc);
     }
+
+	private boolean isRestrictionArgType(Type type, X10TypeSystem xts) {
+		return xts.isPlace(type) || xts.isDistribution(type) || xts.isRegion(type);
+	}
 }
 
