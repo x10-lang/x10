18e83c19d6845514f1f017b6c4167d92ca2eedeb
Igor Peshansky <igorp@us.ibm.com>
2006-09-19 16:35:15 +0000

    Make an interface
    git-svn-id: svn://svn.code.sf.net/p/x10/code@2605 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.runtime/src/x10/lang/BooleanReferenceArray.java
+++ trunk/x10.runtime/src/x10/lang/BooleanReferenceArray.java
@@ -1,68 +1,38 @@
 package x10.lang;
 
-/** The class of all multidimensional, settable distributed int arrays
+
+/**
+ * The interface of all multidimensional, settable distributed int arrays
  * in X10.  Specialized from ReferenceArray by replacing the type parameter
  * with boolean.
 
  * Handtranslated from the X10 code in x10/lang/BooleanReferenceArray.x10
  * 
  * @author vj 1/9/2005
+ * @author igor 09/13/2006 -- made an interface
  */
-
-public abstract class BooleanReferenceArray extends booleanArray {
-	
-	public BooleanReferenceArray( dist D) {
-		super( D );
-	}
-
+public interface BooleanReferenceArray extends booleanArray {
 	abstract public boolean set( boolean v, point/*(region)*/ p);
 	abstract /*value*/ public boolean set(boolean v, int p);
 	abstract /*value*/ public boolean set(boolean v, int p, int q);
 	abstract /*value*/ public boolean set(boolean v, int p, int q, int r);
-	abstract /*value*/ public boolean set(boolean v, int p, int q, int r, int s);	
-	public boolean bitAndSet(boolean v, point/*(region)*/ p) {
-		return set(get(p)&v,p);
-	}
-	public boolean bitAndSet(boolean v, int p) {
-		return set(get(p)&v,p);
-	}
-	public boolean bitAndSet(boolean v, int p, int q) {
-		return set(get(p,q)&v,p,q);
-	}
-	public boolean bitAndSet(boolean v, int p, int q, int r) {
-		return set(get(p,q,r)&v,p,q,r);
-	}
-	public boolean bitAndSet(boolean v, int p, int q, int r, int s) {
-		return set(get(p,q,r,s)&v,p,q,r,s);
-	}
-	public boolean bitOrSet(boolean v, point/*(region)*/ p) {
-		return set(get(p)|v,p);
-	}
-	public boolean bitOrSet(boolean v, int p) {
-		return set(get(p)|v,p);
-	}
-	public boolean bitOrSet(boolean v, int p, int q) {
-		return set(get(p,q)|v,p,q);
-	}
-	public boolean bitOrSet(boolean v, int p, int q, int r) {
-		return set(get(p,q,r)|v,p,q,r);
-	}
-	public boolean bitOrSet(boolean v, int p, int q, int r, int s) {
-		return set(get(p,q,r,s)|v,p,q,r,s);
-	}
-	public boolean bitXorSet(boolean v, point/*(region)*/ p) {
-		return set(get(p)^v,p);
-	}
-	public boolean bitXorSet(boolean v, int p) {
-		return set(get(p)^v,p);
-	}
-	public boolean bitXorSet(boolean v, int p, int q) {
-		return set(get(p,q)^v,p,q);
-	}
-	public boolean bitXorSet(boolean v, int p, int q, int r) {
-		return set(get(p,q,r)^v,p,q,r);
-	}
-	public boolean bitXorSet(boolean v, int p, int q, int r, int s) {
-		return set(get(p,q,r,s)^v,p,q,r,s);
-	}
+	abstract /*value*/ public boolean set(boolean v, int p, int q, int r, int s);
+
+	boolean bitAndSet(boolean v, point/*(region)*/p);
+	boolean bitAndSet(boolean v, int p);
+	boolean bitAndSet(boolean v, int p, int q);
+	boolean bitAndSet(boolean v, int p, int q, int r);
+	boolean bitAndSet(boolean v, int p, int q, int r, int s);
+
+	boolean bitOrSet(boolean v, point/*(region)*/p);
+	boolean bitOrSet(boolean v, int p);
+	boolean bitOrSet(boolean v, int p, int q);
+	boolean bitOrSet(boolean v, int p, int q, int r);
+	boolean bitOrSet(boolean v, int p, int q, int r, int s);
+
+	boolean bitXorSet(boolean v, point/*(region)*/p);
+	boolean bitXorSet(boolean v, int p);
+	boolean bitXorSet(boolean v, int p, int q);
+	boolean bitXorSet(boolean v, int p, int q, int r);
+	boolean bitXorSet(boolean v, int p, int q, int r, int s);
 }
