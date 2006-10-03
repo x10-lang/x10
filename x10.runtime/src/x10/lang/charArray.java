2b52ca71a21ee9c34b9fb6cd4d964acb6ee578f2
Igor Peshansky <igorp@us.ibm.com>
2006-10-03 14:03:46 +0000

    Change uses of binaryOp and unaryOp to Operator.Binary and Operator.Unary
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@2654 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.runtime/src/x10/lang/charArray.java
+++ x10.runtime/src/x10/lang/charArray.java
@@ -1,36 +1,24 @@
 package x10.lang;
 
+import x10.array.Operator;
 
-
-/** The class of all multidimensional, distributed int arrays in X10. Has no mutable data.
- * Specialized from array by replacing the type parameter with int.
- 
+/**
+ * The class of all multidimensional, distributed char arrays in X10.
+ * Has no mutable data.
+ * Specialized from array by replacing the type parameter with char.
+ *
  * Handtranslated from the X10 code in x10/lang/CharArray.x10
- * 
+ *
  * @author vj 12/24/2004
  */
-
-
 abstract public class charArray extends x10Array {
 
 	protected charArray(dist D) {
 		super(D);
 	}
-	
-	public static interface binaryOp {
-		char apply(char r, char s);
-	}
 
-	public static interface unaryOp {
-		char apply(char r);
-	}
-
-	
-	public static interface pointwiseOp/*(region r)*/ {
-		char apply(point/*(r)*/ p);
-	}
-	
-	/** Return the value of the array at the given point in the
+	/**
+	 * Return the value of the array at the given point in the
 	 * region.
 	 */
 	abstract public char get(point/*(region)*/ p);
@@ -38,36 +26,36 @@ abstract public class charArray extends x10Array {
 	abstract /*value*/ public char get(int p, int q);
 	abstract /*value*/ public char get(int p, int q, int r);
 	abstract /*value*/ public char get(int p, int q, int r, int s);
-    abstract public char get(int[] p);
-    
+	abstract public char get(int[] p);
+
 	/** Return the value obtained by reducing the given array with the
 	 function fun, which is assumed to be associative and
 	 commutative. unit should satisfy fun(unit,x)=x=fun(x,unit).
 	 */
-	abstract public char reduce(binaryOp fun, char unit);
-	
-	/** Return a CharArray with the same distribution as this, by 
+	abstract public char reduce(Operator.Binary fun, char unit);
+
+	/** Return a CharArray with the same distribution as this, by
 	 scanning this with the function fun, and unit unit.
 	 */
-	abstract public CharReferenceArray/*(distribution)*/ scan(binaryOp fun, char unit);
-	
+	abstract public CharReferenceArray/*(distribution)*/ scan(Operator.Binary fun, char unit);
+
 	/** Return an array of B@P defined on the intersection of the
 	 region underlying the array and the parameter region R.
 	 */
 	abstract public /*(region(rank) R)*/
 	CharReferenceArray/*(distribution.restriction(R)())*/  restriction(region R);
-	
-	/** Return an array of B@P defined on the intersection of 
+
+	/** Return an array of B@P defined on the intersection of
 	 the region underlying this and the parametric distribution.
-	 */    
-	public  /*(distribution(:rank=this.rank) D)*/ 
+	 */
+	public  /*(distribution(:rank=this.rank) D)*/
 	CharReferenceArray/*(distribution.restriction(D.region)())*/ restriction(dist D) {
-	 return restriction(D.region);
+		return restriction(D.region);
 	}
 	public CharReferenceArray restriction(place P) {
 		return restriction(distribution.restriction(P));
 	}
-	
+
 	/** Take as parameter a distribution D of the same rank as *
 	 * this, and defined over a disjoint region. Take as argument an *
 	 * array other over D. Return an array whose distribution is the
@@ -76,33 +64,33 @@ abstract public class charArray extends x10Array {
 	 * in other.region.
 	 */
 	abstract public /*(distribution(:region.disjoint(this.region) && rank=this.rank) D)*/
-	CharReferenceArray/*(distribution.union(other.distribution))*/ union( charArray other);
-	
+	CharReferenceArray/*(distribution.union(other.distribution))*/ union(charArray other);
+
 	/** Return the array obtained by overlaying this array on top of
 	 other. The method takes as parameter a distribution D over the
 	 same rank. It returns an array over the distribution
 	 dist.asymmetricUnion(D).
 	 */
 	abstract public /*(distribution(:rank=this.rank) D)*/
-	CharReferenceArray/*(distribution.asymmetricUnion(D))*/ overlay( charArray/*(D)*/ other);
-	
+	CharReferenceArray/*(distribution.asymmetricUnion(D))*/ overlay(charArray/*(D)*/ other);
+
 	/** Update this array in place by overlaying the array other on top of this. The distribution
 	 * of the input array must be a subdistribution of D.
 	 * TODO: update the description of the parametric type.
 	 */
-    abstract public void update( charArray/*(D)*/ other);
-    
+	abstract public void update(charArray/*(D)*/ other);
+
 	/** Assume given a CharArray a over the given distribution.
 	 * Assume given a function f: char -> char -> char.
-	 * Return a CharArray with distribution dist 
+	 * Return a CharArray with distribution dist
 	 * containing fun(this.atValue(p),a.atValue(p)) for each p in
 	 * dist.region.
 	 */
-	abstract public 
-	CharReferenceArray/*(distribution)*/ lift(binaryOp fun, charArray/*(distribution)*/ a);
-	abstract public 
-	CharReferenceArray/*(distribution)*/ lift(unaryOp fun);
-	
+	abstract public
+	CharReferenceArray/*(distribution)*/ lift(Operator.Binary fun, charArray/*(distribution)*/ a);
+	abstract public
+	CharReferenceArray/*(distribution)*/ lift(Operator.Unary fun);
+
 	/**
 	 * Return an immutable copy of this array. Note: The implementation actually returns a copy
 	 * at the representation of the X10 type x10.lang.charValueArray, which is charArray.
--- x10.runtime/src/x10/lang/shortArray.java
+++ x10.runtime/src/x10/lang/shortArray.java
@@ -1,39 +1,32 @@
 package x10.lang;
 
+import x10.array.Operator;
 
-
-/** The class of all multidimensional, distributed int arrays in X10. Has no mutable data.
+/**
+ * The class of all multidimensional, distributed int arrays in X10. Has no mutable data.
  * Specialized from array by replacing the type parameter with int.
- 
- * Handtranslated from the X10 code in x10/lang/ShortArray.x10
  * 
+ * Handtranslated from the X10 code in x10/lang/ShortArray.x10
+ *
  * @author vj 12/24/2004
  */
+abstract public class shortArray extends x10Array {
 
-
-abstract public class shortArray extends x10Array{
-
-	protected shortArray( dist D) {
+	protected shortArray(dist D) {
 		super(D);
 	}
-	
-	public static interface binaryOp {
-		int apply(short r, short s);
-	}
-	public static final binaryOp sub = new binaryOp() { public int apply(short r, short s) { return r-s;}};
-	public static final binaryOp add = new binaryOp() { public int apply(short r, short s) { return r+s;}};
-	public static final binaryOp mul = new binaryOp() { public int apply(short r, short s) { return r*s;}};
-	public static final binaryOp div = new binaryOp() { public int apply(short r, short s) { return r/s;}};
-	public static final binaryOp max = new binaryOp() { public int apply(short r, short s) { return Math.max(r,s);}};
-	public static interface unaryOp {
-		int apply(short r);
-	}
-	public static final unaryOp abs = new unaryOp() { public int apply(short r) { return Math.abs(r);}};
-	
+
+	public static final Operator.Binary sub = new Operator.Binary() { public short apply(short r, short s) { return (short)(r-s); }};
+	public static final Operator.Binary add = new Operator.Binary() { public short apply(short r, short s) { return (short)(r+s); }};
+	public static final Operator.Binary mul = new Operator.Binary() { public short apply(short r, short s) { return (short)(r*s); }};
+	public static final Operator.Binary div = new Operator.Binary() { public short apply(short r, short s) { return (short)(r/s); }};
+	public static final Operator.Binary max = new Operator.Binary() { public short apply(short r, short s) { return (short)Math.max(r,s); }};
+	public static final Operator.Unary abs = new Operator.Unary() { public short apply(short r) { return (short)Math.abs(r); }};
+
 	public static interface pointwiseOp/*(region r)*/ {
 		short apply(point/*(r)*/ p);
 	}
-	
+
 	/** Return the value of the array at the given point in the
 	 * region.
 	 */
@@ -42,11 +35,11 @@ abstract public class shortArray extends x10Array{
 	abstract /*value*/ public short get(int p, int q);
 	abstract /*value*/ public short get(int p, int q, int r);
 	abstract /*value*/ public short get(int p, int q, int r, int s);
-    abstract public short get(int[] p);
-    
-    /** Convenience method for returning the sum of the array.
-     * @return sum of the array.
-     */
+	abstract public short get(int[] p);
+
+	/** Convenience method for returning the sum of the array.
+	 * @return sum of the array.
+	 */
 	public int sum() {
 		return reduce(add, (short) 0);
 	}
@@ -62,7 +55,7 @@ abstract public class shortArray extends x10Array{
 	 * @param fun
 	 * @return
 	 */
-	public int max(unaryOp fun) {
+	public int max(Operator.Unary fun) {
 		return lift(fun).reduce(max, (short) 0);
 	}
 	/**
@@ -72,36 +65,36 @@ abstract public class shortArray extends x10Array{
 	public ShortReferenceArray abs() {
 		return lift(abs);
 	}
-	
+
 	/**
 	 * Convenience method for subtracting another array pointwise.
 	 * @return
 	 */
-	public ShortReferenceArray sub( shortArray s) {
+	public ShortReferenceArray sub(shortArray s) {
 		return lift(sub, s);
 	}
 	/**
 	 * Convenience method for subtracting another array pointwise.
 	 * @return
 	 */
-	public ShortReferenceArray add( shortArray s) {
+	public ShortReferenceArray add(shortArray s) {
 		return lift(add, s);
 	}
 	/**
 	 * Convenience method for subtracting another array pointwise.
 	 * @return
 	 */
-	public ShortReferenceArray mul( shortArray s) {
+	public ShortReferenceArray mul(shortArray s) {
 		return lift(mul, s);
 	}
 	/**
 	 * Convenience method for subtracting another array pointwise.
 	 * @return
 	 */
-	public ShortReferenceArray div( shortArray s) {
+	public ShortReferenceArray div(shortArray s) {
 		return lift(div, s);
 	}
-	
+
 	/**
 	 * Convenience method for applying max after applying abs.
 	 * @return
@@ -109,32 +102,32 @@ abstract public class shortArray extends x10Array{
 	public int maxAbs() {
 		return max(abs);
 	}
-    
+
 	/** Return the value obtained by reducing the given array with the
 	 function fun, which is assumed to be associative and
 	 commutative. unit should satisfy fun(unit,x)=x=fun(x,unit).
 	 */
-	abstract public int reduce(binaryOp fun, short unit);
-	
-	/** Return a ShortArray with the same distribution as this, by 
+	abstract public int reduce(Operator.Binary fun, short unit);
+
+	/** Return a ShortArray with the same distribution as this, by
 	 scanning this with the function fun, and unit unit.
 	 */
-	abstract public ShortReferenceArray/*(distribution)*/ scan(binaryOp fun, short unit);
-	
+	abstract public ShortReferenceArray/*(distribution)*/ scan(Operator.Binary fun, short unit);
+
 	/** Return an array of B@P defined on the intersection of the
 	 region underlying the array and the parameter region R.
 	 */
 	abstract public /*(region(rank) R)*/
 	ShortReferenceArray/*(distribution.restriction(R)())*/  restriction(region R);
-	
-	/** Return an array of B@P defined on the intersection of 
+
+	/** Return an array of B@P defined on the intersection of
 	 the region underlying this and the parametric distribution.
-	 */    
-	public  /*(distribution(:rank=this.rank) D)*/ 
+	 */
+	public  /*(distribution(:rank=this.rank) D)*/
 	ShortReferenceArray/*(distribution.restriction(D.region)())*/ restriction(dist D) {
-	 return restriction(D.region);
+		return restriction(D.region);
 	}
-	
+
 	public ShortReferenceArray restriction(place P) {
 		return restriction(distribution.restriction(P));
 	}
@@ -146,33 +139,33 @@ abstract public class shortArray extends x10Array{
 	 * in other.region.
 	 */
 	abstract public /*(distribution(:region.disjoint(this.region) && rank=this.rank) D)*/
-	ShortReferenceArray/*(distribution.union(other.distribution))*/ union( shortArray other);
-	
+	ShortReferenceArray/*(distribution.union(other.distribution))*/ union(shortArray other);
+
 	/** Return the array obtained by overlaying this array on top of
 	 other. The method takes as parameter a distribution D over the
 	 same rank. It returns an array over the distribution
 	 dist.asymmetricUnion(D).
 	 */
 	abstract public /*(distribution(:rank=this.rank) D)*/
-	ShortReferenceArray/*(distribution.asymmetricUnion(D))*/ overlay( shortArray/*(D)*/ other);
-	
+	ShortReferenceArray/*(distribution.asymmetricUnion(D))*/ overlay(shortArray/*(D)*/ other);
+
 	/** Update this array in place by overlaying the array other on top of this. The distribution
 	 * of the input array must be a subdistribution of D.
 	 * TODO: update the description of the parametric type.
 	 */
-    abstract public void update( shortArray/*(D)*/ other);
-    
+	abstract public void update(shortArray/*(D)*/ other);
+
 	/** Assume given a ShortArray a over the given distribution.
 	 * Assume given a function f: short -> short -> short.
-	 * Return a ShortArray with distribution dist 
+	 * Return a ShortArray with distribution dist
 	 * containing fun(this.atValue(p),a.atValue(p)) for each p in
 	 * dist.region.
 	 */
-	abstract public 
-	ShortReferenceArray/*(distribution)*/ lift(binaryOp fun, shortArray/*(distribution)*/ a);
-	abstract public 
-	ShortReferenceArray/*(distribution)*/ lift(unaryOp fun);
-	
+	abstract public
+	ShortReferenceArray/*(distribution)*/ lift(Operator.Binary fun, shortArray/*(distribution)*/ a);
+	abstract public
+	ShortReferenceArray/*(distribution)*/ lift(Operator.Unary fun);
+
 	/**
 	 * Return an immutable copy of this array. Note: The implementation actually returns a copy
 	 * at the representation of the X10 type x10.lang.shortValueArray, which is shortArray.
