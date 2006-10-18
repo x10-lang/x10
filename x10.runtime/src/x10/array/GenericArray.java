dd09e2a540849a8ff0e275a83c26457462ac8448
Igor Peshansky <igorp@us.ibm.com>
2006-10-18 22:46:36 +0000

    Pull up all the get() and set() methods Add newInstance() abstract methods Add getOrdinal() and setOrdinal() abstract methods Pull up lift(), reduce(), scan(), overlay(), update(), union(), restriction() Pull up toValueArray() Pull up refsToValues_ into GenericArray Implement valueEquals() Move Constant to <t>Array More uniform implementation
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@2734 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.runtime/src/x10/array/GenericArray.java
+++ x10.runtime/src/x10/array/GenericArray.java
@@ -7,46 +7,183 @@ package x10.array;
 import java.util.Iterator;
 
 import x10.compilergenerated.Parameter1;
+import x10.lang.GenericReferenceArray;
 import x10.lang.dist;
-import x10.lang.point;
 import x10.lang.place;
+import x10.lang.point;
+import x10.lang.region;
+import x10.lang.Runtime;
+import x10.runtime.Configuration;
 
 /**
  * Generic arrays.
  *
  * @author Christian Grothoff
+ * @author Igor Peshansky
  */
-public abstract class GenericArray extends x10.lang.GenericReferenceArray {
+public abstract class GenericArray extends GenericReferenceArray {
 
-	public static class Constant extends Operator.Unary {
-		private final Parameter1 c_;
-		public Constant(Parameter1 c) { c_ = c; }
-		public Parameter1 apply(Parameter1 i) { return c_; }
+	protected final boolean refsToValues_;
+
+	public GenericArray(dist d, boolean mutable, boolean refs_to_values) {
+		super(d, mutable);
+		this.refsToValues_ = refs_to_values;
 	}
 
-	public GenericArray(dist d) {
-		super(d);
+	protected abstract GenericArray newInstance(dist d);
+	protected abstract GenericArray newInstance(dist d, Parameter1 c);
+	protected final GenericArray newInstance(dist d, Operator.Pointwise p) {
+		GenericArray res = newInstance(d);
+		if (p != null)
+			scan(res, p);
+		return res;
 	}
 
 	/**
-	 * Constructor that implements constant promotion (Chap. 10.4.2).
-	 * @param d  Distribution of the array
-	 * @param c  constant used to intialize all values of the array
+	 * Return the element at position rawIndex in the backing store.
+	 * The canonical index has already be calculated and adjusted.
+	 * Can be used by any dimensioned array.
 	 */
-	protected GenericArray(dist d, Parameter1 c) {
-		super(d);
-		// initialize all array alements
-		scan(this, new Constant(c));
+	public abstract Parameter1 getOrdinal(int rawIndex);
+
+	/**
+	 * Set the element at position rawIndex in the backing store to v.
+	 * The canonical index has already be calculated and adjusted.
+	 * Can be used by any dimensioned array.
+	 */
+	public abstract Parameter1 setOrdinal(Parameter1 v, int rawIndex);
+
+	public Parameter1 set(Parameter1 v, point pos) { return set(v,pos,true,true); }
+	public Parameter1 set(Parameter1 v, point pos,boolean chkPl,boolean chkAOB) {
+		if (Configuration.BAD_PLACE_RUNTIME_CHECK && mutable_)
+			Runtime.hereCheckPlace(distribution.get(pos));
+		int theIndex = Helper.ordinal(distribution,pos,chkAOB);
+		return setOrdinal(v, theIndex);
+	}
+
+	public Parameter1 set(Parameter1 v, int d0) { return set(v,d0,true,true); }
+	public Parameter1 set(Parameter1 v, int d0,boolean chkPl,boolean chkAOB) {
+		if (chkPl && Configuration.BAD_PLACE_RUNTIME_CHECK && mutable_)
+			Runtime.hereCheckPlace(distribution.get(d0));
+		int theIndex = Helper.ordinal(distribution,d0,chkAOB);
+		return setOrdinal(v, theIndex);
+	}
+
+	public Parameter1 set(Parameter1 v, int d0,int d1) { return set(v,d0,d1,true,true); }
+	public Parameter1 set(Parameter1 v, int d0, int d1,boolean chkPl,boolean chkAOB) {
+		if (chkPl && Configuration.BAD_PLACE_RUNTIME_CHECK && mutable_)
+			Runtime.hereCheckPlace(distribution.get(d0, d1));
+		int theIndex = Helper.ordinal(distribution,d0,d1,chkAOB);
+		return setOrdinal(v, theIndex);
+	}
+
+	public Parameter1 set(Parameter1 v, int d0,int d1,int d2) {return set(v,d0,d1,d2,true,true);}
+	public Parameter1 set(Parameter1 v, int d0, int d1, int d2,boolean chkPl,boolean chkAOB) {
+		if (chkPl && Configuration.BAD_PLACE_RUNTIME_CHECK && mutable_)
+			Runtime.hereCheckPlace(distribution.get(d0, d1, d2));
+		int theIndex = Helper.ordinal(distribution,d0,d1,d2,chkAOB);
+		return setOrdinal(v, theIndex);
+	}
+
+	public Parameter1 set(Parameter1 v, int d0,int d1,int d2,int d3) {return set(v,d0,d1,d2,d3,true,true);}
+	public Parameter1 set(Parameter1 v, int d0, int d1, int d2, int d3,boolean chkPl,boolean chkAOB) {
+		if (chkPl && Configuration.BAD_PLACE_RUNTIME_CHECK && mutable_)
+			Runtime.hereCheckPlace(distribution.get(d0, d1, d2, d3));
+		int theIndex = Helper.ordinal(distribution,d0,d1,d2,d3,chkAOB);
+		return setOrdinal(v, theIndex);
+	}
+
+	public Parameter1 get(point pos) {return get(pos,true,true);}
+	public Parameter1 get(point pos,boolean chkPl,boolean chkAOB) {
+		if (chkPl && Configuration.BAD_PLACE_RUNTIME_CHECK && mutable_)
+			Runtime.hereCheckPlace(distribution.get(pos));
+		int theIndex = Helper.ordinal(distribution,pos,chkAOB);
+		return getOrdinal(theIndex);
+	}
+
+	public Parameter1 get(int d0) {return get(d0,true,true);}
+	public Parameter1 get(int d0,boolean chkPl,boolean chkAOB) {
+		if (chkPl && Configuration.BAD_PLACE_RUNTIME_CHECK && mutable_)
+			Runtime.hereCheckPlace(distribution.get(d0));
+		int theIndex = Helper.ordinal(distribution,d0,chkAOB);
+		return getOrdinal(theIndex);
+	}
+
+	public Parameter1 get(int d0,int d1) {return get(d0,d1,true,true);}
+	public Parameter1 get(int d0, int d1,boolean chkPl,boolean chkAOB) {
+		if (chkPl && Configuration.BAD_PLACE_RUNTIME_CHECK && mutable_)
+			Runtime.hereCheckPlace(distribution.get(d0, d1));
+		int theIndex = Helper.ordinal(distribution,d0,d1,chkAOB);
+		return getOrdinal(theIndex);
+	}
+
+	public Parameter1 get(int d0,int d1,int d2) {return get(d0,d1,d2,true,true);}
+	public Parameter1 get(int d0, int d1, int d2,boolean chkPl,boolean chkAOB) {
+		if (chkPl && Configuration.BAD_PLACE_RUNTIME_CHECK && mutable_)
+			Runtime.hereCheckPlace(distribution.get(d0, d1, d2));
+		int theIndex = Helper.ordinal(distribution,d0,d1,d2,chkAOB);
+		return getOrdinal(theIndex);
+	}
+
+	public Parameter1 get(int d0,int d1,int d2,int d3) {return get(d0,d1,d2,d3,true,true);}
+	public Parameter1 get(int d0, int d1, int d2, int d3,boolean chkPl,boolean chkAOB) {
+		if (chkPl && Configuration.BAD_PLACE_RUNTIME_CHECK && mutable_)
+			Runtime.hereCheckPlace(distribution.get(d0, d1, d2, d3));
+		int theIndex = Helper.ordinal(distribution,d0,d1,d2,d3,chkAOB);
+		return getOrdinal(theIndex);
+	}
+
+//	public Parameter1 get(int[] pos) {return get(pos,true,true);}
+//	public Parameter1 get(int[] pos,boolean chkPl,boolean chkAOB) {
+//		if (chkPl && Configuration.BAD_PLACE_RUNTIME_CHECK && mutable_)
+//			Runtime.hereCheckPlace(distribution.get(pos));
+//		final point p = Runtime.factory.getPointFactory().point(pos);
+//		return get(p);
+//	}
+
+	public boolean valueEquals(Indexable other) {
+		GenericArray o = (GenericArray)other;
+		if (!o.distribution.equals(distribution))
+			return false;
+		try {
+			if (refsToValues_) {
+				for (Iterator it = distribution.region.iterator(); it.hasNext(); ) {
+					point pos = (point) it.next();
+					place pl = distribution.get(pos);
+					x10.lang.Runtime.runtime.setCurrentPlace(pl);
+					if (!x10.lang.Runtime.equalsequals(get(i), o.get(i)))
+						return false;
+				}
+			} else {
+				for (Iterator it = distribution.region.iterator(); it.hasNext(); ) {
+					point pos = (point) it.next();
+					place pl = distribution.get(pos);
+					x10.lang.Runtime.runtime.setCurrentPlace(pl);
+					if (get(i) != o.get(i))
+						return false;
+				}
+			}
+		} finally {
+			x10.lang.Runtime.runtime.setCurrentPlace(here);
+		}
+		return true;
 	}
 
 	protected void assign(GenericArray rhs) {
 		assert rhs instanceof GenericArray;
 		assert rhs.distribution.equals(distribution);
 
+		place here = x10.lang.Runtime.runtime.currentPlace();
 		GenericArray rhs_t = (GenericArray) rhs;
-		for (Iterator it = rhs_t.distribution.region.iterator(); it.hasNext();) {
-			point pos = (point) it.next();
-			set(rhs_t.get(pos), pos);
+		try {
+			for (Iterator it = rhs_t.distribution.region.iterator(); it.hasNext(); ) {
+				point pos = (point) it.next();
+				place pl = distribution.get(pos);
+				x10.lang.Runtime.runtime.setCurrentPlace(pl);
+				set(rhs_t.get(pos), pos);
+			}
+		} finally {
+			x10.lang.Runtime.runtime.setCurrentPlace(here);
 		}
 	}
 
@@ -162,52 +299,164 @@ public abstract class GenericArray extends x10.lang.GenericReferenceArray {
 		}
 	}
 
-	/**
-	 * Generic flat access.
+	public GenericReferenceArray lift(Operator.Binary op, x10.lang.genericArray arg) {
+		assert arg.distribution.equals(distribution);
+		GenericReferenceArray result = newInstance(distribution);
+		place here = x10.lang.Runtime.runtime.currentPlace();
+		try {
+			for (Iterator it = distribution.region.iterator(); it.hasNext();) {
+				point p = (point) it.next();
+				place pl = distribution.get(p);
+				x10.lang.Runtime.runtime.setCurrentPlace(pl);
+				result.set(op.apply(this.get(p), arg.get(p)),p);
+			}
+		} finally {
+			x10.lang.Runtime.runtime.setCurrentPlace(here);
+		}
+		return result;
+	}
+	public GenericReferenceArray lift(Operator.Unary op) {
+		GenericReferenceArray result = newInstance(distribution);
+		place here = x10.lang.Runtime.runtime.currentPlace();
+		try {
+			for (Iterator it = distribution.region.iterator(); it.hasNext();) {
+				point p = (point) it.next();
+				place pl = distribution.get(p);
+				x10.lang.Runtime.runtime.setCurrentPlace(pl);
+				result.set(op.apply(this.get(p)),p);
+			}
+		} finally {
+			x10.lang.Runtime.runtime.setCurrentPlace(here);
+		}
+		return result;
+	}
+
+	public Parameter1 reduce(Operator.Binary op, Parameter1 unit) {
+		Parameter1 result = unit;
+		place here = x10.lang.Runtime.runtime.currentPlace();
+		try {
+			for (Iterator it = distribution.region.iterator(); it.hasNext();) {
+				point p = (point) it.next();
+				place pl = distribution.get(p);
+				x10.lang.Runtime.runtime.setCurrentPlace(pl);
+				result = op.apply(this.get(p), result);
+			}
+		} finally {
+			x10.lang.Runtime.runtime.setCurrentPlace(here);
+		}
+		return result;
+	}
+
+	public GenericReferenceArray scan(Operator.Binary op, Parameter1 unit) {
+		Parameter1 temp = unit;
+		GenericReferenceArray result = newInstance(distribution);
+		place here = x10.lang.Runtime.runtime.currentPlace();
+		try {
+			for (Iterator it = distribution.region.iterator(); it.hasNext();) {
+				point p = (point) it.next();
+				place pl = distribution.get(p);
+				x10.lang.Runtime.runtime.setCurrentPlace(pl);
+				temp = op.apply(this.get(p), temp);
+				result.set(temp, p);
+			}
+		} finally {
+			x10.lang.Runtime.runtime.setCurrentPlace(here);
+		}
+		return result;
+	}
+
+	/*
+	 * FIXME: this could be made much more efficient with knowledge of overlay() semantics.
 	 */
-	public abstract Parameter1 setOrdinal(Parameter1 v, int i);
+	public x10.lang.GenericReferenceArray overlay(x10.lang.genericArray d) {
+		dist dist = distribution.overlay(d.distribution);
+		GenericArray ret = newInstance(dist, (Parameter1)null);
+		place here = x10.lang.Runtime.runtime.currentPlace();
+		try {
+			for (Iterator it = dist.iterator(); it.hasNext(); ) {
+				point p = (point) it.next();
+				place pl = dist.get(p);
+				x10.lang.Runtime.runtime.setCurrentPlace(pl);
+				Parameter1 val = (d.distribution.region.contains(p)) ? d.get(p) : get(p);
+				ret.set(val, p);
+			}
+		} finally {
+			x10.lang.Runtime.runtime.setCurrentPlace(here);
+		}
+		return ret;
+	}
 
-	public abstract Parameter1 set(Parameter1 v, point pos);
-	public abstract Parameter1 set(Parameter1 v, point pos,boolean chkPl,boolean chkAOB);
-
-	public abstract Parameter1 set(Parameter1 v, int d0);
-	public abstract Parameter1 set(Parameter1 v, int d0,boolean chkPl,boolean chkAOB);
-
-	public abstract Parameter1 set(Parameter1 v, int d0, int d1);
-	public abstract Parameter1 set(Parameter1 v, int d0, int d1,boolean chkPl,boolean chkAOB);
-
-	public abstract Parameter1 set(Parameter1 v, int d0, int d1, int d2);
-	public abstract Parameter1 set(Parameter1 v, int d0, int d1, int d2,boolean chkPl,boolean chkAOB);
-
-	public abstract Parameter1 set(Parameter1 v, int d0, int d1, int d2, int d3);
-	public abstract Parameter1 set(Parameter1 v, int d0, int d1, int d2, int d3,boolean chkPl,boolean chkAOB);
-
-	/**
-	 * Generic flat access. the least significant dimension is pos[0].
+	/*
+	 * FIXME: this could use the fact that d is rectangular
+	 * FIXME: (in fact, why are we even iterating over the unknown array here?)
 	 */
-	public abstract Parameter1 getOrdinal(int i);
+	public void update(x10.lang.genericArray d) {
+		assert (region.contains(d.region));
+		place here = x10.lang.Runtime.runtime.currentPlace();
+		try {
+			for (Iterator it = d.iterator(); it.hasNext(); ) {
+				point p = (point) it.next();
+				place pl = distribution.get(p);
+				x10.lang.Runtime.runtime.setCurrentPlace(pl);
+				set(d.get(p), p);
+			}
+		} finally {
+			x10.lang.Runtime.runtime.setCurrentPlace(here);
+		}
+	}
 
-	public abstract Parameter1 get(point pos);
-	public abstract Parameter1 get(point pos,boolean chkPl,boolean chkAOB);
+	/*
+	 * FIXME: this could be made much more efficient with knowledge of union() semantics.
+	 */
+	public GenericReferenceArray union(x10.lang.genericArray d) {
+		dist dist = distribution.union(d.distribution);
+		GenericArray ret = newInstance(dist, (Parameter1)null);
+		place here = x10.lang.Runtime.runtime.currentPlace();
+		try {
+			for (Iterator it = dist.iterator(); it.hasNext(); ) {
+				point p = (point) it.next();
+				place pl = dist.get(p);
+				x10.lang.Runtime.runtime.setCurrentPlace(pl);
+				Parameter1 val = (distribution.region.contains(p)) ? get(p) : d.get(p);
+				ret.set(val, p);
+			}
+		} finally {
+			x10.lang.Runtime.runtime.setCurrentPlace(here);
+		}
+		return ret;
+	}
 
-	public abstract Parameter1 get(int d0);
-	public abstract Parameter1 get(int d0,boolean chkPl,boolean chkAOB);
+	public GenericReferenceArray restriction(dist d) {
+		return restriction(d.region);
+	}
 
-	public abstract Parameter1 get(int d0, int d1);
-	public abstract Parameter1 get(int d0, int d1,boolean chkPl,boolean chkAOB);
-
-	public abstract Parameter1 get(int d0, int d1, int d2);
-	public abstract Parameter1 get(int d0, int d1, int d2,boolean chkPl,boolean chkAOB);
-
-	public abstract Parameter1 get(int d0, int d1, int d2, int d3);
-	public abstract Parameter1 get(int d0, int d1, int d2, int d3,boolean chkPl,boolean chkAOB);
+	/*
+	 * FIXME: this could use the fact that the region is rectangular
+	 * FIXME: (in fact, why are we even iterating over the unknown array here?)
+	 */
+	public GenericReferenceArray restriction(region d) {
+		dist dist = distribution.restriction(d);
+		GenericArray ret = newInstance(dist, (Parameter1)null);
+		place here = x10.lang.Runtime.runtime.currentPlace();
+		try {
+			for (Iterator it = dist.iterator(); it.hasNext(); ) {
+				point p = (point) it.next();
+				place pl = dist.get(p);
+				x10.lang.Runtime.runtime.setCurrentPlace(pl);
+				ret.set(get(p), p);
+			}
+		} finally {
+			x10.lang.Runtime.runtime.setCurrentPlace(here);
+		}
+		return ret;
+	}
 
 	public Object toJava() {
 		final int[] dims_tmp = new int[distribution.rank];
 		for (int i = 0; i < distribution.rank; ++i)
 			dims_tmp[i] = distribution.region.rank(i).high() + 1;
 
-		final Object ret = java.lang.reflect.Array.newInstance(Integer.TYPE, dims_tmp);
+		final Object ret = java.lang.reflect.Array.newInstance(Parameter1.class, dims_tmp);
 		pointwise(null, new Operator.Pointwise() {
 			public Parameter1 apply(point p, Parameter1 arg) {
 				Object handle = ret;
@@ -222,8 +471,17 @@ public abstract class GenericArray extends x10.lang.GenericReferenceArray {
 		return ret;
 	}
 
+	public x10.lang.genericArray toValueArray() {
+		if (!mutable_) return this;
+		try {
+			return (x10.lang.genericArray) this.clone();
+		} catch (CloneNotSupportedException x) {
+			throw new Error("TODO: <T>ReferenceArray --> <T>ValueArray");
+		}
+	}
+
 	/* for debugging */
-	public static void printArray(String prefix, int[][] a) {
+	public static void printArray(String prefix, Object[][] a) {
 		System.out.print(prefix + "{");
 		for (int i = 0; i < a.length; ++i) {
 			System.out.print("{");
@@ -238,4 +496,4 @@ public abstract class GenericArray extends x10.lang.GenericReferenceArray {
 		}
 		System.out.println("}");
 	}
-} // end of GenericArray
+}
