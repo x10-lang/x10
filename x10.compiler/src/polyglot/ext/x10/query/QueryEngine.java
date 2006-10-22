d52124cfda712365c126360efc53e68f9a63214d
Vivek Sarkar <vsarkar@rice.edu>
2006-10-22 06:13:08 +0000

    Add query to check if an array declaration has the following attributes, :self.rect&&self.zeroBased&&self.rank==1
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@2769 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.compiler/src/polyglot/ext/x10/query/QueryEngine.java
+++ x10.compiler/src/polyglot/ext/x10/query/QueryEngine.java
@@ -11,10 +11,13 @@ import polyglot.ext.x10.ast.X10ArrayAccess;
 import polyglot.ext.x10.ast.X10ArrayAccess1;
 import polyglot.ext.x10.ast.X10ArrayAccess1Assign;
 import polyglot.ext.x10.ast.X10ArrayAccess1Unary;
+import polyglot.ext.x10.ast.X10ArrayAccess1_c;
 import polyglot.ext.x10.ast.X10ArrayAccessAssign;
 import polyglot.ext.x10.ast.X10ArrayAccessUnary;
+import polyglot.ext.x10.types.X10ParsedClassType;
 import polyglot.ext.x10.types.X10Type;
 import polyglot.ext.x10.types.X10TypeSystem;
+import polyglot.ext.x10.visit.X10PrettyPrinterVisitor.Template;
 import polyglot.types.Type;
 
 /**
@@ -38,18 +41,20 @@ public class QueryEngine {
 		this.ext = ext;
 	}
 	
-	public boolean isRectangularRankOneLowZero(X10ArrayAccess1 a) {
-		return Configuration.RECT_RANK_1_LOW_0_ARRAYS;
+	/**
+	 * @param a Array variable being used in one-dimensional access
+	 * @return true iff a is a dense one-diemnsional array with zero origin
+	 */
+	public boolean isRectangularRankOneLowZero(X10ArrayAccess1_c a) {
+		if ( a.array().type() instanceof X10ParsedClassType ) {
+			X10ParsedClassType t = (X10ParsedClassType) a.array().type();
+			return t.isZeroBased() && t.isRankOne() && t.isRect();
+		}
+		else 
+			// a does not represent an X10 array (it may be a distribution or region)
+			return false;
 	}
 
-	public boolean isRectangularRankOneLowZero(X10ArrayAccess1Assign a) {
-		return Configuration.RECT_RANK_1_LOW_0_ARRAYS;
-	}
-
-	public boolean isRectangularRankOneLowZero(X10ArrayAccess1Unary a) {
-		return Configuration.RECT_RANK_1_LOW_0_ARRAYS;
-	}
-	
 	protected boolean needsHereCheck(Type t) {
 		/*if (!Configuration.BAD_PLACE_RUNTIME_CHECK)
 			return false;*//* Removed by RAJ to disable compile time BAD_PLACE_CHECK option*/
