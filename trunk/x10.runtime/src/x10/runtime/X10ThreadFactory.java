5a19e37f4ab1f7a2d2817b4bd71600f2886e9db2
Allan Kielstra <user@domain>
2006-10-31 02:16:33 +0000

    Initial support for binding PoolRunners to CPUs
    git-svn-id: svn://svn.code.sf.net/p/x10/code@2793 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.runtime/src/x10/runtime/X10ThreadFactory.java
+++ trunk/x10.runtime/src/x10/runtime/X10ThreadFactory.java
@@ -13,6 +13,7 @@ public class X10ThreadFactory implements ThreadFactory {
 	static final AtomicInteger poolNumber = new AtomicInteger(0);
 	final ThreadGroup group;
 	final AtomicInteger threadNumber = new AtomicInteger(0);
+        final AtomicInteger gthreadNumber = new AtomicInteger(0);
 	final String namePrefix;
 
 	public X10ThreadFactory() {
@@ -24,8 +25,33 @@ public class X10ThreadFactory implements ThreadFactory {
 					 "-thread-";
 	}
 
-	public Thread newThread(Runnable r) {
-		Thread t = new PoolRunner(group, r, namePrefix + threadNumber.getAndIncrement());
+        private final native static int getNumCPUs();
+        private final static int numCPUs;
+        private final static String thrdSuppLib = "X10ThreadSupp";
+        private final native static void putMeOnCPU(int i);
+        static {
+            int n = 0;
+            try {
+                System.loadLibrary(thrdSuppLib);
+                n = getNumCPUs();
+            } catch (java.lang.UnsatisfiedLinkError ule) {
+            } finally {
+                numCPUs = n;
+            }
+        }
+	public Thread newThread(final Runnable r) {
+                final int x = threadNumber.getAndIncrement();
+                final int y = threadNumber.getAndIncrement();
+                Runnable rr = r;
+                if (numCPUs != 0) {
+                    rr = new Runnable() {
+                            public void run() {
+                                putMeOnCPU(y % numCPUs);
+                                r.run();
+                            }
+                        };
+                }
+		Thread t = new PoolRunner(group, rr, namePrefix + x);
 		if (t.isDaemon())
 			t.setDaemon(false);
 		if (t.getPriority() != Thread.NORM_PRIORITY)
