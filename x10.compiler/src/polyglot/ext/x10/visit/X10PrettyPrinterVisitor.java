52cfeca02855404bbb8b8fa270f1e05eff18ac7b
Vincent Cave <user@domain>
2006-11-21 20:14:56 +0000

    Replace instanceof and cast code generation from write statement to templates.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@2988 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.compiler/src/polyglot/ext/x10/visit/X10PrettyPrinterVisitor.java
+++ x10.compiler/src/polyglot/ext/x10/visit/X10PrettyPrinterVisitor.java
@@ -22,10 +22,8 @@ import polyglot.ast.Receiver;
 import polyglot.ast.Special;
 import polyglot.ast.Stmt;
 import polyglot.ast.TypeNode;
-import polyglot.ast.Unary;
 import polyglot.ext.jl.ast.Binary_c;
 import polyglot.ext.jl.ast.Call_c;
-import polyglot.ext.jl.ast.Cast_c;
 import polyglot.ext.jl.ast.Field_c;
 import polyglot.ext.jl.ast.MethodDecl_c;
 import polyglot.ext.x10.Configuration;
@@ -44,23 +42,25 @@ import polyglot.ext.x10.ast.Next_c;
 import polyglot.ext.x10.ast.Now_c;
 import polyglot.ext.x10.ast.RemoteCall_c;
 import polyglot.ext.x10.ast.When_c;
-import polyglot.ext.x10.ast.X10ArrayAccess1;
 import polyglot.ext.x10.ast.X10ArrayAccess1Assign_c;
 import polyglot.ext.x10.ast.X10ArrayAccess1Unary_c;
 import polyglot.ext.x10.ast.X10ArrayAccess1_c;
 import polyglot.ext.x10.ast.X10ArrayAccessAssign_c;
 import polyglot.ext.x10.ast.X10ArrayAccessUnary_c;
 import polyglot.ext.x10.ast.X10ArrayAccess_c;
+import polyglot.ext.x10.ast.X10CastInfo;
+import polyglot.ext.x10.ast.X10Cast_c;
 import polyglot.ext.x10.ast.X10ClockedLoop;
 import polyglot.ext.x10.ast.X10Formal;
+import polyglot.ext.x10.ast.X10Instanceof_c;
 import polyglot.ext.x10.ast.X10NodeFactory_c;
+import polyglot.ext.x10.ast.X10Cast_c.X10CastHelper;
 import polyglot.ext.x10.query.QueryEngine;
 import polyglot.ext.x10.types.NullableType;
 import polyglot.ext.x10.types.X10ParsedClassType;
 import polyglot.ext.x10.types.X10ReferenceType;
 import polyglot.ext.x10.types.X10Type;
 import polyglot.ext.x10.types.X10TypeSystem;
-import polyglot.main.Report;
 import polyglot.types.LocalInstance;
 import polyglot.types.ReferenceType;
 import polyglot.types.Type;
@@ -105,11 +105,99 @@ public class X10PrettyPrinterVisitor extends Runabout {
 		n.prettyPrint(w, pp);
 	}
 
-	public void visit(Cast_c c) {
-		// nullable check is now embedded into X10Cast
-		visit((Node)c);
+	public void visit(X10Cast_c c) {
+		if (c.isDynamicCheckNeeded()) {
+			String castBaseType = ((X10Type) c.type()).baseType().toString();
+			Expr exprToCast = c.expr();
+	
+			if (X10CastHelper.isSideEffectFree(exprToCast)) {
+				this.applyInlineTemplate("cast-inlined", c, castBaseType, exprToCast, ((X10Type) c.type()));			
+			} else {
+				if (c.isPrimitiveCast())
+					new Template("cast-primitive",
+							 new Object[] { castBaseType, 
+											X10CastHelper.getRuntimeConstraintTab(((X10Type) c.type())),
+											exprToCast}).expand();
+				else {
+					new Template("cast-reference",
+							 new Object[] { castBaseType, 
+											X10CastHelper.getRuntimeConstraintTab(((X10Type) c.type())), 
+											c.notNullRequired(),c.isToTypeNullable(), exprToCast}).expand();
+				}
+			}
+		} else {
+			visit((Node)c);
+		}
 	}
 
+	public void visit(X10Instanceof_c c) {
+		if (c.isDynamicCheckNeeded()) {
+			String castBaseType = (((X10Type)c.compareType().type()).baseType()).toString();
+			Expr exprToTest = c.expr();
+
+			if (X10CastHelper.isSideEffectFree(exprToTest)) {
+				this.applyInlineTemplate("instanceof-inlined", c, castBaseType, exprToTest, ((X10Type)c.compareType().type()));
+			}
+			else {
+					new Template("instanceof-reference",
+							 new Object[] {castBaseType, 
+											X10CastHelper.getRuntimeConstraintTab(((X10Type) c.compareType().type())), 
+											c.notNullRequired(), c.isToTypeNullable(), c.expr()}).expand();
+			}
+		} else {
+			visit((Node) c);
+		}
+	}
+	
+	private void applyInlineTemplate(String templateToInvoke, X10CastInfo c, 
+			String castBaseType, Expr exprToCast, X10Type toType) {
+
+			List [] conditionsList = X10CastHelper.getRuntimeConstraintInlined(toType, 
+					exprToCast, c.isPrimitiveCast());
+			Object conditionParam = null;
+			if (conditionsList.length != 0) {
+				conditionParam = new Loop("cast-util-runtime-constraint-inlined", conditionsList);
+			}
+
+			// it is not possible to have a these property true simultaneously
+			// First means toType is non nullable and from type yes
+			// Second means toType is nullable
+			assert !(c.notNullRequired() && c.isToTypeNullable());
+
+			Object nullableCheck = null;			
+			if (c.notNullRequired() ) {
+				nullableCheck = new Template("nullable-check",exprToCast, "");
+			}
+			if (c.isToTypeNullable()) {
+				nullableCheck = new Template("nullable-check",exprToCast, "!"); 
+			}
+
+			Object instanceofCheck = null; 
+				if (!c.isPrimitiveCast()) {
+					if (c.notNullRequired()) {
+						instanceofCheck = new Join("&&" , "", new Template("instanceof-java", castBaseType, exprToCast));
+					} else if (c.isToTypeNullable()) {
+						instanceofCheck = new Join("||" , "", new Template("instanceof-java", castBaseType, exprToCast));
+					} else {
+						instanceofCheck = new Template("instanceof-java", castBaseType, exprToCast);
+					}
+				}
+
+			Object conditionCheck = 
+				((conditionParam != null) ? 
+					((!c.isPrimitiveCast() || c.notNullRequired() || c.isToTypeNullable()) ? 
+							new Join("&&" , "", conditionParam) 
+								: conditionParam) 
+									: null);
+
+			// call inlined cast template
+			new Template(templateToInvoke, new Object[] {castBaseType,
+							exprToCast,
+							nullableCheck,
+							instanceofCheck,
+							conditionCheck}).expand();
+	}
+	
 	public void visit(Call_c c) {
 		if (c instanceof RemoteCall_c) {
 			// TODO assert false - that is not implemented yet
