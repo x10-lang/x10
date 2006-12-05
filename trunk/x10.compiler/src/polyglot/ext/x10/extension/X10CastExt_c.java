d17f28a1800fc809882d20a2445eb576e32fafd9
Vincent Cave <user@domain>
2006-12-05 01:08:36 +0000

    added support for runtime checking of cast. This is the good place to deal with boxing/unboxing and runtime constraint checking
    git-svn-id: svn://svn.code.sf.net/p/x10/code@3066 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.compiler/src/polyglot/ext/x10/extension/X10CastExt_c.java
+++ trunk/x10.compiler/src/polyglot/ext/x10/extension/X10CastExt_c.java
@@ -6,9 +6,14 @@ import java.util.List;
 
 import polyglot.ast.Call;
 import polyglot.ast.Cast;
+import polyglot.ast.Expr;
 import polyglot.ast.New;
 import polyglot.ast.Node;
 import polyglot.ast.NodeFactory;
+import polyglot.ext.jl.ast.CanonicalTypeNode_c;
+import polyglot.ext.x10.ast.ParExpr;
+import polyglot.ext.x10.ast.X10DepCastInfo;
+import polyglot.ext.x10.ast.X10NodeFactory;
 import polyglot.ext.x10.types.X10PrimitiveType;
 import polyglot.ext.x10.types.X10TypeSystem;
 import polyglot.frontend.ExtensionInfo;
@@ -23,6 +28,7 @@ public class X10CastExt_c extends X10Ext_c {
 		Cast c = (Cast) node();
 		Type rtype = c.expr().type();
 		Type ltype = c.castType().type();
+		X10NodeFactory xnf = (X10NodeFactory) nf;
 
 		if (!ts.equals(c.type(), ltype))
 			c = (Cast) c.type(ltype);
@@ -32,15 +38,28 @@ public class X10CastExt_c extends X10Ext_c {
 			MethodInstance mi = ts.getter((X10PrimitiveType) ltype.toPrimitive());
 
 			Cast x = nf.Cast(c.position(),
-							 nf.CanonicalTypeNode(c.position(), mi.container()),
-							 c.expr());
+					 nf.CanonicalTypeNode(c.position(), mi.container()),
+					 c.expr());
 			x = (Cast) x.type(mi.container());
-
-			Call y = nf.Call(c.position(), x, mi.name(),
+			ParExpr px = ((X10NodeFactory)nf).ParExpr(x.position(), x);
+			px = (ParExpr) px.type(x.type());
+			Call y = nf.Call(c.position(), px, mi.name(),
 							 Collections.EMPTY_LIST);
+
 			y = (Call) y.type(mi.returnType());
 
-			return y.methodInstance(mi);
+			Node rewrittenNode = y.methodInstance(mi);
+			
+			if (ts.isTypeConstrained(ltype)) {
+				// embedding boxing into another cast that will allows
+				// deptype constraint checking
+				Cast cast  = xnf.DepCast(c.position(), new CanonicalTypeNode_c(c.position(),ltype), 
+						((X10DepCastInfo)c).depParameterExpr(), (Expr)rewrittenNode);
+				
+				rewrittenNode = cast;
+			}
+
+			return rewrittenNode;
 		}
 		else if (ltype.isReference() && rtype.isPrimitive()) {
 			// Box
