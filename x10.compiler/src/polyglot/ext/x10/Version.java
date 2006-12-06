f76c8caf13e544a45e48466d023856425103d43d
Chris Donawa <donawa@domain>
2006-12-06 21:45:48 +0000

    update version to 1.0
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@3118 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.compiler/src/polyglot/ext/x10/Version.java
+++ x10.compiler/src/polyglot/ext/x10/Version.java
@@ -13,7 +13,7 @@ package polyglot.ext.x10;
 public class Version extends polyglot.main.Version {
     public String name() { return "x10"; }
 
-    public int major() { return 0; }
-    public int minor() { return 41; }
+    public int major() { return 1; }
+    public int minor() { return 0; }
     public int patch_level() { return 0; }
 }
