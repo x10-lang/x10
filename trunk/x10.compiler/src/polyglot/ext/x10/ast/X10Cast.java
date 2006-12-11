c6dee375ea2ba23ff2d898c678a25983f2486208
Vincent Cave <user@domain>
2006-12-11 21:48:09 +0000

    added setter and getter to some X10cast properties
    git-svn-id: svn://svn.code.sf.net/p/x10/code@3240 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.compiler/src/polyglot/ext/x10/ast/X10Cast.java
+++ trunk/x10.compiler/src/polyglot/ext/x10/ast/X10Cast.java
@@ -4,5 +4,15 @@ import polyglot.ast.Cast;
 
 public interface X10Cast extends Cast {
 
+	public boolean notNullRequired();
 
+	public boolean isPrimitiveCast();
+	
+	public boolean isToTypeNullable();
+
+	public void setToTypeNullable(boolean b);
+
+	public void setPrimitiveCast(boolean b);
+
+	public void setNotNullRequired(boolean b);
 }
