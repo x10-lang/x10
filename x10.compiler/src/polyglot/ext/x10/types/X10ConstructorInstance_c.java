cfa593327963426434a1f7edf544abc3a2c2a5f6
Vijay Saraswat <vsaraswa@us.ibm.com>
2006-12-26 20:37:59 +0000

    Support checking deptypes for args of constructors.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@3366 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.compiler/src/polyglot/ext/x10/types/X10ConstructorInstance_c.java
+++ x10.compiler/src/polyglot/ext/x10/types/X10ConstructorInstance_c.java
@@ -11,6 +11,7 @@ import polyglot.types.ClassType;
 import polyglot.types.ConstructorInstance_c;
 import polyglot.types.Flags;
 import polyglot.types.TypeSystem;
+import polyglot.types.TypeSystem_c;
 import polyglot.util.Position;
 
 /**
@@ -30,7 +31,7 @@ public class X10ConstructorInstance_c extends ConstructorInstance_c implements
 	public X10ConstructorInstance_c() {
 		super();
 	}
-
+	
 	public X10ConstructorInstance_c(TypeSystem ts, Position pos,
 			ClassType container, Flags flags,  
 			List formalTypes, List excTypes) {
@@ -51,18 +52,31 @@ public class X10ConstructorInstance_c extends ConstructorInstance_c implements
 		super(ts, pos, container, flags, formalTypes, excTypes);
 		this.returnType = returnType;
 	}
-
+	
 	/* (non-Javadoc)
 	 * @see polyglot.ext.x10.types.X10ConstructorInstance#depClause()
 	 */
 	public Constraint constraint() { return returnType.realClause(); }
-	public void setReturnType(X10Type r) { this.returnType = r; }
+	public void setReturnType(X10Type r) { 
+		this.returnType = r; 
+		
+		}
 	public X10Type returnType() { return this.returnType;}
 	protected Constraint supClause;
 	public Constraint supClause() { return supClause; }
 	public void setSupClause(Constraint s) { 
-	
 		this.supClause = s; 
-		}
-
+	}
+	
+	public boolean callValidImplNoClauses(List argTypes) {
+		return X10MethodInstance_c.callValidImplNoClauses(this, argTypes);
+	}
+	@Override
+	public boolean callValidImpl(final List argTypes) {
+		return X10MethodInstance_c.callValidImpl(this, argTypes);
+	}
+	  public String signature() {
+	        return ((returnType != null) ? returnType.toString() : container.toString())
+	        + "(" + X10TypeSystem_c.listToString(formalTypes) + ")";
+	    }
 }
