b68f43e9ce593ee9d4b240a5f94ecd28ebdc5526
Robert Fuhrer <rfuhrer@us.ibm.com>
2007-01-12 23:25:34 +0000

    Added override for getMethod() so that it can properly detect a request for the sole method that this synthetic async type contains.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@3481 a326200e-df2c-42e0-81fa-597e909af41d

--- com.ibm.wala.cast.x10/src/com/ibm/wala/cast/x10/translator/polyglot/X10AsyncObject.java
+++ com.ibm.wala.cast.x10/src/com/ibm/wala/cast/x10/translator/polyglot/X10AsyncObject.java
@@ -8,6 +8,7 @@ import com.ibm.domo.ast.loader.AstFunctionClass;
 import com.ibm.wala.classLoader.IClassLoader;
 import com.ibm.wala.classLoader.IMethod;
 import com.ibm.wala.ipa.cha.*;
+import com.ibm.wala.types.Selector;
 import com.ibm.wala.types.TypeReference;
 
 /**
@@ -17,6 +18,10 @@ import com.ibm.wala.types.TypeReference;
 public class X10AsyncObject extends AstFunctionClass {
     private final ClassHierarchy cha;
 
+    // TODO RMF 1/12/2007 - Should probably take enough info to compute a proper method Selector
+    // for the method that this async pseudo-type contains. The name seems to contain the filename,
+    // and the descriptor/signature is for a no-arg method returning either void (if a syntactic
+    // async) or, if a syntactic future, the type produced by the future.
     public X10AsyncObject(TypeReference reference, TypeReference superReference, IClassLoader loader, CAstSourcePositionMap.Position fileName, ClassHierarchy cha) {
 	super(reference, superReference, loader, fileName);
 	this.cha = cha;
@@ -25,6 +30,13 @@ public class X10AsyncObject extends AstFunctionClass {
     public X10AsyncObject(TypeReference reference, IClassLoader loader, CAstSourcePositionMap.Position fileName, ClassHierarchy cha) {
 	super(reference, loader, fileName);
 	this.cha = cha;
+    }
+
+    @Override
+    public IMethod getMethod(Selector selector) {
+	// TODO RMF 1/12/2007 - Hack: if we had enough info at ctor time, we could
+	// properly compare the selector passed in to what we expect...
+	return functionBody;
     }
 
     public void setCodeBody(IMethod method) {
