7bcc81b578b454d78c27b9963dcf561a517894d6
Robert Fuhrer <rfuhrer@us.ibm.com>
2007-01-16 18:39:13 +0000

    Added new-fangled X10-savvy AnalysisEngine components.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@3484 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ trunk/com.ibm.wala.cast.x10/src/com/ibm/wala/cast/x10/client/X10ZeroXCFACallGraphBuilderFactory.java
@@ -0,0 +1,21 @@
+package com.ibm.domo.ast.x10.client;
+
+import com.ibm.domo.ast.x10.ipa.callgraph.X10ZeroXCFABuilder;
+import com.ibm.wala.client.CallGraphBuilderFactory;
+import com.ibm.wala.ipa.callgraph.AnalysisOptions;
+import com.ibm.wala.ipa.callgraph.AnalysisScope;
+import com.ibm.wala.ipa.callgraph.CallGraphBuilder;
+import com.ibm.wala.ipa.callgraph.impl.Util;
+import com.ibm.wala.ipa.callgraph.propagation.cfa.ZeroXInstanceKeys;
+import com.ibm.wala.ipa.cha.ClassHierarchy;
+import com.ibm.wala.util.warnings.WarningSet;
+
+public class X10ZeroXCFACallGraphBuilderFactory implements CallGraphBuilderFactory {
+
+    public CallGraphBuilder make(AnalysisOptions options, ClassHierarchy cha, AnalysisScope scope, WarningSet warnings,
+	    boolean keepPointsTo) {
+        Util.addDefaultSelectors(options, cha, warnings);
+        Util.addDefaultBypassLogic(options, scope, Util.class.getClassLoader(), cha);
+        return new X10ZeroXCFABuilder(cha, warnings, options, null, null, options.getReflectionSpec(), ZeroXInstanceKeys.NONE);
+    }
+}
