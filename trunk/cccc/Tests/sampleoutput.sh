38f9b61c0055b7c7aea238899c494168adb3fcf7
Paul McKenney <Paul.McKenney@us.ibm.com>
2007-02-01 06:04:44 +0000

    Adding new test files and corresponding sample output.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@3523 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/cccc/Tests/sampleoutput.sh
+++ trunk/cccc/Tests/sampleoutput.sh
@@ -1,19 +1,8 @@
-for i in \
-	1-Dekker.txt \
-	2-IRIW.txt \
-	3-CC.txt \
-	4-DC.txt \
-	fail1.txt \
-	fail2.txt \
-	fail3.txt \
-	lock.txt \
-	test1.txt \
-	test2.txt \
-	test3.txt
+. tests.files
+for i in $FILES
 do
 	echo '=========='
 	echo Generating Sample Output for $i
 	echo '=========='
-	basename=`echo $i | sed -e 's/\.txt//'`
-	../cccc < $i > $basename.out 2>&1
+	../src/cccc < $i.txt > $i.out 2>&1
 done
