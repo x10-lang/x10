e457394a16950e1fd100cf6e3f2f38ed783ffc14
Paul McKenney <Paul.McKenney@us.ibm.com>
2007-02-01 06:04:44 +0000

    Adding new test files and corresponding sample output.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@3523 a326200e-df2c-42e0-81fa-597e909af41d

--- cccc/Tests/testit.sh
+++ cccc/Tests/testit.sh
@@ -1,18 +1,8 @@
-for i in \
-	1-Dekker.txt \
-	2-IRIW.txt \
-	3-CC.txt \
-	4-DC.txt \
-	fail1.txt \
-	fail2.txt \
-	fail3.txt \
-	lock.txt \
-	test1.txt \
-	test2.txt \
-	test3.txt
+. tests.files
+for i in $FILES
 do
 	echo '=========='
 	echo Testing $i
 	echo '=========='
-	../cccc < $i
+	../src/cccc < $i.txt
 done
