003963faa2c795fbbbcc30e65c22e4b7b6a777a8
Vincent Cave <user@domain>
2007-02-05 09:30:44 +0000

    Added comments
    git-svn-id: svn://svn.code.sf.net/p/x10/code@3539 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.runtime/src/x10/runtime/X10Runnable.java
+++ trunk/x10.runtime/src/x10/runtime/X10Runnable.java
@@ -7,7 +7,20 @@
  */
 package x10.runtime;
 
+/**
+ * X10 Activity must implements this interface in order to 
+ * be schedulable by X10 runtime.
+ * 
+ * This interface has been introduced to avoid creation of 
+ * extra activities at runtime.
+ * Now the run method is directly implemented by X10 activity's 
+ * mother abstract class <code>Activity</code>
+ * @author vcave
+ */
 public interface X10Runnable extends Runnable {
 
+	/**
+	 * Method implementation encapsulates user code to run as an X10 activity. 
+	 */
 	public void runX10Task();
 }
