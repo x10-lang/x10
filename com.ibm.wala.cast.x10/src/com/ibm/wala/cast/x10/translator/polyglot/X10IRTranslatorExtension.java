a2f1825a3f6827d2764be7b1a3724d1b64db81c2
Robert Fuhrer <rfuhrer@us.ibm.com>
2007-02-14 17:05:13 +0000

    Accommodate move of CAst from DOMO to WALA.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@3570 a326200e-df2c-42e0-81fa-597e909af41d

--- com.ibm.wala.cast.x10/src/com/ibm/wala/cast/x10/translator/polyglot/X10IRTranslatorExtension.java
+++ com.ibm.wala.cast.x10/src/com/ibm/wala/cast/x10/translator/polyglot/X10IRTranslatorExtension.java
@@ -3,20 +3,16 @@
  */
 package com.ibm.domo.ast.x10.translator.polyglot;
 
-import polyglot.ext.x10.ExtensionInfo;
 import polyglot.frontend.Job;
-import polyglot.frontend.Scheduler;
 import polyglot.frontend.goals.Goal;
 
 import com.ibm.wala.cast.java.translator.JavaCAst2IRTranslator;
-import com.ibm.wala.cast.java.translator.polyglot.IRTranslatorExtension;
-import com.ibm.wala.cast.java.translator.polyglot.PolyglotSourceLoaderImpl;
 
-public class X10IRTranslatorExtension extends X10ExtensionInfo implements IRTranslatorExtension {
-	// Doesn't matter which goal we get here, because they all have the same ExtensionInfo
-	// which is what we're interested in
-	private X10IRGoal g;
-	
+public class X10IRTranslatorExtension extends X10ExtensionInfo {
+    // Doesn't matter which goal we get here, because they all have the same ExtensionInfo
+    // which is what we're interested in
+    private X10IRGoal g;
+
     public Goal getCompileGoal(Job job) {
     	X10IRGoal g = (X10IRGoal) ((DOMOScheduler) this.scheduler).IRGenerated(job);
     	if (this.g == null) 
