60d7068188eed4ca2b5560b1f6d050b7489a5b53
Igor Peshansky <igorp@us.ibm.com>
2007-04-10 03:18:59 +0000

    Add forced()
    git-svn-id: svn://svn.code.sf.net/p/x10/code@3621 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.runtime/src/x10/lang/Future.java
+++ trunk/x10.runtime/src/x10/lang/Future.java
@@ -16,10 +16,15 @@ package x10.lang;
  */
 public abstract class Future extends Object implements ValueType {
 
-	/**    
+	/**
 	 * Wait for the completion of this activity and return the
-	 * return value.   
-	 */    
+	 * return value.
+	 */
 	public abstract Object force();
-	
+
+	/**
+	 * Return true if this activity has completed.
+	 */
+	public abstract boolean forced();
+
 }
