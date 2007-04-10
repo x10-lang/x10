9616bc1687ee0be40c1d30eba3c2015dcf91d77c
Igor Peshansky <igorp@us.ibm.com>
2007-04-10 03:18:59 +0000

    Add forced()
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@3621 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.compiler/src/polyglot/ext/x10/types/FutureType_c.java
+++ x10.compiler/src/polyglot/ext/x10/types/FutureType_c.java
@@ -26,45 +26,52 @@ import polyglot.util.CodeWriter;
 import polyglot.util.Position;
 
 
-/** Implementation of Future type constructor.
- * 
- * <p>This code does not yet implement Termination Equivalence for Future Types, i.e.  
- * future future X = future X 
- * future nullable future X = future nullable X 
- * are not yet implemented.
- * 
- * @author vj
+/**
+ * Implementation of Future type constructor.
  *
+ * <p>This code does not yet implement Termination Equivalence for Future Types, i.e.
+ * future future X = future X
+ * future nullable future X = future nullable X
+ * are not yet implemented.
+ *
+ * @author vj
  */
 public class FutureType_c extends X10ReferenceType_c implements FutureType {
 	protected X10NamedType base;
 	protected List methods;
 	protected X10TypeSystem xts;
 	protected FutureType_c() {}
-	
+
 	/** We type base to Type instead of X10Type because we wish to reuse code from
-	 * jl which traffics in Type's. We rely on the global invariant, maintained manually 
+	 * jl which traffics in Type's. We rely on the global invariant, maintained manually
 	 * in this version of the code, that at runtime the only Type's created are X10Type's.
 
 	 * @param ts
 	 * @param pos
 	 * @param base
 	 */
-	public FutureType_c( TypeSystem ts, Position pos, X10NamedType base ) {
+	public FutureType_c(TypeSystem ts, Position pos, X10NamedType base) {
 		super( ts, pos);
-		this.base =  base;
-		this.methods = new ArrayList(1);
-		this.methods.add(((X10TypeSystem) ts).methodInstance( position(),
+		this.base = base;
+		this.methods = new ArrayList(2);
+		// [IP] FIXME: is the cast below necessary?
+		this.methods.add(((X10TypeSystem) ts).methodInstance(position(),
 				this,
 				ts.Public(),
 				base,
 				"force",
 				Collections.EMPTY_LIST,
-			
-				Collections.EMPTY_LIST	));	
-        xts = (X10TypeSystem) ts;
+				Collections.EMPTY_LIST));
+		this.methods.add(((X10TypeSystem) ts).methodInstance(position(),
+				this,
+				ts.Public(),
+				ts.Boolean(),
+				"forced",
+				Collections.EMPTY_LIST,
+				Collections.EMPTY_LIST));
+		xts = (X10TypeSystem) ts;
 	}
-	
+
 	public Constraint realClause() {
 		return null;
 	}
@@ -78,8 +85,8 @@ public class FutureType_c extends X10ReferenceType_c implements FutureType {
 	public boolean propertiesElaborated() { return true; }
 	public String name() { return base.name();}
 	public String fullName() { return base.fullName();}
-	
-	/* The only method is: public <base> force().
+
+	/* The only methods are: public <base> force() and public boolean forced().
 	 * @see polyglot.types.ReferenceType#methods()
 	 */
 	public List methods() {
@@ -107,7 +114,7 @@ public class FutureType_c extends X10ReferenceType_c implements FutureType {
 		return Collections.EMPTY_LIST;
 	}
 
-	/* 
+	/*
 	 * @see polyglot.types.Type#translate(polyglot.types.Resolver)
 	 */
 	public String translate(Resolver c) {
@@ -121,16 +128,14 @@ public class FutureType_c extends X10ReferenceType_c implements FutureType {
 		w.write("x10.lang.Future");
 	}
 
-	/* 
+	/*
 	 * @see java.lang.Object#toString()
 	 */
 	public String toString() {
-		
 		return toStringForDisplay();
 	}
 	public String toStringForDisplay() {
 		return "future<" + base().toStringForDisplay( ) +">";
-		
 	}
 
 	/* Return the base type.
@@ -146,22 +151,22 @@ public class FutureType_c extends X10ReferenceType_c implements FutureType {
 	public FieldInstance fieldNamed(String name) {
 		return null;
 	}
-	
+
 	public boolean isImplicitCastValidImpl(Type origType) {
-		if ( Report.should_report("debug", 5) )  {
+		if (Report.should_report("debug", 5)) {
 			Report.report(5, "[FutureType_c] |" + this + "|.isImplicitCastValidImpl(|" + origType +"|):");
 		}
 
 		X10Type toType = (X10Type) origType;
-        NullableType targetType = toType.toNullable();
+		NullableType targetType = toType.toNullable();
 		if (targetType != null) {
 			toType = targetType.base();
 		}
 
-        FutureType target = toType.toFuture();
+		FutureType target = toType.toFuture();
 		if (target != null) {
 			boolean result = base().isImplicitCastValidImpl( target.base() );
-			if ( Report.should_report("debug", 5) )  {
+			if (Report.should_report("debug", 5)) {
 				Report.report(5, "[FutureType_c] ..." + result);
 			}
 			return result;
@@ -169,7 +174,7 @@ public class FutureType_c extends X10ReferenceType_c implements FutureType {
 
 		// Otherwise do the default check
 		boolean result = super.isImplicitCastValidImpl(toType);
-		if ( Report.should_report("debug", 5) )  {
+		if (Report.should_report("debug", 5)) {
 			Report.report(5, "[FutureType_c] ... "+result);
 		}
 		return result;
@@ -177,47 +182,46 @@ public class FutureType_c extends X10ReferenceType_c implements FutureType {
 
 	public boolean isCastValidImpl(Type origType) {
 		if (Report.should_report("debug", 5)) {
-			Report.report(5, "[FutureType_c] |" +  this + "|.isCastValidImpl(|" + origType +"|):");
+			Report.report(5, "[FutureType_c] |" + this + "|.isCastValidImpl(|" + origType +"|):");
 		}
 
 		X10Type toType = (X10Type) origType;
-        NullableType nullType = toType.toNullable();
-		if (nullType != null) 
+		NullableType nullType = toType.toNullable();
+		if (nullType != null)
 			toType = nullType.base();
-		
-        FutureType target = X10Type_c.toFuture(toType);
+
+		FutureType target = X10Type_c.toFuture(toType);
 		if (target != null) {
 			boolean result = base().isCastValidImpl( target.base() );
 			if (Report.should_report("debug", 5)) {
-				Report.report(5, "[FutureType_c] ... target=|" +  target + "|.");
-				Report.report(5, "[FutureType_c] ... returns |" +  result + "|.");
+				Report.report(5, "[FutureType_c] ... target=|" + target + "|.");
+				Report.report(5, "[FutureType_c] ... returns |" + result + "|.");
 			}
 			return result;
 		}
 
 		// Otherwise do the default check
 		boolean result = super.isCastValidImpl(toType);
-		if ( Report.should_report("debug", 5) )  {
+		if (Report.should_report("debug", 5)) {
 			Report.report(5, "[FutureType_c] ... "+result);
 		}
 		return result;
 	}
 
-     
-     public boolean typeEqualsImpl(Type o) {
-         return equalsImpl(o);
-     }
-     public int hashCode() {
-         return 
-           (rootType == this ? base.hashCode() << 2 : rootType.hashCode() ) 
-         + (depClause != null ? depClause.hashCode() : 0)
-         + ((typeParameters !=null && ! typeParameters.isEmpty()) ? typeParameters.hashCode() :0);
-         
-     }
+
+	public boolean typeEqualsImpl(Type o) {
+		return equalsImpl(o);
+	}
+	public int hashCode() {
+		return
+		(rootType == this ? base.hashCode() << 2 : rootType.hashCode() )
+		+ (depClause != null ? depClause.hashCode() : 0)
+		+ ((typeParameters !=null && ! typeParameters.isEmpty()) ? typeParameters.hashCode() :0);
+	}
 	public boolean equalsImpl(TypeObject t) {
-        if (! (t instanceof FutureType)) 
-            return false;
-        FutureType other = (FutureType) t;
+		if (! (t instanceof FutureType))
+			return false;
+		FutureType other = (FutureType) t;
 		return ts.typeEquals(base(), other.base());
 	}
 
@@ -229,21 +233,20 @@ public class FutureType_c extends X10ReferenceType_c implements FutureType {
 	public boolean isValueType() {
 		return true;
 	}
-    /**
-     * future<T> has no properties.
-     */
-    public List<FieldInstance> properties() {
-        return Collections.EMPTY_LIST;
-    }
-    public List<FieldInstance> definedProperties() {
-        return Collections.EMPTY_LIST;
-    }
-    public NullableType toNullable() { 
-        return X10Type_c.toNullable(this);
-    }
-    public boolean safe() {
-    	return false;
-    }
-   
+	/**
+	 * future<T> has no properties.
+	 */
+	public List<FieldInstance> properties() {
+		return Collections.EMPTY_LIST;
+	}
+	public List<FieldInstance> definedProperties() {
+		return Collections.EMPTY_LIST;
+	}
+	public NullableType toNullable() {
+		return X10Type_c.toNullable(this);
+	}
+	public boolean safe() {
+		return false;
+	}
 }
 
