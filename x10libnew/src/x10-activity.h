ec3046113a9306674f4345cafc4fc3e70970f55e
Vijay Saraswat <vsaraswa@us.ibm.com>
2007-04-13 02:47:09 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@3629 a326200e-df2c-42e0-81fa-597e909af41d

--- x10libnew/src/x10-activity.h
+++ x10libnew/src/x10-activity.h
@@ -1,37 +1,37 @@
 #ifndef X10ACTIVITY_H_
 #define X10ACTIVITY_H_
 
-typedef int32_t X10_async_arg_t;
-typedef uint8_t X10_async_handler_t;
+typedef int32_t x10_async_arg_t;
+typedef uint8_t x10_async_handler_t;
 
 /**
  * Spawn an async locally.
  */
-extern int X10_async_spawn(X10_async_handler_t handler, int numargs, ...);
+extern int x10_async_spawn(x10_async_handler_t handler, int numargs, ...);
 
 /**
  * Spawn a clocked async locally.
  */
-extern int X10_async_spawn_clocked(X10_async_handler_t handler, X10_clock_t* clocks, int numargs, ...);
+extern int x10_async_spawn_clocked(x10_async_handler_t handler, x10_clock_t* clocks, int numargs, ...);
 
 /**
  * Spawn a switched async locally. Note: switches must always propagated to spawned activities.
  * That is, if an activity A spawns another activity B, it must always pass on its switch (if any) to B.
  */
-extern int X10_async_spawn_switched(X10_async_handler_t handler, X10_switch_t* clocks, int numargs, ...);
+extern int x10_async_spawn_switched(x10_async_handler_t handler, x10_switch_t* clocks, int numargs, ...);
 
 /**
  * Spawn an async in the given place.
  */
-extern int X10_async_spawn(X10_place_t place, X10_async_handler_t handler, int numargs, ...);
+extern int x10_async_spawn(x10_place_t place, x10_async_handler_t handler, int numargs, ...);
 /**
  * Spawn a clocked async in the given place.
  */
-extern int X10_async_spawn_clocked(X10_place_t place, X10_async_handler_t handler, X10_clock_t* clocks, int numargs, ...);
+extern int x10_async_spawn_clocked(x10_place_t place, x10_async_handler_t handler, x10_clock_t* clocks, int numargs, ...);
 /**
  * Spawn a clocked async in the given place.
  */
-extern int X10_async_spawn_switched(X10_place_t place, X10_async_handler_t handler, X10_switch_t* clocks, int numargs, ...);
+extern int x10_async_spawn_switched(x10_place_t place, x10_async_handler_t handler, x10_switch_t* clocks, int numargs, ...);
 
 
 
--- x10libnew/src/x10-switch.h
+++ x10libnew/src/x10-switch.h
@@ -1,5 +1,7 @@
 #ifndef X10SWITCH_H_
 #define X10SWITCH_H_
+
+using namespace x10::lang;
 //*******************SWITCH OPERATIONS**********************************
 /** A switch is an asymmetric clock -- only the thread creating it
  *  may perform a next operation on it. 
@@ -8,48 +10,46 @@
  * to these operations.  The same switch may be used for nonblocking calls
  * issued to different destination nodes. 
  */
-typedef struct {
-	// data needed for book-keeping associaed with remote operation.
-	X10_remoteop_t* next;
-} X10_remoteop_t;
-typedef struct {
-	X10_remoteop_t* op;
-} X10_switch_t;
+typedef Switch& X10_switch_t;
 
-/**
- * Create and return a switch. Nonblocking operations involving any remote
- * node may be posted on this switch. A reference to the switch is 
- * kept in core API datastructures.
- */
-extern X10_switch_t* X10_make_switch();
-
-/** Wait until the switch is closed. The switch must have been created by
- * the current thread, otherwise the result is undefined. 
- * The switch is closed when all operations posted to the switch have
- * been performed. After this call returns, the switch may be used again 
- * (the clock advances).
- * Note that the implementation need not start performing any remote nb operations
- * posted on a switch h until X10_wait(h) has been called. 
- * At this point, all the remote operations to be performed
- * are known, and the implementation may aggregate remote put and get
- * requests for the same node separately.
- * WARNING: The programmer must ensure that the remote operation is scheduled
- * on the switch in the right phase.
- */                    
-extern int X10_wait(X10_switch_t* handle); 
-
-/** Perform an X10_wait(h) on all switches h in the order in which they were created
- * by this thread. 
- */     
-extern int X10_wait(void);
-
-/** Check if the switch is closed. The switch must have been created by
- * the current thread, otherwise the result is undefined.
- * The switch is closed when all operations posted
- * to the switch have been performed. Returns X10_OK if the switch is closed,
- * X10_IN_PROGRESS otherwise.
- * Extension TBD: Consider returning (a lower bound on) the number of incomplete operations.
- */   
-extern int X10_try_wait(X10_switch_t* handle);   
+public class Switch : public Object {
+	public: 
+	/**
+	 * Create and return a switch. Nonblocking operations involving any remote
+	 * node may be posted on this switch. A reference to the switch is 
+	 * kept in core API datastructures.
+	 */
+	  Switch();
+	
+	
+	/** Wait until the switch is closed. The switch must have been created by
+	 * the current thread, otherwise the result is undefined. 
+	 * The switch is closed when all operations posted to the switch have
+	 * been performed. After this call returns, the switch may be used again 
+	 * (the clock advances).
+	 * Note that the implementation need not start performing any remote nb operations
+	 * posted on a switch h until X10_wait(h) has been called. 
+	 * At this point, all the remote operations to be performed
+	 * are known, and the implementation may aggregate remote put and get
+	 * requests for the same node separately.
+	 * WARNING: The programmer must ensure that the remote operation is scheduled
+	 * on the switch in the right phase.
+	 */                    
+	int wait(); 
+	
+	/** Perform an X10_wait(h) on all switches h in the order in which they were created
+	 * by this thread. 
+	 */     
+	static int wait();
+	
+	/** Check if the switch is closed. The switch must have been created by
+	 * the current thread, otherwise the result is undefined.
+	 * The switch is closed when all operations posted
+	 * to the switch have been performed. Returns X10_OK if the switch is closed,
+	 * X10_IN_PROGRESS otherwise.
+	 * Extension TBD: Consider returning (a lower bound on) the number of incomplete operations.
+	 */   
+	int try_wait();   
+}
 
 #endif /*X10SWITCH_H_*/
