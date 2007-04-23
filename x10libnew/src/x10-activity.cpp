d934ad8f25e71cb63804e0065c6dea9db1dbff32
Ganesh Bikshandi <ganeshvb@domain>
2007-04-23 06:56:48 +0000

    few more .cpps
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@3650 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ x10libnew/src/x10-activity.cpp
@@ -0,0 +1,39 @@
+#include "x10-activity.h"
+
+using namespace x10lib;
+
+
+ error_t async_spawn (async_handler_t handler, int numargs, ...)
+{
+	assert(false);
+}	
+		
+
+ error_t async_spawn_clocked (async_handler_t handler, clock_t clocks, int numargs, ...)
+{
+	assert(false);
+}	
+
+
+ error_t async_spawn_switched (async_handler_t handler, switch_t clocks, int numargs, ...)
+{
+	assert(false);
+}	
+
+
+ error_t async_spawn (place_t place, async_handler_t handler, int numargs, ...)
+{
+	assert(false);
+}	
+
+
+ error_t async_spawn_clocked (place_t place, async_handler_t handler, clock_t clocks, int numargs, ...)
+ {
+	assert(false);
+ }	
+
+ error_t async_spawn_switched (place_t place, async_handler_t handler, switch_t clocks, int numargs, ...)
+{
+	assert(false);
+}	
+ 
