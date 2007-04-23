34e7120981de46becc237592012cff78ccd116b4
Ganesh Bikshandi <ganeshvb@domain>
2007-04-23 06:55:27 +0000

    few more .o's
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@3649 a326200e-df2c-42e0-81fa-597e909af41d

--- x10libnew/src/x10-activity.h
+++ x10libnew/src/x10-activity.h
@@ -2,14 +2,15 @@
 #define X10ACTIVITY_H_
 
 #include "x10-common.h"
+#include "x10-switch.h"
 
 namespace x10lib{
-
 
 /**
  * Spawn an async locally.
  */
  error_t async_spawn (async_handler_t handler, int numargs, ...);
+
 		
 /**
  * Spawn a clocked async locally.
@@ -23,59 +24,21 @@ namespace x10lib{
  error_t async_spawn_switched (async_handler_t handler, switch_t clocks, int numargs, ...);
 
 
-
 /**
  * Spawn an async in the given place.
  */
  error_t async_spawn (place_t place, async_handler_t handler, int numargs, ...);
+
 /**
  * Spawn a clocked async in the given place.
  */
  error_t async_spawn_clocked (place_t place, async_handler_t handler, clock_t clocks, int numargs, ...);
+
+
 /**
  * Spawn a clocked async in the given place.
  */
  error_t async_spawn_switched (place_t place, async_handler_t handler, switch_t clocks, int numargs, ...);
- 
- 
-/**
- * Handling Futures
- *
- */
- 
-template<typename result_t>
-class Future {
-	public:
-   		Future () {};
-   		result_t force ();
-  
- 	private:
- 		result_t getResult();
- 		void setResult(const result_t& result);
-};
- 
- /**
-  * Create future locally using either clocks or switches
-  * (Future<result_t> is the return type) 
-  */ 
-template<typename result_t>
-Future<result_t> future_spawn (async_handler_t handler, int numargs, ...);
-template<typename result_t>
-Future<result_t> future_spawn_clocked (async_handler_t handler, clock_t clocks, int numargs, ...);
-template<typename result_t>
-Future<result_t> future_spawn_switched (async_handler_t handler, switch_t clocks, int numargs, ...);
-
-/**
-  * Create future remotely using either clocks or switches
-  * (Future<result_t> is the return type)
-  */ 
-template<typename result_t>
-Future<result_t> future_spawn (place_t place, async_handler_t handler, int numargs, ...);
-template<typename result_t>
-Future<result_t> future_spawn_clocked (place_t place, async_handler_t handler, clock_t clocks, int numargs, ...);
-template<typename result_t>
-Future<result_t> future_spawn_switched (place_t place, async_handler_t handler, switch_t clocks, int numargs, ...);
 
 }
-
 #endif /*X10ACTIVITY_H_*/
