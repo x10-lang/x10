7cad1af1322cf6a7395bf8462018a26efd792e8e
Ganesh Bikshandi <ganeshvb@domain>
2007-04-26 05:27:35 +0000

    ..
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@3660 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.lib/src/assert.h
+++ x10.lib/src/assert.h
@@ -1,6 +1,14 @@
+/*
+ * (c) Copyright IBM Corporation 2007
+ *
+ * This file is part of X10 Runtime System.
+ * Author : Ganesh Bikshandi
+ */
+
+/* $Id: assert.h,v 1.2 2007-04-26 05:27:35 ganeshvb Exp $ */
+
 #ifndef __X10_ASSERT__H__
 #define __X10_ASSERT_H__
-
 
 #ifdef WARN
 #include <iostream>
@@ -9,6 +17,9 @@ using namespace std;
 #define stringize(a) #a
 
 #define assert(cond) \
+
+  //  This is a wrapper around default assert which only prints the message,
+  // but not causes the program to abort.
   do {									\
     if ((cond) == 0)							\
       cout << "assert " << stringize(cond) << " failed: " << __FILE__  << ", line " \
