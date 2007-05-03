27f97b2a955494a8829f8b552bb676d3a51c460b
Sreedhar Kodali <srkodali@in.ibm.com>
2007-05-03 11:40:35 +0000

    Added reference to the original code developer.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@3668 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.lib/benchmarks/rand/cpp/gups.cpp
+++ x10.lib/benchmarks/rand/cpp/gups.cpp
@@ -1,3 +1,13 @@
+/*
+ * (c) Copyright IBM Corporation 2007
+ *
+ * $Id: gups.cpp,v 1.2 2007-05-03 11:40:35 srkodali Exp $
+ * This file is part of X10 Runtime System.
+ *
+ * Original version of this code using LAPI C/C++ is due to
+ * Hanhong Xue <hanhong@us.ibm.com>
+ */
+
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
