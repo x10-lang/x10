def468366ea8306c4c08a3134f9e7e29e85f45a4
Ganesh Bikshandi <ganeshvb@domain>
2007-06-07 10:45:30 +0000

    ..
    git-svn-id: svn://svn.code.sf.net/p/x10/code@3822 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.apps/RandomAccess/run.sh
+++ trunk/x10.apps/RandomAccess/run.sh
@@ -5,7 +5,7 @@ export MP_EUILIB=us
 
 make 
 
-echo "syntax: ./run.sh log_table_size num_procs host_xxx.list"
+echo "syntax: ./run.sh log_table_size num_procs host_XXX.list (e.g. XXX=r36n11)"
 
 poe ./RandomAccess_spmd.exe -o -m $1 -procs $2 -hostfile /u/gbikshan/Work2/x10.lib/hostfiles/$3  >> out.$1.$2
 
