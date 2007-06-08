cf24626d133964079b37744ca79e2c7521898c16
Sreedhar Kodali <srkodali@in.ibm.com>
2007-06-08 07:19:04 +0000

    Minor changes to overcome problems due to namespace lookup.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@3830 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.lib/src/xassert.h
+++ trunk/x10.lib/src/xassert.h
@@ -5,26 +5,30 @@
  * Author : Ganesh Bikshandi
  */
 
-/* $Id: xassert.h,v 1.3 2007-05-31 11:25:57 ganeshvb Exp $ */
+/* $Id: xassert.h,v 1.4 2007-06-08 07:19:04 srkodali Exp $ */
 
 #ifndef __X10_ASSERT_H__
 #define __X10_ASSERT_H__
 
-  /*  This is a wrapper around default assert which only prints the message,
-      but not causes the program to abort.
-      Additionally it also prints the process id.
-      And it does beep :)
-      Use -DWARN compiler flag to enable this  */
+/* This is a wrapper around default assert which only prints
+ * the message, but not causes the program to abort.
+ * Additionally it also prints the process id.
+ * And it does beep :)
+ * Use -DWARN compiler flag to enable this.
+ */
 #define stringize(a) #a
+
 #ifdef __cplusplus
 #ifdef WARN
+#include <iostream>
+using namespace std;
 #define assert(cond) \
-  do {									\
-    if ((cond) == 0)							\
-      cout << "assert " << stringize(cond) << " failed: " << x10lib::here () << " " << __FILE__  << ", line " \
-      << __LINE__ << "\a" << endl;						\
-  }while(0) 
-
+ do { \
+ 	if ((cond) == 0) \
+    	cout << "assert " << stringize(cond) << " failed: " \
+			<< x10lib::here () << " " << __FILE__  << ", line " \
+      		<< __LINE__ << "\a" << endl; \
+ } while(0) 
 #else
 #include <cassert>
 #endif
