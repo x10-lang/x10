e9649c1d04183c56653359aa88b1c6ab9d402489
Igor Peshansky <igorp@us.ibm.com>
2007-06-21 13:34:10 +0000

    Add toString and prettyPrint
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@3928 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.compiler/src/polyglot/ext/x10/ast/ForLoop_c.java
+++ x10.compiler/src/polyglot/ext/x10/ast/ForLoop_c.java
@@ -17,13 +17,14 @@ import polyglot.ast.Formal;
 import polyglot.ast.Loop;
 import polyglot.ast.Node;
 import polyglot.ast.Stmt;
-import polyglot.ast.Term;
 import polyglot.ext.x10.types.X10Type;
 import polyglot.ext.x10.types.X10TypeSystem;
 import polyglot.types.SemanticException;
+import polyglot.util.CodeWriter;
 import polyglot.util.Position;
 import polyglot.visit.CFGBuilder;
 import polyglot.visit.FlowGraph;
+import polyglot.visit.PrettyPrinter;
 import polyglot.visit.TypeChecker;
 
 /**
@@ -31,7 +32,7 @@ import polyglot.visit.TypeChecker;
  *
  * @author vj Dec 9, 2004
  */
-public class ForLoop_c extends X10Loop_c implements Loop, ForLoop {
+public class ForLoop_c extends X10Loop_c implements ForLoop {
 
 	/**
 	 * @param pos
@@ -67,17 +68,26 @@ public class ForLoop_c extends X10Loop_c implements Loop, ForLoop {
 		X10TypeSystem ts = (X10TypeSystem) tc.typeSystem();
 		Expr newDomain = n.domain;
 		X10Type type = (X10Type) newDomain.type();
+		// FIXME: [IP] remove desugaring
 		if (ts.isDistribution(type))
 			newDomain = (Expr) tc.nodeFactory().Field(n.position(), newDomain, tc.nodeFactory().Id(n.position(), "region")).del().typeCheck(tc);
 		return n.domain(newDomain);
 	}
 
-	public Term continueTarget() {
-		return formal.entry();
-	}
-
-	public Expr cond() { return null; }
 	public boolean condIsConstant() { return false; }
 	public boolean condIsConstantTrue() { return false; }
+
+	public String toString() {
+		return "for (" + formal + ":" + domain + ")" + body;
+	}
+
+	public void prettyPrint(CodeWriter w, PrettyPrinter tr) {
+		w.write("for(");
+		printBlock(formal, w, tr);
+		w.write(" : ");
+		printBlock(domain, w, tr);
+		w.write(") ");
+		printSubStmt(body, w, tr);
+	}
 }
 
