7a13272fa1e629497926ae46791be119ba289c83
Sreedhar Kodali <srkodali@in.ibm.com>
2007-06-25 17:03:20 +0000

    X10Lib's exmaple program demonstrating 'put' operation.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@3965 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.lib/examples/run_v80.sh
+++ trunk/x10.lib/examples/run_v80.sh
@@ -3,7 +3,7 @@
 #
 # (c) Copyright IBM Corporation 2007
 #
-# $Id: run_v80.sh,v 1.3 2007-06-25 16:41:32 srkodali Exp $
+# $Id: run_v80.sh,v 1.4 2007-06-25 17:03:20 srkodali Exp $
 # This file is part of X10 Runtime System.
 #
 
@@ -26,4 +26,9 @@ echo "\n.....done"
 echo "Running Example 3 (addr_c.c).....\n"
 echo "poe ./addr_c -procs 2 -hostfile ../hostfiles/host_v80.list"
 poe ./addr_c -procs 2 -hostfile ../hostfiles/host_v80.list
+echo "\n.....done"
+
+echo "Running Example 4 (recv_.c).....\n"
+echo "poe ./recv_c -procs 2 -hostfile ../hostfiles/host_v80.list"
+poe ./recv_c -procs 2 -hostfile ../hostfiles/host_v80.list
 echo "\n.....done"
