7eeedc9eb78f3575f0d729fcd82d28181b3abec1
Ganesh Bikshandi <ganeshvb@domain>
2007-07-03 12:43:53 +0000

    add the result file. Feel free to overwrite it with your results.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@4060 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.apps/experiments/RandomAccess/host.pl
+++ trunk/x10.apps/experiments/RandomAccess/host.pl
@@ -2,8 +2,8 @@
 
 `rm host.list`;
 for ($i = 0; $i < 16; $i = $i + 1) {
-  for ($j = $ARGV[0]; $j <= $ARGV[1]; $j = $j+1) {
-      `echo v80n$j >> host.list`;
+  for ($j = $ARGV[1]; $j <= $ARGV[2]; $j = $j+1) {
+      `echo $ARGV[0]$j >> host.list`;
    }
 }
 
