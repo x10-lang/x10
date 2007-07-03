76da2a3a21cb18577c171fb30a0057c1455ff94f
Ganesh Bikshandi <ganeshvb@domain>
2007-07-03 11:32:26 +0000

    Earlier commit was unsuccesful
    git-svn-id: svn://svn.code.sf.net/p/x10/code@4058 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.apps/RandomAccess/run.sh
+++ trunk/x10.apps/RandomAccess/run.sh
@@ -7,12 +7,12 @@ make
 
 echo "syntax: ./run.sh log_table_size num_procs host_XXX.list (e.g. XXX=r36n11)"
 
-poe ./RandomAccess_spmd.exe -o  -m $1 -procs $2 -hostfile /u/gbikshan/Work2/x10.lib/hostfiles/$3  >> out.$1.$2
+poe ./RandomAccess_spmd.exe -o  -m $1 -procs $2 >> out.$1.$2
 
-poe @versions/RandomAccess_spmd.v01.exe -o  -m $1 -procs $2 -hostfile /u/gbikshan/Work2/x10.lib/hostfiles/$3  >> out.$1.$2
-poe @versions/RandomAccess_spmd.v02.exe -o  -m $1 -procs $2 -hostfile /u/gbikshan/Work2/x10.lib/hostfiles/$3  >> out.$1.$2
-poe @versions/RandomAccess_spmd.v03.exe -o  -m $1 -procs $2 -hostfile /u/gbikshan/Work2/x10.lib/hostfiles/$3  >> out.$1.$2
+#poe @versions/RandomAccess_spmd.v01.exe -o  -m $1 -procs $2  >> out.$1.$2
+poe @versions/RandomAccess_spmd.v02.exe -o  -m $1 -procs $2 >> out.$1.$2
+poe @versions/RandomAccess_spmd.v03.exe -o  -m $1 -procs $2  >> out.$1.$2
 
 a=$(($1+2))
-poe ./gups.exe -a $1 $a -procs $2 -hostfile /u/gbikshan/Work2/x10.lib/hostfiles/$3  >> out.$1.$2
+poe ./gups.exe -a $1 $a -procs $2  >> out.$1.$2
 
