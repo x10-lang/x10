51d249188d331f40cae6cd268cf8780dde9776bc
Ganesh Bikshandi <ganeshvb@domain>
2007-07-03 11:26:00 +0000

    Moved from x10.backend (orquesta) to x10.apps (SF)
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@4057 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ x10.apps/experiments/RandomAccess/run.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+export MP_MSG_API=lapi,mpi
+export MP_TASK_AFFINITY=MCM
+export MP_EUIDEVICE=sn_all
+export MP_ADAPTER_USE=dedicated
+export MP_EUILIB=us
+export MP_CPU_USE=unique
+export MP_CSS_INTERRUPT=no
+export MP_USE_BULK_XFER=yes
+export MP_EAGER_LIMIT=65535
+export MP_PULSE=0
+export MP_POLLING_INTERVAL=2000000000
+export MP_SHARED_MEMORY=yes
+export MP_SINGLE_THREAD=yes
+export MP_WAIT_MODE=poll
+
+make
+
+echo "Syntax: ./run.sh" 
+
+`rm tmp.out`;
+for n in `seq 0 $1`
+do
+./run_internal.sh 16 $((2**$n)) >> tmp.out
+done
+
+./post.pl tmp.out
+
---
+++ x10.apps/experiments/RandomAccess/x10lib/RandomAccess_spmd.h
@@ -0,0 +1,90 @@
+/*
+ * (c) Copyright IBM Corporation 2007
+ *
+ * x10lib version of RandomAccess  
+ * Author : Ganesh Bikshandi
+ */
+
+/* $Id: RandomAccess_spmd.h,v 1.1 2007-07-03 11:26:00 ganeshvb Exp $ */
+
+/* Main version */
+#include <iostream>
+
+#include "x10lang.h"
+#include <x10/x10lib.h>
+
+using namespace std;
+using namespace x10lib;
+using namespace x10::lang;
+using namespace java::lang;
+
+/* This gives the opportunity to use either int64_t or uint64_t */
+
+typedef long long glong_t;
+typedef long long sglong_t;
+
+//typedef int32_t glong_t;
+//typedef int32_t sglong_t;
+
+class RandomAccess_Dist {
+  
+public:  
+  
+  class localTable {
+    
+  public:
+    const glong_t tableSize;
+    const glong_t mask;
+    glong_t *array;
+    
+  public:
+    localTable (glong_t size) :
+      tableSize (size),
+      mask (size-1),
+      array (makeLocalArray<glong_t, 1>(size)) 
+    { }
+        
+    inline void update (glong_t ran); 
+
+    inline void verify (glong_t ran);
+  };  
+ 
+private:
+ 
+  static double mysecond (); 
+  static const glong_t POLY = 0x0000000000000007LL;  
+  static const glong_t PERIOD = 131762457669539401LL;
+ 
+  static Dist<1>*  UNIQUE;
+  static int NUMPLACES;
+  static int PLACEIDMASK;
+
+  static const int UPDATE = 0;
+  static const int VERIFICATION_P = 1;  
+  static const int UPDATE_AND_VERIFICATION = 2;
+
+  static glong_t HPCC_starts(sglong_t n);
+
+  static void Verify (glong_t LogTableSize, bool embarrassing, localTable* Table);
+  
+  //static void RandomAccessUpdate (const glong_t LogTableSize, const bool Embarrasing, localTable* Table); 
+  
+public:
+  static void main (x10::array<x10::ref<x10::lang::String> >& args);
+
+};
+
+struct 
+{
+  RandomAccess_Dist::localTable* Table; 
+  glong_t* SUM;
+}GLOBAL_SPACE;
+
+extern "C" 
+{
+  extern int main (int ac, char** av);
+}
+
+// Local Variables:
+// mode: C++
+// End:
