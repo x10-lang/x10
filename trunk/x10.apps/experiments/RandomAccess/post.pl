aa0d538d38abca9e8ea1dc9ea5953c54b8298d4f
Ganesh Bikshandi <ganeshvb@domain>
2007-07-06 15:26:35 +0000

    Updated the README. Few changes to the  scripts.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@4076 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.apps/experiments/RandomAccess/post.pl
+++ trunk/x10.apps/experiments/RandomAccess/post.pl
@@ -4,9 +4,9 @@
 use POSIX qw (ceil floor);
 open(INPUT, "$ARGV[0]") or die( "Error: cannot open file $SCRATCH" );
 
-@bench0 = ('0', '0', '0', '0', '0');
-@bench1 = ('0', '0', '0', '0', '0');
-@bench2 = ('0', '0', '0', '0', '0');
+@bench0 = ('0');
+@bench1 = ('0');
+@bench2 = ('0');
 my @time = (\@bench0, \@bench1, \@bench2);
 my @total = ([], [], []);
 $procs = 0;
@@ -25,49 +25,49 @@ while( $aline = <INPUT> ){
  if ($aline =~ /NPROCS\ *=\ *(\d*)/ ){
    $procs = log2($1);
    $nprocs++;
+   $i = 0;
  }
 
-  if ($aline =~ /X10LIB/) {
+  if ($aline =~ /EXPT1/) {
     $i = 0;
   } 
 
-  if ($aline =~ /X10C/) {
+  if ($aline =~ /EXPT2/) {
     $i = 1;
   } 
 
-  if ($aline =~ /HPCC/) {
+  if ($aline =~ /EXPT3/) {
     $i = 2;
   } 
 
   if ($aline =~ /CPU time used\ *=\ *(\d*.\d*) seconds/) {
     $time[$i][$procs] = $time[$i][$procs] + $1;
-   # print "$1  $time[$i][$procs]";
+    print "$1  $time[$i][$procs]";
   }
 
   if ($aline =~/\ *(\d*.\d*) Billion/) {
-  #  print "$1  ";
     $gups[$i][$procs] = $gups[$i][$procs] + $1;
     $total[$i][$procs] = $total[$i][$procs] + 1;
   }
 }
 
-  $n = $#bench0;
-  print "==========GUPS=========== \n";
-  printf ("PROCS\tX10LIB\tX10C\tHPCC\n");
+  $n = max ($#bench0, $#bench1, $#bench2);
+  print "==========GUPS===========$n \n";
+  printf ("PROCS\tEXPT1\tEXPT2\tEXPT3\n");
   for ($i= 0; $i <= $n; $i++) {
-    $gups[0][$i] = $gups[0][$i] / $total[0][$i];
-    $gups[1][$i] = $gups[1][$i] / $total[1][$i];
-    $gups[2][$i] = $gups[2][$i] / $total[2][$i]; 
+    $gups[0][$i] = $gups[0][$i] / max ($total[0][$i], 1);
+    $gups[1][$i] = $gups[1][$i] / max ($total[1][$i], 1);
+    $gups[2][$i] = $gups[2][$i] / max ($total[2][$i], 1); 
 
     printf ( "%d\t%0.5f\t%0.5f\t%0.5f\n", 2**$i, $gups[0][$i], $gups[1][$i], $gups[2][$i]);
   }
 
   print "==========TIME(Seconds)=========== \n";
-  printf ("PROCS\tX10LIB\tX10C\tHPCC\n");
+  printf ("PROCS\tEXPT1\tEXPT2\tEXPT3\n");
   for ($i= 0; $i <= $n; $i++) {
-    $time[0][$i] = $time[0][$i] / $total[0][$i];
-    $time[1][$i] = $time[1][$i] / $total[1][$i];
-    $time[2][$i] = $time[2][$i] / $total[2][$i]; 
+    $time[0][$i] = $time[0][$i] / max ($total[0][$i], 1);
+    $time[1][$i] = $time[1][$i] / max ($total[1][$i], 1);
+    $time[2][$i] = $time[2][$i] / max ($total[2][$i], 1); 
 
     printf ( "%d\t%5.2f\t%5.2f\t%5.2f\n", 2**$i, $time[0][$i], $time[1][$i], $time[2][$i]);
   } 
@@ -90,5 +90,16 @@ sub log2 {
     return ($log);
 }
 
+sub max {
+
+   my($max) = shift(@_);
+
+   foreach (@_) {
+
+     $max = $_ if $_  > $max;
+   }
+
+   return ($max);
+}
 
 
--- trunk/x10.apps/experiments/RandomAccess/run_internal.sh
+++ trunk/x10.apps/experiments/RandomAccess/run_internal.sh
@@ -2,16 +2,18 @@
 
 echo "===============RandomAccess -- Table Size = $1 NPROCS = $2==================" 
 
-echo "***************************X10LIB VERSION ******************************" 
-poe "x10lib/RandomAccess_spmd -o  -m $1" -procs $2 
+echo "***************************EXPT1******************************" 
+poe "x10lib/RandomAccess_spmd   -o  -m $1" -procs $2 
 
-echo "***************************X10C VERSION ********************************" 
+echo "***************************EXPT2********************************" 
 poe "x10c/RandomAccess_Dist -o  -m $1"  -procs $2  
 
-echo "***************************HPCC VERSION ********************************" 
+echo "***************************EXPT3********************************" 
 cd hpcc-opt;
 rm host.list;
+export MP_MSG_API=mpi;
+export | grep MP_MSG;
 ln -s ../host.list;
-./script.pl $2 $1   
+./script.pl $2 $1  ;
 cd ..;
 
