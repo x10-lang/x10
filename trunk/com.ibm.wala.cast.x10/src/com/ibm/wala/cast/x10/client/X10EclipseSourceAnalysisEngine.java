14c4890aee82d7a9931aa8e3971c5044ce19d0bb
Mark Stephenson <user@domain>
2007-07-10 14:41:08 +0000

    Properly matches x10 file extension for source files. No longer generates a new TranslatorExtension every time a call to getTranslatorExtension is called.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@4085 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/com.ibm.wala.cast.x10/src/com/ibm/wala/cast/x10/client/X10EclipseSourceAnalysisEngine.java
+++ trunk/com.ibm.wala.cast.x10/src/com/ibm/wala/cast/x10/client/X10EclipseSourceAnalysisEngine.java
@@ -4,21 +4,24 @@ import org.eclipse.jdt.core.IJavaProject;
 
 import com.ibm.domo.ast.x10.translator.polyglot.X10ClassLoaderFactory;
 import com.ibm.domo.ast.x10.translator.polyglot.X10IRTranslatorExtension;
-import com.ibm.wala.eclipse.util.EclipseProjectSourceAnalysisEngine;
 import com.ibm.wala.cast.java.translator.polyglot.IRTranslatorExtension;
 import com.ibm.wala.classLoader.ClassLoaderFactory;
 import com.ibm.wala.ipa.callgraph.impl.SetOfClasses;
+import com.ibm.wala.eclipse.util.EclipseProjectSourceAnalysisEngine;
 
 public class X10EclipseSourceAnalysisEngine extends EclipseProjectSourceAnalysisEngine {
 
-    public X10EclipseSourceAnalysisEngine(IJavaProject javaProject) {
-	super(javaProject);
+	private IRTranslatorExtension fExt;
+	
+	public X10EclipseSourceAnalysisEngine(IJavaProject javaProject) {
+	super(javaProject, "x10");
 	setCallGraphBuilderFactory(new X10ZeroXCFACallGraphBuilderFactory());
+		fExt = new X10IRTranslatorExtension();
     }
 
     @Override
     public IRTranslatorExtension getTranslatorExtension() {
-        return new X10IRTranslatorExtension();
+        return fExt;
     }
 
     @Override
