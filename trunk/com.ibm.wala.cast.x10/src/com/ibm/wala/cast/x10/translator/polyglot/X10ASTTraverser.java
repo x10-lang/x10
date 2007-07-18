2bf8e18767f22c0314aaabf7c7c39627637e9c5b
Robert Fuhrer <rfuhrer@us.ibm.com>
2007-07-18 21:53:06 +0000

    Added support for ParExprs (essentially no-op expression wrappers that represent parenthesized expressions). Reordered types in cascaded if so you can find the little buggers more easily.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@4223 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/com.ibm.wala.cast.x10/src/com/ibm/wala/cast/x10/translator/polyglot/X10ASTTraverser.java
+++ trunk/com.ibm.wala.cast.x10/src/com/ibm/wala/cast/x10/translator/polyglot/X10ASTTraverser.java
@@ -14,6 +14,7 @@ import polyglot.ext.x10.ast.ForLoop;
 import polyglot.ext.x10.ast.Future;
 import polyglot.ext.x10.ast.Here;
 import polyglot.ext.x10.ast.Next;
+import polyglot.ext.x10.ast.ParExpr;
 import polyglot.ext.x10.ast.When;
 import polyglot.ext.x10.ast.X10ArrayAccess;
 import polyglot.ext.x10.ast.X10ArrayAccess1;
@@ -27,34 +28,36 @@ public class X10ASTTraverser extends ASTTraverser {
     private X10ASTTraverser() { }
 
     public static CAstNode visit(Node n, X10TranslatorVisitor xtv, WalkContext wc) {
-	if (n instanceof Async)
+	if (n instanceof ArrayConstructor)
+	    return xtv.visit((ArrayConstructor) n, wc);
+	else if (n instanceof Async)
 	    return xtv.visit((Async) n, wc);
-	else if (n instanceof Here)
-	    return xtv.visit((Here) n, wc);
-	else if (n instanceof Future)
-	    return xtv.visit((Future) n, wc);
-	else if (n instanceof Finish)
-	    return xtv.visit((Finish) n, wc);
-	else if (n instanceof ForLoop)
-	    return xtv.visit((ForLoop) n, wc);
-	else if (n instanceof ForEach)
-	    return xtv.visit((ForEach) n, wc);
 	else if (n instanceof AtEach)
 	    return xtv.visit((AtEach) n, wc);
-	else if (n instanceof When)
-	    return xtv.visit((When) n, wc);
-	else if (n instanceof Next)
-	    return xtv.visit((Next) n, wc);
-	else if (n instanceof ArrayConstructor)
-	    return xtv.visit((ArrayConstructor) n, wc);
-	else if (n instanceof X10Formal)
-	    return xtv.visit((X10Formal) n, wc);
 	else if (n instanceof Closure)
 	    return xtv.visit((Closure) n, wc);
+	else if (n instanceof Finish)
+	    return xtv.visit((Finish) n, wc);
+	else if (n instanceof ForEach)
+	    return xtv.visit((ForEach) n, wc);
+	else if (n instanceof ForLoop)
+	    return xtv.visit((ForLoop) n, wc);
+	else if (n instanceof Future)
+	    return xtv.visit((Future) n, wc);
+	else if (n instanceof Here)
+	    return xtv.visit((Here) n, wc);
+	else if (n instanceof Next)
+	    return xtv.visit((Next) n, wc);
+	else if (n instanceof ParExpr)
+	    return xtv.visit((ParExpr) n, wc);
+	else if (n instanceof When)
+	    return xtv.visit((When) n, wc);
 	else if (n instanceof X10ArrayAccess)
 	    return xtv.visit((X10ArrayAccess) n, wc);
 	else if (n instanceof X10ArrayAccess1)
 	    return xtv.visit((X10ArrayAccess1) n, wc);
+	else if (n instanceof X10Formal)
+	    return xtv.visit((X10Formal) n, wc);
 	else
 	    return ASTTraverser.visit(n, xtv, wc);
     }
