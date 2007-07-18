ef0797f3d4a97958381cb92d8d1279a00b6a020f
Robert Fuhrer <rfuhrer@us.ibm.com>
2007-07-18 15:57:02 +0000

    Can now print ARRAY_REF_BY_POINT nodes, and closure entities.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@4214 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/com.ibm.wala.cast.x10/src/com/ibm/wala/cast/x10/translator/X10CAstPrinter.java
+++ trunk/com.ibm.wala.cast.x10/src/com/ibm/wala/cast/x10/translator/X10CAstPrinter.java
@@ -16,6 +16,7 @@ public class X10CAstPrinter extends CAstPrinter {
 
     public String getKindAsString(int kind) {
 	switch(kind) {
+	    case X10CastNode.ARRAY_REF_BY_POINT: return "ARRAY_REF_BY_POINT";
 	    case X10CastNode.ASYNC_INVOKE: return "ASYNC_INVOKE";
 	    case X10CastNode.ATEACH: return "ATEACH";
 	    case X10CastNode.ATOMIC_ENTER: return "ATOMIC_ENTER";
@@ -45,6 +46,7 @@ public class X10CAstPrinter extends CAstPrinter {
     public String getEntityKindAsString(int kind) {
 	switch(kind) {
 	    case X10CAstEntity.ASYNC_BODY: return "async_body";
+	    case X10CAstEntity.CLOSURE_BODY: return "closure_body";
 	    default: return super.getEntityKindAsString(kind);
 	}
     }
