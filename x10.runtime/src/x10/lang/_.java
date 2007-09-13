64cbbbe6aafb4e179f1b48abc7d63463975512b5
Vijay Saraswat <vsaraswa@us.ibm.com>
2007-09-13 21:53:28 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@4420 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ x10.runtime/src/x10/lang/_.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package x10.lang;
+
+/**
+ * The placeholder class for an unspecified type. This may be used as the type for 
+ * a final field or variable which has an initializer; the actual type of the field or variable will
+ *  be taken from the initializer.
+ * @author vj
+ *
+ */
+public class _ {
+
+	public _() {
+		super();
+	}
+
+}
