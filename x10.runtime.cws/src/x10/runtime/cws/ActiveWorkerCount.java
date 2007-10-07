b06b724ceae56471cb4459957b6a551242aa033d
Vijay Saraswat <vsaraswa@us.ibm.com>
2007-10-07 07:11:49 +0000

    Commits to support phased work-stealing, i.e. integration of a global clock into CWS, as necessary for breadth-first search.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@4500 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.runtime.cws/src/x10/runtime/cws/ActiveWorkerCount.java
+++ x10.runtime.cws/src/x10/runtime/cws/ActiveWorkerCount.java
@@ -11,34 +11,71 @@ import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
  */
 public class ActiveWorkerCount {
 	volatile int numCheckedOut;
-	final AtomicIntegerFieldUpdater<ActiveWorkerCount> updater;
+	final AtomicIntegerFieldUpdater<ActiveWorkerCount> numCheckedOutUpdater;
 	final Runnable barrierAction;
-	
-	public ActiveWorkerCount() {
-		this(null);
-	}
+	volatile int nextCount=0;
+	final AtomicIntegerFieldUpdater<ActiveWorkerCount> nextCountUpdater;
+	volatile int phaseNum;
+
+
 	public ActiveWorkerCount(final Runnable barrierAction) {
 		super();
 		this.barrierAction = barrierAction;
 		this.numCheckedOut = 0;
-		this.updater = AtomicIntegerFieldUpdater.newUpdater(ActiveWorkerCount.class, "numCheckedOut");
+		this.numCheckedOutUpdater = AtomicIntegerFieldUpdater.newUpdater(ActiveWorkerCount.class, "numCheckedOut");
+		this.nextCountUpdater = 
+			AtomicIntegerFieldUpdater.newUpdater(ActiveWorkerCount.class, "nextCount");
 	}
-	
-	public void checkIn() {
-		final int count = updater.addAndGet(this, -1);
-		if (count ==0) {
-			if (Worker.reporting)
-				System.out.println(Thread.currentThread() + " moves barrier up." );
-			if (barrierAction != null)
-				barrierAction.run();
+	public String toString() {
+		return "AWC(phase=" + phaseNum+ ",nextCount="+nextCount + ",numCheckedOut="+numCheckedOut+")";
+	}
+	/**
+	 * A worker checks into the ActiveWorkerCount when it has an empty deque and is hunting for work.
+	 * When all workers have checked in, the barrier is reached. Now if on checking in any worker
+	 * said that there was still work to do, the phase is incremented. Otherwise computation has
+	 * terminated and the barrier action is performed. This will cause pool.activeCount to go to zero
+	 * and as the workers are spinning they will realize this and then go into a wait for the next
+	 * job coming through.
+	 * @param haveWork
+	 */
+	public boolean checkIn(Worker w, int inPhase, boolean haveWork) {
+		if (inPhase != phaseNum) return false;
+		if (haveWork) nextCountUpdater.getAndIncrement(this);
+		final int count = numCheckedOutUpdater.addAndGet(this,-1);
+		if ( Worker.reporting)
+			System.out.println(Thread.currentThread() + " checks in to " + this);
+		if (count == 0) {
+			if ( Worker.reporting)
+				System.out.println(Thread.currentThread() + " moves barrier up to " + this);
+
+			if (nextCount > 0) {
+				numCheckedOut=nextCount;
+				nextCount=0;
+				phaseNum++;
+				w.advancePhase(phaseNum);
+				return true;
+			} else {
+				phaseNum=0;
+				if ( Worker.reporting)
+					System.out.println(Thread.currentThread() + " completes job." );
+				if (barrierAction != null) 	
+					barrierAction.run();
+				
+			}
 		}
+		return false;
 	}
-	
+	/**
+	 * A worker checks out only if it has checked in first, and has found work since it last 
+	 * checked in. 
+	 *
+	 */
 	public void checkOut() {
-		updater.addAndGet(this, 1);
+		numCheckedOutUpdater.addAndGet(this, 1);
+		if ( Worker.reporting)
+			System.out.println(Thread.currentThread() + " checks out of barrier " + this);
+
 	}
-	
-	public int getNumberCheckedOut() {
-		return numCheckedOut;
-	}
+
+
 }
