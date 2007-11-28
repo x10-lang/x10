5b06183959e5f270dac3915a49489fe25a644c79
Ganesh Bikshandi <ganeshvb@domain>
2007-11-28 15:43:38 +0000

    a test case for contiguous transposition added
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@4695 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ x10.lib/test/Test_transpose_contig.cc
@@ -0,0 +1,134 @@
+/*
+ * (c) Copyright IBM Corporation 2007
+ *
+ * $Id: Test_transpose_contig.cc,v 1.1 2007-11-28 15:43:38 ganeshvb Exp $ 
+ * This file is part of X10 Runtime System.
+ */
+
+#include <iostream>
+#include <x10/xassert.h>
+#include <x10/x10lib.h>
+#include "lapi.h"
+
+using namespace std;
+using namespace x10lib;
+
+double* data;
+double* data2;
+const int* lda;
+
+struct __closure__0__args
+{
+  __closure__0__args (int offset)
+    : _offset (offset)
+  {}
+
+  int _offset;
+};
+
+struct __closure__0 : Closure 
+{
+  __closure__0() :
+    Closure (sizeof(__closure__0), 0),
+  _args (0) {}
+
+  __closure__0 (int offset) : 
+    Closure (sizeof(__closure__0), 0),
+    _args (offset)
+  {    
+  }
+
+  __closure__0__args _args;
+};
+
+
+void* arrayCopySwitch (int handler, void* buf)
+{
+  __closure__0__args* closure_args = (__closure__0__args*) buf;
+  
+  return data + closure_args->_offset;
+}
+
+int 
+main (int argc, char* argv[])
+{
+  x10lib::Init(NULL,0);
+
+  int SQRT_N = atoi (argv[1]);
+
+  int X = SQRT_N /  __x10_num_places;
+
+  int Y = SQRT_N;
+
+  int nRows = SQRT_N / __x10_num_places;
+
+  int N = X * Y; 
+  
+  data = new double[N];
+  data2 = new double[N];
+
+  int P = x10lib::__x10_my_place;
+
+  for (int i = 0; i < X; i++)
+    for (int j = 0; j < Y; j++)
+      {
+	data [i * Y + j] = (i + P * nRows) * Y + j;
+       	data2 [i * Y + j] =  0;
+      }
+  
+
+  int rank = 2;
+  Point<2> origin (0, 0);
+  Point<2> diagonal (X-1, Y-1);
+  RectangularRegion<2> r (origin, diagonal);
+
+  lda = r.lda();
+
+  __closure__0* closure = new __closure__0 [__x10_num_places];
+
+  x10lib::SyncGlobal();
+  
+  int n = 0;
+
+  for (int i = 0; i < X; i++)
+   for (int j = 0; j < Y; j++)
+      data2 [j * X + i] = data [ i * Y + j];
+
+  x10lib::SyncGlobal();
+
+  int chunk_size = nRows * nRows;
+  for (int k=0; k <__x10_num_places; ++k) { //for each block
+
+      int srcI= k * chunk_size; 
+      int destI= P * chunk_size;
+     
+      (closure + n)->_args._offset = destI;
+      asyncArrayCopyRaw (data2 + srcI, closure + n, chunk_size*sizeof(double), k);
+      n++;
+  }
+  
+  x10lib::SyncGlobal();
+
+  int n2 = X;
+  int n1 = __x10_num_places;
+  for (int k = 0; k < n2; k++)
+    for (int i = 0; i < n1; i++)
+      for (int j = 0; j < n2; j++)
+      data2 [k * n2 * n1 + j + i * n2] = data [ i * n2 * n2 + k * n2 + j];
+  
+  x10lib::SyncGlobal();
+
+  for (int i = 0; i < X; i++)
+    for (int j = 0; j < Y; j++)
+      {
+	assert (data2 [i * Y + j] == j * Y + i + P * nRows);       	
+	//    cout << data2 [i * Y + j] << endl;
+      }
+   
+  cout << "Test_transpose PASSED" << endl;
+
+  delete [] closure;
+  x10lib::Finalize();
+  
+  return 0;
+}
