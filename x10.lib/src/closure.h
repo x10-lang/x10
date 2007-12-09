0db2bc3919b685d9b4ccf73fad6ab93d4fd520d7
Sreedhar Kodali <srkodali@in.ibm.com>
2007-12-09 11:34:01 +0000

    Clousre type for asyncs.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@4786 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.lib/src/closure.h
+++ x10.lib/src/closure.h
@@ -1,38 +1,42 @@
 /*
  * (c) Copyright IBM Corporation 2007
  *
- * $Id: closure.h,v 1.2 2007-10-11 10:55:56 ganeshvb Exp $
+ * $Id: closure.h,v 1.3 2007-12-09 11:34:01 srkodali Exp $
  * This file is part of X10 Runtime System.
  */
 
 #ifndef __X10_CLOSURE_H
 #define __X10_CLOSURE_H
 
-#include "types.h"
+#include <x10/types.h>
 
+/* C++ Lang Interface */
 #ifdef __cplusplus
 namespace x10lib {
 
-  /*
-   * a type representing the closure for the asyncs
-   * handler = unique handle for the array.
-   * Every closure type argument should have this as its base class.
-   */
-  struct Closure
-  {
-    Closure () :
-      len (0), handler (0) {}
-    Closure (int _len, int _handler) :
-      len (_len), handler (_handler) {}
-    
-    size_t len;
-    x10_async_handler_t handler;
-  };
-}
+	/* A type representing the closure for asyncs.
+	 * handler --> unique handle for the array
+	 * Every closure type argument should have this as its base class.
+	 */
+	struct Closure
+	{
+		Closure () : len(0), handler(0) {}
+		Closure (int _len, int _handler) :
+			len(_len), handler(_handler) {}
+
+		size_t len;
+		x10_async_handler_t handler;
+	};
+
+} /* closing brace for namespace x10lib */
+
 typedef x10lib::Closure* x10_closure_t;
-#else
+
+#else /* C Language Interface */
+
 struct Closure;
 typedef struct Closure* x10_closure_t;
-#endif
 
-#endif
+#endif /* __cplusplus */
+
+#endif /* __X10_CLOSURE_H */
