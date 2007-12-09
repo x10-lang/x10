621231c0ab7d0b0266f2fb650bd71992362cf7fd
Sreedhar Kodali <srkodali@in.ibm.com>
2007-12-09 12:20:09 +0000

    Dummy Clock interface and primitive next operation.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@4788 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.lib/src/clock.cc
+++ trunk/x10.lib/src/clock.cc
@@ -1,12 +1,24 @@
-#include "clock.h"
+/*
+ * (c) Copyright IBM Corporation 2007
+ *
+ * $Id: clock.cc,v 1.2 2007-12-09 12:20:09 srkodali Exp $
+ * This file is part of X10 Runtime System.
+ */
+
+/** Implementation file for (DUMMY) Clock interface. **/
+
+#include <x10/clock.h>
 #include <x10/xmacros.h>
+#include <lapi.h>
 
-x10_err_t
-x10lib::clockNext (Clock* c, int n) 
-{  
+namespace x10lib {
 
-  LRC (LAPI_Gfence (__x10_hndl));
-
-  return X10_OK;
+/* Perform next operation on the specified clock. */
+x10_err_t x10lib::ClockNext(Clock *c, int n)
+{
+	// refine???
+	LRC(LAPI_Gfence(__x10_hndl));
+	return X10_OK;
 }
 
+} /* closing brace for namespace x10lib */
--- trunk/x10.lib/src/clock.h
+++ trunk/x10.lib/src/clock.h
@@ -1,7 +1,7 @@
 /*
  * (c) Copyright IBM Corporation 2007
  *
- * $Id: clock.h,v 1.1 2007-08-07 06:21:52 ganeshvb Exp $
+ * $Id: clock.h,v 1.2 2007-12-09 12:20:09 srkodali Exp $
  * This file is part of X10 Runtime System.
  */
 
@@ -12,24 +12,16 @@
 
 #include <x10/types.h>
 #include <x10/err.h>
-#include <lapi.h>
 
 /* C++ Lang Interface */
 #ifdef __cplusplus
-
-//dummy clock implementation
-
 namespace x10lib {
- 
-  extern lapi_handle_t __x10_hndl;  
-  class Clock 
-    {
-      
-    }; 
-  
-  x10_err_t clockNext (Clock* c, int n);
-  
-}  /* closing brace for namespace x10lib */
-#endif
+	class Clock {
+		/* ... */
+	};
 
-#endif /* __X10_SWITCH_H */
+	x10_err_t ClockNext(Clock *c, int n);
+} /* closing brace for namespace x10lib */
+#endif 
+
+#endif /* __X10_CLOCK_H */
