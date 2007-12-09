e2fa0ffefd2164c094188a9c472cca6dcbdbe348
Sreedhar Kodali <srkodali@in.ibm.com>
2007-12-09 11:16:45 +0000

    Dummy AsyncSwitch to avoid link time error.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@4785 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.lib/src/async_switch.cc
+++ x10.lib/src/async_switch.cc
@@ -1,22 +1,16 @@
 /*
  * (c) Copyright IBM Corporation 2007
  *
+ * $Id: async_switch.cc,v 1.2 2007-12-09 11:16:45 srkodali Exp $
  * This file is part of X10 Runtime System.
  */
 
-/* $Id: async_switch.cc,v 1.1 2007-11-13 05:32:39 ganeshvb Exp $ */
-
-#include "types.h"
+#include <x10/types.h>
 #include <iostream>
 
-using namespace std;
-
 extern "C"
-void 
-asyncSwitch (x10_async_handler_t h, void* args,int niter)
+void AsyncSwitch(x10_async_handler_t h, void *args, int niter)
 {
-   cout << "asyncSwitch should be overriddern \n";
-   exit(-1);
+	cout << "!!AsyncSwitch should be overridden!!\n";
+	exit(-1);
 }
-
-
