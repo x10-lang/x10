c0fe180c9d01728ca90737fc4708f119da654d5a
Sreedhar Kodali <srkodali@in.ibm.com>
2007-12-09 12:56:37 +0000

    Interface for spawning inlinable asyncs.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@4789 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.lib/src/async.h
+++ trunk/x10.lib/src/async.h
@@ -1,9 +1,11 @@
 /*
  * (c) Copyright IBM Corporation 2007
  *
- * $Id: async.h,v 1.18 2007-10-19 16:04:28 ganeshvb Exp $
+ * $Id: async.h,v 1.19 2007-12-09 12:56:37 srkodali Exp $
  * This file is part of X10 Runtime System.
  */
+
+/** X10Lib's Async Interface. **/
 
 #ifndef __X10_ASYNC_H
 #define __X10_ASYNC_H
@@ -11,23 +13,18 @@
 #include <x10/types.h>
 #include <x10/err.h>
 
-/* C++ Lang Interface */
 #define X10_MAX_ASYNC_ARGS 6
 #define X10_MAX_ASYNC_ARGS_SIZE \
-		X10_MAX_ASYNC_ARGS * sizeof(x10_async_arg_t)
+		(X10_MAX_ASYNC_ARGS * sizeof(x10_async_arg_t))
 
-
-
+/* C++ Lang Interface */
 #ifdef __cplusplus
-extern "C" void asyncSwitch(x10_async_handler_t, void *,
-				int niter);
-namespace x10lib {
-extern int __x10_num_places;
-extern int __x10_my_place;
-
-x10_err_t asyncSpawnInline(x10_place_t tgt,
-			x10_async_handler_t hndlr, void* args, size_t size);
-
+namepsace x10lib {
+	
+	/* Spawn an inlinable async on a given place. */
+	x10_err_t AsyncSpawnInline(x10_place_t tgt,
+					x10_async_handler_t hndlr,
+					void *args, size_t size);
 
 } /* closing brace for namespace x10lib */
 #endif
@@ -37,14 +34,13 @@ x10_err_t asyncSpawnInline(x10_place_t tgt,
 extern "C" {
 #endif
 
+/* Spawn an inlinable async on a given place. */
 x10_err_t x10_async_spawn_inline(x10_place_t tgt,
-			x10_async_handler_t hndlr, void* args, size_t size);
-
-
-x10_err_t x10_async_register(void);
+				x10_async_handler_t hndlr,
+				void *args, size_t size);
 
 #ifdef __cplusplus
-}
+} /* closing brace for extern "C" */
 #endif
 
 #endif /* __X10_ASYNC_H */
