8d1802acc106fbc3254b792cd22616b133a1d21f
Sreedhar Kodali <srkodali@in.ibm.com>
2007-12-10 10:22:44 +0000

    x10lib's reduction interface.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@4801 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.lib/src/reduce.cc
+++ trunk/x10.lib/src/reduce.cc
@@ -1,54 +1,57 @@
 /*
  * (c) Copyright IBM Corporation 2007
- * $Id: reduce.cc,v 1.5 2007-10-26 07:33:04 ganeshvb Exp $ 
+ * $Id: reduce.cc,v 1.6 2007-12-10 10:22:44 srkodali Exp $ 
  * This file is part of X10 Runtime System.
  */
 
-#include <x10/x10lib.h>
+#include <x10/types.h>
+#include <x10/reduce.h>
 #include <iostream>
+#include <lapi.h>
 #include <math.h>
-
-#define LOG2(x)  (int) round (log ((double) x) / log (2.0))
-
-#define X10_MAX_REDUCE_OPS_INFLIGHT 4
-
-#define X10_MAX_REDUCE_OBJECT_SIZE 4 * sizeof(double)
+#include "x10libP.h"
 
 namespace x10lib {
 
-lapi_cntr_t   reduce_cntr;
-void**        reduce_cntr_list;
-void*         scratch;
-void**        reduce_list;
-void*         inbuf[X10_MAX_REDUCE_OPS_INFLIGHT];
-int           reduceCount = 0;
+/* reduction variables */
+lapi_cntr_t reduce_cntr;
+void **reduce_cntr_list;
+void *scratch;
+void **reduce_list;
+void *inbuf[X10_MAX_REDUCE_OPS_INFLIGHT];
+int reduceCount = 0;
 
-}
+} /* closing brace for namespace x10lib */
+
+/* The following two methods should be available
+ * during x10lib's initialization; mark them as external.
+ */
 
 using namespace x10lib;
 
-void reduceInit()
+/* Initialize Reduction */
+void ReduceInit()
 {
-  X10_DEBUG (1, "Entry");
+	X10_DEBUG(1, "Entry");
 
-  x10lib::reduce_cntr_list = (void**) malloc (sizeof(void*)*x10lib::__x10_num_places); 
-  
-  LAPI_Address_init (x10lib::__x10_hndl, (void*)&x10lib::reduce_cntr, x10lib::reduce_cntr_list);
-  
-  x10lib::reduce_list = (void**) malloc (sizeof(void*) * x10lib::__x10_num_places);  
-  
-  x10lib::scratch = x10lib::__x10_num_places == 1 ? NULL : new char[X10_MAX_REDUCE_OBJECT_SIZE * X10_MAX_REDUCE_OPS_INFLIGHT * LOG2(x10lib::__x10_num_places)];
-  
-  LAPI_Address_init (x10lib::__x10_hndl, (void*)x10lib::scratch, x10lib::reduce_list);  
+	x10lib::reduce_cntr_list = (void **)malloc(sizeof(void *) * __x10_num_places);
+	LAPI_Address_init(__x10_hndl, (void *)&x10lib::reduce_cntr,
+				x10lib::reduce_cntr_list);
+	x10lib::reduce_list = (void **)malloc(sizeof(void *) * __x10_num_places);
+	x10lib::scratch = (__x10_num_places == 1) ? NULL :
+		new char[X10_MAX_REDUCE_OBJECT_SIZE *
+				X10_MAX_REDUCE_OPS_INFLIGHT *
+				LOG2(__x10_num_places)];
+	LAPI_Address_init(__x10_hndl, (void *)x10lib::scratch, x10lib::reduce_list);
 
-  X10_DEBUG (1, "Exit");
+	X10_DEBUG(1, "Exit");
 }
 
-void reduceFinalize()
+/* Finialize Reduction */
+void ReduceFinalize()
 {
-  if (x10lib::scratch) 
-    delete [] (char*) x10lib::scratch;
-  delete [] x10lib::reduce_cntr_list;
-  delete [] x10lib::reduce_list;
+	if (x10lib::scratch)
+		delete [] (char *)x10lib::scratch;
+	delete [] x10lib::reduce_cntr_list;
+	delete [] x10lib::reduce_list;
 }
-
