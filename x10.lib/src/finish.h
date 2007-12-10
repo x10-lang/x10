1c411ba804cd3a1231f112cc959591f219bd11f3
Sreedhar Kodali <srkodali@in.ibm.com>
2007-12-10 07:28:47 +0000

    x10lib's finish start and finish end interfaces.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@4797 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.lib/src/finish.h
+++ x10.lib/src/finish.h
@@ -1,72 +1,67 @@
 /*
  * (c) Copyright IBM Corporation 2007
  *
+ * $Id: finish.h,v 1.11 2007-12-10 07:28:47 srkodali Exp $
  * This file is part of X10 Runtime System.
  */
 
-/* $Id: finish.h,v 1.10 2007-10-19 16:04:29 ganeshvb Exp $ */
+#ifndef __X10_FINISH_H
+#define __X10_FINISH_H
 
-#ifndef __FINISH_H__
-#define __FINISH_H__
-
-#include <iostream>
-#include <x10/err.h>
 #include <x10/types.h>
-#include <x10/gas.h>
 
+/* exception buffer size */
+#define X10_EX_BUFFER_SIZE 1024
+
+/* C++ Lang Interface */
+#ifdef __cplusplus
 namespace x10lib {
 
-extern int __x10_inited;
+class Exception {
+	public:
+		Exception() {}
 
-
-extern int __x10_num_places;
-extern int __x10_my_place;
-extern int __x10_addr_hndl;
-extern int __x10_addrtbl_sz;
-
-class Exception{
-    public:
-      Exception () {}
-      virtual size_t size() = 0;
-      virtual void print() = 0;
-      virtual x10_place_t proc() = 0;
-  };
-
-  class MultiException {
-    public:
-      MultiException (Exception** e, int n)
-       : exceptions_(e), total_(n) {}
-
-       void print () {
-         for (int i = 0; i < total_; i++)
-           exceptions_[i]->print();
-       }
-
-       int size() const { return total_; }
-
-       Exception* operator [] (int i)
-	 {
-	   return exceptions_[i];
-	 }
-       Exception **const exceptions() const { return exceptions_; }
-
-      ~MultiException () {
-        //x10lib takes care of garbage collection/managing
-        // each of the exceptions_ objects. So just
-        //delete the exceptions_ array only.
-
-	//cout << "hello" << endl;
-        //delete [] exceptions_; 
-       } 
-    private:
-      int  total_;
-      Exception** exceptions_; 
-  };
-
-  void finishEnd (Exception* a);
- 
-  int finishStart (int cs);
+		virtual size_t size() = 0;
+		virtual void print() = 0;
+		virtual x10_place_t proc() = 0;
 };
 
+class MultiException {
+	private:
+		int total_;
+		Exception **exceptions_;
 
-#endif
+	public:
+		MultiException(Exception **e, int n) :
+			exceptions_(e), total_(n) {}
+
+		~MultiException() {
+			/* x10lib takes care of garbage collection/managing
+			 * each of the exceptions_ objections;
+			 * so, just delete the exceptions_ array only.
+			 */
+			// delete [] exceptions_;
+		}
+
+		void print() {
+			for (int i = 0; i < total_; i++)
+				exceptions_[i]->print();
+		}
+
+		int size() const {return total_;}
+
+		Exception* operator [] (int i) {
+			return exceptions_[i];
+		}
+
+		Exception** const exceptions() const {
+			return exceptions_;
+		}
+};
+
+void FinishEnd(Exception *a);
+int FinishStart(int cs);
+
+} /* closing brace for namespace x10lib */
+
+#endif /* __X10_FINISH_H */
