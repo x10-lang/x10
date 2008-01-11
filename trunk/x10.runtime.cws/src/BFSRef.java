81101998f1883e9fa0aac251f108b84657de3c18
Vijay Saraswat <vsaraswa@us.ibm.com>
2008-01-11 00:22:57 +0000

    Now supports TorusGraph and kGraph (thanks to Tong and Guojing). Tests pass on laptop.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@4872 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.runtime.cws/src/BFSRef.java
+++ trunk/x10.runtime.cws/src/BFSRef.java
@@ -10,7 +10,6 @@
 
 import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
 
-
 import x10.runtime.cws.Frame;
 import x10.runtime.cws.Pool;
 import x10.runtime.cws.StealAbort;
@@ -84,12 +83,146 @@ public class BFSRef {
 	
 	int randSeed = 17673573; 
 	int rand32() { return randSeed = 1664525 * randSeed + 1013904223;}
-	public BFSRef (int n, int m){
+	public BFSRef (int n, int m, char graphType){
 		N=n;
 		M=m;
-		/*constructing edges*/
-		G = new V[N]; for(int i=0;i<N;i++) G[i]=new V(i);
-		El = new E [M];
+		if (graphType=='E') {
+			G = new V[N]; for(int i=0;i<N;i++) G[i]=new V(i);
+			randomEdgeGraph(G, new E[M]);
+		} else if (graphType=='T') {
+			N=n*n;
+			G = new V[N]; for(int i=0;i<N;i++) G[i]=new V(i);
+			torusGraph(n,G);
+		} else if (graphType=='K') {
+			G = new V[N]; for(int i=0;i<N;i++) G[i]=new V(i);
+			kGraph(G);
+		} else if (graphType=='R') {
+			G = new V[N]; for(int i=0;i<N;i++) G[i]=new V(i);
+			//rGraph(G,El);
+		}
+	}
+	public void torusGraph (int k, final V [] graph){
+		System.out.println("Generating graph...");
+		int n = k*k,i,j,l,s;
+		int [] buff = new int [n];
+  		V [][] adj = new V [n][4];//Java support arrays of arrays
+  		for(i=0;i<n;i++) buff[i]=i;
+
+  		for(i=0;i<n/2;i++){
+			l=(int)(Math.random()*n)%n;
+ 			s=(int)(Math.random()*n)%n;
+			j=buff[l];
+			buff[l]=buff[s];
+			buff[s]=j;
+  		}
+	
+  		for(i=0;i<k;i++)
+      		for(j=0;j<k;j++)
+	 		adj[buff[i*k+j]] = new V[]{graph[buff[((k+i-1)%k)*k+j]], 
+      				graph[buff[((i+1)%k)*k+j]],
+	  		                    graph[buff[i*k+((k+j-1)%k)]], 
+	  		                    graph[buff[i*k+((j+1)%k)]]};
+		
+   		for(i=0;i<n;i++){
+			// vj -- why is this? graph[i].parent=i;
+        		graph[i].neighbors=adj[i];
+   		}
+   	
+   		System.out.println("Graph generated.");
+	}
+
+	public void kGraph(final V [] graph){
+		int n=N;
+		int k=4;
+		final int TIMES=5;
+		final int THRESHOLD=100;
+		int neighbor;
+		char [] visited = new char [n];
+		int [] stack = new  int [n];
+		int [] SUPER = new int [n];
+		int i,j,u,v,nextn,top=-1,n_comp=0,rep,s;
+		int [] counter = new int [n];
+		final int [][] array = new int [n][k*TIMES];
+
+		for(i=0;i<n;i++){
+			counter[i]=0;
+			visited[i]=0;
+		}
+
+		for(i=0;i<n;i++){
+			for(j=counter[i];j<k;j++){
+				if(i<n-THRESHOLD)
+					neighbor=(int)(Math.random()*(n-i))%(n-i)+i;
+				else 
+					neighbor=(int)(Math.random()*THRESHOLD)%(THRESHOLD);
+				rep=0;
+				for(s=0;s<counter[i];s++) 
+					if(array[i][s]==neighbor) rep=1;
+				while(rep==1){
+					rep=0;
+					if(i<n-THRESHOLD)
+						neighbor=(int)(Math.random()*(n-i))%(n-i)+i;
+					else 
+						neighbor=(int)(Math.random()*THRESHOLD)%(THRESHOLD);
+					for(s=0;s<counter[i];s++) 
+						if(array[i][s]==neighbor) rep=1;
+				}
+
+				while(counter[neighbor]>TIMES*k-1 || neighbor==i) neighbor=(neighbor+1)%n;
+				array[i][counter[i]]=neighbor;
+				counter[i]++;
+				array[neighbor][counter[neighbor]]=i;
+				counter[neighbor]++;
+			}
+		}
+	
+
+  		/* now make the graph connected if it is not*/
+  		for(i=0;i<n;i++){
+      			if(visited[i]==0){
+				visited[i]=1;
+				stack[++top]=i;
+				SUPER[n_comp++]=i;
+
+				while(top!=-1){
+	    				v = stack[top];
+	    				top--;
+
+	    				for (j=0; j<counter[v]; j++) {
+	      					nextn = array[v][j];
+	      					if(visited[nextn]==0) {  /* not seen yet */
+							visited[nextn]=1;
+							stack[++top]=nextn;
+	      					}
+	    				}
+	  			}
+      			}
+    		}
+ 
+  		for(i=1;i<n_comp;i++){
+      			u = SUPER[i];
+      			v = SUPER[i-1];
+      			array[u][counter[u]++]=v;
+      			array[v][counter[v]++]=u;
+    		}
+
+   		for(i=0;i<n;i++){
+			
+			/*graph[i].self=i;*/
+			//graph[i].parent=i;
+ 			//graph[i].degree=counter[i];
+        		graph[i].neighbors=new V [counter[i]];
+	
+        		for(j=0;j<counter[i];j++){
+            			graph[i].neighbors[j]=graph[array[i][j]];
+        		}
+   		}
+   		
+  		System.out.println("generating graph done\n");
+	}
+
+	
+	void randomEdgeGraph(V[] G, E[] El) {
 		for (int i=0; i <M; i++) El[i] = new E(Math.abs(rand32())%N, Math.abs(rand32())%N);
 		
 		int[] D = new int [N];
@@ -204,33 +337,47 @@ public class BFSRef {
 	static final long NPS = (1000L * 1000 * 1000);
 	static boolean graphOnly =false;
 	public static void main(String[] args) {
+		int procs;
 		int num=-1;
-		int procs =1;
 		int D=4;
+		char graphType='E';
 		try {
 			procs = Integer.parseInt(args[0]);
-			System.out.println("P=" + procs);
+			System.out.println("Number of procs=" + procs);
 			if (args.length > 1) {
-				num = Integer.parseInt(args[1]);
-				System.out.println("N=" + num);
+				String s = args[1];
+				if (s.equalsIgnoreCase("T")) {
+					graphType='T'; 
+				}
+				else if (s.equalsIgnoreCase("R"))
+					graphType='R'; 
+				else if (s.equalsIgnoreCase("K"))
+					graphType='K'; 
+				System.out.println("graphType=" + graphType);
 			}
 			if (args.length > 2) {
-				D = Integer.parseInt(args[2]);
-				System.out.println("D=" + D);
+				num = Integer.parseInt(args[2]);
+				System.out.println("N=" + num);
 			}
+			
 			if (args.length > 3) {
-				boolean b = Boolean.parseBoolean(args[3]);
-				reporting=b;
+				D = Integer.parseInt(args[3]);
+				System.out.println("D=" + D);
 			}
 			if (args.length > 4) {
 				boolean b = Boolean.parseBoolean(args[4]);
+				reporting=b;
+			}
+			if (args.length > 5) {
+				boolean b = Boolean.parseBoolean(args[5]);
 				graphOnly=b;
 			}
 		}
 		catch (Exception e) {
-			System.out.println("Usage: java BFS <P>  [<N> [<Degree> [[false|true] [false|true]]]]");
+			System.out.println("Usage: java BFS <threads> [<Type> [<N> [<Degree> [[false|true] [false|true]]]]]");
 			return;
 		}
+		
 		Pool g = new Pool(procs);
 		if (num >= 0) {
 			Ns = new int[] {num};
@@ -241,7 +388,7 @@ public class BFSRef {
 			// ensure the sole reference to the previous graph is nulled before gc.
 			graph = null;
 			System.gc();
-			graph = new BFSRef(N,M);
+			graph = new BFSRef(N,M, graphType);
 			if (graphOnly) return;
 		
 			//System.out.printf("N:%8d ", N);
