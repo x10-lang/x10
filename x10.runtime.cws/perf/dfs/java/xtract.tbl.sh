cdfc43e192e90bdfec768a252e06303e26c3ca4d
Sreedhar Kodali <srkodali@in.ibm.com>
2008-02-05 08:12:09 +0000

    Modifications due to separation of batching information.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@4934 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.runtime.cws/perf/dfs/java/xtract.tbl.sh
+++ x10.runtime.cws/perf/dfs/java/xtract.tbl.sh
@@ -3,7 +3,7 @@
 #
 # (c) IBM Corporation 2008
 #
-# $Id: xtract.tbl.sh,v 1.1 2008-02-04 14:47:30 srkodali Exp $
+# $Id: xtract.tbl.sh,v 1.2 2008-02-05 08:12:09 srkodali Exp $
 #
 # Torus table extraction script for Java.
 #
@@ -17,14 +17,12 @@ fi
 in_file=$1
 out_file=`echo $in_file | sed -e 's;.dat;.tbl;'`
 sed -n '1,7p' $in_file > $out_file
-#sed '/^\#<<<< BEGIN/,/^\#<<<< END/d' $in_file
-egrep '(^# Run|^## Size|^Number.*|^N=|^BATCH_SIZE|^M=.* t=.*|^Completed|^#<<<< BEGIN|^#<<<< END)' $in_file | \
+sed '/^\#<<<< BEGIN/,/^\#<<<< END/d' $in_file | \
+egrep '(^# Run|^## Size|^Number.*|^N=|^BATCH_SIZE|^M=.* t=.*|^Completed)' | \
 	awk '$0 ~ /^# Run.*/ { seq = $3; } \
 			$0 ~ /^## Size.*/ { size = $3; \
-							printf "\n# %10s %10s %5s %5s %10s %15s %10s %10s [Run: %d]\n\n", "VERTICS", "EDGES", "NPROC", "BATCH", "TIME(Sec)", "EDGES/Sec", "STEALS", "ATTEMPTS", seq; \
+							printf "\n# %10s %10s %5s %5s %10s %10s %8s %8s [Run: %d]\n\n", "VERTICS", "EDGES", "NPROC", "BATCH", "TIME(Sec)", "MEDGES/Sec", "STEALS", "ATTEMPTS", seq; \
 			} \
-			$0 ~ /^#<<<< BEGIN.*/ { printf "\n%s\n\n", $0 ; } \
-			$0 ~ /^#<<<< END.*/ { printf "\n%s\n\n", $0 ; } \
 			$0 ~ /^Number.*/ { nproc = substr($3,7); rtime = -1; } \
 			$0 ~ /^BATCH_SIZE.*/ { batch = substr($1,12); } \
 			$0 ~ /^N=.*/ { inval = substr($1,3); vertics = inval * inval; } \
@@ -41,9 +39,42 @@ egrep '(^# Run|^## Size|^Number.*|^N=|^BATCH_SIZE|^M=.* t=.*|^Completed|^#<<<< B
 						attempts = substr($7,4); \
 					} \
 				} \
-				edges_sec = edges / rtime; } \
+				medges_sec = edges / (rtime * 1000000); } \
 			$0 ~ /^Completed.*/ { \
-							printf "  %10d %10d %5d %5d %10.6f %15.2f %10d %10d\n", vertics, edges, nproc, batch, rtime, edges_sec, steals, attempts; }' \
+							printf "  %10d %10d %5d %5d %10.6f %10.6f %8d %8d\n", vertics, edges, nproc, batch, rtime, medges_sec, steals, attempts; }' \
 		>> $out_file
 printf "\n\n" >> $out_file
 tail -3 $in_file >> $out_file
+
+# extract batching information
+bout_file=`echo $in_file | sed -e 's;.dat;.btbl;'`
+sed -n '1,7p' $in_file > $bout_file
+sed -n -e '/^\#<<<< BEGIN/,/^\#<<<< END/p' \
+	-e '/^\# Run/p' -e '/^\## Size/p' $in_file | \
+egrep '(^# Run|^## Size|^Number.*|^N=|^BATCH_SIZE|^M=.* t=.*|^Completed)' | \
+	awk '$0 ~ /^# Run.*/ { seq = $3; } \
+			$0 ~ /^## Size.*/ { size = $3; \
+							printf "\n# %10s %10s %5s %5s %10s %10s %8s %8s [Run: %d]\n\n", "VERTICS", "EDGES", "NPROC", "BATCH", "TIME(Sec)", "MEDGES/Sec", "STEALS", "ATTEMPTS", seq; \
+			} \
+			$0 ~ /^Number.*/ { nproc = substr($3,7); rtime = -1; } \
+			$0 ~ /^BATCH_SIZE.*/ { batch = substr($1,12); } \
+			$0 ~ /^N=.*/ { inval = substr($1,3); vertics = inval * inval; } \
+			$0 ~ /^M=.* t=.*/ { edges = substr($1,3); \
+				if (rtime == -1) { \
+					rtime = substr($2,3); \
+					steals = substr($6,4); \
+					attempts = substr($7,4); \
+				} else { \
+					rtime2 = substr($2,3); \
+					if (rtime > rtime2) { \
+						rtime = rtime2; \
+						steals = substr($6,4); \
+						attempts = substr($7,4); \
+					} \
+				} \
+				medges_sec = edges / (rtime * 1000000); } \
+			$0 ~ /^Completed.*/ { \
+							printf "  %10d %10d %5d %5d %10.6f %10.6f %8d %8d\n", vertics, edges, nproc, batch, rtime, medges_sec, steals, attempts; }' \
+		>> $bout_file
+printf "\n\n" >> $bout_file
+tail -3 $in_file >> $bout_file
