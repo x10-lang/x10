e3f71a575ef49c311dc38604bf3d0461cb7cc15e
Sreedhar Kodali <srkodali@in.ibm.com>
2008-02-25 08:17:57 +0000

    Table extraction scripts and data/table files for v20.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@5044 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.runtime.cws/perf/dfs/cilk/random/v20/run.sh
+++ x10.runtime.cws/perf/dfs/cilk/random/v20/run.sh
@@ -3,7 +3,7 @@
 #
 # (c) IBM Corportation 2007
 #
-# $Id: run.sh,v 1.1 2008-02-24 11:30:23 srkodali Exp $
+# $Id: run.sh,v 1.2 2008-02-25 08:17:57 srkodali Exp $
 # Interactive script for running DFS Cilk.
 #
 
@@ -12,21 +12,22 @@ prog_name=dfs.cilk.random
 . ${TOP}/config/run.header
 
 _CMD_=${TOP}/ST.pwr5
-STACK_DEPTH=80000000
-STACK_SIZE=250000000
+PTHREAD_STACK_MAX=256000000
 
 seq=1
 while [[ $seq -le $MAX_RUNS ]]
 do
 	printf "#\n# Run: %d\n#\n" $seq 2>&1| tee -a $OUT_FILE
-	for size in 250000 1000000 4000000 9000000
+	for size in 250000 1000000
 	do
 		let 'n_edges = size * 4'
+		let 'stack_depth = n_edges * 4'
 		printf "\n## Size: %d\n" $size 2>&1| tee -a $OUT_FILE
 		for nproc in 1 2 4 6 8 10 12 14 16
 		do
+			let 'stack_size = PTHREAD_STACK_MAX / nproc'
 			printf "\n### nproc: %d\n" $nproc 2>&1| tee -a $OUT_FILE
-			CMD="${_CMD_} --nproc $nproc --stack $STACK_DEPTH --pthread-stacksize $STACK_SIZE 0 $size $n_edges"
+			CMD="${_CMD_} --nproc $nproc --stack $stack_depth --pthread-stacksize $stack_size 0 $size $n_edges"
 			printf "${CMD}\n" 2>&1| tee -a $OUT_FILE
 			${CMD} 2>&1| tee -a $OUT_FILE
 		done
