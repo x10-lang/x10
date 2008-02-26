0008d15bade01059f3a90f63e6603f20c66a9129
Ganesh Bikshandi <ganeshvb@domain>
2008-02-26 12:27:21 +0000

    o added powerpc port in xthreads o modified the testcase Test_async_gen.cc and added it to Makefile
    git-svn-id: svn://svn.code.sf.net/p/x10/code@5084 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.lib/src/xthreads.h
+++ trunk/x10.lib/src/xthreads.h
@@ -1,3 +1,12 @@
+/*
+ *
+ * (c) Copyright IBM Corporation 2007
+ *
+ * $Id: xthreads.h,v 1.3 2008-02-26 12:27:21 ganeshvb Exp $
+ * This file is part of X10 Runtime System.
+ */
+
+/** Implementation file for X10Lib's async interface. **/
 
 /**
  * X10 THREADS Interface 
@@ -25,50 +34,131 @@ typedef void(*func)();
 extern x10_async_queue_t ReadyQueue;
 extern x10_async_queue_t WaitQueue;
 
+/* provide an alternate impl. for CYGWIN */
+#ifdef CYGWIN
+
+#define x10_thread_create
+#define x10_thread_run(closure)
+#define x10_thread_yield
+#define x10_thread_wait(c)
+#define x10_thread_free
+#define x10_thread_libcall0(fptr)
+#define x10_thread_libcall1(fptr, arg0)
+#define x10_thread_libcall2(fptr, arg0, arg1)
+#define x10_thread_libcall3(fptr, arg0, arg1, arg2)
+#define x10_thread_libcall4(fptr, arg0, arg1, arg2, arg3)
+#define x10_thread_libcall5(fptr, arg0, arg1, ar2, ar3, arg4)
+
+#else
+
 #define x10_thread_create  \
+	do {\
 	     getcontext(&this->_current_ctxt); \
              this->_current_ctxt.uc_stack.ss_sp = this->_stack; \
              this->_current_ctxt.uc_stack.ss_size = this->_stack_sz;\
              this->_current_ctxt.uc_stack.ss_flags = 0; \
              this->_current_ctxt.uc_link = &this->_caller_ctxt;\
-	     makecontext (&this->_current_ctxt,  (func)AsyncClosure::runMain, 1, this);
+	     makecontext (&this->_current_ctxt,  (func)AsyncClosure::runMain, 1, this); \
+	}while(0);
 
 #define x10_thread_run(closure)  \
-swapcontext (&closure->_caller_ctxt, &closure->_current_ctxt);
+	do {\
+		swapcontext (&closure->_caller_ctxt, &closure->_current_ctxt); \
+	}while(0);
 
-#define x10_thread_yield x10lib::PushQueue(ReadyQueue, this);\
-				swapcontext (&this->_current_ctxt, &this->_caller_ctxt); 
+#define x10_thread_yield  \
+	do {\
+		x10lib::PushQueue(ReadyQueue, this);\
+		swapcontext (&this->_current_ctxt, &this->_caller_ctxt);  \
+	}while(0);
 
-#define x10_thread_wait(c) x10lib::PushQueue(WaitQueue, this);\
- 				this->_cond_number = c;\
-				swapcontext (&this->_current_ctxt, &this->_caller_ctxt); 
+#define x10_thread_wait(c)  \
+	do {\
+		x10lib::PushQueue(WaitQueue, this);\
+ 		this->_cond_number = c;\
+		swapcontext (&this->_current_ctxt, &this->_caller_ctxt);  \
+	}while(0);
 
 #define x10_thread_free this->_done = true;
 
-/* Beware : Switching stack pointers before function calls will not work always */
-#define x10_switch_stack(newsp) \
-__asm__ __volatile__ ("movl %%eax, %%esp" \
-                       : \
-                       : "eax"(newsp));
-
-/* This is a better way to switch to X10LIB context */
-#ifdef X86
-#define x10_thread_async_spawn(place, handler, args, size) \
-                getcontext(&this->_libcall_ctxt);\
-		this->_libcall_ctxt.uc_stack.ss_sp = (void*) this->_caller_ctxt.uc_mcontext.gregs[REG_ESP] ;\
-		this->_libcall_ctxt.uc_stack.ss_size = this->_caller_ctxt.uc_stack.ss_size;\
-             	this->_libcall_ctxt.uc_stack.ss_flags = 0;\
-             	this->_libcall_ctxt.uc_link = &this->_current_ctxt;\
-		makecontext (&this->_libcall_ctxt, (func) x10lib::AsyncSpawnInline, 4, place, handler, args, size);\
-                swapcontext (&this->_current_ctxt, &this->_libcall_ctxt);
+#ifdef X86 
+#define set_stack_ptr \
+		this->_libcall_ctxt.uc_mcontext.gregs[REG_ESP] =  this->_caller_ctxt.uc_mcontext.gregs[REG_ESP];
 #else 
-#define x10_thread_async_spawn(place, handler, args, size) \
+#define set_stack_ptr  \
+		this->_libcall_ctxt.uc_mcontext.jmp_context.gpr[1] =  this->_caller_ctxt.uc_mcontext.jmp_context.gpr[1];
+#endif
+
+#define x10_thread_libcall0(fptr)\
+	do {\
                 getcontext(&this->_libcall_ctxt);\
-		this->_libcall_ctxt.uc_stack.ss_sp = (void*) this->_caller_ctxt.uc_mcontext.jmp_context.  gpr[7] ;\
+		this->_libcall_ctxt.uc_stack.ss_sp = this->_caller_ctxt.uc_stack.ss_sp;\
 		this->_libcall_ctxt.uc_stack.ss_size = this->_caller_ctxt.uc_stack.ss_size;\
              	this->_libcall_ctxt.uc_stack.ss_flags = 0;\
              	this->_libcall_ctxt.uc_link = &this->_current_ctxt;\
-		makecontext (&this->_libcall_ctxt, (func) x10lib::AsyncSpawnInline, 4, place, handler, args, size);\
-                swapcontext (&this->_current_ctxt, &this->_libcall_ctxt);
+		makecontext (&this->_libcall_ctxt, (func) fptr,0);\ 
+		set_stack_ptr; \
+                swapcontext (&this->_current_ctxt, &this->_libcall_ctxt); \
+	}while(0);
 
-#endif
+#define x10_thread_libcall1(fptr, arg0)\
+	do {\
+                getcontext(&this->_libcall_ctxt);\
+		this->_libcall_ctxt.uc_stack.ss_sp = this->_caller_ctxt.uc_stack.ss_sp;\
+		this->_libcall_ctxt.uc_stack.ss_size = this->_caller_ctxt.uc_stack.ss_size;\
+             	this->_libcall_ctxt.uc_stack.ss_flags = 0;\
+             	this->_libcall_ctxt.uc_link = &this->_current_ctxt;\
+		makecontext (&this->_libcall_ctxt, (func) fptr, 1, arg0); \
+		set_stack_ptr; \
+                swapcontext (&this->_current_ctxt, &this->_libcall_ctxt); \
+	}while(0);
+
+#define x10_thread_libcall2(fptr, arg0, arg1)\
+	do {\
+                getcontext(&this->_libcall_ctxt);\
+		this->_libcall_ctxt.uc_stack.ss_sp = this->_caller_ctxt.uc_stack.ss_sp;\
+		this->_libcall_ctxt.uc_stack.ss_size = this->_caller_ctxt.uc_stack.ss_size;\
+             	this->_libcall_ctxt.uc_stack.ss_flags = 0;\
+             	this->_libcall_ctxt.uc_link = &this->_current_ctxt;\
+		makecontext (&this->_libcall_ctxt, (func) fptr, 2, arg0, arg1);\
+		set_stack_ptr; \
+                swapcontext (&this->_current_ctxt, &this->_libcall_ctxt); \
+	}while(0);
+
+#define x10_thread_libcall3(fptr, arg0, arg1, arg2)\
+	do {\
+                getcontext(&this->_libcall_ctxt);\
+		this->_libcall_ctxt.uc_stack.ss_sp = this->_caller_ctxt.uc_stack.ss_sp;\
+		this->_libcall_ctxt.uc_stack.ss_size = this->_caller_ctxt.uc_stack.ss_size;\
+             	this->_libcall_ctxt.uc_stack.ss_flags = 0;\
+             	this->_libcall_ctxt.uc_link = &this->_current_ctxt;\
+		makecontext (&this->_libcall_ctxt, (func) fptr, 3, arg0, arg1, arg2); \
+		set_stack_ptr; \
+                swapcontext (&this->_current_ctxt, &this->_libcall_ctxt); \
+	}while(0);
+
+#define x10_thread_libcall4(fptr, arg0, arg1, arg2, arg3)\
+	do {\
+                getcontext(&this->_libcall_ctxt);\
+		this->_libcall_ctxt.uc_stack.ss_sp = this->_caller_ctxt.uc_stack.ss_sp;\
+		this->_libcall_ctxt.uc_stack.ss_size = this->_caller_ctxt.uc_stack.ss_size;\
+             	this->_libcall_ctxt.uc_stack.ss_flags = 0;\
+             	this->_libcall_ctxt.uc_link = &this->_current_ctxt;\
+		makecontext (&this->_libcall_ctxt, (func) fptr, 4, arg0, arg1, arg2, arg3);\
+		set_stack_ptr; \
+                swapcontext (&this->_current_ctxt, &this->_libcall_ctxt); \
+	}while(0);
+
+#define x10_thread_libcall5(fptr, arg0, arg1, arg2, arg3, arg4)\
+	do {\
+                getcontext(&this->_libcall_ctxt);\
+		this->_libcall_ctxt.uc_stack.ss_sp = this->_caller_ctxt.uc_stack.ss_sp;\
+		this->_libcall_ctxt.uc_stack.ss_size = this->_caller_ctxt.uc_stack.ss_size;\
+             	this->_libcall_ctxt.uc_stack.ss_flags = 0;\
+             	this->_libcall_ctxt.uc_link = &this->_current_ctxt;\
+		makecontext (&this->_libcall_ctxt, (func) fptr, 5, arg0, arg1, arg2, arg3, arg4);\
+		set_stack_ptr; \
+                swapcontext (&this->_current_ctxt, &this->_libcall_ctxt); \
+	}while(0);
+
+#endif 
