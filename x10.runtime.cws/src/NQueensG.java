32b95cef731da971f08696d95ae79a0571097400
Vijay Saraswat <vsaraswa@us.ibm.com>
2008-03-14 03:11:28 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@5106 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.runtime.cws/src/NQueensG.java
+++ x10.runtime.cws/src/NQueensG.java
@@ -3,195 +3,130 @@
 import java.util.concurrent.atomic.AtomicInteger;
 
 import x10.runtime.cws.*;
-import x10.runtime.cws.Closure.Outlet;
 import static x10.runtime.cws.Job.*;
 
-public class NQueensG extends Closure {
+/**
+ * An NQueens program that
+ * -- uses an counter per worker, and a global atomic counter, updated by a worker
+ *    when it quiesces.
+ * -- pushes asyncs directly onto the worker's deque instead of descending into them
+ * -- Further, the board is constructed lazily at the next ply. 
+ * 
+ * @author vj
+ *
+ */
+public class NQueensG {
 	
 	static int boardSize;
+	public  static final class MyWorker extends Worker {
+		MyWorker(Pool p, int index) {
+			super(p, index);
+		}
+		int result;
+	}
+	public static Pool.WorkerMaker workerMaker = new Pool.WorkerMaker() {
+		public Worker makeWorker(Pool p, int i) {
+			return new MyWorker(p, i);
+		}
+	};
 	public boolean requiresGlobalQuiescence() { return true; }
 	public static final int[] expectedSolutions = new int[] {
 		0, 1, 0, 0, 2, 10, 4, 40, 92, 352, 724, 2680, 14200,
 		73712, 365596, 2279184, 14772512};
 	
 	public static void main(String[] args) throws Exception {
-		int procs;
+		int procs, nReps, num;
 		try {
+			num = Integer.parseInt(args[2]);
 			procs = Integer.parseInt(args[0]);
-			System.out.println("Number of procs=" + procs);
+			nReps = Integer.parseInt(args[1]);
+			System.out.println("Number of procs=" + procs +" nReps="+nReps
+					+ " num=" + num);
 		}
 		catch (Exception e) {
-			System.out.println("Usage: java NQueensG <threads> ");
+			System.out.println("Usage: java NQueensG <threads> <numRepeatation> <N>");
 			return;
 		}
-		Pool g = new Pool(procs);
-		for (int i = 11; i < 16; i++) {
-			boardSize = i;
-			GloballyQuiescentJob job = new GloballyQuiescentJob(g) {
-//				Result is accumulated here.
-				AtomicInteger result = new AtomicInteger();
-				
-				public void setResultInt(int x) { result.set(x);}
-				public int resultInt() { return result.get();}
-				public void accumulateResultInt(int x) { result.addAndGet(x);}
-				@Override
-				public int spawnTask(Worker ws) throws StealAbort { 
-					return nQueens(ws, new int[]{});
-				}
-				public String toString() { 
-					return "GJob(NQG,#" + hashCode()+",i="+ boardSize 
-					+ ",status=" + status + ",frame=" + frame + ")";}
-			};
-			System.gc();
+		Pool g = new Pool(procs, workerMaker);
+		long sc=0, sa=0;
+		int[] Ns = new int[] { num};
+		for (int i = 0; i < Ns.length; i++) {
+			boardSize = Ns[i];
+			int result=0;
 			long s = System.nanoTime();
-			g.submit(job);
-			int result = job.getInt();
+			for (int j=0; j<nReps; j++) {				
+				//System.gc();
+				GloballyQuiescentJob job = 
+					new GloballyQuiescentVoidJob(g, new NFrame(new int[0])) {
+					AtomicInteger result = new AtomicInteger();
+					@Override public int resultInt() { return result.get();}
+					@Override
+					protected void onCheckIn(Worker ww) {
+						MyWorker w = (MyWorker) ww;
+						if (w.result != 0) {
+							result.addAndGet(w.result);
+							w.result=0;
+						}
+					}
+				};
+				g.submit(job);
+				result = job.getInt();
+			}
 			long t = System.nanoTime();
-			System.out.println("Result:" + i + 
-					" " + result + (job.resultInt()==expectedSolutions[i]?" ok" : " fail") 
-					+ " Time=" +  (t-s)/1000000  + " ms"
-					+  " Steals=" + g.getStealCount() 
-			);
+			System.out.println("VJXWS Queens(" + (Ns[i]) +")"+"\t"
+					+(t-s)/1000000/nReps  + " ms" + "\t" + 
+					(Ns[i] < expectedSolutions.length ? 
+							(result==expectedSolutions[Ns[i]]?"ok" : "fail") :"")
+					+"\t " + result
+					+ " " + "steals=" +((g.getStealCount()-sc)/nReps)
+	    			+ " " + "stealAttempts=" +((g.getStealAttempts()-sa)/nReps));
+	    	  
+	    	  sc=g.getStealCount();
+	    	  sa=g.getStealAttempts();
+			
 		}
 		g.shutdown();    
 	}
 	
-	// Boards are represented as arrays where each cell 
-	// holds the column number of the queen in that row
-	
-	NQueensG(NFrame f) { 
-		super(f);
-	}
 	public static class NFrame extends GFrame {
-//		 The label at which computation must be continued by the associated
-		// closure.
-		public volatile int PC;
 		final int[] sofar;
-		volatile int q;
-		int sum;
-		public NFrame(int[] a) { sofar=a;}
-		@Override public void setOutletOn(final Closure c) {
-			c.setOutlet(
-					new Outlet() {
-						public void run() {
-							int value = c.resultInt();
-							
-							sum += value;
-							
-							if ( Worker.reporting) {
-								Closure p = c.parent();
-								System.out.println(Thread.currentThread() + " " + c + " --> " + value
-										+ " into " + p);
-							}
-						}
-						public String toString() { return "OutletInto x from " + c;}
-					});
-		}
-		public Closure makeClosure() {
-			Closure c = new NQueensG(this);
-			return c;
+		int newQ;
+		public NFrame(int[] a) { sofar=a; newQ=-1; }
+		public NFrame(int[] a, int q) { sofar=a; newQ=q; }
+		
+		public void compute(Worker ww) throws StealAbort {
+			ww.popAndReturnFrame();
+			int[] a = sofar;
+			int row = a.length;
+			if (newQ >=0) {
+				int[] next = new int[row+1];
+				System.arraycopy(a,0,next,0,row);
+				next[row++]=newQ;
+				a=next;
+			}
+			MyWorker w = (MyWorker) ww;
+			for (int q=0; q < boardSize; ++q) {
+				boolean attacked = false;
+				for (int i = 0;  ! attacked && i < row; ++i) {
+					final int p = q-a[i], delta=row-i;
+					attacked = (p==0 || p == delta || p == -delta);
+				}
+				if (!attacked) { 
+					if (row+1==boardSize) {
+						w.result++;
+					} else {
+						NFrame nextPly = new NFrame(a, q);
+						w.pushFrame(nextPly);
+					}
+				}
+			}
+			
 		}
 		public String toString() { 
 			String s="[";
 			for (int i=0; i < sofar.length; i++) s += sofar[i]+","; 
-			return "GNF(" + s + "],q="  + q + ",sum=" + sum+")";
+			return "GNF(" + s + "]"  +")";
 		}
 	}
-	public static final int LABEL_0=0, LABEL_1=1, LABEL_2=2, LABEL_3=3;
-
-	@Override public void accumulateResultInt(int x) { 
-		Worker w  = (Worker) Thread.currentThread();
-		w.currentJob().accumulateResultInt(x);
-	}
-	int result;
-	@Override public void setResultInt(int x) { result=x;}
-	@Override public int resultInt() { return result;}
-	
-	public static int nQueens(Worker w, int[] a) throws StealAbort {
-		final int row = a.length;
-		if (row >= boardSize) {
-			return 1;
-		}
-		NFrame frame = new NFrame(a);
-		frame.q=1;
-		frame.sum=0;
-		frame.PC=LABEL_1;
-		w.pushFrame(frame);
-		//int sum=0;
-		int q=0;
-		while (q < boardSize) {
-			boolean attacked = false;
-			for (int i = 0; i < row && ! attacked; i++) {
-				int p = a[i];
-				attacked = (q == p || q == p - (row - i) || q == p + (row - i));
-			}
-			if (!attacked) { 
-				int[] next = new int[row+1];
-				for (int k = 0; k < row; ++k) 
-					next[k] = a[k];
-				next[row] = q;
-				
-				final int y = nQueens(w, next);
-				w.abortOnSteal(y);
-				//sum +=y;
-				frame.sum +=y;
-			}
-			q++;
-			frame.q=q+1;
-		}
-		w.popFrame();
-		return frame.sum;
-		
-	}
-	public void compute(Worker w, Frame frame) throws StealAbort {
-		NFrame f = (NFrame) frame;
-		final int[] a = f.sofar;
-		int row = a.length;
-		
-		switch (f.PC) {
-		case LABEL_0:
-			if (row >= boardSize) {
-				result=1;
-				setupReturn();
-				return;
-			}
-		case LABEL_1: 
-			int q=f.q;
-			//int sum=0;
-			while (q < boardSize) {
-				f.q =q+1;
-				boolean attacked = false;
-				for (int i = 0; i < row && ! attacked; i++) {
-					int p = a[i];
-					attacked = (q == p || q == p - (row - i) || q == p + (row - i));
-				}
-				if (!attacked) { 
-					int[] next = new int[row+1];
-					for (int k = 0; k < row; ++k) 
-						next[k] = a[k];
-					next[row] = q;
-					
-					int y= nQueens(w, next);
-					w.abortOnSteal(y);
-					//sum += y;
-					// this cannot be f.sum=y. f.sum may have been updated by other
-					// joiners in the meantime.
-					f.sum +=y;
-					
-				}
-				q++;
-			}
-			/*f.PC=LABEL_2;
-			if (w.sync())
-				return;
-		case LABEL_2:*/
-			result=f.sum;
-			setupReturn();
-		}
-		return;
-	}
-	public String toString() { return "NQG(#" + hashCode()+" " + frame + ",result=" 
-		+ result+",jc=" + joinCount + ")";}
-	
 }
 
