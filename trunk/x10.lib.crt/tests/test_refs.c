35dd2766d6a28c500881221480c84928da2bb97b
Ganesh Bikshandi <ganeshvb@domain>
2008-05-20 13:46:46 +0000

    This commit was generated by cvs2svn to compensate for changes in r5181, which included commits to RCS files with non-trunk default branches.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@5182 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ trunk/x10.lib.crt/tests/test_refs.c
@@ -0,0 +1,73 @@
+#include "x10.h"
+#include <assert.h>
+
+typedef struct
+{
+  int a;
+  char b;
+}foo;
+
+typedef struct 
+{
+  x10_async_closure_t base;
+  x10_remote_ref_t foo_local;
+} my_closure_t;
+
+
+void __x10_callback_asyncswitch (x10_async_closure_t* closure, 
+				 x10_finish_record_t* frecord, 
+				 x10_clock_t* clocks, 
+				 int num_clocks){
+  
+  switch (closure->handler) {
+    
+  case 1: {
+    my_closure_t* cl = (my_closure_t*) closure;
+    void* ref = x10_deserialize_ref(cl->foo_local);
+    printf ("foo_remote_ref : %d %x\n", cl->foo_local.loc, cl->foo_local.addr);
+    printf ("%x \n", ref);
+    printf ("%d\n", x10_get_loc(ref));
+  }
+    break;
+  }
+}
+
+int __xlc_upc_main() {}
+
+int main()
+{
+  x10_init();
+  
+  printf ("hello world: %d %d \n", __x10_here, __x10_numplaces);
+  
+  if (__x10_here ==0) {
+    
+    foo* foo_local = (foo*) malloc(sizeof(foo));
+    
+    foo_local->a = 1;
+    foo_local->b = 'a';
+    
+    assert (x10_get_loc(foo_local) == __x10_here);
+    
+    x10_remote_ref_t foo_ref = x10_serialize_ref (foo_local);
+
+    printf ("foo_ref : %d %x\n", foo_ref.loc, foo_ref.addr);
+
+    my_closure_t closure;
+    closure.base.handler = 1;
+    closure.foo_local = foo_ref;
+
+    x10_finish_record_t frecord = {0, 0};
+
+    x10_comm_handle_t req = x10_async_spawn (1, (x10_async_closure_t*) &closure, sizeof(closure), &frecord, NULL, 0);
+
+    x10_async_spawn_wait (req);
+
+  } else {
+    
+    x10_infinite_poll();
+  }
+  
+  x10_finalize();
+  return 0;
+}
