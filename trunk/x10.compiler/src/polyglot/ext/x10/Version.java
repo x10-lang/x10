c17203fb7e49dabdb80e51a7312ad9cda421825d
Igor Peshansky <igorp@us.ibm.com>
2008-05-21 17:02:11 +0000

    Add timestamp for build time
    git-svn-id: svn://svn.code.sf.net/p/x10/code@5190 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.compiler/src/polyglot/ext/x10/Version.java
+++ trunk/x10.compiler/src/polyglot/ext/x10/Version.java
@@ -4,16 +4,32 @@
 //                                                                             
 // --------------------------------------------------------------------------- 
 
-
 package polyglot.ext.x10;
 
 /**
  * Version information for x10 extension
  */
 public class Version extends polyglot.main.Version {
+    java.util.Date date;
+    public Version() {
+    	Class c = Version.class;
+//    	java.net.URL r = c.getClassLoader().getResource("META-INF/MANIFEST.MF");
+    	java.net.URL r = c.getClassLoader().getResource(c.getName().replace('.','/')+".class");
+    	long lastModified = 0;
+    	try {
+			lastModified = r.openConnection().getLastModified();
+		} catch (java.io.IOException e) { /* ignore */ }
+    	this.date = lastModified == 0 ? null : new java.util.Date(lastModified);
+    }
     public String name() { return "x10"; }
 
     public int major() { return 1; }
     public int minor() { return 0; }
     public int patch_level() { return 0; }
+    public String toString() {
+        String version = super.toString();
+        if (date != null)
+        	version += " (" + new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(date) + ")";
+		return version;
+    }
 }
