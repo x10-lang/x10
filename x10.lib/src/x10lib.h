884647c8ef1f2253d3fe5ffe5a6888f5c9f69d7d
Igor Peshansky <igorp@us.ibm.com>
2008-06-02 16:08:09 +0000

    Reorder headers Remove some internal headers Move internal macro to the internal header Remove declarations of globals Remove x10_cleanup Add TODO
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@5211 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.lib/src/x10lib.h
+++ x10.lib/src/x10lib.h
@@ -1,7 +1,7 @@
 /*
  * (c) Copyright IBM Corporation 2007
  *
- * $Id: x10lib.h,v 1.39 2008-04-19 06:12:16 ganeshvb Exp $
+ * $Id: x10lib.h,v 1.40 2008-06-02 16:08:09 ipeshansky Exp $
  * This file is part of X10 Runtime System.
  */
 
@@ -12,33 +12,24 @@
 
 #include <x10/types.h>
 #include <x10/err.h>
-#include <x10/env.h>
-#include <x10/lock.h>
-#include <x10/register.h>
-#include <x10/rmc.h>
 #include <x10/gas.h>
 #include <x10/progress.h>
-#include <x10/switch.h>
 #include <x10/order.h>
 #include <x10/async.h>
 #include <x10/aggregate.h>
 #include <x10/aggregate_single.h>
-#include <x10/aggregate.h>
 #include <x10/clock.h>
-#include <x10/xassert.h>
 #include <x10/closure.h>
+#include <x10/finish.h>
 #include <x10/utility.h>
 #include <x10/broadcast.h>
-#include <x10/am.h>
 #include <x10/array_copy.h>
 #include <x10/array_table.h>
 
-/* arry operations */
+/* array operations */
 #ifdef __cplusplus
-//#include <x10/alloc.h>
-#include <x10/reduce.h>
+#include <x10/alloc.h>
 #include <x10/xmacros.h>
-#include <x10/finish.h>
 #include <x10/region.h>
 #include <x10/iter.h>
 #include <x10/dist.h>
@@ -50,42 +41,20 @@
 #include <x10/queue.h>
 #endif
 
-#include <x10/utility.h>
-//#include <lapi.h>
-
-/* Maximum message size allowed. */
-#define X10_MAX_MSG_SIZE LAPI_MAX_MSG_SZ
-
-
 #ifdef __cplusplus
 namespace x10lib {
 
 /* x10lib global state */
-extern int __x10_inited;
-extern lapi_handle_t __x10_hndl;
-extern lapi_thread_func_t __x10_tf;
-extern lapi_cntr_t __x10_wait_cntr;
-extern int __x10_num_places;
-extern int __x10_my_place;
-extern int __x10_addr_hndl;
-extern int __x10_addrtbl_sz;
-extern int __x10_max_agg_size;
-
-/** reduction variables **/
-extern lapi_cntr_t reduce_cntr;
-extern void **reduce_cntr_list;
-extern void *scratch;
-extern void **reduce_list;
-extern void *inbuf[];
-extern int reduceCount;
+//extern int __x10_num_places; // not needed -- use method
+//extern int __x10_my_place; // not needed -- use method
 
 /* Initialization */
 x10_err_t Init(x10_async_handler_t *hndlrs, int n);
-	
+
 /* Termination */
 x10_err_t Finalize();
 
-} /* closing brace for namespace x10lib */
+} /* namespace x10lib */
 #endif
 
 #ifdef __cplusplus
@@ -95,14 +64,13 @@ extern "C" {
 
 /* Initialization */
 x10_err_t x10_init(x10_async_handler_t* hndlrs, int n);
-		
+
 /* Termination */
 x10_err_t x10_finalize();
 
-/* Cleanup */
-void x10_cleanup();
-	
 #ifdef __cplusplus
-} /* closing brance for extern "C" */
+} /* extern "C" */
 #endif
 #endif /* __X10_X10LIB_H */
+
+// TODO: Move C wrapper declarations into another directory
