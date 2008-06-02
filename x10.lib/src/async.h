e7f6a0be09582fd47a3fb4e92e94a9af4da09c0c
Igor Peshansky <igorp@us.ibm.com>
2008-06-02 16:07:40 +0000

    Define NO_HANDLER Add comments
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@5207 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.lib/src/async.h
+++ x10.lib/src/async.h
@@ -1,7 +1,7 @@
 /*
  * (c) Copyright IBM Corporation 2007
  *
- * $Id: async.h,v 1.21 2007-12-10 15:52:11 srkodali Exp $
+ * $Id: async.h,v 1.22 2008-06-02 16:07:40 ipeshansky Exp $
  * This file is part of X10 Runtime System.
  */
 
@@ -16,6 +16,8 @@
 #define X10_MAX_ASYNC_ARGS 6
 #define X10_MAX_ASYNC_ARGS_SIZE \
 		(X10_MAX_ASYNC_ARGS * sizeof(x10_async_arg_t))
+
+#define NO_HANDLER ((x10_async_handler_t) -1)
 
 /* C++ Lang Interface */
 #ifdef __cplusplus
@@ -39,6 +41,7 @@ x10_err_t x10_async_spawn_inline(x10_place_t tgt,
 				x10_async_handler_t hndlr,
 				void *args, size_t size);
 
+/* Expected to be defined in the user code */
 void AsyncSwitch(x10_async_handler_t h, void *args, int niter);
 
 #ifdef __cplusplus
