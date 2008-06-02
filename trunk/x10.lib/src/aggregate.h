a9712373fcb582e0318ad6e8cf240990d6679e94
Igor Peshansky <igorp@us.ibm.com>
2008-06-02 16:07:18 +0000

    Add comments
    git-svn-id: svn://svn.code.sf.net/p/x10/code@5204 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.lib/src/aggregate.h
+++ trunk/x10.lib/src/aggregate.h
@@ -1,7 +1,7 @@
 /*
  * (c) Copyright IBM Corporation 2007
  *
- * $Id: aggregate.h,v 1.25 2007-12-08 14:28:40 srkodali Exp $
+ * $Id: aggregate.h,v 1.26 2008-06-02 16:07:18 ipeshansky Exp $
  * This file is part of X10 Runtime System.
  */
 
@@ -18,19 +18,19 @@ namespace x10lib {
 	/* User specified method execution on the remote task. */
 	x10_err_t AsyncSpawnInlineAgg(x10_place_t tgt,
 					x10_async_handler_t hndlr,
-					x10_async_arg_t arg0);
+					x10_async_arg_t arg0); // not compiler interface
 
 	x10_err_t AsyncSpawnInlineAgg(x10_place_t tgt,
 					x10_async_handler_t hndlr,
 					x10_async_arg_t arg0,
-					x10_async_arg_t arg1);
+					x10_async_arg_t arg1); // not compiler interface
 
 	x10_err_t AsyncSpawnInlineAgg(x10_place_t tgt,
 					x10_async_handler_t hndlr,
 					void *args, size_t size);
 
 	/* Flush left-over asyncs during aggregation */
-	x10_err_t AsyncAggFlush(x10_async_handler_t hndlr, size_t size);
+	x10_err_t AsyncAggFlush(x10_async_handler_t hndlr, size_t size); // call from finish/next; make internal
 
 } /* closing brace for namespace x10lib */
 #endif
@@ -44,19 +44,19 @@ extern "C"
 /* User specified method execution on the remote task. */
 x10_err_t x10_async_spawn_inline_agg1(x10_place_t tgt,
 					x10_async_handler_t hndlr,
-					x10_async_arg_t arg0);
+					x10_async_arg_t arg0); // not compiler interface
 
 x10_err_t x10_async_spawn_inline_agg2(x10_place_t tgt,
 					x10_async_handler_t hndlr,
 					x10_async_arg_t arg0,
-					x10_async_arg_t arg1);
+					x10_async_arg_t arg1); // not compiler interface
 
 x10_err_t x10_async_spawn_inline_agg(x10_place_t tgt,
 					x10_async_handler_t hndlr,
 					void *args, size_t size);
 
 /* Flush left-over asyncs during aggregation */
-x10_err_t x10_async_agg_flush(x10_async_handler_t hndlr, size_t size);
+x10_err_t x10_async_agg_flush(x10_async_handler_t hndlr, size_t size); // call from finish/next; make internal
 
 #ifdef __cplusplus
 } /* closing brace for extern "C" */
