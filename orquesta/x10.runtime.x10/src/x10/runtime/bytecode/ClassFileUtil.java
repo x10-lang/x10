dfcf21332da7a135c71e7d8efb656ecff81baa98
Igor Peshansky <igorp@us.ibm.com>
2008-07-01 09:15:15 +0000

    Define constants for signature types Intern all strings Add isArray() and isArrayOf() Add constantPoolType()
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@5309 a326200e-df2c-42e0-81fa-597e909af41d

--- orquesta/x10.runtime.x10/src/x10/runtime/bytecode/ClassFileUtil.java
+++ orquesta/x10.runtime.x10/src/x10/runtime/bytecode/ClassFileUtil.java
@@ -2,13 +2,28 @@ package x10.runtime.bytecode;
 
 import java.util.ArrayList;
 
+import static x10.runtime.bytecode.ClassFileConstants.*;
+
 public class ClassFileUtil {
+	public static final String SIG_byte = "B";
+	public static final String SIG_char = "C";
+	public static final String SIG_double = "D";
+	public static final String SIG_float = "F";
+	public static final String SIG_int = "I";
+	public static final String SIG_long = "J";
+	public static final String SIG_short = "S";
+	public static final String SIG_void = "V";
+	public static final String SIG_boolean = "Z";
+	public static final String SIG_null = "N";
+	public static final String SIG_String = "Ljava/lang/String;";
+	public static final String SIG_Class = "Ljava/lang/Class;";
+
 	/** Order matters! */
 	public static final String[] PRIMITIVES = {
 		"byte", "char", "double", "float", "int", "long", "short", "void", "boolean"
 	};
 	/** Order matters! */
-	public static final String[] PRIM_SIGS = { "B", "C", "D", "F", "I", "J", "S", "V", "Z" };
+	public static final String[] PRIM_SIGS = { SIG_byte, SIG_char, SIG_double, SIG_float, SIG_int, SIG_long, SIG_short, SIG_void, SIG_boolean };
 	public static String typeToSignature(String string) {
 		int bkt_idx = string.lastIndexOf("[]");
 		if (bkt_idx != -1)
@@ -18,7 +33,7 @@ public class ClassFileUtil {
 			if (string.equals(PRIMITIVES[i]))
 				return PRIM_SIGS[i];
 		}
-		return "L" + string.replace('.', '/') + ";";
+		return ("L" + string.replace('.', '/') + ";").intern();
 	}
 	public static String typeFromSignature(String signature) {
 		if (signature.charAt(0) == '[')
@@ -51,6 +66,19 @@ public class ClassFileUtil {
 		}
 		throw new IllegalArgumentException(type.substring(0, 1));
 	}
+	public static boolean isArray(String type) {
+		if (type.charAt(0) != '[')
+			return false;
+		return true;
+	}
+	public static boolean isArrayOf(String type, String base) {
+		if (type.charAt(0) != '[')
+			return false;
+		if (isPrimitive(base))
+			return type.substring(1).equals(base);
+		// TODO: check inheritance
+		return type.substring(1).equals(base);
+	}
 	public static boolean isWidePrimitive(char t) {
 		return t == 'D' || t == 'J';
 	}
@@ -65,12 +93,12 @@ public class ClassFileUtil {
 			int rpr_idx = signature.indexOf(')');
 			assert (lpr_idx != -1 && rpr_idx != -1);
 			MethodSig res = new MethodSig();
-			res.name = signature.substring(0, lpr_idx);
-			res.returnType = signature.substring(rpr_idx+1);
+			res.name = signature.substring(0, lpr_idx).intern();
+			res.returnType = signature.substring(rpr_idx+1).intern();
 			ArrayList<String> args = new ArrayList<String>();
 			for (int i = lpr_idx+1; i < rpr_idx; ) {
 				int j = nextTypePos(signature, i);
-				args.add(signature.substring(i, j));
+				args.add(signature.substring(i, j).intern());
 				i = j;
 			}
 			res.argTypes = args.toArray(new String[args.size()]);
@@ -87,4 +115,46 @@ public class ClassFileUtil {
 	}
 
 	public static final int ACC_STATIC = 0x0008;
+
+	public static String constantPoolType(ClassFile cf, int index) {
+		byte[] entry = cf.constant_pool.get(index);
+		String type = null;
+		switch (entry[0]) {
+		case CONSTANT_Double:
+			type = SIG_double;
+			break;
+		case CONSTANT_Float:
+			type = SIG_float;
+			break;
+		case CONSTANT_Integer:
+			type = SIG_int;
+			break;
+		case CONSTANT_Long:
+			type = SIG_long;
+			break;
+		case CONSTANT_String:
+			type = SIG_String;
+			break;
+		case CONSTANT_Class:
+			type = SIG_Class;
+			break;
+		case CONSTANT_Fieldref:
+		case CONSTANT_Methodref:
+		case CONSTANT_InterfaceMethodref:
+		case CONSTANT_NameAndType:
+		case CONSTANT_Utf8:
+			throw new IllegalArgumentException("Bad ldc type");
+		}
+		return type;
+	}
+
+	public static String[] methodArguments(String container, String signature, boolean isStatic) {
+		MethodSig sig = MethodSig.fromSignature(signature);
+		String[] argTypes = isStatic ? sig.argTypes : new String[1+sig.argTypes.length];
+		if (!isStatic) {
+			argTypes[0] = typeToSignature(container);
+			System.arraycopy(sig.argTypes, 0, argTypes, 1, sig.argTypes.length);
+		}
+		return argTypes;
+	}
 }
