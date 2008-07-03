da44d328be5dec5c9c0688d7997e090597f4fb8f
Igor Peshansky <igorp@us.ibm.com>
2008-07-03 03:25:29 +0000

    Add synthetic constructors and methods Add various instantiations Import relevant classes
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@5319 a326200e-df2c-42e0-81fa-597e909af41d

--- orquesta/x10.runtime.x10/tests/Map.java
+++ orquesta/x10.runtime.x10/tests/Map.java
@@ -1,4 +1,8 @@
-@x10.generics.Parameters({"K","V"})
+import x10.generics.Parameters;
+import x10.generics.Synthetic;
+import x10.runtime.Runtime;
+
+@Parameters({"K","V"})
 public class Map implements Runnable {
 	public class K {};
 	public class V {};
@@ -7,7 +11,12 @@ public class Map implements Runnable {
 	public K key;
 	public V value;
 	public Map() { }
-	public Map(Class V, Class K) { }
+	@Synthetic public Map(Class K, Class V) { this(); }
+	public Map(int i) { }
+	@Synthetic public Map(Class K, Class V, int i) { this(i); }
+	@Synthetic public static boolean instanceof$(Object o, String constraint) { assert(false); return true; }
+	public static boolean instanceof$(Object o, String constraint, boolean b) { /*check constraint*/; return b; }
+	public static Object cast$(Object o, String constraint) { /*check constraint*/; return (Map)o; }
 	private boolean keq(K k1, K k2) {
 		if ((boolean)(k1 == k2))
 			return true;
@@ -27,5 +36,10 @@ public class Map implements Runnable {
 	}
 	public void run() {
 		System.out.println("Loaded and executed: "+getClass().getName());
+		Map m = new Map(int.class, double.class, 3);
+		K k = (Map.K)Runtime.coerce((int)5);
+		m.put(k, (Map.V)Runtime.coerce((double)7.0));
+		System.out.println("Created map m of type"+m.getClass().getName());
+		System.out.println("\tretrieved m{5}="+m.get(k));
 	}
 }
