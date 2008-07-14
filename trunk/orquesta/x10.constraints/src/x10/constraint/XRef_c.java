e640911d30eb26cd66df152a31e1192d1966c1d5
Nathaniel Nystrom <user@domain>
2008-07-14 01:10:51 +0000

    Wrap the self constraint in a closure to prevent it from being evaluated too early.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@5367 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ trunk/orquesta/x10.constraints/src/x10/constraint/XRef_c.java
@@ -0,0 +1,11 @@
+package x10.constraint;
+
+public abstract class XRef_c<T> {
+    public T result;
+    public final T get() {
+	if (result == null)
+	    result = compute();
+	return result;
+    }
+    public abstract T compute();
+}
