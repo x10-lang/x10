435e5c7963d209c7bd710653d67bce81111c4998
No Author <user@domain>
2008-08-07 13:08:58 +0000

    This commit was manufactured by cvs2svn to create tag 'OOPSLA08_CAMERA'.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/tags/OOPSLA08_CAMERA@5513 a326200e-df2c-42e0-81fa-597e909af41d

--- orquesta/x10.runtime.x10/1.7/x10/lang/DistRail.x10
+++
@@ -1,5 +0,0 @@
-package x10.lang;
-
-public class DistRail[T] {
-    public incomplete static def make[T](unique: Dist{unique}, f: (Point) => T): DistRail[T];
-}
--- x10.runtime.cws/src/Test.java
+++
@@ -1,9 +0,0 @@
-
-public class Test {
-
-	public static void main(String[] args) {
-		int[][] v = new int[8][4];
-		for (int i=0; i < 8; i++) for (int j=0; j <4; j++) 
-			if (v[i][j] != 0) System.out.println("huh");
-	}
-}
