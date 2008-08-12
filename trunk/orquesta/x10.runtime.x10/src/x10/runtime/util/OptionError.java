d5d42d6987f11c24ec3efcb3877ae202d0d71645
Nathaniel Nystrom <user@domain>
2008-08-12 22:32:49 +0000

    Copy configuration classes from x10.runtime.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@5586 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ trunk/orquesta/x10.runtime.x10/src/x10/runtime/util/OptionError.java
@@ -0,0 +1,37 @@
+/*
+ *
+ * (C) Copyright IBM Corporation 2006
+ *
+ *  This file is part of X10 Language.
+ *
+ */
+package x10.runtime.util;
+
+/**
+ * An option error.
+ * Used to report an error in option value in the Configuration class.
+ * @author igor
+ */
+public class OptionError extends Exception {
+	/**
+	 * Construct an option error with a detail message.
+	 */
+	public OptionError(String message) {
+		super(message);
+	}
+
+	/**
+	 * Construct an option error with a cause.
+	 */
+	public OptionError(Throwable cause) {
+		super(cause);
+	}
+
+	/**
+	 * Construct an option error with a detail message and a cause.
+	 */
+	public OptionError(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
+
