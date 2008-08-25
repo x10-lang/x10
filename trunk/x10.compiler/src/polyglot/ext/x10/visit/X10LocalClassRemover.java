a05edcfd0a872e0ec2881681b59c96d85b940271
Krishna Nandivada Venkata <user@domain>
2008-08-25 09:54:38 +0000

    X10 classes should have x10.lang.Object as the super type.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@5672 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.compiler/src/polyglot/ext/x10/visit/X10LocalClassRemover.java
+++ trunk/x10.compiler/src/polyglot/ext/x10/visit/X10LocalClassRemover.java
@@ -3,6 +3,7 @@
  */
 package polyglot.ext.x10.visit;
 
+import polyglot.ast.TypeNode;
 import polyglot.ast.NodeFactory;
 import polyglot.ext.x10.types.X10Context;
 import polyglot.frontend.Job;
@@ -10,7 +11,9 @@ import polyglot.types.CodeInstance;
 import polyglot.types.Context;
 import polyglot.types.MethodInstance;
 import polyglot.types.TypeSystem;
+import polyglot.util.Position;
 import polyglot.visit.LocalClassRemover;
+import polyglot.ext.x10.types.X10TypeSystem;
 
 public class X10LocalClassRemover extends LocalClassRemover {
 	public X10LocalClassRemover(Job job, TypeSystem ts, NodeFactory nf) {
@@ -32,5 +35,9 @@ public class X10LocalClassRemover extends LocalClassRemover {
     		c = c.pop();
     	}
     	return false;
+    }
+
+    protected TypeNode defaultSuperType(Position pos) {
+	return nf.CanonicalTypeNode(pos, ((X10TypeSystem)ts).X10Object());
     }
 }
