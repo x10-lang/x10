d6c1cd70a9448608d87b958392d34d67169b524c
Nathaniel Nystrom <user@domain>
2008-09-07 23:42:25 +0000

    Type-checking for a(i)++
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@6014 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.compiler.p3/src/polyglot/ext/x10/ast/X10Unary_c.java
+++ x10.compiler.p3/src/polyglot/ext/x10/ast/X10Unary_c.java
@@ -10,14 +10,22 @@
  */
 package polyglot.ext.x10.ast;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import polyglot.ast.Binary;
+import polyglot.ast.Call;
 import polyglot.ast.Expr;
+import polyglot.ast.Field;
+import polyglot.ast.Id;
+import polyglot.ast.Local;
 import polyglot.ast.Node;
 import polyglot.ast.Precedence;
+import polyglot.ast.Receiver;
+import polyglot.ast.TypeNode;
 import polyglot.ast.Unary;
 import polyglot.ast.Unary_c;
 import polyglot.ast.Binary.Operator;
@@ -55,7 +63,7 @@ public class X10Unary_c extends Unary_c {
 	public Object constantValue() {
 		return super.constantValue();
 	}
-
+	
 	/**
 	 * Type check a binary expression. Must take care of various cases because
 	 * of operators on regions, distributions, points, places and arrays.
@@ -66,9 +74,6 @@ public class X10Unary_c extends Unary_c {
 	    X10TypeSystem ts = (X10TypeSystem) tc.typeSystem();
 
 	        Type t = expr.type();
-//		if ((op == NEG || op == POS) && ts.isPoint(t)) {
-//			return type(t);
-//		}
 		
 	        Name methodName = unaryMethodName(op);
 		if (methodName != null) {
@@ -81,13 +86,77 @@ public class X10Unary_c extends Unary_c {
 			// Cannot find the method.  Fall through.
 		    }
 		}
+		
+		if (op == POST_INC || op == POST_DEC || op == PRE_INC || op == PRE_DEC) {
+		    if (! expr.type().isNumeric()) {
+		        throw new SemanticException("Operand of " + op +
+		                                    " operator must be numeric.", expr.position());
+		    }
+
+		    if (expr instanceof Local) {
+		        Local l = (Local) expr;
+		        if (l.localInstance().flags().isFinal()) {
+		            throw new SemanticException("Cannot apply " + op + " to a final variable.", position());
+		        }
+		        return type(expr.type());
+		    }
+		    else if (expr instanceof Field) {
+		        Field l = (Field) expr;
+		        if (l.fieldInstance().flags().isFinal()) {
+		            throw new SemanticException("Cannot apply " + op + " to a final variable.", position());
+		        }
+		        return type(expr.type());
+		    }
+		    else {
+		        Expr target = null;
+		        List<Expr> args = null;
+		        List<TypeNode> typeArgs = null;
+		        
+		        // Handle a(i)++ and a.apply(i)++
+		        if (expr instanceof ClosureCall) {
+		            ClosureCall e = (ClosureCall) expr;
+		            target = e.target();
+		            args = e.arguments();
+		            typeArgs = e.typeArgs();
+		        }
+		        else if (expr instanceof X10Call) {
+		            X10Call e = (X10Call) expr;
+		            if (e.target() instanceof Expr && e.name().id() == Name.make("apply")) {
+		                target = (Expr) e.target();
+		                args = e.arguments();
+		                typeArgs = e.typeArguments();
+		            }
+		        }
+		        
+		        if (target != null) {
+		            List<Type> setArgTypes = new ArrayList<Type>();
+		            List<Type> setTypeArgs = new ArrayList<Type>();
+		            
+		            // RHS goes before index
+		            setArgTypes.add(expr.type());
+		            for (Expr e : args) {
+		                setArgTypes.add(e.type());
+		            }
+		            for (TypeNode tn : typeArgs) {
+		                setTypeArgs.add(tn.type());
+		            }
+		            try {
+		                X10MethodInstance mi = ts.findMethod(target.type(), ts.MethodMatcher(t, Name.make("set"), setTypeArgs, setArgTypes), tc.context().currentClassDef());
+		                return type(mi.returnType());
+		            }
+		            catch (SemanticException e) {
+		                throw e;
+		            }
+		        }
+		    }
+		}
 
 		X10Unary_c n = (X10Unary_c) super.typeCheck(tc);
 
 		Type resultType = n.type();
 		resultType = ts.performUnaryOperation(resultType, t, op);
 		if (resultType != n.type()) {
-			n = (X10Unary_c) n.type(resultType);
+		    n = (X10Unary_c) n.type(resultType);
 		}
 
 		return n;
