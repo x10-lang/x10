3c40205e3e9ab70569796aafe012f68bedd25d11
Nathaniel Nystrom <user@domain>
2008-09-08 21:33:37 +0000

    Void function interfaces.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk/x10.runtime.17@6061 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ src/x10/core/fun/VoidFun_0_1.java
@@ -0,0 +1,8 @@
+package x10.core.fun;
+
+import x10.types.Type;
+
+public interface VoidFun_0_1<T1> {
+    void apply(T1 o);
+    Type<?> rtt_x10$lang$Fun_0_1_Z1();
+}
---
+++ src/x10/core/fun/VoidFun_0_2.java
@@ -0,0 +1,9 @@
+package x10.core.fun;
+
+import x10.types.Type;
+
+public interface VoidFun_0_2<T1,T2> {
+    void apply(T1 o1, T2 o2);
+    Type<?> rtt_x10$lang$Fun_0_2_Z1();
+    Type<?> rtt_x10$lang$Fun_0_2_Z2();
+}
