bb41933c002722740698b303a1f448b0d2367e18
Nathaniel Nystrom <user@domain>
2008-09-09 20:03:18 +0000

    Check for duplicate typedefs.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@6094 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.compiler.p3/src/polyglot/ext/x10/ast/X10ClassBody_c.java
+++ x10.compiler.p3/src/polyglot/ext/x10/ast/X10ClassBody_c.java
@@ -10,9 +10,15 @@ package polyglot.ext.x10.ast;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+import polyglot.types.ClassDef;
+import polyglot.types.ClassType;
 import polyglot.types.MethodDef;
+import polyglot.types.Named;
+import polyglot.types.Ref;
 import polyglot.types.SemanticException;
 import polyglot.types.Type;
+import polyglot.types.TypeSystem;
+import polyglot.types.Types;
 
 import java.util.LinkedList;
 import java.util.ListIterator;
@@ -25,7 +31,11 @@ import polyglot.ast.MethodDecl;
 import polyglot.ast.MethodDecl_c;
 import polyglot.ast.Formal_c;
 import polyglot.types.ClassType_c;
+import polyglot.ext.x10.types.MacroType;
+import polyglot.ext.x10.types.TypeDef;
+import polyglot.ext.x10.types.X10ClassDef;
 import polyglot.ext.x10.types.X10Flags;
+import polyglot.ext.x10.types.X10TypeMixin;
 import polyglot.ext.x10.types.X10TypeSystem;
 import polyglot.ext.x10.types.X10TypeSystem_c;
 import polyglot.types.MethodInstance;
@@ -34,7 +44,45 @@ import polyglot.visit.ContextVisitor;
 import polyglot.ast.ClassBody_c;
 
 public class X10ClassBody_c extends ClassBody_c {
-	public X10ClassBody_c(Position pos, java.util.List<ClassMember> members) {
-		super(pos,members);
-	}
+    public X10ClassBody_c(Position pos, java.util.List<ClassMember> members) {
+        super(pos, members);
+    }
+
+    public Node conformanceCheck(ContextVisitor tc) throws SemanticException {
+        duplicateTypeDefCheck(tc);
+        return super.conformanceCheck(tc);
+    }
+
+    protected void duplicateTypeDefCheck(ContextVisitor tc) throws SemanticException {
+        X10ClassDef type = (X10ClassDef) tc.context().currentClassDef();
+
+        TypeSystem ts = tc.typeSystem();
+
+        ArrayList<TypeDef> l = new ArrayList<TypeDef>(type.memberTypes());
+
+        for (int i = 0; i < l.size(); i++) {
+            TypeDef mi = l.get(i);
+            MacroType ti = mi.asType();
+
+            for (int j = i + 1; j < l.size(); j++) {
+                TypeDef mj = l.get(j);
+                MacroType tj = mj.asType();
+
+                if (ti.name().equals(tj.name()) && ti.typeParameters().size() == tj.typeParameters().size() && ti.hasFormals(tj.formalTypes())) {
+                    throw new SemanticException("Duplicate type definition \"" + mj + "\".", mj.position());
+                }
+            }
+
+            for (Ref<? extends Type> tref : type.memberClasses()) {
+                Type t = Types.get(tref);
+                t = X10TypeMixin.baseType(t);
+                if (t instanceof ClassType) {
+                    ClassType ct = (ClassType) t;
+                    if (ct.name().equals(ti.name())) {
+                        throw new SemanticException("Type definition " + mi + " has the same name as member class " + ct + ".", mi.position());
+                    }
+                }
+            }
+        }
+    }
 }
