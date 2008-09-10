e831fd22d6c201773942b8edcdceb1cb20aebaaf
Nathaniel Nystrom <user@domain>
2008-09-10 21:25:36 +0000

    RTT for ValRail and Rail.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk/x10.runtime.17@6146 a326200e-df2c-42e0-81fa-597e909af41d

--- src/x10/core/Rail.java
+++ src/x10/core/Rail.java
@@ -1,6 +1,8 @@
 package x10.core;
 
+import x10.core.fun.Fun_0_1;
 import x10.types.Type;
+import x10.types.Types;
 
 public final class Rail<T> extends AnyRail<T> implements Settable<Integer,T> {
     public Rail(Type<T> type, int length) {
@@ -14,4 +16,48 @@ public final class Rail<T> extends AnyRail<T> implements Settable<Integer,T> {
     public T set(T v, Integer i) {
         return type.setArray(value, i, v);
     }
+    
+    //
+    // Runtime type information
+    //
+    
+    static public class RTT extends x10.types.RuntimeType<Rail<?>> {
+        Type<?> type;
+        
+        public RTT(Type<?> type) {
+            super(Rail.class);
+            this.type = type;
+        }
+
+        public boolean instanceof$(java.lang.Object o) {
+            if (!(o instanceof Rail))
+                return false;
+            Rail r = (Rail) o;
+            if (! r.type.equals(type)) // invariant
+                return false;
+            return true;
+        }
+        
+        public boolean isSubtype(Type<?> type) {
+            if (type instanceof Rail.RTT) {
+                Rail.RTT r = (Rail.RTT) type;
+                return r.type.equals(this.type);
+            }
+//            if (type instanceof Fun_0_1.RTT) {
+//                Fun_0_1.RTT r = (Fun_0_1.RTT) type;
+//                return r.I.equals(Types.INT) && r.V.equals(this.type);
+//            }
+//            if (type instanceof Settable.RTT) {
+//                Settable.RTT r = (Settable.RTT) type;
+//                return r.I.equals(Types.INT) && r.V.equals(this.type);
+//            }
+            return false;
+        }
+    }
+
+    public Type<?> rtt_x10$lang$Fun_0_1_Z1() { return Types.INT; }
+    public Type<?> rtt_x10$lang$Fun_0_1_U()  { return type; }
+    public Type<?> rtt_x10$lang$Settable_I() { return Types.INT; }
+    public Type<?> rtt_x10$lang$Settable_V() { return type; }
+
 }
--- src/x10/core/ValRail.java
+++ src/x10/core/ValRail.java
@@ -1,6 +1,7 @@
 package x10.core;
 
 import x10.types.Type;
+import x10.types.Types;
 
 public final class ValRail<T> extends AnyRail<T> {
     public ValRail(Type<T> type, int length) {
@@ -10,4 +11,42 @@ public final class ValRail<T> extends AnyRail<T> {
     public ValRail(Type<T> type, int length, Object array) {
         super(type, length, array);
     }
+    
+    //
+    // Runtime type information
+    //
+    
+    static public class RTT extends x10.types.RuntimeType<Rail<?>> {
+        Type<?> type;
+        
+        public RTT(Type<?> type) {
+            super(Rail.class);
+            this.type = type;
+        }
+
+        public boolean instanceof$(java.lang.Object o) {
+            if (!(o instanceof ValRail))
+                return false;
+            ValRail r = (ValRail) o;
+            if (! r.type.isSubtype(type)) // covariant
+                return false;
+            return true;
+        }
+        
+        
+        public boolean isSubtype(Type<?> type) {
+            if (type instanceof Rail.RTT) {
+                Rail.RTT r = (Rail.RTT) type;
+                return r.type.equals(this.type);
+            }
+//            if (type instanceof Fun_0_1.RTT) {
+//                Fun_0_1.RTT r = (Fun_0_1.RTT) type;
+//                return r.I.equals(Types.INT) && r.V.equals(this.type);
+//            }
+            return false;
+        }
+    }
+
+    public Type<?> rtt_x10$lang$Fun_0_1_Z1() { return Types.INT; }
+    public Type<?> rtt_x10$lang$Fun_0_1_U()  { return type; }
 }
