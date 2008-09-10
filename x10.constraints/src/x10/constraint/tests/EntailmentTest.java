94f4a298914e2b3d1254240bafd3c86adb74da4b
Vijay Saraswat <vsaraswa@us.ibm.com>
2008-09-10 15:28:06 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@6138 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.constraints/src/x10/constraint/tests/EntailmentTest.java
+++ x10.constraints/src/x10/constraint/tests/EntailmentTest.java
@@ -159,7 +159,6 @@ public class EntailmentTest extends TestCase {
 	public void test9() throws Throwable {
 		XConstraint c = new XConstraint_c();
 		
-
 		XConstraint d = new XConstraint_c();
 		XVar x1 = d.genEQV(XTerms.makeName("x1"), true);
 		XTerm x1f = XTerms.makeField(x1, XTerms.makeName(Object.class, "f"));
@@ -187,6 +186,48 @@ public class EntailmentTest extends TestCase {
 		d.addBinding(x2,x1f);
 		d.addBinding(x2,v2);
 
+		boolean result = c.entails(d);
+		assertFalse(result);
+		
+	}
+	/**
+	 *  |- exists x1. x1=v1, x2=v1.f, x2=v2.f -- should fail
+	 * @throws Throwable
+	 */
+	public void test11() throws Throwable {
+		XConstraint c = new XConstraint_c();
+		
+
+		XConstraint d = new XConstraint_c();
+		XVar x1 = d.genEQV(XTerms.makeName("x1"), true);
+		XVar x2 = d.genEQV(XTerms.makeName("x2"), true);
+		XTerm v1f = XTerms.makeField(v1, XTerms.makeName(Object.class, "f"));
+		XTerm v2f = XTerms.makeField(v2, XTerms.makeName(Object.class, "f"));
+		d.addBinding(v1,x1);
+		d.addBinding(x2,v1f);
+		d.addBinding(x2,v2f);
+
+		boolean result = c.entails(d);
+		assertFalse(result);
+		
+	}
+	/**
+	 *   exists x1. x1=v1, x2=v1.f, x2=v2.f |- v1=v2
+	 * @throws Throwable
+	 */
+	public void test12() throws Throwable {
+		XConstraint c = new XConstraint_c();
+		XVar x1 = c.genEQV(XTerms.makeName("x1"), true);
+		XVar x2 = c.genEQV(XTerms.makeName("x2"), true);
+		XTerm v1f = XTerms.makeField(v1, XTerms.makeName(Object.class, "f"));
+		XTerm v2f = XTerms.makeField(v2, XTerms.makeName(Object.class, "f"));
+		c.addBinding(v1,x1);
+		c.addBinding(x2,v1f);
+		c.addBinding(x2,v2f);
+		
+		XConstraint d = new XConstraint_c();
+		c.addBinding(v1,v2);
+		
 		boolean result = c.entails(d);
 		assertFalse(result);
 		
