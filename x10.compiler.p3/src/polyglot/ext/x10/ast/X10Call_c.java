a8ac8165ea3ca1814176b26733d333bfcc931085
Nathaniel Nystrom <user@domain>
2008-09-10 14:35:49 +0000

    Check for ambiguity between method call and closure call.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@6131 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.compiler.p3/src/polyglot/ext/x10/ast/X10Call_c.java
+++ x10.compiler.p3/src/polyglot/ext/x10/ast/X10Call_c.java
@@ -142,7 +142,9 @@ public class X10Call_c extends Call_c implements X10Call {
 		X10TypeSystem xts = (X10TypeSystem) tc.typeSystem();
 		X10Context c = (X10Context) tc.context();
 
-		{
+		Expr cc = null;
+		
+		try {
 			// Check if target.name is a field or local of function type; if so, convert to a closure call.
 			X10NodeFactory nf = (X10NodeFactory) tc.nodeFactory();
 			X10TypeSystem ts = (X10TypeSystem) tc.typeSystem();
@@ -156,8 +158,8 @@ public class X10Call_c extends Call_c implements X10Call {
 			Expr e = null;
 
 			try {
-				Node n;
-				n = f.del().disambiguate(tc);
+				Node n = f;
+				n = n.del().disambiguate(tc);
 				n = n.del().typeCheck(tc);
 				n = n.del().checkConstants(tc);
 				if (n instanceof Expr && n instanceof Variable) {
@@ -171,165 +173,106 @@ public class X10Call_c extends Call_c implements X10Call {
 			}
 
 			if (e != null) {
-				ClosureCall cc = nf.ClosureCall(position(), e, typeArguments(), arguments());
+				ClosureCall ccx = nf.ClosureCall(position(), e, typeArguments(), arguments());
 				X10MethodInstance ci = (X10MethodInstance) ts.createMethodInstance(position(), new ErrorRef_c<MethodDef>(ts, position(), "Cannot get MethodDef before type-checking closure call."));
-				cc = cc.closureInstance(ci);
-				Node n = cc;
+				ccx = ccx.closureInstance(ci);
+				Node n = ccx;
 				n = n.del().disambiguate(tc);
 				n = n.del().typeCheck(tc);
-				return n;
+				cc = (Expr) n;
 			}
 		}
+		catch (SemanticException e) {
+		}
+
+		/////////////////////////////////////////////////////////////////////
+		// Inline the super call here and handle type arguments.
+		/////////////////////////////////////////////////////////////////////
+
+		List<Type> typeArgs = new ArrayList<Type>(this.typeArguments.size());
+
+		for (TypeNode tn : this.typeArguments) {
+		    typeArgs.add(tn.type());
+		}
+
+		List<Type> argTypes = new ArrayList<Type>(this.arguments.size());
+
+		for (Expr e : this.arguments) {
+		    Type et = e.type();
+		    argTypes.add(et);
+		}
+		
+
+		if (this.target == null) {
+		    try {
+		        X10Call_c n = (X10Call_c) this.typeCheckNullTarget(tc, typeArgs, argTypes);
+		        if (cc != null)
+		            throw new SemanticException("Ambiguous call; both " + n.methodInstance() + " and closure match.", position());
+		        return n;
+		    }
+		    catch (SemanticException e) {
+		        if (cc != null)
+		            return cc;
+		        throw e;
+		    }
+		}
+
+		X10Call_c n = this;
+
+		Type targetType = this.target.type();
+		Name name = this.name.id();
+		ClassDef currentClassDef = c.currentClassDef();
+
+		MethodInstance mi;
 
 		try {
-			/////////////////////////////////////////////////////////////////////
-			// Inline the super call here and handle type arguments.
-			/////////////////////////////////////////////////////////////////////
-
-			List<Type> typeArgs = new ArrayList<Type>(this.typeArguments.size());
-
-			for (TypeNode tn : this.typeArguments) {
-				typeArgs.add(tn.type());
-			}
-
-			List<Type> argTypes = new ArrayList<Type>(this.arguments.size());
-
-			for (Expr e : this.arguments) {
-				Type et = e.type();
-				argTypes.add(et);
-			}
-
-			if (this.target == null) {
-				return this.typeCheckNullTarget(tc, typeArgs, argTypes);
-			}
-
-			Type targetType = target.type();
-			Name name = this.name.id();
-			ClassDef currentClassDef = c.currentClassDef();
-
-			X10Call_c n = this;
-
-			{
-				MethodInstance mi;
-
-				try {
-					mi = xts.findMethod(targetType, 
-							xts.MethodMatcher(targetType, name, typeArgs, argTypes),
-							currentClassDef);
-				}
-				catch (SemanticException e) {
-					// Look for a method that will take a rail of a bunch of args.
-					MethodInstance vmi = null;
-					X10Call_c newCall = null;
-
-					for (int numVarArgs = 1; numVarArgs < argTypes.size(); numVarArgs++) {
-						List<Type> newArgTypes = new ArrayList<Type>();
-						List<Expr> newArgs = new ArrayList<Expr>();
-						Expr rail = null;
-						Type railBase = null;
-						List<Expr> railArgs = new ArrayList<Expr>();
-						for (int i = 0; i < argTypes.size() - numVarArgs; i++) {
-							newArgTypes.add(argTypes.get(i));
-							newArgs.add(arguments.get(i));
-						}
-						for (int i = argTypes.size() - numVarArgs; i < argTypes.size(); i++) {
-							if (railBase == null)
-								railBase = argTypes.get(i);
-							else
-								railBase = xts.leastCommonAncestor(railBase, argTypes.get(i));
-							railArgs.add(arguments.get(i));
-
-						}
-						if (railBase != null) {
-							Type railType = xts.ValRail();
-							railType = X10TypeMixin.instantiate(railType, railBase);
-							newArgTypes.add(railType);
-							rail = ((X10NodeFactory) tc.nodeFactory()).Tuple(position(), railArgs);
-							rail = rail.type(railType);
-							newArgs.add(rail);
-						}
-
-						try {
-							vmi = xts.findMethod(targetType, 
-									xts.MethodMatcher(targetType, name, typeArgs, newArgTypes),
-									currentClassDef);
-							newCall = (X10Call_c) this.arguments(newArgs);
-							break;
-						}
-						catch (SemanticException ex) {
-							continue;
-						}
-					}
-
-					if (vmi != null) {
-						mi = vmi;
-						n = newCall;
-					}
-					else
-						throw e;
-				}
-
-				/* This call is in a static context if and only if
-				 * the target (possibly implicit) is a type node.
-				 */
-				boolean staticContext = (n.target instanceof TypeNode);
-
-				if (staticContext && !mi.flags().isStatic()) {
-					throw new SemanticException("Cannot call non-static method " + name
-							+ " of " + n.target.type() + " in static "
-							+ "context.", this.position());
-				}
-
-				// If the target is super, but the method is abstract, then complain.
-				if (n.target instanceof Special && 
-						((Special) n.target).kind() == Special.SUPER &&
-						mi.flags().isAbstract()) {
-					throw new SemanticException("Cannot call an abstract method " +
-							"of the super class", this.position());            
-				}
-
-				// Copy the method instance so we can modify it.
-				X10Call_c result = (X10Call_c) n.methodInstance(mi).type(mi.returnType());
-
-				/////////////////////////////////////////////////////////////////////
-				// End inlined super call.
-				/////////////////////////////////////////////////////////////////////
-
-				// If we found a method, the call must type check, so no need to check
-				// the arguments here.
-				result.checkConsistency(c);
-				//	        	result = result.adjustMI(tc);
-				//	        	result.checkWhereClause(tc);
-				result.checkAnnotations(tc);
-
-				return result;
-			}
+		    mi = xts.findMethod(targetType, 
+		                        xts.MethodMatcher(targetType, name, typeArgs, argTypes),
+		                        currentClassDef);
 		}
-		catch (NoMemberException e) {
-//			if (e.getKind() != NoMemberException.METHOD || this.target == null)
-//			throw e;
-//			Type type = target.type();
-//			String name = nameString();
-//			List arguments = arguments();
-//			StructType java_io_PrintStream = (StructType) xts.forName("java.io.PrintStream");
-//			// FIXME: [IP] HACK: do not typecheck printf beyond the first argument
-//			if (xts.typeEquals(type, java_io_PrintStream)) {
-//			if (name.equals("printf") && arguments.size() >= 1 &&
-//			xts.isSubtype(((Expr) arguments.get(0)).type(), xts.String()))
-//			{
-//			try {
-//			MethodInstance new_mi = xts.findMethod(java_io_PrintStream,
-//			"printf",
-//			Arrays.asList(new Type[] { xts.String(), xts.arrayOf(xts.Object()) }), tc.context().currentClassDef());
-//			return (X10Call_c)this.methodInstance(new_mi).type(new_mi.returnType());
-//			} catch (NoMemberException f) {
-//			// For Java 1.4, we need to emulate this method
-//			// TODO: generate a call to x10.lang.Runtime.printf(PrintStream, String, Object[]) instead
-//			}
-//			}
-//			}
-			throw e;
+		catch (SemanticException e) {
+		    if (cc != null)
+		        return cc;
+		    throw e;
 		}
+
+		if (cc != null)
+		    throw new SemanticException("Ambiguous call; both " + mi + " and closure match.", position());
+
+		/* This call is in a static context if and only if
+		 * the target (possibly implicit) is a type node.
+		 */
+		boolean staticContext = (n.target instanceof TypeNode);
+
+		if (staticContext && !mi.flags().isStatic()) {
+		    throw new SemanticException("Cannot call non-static method " + name
+		                                + " of " + n.target.type() + " in static "
+		                                + "context.", this.position());
+		}
+
+		// If the target is super, but the method is abstract, then complain.
+		if (n.target instanceof Special && 
+		        ((Special) n.target).kind() == Special.SUPER &&
+		        mi.flags().isAbstract()) {
+		    throw new SemanticException("Cannot call an abstract method " +
+		                                "of the super class", this.position());            
+		}
+
+		// Copy the method instance so we can modify it.
+		X10Call_c result = (X10Call_c) n.methodInstance(mi).type(mi.returnType());
+
+		/////////////////////////////////////////////////////////////////////
+		// End inlined super call.
+		/////////////////////////////////////////////////////////////////////
+
+		// If we found a method, the call must type check, so no need to check
+		// the arguments here.
+		result.checkConsistency(c);
+		//	        	result = result.adjustMI(tc);
+		//	        	result.checkWhereClause(tc);
+		result.checkAnnotations(tc);
+
+		return result;
 	}
 
 	private void checkAnnotations(ContextVisitor tc) throws SemanticException {
