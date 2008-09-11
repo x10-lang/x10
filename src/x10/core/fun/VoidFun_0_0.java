2648247ef9ad1d8012e1b44bb95a77c3b30c55dd
Nathaniel Nystrom <user@domain>
2008-09-11 05:48:47 +0000

    RTT classes.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk/x10.runtime.17@6165 a326200e-df2c-42e0-81fa-597e909af41d

--- src/x10/core/fun/VoidFun_0_0.java
+++ src/x10/core/fun/VoidFun_0_0.java
@@ -1,5 +1,20 @@
 package x10.core.fun;
 
+import x10.types.RuntimeType;
+
 public interface VoidFun_0_0 {
     void apply();
+
+    public static class RTT extends RuntimeType<VoidFun_0_0>{
+        public static final RTT it = new RTT();
+
+        public RTT() {
+            super(VoidFun_0_0.class);
+        }
+
+        @Override
+        public boolean instanceof$(Object o) {
+            return o instanceof VoidFun_0_0;
+        }
+    }
 }
