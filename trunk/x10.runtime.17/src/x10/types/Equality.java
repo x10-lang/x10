183f957f241d980ba0fd4257ee464fc38ceede90
Nathaniel Nystrom <user@domain>
2008-10-16 22:21:05 +0000

    Added more equality methods to avoid ambiguous calls.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@6474 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.runtime.17/src/x10/types/Equality.java
+++ trunk/x10.runtime.17/src/x10/types/Equality.java
@@ -14,6 +14,7 @@ import x10.core.Value;
 
 public class Equality {
     public static boolean equalsequals(boolean a, boolean b) { return a == b; }
+    public static boolean equalsequals(boolean a, Object b) { return equalsequals((Object) a, b); }
     
     public static boolean equalsequals(byte a, byte b) { return a == b; }
     public static boolean equalsequals(byte a, short b) { return a == b; }
@@ -21,14 +22,24 @@ public class Equality {
     public static boolean equalsequals(byte a, long b) { return a == b; }
     public static boolean equalsequals(byte a, float b) { return a == b; }
     public static boolean equalsequals(byte a, double b) { return a == b; }
-    
+    public static boolean equalsequals(byte a, Object b) { return equalsequals((Object) a, b); }
+
     public static boolean equalsequals(short a, byte b) { return a == b; }
     public static boolean equalsequals(short a, short b) { return a == b; }
     public static boolean equalsequals(short a, int b) { return a == b; }
     public static boolean equalsequals(short a, long b) { return a == b; }
     public static boolean equalsequals(short a, float b) { return a == b; }
     public static boolean equalsequals(short a, double b) { return a == b; }
+    public static boolean equalsequals(short a, Object b) { return equalsequals((Object) a, b); }
+    
+//    public static boolean equalsequals(char a, byte b) { return a == b; }
+//    public static boolean equalsequals(char a, short b) { return a == b; }
+//    public static boolean equalsequals(char a, int b) { return a == b; }
+//    public static boolean equalsequals(char a, long b) { return a == b; }
+//    public static boolean equalsequals(char a, float b) { return a == b; }
+//    public static boolean equalsequals(char a, double b) { return a == b; }
     public static boolean equalsequals(char a, char b) { return a == b; }
+    public static boolean equalsequals(char a, Object b) { return equalsequals((Object) a, b); }
     
     public static boolean equalsequals(int a, byte b) { return a == b; }
     public static boolean equalsequals(int a, short b) { return a == b; }
@@ -36,6 +47,7 @@ public class Equality {
     public static boolean equalsequals(int a, long b) { return a == b; }
     public static boolean equalsequals(int a, float b) { return a == b; }
     public static boolean equalsequals(int a, double b) { return a == b; }
+    public static boolean equalsequals(int a, Object b) { return equalsequals((Object) a, b); }
     
     public static boolean equalsequals(long a, byte b) { return a == b; }
     public static boolean equalsequals(long a, short b) { return a == b; }
@@ -43,6 +55,7 @@ public class Equality {
     public static boolean equalsequals(long a, long b) { return a == b; }
     public static boolean equalsequals(long a, float b) { return a == b; }
     public static boolean equalsequals(long a, double b) { return a == b; }
+    public static boolean equalsequals(long a, Object b) { return equalsequals((Object) a, b); }
     
     public static boolean equalsequals(float a, byte b) { return a == b; }
     public static boolean equalsequals(float a, short b) { return a == b; }
@@ -50,6 +63,7 @@ public class Equality {
     public static boolean equalsequals(float a, long b) { return a == b; }
     public static boolean equalsequals(float a, float b) { return a == b; }
     public static boolean equalsequals(float a, double b) { return a == b; }
+    public static boolean equalsequals(float a, Object b) { return equalsequals((Object) a, b); }
     
     public static boolean equalsequals(double a, byte b) { return a == b; }
     public static boolean equalsequals(double a, short b) { return a == b; }
@@ -57,10 +71,20 @@ public class Equality {
     public static boolean equalsequals(double a, long b) { return a == b; }
     public static boolean equalsequals(double a, float b) { return a == b; }
     public static boolean equalsequals(double a, double b) { return a == b; }
+    public static boolean equalsequals(double a, Object b) { return equalsequals((Object) a, b); }
     
     public static boolean equalsequals(String a, String b) { return a.equals(b); }
     public static boolean equalsequals(Value a, Value b) { return a.structEquals(b); }
     public static boolean equalsequals(Ref a, Ref b) { return a == b; }
+
+    public static boolean equalsequals(Object a, boolean b) { return equalsequals(a, (Object) b); }
+    public static boolean equalsequals(Object a, byte b) { return equalsequals(a, (Object) b); }
+    public static boolean equalsequals(Object a, short b) { return equalsequals(a, (Object) b); }
+    public static boolean equalsequals(Object a, char b) { return equalsequals(a, (Object) b); }
+    public static boolean equalsequals(Object a, int b) { return equalsequals(a, (Object) b); }
+    public static boolean equalsequals(Object a, float b) { return equalsequals(a, (Object) b); }
+    public static boolean equalsequals(Object a, long b) { return equalsequals(a, (Object) b); }
+    public static boolean equalsequals(Object a, double b) { return equalsequals(a, (Object) b); }
 
     public static boolean equalsequals(Object a, Object b) {
         if (a == b) return true;
