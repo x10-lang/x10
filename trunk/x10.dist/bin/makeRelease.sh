00c2e53e3343b6533a3a94e8fb385e4a84f18fd0
Nathaniel Nystrom <user@domain>
2008-10-31 21:54:21 +0000

    Updated to check out polyglot and build the release zips.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@6594 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.dist/bin/makeRelease.sh
+++ trunk/x10.dist/bin/makeRelease.sh
@@ -3,6 +3,7 @@
 # Dave Grove
 
 set -e
+set -x
 
 if (( $# == 0 )) || [ x$1 = x-h -o x$1 = x--help ] ; then
   echo usage: $0 symbolic_revision
@@ -17,39 +18,68 @@ workdir=$HOME/scratch/distribution
 distdir=$workdir/x10-$revision
 tarfile=x10-$revision.tar.gz
 tarfile2=x10-$revision.tar.bz2
-sed=/bin/sed
 
 echo
 echo cleaning $workdir
-mkdir -p $workdir
-cd $workdir
-rm -rf *
+rm -rf $workdir
+mkdir -p $workdir || exit 1
+mkdir -p $workdir/x10-$revision
 
-cd $workdir
+(
+echo
+echo getting polyglot
+cd $workdir/x10-$revision
+cvs -d:pserver:anonymous@gforge.cis.cornell.edu:/cvsroot/polyglot co polyglot3-dev
+)
+
+(
+cd $workdir/x10-$revision
 
 echo
 echo getting distrib
-svn export https://x10.svn.sourceforge.net/svnroot/x10/tags/$revision x10-$revision 
+for i in \
+	x10.common.17 \
+	x10.compiler.p3 \
+	x10.constraints \
+	x10.dist \
+	x10.runtime.17 \
+	x10.tests
+do
+    svn export https://x10.svn.sourceforge.net/svnroot/x10/tags/$revision/$i
+done
+)
 
-#####
-# x10 build goes here
-#####
-
-
+(
 echo
-echo building $tarfile file
-cd $distdir
-chmod -Rf u+w *
-ls -l
-cd ..
-tar zcf $tarfile $(basename $distdir)
+echo building polyglot
+cd $workdir/x10-$revision/polyglot3-dev
+ant eclipse-plugin-jar
+)
 
-echo building $tarfile2 file
-tar jcf $tarfile2 $(basename $distdir)
+(
+echo
+echo building x10
+cd $workdir/x10-$revision/x10.dist
+LOCAL_POLYGLOT_JAR=$workdir/x10-$revision/polyglot3-dev/lib/polyglot-epl.jar
+export LOCAL_POLYGLOT_JAR
+ant zips
+)
 
-echo
-date
-echo
-echo done.
+
+#echo
+#echo building $tarfile file
+#cd $distdir
+#chmod -Rf u+w *
+#ls -l
+#cd ..
+#tar zcf $tarfile $(basename $distdir)
+#
+#echo building $tarfile2 file
+#tar jcf $tarfile2 $(basename $distdir)
+#
+#echo
+#date
+#echo
+#echo done.
 
 echo "The distribution ($tarfile) is now in the directory $workdir"
