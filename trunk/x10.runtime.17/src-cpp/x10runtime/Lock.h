475e4bd40f074cc15e0e877ee8ecc258feb42b32
David Grove <groved@us.ibm.com>
2008-11-01 00:13:15 +0000

    Checkin code that Sreedhar Kodali wrote in case anyone is waiting on it.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@6596 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ trunk/x10.runtime.17/src-cpp/x10runtime/Lock.h
@@ -0,0 +1,44 @@
+/*
+ * (c) Copyright IBM Corporation 2008
+ *
+ * $Id$
+ * This file is part of XRX Kernel implementation in C++.
+ */
+
+/** Low level lock interface. **/
+
+#ifndef __XRX_KERNEL_LOCK_H
+#define __XRX_KERNEL_LOCK_H
+
+#include <pthread.h>
+
+namespace xrx_kernel {
+
+class Lock {
+public:
+
+	// constructors
+	Lock(void);
+
+	// destructor
+	~Lock(void);
+
+	// acquire lock blockingly
+	void lock(void);
+
+	// release lock
+	void unlock(void);
+
+	// acquire lock (non-blocking)
+	bool tryLock(void);
+
+protected:
+	pthread_mutex_t __lock_mutex;
+	// do we really need a private copy??
+	// (perhaps to change lock behavior dynamically)
+	pthread_mutexattr_t __lock_mutexattr;
+};
+
+} /* closing brace for namespace xrx_kernel */
+
+#endif /* __XRX_KERNEL_LOCK_H */
