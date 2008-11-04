b8a32c2feaf15dca3c6be4fa8573e9dcb0368f89
Sreedhar Kodali <srkodali@in.ibm.com>
2008-11-04 15:09:42 +0000

    Need changes in various method interfaces in tune with C++ requirements.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk/x10.runtime.17@6622 a326200e-df2c-42e0-81fa-597e909af41d

--- src-cpp/x10runtime/Thread.h
+++ src-cpp/x10runtime/Thread.h
@@ -5,21 +5,29 @@
  * This file is part of XRX Kernel implementation in C++.
  */
 
-/** Low level worker interface based on pthreads. **/
+/************************************************************
+ * A thread is a unit of execution in a place.
+ * In a given place, we can have multiple threads of execution.
+ *
+ * A union of a subset of the functionality of java.lang.Thread
+ * augmented with the park/unpark API of java.util.concurrent.
+ * locks.LockSupport.
+ ************************************************************/
 
 #ifndef __XRX_KERNEL_THREAD_H
 #define __XRX_KERNEL_THREAD_H
 
 #include "Types.h"
+#include "Object.h"
 #include "Runnable.h"
 #include "InterruptedException.h"
 
 namespace xrx_kernel {
 
-class Thread {
+class Thread : public Runnable {
 public:
 	
-	// constructors
+	// [constructors] Allocates a new Thread object.
 	Thread(void);
 	Thread(Runnable task);
 	Thread(Runnable task, String name);
@@ -27,36 +35,125 @@ public:
 	// destructor
 	~Thread(void);
 
-	// return the currently executing thread
+	// Returns a reference to the currently executing thread object.
 	static Thread currentThread(void);
 
-	// begin thread execution
+	/**
+	 * Causes this thread to begin execution; the XRX runtime
+	 * calls the run method of this thread.
+	 * The result is that two threads are running concurrently:
+	 * the current thread (which returns from the call to the
+	 * start method) and the other thread (which executes its
+	 * run method).
+	 * Throws IllegalThreadStateException, if the thread was
+	 * already started.
+	 */
 	void start(void);
 
-	// put the current thread to sleep for the specified number
-	// of milli seconds
-	void sleep(Long millis) throw (InterruptedException);
+	/**
+	 * If this thread was constructed using a separate Runnable
+	 * run object, then the Runnable objects' run method is called;
+	 * otherwise, this method does nothing and returns.
+	 */
+	void run(void);
 
-	// put the current thread to sleep for the specified number
-	// of milli seconds plus the specified number of nano seconds
-	void sleep(Long millis, Int nanos) throw (InterruptedException);
+	/**
+	 * Causes the currently executing thread to sleep (cease
+	 * execution) for the specified number of milliseconds.
+	 * The thread does not lose ownership of any monitors.
+	 * Throws InterruptedException, if any thread has interrupted
+	 * the current thread.
+	 */
+	static void sleep(Long millis) throw (InterruptedException);
 
-	// disable the current thread for thread scheduling purposes
-	void park(void);
+	/**
+	 * Causes the currently executing thread to sleep (cease
+	 * execution) for the specified number of milliseconds plus
+	 * the specified number of nanoseconds.
+	 * The thread does not lose ownership of any monitors.
+	 * Throws InterruptedException, if any thread has interrupted
+	 * the current thread.
+	 */
+	static void sleep(Long millis, Int nanos) throw (InterruptedException);
+	/**
+	 * Disables the current thread for thread scheduling purposes
+	 * unless the permit is available.
+	 * If the permit is available then it is consumed and the call
+	 * returns immediately; otherwise the current thread becomes
+	 * disabled for thread scheduling purposes and lies dormant
+	 * until one of three things happen:
+	 * 1) Some other thread invokes unpark with the current thread
+	 * as the target; or
+	 * 2) Some other thread interrupts the current thread; or
+	 * 3) The call spuriously (ie, for no reason) returns.
+	 * This method does not report which of these caused the method
+	 * to return.  Callers should re-check the conditions which
+	 * caused the thread to park in the first place.
+	 */
+	static void park(void);
 
-	// disable the current thread for thread scheduling purposes
-	// for up to the specified waiting time
-	void parkNanos(Long nanos);
+	/**
+	 * Disables the current thread for thread scheduling purposes,
+	 * for up to the specified waiting time, unless the permit is
+	 * available.
+	 * If the permit is available then it is consumed and the call
+	 * returns immediately; otherwise the current thread becomes
+	 * disabled for thread scheduling purposes and lies dormant
+	 * until one of four things happen:
+	 * 1) Some other thread invokes unpark with the current thread
+	 * as the target; or
+	 * 2) Some other thread interrupts the current thread; or
+	 * 3) The specified waiting time elapses; or
+	 * 4) The call spuriously (ie, for no reason) returns.
+	 * This method does not report which of these caused the method
+	 * to return.  Callers should re-check the conditions which
+	 * caused the thread to park in the first place.
+	 */
+	static void parkNanos(Long nanos);
 
-	// make available the current thread for thread scheduling
-	void unpark(Thread thread);
+	/**
+	 * Makes available the permit for the given thread, if it was
+	 * not already available.  If the thread was blocked on park
+	 * then it will unblock.  Otherwise, its next call to park is
+	 * guaranteed not to block.  This operation is not guaranteed
+	 * to have any effect at all if the given thread has not been
+	 * started.
+	 */
+	static void unpark(Thread thread);
 
-protected:
+	// Returns the current activity.
+	Object activity(void);
+
+	// Set the current activity.
+	void activity(Object activity);
+
+	// Returns the current place.
+	Object place(void);
+
+	// Set the current place.
+	void place(Object place);
+
+	// Returns this thread's name.
+	String getName(void);
+
+	// Changes the name of this thread to be equal to the argument name.
+	void setName(String name);
+
+private:
+	// the current place
+	static Object __current_place;
+	// the current activity
+	static Object __current_activity;
+	// the current thread
+	static Thread __current_thread;
+	// thread id
+	pthread_t __threadid;
+	// thread name
+	String __thread_name;
+	// thread attributes
 	pthread_attr_t __thread_attr;
+	// thread runnable object
 	Runnable __thread_runobj;
-	static Thread &__current_thread;
-	// ?? more state as per x10 thread pool executor
-
 };
 
 } /* closing brace for namespace xrx_kernel */
