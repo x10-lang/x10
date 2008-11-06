dfd8c0824877f5496288eec736de4ac08935d010
Nathaniel Nystrom <user@domain>
2008-11-06 05:19:13 +0000

    Move CompoundStmt interface from Async_c to Async
    git-svn-id: svn://svn.code.sf.net/p/x10/code@6654 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.compiler.p3/src/polyglot/ext/x10/ast/Async.java
+++ trunk/x10.compiler.p3/src/polyglot/ext/x10/ast/Async.java
@@ -12,13 +12,14 @@ package polyglot.ext.x10.ast;
 
 import java.util.List;
 
+import polyglot.ast.CompoundStmt;
 import polyglot.ast.Expr;
 import polyglot.ast.Stmt;
 
 /** The node constructed for the X10 construct async (P) {S}.
  * @author Christian Grothoff
  */
-public interface Async extends Stmt, RemoteActivityInvocation, Clocked {
+public interface Async extends CompoundStmt, RemoteActivityInvocation, Clocked {
     
     /** Set the Async's body */
     Async body(Stmt body);
