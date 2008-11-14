c5cfd612512b47c653c25cebfdb85602b2a6c92d
David Grove <groved@us.ibm.com>
2008-11-14 22:54:50 +0000

    specialized ref constructor for const strings
    git-svn-id: svn://svn.code.sf.net/p/x10/code@6783 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ trunk/x10.runtime.17/src-cpp/x10aux/ref.cc
@@ -0,0 +1,11 @@
+#include <x10aux/config.h>
+
+#include <x10aux/ref.h>
+#include <x10/lang/String.h>
+
+template<> x10aux::ref<x10::lang::String>::ref(const x10::lang::String& val)
+    : REF_INIT(new (x10aux::alloc<x10::lang::String>()) x10::lang::String(val))
+{
+    _R_("Copying '" << val << "' (" << (void*)&val << ") to " << this);
+    INC(_val);
+}
