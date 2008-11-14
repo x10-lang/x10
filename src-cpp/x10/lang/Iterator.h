3bff191abd79b62d524cd59ceae6035ab577aae3
David Cunningham <dcunnin@us.ibm.com>
2008-11-14 16:47:04 +0000

    forgot this file
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk/x10.runtime.17@6772 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ src-cpp/x10/lang/Iterator.h
@@ -0,0 +1,62 @@
+#ifndef X10_LANG_ITERATOR_H
+#define X10_LANG_ITERATOR_H
+
+#include <sstream>
+
+
+#include <x10aux/config.h>
+#include <x10aux/RTT.h>
+
+#include <x10/lang/Object.h>
+
+
+
+namespace x10 {
+
+    namespace lang {
+
+        template<class T> class Iterator : public x10::lang::Object {
+
+            public:
+
+            class RTT : public x10aux::RuntimeType {
+                public:
+                static const RTT* it;
+
+                virtual std::string name() const {
+                    std::stringstream ss;
+                    ss<<"x10.lang.Iterator["<<x10aux::getRTT<T>()->name()<<"]";
+                    return ss.str();
+                }
+                 
+            };
+
+            virtual const x10aux::RuntimeType *_type() const {
+                return x10aux::getRTT<Iterator<T> >();
+            }
+
+            Iterator() { }
+
+            private:
+
+            Iterator(const Iterator<T>& arr); // disabled
+
+            public:
+
+            ~Iterator() { }
+
+            virtual x10_boolean hasNext() = 0;
+
+            virtual T next() = 0;
+
+        };
+
+        template<class T>
+            const typename Iterator<T>::RTT *Iterator<T>::RTT::it =
+                new typename Iterator<T>::RTT();
+
+    }
+}
+
+
+#endif
