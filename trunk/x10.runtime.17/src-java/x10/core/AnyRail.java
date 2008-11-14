91d2570bcf40b31c950867c1bd49cdf3a891ebc1
Olivier Tardieu <tardieu@us.ibm.com>
2008-11-14 04:56:37 +0000

    Rail extends Ref
    git-svn-id: svn://svn.code.sf.net/p/x10/code@6765 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.runtime.17/src-java/x10/core/AnyRail.java
+++ trunk/x10.runtime.17/src-java/x10/core/AnyRail.java
@@ -12,125 +12,33 @@ import x10.core.Iterator;
 import x10.core.Iterable;
 import x10.core.fun.Fun_0_1;
 import x10.types.Type;
-import x10.types.Types;
 
-public class AnyRail<T> extends Value implements Indexable<Integer,T>, Fun_0_1<Integer,T>, Iterable<T> {
-    public final int length;
-    
-    public final Object value;
-    public final Type<T> type;
-    
-    protected AnyRail(Type<T> type, int length) {
-        this(type, length, type.makeArray(length));
-    }
-    
-    protected AnyRail(Type<T> type, int length, Object array) {
-        this.length = length;
-        this.type = type;
-        this.value = array;
-    }
-    
-	public Iterator<T> iterator() {
-		return new RailIterator();
-	}
+public interface AnyRail<T> extends Indexable<Integer,T>, Fun_0_1<Integer,T>, Iterable<T> {
+	public Iterator<T> iterator();
 
-	protected class RailIterator implements Iterator<T> {
-		int i = 0;
-
-		public boolean hasNext() {
-			return i < length;
-		}
-
-		public T next() {
-			return apply(i++);
-		}
-	}
-
-    public Type<?> rtt_x10$lang$Fun_0_1_Z1() { return Types.INT; }
-    public Type<?> rtt_x10$lang$Fun_0_1_U()  { return type; }
+    public Type<?> rtt_x10$lang$Fun_0_1_Z1();
+    public Type<?> rtt_x10$lang$Fun_0_1_U();
     
     // Methods to get the backing array.   May be called by generated code.
-    public Object getBackingArray() { return value; }
+    public Object getBackingArray();
     
-    public boolean[] getBooleanArray() { return (boolean[]) value; }
-    public byte[] getByteArray() { return (byte[]) value; }
-    public short[] getShortArray() { return (short[]) value; }
-    public char[] getCharArray() { return (char[]) value; }
-    public int[] getIntArray() { return (int[]) value; }
-    public long[] getLongArray() { return (long[]) value; }
-    public float[] getFloatArray() { return (float[]) value; }
-    public double[] getDoubleArray() { return (double[]) value; }
-    public Object[] getObjectArray() { return (Object[]) value; }
+    public boolean[] getBooleanArray();
+    public byte[] getByteArray();
+    public short[] getShortArray();
+    public char[] getCharArray();
+    public int[] getIntArray();
+    public long[] getLongArray();
+    public float[] getFloatArray();
+    public double[] getDoubleArray();
+    public Object[] getObjectArray();
     
-    public Object[] getBoxedArray() {
-    	if (value instanceof boolean[]) {
-    		boolean[] a = (boolean[]) value;
-    		Boolean[] b = new Boolean[a.length];
-    		for (int i = 0; i < a.length; i++) b[i] = a[i];
-    	}
-    	if (value instanceof byte[]) {
-    		byte[] a = (byte[]) value;
-    		Byte[] b = new Byte[a.length];
-    		for (int i = 0; i < a.length; i++) b[i] = a[i];
-    	}
-    	if (value instanceof char[]) {
-    		char[] a = (char[]) value;
-    		Character[] b = new Character[a.length];
-    		for (int i = 0; i < a.length; i++) b[i] = a[i];
-    	}
-    	if (value instanceof short[]) {
-    		short[] a = (short[]) value;
-    		Short[] b = new Short[a.length];
-    		for (int i = 0; i < a.length; i++) b[i] = a[i];
-    	}
-    	if (value instanceof int[]) {
-    		int[] a = (int[]) value;
-    		Integer[] b = new Integer[a.length];
-    		for (int i = 0; i < a.length; i++) b[i] = a[i];
-    	}
-    	if (value instanceof long[]) {
-    		long[] a = (long[]) value;
-    		Long[] b = new Long[a.length];
-    		for (int i = 0; i < a.length; i++) b[i] = a[i];
-    	}
-    	if (value instanceof float[]) {
-    		float[] a = (float[]) value;
-    		Float[] b = new Float[a.length];
-    		for (int i = 0; i < a.length; i++) b[i] = a[i];
-    	}
-    	if (value instanceof double[]) {
-    		double[] a = (double[]) value;
-    		Double[] b = new Double[a.length];
-    		for (int i = 0; i < a.length; i++) b[i] = a[i];
-    	}
-    	return (Object[]) value;
-    }
+    public Object[] getBoxedArray();
 
-    public Integer length() {
-    	return length;
-    }
+    public Integer length();
     
-    public T get(Integer i) {
-    	return apply(i);
-    }
+    public T get(Integer i);
+
+    public T apply(Integer i);
     
-    public T apply(Integer i) {
-    	return type.getArray(value, i);
-    }
-    
-    protected T set$(T v, Integer i) {
-    	return type.setArray(value, i, v);
-    }
-    
-    public boolean isZero() {
-    	boolean zero = true;
-		for (int i = 0; i < length && zero; ++i) {
-			zero &= get(i) == type.zeroValue();
-		}
-		return zero;
-    }
-    
-    public String toString() {
-    	return value.toString();
-    }
+    public boolean isZero();
 }
