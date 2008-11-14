e0640e7efc55f43be47267006a89d4887a47ccec
David Grove <groved@us.ibm.com>
2008-11-14 22:57:47 +0000

    include int_utils.h
    git-svn-id: svn://svn.code.sf.net/p/x10/code@6786 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.runtime.17/src-cpp/x10rt17.h
+++ trunk/x10.runtime.17/src-cpp/x10rt17.h
@@ -9,6 +9,7 @@
 #include <x10aux/alloc.h>
 #include <x10aux/serialization.h>
 #include <x10aux/RTT.h>
+#include <x10aux/int_utils.h>
 #include <x10aux/string_utils.h>
 #include <x10aux/rail_utils.h>
 #include <x10aux/place.h>
