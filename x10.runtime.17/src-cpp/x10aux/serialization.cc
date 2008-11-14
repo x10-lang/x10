8a83612857dcda24c14777b4367ed207ac6fa69b
Igor Peshansky <igorp@us.ibm.com>
2008-11-14 20:50:42 +0000

    Add missing serialization buffer field Make Object::equals() pure Add Value::_serialize_fields()
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@6776 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ x10.runtime.17/src-cpp/x10aux/serialization.cc
@@ -0,0 +1,10 @@
+#include <x10aux/config.h>
+
+#include <x10aux/serialization.h>
+
+//////////////////////////////////////////////////////////////
+// x10::serialization_buffer implementation
+//////////////////////////////////////////////////////////////
+
+const double x10aux::serialization_buffer::FACTOR = 2.0;
+
