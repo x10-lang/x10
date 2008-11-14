d0f6788bc67d2e4e08b954585b21efd71a7464f0
David Cunningham <dcunnin@us.ibm.com>
2008-11-14 19:34:47 +0000

    bugfixes, new Fun_0_0 file, code cleanups
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@6774 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ x10.runtime.17/src-cpp/x10/lang/Fun_0_0.h
@@ -0,0 +1,14 @@
+#ifndef X10_LANG_FUN_0_0_H
+#define X10_LANG_FUN_0_0_H
+
+#include <x10aux/config.h>
+
+namespace x10 {
+    namespace lang {
+        template<class R> class Fun_0_0 {
+            virtual ~Fun_0_0() { }
+            virtual R apply() = 0;
+        };
+    }
+}
+#endif
