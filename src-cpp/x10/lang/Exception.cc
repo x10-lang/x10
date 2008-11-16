5bd030df064dc924f5152b6f55ee483fa9a6efd6
David Cunningham <dcunnin@us.ibm.com>
2008-11-16 08:36:48 +0000

    some to_string linker fixes
    Exceptions and Throwable reworked to have more features
    
    updated Makefile to provide a slightly more helpful error if pgas not found
    
    
    
    
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk/x10.runtime.17@6809 a326200e-df2c-42e0-81fa-597e909af41d

--- src-cpp/x10/lang/Exception.cc
+++ src-cpp/x10/lang/Exception.cc
@@ -2,5 +2,39 @@
 
 #include <x10/lang/Exception.h>
 
-const x10::lang::Exception::RTT * const x10::lang::Exception::RTT::it =
-    new x10::lang::Exception::RTT();
+using namespace x10::lang;
+using namespace x10aux;
+
+Exception::Exception() : Throwable() {
+}
+    
+    
+//#line 17 "/home/spark/x10-cvs/x10.dist/lib/x10.jar:x10/lang/Exception.x10"
+Exception::Exception(const ref<String> &message) : Throwable(message) {   
+}
+    
+    
+//#line 18 "/home/spark/x10-cvs/x10.dist/lib/x10.jar:x10/lang/Exception.x10"
+Exception::Exception(const ref<String> &message, const ref<Throwable> &cause)
+  : Throwable(message, cause) {
+} 
+   
+
+//#line 19 "/home/spark/x10-cvs/x10.dist/lib/x10.jar:x10/lang/Exception.x10"
+Exception::Exception(const ref<Throwable> &cause) : Throwable(cause) {
+}
+
+
+
+#if 0
+void Exception::_serialize_fields(x10aux::serialization_buffer& buf, x10aux::addr_map& m) {
+    Throwable::_serialize_fields(buf, m);
+    
+}
+
+void Exception::_deserialize_fields(x10aux::serialization_buffer& buf) {
+    
+}
+#endif
+
+const Exception::RTT * const Exception::RTT::it = new Exception::RTT();
--- src-cpp/x10/lang/Exception.h
+++ src-cpp/x10/lang/Exception.h
@@ -4,13 +4,13 @@
 #include <x10aux/config.h>
 #include <x10aux/RTT.h>
 
-#include <x10/lang/Value.h>
+#include <x10/lang/Throwable.h>
 
 namespace x10 {
 
     namespace lang {
 
-        class Exception : public Value {
+        class Exception : public Throwable {
         public:
             class RTT : public x10aux::RuntimeType { 
                 public:
@@ -27,6 +27,23 @@ namespace x10 {
             virtual const x10aux::RuntimeType *_type() const {
                 return x10aux::getRTT<Exception>();
             }
+
+            Exception();
+            Exception(const x10aux::ref<x10::lang::String> &message);
+            Exception(const x10aux::ref<x10::lang::String> &message,
+                      const x10aux::ref<Throwable> &cause);
+            Exception(const x10aux::ref<Throwable> &cause);
+            
+#if 0
+TODO: 
+            // Serialization
+            public: static const int SERIALIZATION_ID = 16;
+            public: explicit Exception(x10aux::SERIALIZATION_MARKER m) : Throwable(m){ (void) m; }
+            public: virtual void _serialize(x10aux::serialization_buffer& buf, x10aux::addr_map& m) { x10aux::_serialize_ref(this, buf, m); }
+            public: virtual void _serialize_fields(x10aux::serialization_buffer& buf, x10aux::addr_map& m);
+            public: virtual void _deserialize_fields(x10aux::serialization_buffer& buf);
+#endif
+
 
         };
 
