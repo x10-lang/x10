edadaa368483a351efb429c7735f347076533319
David Cunningham <dcunnin@us.ibm.com>
2008-11-16 08:36:48 +0000

    some to_string linker fixes
    Exceptions and Throwable reworked to have more features
    
    updated Makefile to provide a slightly more helpful error if pgas not found
    
    
    
    
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@6809 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.runtime.17/src-cpp/x10/lang/Throwable.h
+++ x10.runtime.17/src-cpp/x10/lang/Throwable.h
@@ -4,14 +4,22 @@
 #include <x10aux/config.h>
 #include <x10aux/RTT.h>
 
-#include <x10/lang/Object.h>
+#include <x10/lang/Value.h>
 
 namespace x10 {
 
+    namespace io {
+        class Printer;
+    }
+
     namespace lang {
 
-        class Throwable : public Object {
-        public:
+        class String;
+
+        class Throwable : public Value {
+
+            public:
+
             class RTT : public x10aux::RuntimeType { 
                 public:
                 static const RTT* const it; 
@@ -28,7 +36,34 @@ namespace x10 {
                 return x10aux::getRTT<Throwable>();
             }
 
+            x10aux::ref<Throwable> FMGL(cause);
+            x10aux::ref<String> FMGL(message);
+
+            Throwable();
+            Throwable(const x10aux::ref<String> &message);
+            Throwable(const x10aux::ref<Throwable> &cause);
+            Throwable(const x10aux::ref<String> &message,
+                      const x10aux::ref<Throwable> &cause);
+
+            virtual x10aux::ref<String> getMessage();
+            virtual x10aux::ref<Throwable> getCause();
+            virtual x10aux::ref<String> toString();
+            virtual void printStackTrace();
+            virtual void printStackTrace(x10aux::ref<x10::io::Printer> p);
+
+            /* TODO: don't care about this just yet
+            // Serialization
+            static const int SERIALIZATION_ID = 17;
+            explicit Throwable(x10aux::SERIALIZATION_MARKER m) : Value(m){
+                (void) m;
+            }
+            virtual void _serialize(x10aux::serialization_buffer& buf, x10aux::addr_map& m) { x10aux::_serialize_ref(this, buf, m); }
+            virtual void _serialize_fields(x10aux::serialization_buffer& buf, x10aux::addr_map& m);
+            virtual void _deserialize_fields(x10aux::serialization_buffer& buf);
+            */
+
         };
+
 
     }
 }
