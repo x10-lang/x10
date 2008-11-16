fa709e6aec21cc6abde2e626c841e844da77e4d5
David Cunningham <dcunnin@us.ibm.com>
2008-11-16 07:24:38 +0000

    RTTs for Short and Char
    more string functionality
    
    note the generalised x10aux::to_string and x10aux::from_string functions
    
    If stringstream turns out to be too bloated we can use Igor's lightweight
    c-like implementations.  Either way, the API can remain the same.  This means
    we can start using these functions everywhere without worrying about whether
    their implementation is bloated.
    
    
    
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@6808 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.runtime.17/src-cpp/x10/lang/String.h
+++ x10.runtime.17/src-cpp/x10/lang/String.h
@@ -30,18 +30,6 @@ namespace x10 {
 
             };
 
-/*
-            static std::string to_string(const x10_boolean b);
-            static std::string to_string(const x10_char c);
-            static std::string to_string(const x10_int i);
-            static std::string to_string(const x10_long i);
-            static std::string to_string(const x10_double i);
-            //static std::string to_string(const place& p);
-            static std::string to_string(const x10aux::ref<Rail<x10_char> >& value);
-            static std::string to_string(const x10aux::ref<Rail<x10_char> >& value, x10_int offset, x10_int count);
-            static const std::string to_string(const x10aux::ref<String>& s);
-//            static const string to_string(const x10aux::ref<Object>& o);
-//            */
         protected:
             virtual const x10aux::RuntimeType *_type() const {
                 return x10aux::getRTT<String>();
@@ -51,19 +39,6 @@ namespace x10 {
             String() : Value(), std::string("") { }
             String(const std::string& content) : Value(), std::string(content){}
             String(const char* s) : Value(), std::string(s) { }
-/*
-            String(const x10_boolean b) : Object(), string(to_string(b)) { }
-            String(const x10_int i) : Value(), string(to_string(i)) { }
-            String(const x10_char c) : Value(), string(to_string(c)) { }
-            String(const x10_short s) : Value(), string(to_string((x10_int)s)) { }
-            String(const x10_long i) : Value(), string(to_string(i)) { }
-            String(const x10_float f) : Value(), string(to_string((x10_double)f)) { }
-            String(const x10_double i) : Value(), string(to_string(i)) { }
-            //String(const place& p) : Value(), string(to_string(p)) { }
-            String(const x10aux::ref<Rail<x10_char> >& value) : Value(), string(to_string(value)) { }
-            String(const x10aux::ref<Rail<x10_char> >& value, x10_int offset, x10_int count) : Value(), string(to_string(value, offset, count)) { }
-            String(const String& s) : Value(), string(dynamic_cast<const string&>(s)) { }
-*/
             explicit String(const x10aux::ref<String>& s)
               : Value(), std::string(*static_cast<std::string*>(&*s)) { }
 /*
@@ -79,16 +54,20 @@ namespace x10 {
 
             x10_boolean equals(const x10aux::ref<Object> &s);
 
-            x10_int length() const { return (x10_int) std::string::length(); }
-            //String operator+(const String& s) const;
+            x10_int length() { return (x10_int) std::string::length(); }
+            String operator+(const String& s);
             x10_int indexOf(const x10aux::ref<String>& s, x10_int i = 0);
             x10_int indexOf(x10_char c, x10_int i = 0);
             x10_int lastIndexOf(const x10aux::ref<String>& s, x10_int i = 0);
             x10_int lastIndexOf(x10_char c, x10_int i = 0);
-            //String substring(x10_int start, x10_int end) const;
-            //String substring(x10_int start) const { return substring(start, this->length()); }
-            //x10_char charAt(x10_int i) const;
-            //x10aux::ref<Rail<x10_char> > toCharArray() const;
+            String substring(x10_int start, x10_int end);
+            String substring(x10_int start) {
+                return substring(start, this->length());
+            }
+
+            x10_char charAt(x10_int i);
+
+            x10aux::ref<Rail<x10_char> > toCharRail(void);
 
             //friend Rail<x10aux::ref<String> >* x10::convert_args(int ac, char **av);
             //friend void x10::free_args(Rail<x10aux::ref<String> > *arr);
--- x10.runtime.17/src-cpp/x10aux/string_utils.h
+++ x10.runtime.17/src-cpp/x10aux/string_utils.h
@@ -16,7 +16,21 @@ namespace x10aux {
 
     void free_args(const ref<x10::lang::Rail<ref<x10::lang::String> > > &arr);
 
+    // these deliberately do not use the x10_ primitive typedefs, because their
+    // implementations are tailored for the c types.  
+    // TODO: perhaps only bool, char, int64_t, and const char* are necessary?
+    ref<x10::lang::String> to_string(bool v);
+    ref<x10::lang::String> to_string(char v);
+    ref<x10::lang::String> to_string(short v);
+    ref<x10::lang::String> to_string(int v);
+    ref<x10::lang::String> to_string(int64_t v);
+    ref<x10::lang::String> to_string(float v);
+    ref<x10::lang::String> to_string(double v);
+    ref<x10::lang::String> to_string(const char *v);
+
+    template<class T> T from_string(const ref<x10::lang::String> &s);
 }
+
     
 
 #endif
