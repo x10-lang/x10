a084953a407917519eb76690c4d1fb66d91b7575
David Grove <groved@us.ibm.com>
2008-11-18 21:07:34 +0000

    Stub implementation of native methods for the rest of the primitive types.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@6840 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ trunk/x10.runtime.17/src-cpp/x10aux/double_utils.h
@@ -0,0 +1,27 @@
+#ifndef X10AUX_DOUBLE_UTILS_H
+#define X10AUX_DOUBLE_UTILS_H
+
+#include <x10aux/config.h>
+#include <x10aux/ref.h>
+
+namespace x10 {
+    namespace lang {
+        class String;
+    }
+}
+
+namespace x10aux {
+	class double_utils {
+	public:
+		static const ref<x10::lang::String> toHexString(x10_double value);
+		static const ref<x10::lang::String> toString(x10_double value);
+		static x10_double parseDouble(const ref<x10::lang::String>& s);
+		static x10_boolean isNaN(x10_double value);
+		static x10_boolean isInfinite(x10_double value);
+		static x10_long toLongBits(x10_double value);
+		static x10_long toRawLongBits(x10_double value);
+		static x10_double fromLongBits(x10_long value);
+	};
+}
+
+#endif
