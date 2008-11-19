213ec14d49548ffc530a9672482144be98a585cd
David Grove <groved@us.ibm.com>
2008-11-19 21:52:06 +0000

    Better way to express r6855.  Thanks Igor.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk/x10.runtime.17@6861 a326200e-df2c-42e0-81fa-597e909af41d

--- src-cpp/x10aux/math_utils.h
+++ src-cpp/x10aux/math_utils.h
@@ -6,11 +6,11 @@
 namespace x10aux {
     class math_utils {
     public:
-        static x10_double pow_mu(x10_double x, x10_double y);
-        static x10_double exp_mu(x10_double x);
-        static x10_double cos_mu(x10_double x);
-        static x10_double sin_mu(x10_double x);
-        static x10_double sqrt_mu(x10_double x);
+        static x10_double pow(x10_double x, x10_double y);
+        static x10_double exp(x10_double x);
+        static x10_double cos(x10_double x);
+        static x10_double sin(x10_double x);
+        static x10_double sqrt(x10_double x);
     };
 }
 
