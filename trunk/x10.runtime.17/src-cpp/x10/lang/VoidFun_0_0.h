5463ad9f5415db555fea338305f9a211b844ef4f
David Cunningham <dcunnin@us.ibm.com>
2008-11-21 20:21:26 +0000

    Added RTT support to VoidFun_0_0
    git-svn-id: svn://svn.code.sf.net/p/x10/code@6920 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.runtime.17/src-cpp/x10/lang/VoidFun_0_0.h
+++ trunk/x10.runtime.17/src-cpp/x10/lang/VoidFun_0_0.h
@@ -2,11 +2,22 @@
 #define X10_LANG_VOIDFUN_0_0_H
 
 #include <x10aux/config.h>
+#include <x10aux/RTT.h>
+#include <x10/lang/Value.h>
 
 namespace x10 {
     namespace lang {
-        class VoidFun_0_0 {
+        class VoidFun_0_0 : public Value {
         public:
+            class RTT : public x10aux::RuntimeType {
+                public:
+                static RTT * const it;
+                virtual void init() { initParents(1,x10aux::getRTT<Value>()); }
+                virtual std::string name() const { return "x10.lang.VoidFun"; }
+            };
+            virtual const x10aux::RuntimeType *_type() const {
+                return x10aux::getRTT<VoidFun_0_0>();
+            }
             virtual ~VoidFun_0_0() { }
             virtual void apply() = 0;
         };
