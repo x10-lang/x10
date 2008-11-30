9669d64057ae857f340d2cf1eeac3de38e5ee469
David Cunningham <dcunnin@us.ibm.com>
2008-11-30 09:08:07 +0000

    Add support for counting total number of serialised bytes
    git-svn-id: svn://svn.code.sf.net/p/x10/code@7086 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.runtime.17/src-cpp/x10aux/pgas.h
+++ trunk/x10.runtime.17/src-cpp/x10aux/pgas.h
@@ -71,6 +71,9 @@ namespace x10aux {
         _X_("PGAS shutdown complete");
     }
 
+    extern x10_long serialized_bytes;
+    extern x10_long deserialized_bytes;
+
     inline void event_loop() {
         x10_wait();
     }
