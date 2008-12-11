911dfef1e42ad55651e13e2533b6932b2189161a
David Cunningham <dcunnin@us.ibm.com>
2008-12-11 04:03:20 +0000

    changed primitive toString() to use snprintf
    added x10aux::main which is templated over "Runtime" (one should usually give
    it x10::runtime::Runtime) and "T" which is the class containing the static main
    method we will invoke.
    
    Uncommented some code from StringBuilder that ought to now work (code gen bug fixed some time ago)
    
    Refactored NQueensPar to not depend on DistUtil
    
    Removed (broken) use of printf from FRASimpleDist.x10
    (native printf does not understand boxes)
    
    
    
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk/x10.runtime.17@7220 a326200e-df2c-42e0-81fa-597e909af41d

--- src-cpp/x10aux/string_utils.cc
+++ src-cpp/x10aux/string_utils.cc
@@ -1,3 +1,6 @@
+#include <cmath>
+
+#include <x10aux/config.h>
 #include <x10aux/string_utils.h>
 #include <x10aux/rail_utils.h>
 #include <x10aux/alloc.h>
@@ -27,7 +30,9 @@ void x10aux::free_args(const ref<Rail<ref<String> > > &arr) {
     //cerr << "free_args: freed array " << arr << endl;
 }
 
+
 // [DC] I'm sure Igor will hate this but it will do for now.
+/*
 template<class T> T x10aux::from_string(const ref<String> &s) {
     std::istringstream ss(*s);
     T x;
@@ -36,6 +41,8 @@ template<class T> T x10aux::from_string(const ref<String> &s) {
     }
     return x;
 }
+*/
+
 
 // [DC] I'm sure Igor will hate this but it will do for now.
 template<class T> String to_string_general(T v) {
@@ -46,45 +53,96 @@ template<class T> String to_string_general(T v) {
     return r;
 }
 
-String x10aux::to_string(x10_boolean v) {
+#define TO_STRING(SZ,T,FMT) \
+String x10aux::to_string(T v) { \
+    char buf[SZ]; \
+    int amt = ::snprintf(buf, sizeof buf, FMT, v); \
+    assert((size_t)amt<sizeof buf && "buf too small "__TOKEN_STRING(SZ)" for "__TOKEN_STRING(T)); \
+    String r; \
+    r._constructor(buf); \
+    return r; \
+}
+
+// hh is C99, not ansi c, so we use h instead.
+// This is fine as va_args turns everything to int anyway
+TO_STRING(4, unsigned char, "%hu")
+TO_STRING(5, signed char, "%hd")
+
+//TO_STRING(6, unsigned short, "%hu")
+TO_STRING(7, signed short, "%hd")
+
+TO_STRING(11, unsigned int, "%u")
+TO_STRING(12, signed int, "%d")
+TO_STRING(11, unsigned long, "%lu")
+TO_STRING(12, signed long, "%ld")
+TO_STRING(20, unsigned long long, "%llu")
+TO_STRING(21, signed long long, "%lld")
+
+String x10aux::to_string(float v) {
+    return x10aux::to_string((double)v);
+}
+
+// precondition: buf contains decimal point
+void kill_excess_zeroes(char *buf, size_t sz) {
+    for(int i=sz-1 ; i>0 && (buf[i]=='0' || buf[i]=='\0') ; --i) {
+        if (buf[i-1]=='.') break;
+        buf[i] = '\0';
+    } 
+}
+
+
+String x10aux::to_string(double v) {
+    char buf[120] = "";
+    if (::isnan(v)) {
+        ::snprintf(buf, sizeof buf, "NaN");
+    } else if (v==INFINITY) {
+        ::snprintf(buf, sizeof buf, "Infinity");
+    } else if (v==-INFINITY) {
+        ::snprintf(buf, sizeof buf, "-Infinity");
+    } else if (fabs(v)>=1E-3 && fabs(v)<1E7) {
+        ::snprintf(buf, sizeof buf, "%.15f", v);
+        kill_excess_zeroes(buf, sizeof buf);
+    } else if (v==0) {
+        ::snprintf(buf, sizeof buf, "%.1f", v);
+    } else {
+        // scientific notation
+        int e = ::floor(::log(::fabs(v))/::log(10)); //exponent
+        // volatile because reordering could change computed floating point value
+        volatile double m = v / pow(10,e); //mantissa
+        if (e<-10) {
+            // avoid touching -Infinity
+            m = v * 1E10;
+            m /= pow(10,e+10);
+        }
+        if (e<0) {
+            ::snprintf(buf, sizeof buf, "%.1f", m);
+        } else {
+            ::snprintf(buf, sizeof buf, "%.16f", m);
+        }
+        kill_excess_zeroes(buf, sizeof buf);
+        char *rest = buf + strlen(buf);
+        ::snprintf(rest, sizeof(buf) + buf - rest, "E%d", e);
+    }
+    String r;
+    r._constructor(buf);
+    return r;
+}
+    
+String x10aux::to_string(bool v) {
     String r;
     r._constructor(v?"true":"false");
     return r;
 }
     
-String x10aux::to_string(x10_byte v) {
-    return to_string_general((int)v);
-}
-    
 String x10aux::to_string(x10_char v) {
-    return to_string_general((char)v);
-}
-    
-String x10aux::to_string(x10_short v) {
-    return to_string_general((short)v);
-}
-    
-String x10aux::to_string(x10_int v) {
-    return to_string_general((int)v);
-}
-    
-String x10aux::to_string(x10_long v) {
-    return to_string_general((int64_t)v);
-}
-    
-String x10aux::to_string(x10_float v) {
-    return to_string_general((float)v);
+    char v_[] = {(char)v,'\0'};
+    String r;
+    r._constructor(v_);
+    return r;
 }
 
-String x10aux::to_string(x10_double v) {
-    return to_string_general((double)v);
-}
 
-String x10aux::to_string(const char *v) {
-    return to_string_general(std::string(v));
-}
-
-String x10aux::to_string(ref<ValRail<x10_char> > v) {
+String x10aux::vrc_to_string(ref<ValRail<x10_char> > v) {
     std::string str(v->FMGL(length), '\0');
     for (int i = 0; i < v->FMGL(length); ++i)
         str[i] = (*v)[i];
