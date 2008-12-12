0db16630b5fc77bdd081a0aaee6f26cc88bb7031
Bruce Lucas <bdlucas@us.ibm.com>
2008-12-12 20:21:50 +0000

    Updated status report. Additional measurements.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@7227 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.tests/examples/Benchmarks/Report.java
+++ x10.tests/examples/Benchmarks/Report.java
@@ -18,11 +18,12 @@ class Report {
     static class Entries {
 
         HashMap<String,Collection<Entry>> map = new HashMap<String,Collection<Entry>>();
+        HashMap<String,Entry> shown = new HashMap<String,Entry>();
 
         private String key(String test, String lgc) {
             return test + "," + lgc;
         }
-
+        
         void put(Entry e) {
             String key = key(e.get("test"), e.get("lg-code"));
             Collection<Entry> l = map.get(key);
@@ -38,11 +39,23 @@ class Report {
             Collection<Entry> l = map.get(key);
             return l;
         }
+
+        void putShown(Entry e) {
+            String key = key(e.get("test"), e.get("lg-code"));
+            shown.put(key, e);
+        }
+
+        Entry getShown(String test, String lgc) {
+            String key = key(test, lgc);        
+            return shown.get(key);
+        }
     }
 
     static Entries entries = new Entries();
         
     static class Entry extends HashMap<String,String> implements Comparable<Entry> {
+
+        char letter;
 
         Entry(String line) {
             String [] nvs = line.split(" ");
@@ -72,9 +85,12 @@ class Report {
         tests.add("SeqPseudoArray2a");
         tests.add("SeqPseudoArray2b");
         tests.add("SeqArray2");
+        tests.add("SeqUTSBin1");
 
         lgcs.add("cpp-opt");
-
+        lgcs.add("x10-cpp-opt");
+        lgcs.add("java-opt");
+        lgcs.add("x10-java-opt");
 
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         String line = in.readLine();
@@ -93,19 +109,21 @@ class Report {
             out.printf("%-20s", test);
             Set<String> dates = new LinkedHashSet<String>();
             for (String lgc : lgcs) {
-                double ops = 0.0;
                 Collection<Entry> l = entries.get(test, lgc);
+                Entry show = null;
                 if (l!=null) {
                     for (Entry e : l) {
                         dates.add(e.get("date"));
-                        ops = e.ops();
+                        show = e;
                     }
                 }
-                if (ops!=0.0) {
+                if (show!=null) {
+                    double ops = show.ops();
+                    entries.putShown(show);
                     if (ops<1e6)      out.printf("%c: %-4.3g kop/s  ", letter, ops/1e3);
                     else if (ops<1e9) out.printf("%c: %-4.3g Mop/s  ", letter, ops/1e6);
                     else              out.printf("%c: %-4.3g Gop/s  ", letter, ops/1e9);
-                    letter++;
+                    show.letter = letter++;
                 } else
                     out.printf("%15s", "");
             }
@@ -113,6 +131,42 @@ class Report {
                 out.printf("%s ", d);
             out.printf("\n");
         }
+
+        pr("C++ back end relative to hand-coded");
+        compare("    rail access",              "SeqRail2",        "x10-cpp-opt", "SeqRail2",        "cpp-opt");
+        compare("    non-generic pseudo-array", "SeqPseudoArray2a","x10-cpp-opt", "SeqPseudoArray2a","cpp-opt");
+        compare("    generic pseudo-array",     "SeqPseudoArray2b","x10-cpp-opt", "SeqPseudoArray2a","cpp-opt");
+        compare("    array",                    "SeqArray2",       "x10-cpp-opt", "SeqPseudoArray2a","cpp-opt");
+        compare("    sequential UTS",           "SeqUTSBin1",      "x10-cpp-opt", "SeqUTSBin1",      "cpp-opt");
+
+        pr("Java back end relative to hand-coded");
+        compare("    rail access",              "SeqRail2",        "x10-java-opt", "SeqRail2",        "java-opt");
+        compare("    non-generic pseudo-array", "SeqPseudoArray2a","x10-java-opt", "SeqPseudoArray2a","java-opt");
+        compare("    generic pseudo-array",     "SeqPseudoArray2b","x10-java-opt", "SeqPseudoArray2a","java-opt");
+        compare("    array",                    "SeqArray2",       "x10-java-opt", "SeqPseudoArray2a","java-opt");
+        compare("    sequential UTS",           "SeqUTSBin1",      "x10-java-opt", "SeqUTSBin1",      "java-opt");
+
+        pr("C++ back end relative to Java back end");
+        compare("    rail access",              "SeqRail2",        "x10-cpp-opt", "SeqRail2",        "x10-java-opt");
+        compare("    non-generic pseudo-array", "SeqPseudoArray2a","x10-cpp-opt", "SeqPseudoArray2a","x10-java-opt");
+        compare("    generic pseudo-array",     "SeqPseudoArray2b","x10-cpp-opt", "SeqPseudoArray2b","x10-java-opt");
+        compare("    array",                    "SeqArray2",       "x10-cpp-opt", "SeqArray2",       "x10-java-opt");
+        compare("    sequential UTS",           "SeqUTSBin1",      "x10-cpp-opt", "SeqUTSBin1",      "x10-java-opt");
+
+        compare("C++ generic vs non-generic", "SeqPseudoArray2b","x10-cpp-opt", "SeqPseudoArray2a","x10-cpp-opt");
+        //compare("Java generic vs non-generic", "SeqPseudoArray2b","x10-java-opt", "SeqPseudoArray2a","x10-java-opt");
+
+        compare("UTS parallel vs sequential", "ParUTSBin1","x10-java-opt", "SeqUTSBin1","x10-java-opt");
+
+    }
+
+    static void compare(String name, String t1, String l1, String t2, String l2) {
+        Entry e1 = entries.getShown(t1,l1);
+        Entry e2 = entries.getShown(t2,l2);
+        if (e2.ops()>e1.ops())
+            out.printf("%-30s %5.1fx slower (%s/%s)\n", name, e2.ops()/e1.ops(), e2.letter, e1.letter);
+        else
+            out.printf("%-30s %5.1fx faster (%s/%s)\n", name, e1.ops()/e2.ops(), e1.letter, e2.letter);
     }
 
     static void pr(String s) {
