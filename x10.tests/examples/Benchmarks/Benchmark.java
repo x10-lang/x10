eabede0c5081c3351be8acf0304544987a7c0677
Bruce Lucas <bdlucas@us.ibm.com>
2008-12-12 20:14:59 +0000

    Add warmup.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@7223 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.tests/examples/Benchmarks/Benchmark.java
+++ x10.tests/examples/Benchmarks/Benchmark.java
@@ -1,6 +1,9 @@
 // (C) Copyright IBM Corporation 2006
 // This file is part of X10 Test.
 
+import java.io.*;
+
+
 /**
  * @author bdlucas
  */
@@ -11,52 +14,55 @@ abstract public class Benchmark {
     abstract double expected();
     abstract double operations();
 
-    int reps = 10;
-
-    double now() {
-        return System.nanoTime() * 1e-9;
-    }
-
-    void pr(String s) {
-        System.out.println(s);
-    }
+    double TIMING = 10.0;
+    double WARMUP = 10.0;
 
     void run() {
 
-        boolean first = true;
-        double min = Double.POSITIVE_INFINITY;
-        for (int i=0; i<reps; i++) {
-            double start = now();
-            double x = once();
-            double time = now() - start;
-            if (time<min) {
-                min = time;
-                i = 0;
-            }
-            System.out.printf("time %.3f, min %.3f\n", time, min);
-            if (first && x!=expected()) {
-                pr("got " + x + " expected " + expected());
-                System.exit(-1);
-            }
-            first = false;
-        }
+        PrintStream out = System.out;
+        String name = this.getClass().getName();
 
-        // now run reps times and average the time, and compute new min
-        min = Double.POSITIVE_INFINITY;
-        double time = 0;
-        if (reps>0) pr("--- averaging");
-        for (int i=0; i<reps; i++) {
-            double s = now();
-            double result = once();
-            double t = now() - s;
+        // functional check;
+        out.printf("functional check\n");
+        double warmup = now();
+        double result = once();
+        if (result!=expected()) {
+            out.printf("exected %f, result %f\n", expected(), result);
+            System.exit(-1);
+        }
+        
+        // do warmup
+        out.println("warmup for >" + WARMUP + "s");
+        while (now()-warmup < WARMUP)
+            once();
+
+        // run it for >TIMING secs
+        out.println("timing for >" + TIMING + "s");
+        double avg = 0.0;
+        double min = 1e100;
+        int count = 0;
+        while (avg < TIMING) {
+            double start = now();
+            once();
+            double t = now() - start;
             if (t<min)
                 min = t;
-            System.out.printf("time %.3f, min %.3f\n", t, min);
-            time += t;
-        }            
-        time = time / reps;
+            avg += t;
+            count++;
+        }
+        avg /= count;
+        
+        // print info
+        double ops = operations() / avg;
+        out.printf("time: %.3f; count: %d; min/time: %.2f\n", avg, count, min/avg);
+        if      (ops<1e6) out.printf("%.3g kop/s\n", ops/1e3);
+        else if (ops<1e9) out.printf("%.3g Mop/s\n", ops/1e6);
+        else              out.printf("%.3g Gop/s\n", ops/1e9);
+        
+        out.printf("test=%s lg=java ops=%g\n", name, ops);
+    }
 
-        System.out.printf("op/s: %.5e\n", operations() / time);
-        System.out.printf("min/time: %.2f\n", min/time);
+    double now() {
+        return System.nanoTime() * 1e-9;
     }
 }
