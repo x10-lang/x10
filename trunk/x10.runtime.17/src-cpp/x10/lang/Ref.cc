e283c8ed1c70cc08ec5a1e1168f7e7e39ad2d237
David Grove <groved@us.ibm.com>
2009-01-22 22:48:52 +0000

    XTENLANG-226 WIP
    Simple infrastructure to log remote references when built with GC.
    Code should be complete inactive unless building with GC enabled.
    
    
    
    git-svn-id: svn://svn.code.sf.net/p/x10/code@7306 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.runtime.17/src-cpp/x10/lang/Ref.cc
+++ trunk/x10.runtime.17/src-cpp/x10/lang/Ref.cc
@@ -6,9 +6,46 @@
 #include <x10/lang/Ref.h>
 #include <x10/lang/String.h>
 
+#include <x10/runtime/Lock.h>
+
 using namespace x10::lang;
+using namespace x10::runtime;
 using namespace x10aux;
 
+#if X10_USE_BDWGC
+Ref::ReferenceLogger* Ref::refLogger = new (x10aux::alloc<Ref::ReferenceLogger>()) ReferenceLogger();
+
+#define SPINE_SIZE 1024
+#define BUCKET_LOG_SIZE 4
+#define BUCKET_SIZE (1<<BUCKET_LOG_SIZE)
+#define BUCKET_MASK ((1<<BUCKET_LOG_SIZE)-1)
+
+Ref::ReferenceLogger::ReferenceLogger() {
+    escapedReferences = (Ref***)(x10aux::alloc<Ref>(SPINE_SIZE));
+    nextSlot = 0;
+    lock = Lock::_make();
+}
+
+void Ref::ReferenceLogger::log(ref<Ref> x) {
+
+    // Critical section guarded by lock:
+    //   (1) Get a slot assigned to log this reference
+    //   (2) If that reference would be the first one in a bucket, allocate the backing bucket.
+    lock->lock();
+    int mySlot = nextSlot++;
+    int spineIndex = mySlot >> BUCKET_LOG_SIZE;
+    int bucketIndex = mySlot & BUCKET_MASK;
+    if (0 == bucketIndex) {
+        assert(spineIndex<SPINE_SIZE); /* TODO: We've logged so many references that we're out of space to log them */
+        escapedReferences[spineIndex] = (Ref**)(x10aux::alloc<Ref>(BUCKET_SIZE));
+    }
+    lock->unlock();
+
+    // Actually log the reference in its assigned slot.  Don't have to hold the lock to do this work.
+    escapedReferences[spineIndex][bucketIndex] = x.get();
+}
+#endif /* X10_USE_BDWGC */
+    
 x10_int x10::lang::Ref::hashCode() {
     return (x10_int) (int64_t)(void*)this;
 }
