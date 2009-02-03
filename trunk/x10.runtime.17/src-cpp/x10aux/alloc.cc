86d9e87a9aa7ad83c637bf38a757add2237488de
David Cunningham <dcunnin@us.ibm.com>
2009-02-03 00:16:18 +0000

    Implement realloc_printf
    A light sprinkling of GPUSAFE which should ordinarily be an empty macro but is
    currently helping me play with CUDA.  We can discuss whether this is the right
    way to proceed long term.
    
    
    
    git-svn-id: svn://svn.code.sf.net/p/x10/code@7342 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.runtime.17/src-cpp/x10aux/alloc.cc
+++ trunk/x10.runtime.17/src-cpp/x10aux/alloc.cc
@@ -8,14 +8,27 @@ using namespace x10aux;
 #ifdef __CYGWIN__
 extern "C" int vsnprintf(char *, size_t, const char *, va_list); 
 #endif
-const char *x10aux::alloc_printf(const char *fmt, ...) {
+
+char *x10aux::alloc_printf(const char *fmt, ...) {
     va_list args;
     va_start(args, fmt);
-    size_t sz = vsnprintf(NULL,0,fmt,args);
+    std::size_t sz = vsnprintf(NULL,0,fmt,args);
     char *r = x10aux::alloc<char>(sz+1);
-    r[sz] = '\0';
     vsprintf(r, fmt, args);
     va_end(args);
     return r;
 }
+
+char *x10aux::realloc_printf(char *buf, const char *fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+    std::size_t original_sz = strlen(buf);
+    std::size_t sz = original_sz + vsnprintf(NULL,0,fmt,args);
+    char *r = x10aux::realloc(buf,sz+1);
+    // append the new stuff onto the original stuff
+    vsprintf(&r[original_sz], fmt, args);
+    va_end(args);
+    return r;
+}
+
 
