7ebd27768f747c5eb2045a51423679c1cdb2652f
Pradeep Varma <user@domain>
2009-02-19 14:35:23 +0000

    Committed on Fri Oct 17 05:21:49 2008 using namespace x10::lang made canonical;
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@7574 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.cppbackend.17/trunk-cpp/src/polyglot/ext/x10cpp/visit/X10CPPTranslator.java
+++ x10.cppbackend.17/trunk-cpp/src/polyglot/ext/x10cpp/visit/X10CPPTranslator.java
@@ -386,6 +386,7 @@ public class X10CPPTranslator extends Translator {
 		w.write("#include \""+wstreams.getFile(WriterStreams.StreamClass.Header).getName()+"\""); w.newline();
 		w.forceNewline(0);
 		w.write("using namespace x10;"); w.newline();
+		w.write("using namespace x10::lang;"); w.newline();
 		w.forceNewline(0);
 
 		boolean newline = false;
@@ -413,7 +414,7 @@ public class X10CPPTranslator extends Translator {
 			w.newline();
 		}
 
-		w.write("static "+make_ref("x10::lang::Exception")+" EXCEPTION = NULL;"); w.newline();
+		w.write("static "+make_ref("Exception")+" EXCEPTION = NULL;"); w.newline();
 		w.write("#include \""+wstreams.getFile(WriterStreams.StreamClass.Closures).getName() + "\""); w.newline();
 		w.forceNewline(0);
 
