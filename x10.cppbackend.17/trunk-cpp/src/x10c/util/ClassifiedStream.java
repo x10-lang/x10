8b31088b2e0305bc559ebedd7bb8c140e4a8332b
Igor Peshansky <igorp@us.ibm.com>
2009-02-19 14:36:03 +0000

    Committed on Sat Dec  6 16:10:35 2008 Add ClassifiedStream.contents() method to use instead of toString() More informative ClassifiedStream.toString()
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@7614 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.cppbackend.17/trunk-cpp/src/x10c/util/ClassifiedStream.java
+++ x10.cppbackend.17/trunk-cpp/src/x10c/util/ClassifiedStream.java
@@ -5,8 +5,6 @@ package x10c.util;
 
 import java.io.ByteArrayOutputStream;
 
-
-import polyglot.main.Options;
 import polyglot.util.SimpleCodeWriter;
 
 
@@ -16,13 +14,14 @@ public class ClassifiedStream extends SimpleCodeWriter {
     private ClassifiedStream(ByteArrayOutputStream bs, StreamWrapper.StreamClass c, int width) {
         super(bs, width);
         stream = bs;
-        this.sClass=c;
+        this.sClass = c;
     }
     public ClassifiedStream(StreamWrapper.StreamClass c, int width) {
         this(new ByteArrayOutputStream(), c, width);
     }
+    public String contents() { output.flush(); return stream.toString(); }
     public void forceNewline() { pos = -1; newline(); }
     public void forceNewline(int n) { pos = -1; newline(n); }
-    public String toString() { return stream.toString(); }
+    public String toString() { return "ClassifiedStream: '"+contents()+"'"; }
 }
 // vim:tabstop=4:shiftwidth=4:expandtab
