d47251e2d202cce99a4a71b8d64af33ed715990f
Igor Peshansky <igorp@us.ibm.com>
2009-02-19 14:35:43 +0000

    Committed on Mon Nov 24 01:54:28 2008 Add VIm modeline d2u and whitespace
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@7594 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.cppbackend.17/trunk-cpp/src/x10c/util/ClassifiedStream.java
+++ x10.cppbackend.17/trunk-cpp/src/x10c/util/ClassifiedStream.java
@@ -11,18 +11,18 @@ import polyglot.util.SimpleCodeWriter;
 
 
 public class ClassifiedStream extends SimpleCodeWriter {
-	final ByteArrayOutputStream stream;
-	public final WriterStreams.StreamClass sClass;
-	private ClassifiedStream(ByteArrayOutputStream bs, WriterStreams.StreamClass c, int width) {
-		super(bs, width);
-		stream = bs;
-		this.sClass=c;
-	}
-	public ClassifiedStream(WriterStreams.StreamClass c, int width) {
-		this(new ByteArrayOutputStream(), c, width);
-	}
-	public void forceNewline(){ pos = -1; newline();}
-	public void forceNewline(int n) { pos = -1; newline(n); }
-	public String toString() { return stream.toString();}
-
+    final ByteArrayOutputStream stream;
+    public final WriterStreams.StreamClass sClass;
+    private ClassifiedStream(ByteArrayOutputStream bs, WriterStreams.StreamClass c, int width) {
+        super(bs, width);
+        stream = bs;
+        this.sClass=c;
+    }
+    public ClassifiedStream(WriterStreams.StreamClass c, int width) {
+        this(new ByteArrayOutputStream(), c, width);
+    }
+    public void forceNewline() { pos = -1; newline(); }
+    public void forceNewline(int n) { pos = -1; newline(n); }
+    public String toString() { return stream.toString(); }
 }
+// vim:tabstop=4:shiftwidth=4:expandtab
