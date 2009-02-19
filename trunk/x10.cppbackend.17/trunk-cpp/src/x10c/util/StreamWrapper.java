a32cc064bab5c10a87c9cfa0459a9a90a6cbb119
Igor Peshansky <igorp@us.ibm.com>
2009-02-19 14:35:43 +0000

    Committed on Mon Nov 24 01:54:28 2008 Add VIm modeline d2u and whitespace
    git-svn-id: svn://svn.code.sf.net/p/x10/code@7594 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.cppbackend.17/trunk-cpp/src/x10c/util/StreamWrapper.java
+++ trunk/x10.cppbackend.17/trunk-cpp/src/x10c/util/StreamWrapper.java
@@ -23,46 +23,48 @@ import polyglot.util.SimpleCodeWriter;
  * 
  * @author nvk
  * @author vj
- *
  */
 public class StreamWrapper extends SimpleCodeWriter {
-	public WriterStreams ws;
-	public ClassifiedStream cs; // current stream;
-	java.util.Stack <ClassifiedStream> csStack; // Stream stack.
-	public StreamWrapper(ClassifiedStream b, int w, WriterStreams ws) throws IOException{
-		 // we override all methods, so super methods will never be called.
-		// hence it is ok to pass null to the super constructor.
-		super (new ByteArrayOutputStream(), w);
-		this.ws = ws;
-		this.cs = b;
-		csStack = new java.util.Stack <ClassifiedStream>();
-	}
-	@Override public void newline(){ cs.newline(); }
-	@Override public void newline(int n){ cs.newline(n);}
-	@Override public void newline(int n, int level){ cs.newline(n, level);}
-	@Override public void begin(int n){ cs.begin(n); }
-	@Override public void end(){ cs.end(); }
-	@Override public void allowBreak(int n, int level, String alt, int altlen) { cs.allowBreak(n,level,alt,altlen); }
-	@Override public void allowBreak(int n) { cs.allowBreak(n); }
-	@Override public void allowBreak(int n, String alt) { cs.allowBreak(n, alt); }
-	@Override public void unifiedBreak(int n, int level, String alt, int altlen) {
-			cs.unifiedBreak(n, level, alt, altlen);
-	}
-	@Override  public void unifiedBreak(int n) { cs.unifiedBreak(n); }
-	@Override  public void write (String str){ cs.write (str);}
-	@Override public void write(String s, int length) { cs.write(s, length);}
-	@Override public boolean flush() throws IOException { return cs.flush();}
-	@Override public boolean flush(boolean fmt) throws IOException { return cs.flush(fmt);}
-	@Override public String toString() {
-		assert false;
-		return null;
-	}
-	@Override public void close() throws IOException { cs.close();}
-	public void forceNewline(){ cs.forceNewline(); }
-	public void forceNewline(int n){ cs.forceNewline(n); }
-	public void popCurrentStream() { this.cs = csStack.pop(); }
-	public void pushCurrentStream(ClassifiedStream cs) {
-		csStack.push(this.cs);
-		this.cs = cs;
-	}
+    public WriterStreams ws;
+    public ClassifiedStream cs; // current stream;
+    java.util.Stack<ClassifiedStream> csStack; // Stream stack.
+    public StreamWrapper(ClassifiedStream b, int w, WriterStreams ws) throws IOException {
+        // we override all methods, so super methods will never be called.
+        // hence it is ok to pass null to the super constructor.
+        super(new ByteArrayOutputStream(), w);
+        this.ws = ws;
+        this.cs = b;
+        csStack = new java.util.Stack<ClassifiedStream>();
+    }
+    @Override public void newline() { cs.newline(); }
+    @Override public void newline(int n) { cs.newline(n); }
+    @Override public void newline(int n, int level) { cs.newline(n, level); }
+    @Override public void begin(int n) { cs.begin(n); }
+    @Override public void end() { cs.end(); }
+    @Override public void allowBreak(int n, int level, String alt, int altlen) {
+        cs.allowBreak(n,level,alt,altlen);
+    }
+    @Override public void allowBreak(int n) { cs.allowBreak(n); }
+    @Override public void allowBreak(int n, String alt) { cs.allowBreak(n, alt); }
+    @Override public void unifiedBreak(int n, int level, String alt, int altlen) {
+        cs.unifiedBreak(n, level, alt, altlen);
+    }
+    @Override public void unifiedBreak(int n) { cs.unifiedBreak(n); }
+    @Override public void write(String str) { cs.write(str); }
+    @Override public void write(String s, int length) { cs.write(s, length); }
+    @Override public boolean flush() throws IOException { return cs.flush(); }
+    @Override public boolean flush(boolean fmt) throws IOException { return cs.flush(fmt); }
+    @Override public String toString() {
+        assert false;
+        return null;
+    }
+    @Override public void close() throws IOException { cs.close(); }
+    public void forceNewline(){ cs.forceNewline(); }
+    public void forceNewline(int n){ cs.forceNewline(n); }
+    public void popCurrentStream() { this.cs = csStack.pop(); }
+    public void pushCurrentStream(ClassifiedStream cs) {
+        csStack.push(this.cs);
+        this.cs = cs;
+    }
 }
+// vim:tabstop=4:shiftwidth=4:expandtab
--- trunk/x10.cppbackend.17/trunk-cpp/src/x10c/util/WriterStreams.java
+++ trunk/x10.cppbackend.17/trunk-cpp/src/x10c/util/WriterStreams.java
@@ -28,117 +28,122 @@ import polyglot.util.SimpleCodeWriter;
  *
  */
 public class WriterStreams {
-	public static enum StreamClass {Header("h"), CC("cc"), Closures("inc");
-		String ext;
-		private StreamClass(String e) { ext=e;}
-		public String toString() { return ext;}
-	};
-	private Map<StreamClass,SimpleCodeWriter> codeWriters;
-	private Map<StreamClass, File> codeFiles;
-	private Vector<ClassifiedStream> streams;
-	private DelegateTargetFactory targetFactory;
-	Job job;
+    public static enum StreamClass { Header("h"), CC("cc"), Closures("inc");
+        String ext;
+        private StreamClass(String e) { ext = e; }
+        public String toString() { return ext; }
+    };
+    private Map<StreamClass,SimpleCodeWriter> codeWriters;
+    private Map<StreamClass, File> codeFiles;
+    private Vector<ClassifiedStream> streams;
+    private DelegateTargetFactory targetFactory;
+    Job job;
 
-	public WriterStreams(String className, SourceFile sfn, String pkg, DelegateTargetFactory tf, List exports, Job job)
-	throws IOException{
-		streams = new Vector<ClassifiedStream>();
-		codeWriters = new TreeMap<StreamClass,SimpleCodeWriter>();
-		codeFiles = new TreeMap<StreamClass,File>();
-		targetFactory = tf;
-		this.job=job;
+    public WriterStreams(String className, SourceFile sfn, String pkg,
+                         DelegateTargetFactory tf, List exports, Job job) throws IOException
+    {
+        streams = new Vector<ClassifiedStream>();
+        codeWriters = new TreeMap<StreamClass,SimpleCodeWriter>();
+        codeFiles = new TreeMap<StreamClass,File>();
+        targetFactory = tf;
+        this.job = job;
 
-		//List exports = exports(sfn);
+        //List exports = exports(sfn);
 
-		for (StreamClass sc : StreamClass.values()) {
-			final File file = tf.integratedOutputFile(pkg, className,sfn.source(), 
-					sc.toString());
-			codeFiles.put(sc, file);
-		}
-		
-	}
+        for (StreamClass sc : StreamClass.values()) {
+            final File file = tf.integratedOutputFile(pkg, className,sfn.source(), sc.toString());
+            codeFiles.put(sc, file);
+        }
+    }
 
-	/**
-	 * Write out all the streams associated with this object to their correspodnding files.
-	 * Note that all streams of the same type are written to the same file, their contents 
-	 * concatenated in the order in which the streams were created.
-	 * @throws IOException
-	 */
-	public void commitStreams() throws IOException {
-		for (StreamClass sc : StreamClass.values()) {
-			final File file = codeFiles.get(sc);
-			codeWriters.put(sc, new SimpleCodeWriter(targetFactory.outputWriter(file),
-					job.compiler().outputWidth()));
-		}
-		for (ClassifiedStream s : streams) {
-			s.flush();
-			codeWriters.get(s.sClass).write(s.toString());
-		}
-		for (StreamClass sc : StreamClass.values()) {
-			SimpleCodeWriter w = codeWriters.get(sc);
-			w.flush();
-			w.close();
-		}
-	}
-	public File getFile(StreamClass sc) {
-		return codeFiles.get(sc);
-	}
-	public File getHeader() {
-		return codeFiles.get(StreamClass.Header);
-	}
-	/**
-	 * Return the current stream of type sc, creating one if there is none.
-	 * @param sc
-	 * @return
-	 */
-	public ClassifiedStream getCurStream(StreamClass sc) {
-		ClassifiedStream last = null;
-		for (ClassifiedStream cfs: streams) {
-			if (cfs.sClass != sc) continue;
-			last = cfs;
-		}
-		if (last != null) return last;
-		return getNewStream(sc);
-	}
-	/**
-	 * Get the first stream of type sc, creating one if there is none.
-	 * @param sc
-	 * @return the first stream of type sc
-	 */
-	public ClassifiedStream getFirstStream(StreamClass sc) {
-		for (ClassifiedStream cfs: streams) {
-			if (cfs.sClass != sc) continue;
-			return cfs;
-		}
-		return getNewStream(sc);
-	}
-	private int headerCount = 0;
-	/**
-	 * Create and return a new stream of type sc. Until a new stream of 
-	 * type sc is created, this stream will be the current stream of type sc.
-	 * Can only create a new stream for the non-header class.
-	 * @param sc
-	 * @return a new stream of type sc
-	 */
-	public ClassifiedStream getNewStream(StreamClass sc) { return getNewStream(sc,true); }
+    /**
+     * Write out all the streams associated with this object to their correspodnding files.
+     * Note that all streams of the same type are written to the same file, their contents 
+     * concatenated in the order in which the streams were created.
+     * @throws IOException
+     */
+    public void commitStreams() throws IOException {
+        for (StreamClass sc : StreamClass.values()) {
+            final File file = codeFiles.get(sc);
+            codeWriters.put(sc,
+                            new SimpleCodeWriter(targetFactory.outputWriter(file),
+                                                 job.compiler().outputWidth()));
+        }
+        for (ClassifiedStream s : streams) {
+            s.flush();
+            codeWriters.get(s.sClass).write(s.toString());
+        }
+        for (StreamClass sc : StreamClass.values()) {
+            SimpleCodeWriter w = codeWriters.get(sc);
+            w.flush();
+            w.close();
+        }
+    }
 
-	/**
-	 * Create and return a new stream of type sc. Until a new stream of 
-	 * type sc is created, this stream will be the current stream of type sc.
-	 * Can only create a new stream for the non-header class.
-	 * @param sc
-	 * @param prepend Whether to prepend the new stream (true) or append it (false)
-	 * @return a new stream of type sc
-	 */
-	public ClassifiedStream getNewStream(StreamClass sc, boolean prepend) {
-		if (sc == StreamClass.Header) headerCount++;
-		assert (sc != StreamClass.Header || headerCount < 2);
-		ClassifiedStream cs = new ClassifiedStream(sc, job.compiler().outputWidth());
-		if (prepend) {
-                        streams.add(0, cs);
-                } else {
-                        streams.add(cs);
-                }
-		return cs;
-	}
+    public File getFile(StreamClass sc) {
+        return codeFiles.get(sc);
+    }
 
+    public File getHeader() {
+        return codeFiles.get(StreamClass.Header);
+    }
+
+    /**
+     * Return the current stream of type sc, creating one if there is none.
+     * @param sc
+     * @return
+     */
+    public ClassifiedStream getCurStream(StreamClass sc) {
+        ClassifiedStream last = null;
+        for (ClassifiedStream cfs: streams) {
+            if (cfs.sClass != sc) continue;
+            last = cfs;
+        }
+        if (last != null) return last;
+        return getNewStream(sc);
+    }
+
+    /**
+     * Get the first stream of type sc, creating one if there is none.
+     * @param sc
+     * @return the first stream of type sc
+     */
+    public ClassifiedStream getFirstStream(StreamClass sc) {
+        for (ClassifiedStream cfs: streams) {
+            if (cfs.sClass != sc) continue;
+            return cfs;
+        }
+        return getNewStream(sc);
+    }
+
+    /**
+     * Create and return a new stream of type sc. Until a new stream of 
+     * type sc is created, this stream will be the current stream of type sc.
+     * Can only create a new stream for the non-header class.
+     * @param sc
+     * @return a new stream of type sc
+     */
+    public ClassifiedStream getNewStream(StreamClass sc) { return getNewStream(sc, true); }
+
+    private int headerCount = 0;
+    /**
+     * Create and return a new stream of type sc. Until a new stream of 
+     * type sc is created, this stream will be the current stream of type sc.
+     * Can only create a new stream for the non-header class.
+     * @param sc
+     * @param prepend Whether to prepend the new stream (true) or append it (false)
+     * @return a new stream of type sc
+     */
+    public ClassifiedStream getNewStream(StreamClass sc, boolean prepend) {
+        if (sc == StreamClass.Header) headerCount++;
+        assert (sc != StreamClass.Header || headerCount < 2);
+        ClassifiedStream cs = new ClassifiedStream(sc, job.compiler().outputWidth());
+        if (prepend) {
+            streams.add(0, cs);
+        } else {
+            streams.add(cs);
+        }
+        return cs;
+    }
 }
+// vim:tabstop=4:shiftwidth=4:expandtab
