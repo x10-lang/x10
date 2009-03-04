04b31decb0d4651d55ef695d46ac99153301023d
David Grove <groved@us.ibm.com>
2009-03-04 21:46:49 +0000

    Import of all public domain code from the jsr166 cvs repository (pserver:anonymous@gee.cs.oswego.edu/export/home/jsr166/jsr166) as it existed on March 4, 2009.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@7873 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ trunk/jsr166-pd/src/extra166y/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * Written by Doug Lea with assistance from members of JCP JSR-166
+ * Expert Group and released to the public domain, as explained at
+ * http://creativecommons.org/licenses/publicdomain
+ */
+
+
+/**
+ * Collections supporting parallel operations.
+ */
+package extra166y;
