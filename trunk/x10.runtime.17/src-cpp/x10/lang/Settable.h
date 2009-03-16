f14622de124b3ce32efa4d77cbdcbbf6ca80cc53
Igor Peshansky <igorp@us.ibm.com>
2009-03-16 18:24:55 +0000

    Clean up C++ implementation Add C++ native annotations
    git-svn-id: svn://svn.code.sf.net/p/x10/code@7957 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.runtime.17/src-cpp/x10/lang/Settable.h
+++ trunk/x10.runtime.17/src-cpp/x10/lang/Settable.h
@@ -1,47 +1,35 @@
-#ifndef __X10_LANG_SETTABLE_H
-#define __X10_LANG_SETTABLE_H
+#ifndef X10_LANG_SETTABLE_H
+#define X10_LANG_SETTABLE_H
 
 #include <x10aux/config.h>
-
+#include <x10aux/RTT.h>
 #include <x10/lang/Object.h>
 
-namespace x10 { namespace lang { 
+namespace x10 {
+    namespace lang {
+        template<class I, class V> class Settable : public virtual Object {
+            public:
+            class RTT : public x10aux::RuntimeType {
+                public:
+                static RTT * const it;
+                virtual void init() { initParents(1,x10aux::getRTT<Object>()); }
+                virtual const char *name() const {
+                    static const char *name =
+                        x10aux::alloc_printf("x10.lang.Settable[%s,%s]",
+                                             x10aux::getRTT<I>()->name(),
+                                             x10aux::getRTT<V>()->name());
+                    return name;
+                }
+            };
+            virtual const x10aux::RuntimeType *_type() const {
+                return x10aux::getRTT<x10::lang::Settable<I, V> >();
+            }
 
-class Ref;
-
-template<class FMGL(I), class FMGL(V)> class Settable  : public virtual x10::lang::Object {
-    public:
-    class RTT : public x10aux::RuntimeType {
-        public:
-        static RTT * const it;
-        virtual void init() {
-            initParents(2, x10aux::getRTT<x10::lang::Ref>(), x10aux::getRTT<x10::lang::Object>());
-        }
-        virtual const char *name() const { 
-            static const char *name = 
-                x10aux::alloc_printf("x10.lang.Settable[%s,%s]",
-                                     x10aux::getRTT<FMGL(I)>()->name(),
-                                     x10aux::getRTT<FMGL(V)>()->name());
-            return name;
-        }
-    };
-    virtual const x10aux::RuntimeType *_type () const {
-        return x10aux::getRTT<x10::lang::Settable<FMGL(I), FMGL(V)> >();
+            virtual V set(V v, I i) = 0;
+        };
+        template<class I, class V> typename Settable<I, V>::RTT * const Settable<I, V>::RTT::it =
+            new (x10aux::alloc<typename Settable<I, V>::RTT>()) typename Settable<I, V>::RTT();
     }
-    
-    virtual FMGL(V) set(FMGL(V) v, FMGL(I) i) = 0;
-    
-};
-
-} } 
-
-namespace x10 { namespace lang { 
-
-
-//#line 16 "/home/spark/x10-cvs/x10.runtime.17/src-x10/x10/lang/Settable.x10"
-template<class FMGL(I), class FMGL(V)> typename x10::lang::Settable<FMGL(I), FMGL(V)>::RTT * const x10::lang::Settable<FMGL(I), FMGL(V)>::RTT::it = 
-    new (x10aux::alloc<typename x10::lang::Settable<FMGL(I), FMGL(V)>::RTT>()) typename x10::lang::Settable<FMGL(I), FMGL(V)>::RTT();
-
-} } 
-
-#endif // X10_LANG_SETTABLE_H
+}
+#endif
+// vim:tabstop=4:shiftwidth=4:expandtab
