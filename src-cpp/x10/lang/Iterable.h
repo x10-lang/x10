d67e73012c5d3b48a9e6c1885982c200aa7213aa
Igor Peshansky <igorp@us.ibm.com>
2009-03-16 18:24:55 +0000

    Clean up C++ implementation Add C++ native annotations
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk/x10.runtime.17@7957 a326200e-df2c-42e0-81fa-597e909af41d

--- src-cpp/x10/lang/Iterable.h
+++ src-cpp/x10/lang/Iterable.h
@@ -1,46 +1,35 @@
-#ifndef __X10_LANG_ITERABLE_H
-#define __X10_LANG_ITERABLE_H
+#ifndef X10_LANG_ITERABLE_H
+#define X10_LANG_ITERABLE_H
 
 #include <x10aux/config.h>
-
+#include <x10aux/RTT.h>
 #include <x10/lang/Object.h>
 
-namespace x10 { namespace lang { 
+namespace x10 {
+    namespace lang {
+        template<class T> class Iterable : public virtual Object {
+            public:
+            class RTT : public x10aux::RuntimeType {
+                public:
+                static RTT * const it;
+                virtual void init() { initParents(1,x10aux::getRTT<Object>()); }
+                virtual const char *name() const {
+                    static const char *name =
+                        x10aux::alloc_printf("x10.lang.Iterable[%s]",
+                                             x10aux::getRTT<T>()->name());
+                    return name;
+                }
+            };
+            virtual const x10aux::RuntimeType *_type() const {
+                return x10aux::getRTT<Iterable<T> >();
+            }
 
-class Ref;
+            virtual x10aux::ref<Iterator<T > > iterator() = 0;
+        };
+        template<class T> typename Iterable<T>::RTT * const Iterable<T>::RTT::it =
+            new (x10aux::alloc<typename Iterable<T>::RTT>()) typename Iterable<T>::RTT();
 
-template<class FMGL(T)> class Iterable  : public virtual x10::lang::Object
- {
-    public:
-    class RTT : public x10aux::RuntimeType {
-        public:
-        static RTT * const it;
-        virtual void init() {
-            initParents(2, x10aux::getRTT<x10::lang::Ref>(), x10aux::getRTT<x10::lang::Object>());
-        }
-        virtual const char *name() const { 
-            static const char *name = 
-                x10aux::alloc_printf("x10.lang.Iterable[%s]",
-                                     x10aux::getRTT<FMGL(T)>()->name());
-            return name;
-        }
-    };
-    virtual const x10aux::RuntimeType *_type () const {
-        return x10aux::getRTT<x10::lang::Iterable<FMGL(T)> >();
     }
-    
-    virtual x10aux::ref<x10::lang::Iterator<FMGL(T) > > iterator() = 0;
-    
-};
-
-} } 
-
-namespace x10 { namespace lang { 
-
-//#line 20 "/home/spark/x10-cvs/x10.runtime.17/src-x10/x10/lang/Iterable.x10"
-template<class FMGL(T)> typename x10::lang::Iterable<FMGL(T)>::RTT * const x10::lang::Iterable<FMGL(T)>::RTT::it = 
-    new (x10aux::alloc<typename x10::lang::Iterable<FMGL(T)>::RTT>()) typename x10::lang::Iterable<FMGL(T)>::RTT();
-
-} } 
-
-#endif // X10_LANG_ITERABLE_H
+}
+#endif
+// vim:tabstop=4:shiftwidth=4:expandtab
