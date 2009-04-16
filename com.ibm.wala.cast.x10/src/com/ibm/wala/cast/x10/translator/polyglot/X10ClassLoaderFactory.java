eb11cfba2eb68c23ec877a4ef0bb841dc1adcc0d
Robert Fuhrer <rfuhrer@us.ibm.com>
2009-04-16 20:39:24 +0000

    Fixed indentation.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/branches/polyglot3@8223 a326200e-df2c-42e0-81fa-597e909af41d

--- com.ibm.wala.cast.x10/src/com/ibm/wala/cast/x10/translator/polyglot/X10ClassLoaderFactory.java
+++ com.ibm.wala.cast.x10/src/com/ibm/wala/cast/x10/translator/polyglot/X10ClassLoaderFactory.java
@@ -35,15 +35,15 @@ public class X10ClassLoaderFactory extends PolyglotClassLoaderFactory {
 	    ClassLoaderImpl cl = new X10PrimordialClassLoader(classLoaderReference, scope.getArrayClassLoader(), parent, getExclusions(), cha);
 	    cl.init( scope.getModules( classLoaderReference ));
 	    return cl;
-    } else if (classLoaderReference.equals(JavaSourceAnalysisScope.SOURCE)) {
-		// Don't let the JavaSourceLoaderImpl handle Java source that's generated from
-		// X10 source; that would create pseudo-duplicate classes. The following variant
-		// just skips over such source files.
-		ClassLoaderImpl cl = new JavaFilteredSourceLoaderImpl(classLoaderReference, parent, getExclusions(), cha, getExtensionFor(classLoaderReference));
-        cl.init( scope.getModules( classLoaderReference ));
-		return cl;
-	} else {
-	    return super.makeNewClassLoader(classLoaderReference, cha, parent, scope);
-	}
+ 	} else if (classLoaderReference.equals(JavaSourceAnalysisScope.SOURCE)) {
+ 	    // Don't let the JavaSourceLoaderImpl handle Java source that's generated from
+ 	    // X10 source; that would create pseudo-duplicate classes. The following variant
+ 	    // just skips over such source files.
+ 	    ClassLoaderImpl cl = new JavaFilteredSourceLoaderImpl(classLoaderReference, parent, getExclusions(), cha, getExtensionFor(classLoaderReference));
+ 	    cl.init( scope.getModules( classLoaderReference ));
+ 	    return cl;
+ 	} else {
+ 	    return super.makeNewClassLoader(classLoaderReference, cha, parent, scope);
+ 	}
     }
 }
