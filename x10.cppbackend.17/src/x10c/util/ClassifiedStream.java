c51d26a777facc3119e3b23b48726fba0d949633
Igor Peshansky <igorp@us.ibm.com>
2009-05-11 19:57:33 +0000

    Allow programmatic access to the current line number and filename.
    The line number computation is not always correct, and can really
    only be computed on commit.  Redo this later.
    
    
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@8296 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.cppbackend.17/src/x10c/util/ClassifiedStream.java
+++ x10.cppbackend.17/src/x10c/util/ClassifiedStream.java
@@ -7,10 +7,10 @@ import java.io.ByteArrayOutputStream;
 
 import polyglot.util.SimpleCodeWriter;
 
-
 public class ClassifiedStream extends SimpleCodeWriter {
     final ByteArrayOutputStream stream;
     public final String ext;
+    public int lineNumber = 1;
     private ClassifiedStream(ByteArrayOutputStream bs, String ext, int width) {
         super(bs, width);
         stream = bs;
@@ -23,5 +23,13 @@ public class ClassifiedStream extends SimpleCodeWriter {
     public void forceNewline() { pos = -1; newline(); }
     public void forceNewline(int n) { pos = -1; newline(n); }
     public String toString() { return "ClassifiedStream: '"+contents()+"'"; }
+
+    public void newline() {
+        if (pos != lmargin)
+            lineNumber++;
+        super.newline();
+    }
+    // FIXME: this is wrong -- need to take into account the whole file.
+    public int getLineNumber() { return lineNumber; }
 }
 // vim:tabstop=4:shiftwidth=4:expandtab
