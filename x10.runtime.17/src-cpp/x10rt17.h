77bd90ad7272af03ff5373c8ba76a0fc0e4771c3
David Grove <groved@us.ibm.com>
2009-05-15 13:30:25 +0000

    add x10aux/atomic_ops.h to the set of standard includes
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@8352 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.runtime.17/src-cpp/x10rt17.h
+++ x10.runtime.17/src-cpp/x10rt17.h
@@ -21,6 +21,7 @@
 #include <x10aux/init_dispatcher.h>
 #include <x10aux/hash.h>
 #include <x10aux/basic_functions.h>
+#include <x10aux/atomic_ops.h>
 
 #include <x10aux/boolean_utils.h>
 #include <x10aux/byte_utils.h>
