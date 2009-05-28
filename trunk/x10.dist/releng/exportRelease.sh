4cf661bfdd5594b1e65e45d8a2aa4ab3daabf59c
David Grove <groved@us.ibm.com>
2009-05-28 21:07:16 +0000

    update exportRelease script for polyglot move to google code svn.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@8430 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.dist/releng/exportRelease.sh
+++ trunk/x10.dist/releng/exportRelease.sh
@@ -2,38 +2,66 @@
 
 # Dave Grove
 
-set -e
-set -x
+while [ $# != 0 ]; do
 
-if (( $# == 0 )) || [ x$1 = x-h -o x$1 = x--help ] ; then
-  echo usage: $0 symbolic_revision
-  exit 1
+  case $1 in
+    -version)
+	export X10_VERSION=$2
+	shift
+    ;;
+
+    -x10-tag)
+	export X10_TAG=$2
+	shift
+    ;;
+
+    -polyglot-tag)
+	export POLYGLOT_TAG=$2
+	shift
+    ;;
+   esac
+   shift
+done
+
+if [[ -z "$X10_VERSION" ]]; then
+    echo "usage: $0 must give X10 version as -x10-tag <tag>"
+    exit 1
+fi
+
+if [[ -z "$X10_TAG" ]]; then
+    echo "usage: $0 must give X10 tag as -x10-tag <tag>"
+    exit 1
+fi
+
+if [[ -z "$POLYGLOT_TAG" ]]; then
+    echo "usage: $0 must give polyglot tag as -polyglot-tag <tag>"
+    exit 1
 fi
 
 date
 
-revision=$1
-
 workdir=$HOME/scratch/distribution
-distdir=$workdir/x10-$revision
-tarfile=x10-$revision.tar.gz
-tarfile2=x10-$revision.tar.bz2
+distdir=$workdir/x10-$X10_VERSION
+tarfile=x10-$X10_VERSION.tar.gz
+tarfile2=x10-$X10_VERSION.tar.bz2
 
 echo
 echo cleaning $workdir
 rm -rf $workdir
 mkdir -p $workdir || exit 1
-mkdir -p $workdir/x10-$revision
+mkdir -p $workdir/x10-$X10_VERSION
+
+
 
 (
 echo
 echo getting polyglot
-cd $workdir/x10-$revision
-cvs -d:pserver:anonymous@gforge.cis.cornell.edu:/cvsroot/polyglot co polyglot3-dev
+cd $workdir/x10-$X10_VERSION
+svn co http://polyglot-compiler.googlecode.com/svn/tags/$POLYGLOT_TAG/polyglot
 )
 
 (
-cd $workdir/x10-$revision
+cd $workdir/x10-$X10_VERSION
 
 echo
 echo getting distrib
@@ -46,7 +74,7 @@ for i in \
 	x10.runtime.17 \
 	x10.tests
 do
-    svn export https://x10.svn.sourceforge.net/svnroot/x10/tags/$revision/$i
+    svn export https://x10.svn.sourceforge.net/svnroot/x10/tags/$X10_TAG/$i
 done
 )
 
