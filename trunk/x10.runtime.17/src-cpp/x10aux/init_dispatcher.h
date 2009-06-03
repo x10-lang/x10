7913e6c5272b4970cbc9d95a05a09acfc2185ae6
David Grove <groved@us.ibm.com>
2009-06-03 17:22:22 +0000

    Move all code for deserialization_dispatcher and init_dispatcher from .h to .cc files.  Removes 2.5 Mb from libx10rt17.a by avoiding inlining copies of this code into the static initialization code of every object file.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@8452 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.runtime.17/src-cpp/x10aux/init_dispatcher.h
+++ trunk/x10.runtime.17/src-cpp/x10aux/init_dispatcher.h
@@ -13,10 +13,6 @@ namespace x10aux {
         protected:
         static InitDispatcher *it;
 
-        static void ensure_created() {
-            if (it==NULL) it = new (alloc<InitDispatcher>()) InitDispatcher();
-        }
-
         Initializer *initv;
         int initc;
         size_t initsz;
@@ -25,38 +21,12 @@ namespace x10aux {
         InitDispatcher () : initv(NULL), initc(0), initsz(0) { }
         ~InitDispatcher () { dealloc(initv); }
         
-        static void runInitializers() {
-            ensure_created();
-            it->runInitializers_();
-        }
-        void runInitializers_() {
-            for (int i=0 ; i<initc ; ++i) {
-                initv[i]();
-            }
-            initc = -1;
-        }
+        static void runInitializers();
+        void runInitializers_(); 
 
-        static void* addInitializer(Initializer init) {
-            ensure_created();
-            it->addInitializer_(init);
-            return (void*)0x7777; // we call addInitializer(...) to initialise the global vars
-        }
-        void addInitializer_(Initializer init) {
-            if (initc<0) {
-                fprintf(stderr,"Adding initializer too late!");
-                abort();
-            }
-            if (initsz<=(size_t)initc) {
-                // grow slowly as this is init phase and we don't want to take
-                // up RAM unnecessarily
-                size_t newsz = initsz+1;
-                initv = realloc(initv,initsz*sizeof(Initializer),newsz*sizeof(Initializer));
-                initsz = newsz;
-            }
-            initv[initc++] = init;
-        }
+        static void* addInitializer(Initializer init);
+        void addInitializer_(Initializer init);
     };
-
 }
 
 #endif
