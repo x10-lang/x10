d83e5e5512bd6693b3a59ee47082daf463827321
David Grove <groved@us.ibm.com>
2009-06-04 16:09:14 +0000

    micro-optimization of rail bound checks optimization. Since its a zero-based index, we can do a single unsigned compare instead of two signed compares.
    Also restructure code so that the compare is inlined
    while the exception raising code when the check fails
    remains out-of-line (and now has a NORETURN pragma).
    
    
    
    git-svn-id: svn://svn.code.sf.net/p/x10/code@8458 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.runtime.17/src-cpp/x10aux/rail_utils.cc
+++ trunk/x10.runtime.17/src-cpp/x10aux/rail_utils.cc
@@ -8,19 +8,10 @@
 using namespace x10::lang;
 using namespace x10aux;
 
-void x10aux::_check_bounds(x10_int index, x10_int length) {
-    (void)index;
-    (void)length;
-    #ifndef NO_BOUNDS_CHECKS
-    #ifndef NO_EXCEPTIONS
-    if (index < 0 || index > length) {
-        std::stringstream msg;
-        msg << index << " not in [0," << length << ")";
-        typedef x10::lang::ArrayIndexOutOfBoundsException Err;
-        throwException(Err::_make(String::Lit(msg.str().c_str())));
-    }
-    #endif
-    #endif
+void x10aux::throwArrayIndexOutOfBoundsException(x10_int index, x10_int length) {
+#ifndef NO_EXCEPTIONS
+    char *msg = alloc_printf("%d not in [0,%d)", index, length);
+    throwException(x10::lang::ArrayIndexOutOfBoundsException::_make(String::Lit(msg)));
+#endif
 }
-
 // vim:tabstop=4:shiftwidth=4:expandtab
