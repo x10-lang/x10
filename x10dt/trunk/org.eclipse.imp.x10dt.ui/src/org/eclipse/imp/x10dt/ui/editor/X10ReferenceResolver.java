5fa0497a448d6ea59c20b3abcaf90d3cb488fadd
Robert Fuhrer <rfuhrer@us.ibm.com>
2009-06-22 21:49:16 +0000

    Committed on Tue Jul 29 10:42:49 2008 Fixed indentation.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@9916 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt/trunk/org.eclipse.imp.x10dt.ui/src/org/eclipse/imp/x10dt/ui/editor/X10ReferenceResolver.java
+++ x10dt/trunk/org.eclipse.imp.x10dt.ui/src/org/eclipse/imp/x10dt/ui/editor/X10ReferenceResolver.java
@@ -1,14 +1,14 @@
 /*******************************************************************************
-* Copyright (c) 2008 IBM Corporation.
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Eclipse Public License v1.0
-* which accompanies this distribution, and is available at
-* http://www.eclipse.org/legal/epl-v10.html
-*
-* Contributors:
-*    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
+ * Copyright (c) 2008 IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
 
-*******************************************************************************/
+ *******************************************************************************/
 
 /*
  * (C) Copyright IBM Corporation 2007
@@ -38,75 +38,74 @@ import polyglot.visit.NodeVisitor;
 
 public class X10ReferenceResolver implements IReferenceResolver, ILanguageService {
     /**
-     * Get the target for a given referencing source node in the AST represented
-     * by a given ParseController.
+     * Get the target for a given referencing source node in the AST represented by a given ParseController.
      */
     public Object getLinkTarget(Object node, IParseController parseController) {
-	if (node instanceof Ambiguous) {
-	    return null;
-	}
-	if (node instanceof Id) {
-	    Id id= (Id) node;
-	    node= findParent(id, parseController);
-	}
-	if (node instanceof TypeNode) {
-	    TypeNode typeNode= (TypeNode) node;
-	    PolyglotNodeLocator locator= (PolyglotNodeLocator) parseController.getNodeLocator();
-	    Node parent= (Node) locator.findParentNode(parseController.getCurrentAst(), typeNode.position().offset(), typeNode.position().endOffset());
+        if (node instanceof Ambiguous) {
+            return null;
+        }
+        if (node instanceof Id) {
+            Id id= (Id) node;
+            node= findParent(id, parseController);
+        }
+        if (node instanceof TypeNode) {
+            TypeNode typeNode= (TypeNode) node;
+            PolyglotNodeLocator locator= (PolyglotNodeLocator) parseController.getNodeLocator();
+            Node parent= (Node) locator.findParentNode(parseController.getCurrentAst(), typeNode.position().offset(), typeNode.position().endOffset());
 
-	    if (parent instanceof New) {
-		New n= (New) parent;
-		return n.constructorInstance();
-	    }
-	    return typeNode.type();
-	} else if (node instanceof Call) {
-	    Call call= (Call) node;
-	    MethodInstance mi= call.methodInstance();
-	    if (mi != null)
-		return mi.declaration();
-	} else if (node instanceof Field) {
-	    Field field= (Field) node;
-	    FieldInstance fi= field.fieldInstance();
-	    if (fi != null)
-		return fi.declaration();
-	} else if (node instanceof Local) {
-	    Local local= (Local) node;
-	    LocalInstance li= local.localInstance();
-	    if (li != null)
-		return li.declaration();
-	}
-	return null; // If it's not something we know how to resolve, just return the node itself
+            if (parent instanceof New) {
+                New n= (New) parent;
+                return n.constructorInstance();
+            }
+            return typeNode.type();
+        } else if (node instanceof Call) {
+            Call call= (Call) node;
+            MethodInstance mi= call.methodInstance();
+            if (mi != null)
+                return mi.declaration();
+        } else if (node instanceof Field) {
+            Field field= (Field) node;
+            FieldInstance fi= field.fieldInstance();
+            if (fi != null)
+                return fi.declaration();
+        } else if (node instanceof Local) {
+            Local local= (Local) node;
+            LocalInstance li= local.localInstance();
+            if (li != null)
+                return li.declaration();
+        }
+        return null; // If it's not something we know how to resolve, just return the node itself
     }
-    private Object findParent(Id id, IParseController parseController) {
-	PolyglotNodeLocator locator= (PolyglotNodeLocator) parseController.getNodeLocator();
 
-	return locator.findParentNode(parseController.getCurrentAst(), id.position().offset(), id.position().endOffset());
+    private Object findParent(Id id, IParseController parseController) {
+        PolyglotNodeLocator locator= (PolyglotNodeLocator) parseController.getNodeLocator();
+
+        return locator.findParentNode(parseController.getCurrentAst(), id.position().offset(), id.position().endOffset());
     }
 
     public static Node findVarDefinition(Local local, Node ast) {
-	final LocalInstance li= local.localInstance();
-	final LocalDecl ld[]= new LocalDecl[1];
-	NodeVisitor finder= new NodeVisitor() {
-	    public NodeVisitor enter(Node n) {
-		if (n instanceof LocalDecl) {
-		    LocalDecl thisLD= (LocalDecl) n;
-		    if (thisLD.localInstance() == li)
-			ld[0]= thisLD;
-		}
-		return super.enter(n);
-	    }
-	};
-	ast.visit(finder);
-	return ld[0];
+        final LocalInstance li= local.localInstance();
+        final LocalDecl ld[]= new LocalDecl[1];
+        NodeVisitor finder= new NodeVisitor() {
+            public NodeVisitor enter(Node n) {
+                if (n instanceof LocalDecl) {
+                    LocalDecl thisLD= (LocalDecl) n;
+                    if (thisLD.localInstance() == li)
+                        ld[0]= thisLD;
+                }
+                return super.enter(n);
+            }
+        };
+        ast.visit(finder);
+        return ld[0];
     }
 
     /**
-     * Get the text associated with a given node for use in a link
-     * from (or to) that node
+     * Get the text associated with a given node for use in a link from (or to) that node
      */
     public String getLinkText(Object node) {
-	// In the original, link text is determined by ISourceHyperlink,
-	// and this is all that does
-	return node.toString();
+        // In the original, link text is determined by ISourceHyperlink,
+        // and this is all that does
+        return node.toString();
     }
 }
