d35518a50692984e45c8da1617d9ba9a17d057ae
Robert Fuhrer <rfuhrer@us.ibm.com>
2009-06-22 21:44:00 +0000

    Committed on Mon Dec 19 13:38:58 2005 Removed dead code, variables and imports.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@9600 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt/trunk/org.eclipse.imp.x10dt.core/src/org/eclipse/imp/x10dt/core/builder/X10ProjectNature.java
+++ x10dt/trunk/org.eclipse.imp.x10dt.core/src/org/eclipse/imp/x10dt/core/builder/X10ProjectNature.java
@@ -1,12 +1,5 @@
 package com.ibm.watson.safari.x10.builder;
 
-import java.util.HashMap;
-
-import org.eclipse.core.resources.ICommand;
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IProjectDescription;
-import org.eclipse.core.resources.IProjectNature;
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.uide.core.ProjectNatureBase;
 import org.eclipse.uide.runtime.IPluginLog;
 import com.ibm.watson.safari.x10.X10Plugin;
