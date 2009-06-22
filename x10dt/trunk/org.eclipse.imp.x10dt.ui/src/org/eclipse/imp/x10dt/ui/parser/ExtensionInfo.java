ebe526841f9fd36d204de7968769549fe2503c39
Robert Fuhrer <rfuhrer@us.ibm.com>
2009-06-22 21:54:41 +0000

    Committed on Mon May 11 13:42:56 2009 No longer uses deprecated LPG runtime API. Removed commented-out code and bogus stale comments.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@10241 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt/trunk/org.eclipse.imp.x10dt.ui/src/org/eclipse/imp/x10dt/ui/parser/ExtensionInfo.java
+++ x10dt/trunk/org.eclipse.imp.x10dt.ui/src/org/eclipse/imp/x10dt/ui/parser/ExtensionInfo.java
@@ -72,56 +72,35 @@ public class ExtensionInfo extends polyglot.ext.x10.ExtensionInfo
     }
     
     public Parser parser(Reader reader, FileSource source, ErrorQueue eq) {
-        // try {
-            //
-            // X10Lexer may be invoked using one of two constructors.
-            // One constructor takes as argument a string representing
-            // a (fully-qualified) filename; the other constructor takes
-            // as arguments a (file) Reader and a string representing the
-            // name of the file in question. Invoking the first
-            // constructor is more efficient because a buffered File is created
-            // from that string and read with one (read) operation. However,
-            // we depend on Polyglot to provide us with a fully qualified
-            // name for the file. In Version 1.3.0, source.name() yielded a
-            // fully-qualified name. In 1.3.2, source.path() yields a fully-
-            // qualified name. If this assumption still holds then the 
-            // first constructor will work.
-            // The advantage of using the Reader constructor is that it
-            // will always work, though not as efficiently.
-            //
-            // X10Lexer x10_lexer = new X10Lexer(reader, source.name());
-            //
-            if (reader instanceof CharBufferReader)
-            {
-                x10_lexer.initialize(((CharBufferReader) reader).getBuffer(), source.path());
-                x10_parser.initialize(typeSystem(), nodeFactory(), source, eq); // PORT1.7 Now created early, but initialized here once the source is known
-//                x10_parser = new X10Parser(x10_lexer, ts, nf, source, eq); // Create the parser
-                x10_lexer.lexer(x10_parser.getIPrsStream());
-                x10_parser.getIPrsStream().setMessageHandler(handler);
-                return x10_parser; // Parse the token stream to produce an AST
-            }
-            if (reader instanceof Reader) {
-            	char[] buffer= StreamUtils.readReaderContents(reader).toCharArray();
-            	x10_lexer.initialize(buffer, source.path());
-            	x10_parser.initialize(typeSystem(), nodeFactory(), source, eq);
-            	x10_lexer.lexer(x10_parser.getIPrsStream());
-            	x10_parser.getIPrsStream().setMessageHandler(handler);
-            	return x10_parser;
-            }
-            //
-            // TODO: FIX ME! FIX ME!! FIX ME!!!
-            //
-            // Note that this temporary code will not work properly if the
-            // input in question is in an editor buffer that has been altered.
-            // When using IMP, it is important that all request for new
-            // source be processed by IMP. However, we cannot do so now without
-            // changing the base Polyglot code.
-            //
-            else return super.parser(reader, source, eq);
-        //} catch (IOException e) {
-        //    e.printStackTrace();
-        //}
-        //throw new IllegalStateException("Could not parse " + source.path());
+        //
+        // X10Lexers may be created using one of two constructors.
+        // The first takes a string representing a full-qualified filename;
+        // the other takes a (file) Reader and a string representing the
+        // name of the file in question. Invoking the first is more efficient
+        // because a buffered stream is created from that string and read in
+        // in a single operation. However, we depend on Polyglot to provide a
+        // fully qualified file path. In Version 1.3.0, source.name() yielded
+        // a fully-qualified name. In 1.3.2, source.path() yields a fully-
+        // qualified name. If this assumption still holds then the first
+        // constructor will work. The advantage of using the Reader constructor
+        // is that it will always work, though not as efficiently.
+        //
+        if (reader instanceof CharBufferReader) {
+            x10_lexer.reset(((CharBufferReader) reader).getBuffer(), source.path());
+            x10_parser.initialize(typeSystem(), nodeFactory(), source, eq); // PORT1.7 Now created early, but initialized here once the source is known
+            x10_lexer.lexer(x10_parser.getIPrsStream());
+            x10_parser.getIPrsStream().setMessageHandler(handler);
+            return x10_parser; // Parse the token stream to produce an AST
+        }
+        if (reader instanceof Reader) {
+            char[] buffer= StreamUtils.readReaderContents(reader).toCharArray();
+            x10_lexer.reset(buffer, source.path());
+            x10_parser.initialize(typeSystem(), nodeFactory(), source, eq);
+            x10_lexer.lexer(x10_parser.getIPrsStream());
+            x10_parser.getIPrsStream().setMessageHandler(handler);
+            return x10_parser;
+        }
+        return super.parser(reader, source, eq);
     }
 
     /**
