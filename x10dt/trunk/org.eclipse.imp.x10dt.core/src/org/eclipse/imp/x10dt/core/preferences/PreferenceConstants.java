ff89a724ff07bc11e2f837a799f3aa37ad295cd0
Robert Fuhrer <rfuhrer@us.ibm.com>
2009-06-22 21:28:01 +0000

    Committed on Sun Feb 19 11:20:20 2006 Added JavaDoc.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@8639 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt/trunk/org.eclipse.imp.x10dt.core/src/org/eclipse/imp/x10dt/core/preferences/PreferenceConstants.java
+++ x10dt/trunk/org.eclipse.imp.x10dt.core/src/org/eclipse/imp/x10dt/core/preferences/PreferenceConstants.java
@@ -1,20 +1,51 @@
 package com.ibm.watson.safari.x10.preferences;
 
 /**
- * Constant definitions for X10 plug-in preferences
+ * Holds the ID's of the various preferences for the X10 plugin.
  */
 public class PreferenceConstants {
+    /**
+     * The path to the x10.common directory
+     */
     public static final String P_X10COMMON_PATH= "commonPath";
 
+    /**
+     * The path to the X10 configuration file (e.g. standard.cfg)
+     */
     public static final String P_X10CONFIG_FILE= "configFile";
 
+    /**
+     * true if the builder and various other entities should emit diagnostic
+     * messages to the plugin log while running
+     */
     public static final String P_EMIT_MESSAGES= "emitMessages";
 
+    /**
+     * true if the X10 builder should automatically add x10.runtime/classes
+     * to the Polyglot build path.
+     * @deprecated This has been superceded by the "New X10 Project" wizard
+     * adding explicit entries to the classpath
+     */
     public static final String P_AUTO_ADD_RUNTIME= "autoAddRuntimme";
 
+    /**
+     * The path to the compiler template directory (used by X10PrettyPrinterVisitor).
+     * A mirror of the COMPILER_FRAGMENT_DATA_DIRECTORY setting in the X10
+     * configuration file (e.g. standard.cfg).
+     */
     public static final String P_COMPILER_DATA_DIR= "compilerDataDir";
 
+    /**
+     * The runtime sampling frequency.
+     * A mirror of the SAMPLING_FREQUENCY setting in the X10
+     * configuration file (e.g. standard.cfg).
+     */
     public static final String P_SAMPLING_FREQ= "samplingFreq";
 
+    /**
+     * 
+     * A mirror of the STATISTICS_DISABLE setting in the X10
+     * configuration file (e.g. standard.cfg).
+     */
     public static final String P_STATS_DISABLE= "statsDisable";
 }
