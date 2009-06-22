2740f1338c584c4238d5bd821e6c13ec2f9901be
Robert Fuhrer <rfuhrer@us.ibm.com>
2009-06-22 21:41:57 +0000

    Committed on Fri May 22 18:21:50 2009 The refactoring name is now (mostly?) defined in just one place.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@9476 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt/trunk/x10.refactoring/src/x10/refactorings/actions/ExtractAsyncRefactoringAction.java
+++ x10dt/trunk/x10.refactoring/src/x10/refactorings/actions/ExtractAsyncRefactoringAction.java
@@ -2,28 +2,26 @@ package x10.refactorings.actions;
 
 import java.util.ResourceBundle;
 
-import org.eclipse.imp.editor.UniversalEditor;
 import org.eclipse.imp.refactoring.RefactoringStarter;
+import org.eclipse.ui.texteditor.ITextEditor;
 import org.eclipse.ui.texteditor.TextEditorAction;
 
 import x10.refactorings.ExtractAsyncRefactoring;
 import x10.refactorings.wizards.ExtractAsyncWizard;
 
 public class ExtractAsyncRefactoringAction extends TextEditorAction {
-    // private final UniversalEditor fEditor;
-
     static ResourceBundle ResBundle = ResourceBundle.getBundle("x10.refactorings.RefactoringMessages");
 
-    public ExtractAsyncRefactoringAction(UniversalEditor editor) {
-        super(ResBundle, "extractConcurrent.", editor);
-        // fEditor= editor;
+    public ExtractAsyncRefactoringAction(ITextEditor editor) {
+        super(ResBundle, "extractAsync.", editor);
     }
 
     public void run() {
-        final ExtractAsyncRefactoring refactoring = new ExtractAsyncRefactoring((UniversalEditor) this.getTextEditor());
+        final ExtractAsyncRefactoring refactoring = new ExtractAsyncRefactoring(getTextEditor());
 
-        if (refactoring != null)
-            new RefactoringStarter().activate(refactoring, new ExtractAsyncWizard(refactoring, "Extract Concurrent"),
-                    this.getTextEditor().getSite().getShell(), "Extract Concurrent", false);
+        if (refactoring != null) {
+            new RefactoringStarter().activate(refactoring, new ExtractAsyncWizard(refactoring),
+                    getTextEditor().getSite().getShell(), refactoring.getName(), false);
+        }
     }
 }
