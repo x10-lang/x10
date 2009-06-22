acc0f10caa28e5652bcace2d3518e436c39d65f8
Beth Tibbitts <btibbitts@us.ibm.com>
2009-06-22 21:57:15 +0000

    Committed on Fri May 29 17:48:57 2009 remove printlns; handle packages more coherently and allowing multiple source folders and packages
    git-svn-id: svn://svn.code.sf.net/p/x10/code@10395 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt/trunk/org.eclipse.imp.x10dt.core/src/org/eclipse/imp/x10dt/core/builder/CheckPackageDeclVisitor.java
+++ x10dt/trunk/org.eclipse.imp.x10dt.core/src/org/eclipse/imp/x10dt/core/builder/CheckPackageDeclVisitor.java
@@ -6,6 +6,12 @@ package org.eclipse.imp.x10dt.core.builder;
 import java.io.File;
 
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.imp.x10dt.core.X10DTCorePlugin;
+import org.eclipse.jdt.core.IClasspathEntry;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.JavaModelException;
 
 import polyglot.ast.Node;
 import polyglot.ast.PackageNode;
@@ -26,11 +32,10 @@ public class CheckPackageDeclVisitor extends NodeVisitor {
     }
     @Override
     public NodeVisitor begin() {
-        String path= fJob.source().path();
-        // BRT don't bother looking for dependencies if we're in jar/zip
-        //PORT1.7
+        String path= fJob.source().path(); 
+        //PORT1.7 don't bother looking for dependencies if we're in jar/zip
         if(path.endsWith(".jar")|| path.endsWith(".zip")) {
-        	System.out.println("looking for resource in zip/jar???");
+        	System.out.println("CheckPackageDeclVisitor.begin() looking for resource (path="+path+") in zip/jar???");
         	return null;
         }
         return super.begin();
@@ -38,6 +43,8 @@ public class CheckPackageDeclVisitor extends NodeVisitor {
     private void checkPackage(String declaredPkg, String actualPkg, Position pos) {
         if (!actualPkg.equals(declaredPkg)) {
             fJob.extensionInfo().compiler().errorQueue().enqueue(new ErrorInfo(ErrorInfo.SEMANTIC_ERROR, "Declared package doesn't match source file location.", pos));
+            System.out.println("===> actualPkg: "+actualPkg+"  declaredPkg: "+declaredPkg);
+            int stophere=1;
         }
     }
 
@@ -48,8 +55,9 @@ public class CheckPackageDeclVisitor extends NodeVisitor {
             Source src= fJob.source();
             String declaredPkg= pkg.package_().get().fullName().name().toString();
             String actualPkg= determineActualPackage(src);
-
-            checkPackage(declaredPkg, actualPkg, pkg.position());
+            if(actualPkg!=null) {
+            	checkPackage(declaredPkg, actualPkg, pkg.position());    	
+            }
             fSeenPkg= true;
         }
         return super.enter(n);
@@ -57,22 +65,44 @@ public class CheckPackageDeclVisitor extends NodeVisitor {
 
     private String determineActualPackage(Source src) {
         String srcPath= src.path();
-        String projPath= fProject.getLocation().toOSString();
-        String pkgPath;
+        String srcFileName= src.name();
+        String wsPath= fProject.getLocation().removeLastSegments(1).toOSString();
+        String pkgPath; // rel to ws loc
 
-        if (srcPath.startsWith(projPath)) {
-            pkgPath= srcPath.substring(projPath.length()+1);
+        if (srcPath.startsWith(wsPath)) {
+            pkgPath= srcPath.substring(wsPath.length());
         } else {
             pkgPath= srcPath;
         }
-        if (pkgPath.startsWith("src" + File.separator)) {
-            pkgPath= pkgPath.substring(4);
-        }
-        if (pkgPath.length() == src.name().length()) {
-            return ""; // It's in the default pkg
-        } else {
-            return pkgPath.substring(0, pkgPath.length() - src.name().length() - 1).replace(File.separatorChar, '.');
-        }
+
+        IJavaProject javaProject = JavaCore.create(fProject);
+        IClasspathEntry[] cpEntries;
+		try {
+			cpEntries = javaProject.getResolvedClasspath(true);
+		} catch (JavaModelException e) {
+			X10DTCorePlugin.getInstance().logException(e.getMessage(), e);
+			// remember to test if java nature is still on cpp-backend projects etc.
+			return null;
+		}
+
+        for (int i = 0; i < cpEntries.length; i++) {
+			IClasspathEntry classpathEntry = cpEntries[i];
+			if (classpathEntry.getEntryKind() == IClasspathEntry.CPE_SOURCE) {
+				IPath cpPath= classpathEntry.getPath(); // ws rel
+				String cpPathStr = cpPath.toOSString();
+
+				if (pkgPath.startsWith(cpPathStr)) {
+					int cpPathLen = cpPathStr.length();
+					String srcFolderRelPath= pkgPath.substring(cpPathLen + 1, pkgPath.length() - srcFileName.length() );
+					String result= srcFolderRelPath.replace(File.separatorChar, '.');
+					if(result.endsWith(".")) {
+						result=result.substring(0, result.length()-1);
+					}
+					return result;
+				}
+			}
+		}
+        return null;
     }
 
     @Override
