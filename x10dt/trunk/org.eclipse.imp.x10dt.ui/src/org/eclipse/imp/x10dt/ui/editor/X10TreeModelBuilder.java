615b3c4cc5d58e339b8224caee23f1656001db55
Matthew Kaplan <user@domain>
2009-06-22 21:49:19 +0000

    Committed on Wed May 21 18:02:57 2008 Copyright added
    git-svn-id: svn://svn.code.sf.net/p/x10/code@9919 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt/trunk/org.eclipse.imp.x10dt.ui/src/org/eclipse/imp/x10dt/ui/editor/X10TreeModelBuilder.java
+++ x10dt/trunk/org.eclipse.imp.x10dt.ui/src/org/eclipse/imp/x10dt/ui/editor/X10TreeModelBuilder.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+* Copyright (c) 2008 IBM Corporation.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
+
+*******************************************************************************/
+
 package org.eclipse.imp.x10dt.ui.editor;
 
 import org.eclipse.imp.services.base.TreeModelBuilderBase;
