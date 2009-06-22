c01f3a474d8c09ebaaedbecfa0dbb8dfaaf8e8a7
Robert Fuhrer <rfuhrer@us.ibm.com>
2009-06-22 21:41:23 +0000

    Committed on Fri Dec  2 13:07:42 2005 Now makes sure to initialize X10Preferences static fields from the preference store data. Added an override of refreshPrefs() to set the "x10.configuration" System property from the corresponding preference value.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@9442 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt/trunk/org.eclipse.imp.x10dt.core/src/org/eclipse/imp/x10dt/core/X10Plugin.java
+++ x10dt/trunk/org.eclipse.imp.x10dt.core/src/org/eclipse/imp/x10dt/core/X10Plugin.java
@@ -1,11 +1,13 @@
 package com.ibm.watson.safari.x10;
 
+import java.io.File;
+import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
-import org.eclipse.uide.runtime.IPluginLog;
 import org.eclipse.uide.runtime.UIDEPluginBase;
 import org.osgi.framework.BundleContext;
-
+import com.ibm.watson.safari.x10.preferences.PreferenceConstants;
+import com.ibm.watson.safari.x10.preferences.X10Preferences;
 
 /**
  * The main plugin class to be used in the desktop.
@@ -26,6 +28,13 @@ public class X10Plugin extends UIDEPluginBase {
      */
     public void start(BundleContext context) throws Exception {
 	super.start(context);
+
+	// Initialize the X10Preferences fields with the preference store data.
+	IPreferenceStore prefStore= getPreferenceStore();
+
+	X10Preferences.builderEmitMessages= prefStore.getBoolean(PreferenceConstants.P_EMIT_MESSAGES);
+	X10Preferences.x10CommonPath= prefStore.getString(PreferenceConstants.P_X10COMMON_PATH);
+	X10Preferences.x10ConfigFile= prefStore.getString(PreferenceConstants.P_X10CONFIG_FILE);
     }
 
     /**
@@ -39,11 +48,19 @@ public class X10Plugin extends UIDEPluginBase {
     /**
      * Returns an image descriptor for the image file at the given plug-in relative path.
      * 
-     * @param path
-     *                the path
+     * @param path the path
      * @return the image descriptor
      */
     public static ImageDescriptor getImageDescriptor(String path) {
 	return AbstractUIPlugin.imageDescriptorFromPlugin(kPluginID, path);
+    }
+
+    public void refreshPrefs() {
+        super.refreshPrefs();
+        // Set the "x10.configuration" System property from the corresponding preference
+        // value, since polyglot.ext.x10.Configuration relies on its being set, and a
+        // static initializer there throws an exception if it can't find the config file.
+        final String configFile= (X10Preferences.x10ConfigFile!= null ? X10Preferences.x10ConfigFile : X10Preferences.x10CommonPath + File.separator + "etc" + File.separator + "standard.cfg");
+	System.setProperty("x10.configuration", configFile);
     }
 }
