ce1012f8153e9602ada8d42cab239086547ea6c2
Matthew Kaplan <user@domain>
2009-06-22 21:34:48 +0000

    Committed on Mon Jun  2 16:18:05 2008 Test menu bar tab
    git-svn-id: svn://svn.code.sf.net/p/x10/code@9046 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ x10dt/trunk/org.eclipse.imp.x10dt.ui/src/org/eclipse/imp/x10dt/ui/commands/handlers/FormatCmdHandler.java
@@ -0,0 +1,40 @@
+package org.eclipse.imp.x10dt.ui.commands.handlers;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+import org.eclipse.core.commands.IHandlerListener;
+import org.eclipse.core.commands.AbstractHandler;
+
+
+public class FormatCmdHandler extends AbstractHandler implements IHandler {
+
+	public void addHandlerListener(IHandlerListener handlerListener) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		System.out.println("Formatting...");
+		return null;
+	}
+
+	public boolean isEnabled() {
+		return true;
+	}
+
+	public boolean isHandled() {
+		return true;
+	}
+
+	public void removeHandlerListener(IHandlerListener handlerListener) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ x10dt/trunk/org.eclipse.imp.x10dt.ui/src/org/eclipse/imp/x10dt/ui/views/SoundView.java
@@ -0,0 +1,24 @@
+package org.eclipse.imp.x10dt.ui.views;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class SoundView extends ViewPart {
+
+	public SoundView() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
