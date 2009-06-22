ffac43cd299ac09a3e4431acf43ec470d52af327
Robert Fuhrer <rfuhrer@us.ibm.com>
2009-06-22 21:54:42 +0000

    Committed on Tue May 12 20:44:53 2009 Now defines its own Scheduler class to control what work gets done for each job. Now maintains maps from Sources to parsers, lexers and ASTs. Necessary b/c the ExtensionInfo may encounter many source files while processing the one nominally submitted for compilation.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@10242 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt/trunk/org.eclipse.imp.x10dt.ui/src/org/eclipse/imp/x10dt/ui/parser/ExtensionInfo.java
+++ x10dt/trunk/org.eclipse.imp.x10dt.ui/src/org/eclipse/imp/x10dt/ui/parser/ExtensionInfo.java
@@ -7,107 +7,152 @@
 *
 * Contributors:
 *    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
-
 *******************************************************************************/
 
-/*
- * (C) Copyright IBM Corporation 2007
- * 
- * This file is part of the Eclipse IMP.
- */
 package org.eclipse.imp.x10dt.ui.parser;
 
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.Reader;
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Iterator;
-
-import org.eclipse.imp.utils.StreamUtils;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import lpg.runtime.IMessageHandler;
 import lpg.runtime.Monitor;
 
+import org.eclipse.imp.utils.StreamUtils;
+
+import polyglot.ast.Node;
+import polyglot.frontend.AllBarrierGoal;
 import polyglot.frontend.FileSource;
-import polyglot.frontend.Goal;//PORT1.7 was polyglot.frontend.goals.Goal;
+import polyglot.frontend.Goal;
 import polyglot.frontend.Job;
 import polyglot.frontend.Parser;
+import polyglot.frontend.Scheduler;
 import polyglot.frontend.Source;
+import polyglot.frontend.SourceGoal_c;
 import polyglot.util.ErrorQueue;
 import x10.parser.X10Lexer;
 import x10.parser.X10Parser;
 
 /**
- * Information about our extension of the polyglot compiler
+ * Information about our extension of the polyglot compiler. This derives from
+ * the ExtensionInfo class used by the X10 compiler, and specializes it to create
+ * a parser/scanner that can read from an arbitrary Reader, and to save the parser
+ * and lexer for "interesting" source files for later reference. It also creates a
+ * dummy goal to record the AST of "interesting" source files.
  * @author beth
- *
+ * @author rfuhrer@watson.ibm.com
  */
-public class ExtensionInfo extends polyglot.ext.x10.ExtensionInfo
-{
-    X10Lexer x10_lexer;
-    X10Parser x10_parser;
-    Monitor monitor;
-    IMessageHandler handler;
-    
-    ExtensionInfo(Monitor monitor, IMessageHandler handler) {
+public class ExtensionInfo extends polyglot.ext.x10.ExtensionInfo {
+    private X10Lexer x10_lexer;
+    private X10Parser x10_parser;
+    private final Monitor monitor;
+    private final IMessageHandler handler;
+    private final Set<Source> fInterestingSources = new HashSet<Source>();
+    private final Map<Source,Node> fInterestingASTs = new HashMap<Source,Node>();
+    private final Map<Source,X10Parser> fInterestingParsers = new HashMap<Source,X10Parser>();
+    private final Map<Source,X10Lexer> fInterestingLexers = new HashMap<Source,X10Lexer>();
+
+    public ExtensionInfo(Monitor monitor, IMessageHandler handler) {
         this.monitor = monitor;
         this.handler = handler;
         x10_lexer = new X10Lexer();
         x10_lexer.reset(new char[0], ""); // PORT1.7 BOB HACK to make sure lexer has a lexstream, needed by remapTerminalSymbols() (which probably shouldn't need it!)
         x10_parser = new X10Parser(x10_lexer.getILexStream()); // PORT1.7 Create this early
     }
-    
-    public X10Lexer getLexer() { return x10_lexer; }
-    public X10Parser getParser() { return x10_parser; }
 
-    public Job getJob(Source source) {
-        Collection jobs = scheduler.jobs();
-        for (Iterator i = jobs.iterator(); i.hasNext(); )
-        {
-            Job job = (Job) i.next();
-            if (job.source() == source)
-                return job;
-        }
-        return null;
+    public void setInterestingSources(Collection<Source> sources) {
+        fInterestingSources.clear();
+        fInterestingASTs.clear();
+        fInterestingLexers.clear();
+        fInterestingParsers.clear();
+        fInterestingSources.addAll(sources);
     }
-    
+
+    public X10Lexer getLexerFor(Source src) { return fInterestingLexers.get(src); }
+    public X10Parser getParserFor(Source src) { return fInterestingParsers.get(src); }
+    public Node getASTFor(Source src) { return fInterestingASTs.get(src); }
+
+    @Override
+    protected Scheduler createScheduler() {
+        return new X10Scheduler(this) {
+            @Override
+            public List<Goal> goals(Job job) {
+                List<Goal> goals = new ArrayList<Goal>();
+                // This is essentially the list of goals specified by the base class,
+                // up through and including type-checking.
+                goals.add(Parsed(job));
+                goals.add(TypesInitialized(job));
+                goals.add(ImportTableInitialized(job));
+                goals.add(CastRewritten(job));
+                goals.add(PropagateAnnotations(job));
+                goals.add(PreTypeCheck(job));
+                goals.add(TypeChecked(job));
+
+                if (ExtensionInfo.this.fInterestingSources.contains(job.source())) {
+                    goals.add(RetrieveAST(job));
+                }
+
+                goals.add(End(job));
+                return goals;
+            }
+            /**
+             * This goal simply retrieves the AST for the given job and squirrels it
+             * away into the map fInterestingASTs.
+             */
+            Goal RetrieveAST(Job job) {
+                return new SourceGoal_c("AST retriever", job) {
+                    @Override
+                    public boolean runTask() {
+                        ExtensionInfo.this.fInterestingASTs.put(job().source(), job().ast());
+                        return true;
+                    }
+                }.intern(scheduler);
+            }
+            @Override
+            protected Goal EndAll() { // a dummy goal that replaces PostCompiled
+                return new AllBarrierGoal(this) {
+                    @Override
+                    public Goal prereqForJob(Job job) {
+                        if (scheduler.shouldCompile(job)) {
+                            return scheduler.End(job);
+                        }
+                        else {
+                            return new SourceGoal_c("DummyEnd", job) {
+                                public boolean runTask() { return true; }
+                            }.intern(scheduler);
+                        }
+                    }
+                };
+            }
+        };
+    }
+
     public Parser parser(Reader reader, FileSource source, ErrorQueue eq) {
-        //
-        // X10Lexers may be created using one of two constructors.
-        // The first takes a string representing a full-qualified filename;
-        // the other takes a (file) Reader and a string representing the
-        // name of the file in question. Invoking the first is more efficient
-        // because a buffered stream is created from that string and read in
-        // in a single operation. However, we depend on Polyglot to provide a
-        // fully qualified file path. In Version 1.3.0, source.name() yielded
-        // a fully-qualified name. In 1.3.2, source.path() yields a fully-
-        // qualified name. If this assumption still holds then the first
-        // constructor will work. The advantage of using the Reader constructor
-        // is that it will always work, though not as efficiently.
-        //
+        // PORT1.7 Create a new lexer/parser for every parse - unlike with 1.5,
+        // this method can be called on behalf of different source files (including
+        // source files residing in the runtime jar), and the front-end can be in
+        // the middle of processing one source when it starts processing another.
+        x10_lexer = new X10Lexer();
+        x10_lexer.reset(new char[0], ""); // PORT1.7 BOB HACK to make sure lexer has a lexstream, needed by remapTerminalSymbols() (which probably shouldn't need it!)
+        x10_parser = new X10Parser(x10_lexer.getILexStream());
         if (reader instanceof CharBufferReader) {
             x10_lexer.reset(((CharBufferReader) reader).getBuffer(), source.path());
-            x10_parser.initialize(typeSystem(), nodeFactory(), source, eq); // PORT1.7 Now created early, but initialized here once the source is known
-            x10_lexer.lexer(x10_parser.getIPrsStream());
-            x10_parser.getIPrsStream().setMessageHandler(handler);
-            return x10_parser; // Parse the token stream to produce an AST
-        }
-        if (reader instanceof Reader) {
+        } else {
             char[] buffer= StreamUtils.readReaderContents(reader).toCharArray();
             x10_lexer.reset(buffer, source.path());
-            x10_parser.initialize(typeSystem(), nodeFactory(), source, eq);
-            x10_lexer.lexer(x10_parser.getIPrsStream());
-            x10_parser.getIPrsStream().setMessageHandler(handler);
-            return x10_parser;
         }
-        return super.parser(reader, source, eq);
-    }
-
-    /**
-     * Return the <code>Goal</code> to compile the source file associated with
-     * <code>job</code> to completion.
-     */
-    public Goal getCompileGoal(Job job) {  
-        return scheduler.TypeChecked(job); // CodeGenerated(job);
+        x10_parser.initialize(typeSystem(), nodeFactory(), source, eq); // PORT1.7 Now created early, but initialized here once the source is known
+        x10_lexer.lexer(x10_parser.getIPrsStream());
+        x10_parser.getIPrsStream().setMessageHandler(handler);
+        if (fInterestingSources.contains(source)) {
+            fInterestingLexers.put(source, x10_lexer);
+            fInterestingParsers.put(source, x10_parser);
+        }
+        return x10_parser;
     }
 }
