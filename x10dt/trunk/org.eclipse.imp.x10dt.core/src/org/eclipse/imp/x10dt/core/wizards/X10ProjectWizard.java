c1fdec54d7441db24452b971204e3cb2c8e7742b
Emmanuel Geay <egeay@us.ibm.com>
2009-06-22 21:52:50 +0000

    Committed on Wed Apr 29 13:51:05 2009 - Changed window title name. - Reformat.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@10130 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt/trunk/org.eclipse.imp.x10dt.core/src/org/eclipse/imp/x10dt/core/wizards/X10ProjectWizard.java
+++ x10dt/trunk/org.eclipse.imp.x10dt.core/src/org/eclipse/imp/x10dt/core/wizards/X10ProjectWizard.java
@@ -1,14 +1,14 @@
 /*******************************************************************************
-* Copyright (c) 2008 IBM Corporation.
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Eclipse Public License v1.0
-* which accompanies this distribution, and is available at
-* http://www.eclipse.org/legal/epl-v10.html
-*
-* Contributors:
-*    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
+ * Copyright (c) 2008 IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
 
-*******************************************************************************/
+ *******************************************************************************/
 
 /*
  * (C) Copyright IBM Corporation 2007
@@ -61,85 +61,88 @@ import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.wizards.newresource.BasicNewResourceWizard;
 
 public class X10ProjectWizard extends Wizard implements INewWizard {
-    private X10ProjectWizardFirstPage fFirstPage;
+  private X10ProjectWizardFirstPage fFirstPage;
 
-    private NewProjectWizardSecondPage fSecondPage;
+  private NewProjectWizardSecondPage fSecondPage;
 
-    public X10ProjectWizard() {
-	super();
-	// setDefaultPageImageDescriptor(JavaPluginImages.DESC_WIZBAN_NEWJPRJ);
-	// setDialogSettings(JavaPlugin.getDefault().getDialogSettings());
-	setWindowTitle("New X10 Project");
+  public X10ProjectWizard() {
+    super();
+    // setDefaultPageImageDescriptor(JavaPluginImages.DESC_WIZBAN_NEWJPRJ);
+    // setDialogSettings(JavaPlugin.getDefault().getDialogSettings());
+    setWindowTitle("New X10 Project (Java back-end)");
+  }
+
+  public boolean performFinish() {
+    IWorkspaceRunnable op = new IWorkspaceRunnable() {
+      public void run(IProgressMonitor monitor) throws CoreException, OperationCanceledException {
+        try {
+          finishPage(monitor);
+        } catch (InterruptedException e) {
+          throw new OperationCanceledException(e.getMessage());
+        }
+      }
+    };
+    try {
+      ISchedulingRule rule = null;
+      Job job = Platform.getJobManager().currentJob();
+      if (job != null)
+        rule = job.getRule();
+      IRunnableWithProgress runnable = null;
+      if (rule != null)
+        runnable = new WorkbenchRunnableAdapter(op, rule, true);
+      else
+        runnable = new WorkbenchRunnableAdapter(op, getSchedulingRule());
+      getContainer().run(canRunForked(), true, runnable);
+    } catch (InvocationTargetException e) {
+      handleFinishException(getShell(), e);
+      return false;
+    } catch (InterruptedException e) {
+      return false;
     }
+    return true;
+  }
 
-    public boolean performFinish() {
-	IWorkspaceRunnable op= new IWorkspaceRunnable() {
-	    public void run(IProgressMonitor monitor) throws CoreException, OperationCanceledException {
-		try {
-		    finishPage(monitor);
-		} catch (InterruptedException e) {
-		    throw new OperationCanceledException(e.getMessage());
-		}
-	    }
-	};
-	try {
-	    ISchedulingRule rule= null;
-	    Job job= Platform.getJobManager().currentJob();
-	    if (job != null)
-		rule= job.getRule();
-	    IRunnableWithProgress runnable= null;
-	    if (rule != null)
-		runnable= new WorkbenchRunnableAdapter(op, rule, true);
-	    else
-		runnable= new WorkbenchRunnableAdapter(op, getSchedulingRule());
-	    getContainer().run(canRunForked(), true, runnable);
-	} catch (InvocationTargetException e) {
-	    handleFinishException(getShell(), e);
-	    return false;
-	} catch (InterruptedException e) {
-	    return false;
-	}
-	return true;
-    }
+  /**
+   * Returns the scheduling rule for creating the element.
+   */
+  protected ISchedulingRule getSchedulingRule() {
+    return ResourcesPlugin.getWorkspace().getRoot(); // look all by default
+  }
 
-    /**
-     * Returns the scheduling rule for creating the element.
-     */
-    protected ISchedulingRule getSchedulingRule() {
-	return ResourcesPlugin.getWorkspace().getRoot(); // look all by default
-    }
+  protected boolean canRunForked() {
+    return true;
+  }
 
-    protected boolean canRunForked() {
-	return true;
-    }
+  protected void handleFinishException(Shell shell, InvocationTargetException e) {
+    String title = NewWizardMessages.NewElementWizard_op_error_title;
+    String message = NewWizardMessages.NewElementWizard_op_error_message;
+    ExceptionHandler.handle(e, shell, title, message);
+  }
 
-    protected void handleFinishException(Shell shell, InvocationTargetException e) {
-	String title= NewWizardMessages.NewElementWizard_op_error_title;
-	String message= NewWizardMessages.NewElementWizard_op_error_message;
-	ExceptionHandler.handle(e, shell, title, message);
-    }
+  /*
+   * @see Wizard#addPages
+   */
+  public void addPages() {
+    super.addPages();
+    fFirstPage = new X10ProjectWizardFirstPage();
+    addPage(fFirstPage);
+    fSecondPage = new X10ProjectWizardSecondPage(fFirstPage);
+    addPage(fSecondPage);
+  }
 
-    /*
-     * @see Wizard#addPages
-     */
-    public void addPages() {
-	super.addPages();
-	fFirstPage= new X10ProjectWizardFirstPage();
-	addPage(fFirstPage);
-	fSecondPage= new X10ProjectWizardSecondPage(fFirstPage);
-	addPage(fSecondPage);
-    }
+  /*
+   * (non-Javadoc)
+   * 
+   * @see org.eclipse.jdt.internal.ui.wizards.NewElementWizard#finishPage(org.eclipse.core.runtime.IProgressMonitor)
+   */
+  protected void finishPage(IProgressMonitor monitor) throws InterruptedException, CoreException {
+    fSecondPage.performFinish(monitor); // use the full progress monitor
+  }
 
-    /* (non-Javadoc)
-     * @see org.eclipse.jdt.internal.ui.wizards.NewElementWizard#finishPage(org.eclipse.core.runtime.IProgressMonitor)
-     */
-    protected void finishPage(IProgressMonitor monitor) throws InterruptedException, CoreException {
-	fSecondPage.performFinish(monitor); // use the full progress monitor
-    }
+  public void init(IWorkbench workbench, IStructuredSelection selection) {
+  }
 
-    public void init(IWorkbench workbench, IStructuredSelection selection) {}
-
-    public void selectAndReveal(IFile newFile) {
-        BasicNewResourceWizard.selectAndReveal(newFile, PlatformUI.getWorkbench().getActiveWorkbenchWindow());
-    }
+  public void selectAndReveal(IFile newFile) {
+    BasicNewResourceWizard.selectAndReveal(newFile, PlatformUI.getWorkbench().getActiveWorkbenchWindow());
+  }
 }
