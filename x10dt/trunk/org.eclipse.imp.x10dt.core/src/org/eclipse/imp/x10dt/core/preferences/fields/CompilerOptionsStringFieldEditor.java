158b748302538093b7b57ecf5739db40662bafc5
Matthew Kaplan <user@domain>
2009-06-22 21:56:28 +0000

    Committed on Mon Jul 28 16:31:15 2008 Options-line error tuning and adaptation to Igor's recommendations re: how to make safe changes to X10 compiler options processing.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@10348 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt/trunk/org.eclipse.imp.x10dt.core/src/org/eclipse/imp/x10dt/core/preferences/fields/CompilerOptionsStringFieldEditor.java
+++ x10dt/trunk/org.eclipse.imp.x10dt.core/src/org/eclipse/imp/x10dt/core/preferences/fields/CompilerOptionsStringFieldEditor.java
@@ -8,7 +8,10 @@ import org.eclipse.imp.preferences.PreferencesTab;
 import org.eclipse.jface.preference.PreferencePage;
 import org.eclipse.swt.widgets.Composite;
 
+import polyglot.ext.x10.Configuration;
+import polyglot.main.Main;
 import polyglot.main.UsageError;
+import x10.runtime.util.OptionsError;
 
 public class CompilerOptionsStringFieldEditor extends StringFieldEditor {
 
@@ -29,10 +32,14 @@ public class CompilerOptionsStringFieldEditor extends StringFieldEditor {
 			Set sources = new HashSet();
 			sources.add(".");
 
-			polyglot.main.Options options = new polyglot.ext.x10.X10CompilerOptions(null); // Doesn't actually need an ExtensionInfo unless you ask about the version current directory
 			String[] args = argString.split(" ");
-			options.parseCommandLine(args, sources);
+			parseCommandLine(args, sources);
 		} catch (UsageError e) {
+			String msg = e.getMessage();
+			// Inform the prefs UI component about the validation failure
+			setErrorMessage(e.getMessage());
+			return false;
+		} catch (OptionsError e) {
 			String msg = e.getMessage();
 			// Inform the prefs UI component about the validation failure
 			setErrorMessage(e.getMessage());
@@ -40,5 +47,41 @@ public class CompilerOptionsStringFieldEditor extends StringFieldEditor {
 		}
 		return super.doCheckState();
 	}
+	
+    /**
+     * Copied from polyglot.main to permit handling of new exception: OptionsError, introduced for x10dt error handling
+     * Parse the command line
+     * 
+     * @throws UsageError if the usage is incorrect.
+     */
+    private void parseCommandLine(String args[], Set source) throws UsageError, OptionsError {
+        if(args.length < 1) {
+            throw new UsageError("No command line arguments given");
+        }
+		polyglot.ext.x10.X10CompilerOptions options = new polyglot.ext.x10.X10CompilerOptions(null); // Doesn't actually need an ExtensionInfo unless you ask about the version current directory
+    
+        for(int i = 0; i < args.length; ) {
+            try {
+                int ni = options.parseCommandForOptionsChecking(args, i, source);                
+                if (ni == i) {
+                    throw new UsageError("illegal option -- " + args[i]);
+                }
+                
+                i = ni;
+
+            }
+            catch (ArrayIndexOutOfBoundsException e) {
+                throw new UsageError("missing argument");
+            }
+            catch (Main.TerminationException e) {
+            	i++;
+            }
+        }
+                    
+        if (source.size() < 1) {
+          throw new UsageError("must specify at least one source file");
+        }
+    }
+    
 
 }
