44a45b23b7231cbd78bab35571ffb418c4e6346f
Robert Fuhrer <rfuhrer@us.ibm.com>
2009-06-22 21:58:23 +0000

    Committed on Thu Jan 15 13:53:35 2009 Reformatted to fix indentation. Marked class as deprecated, since base Compiler class does everything this did, as of Polyglot 2.0a3.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@10463 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt/branches/X10v17/org.eclipse.imp.x10dt.core/src/org/eclipse/imp/x10dt/core/builder/PolyglotFrontEnd.java
+++ x10dt/branches/X10v17/org.eclipse.imp.x10dt.core/src/org/eclipse/imp/x10dt/core/builder/PolyglotFrontEnd.java
@@ -7,17 +7,8 @@
 *
 * Contributors:
 *    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
-
 *******************************************************************************/
 
-/*
- * (C) Copyright IBM Corporation 2007
- * 
- * This file is part of the Eclipse IMP.
- */
-/*
- * Created on Oct 6, 2005
- */
 package org.eclipse.imp.x10dt.core.builder;
 
 import java.util.ArrayList;
@@ -25,10 +16,8 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 
-import polyglot.frontend.AllBarrierGoal;
 import polyglot.frontend.Compiler;
 import polyglot.frontend.ExtensionInfo;
-import polyglot.frontend.Goal;
 import polyglot.frontend.Job;
 import polyglot.frontend.Scheduler;
 import polyglot.frontend.Source;
@@ -44,72 +33,72 @@ import polyglot.util.StdErrorQueue;
  * Enhancement of core Polyglot compiler that takes as input a Collection of StreamSources.
  * Identical to Compiler in all other respects.
  * @author rfuhrer
+ * @deprecated Compiler.compile() now takes a collection of Sources, as of 2.0a3
  */
-// TODO Remove this class; Compiler.compile(), takes a collection of Source's, as of 2.0a3
 public final class PolyglotFrontEnd extends Compiler {
     public PolyglotFrontEnd(ExtensionInfo info, ErrorQueue eq) {
-	super(info, eq);
-        Report.setQueue(eq);
+    	super(info, eq);
+    	Report.setQueue(eq);
     }
     public PolyglotFrontEnd(ExtensionInfo info) {
-	this(info, new StdErrorQueue(System.err, 1000 * 1000, info.compilerName()));
+    	this(info, new StdErrorQueue(System.err, 1000 * 1000, info.compilerName()));
     }
 
     public boolean compile(Collection/*<StreamSource>*/sources) {
-	boolean okay= false;
+    	boolean okay= false;
 
-	try {
-	    try {
-		Scheduler scheduler= sourceExtension().scheduler();
-		List<Job> jobs= new ArrayList<Job>();
-
-		//PORT1.7 --- Nate says this should do what polyglot.frontend.Compiler.compile() does:
-
-		// First, create a goal to compile every source file.
-		for(Iterator<Source> i= sources.iterator(); i.hasNext(); ) {
-		    Source source= (Source) i.next();
-
-		    // mark this source as being explicitly specified by the user.
-		    source.setUserSpecified(true);
-
-		    // Add a new SourceJob for the given source. If a Job for the source
-		    // already exists, then we will be given the existing job.
-		    Job job= scheduler.addJob(source);
-		    jobs.add(job);
-//		    scheduler.addGoal(sourceExtension().getCompileGoal(job));  
-		}
-
-		scheduler.setCommandLineJobs(jobs);
-
-		for(Job job: jobs) {
-		    // Now, add a goal for completing the job.
-		    scheduler.addDependenciesForJob(job, false);
-		}
-		// Then, compile the files to completion.
-
-		okay= scheduler.runToCompletion();
-	    } catch (InternalCompilerError e) {
-		// Report it like other errors, but rethrow to get the stack trace.
 		try {
-		    errorQueue().enqueue(ErrorInfo.INTERNAL_ERROR, e.message(), e.position());
-		} catch (ErrorLimitError e2) {
+		    try {
+		    	Scheduler scheduler= sourceExtension().scheduler();
+		    	List<Job> jobs= new ArrayList<Job>();
+
+		    	//PORT1.7 --- Nate says this should do what polyglot.frontend.Compiler.compile() does:
+
+		    	// First, create a goal to compile every source file.
+				for(Iterator<Source> i= sources.iterator(); i.hasNext(); ) {
+					Source source= (Source) i.next();
+
+					// mark this source as being explicitly specified by the user.
+					source.setUserSpecified(true);
+
+					// Add a new SourceJob for the given source. If a Job for the source
+					// already exists, then we will be given the existing job.
+					Job job= scheduler.addJob(source);
+					jobs.add(job);
+	//		    	scheduler.addGoal(sourceExtension().getCompileGoal(job));  
+				}
+
+				scheduler.setCommandLineJobs(jobs);
+
+				for(Job job: jobs) {
+					// Now, add a goal for completing the job.
+					scheduler.addDependenciesForJob(job, false);
+				}
+				// Then, compile the files to completion.
+
+				okay= scheduler.runToCompletion();
+		    } catch (InternalCompilerError e) {
+		    	// Report it like other errors, but rethrow to get the stack trace.
+		    	try {
+		    		errorQueue().enqueue(ErrorInfo.INTERNAL_ERROR, e.message(), e.position());
+		    	} catch (ErrorLimitError e2) {
+		    	}
+		    	errorQueue().flush();
+		    	throw e;
+		    } catch (RuntimeException e) {
+		    	// Flush the error queue, then rethrow to get the stack trace.
+		    	errorQueue().enqueue(ErrorInfo.INTERNAL_ERROR, "Internal polyglot compiler error: " + e.getMessage(), Position.COMPILER_GENERATED);
+		    	errorQueue().flush();
+		    	throw e;
+		    }
+		} catch (ErrorLimitError e) {
 		}
 		errorQueue().flush();
-		throw e;
-	    } catch (RuntimeException e) {
-		// Flush the error queue, then rethrow to get the stack trace.
-		errorQueue().enqueue(ErrorInfo.INTERNAL_ERROR, "Internal polyglot compiler error: " + e.getMessage(), Position.COMPILER_GENERATED);
-		errorQueue().flush();
-		throw e;
-	    }
-	} catch (ErrorLimitError e) {
-	}
-	errorQueue().flush();
 
-	for(Iterator<ExtensionInfo> i= allExtensions().iterator(); i.hasNext(); ) {
-	    ExtensionInfo ext= (ExtensionInfo) i.next();
-	    ext.getStats().report();
-	}
-	return okay;
+		for(Iterator<ExtensionInfo> i= allExtensions().iterator(); i.hasNext(); ) {
+			ExtensionInfo ext= (ExtensionInfo) i.next();
+			ext.getStats().report();
+		}
+		return okay;
     }
 }
