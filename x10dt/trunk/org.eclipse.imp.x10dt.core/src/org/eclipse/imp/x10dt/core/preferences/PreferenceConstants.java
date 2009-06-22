0c70d97d6df559f9c4dad1da3ba6cd3a6e348607
Robert Fuhrer <rfuhrer@us.ibm.com>
2009-06-22 21:27:54 +0000

    Committed on Fri Dec  2 13:16:42 2005 Added pref ID constants for the X10 common directory path and the X10 configuration file location. Deleted bogus unused prefs constants.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@8632 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt/trunk/org.eclipse.imp.x10dt.core/src/org/eclipse/imp/x10dt/core/preferences/PreferenceConstants.java
+++ x10dt/trunk/org.eclipse.imp.x10dt.core/src/org/eclipse/imp/x10dt/core/preferences/PreferenceConstants.java
@@ -4,11 +4,9 @@ package com.ibm.watson.safari.x10.preferences;
  * Constant definitions for X10 plug-in preferences
  */
 public class PreferenceConstants {
-    public static final String P_PATH= "pathPreference";
+    public static final String P_X10COMMON_PATH= "commonPath";
+
+    public static final String P_X10CONFIG_FILE= "configFile";
 
     public static final String P_EMIT_MESSAGES= "emitMessages";
-
-    public static final String P_CHOICE= "choicePreference";
-
-    public static final String P_STRING= "stringPreference";
 }
