fead408d874b7de2bee0321b3bf9a36dfe6697e4
Shivali Agarwal <shivaliagarwal@domain>
2009-06-22 21:29:18 +0000

    Committed on Thu Nov  6 23:49:49 2008 added getName() to represent elaborated description.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@8716 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt/trunk/org.eclipse.imp.x10dt.debug/src/org/eclipse/imp/x10dt/debug/model/impl/X10Thread.java
+++ x10dt/trunk/org.eclipse.imp.x10dt.debug/src/org/eclipse/imp/x10dt/debug/model/impl/X10Thread.java
@@ -1,5 +1,9 @@
 package org.eclipse.imp.x10dt.debug.model.impl;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+
 import org.eclipse.debug.core.DebugException;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.model.IBreakpoint;
@@ -12,11 +16,27 @@ import org.eclipse.jdt.internal.debug.core.model.JDIDebugTarget;
 import org.eclipse.jdt.internal.debug.core.model.JDIThread;
 import org.eclipse.jdt.internal.debug.core.model.JDIThreadGroup;
 
+import org.eclipse.imp.x10dt.debug.model.IX10Activity;
+import org.eclipse.imp.x10dt.debug.model.IX10Place;
+import org.eclipse.imp.x10dt.debug.model.IX10Clock;
+import org.eclipse.imp.x10dt.debug.model.IX10StackFrame;
+import org.eclipse.imp.x10dt.debug.model.IX10Activity.X10ActivityState;
+
+import com.sun.jdi.Method;
 import com.sun.jdi.ObjectCollectedException;
 import com.sun.jdi.ThreadGroupReference;
 import com.sun.jdi.ThreadReference;
+import com.sun.jdi.ObjectReference;
+import com.sun.jdi.Value;
 
-public class X10Thread extends JDIThread implements IThread {
+public class X10Thread extends JDIThread implements IThread, IX10Activity {
+	
+	private IX10Place fPlace;
+	private IX10Activity fParent;
+	private HashSet<IX10Clock> fClocks;
+    private HashSet<IX10Activity> fChildren;
+    private ObjectReference fActivity;
+	
 	/**
 	 * Constructs a new thread group in the given target based on the underlying
 	 * thread group reference.
@@ -41,9 +61,65 @@ public class X10Thread extends JDIThread implements IThread {
 
 	}
 	public String getModelIdentifier() {
-		return Activator.getUniqueIdentifier();
+		return Activator.getUniqueIdentifier()+".model";
 	}
 	
+	public ObjectReference getFreshActivityObject(ThreadReference t) {
+		Value result=null;
+		if (t.referenceType().name().equals("x10.runtime.PoolRunner")) {
+			System.out.println("getFreshActivityObject: t is of type PoolRunner "+t.name());
+			//if (t.name().contains("pool-0-thread-2")){
+			
+			List<Method> m = (List<Method>)((ObjectReference)t).referenceType().methodsByName("getActivity");
+			System.out.println("Shivali:getFreshActivityObject() Method found "+m.size());
+			Method meth=null;
+			for (Method m1: m) {
+				meth = m1; 
+			}
+			List args=Collections.EMPTY_LIST;
+			
+			result = ((X10DebugTargetAlt)(this.getDebugTarget())).invokeX10RTMethod((ObjectReference)t,meth, args);
+			
+			System.out.println("After invoking getActivity() method ");
+			if (result==null){
+				System.out.println("getFreshActivityObject returning null");
+				return null;
+			}
+			if (result instanceof ObjectReference) {
+				System.out.println("Shivali:getFreshActivityObject got Activity");
+				fActivity = (ObjectReference)result;
+			}
+			return fActivity;
+		}
+		else {
+			System.out.println("getFreshActivityObject: t not a PoolRunner");
+			return null;
+		}
+	}
+	
+	public String getName() {
+		if (fActivity!=null){
+			System.out.println("X10Thread:getName() ");
+			String place = this.getUnderlyingThread().name();
+			if (place.contains("Main Activity"))
+				return "ACTIVITY" + "@PLACE 0" + ": Main Activity";
+			else {
+			String aname = ((X10DebugTargetAlt)(this.getDebugTarget())).getActivityString(fActivity);
+			//String aname= ((X10DebugTargetAlt)((IThread)this.getDebugTarget())).getActivityString(fActivity);
+			System.out.println("X10Thread:getName , aname = "+aname);
+			place = place.substring(4, 6);
+		    return "ACTIVITY" + "@PLACE" +place +":" +aname ;
+			}
+		}  
+		else {
+			System.out.println("X10Thread:getName - fActivity is null");
+		    return "ACTIVITY: "+this.getUnderlyingThread().toString();
+		}
+	}
+	
+	public ObjectReference getActivityObject() {
+		return fActivity;
+	}
 	// Copied from superclass to get around visibility gotchas
 	public void terminated() {
 		super.terminated();
@@ -60,4 +136,29 @@ public class X10Thread extends JDIThread implements IThread {
 	protected synchronized void resumedByVM() throws DebugException{
 		super.resumedByVM();
 	}
+	 public IX10Clock[] getClocks() {
+         return fClocks.toArray(new IX10Clock[fClocks.size()]);
+ }
+
+ public IX10Activity[] getFinishChildren() {
+         return fChildren.toArray(new IX10Activity[fChildren.size()]);
+ }
+
+ public IX10Activity getFinishParent() {
+         return fParent;
+ }
+
+ public IX10Place getPlace() {
+         return fPlace;
+ }
+
+ public X10ActivityState getRunState() {
+		return X10ActivityState.Running;
+	}
+
+ public IX10StackFrame[] getStackFrames() {
+         // TODO create stack frames in sample model
+         return new IX10StackFrame[0];
+ }
+
 }
