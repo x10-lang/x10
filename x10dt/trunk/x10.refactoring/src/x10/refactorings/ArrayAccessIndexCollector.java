b9f65b22175191f10c2e6782bd6060442d19ac5a
Robert Fuhrer <rfuhrer@us.ibm.com>
2009-06-22 21:41:00 +0000

    Committed on Sat May 16 14:57:12 2009 Partially converted to new Polyglot 3/X10 1.7 APIs (enough to compile, at least).
    git-svn-id: svn://svn.code.sf.net/p/x10/code@9419 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt/trunk/x10.refactoring/src/x10/refactorings/ArrayAccessIndexCollector.java
+++ x10dt/trunk/x10.refactoring/src/x10/refactorings/ArrayAccessIndexCollector.java
@@ -9,27 +9,41 @@ import java.util.List;
 import polyglot.ast.ArrayAccess;
 import polyglot.ast.Expr;
 import polyglot.ast.Node;
-import polyglot.ext.x10.ast.X10ArrayAccess1;
+import polyglot.ast.Receiver;
+import polyglot.ext.x10.ast.X10Call;
+import polyglot.types.ClassType;
+import polyglot.types.Type;
 import polyglot.visit.NodeVisitor;
 
 public class ArrayAccessIndexCollector extends NodeVisitor {
     private List<Expr> fResult;
 
     public ArrayAccessIndexCollector() {
-    	fResult = new ArrayList<Expr>();
+        fResult = new ArrayList<Expr>();
     }
-    
-	@Override
+
+    @Override
     public Node leave(Node old, Node n, NodeVisitor v) {
-		if (n instanceof X10ArrayAccess1)
-			fResult.add(((X10ArrayAccess1)n).index());
-		else if (n instanceof ArrayAccess)
-			fResult.add(((ArrayAccess)n).index());
-		return n;
-	}
-	
-	public List<Expr> getResult() {
-		return fResult;
-	}
-    
+        // PORT1.7 Array accesses now look like ordinary method calls
+//      if (n instanceof X10ArrayAccess1)
+//          fResult.add(((X10ArrayAccess1) n).index());
+        if (n instanceof X10Call) {
+            X10Call call = (X10Call) n;
+            Receiver rcvr = call.target();
+            Type rcvrType = rcvr.type();
+            if (rcvrType instanceof ClassType) {
+                ClassType rcvrClassType = (ClassType) rcvrType;
+                if (rcvrClassType.name().toString().equals("")) {
+                    
+                }
+            }
+        }
+        else if (n instanceof ArrayAccess)
+            fResult.add(((ArrayAccess) n).index());
+        return n;
+    }
+
+    public List<Expr> getResult() {
+        return fResult;
+    }
 }
