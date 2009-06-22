8abc3df605c181cb4d48a6972f6d65d3bb9f86de
Beth Tibbitts <btibbitts@us.ibm.com>
2009-06-22 21:57:44 +0000

    Committed on Fri May 29 17:54:33 2009 clean up comments, remove printlns
    git-svn-id: svn://svn.code.sf.net/p/x10/code@10424 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt/trunk/org.eclipse.imp.x10dt.core/src/org/eclipse/imp/x10dt/core/builder/PolyglotDependencyInfo.java
+++ x10dt/trunk/org.eclipse.imp.x10dt.core/src/org/eclipse/imp/x10dt/core/builder/PolyglotDependencyInfo.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright (c) 2008 IBM Corporation.
+* Copyright (c) 2008,2009 IBM Corporation.
 * All rights reserved. This program and the accompanying materials
 * are made available under the terms of the Eclipse Public License v1.0
 * which accompanies this distribution, and is available at
@@ -22,7 +22,6 @@ import java.util.Set;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.imp.builder.DependencyInfo;
-import org.eclipse.imp.x10dt.core.X10DTCorePlugin;
 
 import polyglot.types.Type;
 
@@ -34,39 +33,27 @@ public class PolyglotDependencyInfo extends DependencyInfo {
     public PolyglotDependencyInfo(IProject project) {
         super(project);
     }
-    private static boolean printOnce=true;
 
-    // BRT -- HACK don't try to chop off wsPath if it's not there, e.g. an x10 source like Ref.x10  
-    // BRT todo: if inside jar/zip then .. don't need??  see ComputeDependenciesVisitor
     //  don't forget the case when resource is outside the WS
     protected String typeToPath(Type type) {
         final String filePath= type.position().file().replace(File.separatorChar, '/');
         String result=null;
-//        assert(filePath.startsWith(wsPath));
         String wsPath= fWorkspacePath;
         if(filePath.startsWith(wsPath)){
-        	result = filePath.substring(fWorkspacePath.length());
+        	result = filePath.substring(fWorkspacePath.length());//PORT 1.7 don't complain if path doesn't start with wsPath
         }else{
-        	if (printOnce) {
-				String msg = "PolyglotDependencyInfo.typeToPath finds file: " + filePath + " does not start with wsPath=" + wsPath + " so not removing it. Suppress msg for subsequent files.";
-				X10DTCorePlugin.getInstance().writeInfoMsg(msg);
-				printOnce=false;
-			}
         	result=filePath;
         }
         return result;
-        //return filePath.substring(fWorkspacePath.length());
     }
 
     public void addDependency(Type fromType, Type uponType) {
         String fromPath= typeToPath(fromType);
         String uponPath= typeToPath(uponType);
-        // BRT PORT1.7  ...
+        // PORT1.7  ...
         if(!(uponPath.contains(".zip") || uponPath.contains(".jar"))) {
         	addDependency(fromPath, uponPath);
         }
-
-        
     }
 
     public void clearDependenciesOf(Type type) {
