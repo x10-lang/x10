ede7d70a6ccf75788a56883e79b7de7002a60549
Robert Fuhrer <rfuhrer@us.ibm.com>
2009-06-22 21:53:39 +0000

    Committed on Wed Jul  2 08:58:00 2008 Added a couple more exemplar behaviors.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@10179 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt/trunk/org.eclipse.imp.x10dt.ui/src/org/eclipse/imp/x10dt/ui/help/X10ContextHelper.java
+++ x10dt/trunk/org.eclipse.imp.x10dt.ui/src/org/eclipse/imp/x10dt/ui/help/X10ContextHelper.java
@@ -1,13 +1,28 @@
 package org.eclipse.imp.x10dt.ui.help;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
+import lpg.runtime.IToken;
+
 import org.eclipse.imp.parser.IParseController;
+import org.eclipse.imp.parser.ISourcePositionLocator;
 import org.eclipse.imp.services.IHelpService;
+import org.eclipse.imp.x10dt.ui.parser.ParseController;
+import org.eclipse.jface.text.IRegion;
 
 import polyglot.ast.Id;
 import polyglot.ast.Node;
 
 public class X10ContextHelper implements IHelpService {
+    private final static Map<String,String> sKeywordHelp= new HashMap<String, String>();
+
+    {
+        sKeywordHelp.put("static", "static: Identifies the given member as a class member (cf. instance member).");
+        sKeywordHelp.put("private", "private: Specifies that the given member is only visible to members of the same class.");
+        sKeywordHelp.put("async", "async: runs the child statement block in parallel with the statement(s) following the async.");
+        sKeywordHelp.put("future", "future: arranges for the child expression to be lazily evaluated; see 'force'.");
+    }
 
     public String getHelp(Object target, IParseController parseController) {
         Node root= (Node) parseController.getCurrentAst();
@@ -17,4 +32,22 @@ public class X10ContextHelper implements IHelpService {
         return "This is a " + target.getClass().getCanonicalName();
     }
 
+    public String getHelp(IRegion target, IParseController parseController) {
+        ParseController pc= (ParseController) parseController;
+        IToken token= pc.getLexer().getLexStream().getPrsStream().getTokenAtCharacter(target.getOffset());
+
+        if (token != null) {
+            String tokenStr= token.toString();
+
+            if (sKeywordHelp.containsKey(tokenStr)) {
+                return sKeywordHelp.get(tokenStr);
+            }
+        }
+        ISourcePositionLocator nodeLocator= parseController.getNodeLocator();
+        Object node= nodeLocator.findNode(parseController.getCurrentAst(), target.getOffset());
+        if (node != null) {
+            return getHelp(node, parseController);
+        }
+        return null;
+    }
 }
