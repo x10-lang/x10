3945710b5041249073b65a508b6893226119ef70
Beth Tibbitts <btibbitts@us.ibm.com>
2009-06-22 21:50:11 +0000

    Committed on Sun May 31 15:07:13 2009 disabling rename refactoring until we can fix it for 1.7 http://jira.codehaus.org/browse/XTENLANG-409
    git-svn-id: svn://svn.code.sf.net/p/x10/code@9971 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt/trunk/org.eclipse.imp.x10dt.ui/src/org/eclipse/imp/x10dt/ui/refactoring/actions/RefactoringContributor.java
+++ x10dt/trunk/org.eclipse.imp.x10dt.ui/src/org/eclipse/imp/x10dt/ui/refactoring/actions/RefactoringContributor.java
@@ -1,14 +1,14 @@
 /*******************************************************************************
-* Copyright (c) 2008 IBM Corporation.
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Eclipse Public License v1.0
-* which accompanies this distribution, and is available at
-* http://www.eclipse.org/legal/epl-v10.html
-*
-* Contributors:
-*    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
+ * Copyright (c) 2008 IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
 
-*******************************************************************************/
+ *******************************************************************************/
 
 /*
  * (C) Copyright IBM Corporation 2007
@@ -23,10 +23,12 @@ import org.eclipse.jface.action.IAction;
 
 
 public class RefactoringContributor implements IRefactoringContributor {
-    public RefactoringContributor() { }
+	public RefactoringContributor() { }
 
-    public IAction[] getEditorRefactoringActions(UniversalEditor editor) {
-	return new IAction[] {new RenameRefactoringAction(editor),
-	};
-    }
+	public IAction[] getEditorRefactoringActions(UniversalEditor editor) {
+		return new IAction[] {
+				// disable until it works correctly
+				//new RenameRefactoringAction(editor),
+		};
+	}
 }
