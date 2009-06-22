08748504d9b224e2d1bd1ab676322af5e4aa8e8c
Robert Fuhrer <rfuhrer@us.ibm.com>
2009-06-22 21:36:42 +0000

    Committed on Fri Feb 17 19:11:03 2006 Ctor now takes an IProject argument, so that buildClassPathSpec() can do something reasonable.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@9160 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt/trunk/org.eclipse.imp.x10dt.ui/src/org/eclipse/imp/x10dt/ui/parser/ParserDelegate.java
+++ x10dt/trunk/org.eclipse.imp.x10dt.ui/src/org/eclipse/imp/x10dt/ui/parser/ParserDelegate.java
@@ -3,14 +3,19 @@ package x10.uide.parser;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashSet;
-import java.util.List;
+
+import lpg.lpgjavaruntime.LexStream;
+import lpg.lpgjavaruntime.Monitor;
+import lpg.lpgjavaruntime.PrsStream;
+
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.IPath;
-import org.eclipse.core.runtime.Platform;
 import org.eclipse.jdt.core.IClasspathEntry;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.uide.parser.IParser;
-import com.ibm.watson.safari.x10.X10Plugin;
-import com.ibm.watson.safari.x10.preferences.X10Preferences;
+
 import polyglot.ext.x10.ExtensionInfo;
 import polyglot.frontend.Compiler;
 import polyglot.frontend.FileSource;
@@ -18,12 +23,12 @@ import polyglot.main.Options;
 import polyglot.main.UsageError;
 import polyglot.util.StdErrorQueue;
 import x10.parser.X10Parser;
-import lpg.lpgjavaruntime.LexStream;
-import lpg.lpgjavaruntime.Monitor;
-import lpg.lpgjavaruntime.PrsStream;
+
+import com.ibm.watson.safari.x10.X10Plugin;
 
 public class ParserDelegate implements IParser {
     X10Parser myParser;
+    private final IJavaProject javaProject;
 
     public PrsStream getParseStream() {
 	return myParser.getParseStream();
@@ -41,19 +46,18 @@ public class ParserDelegate implements IParser {
 	return myParser.orderedTerminalSymbols();
     }
 
-    ParserDelegate(LexStream lexStream) {
-	ExtensionInfo extInfo= new ExtensionInfo();
+    ParserDelegate(LexStream lexStream, IProject project) {
+	this.javaProject= JavaCore.create(project);
+
+        ExtensionInfo extInfo= new ExtensionInfo();
 	StdErrorQueue eq= new StdErrorQueue(System.err, 100000, "stderr");// TODO replace me
 	Compiler compiler= new Compiler(extInfo, eq);
 
 	buildOptions(extInfo);
 	extInfo.initCompiler(compiler);
 	try {
-	    // TODO Need to know a/the real file name (must exist or File() ctor will barf)
-	    // The lexStream should know but doesn't, because noone's telling it...
-//	    String fileName= lexStream.getFileName();
-	    String fileName= Platform.getOS().equals("win32") ? "c:\\" : "/tmp";
-	    FileSource fileSource= new FileSource(new File(fileName));
+//	    String fileName= project.getLocation().append(lexStream.getFileName()).toString();
+	    FileSource fileSource= new FileSource(new File(lexStream.getFileName()));
 
 	    myParser= new X10Parser(lexStream, extInfo.typeSystem(), extInfo.nodeFactory(), fileSource, extInfo.compiler().errorQueue());
 	} catch (IOException e) {
@@ -71,22 +75,48 @@ public class ParserDelegate implements IParser {
 	} catch (UsageError e) {
 	    if (!e.getMessage().equals("must specify at least one source file"))
 		System.err.println(e.getMessage());
+	} catch (JavaModelException e) {
+            X10Plugin.getInstance().writeErrorMsg("Unable to obtain resolved class path: " + e.getMessage());
 	}
 	X10Plugin.getInstance().maybeWriteInfoMsg("Source path = " + opts.source_path);
 	X10Plugin.getInstance().maybeWriteInfoMsg("Class path = " + opts.classpath);
     }
 
-    private String buildClassPathSpec() {
+    private String buildClassPathSpec() throws JavaModelException {
 	StringBuffer buff= new StringBuffer();
 
-	// HACK RMF 2/3/2005 - Really, should copy the classpath of the source file's
-	// containing project, but we don't even know what bloody file we're parsing.
-	// This will become critical when we start providing services that require
-	// name-resolved AST's.
-	String commonPath= X10Preferences.x10CommonPath;
-	String runtimePath= commonPath.substring(0, commonPath.lastIndexOf(File.separator) + 1) + "x10.runtime" + File.separator + "classes";
+	IClasspathEntry[] classpath= javaProject.getResolvedClasspath(true);
+	IPath projectPath= javaProject.getProject().getLocation();
+        IPath wsPath= javaProject.getProject().getWorkspace().getRoot().getLocation();
 
-	buff.append(runtimePath);
+        for(int i= 0; i < classpath.length; i++) {
+            IClasspathEntry entry= classpath[i];
+            String entryPath;
+
+            if (buff.length() > 0) buff.append(";");
+            switch(entry.getEntryKind()) {
+                case IClasspathEntry.CPE_CONTAINER: {
+                    entryPath= projectPath.append(entry.getPath()).toString();
+                    break;
+                }
+                case IClasspathEntry.CPE_LIBRARY:
+                    if (entry.getPath().isAbsolute())
+                        entryPath= entry.getPath().toString();
+                    else
+                        entryPath= projectPath.append(entry.getPath()).toString();
+                    break;
+                case IClasspathEntry.CPE_PROJECT:
+                    entryPath= wsPath.append(entry.getPath()).toString();
+                    break;
+                case IClasspathEntry.CPE_SOURCE:
+                    entryPath= wsPath.append(entry.getPath()).toString();
+                    break;
+                default: // IClasspathEntry.CPE_VARIABLE:
+                    entryPath= "";
+                    break;
+            }
+            buff.append(entryPath);
+        }
 	return buff.toString();
     }
 }
