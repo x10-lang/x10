47c71f053ad1bae65007ef3c3e840c19237a1c40
Stan Sutton <suttons@us.ibm.com>
2009-06-22 21:36:06 +0000

    Committed on Mon Jul 17 16:37:21 2006 Updated version of the hyperlink service.  Still does not work correctly due to inability to resolve ambiguous nodes, but at least it doesn't work with the latest template and the new reference-resolver component (which is really where node disambiguation needs to occur).
    git-svn-id: svn://svn.code.sf.net/p/x10/code@9124 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt/trunk/org.eclipse.imp.x10dt.ui/src/org/eclipse/imp/x10dt/ui/editor/HyperlinkDetector.java
+++ x10dt/trunk/org.eclipse.imp.x10dt.ui/src/org/eclipse/imp/x10dt/ui/editor/HyperlinkDetector.java
@@ -1,143 +1,88 @@
-/*
- * Created on Feb 8, 2006
- */
 package x10.uide.editor;
 
 import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.ITextViewer;
+import org.eclipse.jface.text.Region;
 import org.eclipse.jface.text.hyperlink.IHyperlink;
-import org.eclipse.jface.text.hyperlink.IHyperlinkDetector;
 import org.eclipse.uide.core.ILanguageService;
+import org.eclipse.uide.editor.IReferenceResolver;
 import org.eclipse.uide.editor.ISourceHyperlinkDetector;
 import org.eclipse.uide.parser.IASTNodeLocator;
 import org.eclipse.uide.parser.IParseController;
 
-import polyglot.ast.Ambiguous;
-import polyglot.ast.Call;
-import polyglot.ast.ClassDecl;
-import polyglot.ast.Field;
-import polyglot.ast.Local;
-import polyglot.ast.LocalDecl;
-import polyglot.ast.Node;
-import polyglot.ast.Receiver;
-import polyglot.ast.TypeNode;
-import polyglot.types.LocalInstance;
-import polyglot.types.Type;
-import polyglot.visit.NodeVisitor;
-import x10.uide.parser.ParseController;
-import x10.uide.parser.PolyglotNodeLocator;
+
+/**
+ * Provides a method to detect hyperlinks originating from a
+ * given region in the parse stream of a given parse controller.
+ */
 
 public class HyperlinkDetector implements ISourceHyperlinkDetector, ILanguageService {
-    ParseController fParseController;
+	
+    public HyperlinkDetector() { }
 
-    private final static boolean sDebugLink= false;
+    public IHyperlink[] detectHyperlinks(
+    		final ITextViewer textViewer, final IRegion region, IParseController parseController)
+    {
+    	// This is the only language-specific bit ...
+    	// Note:  X10 should be the name of the language
+    	// represented typographically like a class name
+    	IReferenceResolver linkMapper = new X10ReferenceResolver();
+    	
+    	// Get stuff for getting link source node
+        Object ast= parseController.getCurrentAst();
+        if (ast == null) return null;
+        int offset= region.getOffset();
+        IASTNodeLocator nodeLocator = parseController.getNodeLocator();
 
-    public IHyperlink[] detectHyperlinks(ITextViewer textViewer, IRegion region, IParseController parseController) {
-	fParseController= (ParseController) parseController;
-	Node ast= (Node) fParseController.getCurrentAst();
-	if (ast == null) return new IHyperlink[0];
-	// fParseController.getNodeLocator() should do what the following does...
-	IASTNodeLocator locator= new PolyglotNodeLocator(fParseController.getLexer().getLexStream());
-	final Node node= (Node) locator.findNode(ast, region.getOffset());
+        // Get link source node
+        Object source = nodeLocator.findNode(ast, offset);
+        if (source == null) return null;
+        //if (!(source instanceof IASTNodeToken)) return null;
+        if (!linkMapper.hasSuitableLinkSourceType(source)) return null;
 
-	if (!isBoundReference(node))
-	    return sDebugLink ? dummyHyperlink(textViewer, region, node) : null;
-	System.out.println("Creating hyperlink for " + node);
-	return createDefHyperlink(node, region, textViewer);
+        // Got a suitable link source node; get link target node
+       	final Object target = linkMapper.getLinkTarget(source, parseController);
+       	if (target == null) return null;
+
+        // Link target node exists; get info for new hyperlink
+       	// Note:  source presumably has a legitimate starting offset
+       	// and length (since they have been selected from the source file)
+        final int srcStart= nodeLocator.getStartOffset(source);
+        final int srcLength= nodeLocator.getEndOffset(source) - srcStart + 1;
+        // But the target (depending on what--and where--the target is)
+        // may not have a legitimate location (or one wihtin the file).
+        // In that case, set the target to the beginning of the file
+        // and give it a nominal length.
+        final int targetStart;
+        if (nodeLocator.getStartOffset(target) < 0) targetStart = 0;
+        else targetStart = nodeLocator.getStartOffset(target);
+        final int targetLength;
+        if (targetStart == 0) targetLength = 1;
+        else targetLength = nodeLocator.getEndOffset(target) - targetStart + 1;
+        // Get the link text
+        final String linkText = linkMapper.getLinkText(source);
+       	
+        // Create and return the new hyperlink
+        IHyperlink[] result = new IHyperlink[] {
+            new IHyperlink() {
+                public IRegion getHyperlinkRegion() {
+                    return new Region(srcStart, srcLength);
+                }
+                public String getTypeLabel() {
+               	    return target.getClass().getName();
+                }
+                public String getHyperlinkText() {
+                    return new String(linkText);
+                }
+                public void open() {
+                    textViewer.setSelectedRange(targetStart, targetLength);
+                    textViewer.revealRange(targetStart, targetLength);
+                }
+            }
+        };
+        
+        return result;
     }
-
-    private IHyperlink[] createDefHyperlink(Node node, IRegion region, ITextViewer textViewer) {
-	if (node instanceof TypeNode) {
-	    TypeNode typeNode= (TypeNode) node;
-
-	    return createHyperlinkToType(typeNode.type(), region, textViewer);
-	} else if (node instanceof Call) {
-	    Call call= (Call) node;
-	    Receiver rcvr= call.target();
-
-	    return createDefHyperlink(rcvr, region, textViewer);
-	} else if (node instanceof Field) {
-	    Field field= (Field) node;
-
-	    return createHyperlinkToType(field.type(), region, textViewer);
-	} else if (node instanceof Local) {
-	    Local local= (Local) node;
-
-	    return createHyperlinkToVariable(local, region, textViewer);
-	}
-	return null;
-    }
-
-    private IHyperlink[] createHyperlinkToVariable(Local local, IRegion region, ITextViewer textViewer) {
-	Node node= findVarDefinition(local, (Node) fParseController.getCurrentAst());
-
-	if (node != null)
-	    return new IHyperlink[] { new SourceHyperlink(textViewer, region, node) };
-	else
-	    return null;
-    }
-
-    public static Node findVarDefinition(Local local, Node ast) {
-	final LocalInstance li= local.localInstance();
-	final LocalDecl ld[]= new LocalDecl[1];
-
-	NodeVisitor finder= new NodeVisitor() {
-	    public NodeVisitor enter(Node n) {
-		if (n instanceof LocalDecl) {
-		    LocalDecl thisLD= (LocalDecl) n;
-
-		    if (thisLD.localInstance() == li)
-			ld[0]= thisLD;
-		}
-		return super.enter(n);
-	    }
-	};
-	ast.visit(finder);
-	return ld[0];
-    }
-
-    private IHyperlink[] createHyperlinkToType(Type type, IRegion region, ITextViewer textViewer) {
-	Node node= findTypeDefinition(type, (Node) fParseController.getCurrentAst());
-
-	if (node != null)
-	    return new IHyperlink[] { new SourceHyperlink(textViewer, region, node) };
-	else
-	    return null;
-    }
-
-    public static Node findTypeDefinition(final Type type, Node ast) {
-	final ClassDecl cd[]= new ClassDecl[1];
-
-	NodeVisitor finder= new NodeVisitor() {
-	    public NodeVisitor enter(Node n) {
-		if (n instanceof ClassDecl) {
-		    ClassDecl thisCD= (ClassDecl) n;
-
-		    if (thisCD.type() == type)
-			cd[0]= thisCD;
-		}
-		return super.enter(n);
-	    }
-	};
-	ast.visit(finder);
-	return cd[0];
-    }
-
-    private boolean isBoundReference(final Node node) {
-	if (node instanceof Ambiguous)
-	    return false;
-	if (node instanceof TypeNode)
-	    return true;
-	if (node instanceof Call)
-	    return true;
-	if (node instanceof Field)
-	    return true;
-	if (node instanceof Local)
-	    return true;
-	return false;
-    }
-
-    private IHyperlink[] dummyHyperlink(final ITextViewer textViewer, final IRegion region, final Node node) {
-	return new IHyperlink[] { new SourceHyperlink(textViewer, region, node) };
-    }
+    
 }
+
