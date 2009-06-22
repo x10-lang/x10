4911af7185599e5d77523834ef673734c5c31499
Robert Fuhrer <rfuhrer@us.ibm.com>
2009-06-22 21:49:31 +0000

    Committed on Tue Aug 12 14:47:17 2008 Added support for X10-specific categories for the sake of sorted outlines, to keep all fields together, all methods together, etc.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@9931 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt/trunk/org.eclipse.imp.x10dt.ui/src/org/eclipse/imp/x10dt/ui/editor/X10TreeModelBuilder.java
+++ x10dt/trunk/org.eclipse.imp.x10dt.ui/src/org/eclipse/imp/x10dt/ui/editor/X10TreeModelBuilder.java
@@ -12,6 +12,7 @@
 
 package org.eclipse.imp.x10dt.ui.editor;
 
+import org.eclipse.imp.editor.ModelTreeNode;
 import org.eclipse.imp.services.base.TreeModelBuilderBase;
 
 import polyglot.ast.Call;
@@ -38,6 +39,12 @@ import polyglot.visit.HaltingVisitor;
 import polyglot.visit.NodeVisitor;
 
 public class X10TreeModelBuilder extends TreeModelBuilderBase {
+    private static final int PACKAGE_CATEGORY= 0;
+    private static final int TYPE_CATEGORY= 1;
+    private static final int FIELD_CATEGORY= 2;
+    private static final int METHOD_CATEGORY= 3;
+    private static final int STATEMENT_CATEGORY= ModelTreeNode.DEFAULT_CATEGORY;
+
     @Override
     protected void visitTree(Object root) {
     	if (root == null) return;
@@ -47,34 +54,34 @@ public class X10TreeModelBuilder extends TreeModelBuilderBase {
         node.visit(new HaltingVisitor() {
             public NodeVisitor enter(Node n) {
                 if (n instanceof PackageNode) {
-                    createSubItem(n);
+                    createSubItem(n, PACKAGE_CATEGORY);
                 } else if (n instanceof TopLevelDecl) {
-                    pushSubItem(n);
+                    pushSubItem(n, TYPE_CATEGORY);
                 } else if (n instanceof ClassDecl) {
-                    pushSubItem(n);
+                    pushSubItem(n, TYPE_CATEGORY);
                 } else if (n instanceof ProcedureDecl) {
                     if (((ProcedureDecl) n).position() != Position.COMPILER_GENERATED) {
-                        pushSubItem(n);
+                        pushSubItem(n, METHOD_CATEGORY);
                     }
                 } else if (n instanceof FieldDecl) {
-                    createSubItem(n);
+                    createSubItem(n, FIELD_CATEGORY);
                 } else if (n instanceof Async || n instanceof AtEach || n instanceof ForEach ||
                            n instanceof Future || n instanceof Finish || n instanceof Atomic ||
                            n instanceof Next) {
-                    pushSubItem(n);
+                    pushSubItem(n, STATEMENT_CATEGORY);
                 } else if (n instanceof X10Loop) {
-                    pushSubItem(n);
+                    pushSubItem(n, STATEMENT_CATEGORY);
                 } else if (n instanceof ArrayConstructor) {
                     ArrayConstructor cons = (ArrayConstructor) n;
                     if (cons.initializer() != null) {
-                        pushSubItem(n);
+                        pushSubItem(n, STATEMENT_CATEGORY);
                     }
                 } else if (n instanceof Expr) {
                     return bypassChildren(n);
                 } else if (n instanceof Call) {
                     Call call = (Call) n;
                     if (call.name().equals("force") && call.arguments().size() == 0) {
-                        createSubItem(n);
+                        createSubItem(n, STATEMENT_CATEGORY);
                     }
                 }
                 return this;
