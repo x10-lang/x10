fd8c576b77141792d4ba4ff33bbff83cbe27d058
Robert Fuhrer <rfuhrer@us.ibm.com>
2009-06-22 21:41:58 +0000

    Committed on Sun May 17 00:20:18 2009 Added javaHomeDir.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@9477 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt/trunk/x10.refactoring/src/x10/refactorings/utils/EclipseProjectUtils.java
+++ x10dt/trunk/x10.refactoring/src/x10/refactorings/utils/EclipseProjectUtils.java
@@ -11,6 +11,7 @@ import org.osgi.framework.Bundle;
 
 public class EclipseProjectUtils {
     public static String eclipseHomePath = System.getProperty("user.dir");
+    public static String javaHomePath = System.getProperty("java.home");
 
     private EclipseProjectUtils() { }
 
