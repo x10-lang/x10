28b4431d0a00684fa62421f67346b128b5ef535c
Robert Fuhrer <rfuhrer@us.ibm.com>
2009-06-22 21:36:20 +0000

    Committed on Thu Dec 15 19:09:08 2005 The ctor no longer blindly uses c:\ on non-win32 systems as a dummy file name (which must nevertheless exist).
    git-svn-id: svn://svn.code.sf.net/p/x10/code@9138 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt/trunk/org.eclipse.imp.x10dt.ui/src/org/eclipse/imp/x10dt/ui/parser/ParserDelegate.java
+++ x10dt/trunk/org.eclipse.imp.x10dt.ui/src/org/eclipse/imp/x10dt/ui/parser/ParserDelegate.java
@@ -2,43 +2,52 @@ package x10.uide.parser;
 
 import java.io.File;
 import java.io.IOException;
-
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.uide.parser.IParser;
-
 import polyglot.ext.x10.ExtensionInfo;
 import polyglot.frontend.Compiler;
 import polyglot.frontend.FileSource;
 import polyglot.util.StdErrorQueue;
 import x10.parser.X10Parser;
-
 import com.ibm.lpg.LexStream;
 import com.ibm.lpg.Monitor;
 import com.ibm.lpg.PrsStream;
 
-public class ParserDelegate implements IParser
-{
-	X10Parser myParser;
+public class ParserDelegate implements IParser {
+    X10Parser myParser;
 
-	public PrsStream getParseStream() { return myParser.getParseStream(); }
-    public int getEOFTokenKind() { return myParser.getEOFTokenKind(); }
-    public Object parser(Monitor monitor, int error_repair_count) {
-        return myParser.parser(monitor);
+    public PrsStream getParseStream() {
+	return myParser.getParseStream();
     }
 
-    String[] orderedTerminalSymbols() { return myParser.orderedTerminalSymbols(); }
-    
-    ParserDelegate(LexStream lexStream) {
-    	ExtensionInfo extInfo= new ExtensionInfo();
-    	StdErrorQueue eq = new StdErrorQueue(System.err, 100000, "stderr");// TODO replace me
-		Compiler compiler= new Compiler(extInfo, eq);
-		extInfo.initCompiler(compiler);
-		try {
-	    	FileSource fileSource= new FileSource(new File("c:\\"));
+    public int getEOFTokenKind() {
+	return myParser.getEOFTokenKind();
+    }
 
-	    	myParser = new X10Parser(lexStream, extInfo.typeSystem(), extInfo.nodeFactory(), fileSource, extInfo.compiler().errorQueue());
-		} catch (IOException e) {
-			e.printStackTrace();
-			throw new IllegalArgumentException(e.getMessage());
-		}
-   	}
+    public Object parser(Monitor monitor, int error_repair_count) {
+	return myParser.parser(monitor);
+    }
+
+    String[] orderedTerminalSymbols() {
+	return myParser.orderedTerminalSymbols();
+    }
+
+    ParserDelegate(LexStream lexStream) {
+	ExtensionInfo extInfo= new ExtensionInfo();
+	StdErrorQueue eq= new StdErrorQueue(System.err, 100000, "stderr");// TODO replace me
+	Compiler compiler= new Compiler(extInfo, eq);
+	extInfo.initCompiler(compiler);
+	try {
+	    // TODO Need to know a/the real file name (must exist or File() ctor will barf)
+	    // The lexStream should know but doesn't, because noone's telling it...
+//	    String fileName= lexStream.getFileName();
+	    String fileName= Platform.getOS().equals("win32") ? "c:\\" : "/tmp";
+	    FileSource fileSource= new FileSource(new File(fileName));
+
+	    myParser= new X10Parser(lexStream, extInfo.typeSystem(), extInfo.nodeFactory(), fileSource, extInfo.compiler().errorQueue());
+	} catch (IOException e) {
+	    e.printStackTrace();
+	    throw new IllegalArgumentException(e.getMessage());
+	}
+    }
 }
