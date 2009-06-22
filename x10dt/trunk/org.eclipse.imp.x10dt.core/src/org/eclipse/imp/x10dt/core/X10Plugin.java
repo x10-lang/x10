02693335ca36f25b3b0a158f31ad3fc3208afe7d
Robert Fuhrer <rfuhrer@us.ibm.com>
2009-06-22 21:43:47 +0000

    Committed on Thu Jun  8 19:27:56 2006 Moved X10 common path here from prefs since it's not really configurable (now always comes from the x10.common plugin path). Accommodate changes to preferences in start() and refreshPrefs() code that uses them.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@9587 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt/trunk/org.eclipse.imp.x10dt.core/src/org/eclipse/imp/x10dt/core/X10Plugin.java
+++ x10dt/trunk/org.eclipse.imp.x10dt.core/src/org/eclipse/imp/x10dt/core/X10Plugin.java
@@ -1,11 +1,15 @@
 package com.ibm.watson.safari.x10;
 
 import java.io.File;
+import java.net.URL;
 
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.eclipse.uide.runtime.SAFARIPluginBase;
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 
 import com.ibm.watson.safari.x10.preferences.PreferenceConstants;
@@ -21,6 +25,8 @@ public class X10Plugin extends SAFARIPluginBase {
      * The unique instance of this plugin class
      */
     protected static X10Plugin sPlugin;
+
+    public static String x10CommonPath;
 
     public static X10Plugin getInstance() {
         return sPlugin;
@@ -43,9 +49,15 @@ public class X10Plugin extends SAFARIPluginBase {
 	// Initialize the X10Preferences fields with the preference store data.
 	IPreferenceStore prefStore= getPreferenceStore();
 
+	Bundle x10CommonBundle= Platform.getBundle("x10.common");
+	URL x10CommonURL= Platform.asLocalURL(Platform.find(x10CommonBundle, new Path("")));
+
+	x10CommonPath= x10CommonURL.getPath();
+
 	X10Preferences.builderEmitMessages= prefStore.getBoolean(PreferenceConstants.P_EMIT_MESSAGES);
 	X10Preferences.autoAddRuntime= prefStore.getBoolean(PreferenceConstants.P_AUTO_ADD_RUNTIME);
-	X10Preferences.x10CommonPath= prefStore.getString(PreferenceConstants.P_X10COMMON_PATH);
+//	x10CommonPath= prefStore.getString(PreferenceConstants.P_X10COMMON_PATH);
+	X10Preferences.x10Config= prefStore.getString(PreferenceConstants.P_X10CONFIG);
 	X10Preferences.x10ConfigFile= prefStore.getString(PreferenceConstants.P_X10CONFIG_FILE);
 	fEmitInfoMessages= X10Preferences.builderEmitMessages;
     }
@@ -74,7 +86,7 @@ public class X10Plugin extends SAFARIPluginBase {
         // Set the "x10.configuration" System property from the corresponding preference
         // value, since polyglot.ext.x10.Configuration relies on its being set, and a
         // static initializer there throws an exception if it can't find the config file.
-        final String configFile= (X10Preferences.x10ConfigFile!= null ? X10Preferences.x10ConfigFile : X10Preferences.x10CommonPath + File.separator + "etc" + File.separator + "standard.cfg");
+        final String configFile= (X10Preferences.x10ConfigFile!= null ? X10Preferences.x10ConfigFile : x10CommonPath + File.separator + "etc" + File.separator + "standard.cfg");
 	System.setProperty("x10.configuration", configFile);
     }
 }
