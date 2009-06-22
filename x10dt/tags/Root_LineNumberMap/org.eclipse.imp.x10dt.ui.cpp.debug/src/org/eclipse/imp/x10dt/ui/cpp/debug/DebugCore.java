3e9981b41ca03b7e828739d63df70ed03b5e7f67
No Author <user@domain>
2009-06-22 21:37:48 +0000

    This commit was manufactured by cvs2svn to create tag 'Root_LineNumberMap'.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@9226 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ x10dt/tags/Root_LineNumberMap/org.eclipse.imp.x10dt.ui.cpp.debug/src/org/eclipse/imp/x10dt/ui/cpp/debug/DebugCore.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2009 IBM Corporation.                                         *
+ * All rights reserved. This program and the accompanying materials            *
+ * are made available under the terms of the Eclipse Public License v1.0       *
+ * which accompanies this distribution, and is available at                    *
+ * http://www.eclipse.org/legal/epl-v10.html                                   *
+ *******************************************************************************/
+package org.eclipse.imp.x10dt.ui.cpp.debug;
+
+import org.eclipse.imp.x10dt.ui.cpp.launch.AbstractUIBasePlugin;
+
+/**
+ * Controls the plug-in life cycle for this project.
+ * 
+ * @author egeay
+ */
+public class DebugCore extends AbstractUIBasePlugin {
+  
+  /**
+   * Unique id for this plugin.
+   */
+  public static final String PLUGIN_ID = "org.eclipse.imp.x10dt.ui.cpp.debug"; //$NON-NLS-1$
+  
+}
