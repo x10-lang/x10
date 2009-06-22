e4789d84217eb776430d857eda5260cac94e6327
Robert Fuhrer <rfuhrer@us.ibm.com>
2009-06-22 21:34:44 +0000

    Committed on Mon Jun  1 23:33:12 2009 Added basic breakpoint support. Seems to work if you re-build the application after setting a breakpoint, but perhaps not if you set the breakpoint and then run the app under the debugger without rebuilding. (I think.)
    git-svn-id: svn://svn.code.sf.net/p/x10/code@9042 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ x10dt/trunk/org.eclipse.imp.x10dt.ui/src/org/eclipse/imp/x10dt/debug/X10ToggleBreakpointHandler.java
@@ -0,0 +1,10 @@
+package org.eclipse.imp.x10dt.debug;
+
+import org.eclipse.imp.services.base.JavaBreakpointHandler;
+
+public class X10ToggleBreakpointHandler extends JavaBreakpointHandler {
+
+    public X10ToggleBreakpointHandler() {
+        super("x10");
+    }
+}
