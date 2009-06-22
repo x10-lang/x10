78317cbe88e964cb2e1c203448ac13634e2af265
Robert Fuhrer <rfuhrer@us.ibm.com>
2009-06-22 21:50:09 +0000

    Committed on Tue Jul 29 12:44:16 2008 Fixed indentation.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@9969 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt/trunk/org.eclipse.imp.x10dt.ui/src/org/eclipse/imp/x10dt/ui/refactoring/RenameWizard.java
+++ x10dt/trunk/org.eclipse.imp.x10dt.ui/src/org/eclipse/imp/x10dt/ui/refactoring/RenameWizard.java
@@ -1,14 +1,14 @@
 /*******************************************************************************
-* Copyright (c) 2008 IBM Corporation.
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Eclipse Public License v1.0
-* which accompanies this distribution, and is available at
-* http://www.eclipse.org/legal/epl-v10.html
-*
-* Contributors:
-*    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
+ * Copyright (c) 2008 IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
 
-*******************************************************************************/
+ *******************************************************************************/
 
 /*
  * (C) Copyright IBM Corporation 2007
@@ -21,17 +21,17 @@ import org.eclipse.ltk.ui.refactoring.RefactoringWizard;
 
 public class RenameWizard extends RefactoringWizard {
     public RenameWizard(RenameRefactoring refactoring, String pageTitle) {
-	super(refactoring, DIALOG_BASED_USER_INTERFACE | PREVIEW_EXPAND_FIRST_NODE);
-	setDefaultPageTitle(pageTitle);
+        super(refactoring, DIALOG_BASED_USER_INTERFACE | PREVIEW_EXPAND_FIRST_NODE);
+        setDefaultPageTitle(pageTitle);
     }
 
     protected void addUserInputPages() {
-	RenameInputPage page= new RenameInputPage("Rename", (RenameRefactoring) this.getRefactoring());
+        RenameInputPage page= new RenameInputPage("Rename", (RenameRefactoring) this.getRefactoring());
 
-	addPage(page);
+        addPage(page);
     }
 
     public RenameRefactoring getRenameRefactoring() {
-	return (RenameRefactoring) getRefactoring();
+        return (RenameRefactoring) getRefactoring();
     }
 }
