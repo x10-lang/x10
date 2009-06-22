d090328ed82c32256812da04cd6a85674e2df69a
Robert Fuhrer <rfuhrer@us.ibm.com>
2009-06-22 21:46:48 +0000

    Committed on Fri Feb  2 09:51:55 2007 Some changes to support refactoring across compilation units.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@9768 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt/trunk/org.eclipse.imp.x10dt.ui/src/org/eclipse/imp/x10dt/ui/refactoring/actions/RenameRefactoringAction.java
+++ x10dt/trunk/org.eclipse.imp.x10dt.ui/src/org/eclipse/imp/x10dt/ui/refactoring/actions/RenameRefactoringAction.java
@@ -1,12 +1,16 @@
 package safari.X10.refactoring.actions;
 
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.jdt.core.dom.MethodInvocation;
 import org.eclipse.jdt.internal.corext.callhierarchy.MethodCall;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.text.IDocument;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IFileEditorInput;
+import org.eclipse.ui.texteditor.ITextEditor;
 import org.eclipse.ui.texteditor.TextEditorAction;
 import org.eclipse.uide.editor.UniversalEditor;
 import org.eclipse.uide.parser.IASTNodeLocator;
@@ -27,10 +31,12 @@ import polyglot.ast.TypeNode;
 import polyglot.types.Declaration;
 import polyglot.types.LocalInstance;
 import polyglot.types.Type;
+import polyglot.util.Position;
 import polyglot.visit.NodeVisitor;
 import safari.X10.refactoring.RefactoringMessages;
 import safari.X10.refactoring.RenameRefactoring;
 import safari.X10.refactoring.RenameWizard;
+import x10.uide.parser.ParseController;
 
 public class RenameRefactoringAction extends TextEditorAction {
     private final Node fNode;
@@ -99,13 +105,15 @@ public class RenameRefactoringAction extends TextEditorAction {
             return;
         }
 
-        findDeclaration(decl);
+        IFileEditorInput fileInput= (IFileEditorInput) getTextEditor().getEditorInput();
 
-        if (fDeclaringNode == null)
+        if (decl.position().file().equals(fileInput.getFile().getLocation().toOSString()))
+            findDeclaration(fRoot, decl);
+        else
             findExternalDecl(decl);
 
         if (fDeclaringNode == null) {
-            MessageDialog.openError(shell, "Cannot rename", "Renaming entities across source files not yet supported.");
+//          MessageDialog.openError(shell, "Cannot rename", "Renaming entities across source files not yet supported.");
             return;
         }
 
@@ -119,14 +127,45 @@ public class RenameRefactoringAction extends TextEditorAction {
      * @param decl
      */
     private void findExternalDecl(Declaration decl) {
-        System.out.println(decl.position());
+        final Position position= decl.position();
+
+        System.out.println("declaration is located in " + position.file() + ": " + position);
+
+        ITextEditor textEditor= getTextEditor();
+	IEditorInput input= textEditor.getEditorInput();
+
+	if (isBinary(decl)) {
+	    MessageDialog.openError(textEditor.getSite().getShell(), "Cannot rename", "The given entity is defined in a binary file and cannot be changed.");
+	    return;
+	}
+
+	if (input instanceof IFileEditorInput) {
+            IFileEditorInput fileInput= (IFileEditorInput) input;
+            IProject project= fileInput.getFile().getProject();
+	    IParseController parseCtrlr= new ParseController();
+	    // TODO Handle cross-project references
+	    String declFilePath= position.file().substring(project.getLocation().toOSString().length()+1);
+
+            parseCtrlr.initialize(declFilePath, fileInput.getFile().getProject(), null);
+            IDocument document= textEditor.getDocumentProvider().getDocument(fileInput);
+            Node declRoot= (Node) parseCtrlr.parse(document.get(), false, null);
+
+            System.out.println(declRoot);
+            findDeclaration(declRoot, decl);
+            System.out.println(fDeclaringNode);
+        }
+    }
+
+    private boolean isBinary(Declaration decl) {
+	final String file= decl.position().file();
+	return file.endsWith(".class") || file.endsWith(".jar");
     }
 
     /**
      * @param decl
      * @return
      */
-    private void findDeclaration(final Declaration decl) {
+    private void findDeclaration(Node root, final Declaration decl) {
         NodeVisitor visitor= new NodeVisitor() {
             /* (non-Javadoc)
              * @see polyglot.visit.NodeVisitor#enter(polyglot.ast.Node)
@@ -157,6 +196,6 @@ public class RenameRefactoringAction extends TextEditorAction {
                 return this;
             }
         };
-        fRoot.visit(visitor);
+        root.visit(visitor);
     }
 }
