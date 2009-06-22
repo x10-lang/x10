b3b3da96569c65908bcb19da68d24d868a3386bf
Robert Fuhrer <rfuhrer@us.ibm.com>
2009-06-22 21:44:01 +0000

    Committed on Thu Jul 27 18:00:54 2006 addToProject() now also adds the SMAPI nature, so that breakpoints automagically work.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@9601 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt/trunk/org.eclipse.imp.x10dt.core/src/org/eclipse/imp/x10dt/core/builder/X10ProjectNature.java
+++ x10dt/trunk/org.eclipse.imp.x10dt.core/src/org/eclipse/imp/x10dt/core/builder/X10ProjectNature.java
@@ -1,8 +1,10 @@
 package com.ibm.watson.safari.x10.builder;
 
+import org.eclipse.core.resources.IProject;
 import org.eclipse.uide.core.ProjectNatureBase;
 import org.eclipse.uide.runtime.IPluginLog;
 import com.ibm.watson.safari.x10.X10Plugin;
+import com.ibm.watson.smapifier.builder.SmapiProjectNature;
 
 public class X10ProjectNature extends ProjectNatureBase {
     public static final String k_natureID= X10Plugin.kPluginID + ".x10nature";
@@ -13,6 +15,11 @@ public class X10ProjectNature extends ProjectNatureBase {
 
     public String getBuilderID() {
 	return X10Builder.BUILDER_ID;
+    }
+
+    public void addToProject(IProject project) {
+        super.addToProject(project);
+        new SmapiProjectNature().addToProject(project);
     }
 
     protected void refreshPrefs() {}
