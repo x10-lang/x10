1785e9f3594cb3e181b5aa9debae0095f3efe138
Robert Fuhrer <rfuhrer@us.ibm.com>
2009-06-22 21:36:25 +0000

    Committed on Mon Dec 17 13:53:19 2007 Implementation of new IAnnotationTypeInfo interface.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@9143 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ x10dt/trunk/org.eclipse.imp.x10dt.ui/src/org/eclipse/imp/x10dt/ui/parser/X10AnnotationTypeInfo.java
@@ -0,0 +1,24 @@
+package org.eclipse.imp.x10dt.ui.parser;
+
+import java.util.List;
+
+import org.eclipse.imp.language.IAnnotationTypeInfo;
+
+public class X10AnnotationTypeInfo implements IAnnotationTypeInfo {
+
+    public void addProblemMarkerType(String problemMarkerType) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public List getProblemMarkerTypes() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public void removeProblemMarkerType(String problemMarkerType) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
