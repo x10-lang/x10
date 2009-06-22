37aa650233caf25fe1719ec715b3701817fb84cb
Igor Peshansky <igorp@us.ibm.com>
2009-06-22 21:37:27 +0000

    Committed on Mon May 11 16:43:09 2009 Fix API to not throw exceptions. Better breakpoint handling.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@9205 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt/trunk/org.eclipse.imp.x10dt.ui.cpp.debug/src/org/eclipse/imp/x10dt/ui/cpp/debug/breakpoints/X10ToggleBreakpointsHandler.java
+++ x10dt/trunk/org.eclipse.imp.x10dt.ui.cpp.debug/src/org/eclipse/imp/x10dt/ui/cpp/debug/breakpoints/X10ToggleBreakpointsHandler.java
@@ -2,8 +2,8 @@ package org.eclipse.imp.x10dt.ui.cpp.debug.breakpoints;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.model.IBreakpoint;
+import org.eclipse.imp.runtime.RuntimePlugin;
 import org.eclipse.imp.services.IToggleBreakpointsHandler;
 import org.eclipse.ptp.debug.core.PDebugModel;
 import org.eclipse.ptp.debug.core.model.IPLineBreakpoint;
@@ -18,35 +18,43 @@ public class X10ToggleBreakpointsHandler implements IToggleBreakpointsHandler {
 
   // --- Interface methods implementation
   
-  public void clearLineBreakpoint(final IFile file, final int lineNumber) throws CoreException {
-    for (final IBreakpoint breakpoint : PDebugModel.getPBreakpoints()) {
-      if (breakpoint instanceof IPLineBreakpoint) {
-        final IPLineBreakpoint pLineBreakPoint = (IPLineBreakpoint) breakpoint;
-        if (pLineBreakPoint.getMarker().getResource().equals(file) && pLineBreakPoint.getLineNumber() == lineNumber) {
-          DebugPlugin.getDefault().getBreakpointManager().removeBreakpoint(pLineBreakPoint, true /* delete */);
+  public void clearLineBreakpoint(final IFile file, final int lineNumber) {
+    try {
+      for (final IBreakpoint breakpoint : PDebugModel.getPBreakpoints()) {
+        if (breakpoint instanceof IPLineBreakpoint) {
+          final IPLineBreakpoint pLineBreakPoint = (IPLineBreakpoint) breakpoint;
+          if (pLineBreakPoint.getMarker().getResource().equals(file) && pLineBreakPoint.getLineNumber() == lineNumber) {
+        	pLineBreakPoint.delete();
+          }
         }
       }
+    } catch (CoreException e) {
+    	RuntimePlugin.getInstance().logException(e.getMessage(), e);
     }
   }
 
-  public void disableLineBreakpoint(final IFile file, final int lineNumber) throws CoreException {
+  public void disableLineBreakpoint(final IFile file, final int lineNumber) {
     createLineBreakpoint(file, lineNumber, false);
   }
 
-  public void enableLineBreakpoint(final IFile file, final int lineNumber) throws CoreException {
+  public void enableLineBreakpoint(final IFile file, final int lineNumber) {
     createLineBreakpoint(file, lineNumber, true);
   }
 
-  public void setLineBreakpoint(final IFile file, final int lineNumber) throws CoreException {
+  public void setLineBreakpoint(final IFile file, final int lineNumber) {
     createLineBreakpoint(file, lineNumber, true);
   }
 
   // --- Private code
   
-  private void createLineBreakpoint(final IFile file, final int lineNumber, final boolean enable) throws CoreException {
-    PDebugModel.createLineBreakpoint(file.getLocation().toOSString(), file, lineNumber, true /* enabled */, 
-                                     0 /* ignoreCount */, "" /* condition */, true /* register */, //$NON-NLS-1$
-                                     IElementHandler.SET_ROOT_ID /* set_id */, null /* job */);
+  private void createLineBreakpoint(final IFile file, final int lineNumber, final boolean enable) {
+    try {
+      PDebugModel.createLineBreakpoint(file.getLocation().toOSString(), file, lineNumber, enable,
+                                       0 /* ignoreCount */, "" /* condition */, true /* register */,
+                                       IElementHandler.SET_ROOT_ID /* set_id */, null /* job */);
+	} catch (CoreException e) {
+		RuntimePlugin.getInstance().logException(e.getMessage(), e);
+	}
   }
     
 }
