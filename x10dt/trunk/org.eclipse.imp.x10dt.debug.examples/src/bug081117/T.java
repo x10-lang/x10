3d9c942f8cd3d38166165ba7492a08ae9547394d
Matthew Kaplan <user@domain>
2009-06-22 21:30:09 +0000

    Committed on Mon Nov 17 09:32:24 2008 *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/x10/code@8767 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ x10dt/trunk/org.eclipse.imp.x10dt.debug.examples/src/bug081117/T.java
@@ -0,0 +1,27 @@
+package bug081117;
+
+import x10.lang.*;
+
+class T
+extends bug081117.
+  Test
+{
+    
+//#line 3
+private int
+      target;
+    
+    
+//#line 4
+T() {
+        
+//#line 5
+super();
+        
+//#line 6
+/* template:place-check { */((java.io.PrintStream) x10.lang.Runtime.hereCheck(java.
+                                                                                                   lang.
+                                                                                                   System.
+                                                                                                   out))/* } */.println("After super ");
+    }
+}
