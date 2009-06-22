a15db090107b2486bdcc2ae55f0f67cc9e10736e
Beth Tibbitts <btibbitts@us.ibm.com>
2009-06-22 21:57:21 +0000

    Committed on Thu May 28 11:00:06 2009 Allow options that don't start with '-',  and correctly parse quoted options.  Revisit this when options.checkCommand is smarter.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@10401 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt/trunk/org.eclipse.imp.x10dt.core/src/org/eclipse/imp/x10dt/core/preferences/fields/CompilerOptionsValidator.java
+++ x10dt/trunk/org.eclipse.imp.x10dt.core/src/org/eclipse/imp/x10dt/core/preferences/fields/CompilerOptionsValidator.java
@@ -7,19 +7,28 @@ import org.eclipse.imp.preferences.fields.StringFieldEditor.Validator;
 
 import polyglot.main.Main;
 import polyglot.main.UsageError;
+import polyglot.util.QuotedStringTokenizer;
 import x10.config.ConfigurationError;
 import x10.config.OptionError;
 
 public class CompilerOptionsValidator implements Validator {
     public String validate(String value) {
-        String argString = "";
+       String argString = "";
         argString += value;
         argString += " ."; // need to specify at
         // least one file
         Set sources = new HashSet();
         sources.add(".");
 
-        String[] args = argString.split("\\s+");
+        //String[] args = argString.split("\\s+");
+        // Don't break args up on spaces only.  Consider things in quotes.
+        // allows handling of e.g.  -report "my -very- weird report topic=42" 
+        QuotedStringTokenizer st = new QuotedStringTokenizer(argString);
+        String[] args = new String[st.countTokens()];
+        for (int i = 0; i < args.length; i++)
+          args[i] = st.nextToken();
+        
+        
         // TODO need api to list the possible options?  to have field specific content assist here
         polyglot.ext.x10.X10CompilerOptions options = new polyglot.ext.x10.X10CompilerOptions(null); // Doesn't actually need an ExtensionInfo unless you ask about the version current directory
         try {
@@ -27,9 +36,10 @@ public class CompilerOptionsValidator implements Validator {
                 if (args[i].length() == 0) {
                     continue;
                 }
-                if (!args[i].startsWith("-") && !args[i].equals(".")) {
-                    throw new OptionError("Invalid option: " + args[i]);
-                }
+                // allow options that don't start with '-' - revisit this when options.checkCommand can be smarter???]
+//                if (!args[i].startsWith("-") && !args[i].equals(".")) {
+//                    throw new OptionError("Invalid option: " + args[i]);
+//                }
                 try {
                     options.checkCommand(args, i, sources);
                 } catch (Main.TerminationException e) {
