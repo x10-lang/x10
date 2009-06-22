ae99d1f4fdc11e07e85b4c3a82823a193ae98e56
Shivali Agarwal <shivaliagarwal@domain>
2009-06-22 21:29:26 +0000

    Committed on Fri Nov 14 12:47:53 2008 Additions for Stack Frames
    git-svn-id: svn://svn.code.sf.net/p/x10/code@8724 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt/trunk/org.eclipse.imp.x10dt.debug/src/org/eclipse/imp/x10dt/debug/model/impl/X10Thread.java
+++ x10dt/trunk/org.eclipse.imp.x10dt.debug/src/org/eclipse/imp/x10dt/debug/model/impl/X10Thread.java
@@ -2,25 +2,40 @@ package org.eclipse.imp.x10dt.debug.model.impl;
 
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ArrayList;
 
+import org.eclipse.debug.core.DebugEvent;
 import org.eclipse.debug.core.DebugException;
 import org.eclipse.debug.core.model.IBreakpoint;
 import org.eclipse.debug.core.model.IThread;
+import org.eclipse.debug.core.model.IStackFrame;
+
+import org.eclipse.imp.x10dt.debug.Activator;
+import org.eclipse.jdt.debug.core.JDIDebugModel;
+import org.eclipse.jdt.internal.debug.core.model.JDIDebugModelMessages;
+import org.eclipse.jdt.internal.debug.core.model.JDIDebugTarget;
+import org.eclipse.jdt.internal.debug.core.model.JDIThread;
+import org.eclipse.jdt.internal.debug.core.model.JDIStackFrame;
+import org.eclipse.jdt.internal.debug.core.model.JDIThreadGroup;
+
+
 import org.eclipse.imp.x10dt.debug.model.IX10Activity;
 import org.eclipse.imp.x10dt.debug.model.IX10Clock;
 import org.eclipse.imp.x10dt.debug.model.IX10Place;
 import org.eclipse.imp.x10dt.debug.model.IX10StackFrame;
-import org.eclipse.imp.x10dt.debug.model.impl.jdi.X10DelegatingStackFrame;
-import org.eclipse.jdt.internal.debug.core.model.JDIDebugTarget;
-import org.eclipse.jdt.internal.debug.core.model.JDIStackFrame;
-import org.eclipse.jdt.internal.debug.core.model.JDIThread;
 
+
+import com.sun.jdi.InvalidStackFrameException;
 import com.sun.jdi.Method;
 import com.sun.jdi.ObjectCollectedException;
+import com.sun.jdi.IncompatibleThreadStateException;
+import com.sun.jdi.ThreadGroupReference;
 import com.sun.jdi.ObjectReference;
 import com.sun.jdi.ThreadReference;
 import com.sun.jdi.Value;
+import com.sun.jdi.StackFrame;
 
 public class X10Thread extends JDIThread implements IThread, IX10Activity {
 	
@@ -29,6 +44,8 @@ public class X10Thread extends JDIThread implements IThread, IX10Activity {
 	private HashSet<IX10Clock> fClocks;
     private HashSet<IX10Activity> fChildren;
     private ObjectReference fActivity;
+    private List fStackFrames;
+    private boolean fRefreshChildren = true;
 	
 	/**
 	 * Constructs a new thread group in the given target based on the underlying
@@ -48,14 +65,136 @@ public class X10Thread extends JDIThread implements IThread, IX10Activity {
 	 */
 	public X10Thread(JDIDebugTarget target, ThreadReference thread) throws ObjectCollectedException {
 		super(target, thread);
+		//if (thread.name().compareTo("InvokeMethods")==0) {
+			//System.out.println("About to fire");
+			//fireSuspendEvent(DebugEvent.BREAKPOINT);
+		//}
+		fStackFrames = new ArrayList();
 		/**
 		 * @see org.eclipse.debug.core.model.IDebugElement#getModelIdentifier()
 		 */
 
 	}
 	public String getModelIdentifier() {
-		return getDebugTarget().getModelIdentifier();
+		return Activator.getUniqueIdentifier();
+
+		//return getDebugTarget().getModelIdentifier();
 	}
+	
+	protected synchronized List computeStackFrames(boolean refreshChildren) throws DebugException {
+		if (isSuspended()) {
+			if (isTerminated()) {
+				fStackFrames.clear();
+			} else if (refreshChildren) {
+				List frames = getUnderlyingFrames();
+				int oldSize = fStackFrames.size();
+				int newSize = frames.size();
+				int discard = oldSize - newSize; // number of old frames to discard, if any
+				for (int i = 0; i < discard; i++) {
+					X10StackFrame invalid = (X10StackFrame) fStackFrames.remove(0);
+					invalid.bind(null, -1, true);
+				}
+				int newFrames = newSize - oldSize; // number of frames to create, if any
+				int depth = oldSize;
+				for (int i = newFrames - 1; i >= 0; i--) {
+					fStackFrames.add(0, new X10StackFrame(this, (StackFrame) frames.get(i), depth));
+					depth++;
+				}
+				int numToRebind = Math.min(newSize, oldSize); // number of frames to attempt to rebind
+				int offset = newSize - 1;
+				for (depth = 0; depth < numToRebind; depth++) {
+					X10StackFrame oldFrame = (X10StackFrame) fStackFrames.get(offset);
+					StackFrame frame = (StackFrame) frames.get(offset);
+					JDIStackFrame newFrame = oldFrame.bind(frame, depth,true);
+					if (newFrame != oldFrame) {
+						fStackFrames.set(offset, newFrame);
+					}
+					offset--;
+				}
+				
+
+			}
+			fRefreshChildren = false;
+		} else {
+			return Collections.EMPTY_LIST;
+		}
+		return fStackFrames;
+	}	
+	
+	public synchronized List computeStackFrames() throws DebugException {
+		return computeStackFrames(fRefreshChildren);
+	}
+	
+	protected synchronized void preserveStackFrames() {
+		fRefreshChildren = true;
+		Iterator frames = fStackFrames.iterator();
+		while (frames.hasNext()) {
+			((X10StackFrame)frames.next()).setUnderlyingStackFrame(null);
+		}
+	}
+	
+	protected synchronized void disposeStackFrames() {
+		fStackFrames.clear();
+		fRefreshChildren = true;
+	}
+	
+	protected void popFrame(IStackFrame frame) throws DebugException {
+		JDIDebugTarget target= (JDIDebugTarget)getDebugTarget();
+		if (target.canPopFrames()) {
+			// JDK 1.4 support
+			try {
+				// Pop the frame and all frames above it
+				StackFrame jdiFrame= null;
+				int desiredSize= fStackFrames.size() - fStackFrames.indexOf(frame) - 1;
+				int lastSize= fStackFrames.size() + 1; // Set up to pass the first test
+				int size= fStackFrames.size();
+				while (size < lastSize && size > desiredSize) {
+					// Keep popping frames until the stack stops getting smaller
+					// or popFrame is gone.
+					// see Bug 8054
+					jdiFrame = ((X10StackFrame) frame).getUnderlyingStackFrame();
+					preserveStackFrames();
+					super.getUnderlyingThread().popFrames(jdiFrame);
+					lastSize= size;
+					size= computeStackFrames().size();
+				}
+			} catch (IncompatibleThreadStateException exception) {
+				//targetRequestFailed(MessageFormat.format(JDIDebugModelMessages.JDIThread_exception_popping, new String[] {exception.toString()}),exception); 
+			} catch (InvalidStackFrameException exception) {
+				// InvalidStackFrameException can be thrown when all but the
+				// deepest frame were popped. Fire a changed notification
+				// in case this has occured.
+				fireChangeEvent(DebugEvent.CONTENT);
+				targetRequestFailed(exception.toString(),exception); 
+			} catch (RuntimeException exception) {
+				//targetRequestFailed(MessageFormat.format(JDIDebugModelMessages.JDIThread_exception_popping, new String[] {exception.toString()}),exception); 
+			}
+		}
+	}
+	
+	
+	
+	private List getUnderlyingFrames() throws DebugException {
+		if (!isSuspended()) {
+			// Checking isSuspended here eliminates a race condition in resume
+			// between the time stack frames are preserved and the time the
+			// underlying thread is actually resumed.
+			//requestFailed(JDIDebugModelMessages.JDIThread_Unable_to_retrieve_stack_frame___thread_not_suspended__1, null, IJavaThread.ERR_THREAD_NOT_SUSPENDED); 
+		}
+		try {
+			return super.getUnderlyingThread().frames(); 
+		} catch (IncompatibleThreadStateException e) {
+			//requestFailed(JDIDebugModelMessages.JDIThread_Unable_to_retrieve_stack_frame___thread_not_suspended__1, e, IJavaThread.ERR_THREAD_NOT_SUSPENDED); 
+		} catch (RuntimeException e) {
+			//targetRequestFailed(MessageFormat.format(JDIDebugModelMessages.JDIThread_exception_retrieving_stack_frames_2, new String[] {e.toString()}), e); 
+		} catch (InternalError e) {
+			//targetRequestFailed(MessageFormat.format(JDIDebugModelMessages.JDIThread_exception_retrieving_stack_frames_2, new String[] {e.toString()}), e); 
+		}
+		// execution will not reach this line, as
+		// #targetRequestFailed will thrown an exception
+		return null;
+	}
+	
 	
 	public ObjectReference getFreshActivityObject(ThreadReference t) {
 		Value result=null;
@@ -117,9 +256,11 @@ public class X10Thread extends JDIThread implements IThread, IX10Activity {
 	public void terminated() {
 		super.terminated();
 	}
-	public void disposeStackFrames() {
-		super.disposeStackFrames();
-	}
+	
+	//public void disposeStackFrames() {
+		//super.disposeStackFrames();
+	//}
+	
 	protected void removeCurrentBreakpoint(IBreakpoint bp) {
 		super.removeCurrentBreakpoint(bp);
 	}
@@ -149,6 +290,21 @@ public class X10Thread extends JDIThread implements IThread, IX10Activity {
 		return X10ActivityState.Running;
 	}
 
+/*
+ public IStackFrame[] getStackFrames() throws DebugException {
+	 if (isSuspendedQuiet()) {
+			return new IX10StackFrame[0];
+		}
+		List list = computeStackFrames();
+		IX10StackFrame x10StackFrames[] = new IX10StackFrame[list.size()];
+		int i=0;
+		for (Object jsfo: list) {
+			x10StackFrames[i++] = new X10StackFrame(getDebugTarget(), (JDIStackFrame)jsfo);
+			
+		}
+		return x10StackFrames;
+=======
+
  // mmk re-added java stack frames for testing purposes.  Feel free to remove once the new impl in place.
 public IX10StackFrame[] getStackFrames() {
 //         // TODO create stack frames in sample model
@@ -171,6 +327,18 @@ public IX10StackFrame[] getStackFrames() {
 		}
 	}
 	return x10StackFrames;
+>>>>>>> 1.5
  }
-
+ 
+ public synchronized IStackFrame getTopStackFrame() throws DebugException {
+		List c= computeStackFrames();
+		if (c.isEmpty()) {
+			return null;
+		}
+		IX10StackFrame tos = null;
+		tos = new X10StackFrame(getDebugTarget(), (JDIStackFrame)(c.get(0)));
+		return tos;
+		//return (IStackFrame) c.get(0);
+	}
+*/
 }
