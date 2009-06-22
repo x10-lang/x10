ea437baac2de079038ecba4426e3bcb9845d967d
Robert Fuhrer <rfuhrer@us.ibm.com>
2009-06-22 21:54:51 +0000

    Committed on Tue May 12 20:37:30 2009 Added a toString() impl.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@10251 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt/trunk/org.eclipse.imp.x10dt.ui/src/org/eclipse/imp/x10dt/ui/parser/StringResource.java
+++ x10dt/trunk/org.eclipse.imp.x10dt.ui/src/org/eclipse/imp/x10dt/ui/parser/StringResource.java
@@ -8,26 +8,30 @@ import java.io.StringBufferInputStream;
 import polyglot.frontend.Resource;
 
 public class StringResource implements Resource {
-	private final String fileName;
-	private final File file;
-	private final String contents;
+    private final String fileName;
+    private final File file;
+    private final String contents;
 
-	public StringResource(String contents, File file, String fileName) {
-		this.contents = contents;
-		this.file = file;
-		this.fileName = fileName;
-	}
+    public StringResource(String contents, File file, String fileName) {
+        this.contents = contents;
+        this.file = file;
+        this.fileName = fileName;
+    }
 
-	public File file() {
-		return this.file;
-	}
+    public File file() {
+        return this.file;
+    }
 
-	public InputStream getInputStream() throws IOException {
-		return new StringBufferInputStream(contents);
-	}
+    public InputStream getInputStream() throws IOException {
+        return new StringBufferInputStream(contents);
+    }
 
-	public String name() {
-		return this.fileName;
-	}
+    public String name() {
+        return this.fileName;
+    }
 
+    @Override
+    public String toString() {
+        return name();
+    }
 }
