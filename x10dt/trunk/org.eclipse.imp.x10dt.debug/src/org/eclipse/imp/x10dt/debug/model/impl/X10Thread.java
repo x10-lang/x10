652eec3bc152df3bf27929ed849dfa39fa7ff9ec
Shivali Agarwal <shivaliagarwal@domain>
2009-06-22 21:29:48 +0000

    Committed on Fri Nov 28 14:56:44 2008 uncommented out the state code in getThreadText()
    git-svn-id: svn://svn.code.sf.net/p/x10/code@8746 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt/trunk/org.eclipse.imp.x10dt.debug/src/org/eclipse/imp/x10dt/debug/model/impl/X10Thread.java
+++ x10dt/trunk/org.eclipse.imp.x10dt.debug/src/org/eclipse/imp/x10dt/debug/model/impl/X10Thread.java
@@ -268,16 +268,11 @@ public class X10Thread extends JDIThread implements IThread, IX10Activity {
 	
 	public String getName() {
 		if (fActivity!=null){
-			System.out.println("X10Thread:getName() ");
+			System.out.println("X10Thread:getName() for fActivity:"+fActivity.toString());
 			String place = this.getUnderlyingThread().name();
-			if (place!=null) {
-				place = place.substring(4, 6);
-				if (fFileNameAndLineNo!=null) {
-					return "ACTIVITY-" + uid + "@PLACE" +place +":" +fFileNameAndLineNo ;
-				}
-			}
 			if (place.contains("Main Activity"))
-				return "ACTIVITY-" + uid + "@PLACE 0" + ": Main Activity";
+				{place = place.substring(4, 6);
+				return "ACTIVITY-" + uid + "@PLACE" +place +":" +fFileNameAndLineNo ;}
 			/*
 			else if (place.contains("Finished")){
 				place=place.replace("pool", "PLACE");�
@@ -410,74 +405,43 @@ public class X10Thread extends JDIThread implements IThread, IX10Activity {
  
  public String getThreadText() {
 	 
-		if (fActivity!=null){
-			System.out.println("X10Thread:getName() ");
-			String place = this.getUnderlyingThread().name();
-			if (place.contains("Main Activity"))
-                                               {  place = place.substring(4, 6);
-				return "ACTIVITY-" + uid + "@PLACE" +place +":" +fFileNameAndLineNo ;}
-			/*
-			else if (place.contains("Finished")){
+	 if (isSuspended() && fActivity!=null){
+		 fState=X10ActivityState.Suspended;
+	 }
+	 if (fState!=X10ActivityState.Idle) {
+		    System.out.println("X10ModelPresentation: a!=null");	
+		    String name = this.getName();
+		    System.out.println("X10ModelPresentation: name ="+name);
+		    /*if (name.contains("terminated")) {
+		    	String place=t.name();
 				place=place.replace("pool", "PLACE");
 				place=place.substring(0, 16);
 			    return place + "(Idle)";
-				//return "ACTIVITY: "+this.getUnderlyingThread().toString();
-			}*/
-			else {
-			String aname = ((X10DebugTargetAlt)getDebugTarget()).getActivityString(fActivity);
-			System.out.println("X10Thread:getName , aname = "+aname);
-			//if (aname.contains("false")) {
-				//return "terminated";
-			//}
-			if (aname==null) {
-				aname=" ";
+		    }*/
+		    //if (((X10Thread)element).isSuspended()){
+		    if (fState==IX10Activity.X10ActivityState.Suspended) {
+		    	//name=name.replace("true","Suspended");
+		    	name=name.concat(" Suspended");
+		    }
+		    else if (fState == IX10Activity.X10ActivityState.Blocked) {
+		        //name=name.replace("true","Running");
+		    	name=name.concat(" Blocked");
+		    }
+		    else name=name.concat(" Running"); 
+		    return name;
+		}	
+	 else{
+			
+			String place=this.getUnderlyingThread().name();
+			if (place.contains("pool")) {
+			  place=place.replace("pool", "PLACE");
+			  place=place.substring(0, 16);
+		      return place + "(Idle)";// need to call getActivity().toString() on PoolRunner (on VM Side)
 			}
-                                    place = place.substring(4, 6);
-		            return "ACTIVITY-" + uid + "@PLACE" +place +":" +aname ;
-		       }
-		}  
-		else {
-			System.out.println("X10Thread:getName - fActivity is null");
-			return this.getUnderlyingThread().name();
-		    //return "ACTIVITY: "+this.getUnderlyingThread().toString();
+			return place;
+			//return "Activity:" + t.toString();
 		}
-//	 if (isSuspended() && fActivity!=null){
-//		 fState=X10ActivityState.Suspended;
-//	 }
-//	 if (fState!=X10ActivityState.Idle) {
-//		    System.out.println("X10ModelPresentation: a!=null");	
-//		    String name = this.getName();
-//		    System.out.println("X10ModelPresentation: name ="+name);
-//		    /*if (name.contains("terminated")) {
-//		    	String place=t.name();
-//				place=place.replace("pool", "PLACE");
-//				place=place.substring(0, 16);
-//			    return place + "(Idle)";
-//		    }*/
-//		    //if (((X10Thread)element).isSuspended()){
-//		    if (fState==IX10Activity.X10ActivityState.Suspended) {
-//		    	//name=name.replace("true","Suspended");
-//		    	name=name.concat(" Suspended");
-//		    }
-//		    else if (fState == IX10Activity.X10ActivityState.Blocked) {
-//		        //name=name.replace("true","Running");
-//		    	name=name.concat(" Blocked");
-//		    }
-//		    else name=name.concat(" Running"); 
-//		    return name;
-//		}	
-//	 else{
-//			
-//			String place=this.getUnderlyingThread().name();
-//			if (place.contains("pool")) {
-//			  place=place.replace("pool", "PLACE");
-//			  place=place.substring(0, 16);
-//		      return place + "(Idle)";// need to call getActivity().toString() on PoolRunner (on VM Side)
-//			}
-//			return place;
-//			//return "Activity:" + t.toString();
-//		}
-//	 
+	 
  }
  
  //Should be moved to utils eventually
