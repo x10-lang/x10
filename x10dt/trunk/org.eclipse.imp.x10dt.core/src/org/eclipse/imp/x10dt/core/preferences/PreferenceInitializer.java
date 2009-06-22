b95f92a53bc7910b5f7af9962119965d46038c5e
Robert Fuhrer <rfuhrer@us.ibm.com>
2009-06-22 21:27:59 +0000

    Committed on Fri Feb 17 20:16:26 2006 Now tries to come up with a more reasonable default for the location of x10.common and the standard.cfg file.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@8637 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt/trunk/org.eclipse.imp.x10dt.core/src/org/eclipse/imp/x10dt/core/preferences/PreferenceInitializer.java
+++ x10dt/trunk/org.eclipse.imp.x10dt.core/src/org/eclipse/imp/x10dt/core/preferences/PreferenceInitializer.java
@@ -1,8 +1,14 @@
 package com.ibm.watson.safari.x10.preferences;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
 import org.eclipse.jface.preference.IPreferenceStore;
+import org.osgi.framework.Bundle;
 
 import com.ibm.watson.safari.x10.X10Plugin;
 
@@ -21,8 +27,23 @@ public class PreferenceInitializer extends AbstractPreferenceInitializer {
 	// Disabled "auto-add runtime", since that causes subsequent Java compiles to
 	// fail, because then x10.runtime won't be in the Java build-time classpath.
 	store.setDefault(PreferenceConstants.P_AUTO_ADD_RUNTIME, false);
-	store.setDefault(PreferenceConstants.P_X10COMMON_PATH, "???");
-	store.setDefault(PreferenceConstants.P_X10CONFIG_FILE, store.getString(PreferenceConstants.P_X10COMMON_PATH) + File.separator + "standard.cfg");
+        
+	try {
+	    Bundle x10CommonBundle= Platform.getBundle("x10.common");
+            URL x10CommonURL= Platform.asLocalURL(Platform.find(x10CommonBundle, new Path("")));
+            URL stdCfgURL= Platform.asLocalURL(Platform.find(x10CommonBundle, new Path("etc/standard.cfg")));
+	    String x10CommonPath= x10CommonURL.getPath();
+            String stdCfgPath= stdCfgURL.getPath();
+
+            if (Platform.getOS().equals("win32")) {
+                x10CommonPath= x10CommonPath.substring(1);
+                stdCfgPath= stdCfgPath.substring(1);
+            }
+            store.setDefault(PreferenceConstants.P_X10COMMON_PATH, x10CommonPath);
+            store.setDefault(PreferenceConstants.P_X10CONFIG_FILE, stdCfgPath);
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
 //	store.setDefault(PreferenceConstants.P_CHOICE, "choice2");
     }
 }
