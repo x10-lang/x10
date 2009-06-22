06614cbeb5cb01f9b65aa174119bf6e318093fb9
Robert Fuhrer <rfuhrer@us.ibm.com>
2009-06-22 21:36:41 +0000

    Committed on Fri Feb  3 19:53:38 2006 Now gives Polyglot a classpath with the x10 runtime in it so that the parser can load classes like x10.lang.genericArray$pointwiseOp. Not doing so was the cause of parsing failures and subsequent loss of syntax highlighting and outline contents.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@9159 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt/trunk/org.eclipse.imp.x10dt.ui/src/org/eclipse/imp/x10dt/ui/parser/ParserDelegate.java
+++ x10dt/trunk/org.eclipse.imp.x10dt.ui/src/org/eclipse/imp/x10dt/ui/parser/ParserDelegate.java
@@ -2,11 +2,20 @@ package x10.uide.parser;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Platform;
+import org.eclipse.jdt.core.IClasspathEntry;
+import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.uide.parser.IParser;
+import com.ibm.watson.safari.x10.X10Plugin;
+import com.ibm.watson.safari.x10.preferences.X10Preferences;
 import polyglot.ext.x10.ExtensionInfo;
 import polyglot.frontend.Compiler;
 import polyglot.frontend.FileSource;
+import polyglot.main.Options;
+import polyglot.main.UsageError;
 import polyglot.util.StdErrorQueue;
 import x10.parser.X10Parser;
 import lpg.lpgjavaruntime.LexStream;
@@ -36,6 +45,8 @@ public class ParserDelegate implements IParser {
 	ExtensionInfo extInfo= new ExtensionInfo();
 	StdErrorQueue eq= new StdErrorQueue(System.err, 100000, "stderr");// TODO replace me
 	Compiler compiler= new Compiler(extInfo, eq);
+
+	buildOptions(extInfo);
 	extInfo.initCompiler(compiler);
 	try {
 	    // TODO Need to know a/the real file name (must exist or File() ctor will barf)
@@ -49,5 +60,33 @@ public class ParserDelegate implements IParser {
 	    e.printStackTrace();
 	    throw new IllegalArgumentException(e.getMessage());
 	}
+    }
+
+    private void buildOptions(ExtensionInfo extInfo) {
+	Options opts= extInfo.getOptions();
+
+	Options.global= opts;
+	try {
+	    opts.parseCommandLine(new String[] { "-cp", buildClassPathSpec(), }, new HashSet());
+	} catch (UsageError e) {
+	    if (!e.getMessage().equals("must specify at least one source file"))
+		System.err.println(e.getMessage());
+	}
+	X10Plugin.getInstance().maybeWriteInfoMsg("Source path = " + opts.source_path);
+	X10Plugin.getInstance().maybeWriteInfoMsg("Class path = " + opts.classpath);
+    }
+
+    private String buildClassPathSpec() {
+	StringBuffer buff= new StringBuffer();
+
+	// HACK RMF 2/3/2005 - Really, should copy the classpath of the source file's
+	// containing project, but we don't even know what bloody file we're parsing.
+	// This will become critical when we start providing services that require
+	// name-resolved AST's.
+	String commonPath= X10Preferences.x10CommonPath;
+	String runtimePath= commonPath.substring(0, commonPath.lastIndexOf(File.separator) + 1) + "x10.runtime" + File.separator + "classes";
+
+	buff.append(runtimePath);
+	return buff.toString();
     }
 }
