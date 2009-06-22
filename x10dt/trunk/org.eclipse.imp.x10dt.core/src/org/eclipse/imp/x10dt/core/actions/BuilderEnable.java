311df47bba314ee88640a6a405aea688c0e5b08e
Robert Fuhrer <rfuhrer@us.ibm.com>
2009-06-22 21:43:34 +0000

    Committed on Fri Dec  2 15:24:45 2005 Action should now work whether projects are selected in the Navigator or Package Explorer.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@9574 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt/trunk/org.eclipse.imp.x10dt.core/src/org/eclipse/imp/x10dt/core/actions/BuilderEnable.java
+++ x10dt/trunk/org.eclipse.imp.x10dt.core/src/org/eclipse/imp/x10dt/core/actions/BuilderEnable.java
@@ -1,21 +1,19 @@
 package com.ibm.watson.safari.x10.actions;
 
 import org.eclipse.core.resources.IProject;
+import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
-
 import com.ibm.watson.safari.x10.X10ProjectNature;
 
 /**
- * Our sample action implements workbench action delegate. The action proxy will be created by the workbench and shown in the UI. When the user tries to use the
- * action, this delegate will be created and execution will be delegated to it.
+ * A simple action to enable the X10 builder for the currently-selected project.
  * @see IWorkbenchWindowActionDelegate
  */
 public class BuilderEnable implements IWorkbenchWindowActionDelegate {
-    private IWorkbenchWindow window;
     private IProject fProject;
 
     public BuilderEnable() {}
@@ -37,24 +35,15 @@ public class BuilderEnable implements IWorkbenchWindowActionDelegate {
 	if (selection instanceof IStructuredSelection) {
 	    IStructuredSelection ss= (IStructuredSelection) selection;
 	    Object first= ss.getFirstElement();
-
 	    if (first instanceof IProject) {
 		fProject= (IProject) first;
+	    } else if (first instanceof IJavaProject) {
+		fProject= ((IJavaProject) first).getProject();
 	    }
 	}
     }
 
-    /**
-     * We can use this method to dispose of any system resources we previously allocated.
-     * @see IWorkbenchWindowActionDelegate#dispose
-     */
     public void dispose() {}
 
-    /**
-     * We will cache window object in order to be able to provide parent shell for the message dialog.
-     * @see IWorkbenchWindowActionDelegate#init
-     */
-    public void init(IWorkbenchWindow window) {
-	this.window= window;
-    }
+    public void init(IWorkbenchWindow window) {}
 }
