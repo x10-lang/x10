b4ab09d4225efb53ee68672426f3fab7cb2a6474
Matthew Kaplan <user@domain>
2009-06-22 21:44:53 +0000

    Committed on Wed Jun  4 02:56:15 2008 Update-site build 6/3/2008
    git-svn-id: svn://svn.code.sf.net/p/x10/code@9653 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt/trunk/org.eclipse.imp.x10dt.core/src/org/eclipse/imp/x10dt/core/preferences/specialized/X10PreferencesOneTab.java
+++ x10dt/trunk/org.eclipse.imp.x10dt.core/src/org/eclipse/imp/x10dt/core/preferences/specialized/X10PreferencesOneTab.java
@@ -7,6 +7,18 @@
 *
 * Contributors:
 *    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
+
+*******************************************************************************/
+
+/*******************************************************************************
+* Copyright (c) 2008 IBM Corporation.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
 *    Matthew Kaplan (mmk@us.ibm.com) - specialization to show only one tab (to simplify options for novices/typical users)
 *******************************************************************************/
 
