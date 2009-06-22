9c5102494b95d8d960683d0aeb7799cf76dcbba0
Robert Fuhrer <rfuhrer@us.ibm.com>
2009-06-22 21:44:14 +0000

    Committed on Wed Aug  2 13:52:04 2006 Removed unnecessary imports.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@9614 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt/trunk/org.eclipse.imp.x10dt.core/src/org/eclipse/imp/x10dt/core/wizards/X10ProjectWizardSecondPage.java
+++ x10dt/trunk/org.eclipse.imp.x10dt.core/src/org/eclipse/imp/x10dt/core/wizards/X10ProjectWizardSecondPage.java
@@ -3,41 +3,14 @@
  */
 package com.ibm.watson.safari.x10.wizards;
 
-import java.io.IOException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-
-import org.eclipse.core.resources.IFolder;
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.core.runtime.OperationCanceledException;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Platform;
-import org.eclipse.core.runtime.SubProgressMonitor;
-import org.eclipse.jdt.core.IClasspathEntry;
-import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.jdt.core.JavaCore;
-import org.eclipse.jdt.internal.corext.util.JavaModelUtil;
-import org.eclipse.jdt.internal.ui.util.CoreUtility;
-import org.eclipse.jdt.internal.ui.wizards.ClassPathDetector;
-import org.eclipse.jdt.internal.ui.wizards.NewWizardMessages;
-import org.eclipse.jdt.ui.PreferenceConstants;
-import org.eclipse.jdt.ui.wizards.JavaCapabilityConfigurationPage;
-import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.uide.core.ProjectNatureBase;
 import org.eclipse.uide.wizards.NewProjectWizardSecondPage;
 import org.osgi.framework.Bundle;
 
-import com.ibm.watson.safari.x10.X10Plugin;
-import com.ibm.watson.safari.x10.builder.X10Builder;
 import com.ibm.watson.safari.x10.builder.X10ProjectNature;
-import com.ibm.watson.safari.x10.preferences.X10Preferences;
 
 public class X10ProjectWizardSecondPage extends NewProjectWizardSecondPage {
     public X10ProjectWizardSecondPage(X10ProjectWizardFirstPage firstPage) {
