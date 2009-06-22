91e37b60134137a379c72a67d245b6f11c3e6432
Robert Fuhrer <rfuhrer@us.ibm.com>
2009-06-22 21:36:37 +0000

    Committed on Fri Feb 17 19:10:08 2006 Default ctor superceded by an explicit ctor taking a file path so that the X10Lexer can be told what file it's scanning.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@9155 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt/trunk/org.eclipse.imp.x10dt.ui/src/org/eclipse/imp/x10dt/ui/parser/LexerDelegate.java
+++ x10dt/trunk/org.eclipse.imp.x10dt.ui/src/org/eclipse/imp/x10dt/ui/parser/LexerDelegate.java
@@ -1,5 +1,7 @@
 package x10.uide.parser;
 
+import java.io.IOException;
+
 import org.eclipse.uide.parser.ILexer;
 import x10.parser.X10Lexer;
 
@@ -9,8 +11,11 @@ import lpg.lpgjavaruntime.PrsStream;
 
 public class LexerDelegate implements ILexer
 {
-	X10Lexer myLexer = new X10Lexer();
+	X10Lexer myLexer;
 
+        public LexerDelegate(String absFilePath) throws IOException {
+            myLexer= new X10Lexer(absFilePath);
+        }
 	public int [] getKeywordKinds() { return myLexer.getKeywordKinds(); }
 	public LexStream getLexStream() { return myLexer.getLexStream(); }
 	public void initialize(char [] content, String filename) { myLexer.initialize(content, filename); }
