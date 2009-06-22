59b485fb943cf6c20cf060ba27fc5886f418ce75
Robert Fuhrer <rfuhrer@us.ibm.com>
2009-06-22 21:35:13 +0000

    Committed on Tue Sep 12 18:29:46 2006 Accommodated removal of base interface method hasSuitableLinkSourceType(), and simplified things slightly. Fixed getLinkTarget() to produce something more sensible (e.g. Declaration's) for the various node types.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@9071 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt/trunk/org.eclipse.imp.x10dt.ui/src/org/eclipse/imp/x10dt/ui/editor/X10ReferenceResolver.java
+++ x10dt/trunk/org.eclipse.imp.x10dt.ui/src/org/eclipse/imp/x10dt/ui/editor/X10ReferenceResolver.java
@@ -1,113 +1,81 @@
 package x10.uide.editor;
 
-
+import java.util.List;
+import org.eclipse.uide.core.ILanguageService;
+import org.eclipse.uide.editor.IReferenceResolver;
 import org.eclipse.uide.editor.ReferenceResolver;
 import org.eclipse.uide.parser.IParseController;
-
+import polyglot.ast.Ambiguous;
+import polyglot.ast.Call;
+import polyglot.ast.Field;
+import polyglot.ast.Local;
+import polyglot.ast.LocalDecl;
+import polyglot.ast.Node;
+import polyglot.ast.Receiver;
+import polyglot.ast.Special;
+import polyglot.ast.TypeNode;
+import polyglot.types.Declaration;
+import polyglot.types.FieldInstance;
 import polyglot.types.LocalInstance;
+import polyglot.types.MethodInstance;
+import polyglot.types.ReferenceType;
 import polyglot.visit.NodeVisitor;
 
-//import lpg.lpgjavaruntime.*;
-
-//import x10.safari.parser.Ast.*;
-import polyglot.ast.*;
-
-
-public class X10ReferenceResolver extends ReferenceResolver {
-
-	public X10ReferenceResolver () {
-		super();
-
-		// If you want to stipulate at construction time the
-		// node types that are legal link source or target types,
-		// you can do that here ...
-		Class[] sourceTypes = new Class[4];
-		sourceTypes[0] = TypeNode.class;
-		sourceTypes[1] = Call.class;
-		sourceTypes[2] = Field.class;
-		sourceTypes[3] = Local.class;
-		setSourceTypes(sourceTypes);
+public class X10ReferenceResolver implements IReferenceResolver, ILanguageService {
+    /**
+     * Get the target for a given source node in the AST represented by a
+     * given Parse Controller.
+     */
+    public Object getLinkTarget(Object node, IParseController parseController) {
+	if (node instanceof Ambiguous) {
+	    return null;
 	}
-	
-	
-	/**
-	 * Get the target for a given source node in the AST represented by a
-	 * given Parse Controller.
-	 */
-	public Object getLinkTarget(Object node, IParseController parseController)
-	{
-		if (node instanceof TypeNode) {
-		    TypeNode typeNode= (TypeNode) node;
-		    return typeNode.type();
-		} else if (node instanceof Call) {
-		    Call call= (Call) node;
-		    Receiver rcvr= call.target();
-		    return getLinkTarget(rcvr, parseController);
-		} else if (node instanceof Field) {
-		    Field field= (Field) node;
-		    return field.type();
-		} else if (node instanceof Local) {
-		    Local local= (Local) node;
-			Node varDefNode = findVarDefinition(local, (Node) parseController.getCurrentAst());
-			return varDefNode;
-		}
-		return null;
+	if (node instanceof TypeNode) {
+	    TypeNode typeNode= (TypeNode) node;
+	    return typeNode.type();
+	} else if (node instanceof Call) {
+	    Call call= (Call) node;
+	    MethodInstance mi= call.methodInstance();
+	    Declaration decl= mi.declaration();
+	    return decl;
+	} else if (node instanceof Field) {
+	    Field field= (Field) node;
+	    FieldInstance fi= field.fieldInstance();
+	    Declaration decl= fi.declaration();
+	    return decl;
+	} else if (node instanceof Local) {
+	    Local local= (Local) node;
+	    LocalInstance li= local.localInstance();
+	    Declaration decl= li.declaration();
+	    return decl;
 	}
-	
-	
-    public static Node findVarDefinition(Local local, Node ast) {
-    	final LocalInstance li= local.localInstance();
-    	final LocalDecl ld[]= new LocalDecl[1];
-
-    	NodeVisitor finder= new NodeVisitor() {
-    	    public NodeVisitor enter(Node n) {
-    		if (n instanceof LocalDecl) {
-    		    LocalDecl thisLD= (LocalDecl) n;
-
-    		    if (thisLD.localInstance() == li)
-    			ld[0]= thisLD;
-    		}
-    		return super.enter(n);
-    	    }
-    	};
-    	ast.visit(finder);
-    	return ld[0];
+	return null;
     }
-    
-	public boolean hasSuitableLinkSourceType(Object node) {
-		if (node instanceof Ambiguous) {
-			//System.out.println("hasSuitableLinkSourceType found Ambiguous node (returning false)");
-			return false;
-		}
-		if (node instanceof TypeNode) {
-			//System.out.println("hasSuitableLinkSourceType found TypeNode (returning true)");
-		    return true;
-		}
-		if (node instanceof Call) {
-			//System.out.println("hasSuitableLinkSourceType found Call (returning true)");
-		    return true;
-		}
-		if (node instanceof Field) {
-			//System.out.println("hasSuitableLinkSourceType found Field (returning true)");
-		    return true;
-		}
-		if (node instanceof Local) {
-			//System.out.println("hasSuitableLinkSourceType found Local (returning true)");
-		    return true;
-		}
-		return false;
-	}
-    
-		
-	/**
-	 * Get the text associated with a given node for use in a link
-	 * from (or to) that node
-	 */
-	public String getLinkText(Object node) {
-		// In the original, link text is determined by ISourceHyperlink,
-		// and this is all that does
-		return node.toString();
-	}
 
+    public static Node findVarDefinition(Local local, Node ast) {
+	final LocalInstance li= local.localInstance();
+	final LocalDecl ld[]= new LocalDecl[1];
+	NodeVisitor finder= new NodeVisitor() {
+	    public NodeVisitor enter(Node n) {
+		if (n instanceof LocalDecl) {
+		    LocalDecl thisLD= (LocalDecl) n;
+		    if (thisLD.localInstance() == li)
+			ld[0]= thisLD;
+		}
+		return super.enter(n);
+	    }
+	};
+	ast.visit(finder);
+	return ld[0];
+    }
 
+    /**
+     * Get the text associated with a given node for use in a link
+     * from (or to) that node
+     */
+    public String getLinkText(Object node) {
+	// In the original, link text is determined by ISourceHyperlink,
+	// and this is all that does
+	return node.toString();
+    }
 }
