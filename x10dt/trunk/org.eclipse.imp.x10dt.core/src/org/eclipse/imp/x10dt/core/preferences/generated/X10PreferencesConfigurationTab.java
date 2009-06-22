1181c7a28ac1acbeae477a6ad7231c4ec0792fd3
Matthew Kaplan <user@domain>
2009-06-22 21:28:15 +0000

    Committed on Wed May 21 17:49:20 2008 Created simplified prefs page for tab
    git-svn-id: svn://svn.code.sf.net/p/x10/code@8653 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ x10dt/trunk/org.eclipse.imp.x10dt.core/src/org/eclipse/imp/x10dt/core/preferences/generated/X10PreferencesConfigurationTab.java
@@ -0,0 +1,72 @@
+/*******************************************************************************
+* Copyright (c) 2008 IBM Corporation.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
+
+*******************************************************************************/
+
+package org.eclipse.imp.x10dt.core.preferences.generated;
+
+import java.util.List;
+import java.util.ArrayList;
+import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
+import org.eclipse.core.runtime.preferences.IEclipsePreferences;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Link;
+import org.eclipse.imp.preferences.*;
+import org.eclipse.imp.preferences.fields.*;
+import org.osgi.service.prefs.Preferences;
+
+//		 TODO:  Import additional classes for specific field types from
+//		 org.eclipse.uide.preferences.fields
+
+/**
+ * The configuration level preferences tab.
+ */
+
+
+public class X10PreferencesConfigurationTab extends ConfigurationPreferencesTab {
+
+	public X10PreferencesConfigurationTab(IPreferencesService prefService) {
+		super(prefService);
+	}
+
+	/**
+	 * Creates specific preference fields with settings appropriate to
+	 * the configuration preferences level.
+	 *
+	 * Overrides an unimplemented method in PreferencesTab.
+	 *
+	 * @return    An array that contains the created preference fields
+	 *
+	 */
+	protected FieldEditor[] createFields(
+		TabbedPreferencesPage page, PreferencesTab tab, String tabLevel,
+		Composite parent)
+	{
+		List fields = new ArrayList();
+
+		IntegerFieldEditor TabSize = fPrefUtils.makeNewIntegerField(
+			page, tab, fPrefService,
+			"configuration", "TabSize", "TabSize",
+			parent,
+			true, true,
+			true, String.valueOf(8),
+			false, "0",
+			false);
+			Link TabSizeDetailsLink = fPrefUtils.createDetailsLink(parent, TabSize, TabSize.getTextControl().getParent(), "Details ...");
+
+		fields.add(TabSize);
+
+		FieldEditor[] fieldsArray = new FieldEditor[fields.size()];
+		for (int i = 0; i < fields.size(); i++) {
+			fieldsArray[i] = (FieldEditor) fields.get(i);
+		}
+		return fieldsArray;
+	}
+}
---
+++ x10dt/trunk/org.eclipse.imp.x10dt.core/src/org/eclipse/imp/x10dt/core/preferences/generated/X10PreferencesConstants.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+* Copyright (c) 2008 IBM Corporation.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
+
+*******************************************************************************/
+
+package org.eclipse.imp.x10dt.core.preferences.generated;
+
+/**
+ * Constant definitions for preferences.
+ *
+ * The preferences service uses Strings as keys for preference values,
+ * so Strings defined here are used here to designate preference fields.
+ * These strings are generated automatically from a preferences specification.
+ * Other constants may be defined here manually.
+ *
+ */
+
+
+public class X10PreferencesConstants {
+
+	public static final String P_TABSIZE = "TabSize";
+
+}
