1242269d95f98313d0b6ff43396169783751ea61
Robert Fuhrer <rfuhrer@us.ibm.com>
2009-06-22 21:41:47 +0000

    Committed on Fri May 22 18:25:07 2009 computeEffect(Local) now properly omits the write to "val" locals, and includes the write effect to "var" locals.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@9466 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt/trunk/x10.refactoring/src/x10/refactorings/EffectsVisitor.java
+++ x10dt/trunk/x10.refactoring/src/x10/refactorings/EffectsVisitor.java
@@ -8,30 +8,21 @@ import java.util.Map;
 
 import polyglot.ast.Binary;
 import polyglot.ast.Block;
-import polyglot.ast.BooleanLit;
 import polyglot.ast.Call;
 import polyglot.ast.CanonicalTypeNode;
-import polyglot.ast.CharLit;
-import polyglot.ast.ClassLit;
 import polyglot.ast.Eval;
 import polyglot.ast.Expr;
 import polyglot.ast.Field;
 import polyglot.ast.FieldAssign;
-import polyglot.ast.FloatLit;
 import polyglot.ast.Formal;
-import polyglot.ast.Id;
 import polyglot.ast.If;
-import polyglot.ast.IntLit;
 import polyglot.ast.Local;
 import polyglot.ast.LocalAssign;
 import polyglot.ast.LocalDecl;
-import polyglot.ast.NamedVariable;
 import polyglot.ast.New;
 import polyglot.ast.Node;
 import polyglot.ast.Receiver;
-import polyglot.ast.Special;
 import polyglot.ast.Stmt;
-import polyglot.ast.StringLit;
 import polyglot.ast.Unary;
 import polyglot.ast.VarDecl;
 import polyglot.ast.While;
@@ -46,31 +37,22 @@ import polyglot.ext.x10.ast.X10Formal;
 import polyglot.ext.x10.ast.X10MethodDecl;
 import polyglot.ext.x10.types.X10FieldInstance;
 import polyglot.ext.x10.types.X10Flags;
-import polyglot.ext.x10.types.X10LocalDef;
 import polyglot.ext.x10.types.X10LocalInstance;
 import polyglot.ext.x10.types.X10ProcedureDef;
 import polyglot.ext.x10.types.X10ProcedureInstance;
 import polyglot.ext.x10.types.X10Type;
 import polyglot.ext.x10.types.X10TypeMixin;
-import polyglot.ext.x10.types.X10TypeSystem;
 import polyglot.types.ClassType;
 import polyglot.types.ConstructorInstance;
-import polyglot.types.FieldInstance;
 import polyglot.types.MethodInstance;
 import polyglot.types.ProcedureInstance;
-import polyglot.types.Qualifier;
 import polyglot.types.StructType;
 import polyglot.types.Type;
-import polyglot.types.VarDef;
 import polyglot.visit.NodeVisitor;
-import x10.constraint.XArray;
 import x10.constraint.XConstraint;
 import x10.constraint.XFailure;
-import x10.constraint.XName;
-import x10.constraint.XNameWrapper;
 import x10.constraint.XTerm;
 import x10.constraint.XTerms;
-import x10.constraint.XVar;
 import x10.effects.constraints.ArrayElementLocs;
 import x10.effects.constraints.Effect;
 import x10.effects.constraints.Effects;
@@ -80,206 +62,24 @@ import x10.effects.constraints.Locs;
 import x10.refactorings.ReachingDefsVisitor.ValueMap;
 
 public class EffectsVisitor extends NodeVisitor {
-    public static class XVarDefWrapper implements XName {
-        private final VarDef fVarDef;
-        public XVarDefWrapper(VarDef vd) {
-            fVarDef= vd;
-        }
-        public XVarDefWrapper(NamedVariable var) {
-            this((VarDef) var.varInstance().def());
-        }
-        @Override
-        public int hashCode() {
-            return fVarDef.name().toString().hashCode();
-        }
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj) return true;
-            if (!(obj instanceof XVarDefWrapper)) return false;
-            XVarDefWrapper other= (XVarDefWrapper) obj;
-            if (fVarDef == other.fVarDef) return true;
-            // Probably should never need the following...
-            if (!fVarDef.position().equals(other.fVarDef.position()))
-                return false;
-            if (!fVarDef.name().toString().equals(other.fVarDef.name().toString()))
-                return false;
-            return true;
-        }
-        @Override
-        public String toString() {
-            return fVarDef.name().toString();
-        }
-    }
-
-    private static class TermCreator {
-        private static Map<Unary.Operator,XName> sUnaryOpMap= new HashMap<Unary.Operator,XName>();
-        static {
-            sUnaryOpMap.put(Unary.BIT_NOT, new XNameWrapper<Unary.Operator>(Unary.BIT_NOT));
-            sUnaryOpMap.put(Unary.NEG, new XNameWrapper<Unary.Operator>(Unary.NEG));
-            sUnaryOpMap.put(Unary.NOT, new XNameWrapper<Unary.Operator>(Unary.NOT));
-            sUnaryOpMap.put(Unary.POS, new XNameWrapper<Unary.Operator>(Unary.POS));
-            sUnaryOpMap.put(Unary.BIT_NOT, new XNameWrapper<Unary.Operator>(Unary.BIT_NOT));
-        }
-
-        private static Map<Binary.Operator,XName> sBinaryOpMap= new HashMap<Binary.Operator,XName>();
-        static {
-            sBinaryOpMap.put(Binary.ADD, new XNameWrapper<Binary.Operator>(Binary.ADD));
-            sBinaryOpMap.put(Binary.BIT_AND, new XNameWrapper<Binary.Operator>(Binary.BIT_AND));
-            sBinaryOpMap.put(Binary.BIT_OR, new XNameWrapper<Binary.Operator>(Binary.BIT_OR));
-            sBinaryOpMap.put(Binary.BIT_XOR, new XNameWrapper<Binary.Operator>(Binary.BIT_XOR));
-            sBinaryOpMap.put(Binary.COND_AND, new XNameWrapper<Binary.Operator>(Binary.COND_AND));
-            sBinaryOpMap.put(Binary.COND_OR, new XNameWrapper<Binary.Operator>(Binary.COND_OR));
-            sBinaryOpMap.put(Binary.DIV, new XNameWrapper<Binary.Operator>(Binary.DIV));
-            sBinaryOpMap.put(Binary.EQ, new XNameWrapper<Binary.Operator>(Binary.EQ));
-            sBinaryOpMap.put(Binary.GE, new XNameWrapper<Binary.Operator>(Binary.GE));
-            sBinaryOpMap.put(Binary.GT, new XNameWrapper<Binary.Operator>(Binary.GT));
-            sBinaryOpMap.put(Binary.LE, new XNameWrapper<Binary.Operator>(Binary.LE));
-            sBinaryOpMap.put(Binary.LT, new XNameWrapper<Binary.Operator>(Binary.LT));
-            sBinaryOpMap.put(Binary.MOD, new XNameWrapper<Binary.Operator>(Binary.MOD));
-            sBinaryOpMap.put(Binary.MUL, new XNameWrapper<Binary.Operator>(Binary.MUL));
-            sBinaryOpMap.put(Binary.NE, new XNameWrapper<Binary.Operator>(Binary.NE));
-            sBinaryOpMap.put(Binary.SHL, new XNameWrapper<Binary.Operator>(Binary.SHL));
-            sBinaryOpMap.put(Binary.SHR, new XNameWrapper<Binary.Operator>(Binary.SHR));
-            sBinaryOpMap.put(Binary.SUB, new XNameWrapper<Binary.Operator>(Binary.SUB));
-            sBinaryOpMap.put(Binary.USHR, new XNameWrapper<Binary.Operator>(Binary.USHR));
-        }
-
-        private XName getNameFor(Binary.Operator op) {
-            return sBinaryOpMap.get(op);
-        }
-
-        private XName getNameFor(Unary.Operator op) {
-            return sUnaryOpMap.get(op);
-        }
-
-        private class TermVisitor extends NodeVisitor {
-            @Override
-            public Node leave(Node parent, Node old, Node n, NodeVisitor v) {
-                if (old instanceof BooleanLit) {
-                    BooleanLit booleanLit = (BooleanLit) old;
-
-                    fTermMap.put(old, XTerms.makeLit(booleanLit.value()));
-                } else if (old instanceof FloatLit) {
-                    FloatLit floatLit = (FloatLit) old;
-
-                    fTermMap.put(old, XTerms.makeLit(floatLit.value()));
-                } else if (old instanceof CharLit) {
-                    CharLit charLit = (CharLit) old;
-
-                    fTermMap.put(old, XTerms.makeLit(charLit.value()));
-                } else if (old instanceof IntLit) {
-                    IntLit intLit = (IntLit) old;
-
-                    fTermMap.put(old, XTerms.makeLit(intLit.value()));
-                } else if (old instanceof StringLit) {
-                    StringLit stringLit = (StringLit) old;
-
-                    fTermMap.put(old, XTerms.makeLit(stringLit.value()));
-                } else if (old instanceof ClassLit) {
-                    throw new UnsupportedOperationException("Can't handle class literals.");
-                } else if (old instanceof CanonicalTypeNode) {
-                    CanonicalTypeNode canonicalTypeNode = (CanonicalTypeNode) old;
-                    Qualifier qualifier= canonicalTypeNode.qualifierRef().get();
-                    String shortName= canonicalTypeNode.nameString();
-
-                    fTermMap.put(old, XTerms.makeLit(qualifier.toString() + "." + shortName));
-                } else if (old instanceof Field) {
-                    Field field = (Field) old;
-                    Receiver target= field.target();
-                    Id name= field.name();
-
-                    fTermMap.put(old, XTerms.makeField((XVar) fTermMap.get(target), new XVarDefWrapper(field)));
-                } else if (old instanceof Local) {
-                    Local local = (Local) old;
-                    Type localType= local.type();
-                    X10TypeSystem ts= (X10TypeSystem) localType.typeSystem();
-
-                    if (local.type().isArray() || local.type().descendsFrom(ts.Array())) {
-                        fTermMap.put(old, XTerms.makeArray(new XVarDefWrapper(local)));
-                    } else {
-                        fTermMap.put(old, XTerms.makeLocal(new XVarDefWrapper(local)));
-                    }
-                } else if (old instanceof Binary) {
-                    Binary binary = (Binary) old;
-                    Binary.Operator op= binary.operator();
-                    Expr lhs= binary.left();
-                    Expr rhs= binary.right();
-                    XTerm lhsTerm= fTermMap.get(lhs);
-                    XTerm rhsTerm= fTermMap.get(rhs);
-
-                    fTermMap.put(old, XTerms.makeAtom(getNameFor(op), lhsTerm, rhsTerm));
-                } else if (old instanceof Unary) {
-                    Unary unary = (Unary) old;
-                    Unary.Operator op= unary.operator();
-                    Expr opnd= unary.expr();
-                    XTerm opndTerm= fTermMap.get(opnd);
-
-                    fTermMap.put(old, XTerms.makeAtom(getNameFor(op), opndTerm));
-                } else if (old instanceof Call) {
-                    Call call = (Call) old;
-                    
-                    throw new UnsupportedOperationException("Don't know how to create an XTerm for a method call.");
-                } else if (old instanceof SettableAssign) {
-                    SettableAssign sa= (SettableAssign) old;
-                    Expr array= sa.array();
-                    List<Expr> indices= sa.index();
-                    XTerm arrayTerm= fTermMap.get(array);
-                    XTerm indexTerm= fTermMap.get(indices.get(0));
-
-                    fTermMap.put(old, XTerms.makeArrayElement((XArray) arrayTerm, indexTerm));
-                } else if (old instanceof FieldAssign) {
-                    FieldAssign fa= (FieldAssign) old;
-                    FieldInstance fi= fa.fieldInstance();
-                    Receiver target= fa.target();
-
-                    fTermMap.put(old, XTerms.makeField((XVar) fTermMap.get(target), new XVarDefWrapper(fi.def())));
-                } else if (old instanceof LocalAssign) {
-                    LocalAssign la= (LocalAssign) old;
-                    Local l= la.local();
-
-                    fTermMap.put(old, XTerms.makeLocal(new XVarDefWrapper(l)));
-                } else if (old instanceof Special) {
-                    Special special = (Special) old;
-                    if (special.kind() == Special.SUPER) {
-                        fTermMap.put(old, XTerms.makeLocal(XTerms.makeName("super")));
-                    } else {
-                        fTermMap.put(old, XTerms.makeLocal(XTerms.makeName("this")));
-                    }
-                } else if (old instanceof Id) {
-                    // do nothing
-                    EffectsVisitor.fDiagStream.println("TermVisitor doing nothing for expr of type " + old.getClass().getCanonicalName());
-                } else {
-                    throw new UnsupportedOperationException("Unknown expression type");
-                }
-                return super.leave(parent, old, n, v);
-            }
-        }
-
-        private final Expr fExpr;
-        private final Map<Node,XTerm> fTermMap= new HashMap<Node,XTerm>();
-        private final TermVisitor fVisitor= new TermVisitor();
-
-        public TermCreator(Expr e) {
-            fExpr= e;
-            fExpr.visit(fVisitor);
-        }
-
-        public XTerm getTerm() {
-            return fTermMap.get(fExpr);
-        }
-    }
-
     private final Map<Node,Effect> fEffects= new HashMap<Node, Effect>();
 
     private final XConstraint fMethodContext;
 
     private final ValueMap fValueMap;
 
-    private static final PrintStream fDiagStream= X10RefactoringPlugin.getInstance().getConsoleStream();
+    private boolean fVerbose;
+
+    private PrintStream fDiagStream= System.out;
 
     public EffectsVisitor(ValueMap valueMap, X10MethodDecl method) throws XFailure {
         fValueMap = valueMap;
         fMethodContext= computeMethodContextConstraint(method);
+    }
+
+    public void setVerbose(PrintStream diagStream) {
+        fVerbose= true;
+        fDiagStream= diagStream;
     }
 
     private final XConstraint computeMethodContextConstraint(X10MethodDecl method) throws XFailure {
@@ -291,7 +91,7 @@ public class EffectsVisitor extends NodeVisitor {
         List<Formal> formals= method.formals();
         for(Formal formal: formals) {
             X10Formal xFormal= (X10Formal) formal;
-            X10LocalDef xDef= (X10LocalDef) xFormal.localDef();
+//          X10LocalDef xDef= (X10LocalDef) xFormal.localDef();
             X10Type xType= (X10Type) xFormal.type().type();
             XConstraint xc= X10TypeMixin.xclause(xType);
             if (xc != null) {
@@ -446,11 +246,15 @@ public class EffectsVisitor extends NodeVisitor {
 
         if (((X10Flags) li.flags()).isValue()) {
             Effect rhsEff= fEffects.get(rhs);
+//            Effect writeEff= Effects.makeEffect(Effects.FUN);
+//            writeEff.addWrite(Effects.makeLocalLocs(XTerms.makeLocal(new XVarDefWrapper(l))));
+//            result= followedBy(rhsEff, writeEff);
+            result= rhsEff;
+        } else {
+            Effect rhsEff= fEffects.get(rhs);
             Effect writeEff= Effects.makeEffect(Effects.FUN);
             writeEff.addWrite(Effects.makeLocalLocs(XTerms.makeLocal(new XVarDefWrapper(l))));
             result= followedBy(rhsEff, writeEff);
-        } else {
-            return Effects.makeBottomEffect();
         }
         return result;
     }
@@ -598,7 +402,9 @@ public class EffectsVisitor extends NodeVisitor {
                 result= followedBy(result, getMethodEffects(methodInstance));
             }
         }
-        fDiagStream.println("Effect of call to method " + methodInstance.container() + "." + methodInstance.signature() + ": " + result);
+        if (fVerbose) {
+            fDiagStream.println("Effect of call to method " + methodInstance.container() + "." + methodInstance.signature() + ": " + result);
+        }
         return result;
     }
 
@@ -660,15 +466,15 @@ public class EffectsVisitor extends NodeVisitor {
         Effect result= null;
         // aggregate effects of the individual statements.
         // prune out the effects on local vars whose scope is this block.
-        fDiagStream.println("Computing effect of block " + b);
+        if (fVerbose) fDiagStream.println("Computing effect of block " + b);
         List<LocalDecl> blockDecls= collectDecls(b);
         for(Stmt s: b.statements()) {
             Effect stmtEffect= fEffects.get(s);
-            fDiagStream.println("   statement = " + s + "; effect = " + stmtEffect);
+            if (fVerbose) fDiagStream.println("   statement = " + s + "; effect = " + stmtEffect);
             Effect filteredEffect= removeLocalVarsFromEffect(blockDecls, stmtEffect);
-            fDiagStream.println("             filtered effect = " + filteredEffect);
+            if (fVerbose) fDiagStream.println("             filtered effect = " + filteredEffect);
             result= followedBy(result, filteredEffect);
-            fDiagStream.println("   aggregate effect = " + result);
+            if (fVerbose) fDiagStream.println("   aggregate effect = " + result);
         }
         return result;
     }
