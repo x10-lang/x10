9f61c9b8a886d4154e5335aa89c8e6f6f5e42ab6
David Grove <groved@us.ibm.com>
2009-06-25 15:17:36 +0000

    bump version to 1.7.5
    git-svn-id: svn://svn.code.sf.net/p/x10/code@10542 a326200e-df2c-42e0-81fa-597e909af41d

--- tags/SF_RELEASE_1_7_5/x10.cppbackend.17/src/polyglot/ext/x10cpp/Version.java
+++ tags/SF_RELEASE_1_7_5/x10.cppbackend.17/src/polyglot/ext/x10cpp/Version.java
@@ -14,5 +14,5 @@ public class Version extends polyglot.main.Version {
 
 	public int major() { return 1; }
 	public int minor() { return 7; }
-	public int patch_level() { return 4; }
+	public int patch_level() { return 5; }
 }
