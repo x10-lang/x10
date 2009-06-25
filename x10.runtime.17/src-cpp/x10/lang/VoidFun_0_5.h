f2ccdc45dd5054e05735fc511b8a963492466d62
Igor Peshansky <igorp@us.ibm.com>
2009-06-25 03:56:16 +0000

    Apply r10505 to the x10-1.7.5 branch
    git-svn-id: svn://svn.code.sf.net/p/x10/code/branches/x10-1.7.5@10516 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.runtime.17/src-cpp/x10/lang/VoidFun_0_5.h
+++ x10.runtime.17/src-cpp/x10/lang/VoidFun_0_5.h
@@ -27,7 +27,7 @@ namespace x10 {
         };
 
         template<class P1, class P2, class P3, class P4, class P5>
-            const x10aux::RuntimeType*::_initRTT() {
+            const x10aux::RuntimeType* VoidFun_0_5<P1,P2,P3,P4,P5>::_initRTT() {
             return x10::lang::_initRTTHelper_VoidFun_0_5(&rtt, x10aux::getRTT<P1>(), x10aux::getRTT<P2>(), 
                                                          x10aux::getRTT<P3>(), x10aux::getRTT<P4>(), 
                                                          x10aux::getRTT<P5>());
