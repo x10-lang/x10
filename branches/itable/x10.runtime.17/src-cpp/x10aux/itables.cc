168121b15f46700b1ca1c8cfc49fa49cfaccb01e
David Grove <groved@us.ibm.com>
2009-07-14 14:39:18 +0000

    tracing debug messages for itable implementation
    git-svn-id: svn://svn.code.sf.net/p/x10/code@10706 a326200e-df2c-42e0-81fa-597e909af41d

--- branches/itable/x10.runtime.17/src-cpp/x10aux/itables.cc
+++ branches/itable/x10.runtime.17/src-cpp/x10aux/itables.cc
@@ -4,13 +4,30 @@
 using namespace x10aux;
 using namespace x10::lang;
 
+#define TRACE_ITABLES 1
+
 void*
 x10aux::findITable(ref<Object> obj, const RuntimeType **id) {
+#if TRACE_ITABLES
+    printf("Begin ITable search on %p for interface %p\n", obj.get(), id);
+    printf("\tObject RTT is %s\n", obj->_type()->name());
+    printf("\tInterface RTT is %s\n", (*id)->name());
+#endif
     itable_entry* itables = obj->_getITables();
-
+#if TRACE_ITABLES
+    printf("\tFound itables array %p\n", itables);
+#endif    
+    
     int i = 0;
-    do { 
+    do {
+#if TRACE_ITABLES
+        printf("\tChecking %d:  %p\n", i, itables[i].id);
+        printf("\t\tRTT of candidate %s\n", (*(itables[i].id))->name());
+#endif        
         if (itables[i].id == id) {
+#if TRACE_ITABLES
+            printf("\tMatch: Returning %p\n", itables[i].itable);
+#endif            
             return itables[i].itable;
         }
     } while (itables[++i].id != 0);
