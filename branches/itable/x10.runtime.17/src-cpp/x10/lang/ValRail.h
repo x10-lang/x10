276056ca7ad87c558ac959057d6442d716e851fe
David Grove <groved@us.ibm.com>
2009-07-22 18:22:08 +0000

    itable dispatch optimization:   makeFindITable be an inline function that takes the iTables for the object as a parameter (avoids circular dependency on Object.h). This tweak gets itable dispatch for closure application to be 1.7x the cost of the multiple inheritance passed approach.  Two further optimizations of interface dispatch in loops (a) lifting the call to getITable() out of the loop to gets 1.3x and (b) lifting the whole itable search out (the call to findITable) gets to .85x (ie, 15% faster than the original multiple dispatch code).  Both of these optimizations were done by hand for the experiment in the C++ code; doing them automatically is left for future work (see JIRA).
    git-svn-id: svn://svn.code.sf.net/p/x10/code@10844 a326200e-df2c-42e0-81fa-597e909af41d

--- branches/itable/x10.runtime.17/src-cpp/x10/lang/ValRail.h
+++ branches/itable/x10.runtime.17/src-cpp/x10/lang/ValRail.h
@@ -149,8 +149,8 @@ namespace x10 {
         template <class T> x10aux::ref<ValRail<T> > ValRail<T>::make(x10_int length,
                                                                      x10aux::ref<Fun_0_1<x10_int,T> > init ) {
             x10aux::ref<ValRail<T> > rail = x10aux::alloc_rail<T,ValRail<T> >(length);
-            typename Fun_0_1<x10_int,T>::template itable<x10::lang::Object> *it = x10aux::findITable<Fun_0_1<x10_int,T> >(init);
             x10aux::ref<x10::lang::Object> initAsObj = init;
+            typename Fun_0_1<x10_int,T>::template itable<x10::lang::Object> *it = x10aux::findITable<Fun_0_1<x10_int,T> >(initAsObj->_getITables());
             for (x10_int i=0 ; i<length ; ++i) {
                 (*rail)[i] = (initAsObj.get()->*(it->apply))(i);
             }
