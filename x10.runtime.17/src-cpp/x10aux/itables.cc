01b547901fea6671104337f5f566a42ab7123861
David Grove <groved@us.ibm.com>
2009-07-25 01:16:39 +0000

    Get the RTT of the interface by calling x10aux::getRTT<I> when building the itable to cause initRTT to get called.  Partially fixes uninitialized name problem.
    Store the RTT of the class itself in the itable field of the sentinel
    itable_entry at the end of the itable to enable better debug message
    from x10aux::reportITableLookupFailure
    
    There are still problems with the names not being fully initialized,
    which I suspect means that there are still code paths that are not 
    going through getRTT properly, thus causing some RTT objects to be 
    uninitialized.  This needs to be fixed.....
    
    
    
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@10874 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.runtime.17/src-cpp/x10aux/itables.cc
+++ x10.runtime.17/src-cpp/x10aux/itables.cc
@@ -9,9 +9,11 @@ void x10aux::reportITableLookupFailure(itable_entry* itables, RuntimeType* targe
     fprintf(stderr, "\nITable lookup failure!!\n");
     fprintf(stderr, "\tTarget interface was %s\n", targetInterface->name());
     fprintf(stderr, "\tInterfaces actually implemented by receiver\n");
-    for (int i=0; itables[i].id != 0; i++) {
+    int i = 0;
+    for (; itables[i].id != 0; i++) {
         fprintf(stderr, "\t\t%s\n", itables[i].id->name());
     }
+    fprintf(stderr, "\tReceiver object was %s\n", ((RuntimeType*)(itables[i].itable))->name());
     fprintf(stderr, "\n");
     fflush(stderr);
 }
