1d1dacd3b3f95be0ec04d069578d65273ef2e33d
Vijay Saraswat <vsaraswa@us.ibm.com>
2009-08-05 11:50:20 +0000

    Further cleanup for X10ClassDecl_c.addSyntheticMethod
    git-svn-id: svn://svn.code.sf.net/p/x10/code@10916 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.compiler.p3/src/polyglot/ext/x10/ast/X10ClassDecl.java
+++ trunk/x10.compiler.p3/src/polyglot/ext/x10/ast/X10ClassDecl.java
@@ -14,8 +14,10 @@ import polyglot.ast.Block;
 import polyglot.ast.ClassDecl;
 import polyglot.ast.Formal;
 import polyglot.ast.Id;
+import polyglot.ast.MethodDecl;
 import polyglot.ast.TypeNode;
 import polyglot.ext.x10.types.TypeDef;
+import polyglot.ext.x10.types.X10TypeSystem;
 import polyglot.types.Flags;
 import polyglot.types.LocalDef;
 import polyglot.types.Name;
@@ -33,5 +35,28 @@ public interface X10ClassDecl extends ClassDecl {
 	List<PropertyDecl> properties();
 	X10ClassDecl properties(List<PropertyDecl> ps);
 	
+	  /**
+     * Create a synthetic MethodDecl from the given data and return
+     * a new Class with this MethodDecl added in. No duplicate method
+     * checks will be performed. It is up to the user to make sure
+     * the constructed method will not duplicate an existing method.
+     * 
+     * Should be called after the class has been typechecked.
+     * 
+     * @param flags -- The flags for the method
+     * @param name -- The name of the method
+     * @param fmls -- A list of LocalDefs specifying the parameters to the method.
+     * @param returnType -- The return type of this method.
+     * @param trow  -- The types of throwables from the method.
+     * @param block -- The body of the method
+     * @param xnf -- The X10NodeFactory to be used to create new AST nodes.
+     * @param xts  -- The X10TypeSystem object to be used to create new types.
+     * @return  this, with the method added.
+     * 
+     * TODO: Ensure that type parameters and a guard can be supplied as well.
+     */
 	
+	X10ClassDecl_c addSyntheticMethod(Flags flags, Name name, List<LocalDef> formals, 
+    		Type returnType, List<Type> throwTypes, Block block,
+    		X10NodeFactory xnf, X10TypeSystem xts);
 }
