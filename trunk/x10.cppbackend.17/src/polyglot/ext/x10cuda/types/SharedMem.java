4ac0ea909e6154444eeaa10d08931e4f62bd21bb
David Grove <groved@us.ibm.com>
2009-08-13 01:42:21 +0000

    fix build break with dummy class.  My guess is that a svn add was missed in r10962....
    git-svn-id: svn://svn.code.sf.net/p/x10/code@10963 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ trunk/x10.cppbackend.17/src/polyglot/ext/x10cuda/types/SharedMem.java
@@ -0,0 +1,19 @@
+package polyglot.ext.x10cuda.types;
+
+import polyglot.ast.Expr;
+import polyglot.ast.LocalDecl;
+import polyglot.types.Name;
+
+public class SharedMem {
+
+	public void addRail(LocalDecl ld, Expr num_elements, Expr rail_init_closure) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public boolean has(Name ln) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
