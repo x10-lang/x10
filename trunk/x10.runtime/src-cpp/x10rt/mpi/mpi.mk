594965ead0cbcb07a537f5246199667c8f0c64ff
David Cunningham <dcunnin@us.ibm.com>
2009-09-10 22:21:24 +0000

    Build properties
    git-svn-id: svn://svn.code.sf.net/p/x10/code@11193 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.runtime/src-cpp/x10rt/mpi/mpi.mk
+++ trunk/x10.runtime/src-cpp/x10rt/mpi/mpi.mk
@@ -2,7 +2,7 @@ TESTS += $(patsubst test/%,test/%.mpi,$(BASE_TESTS))
 
 LIBS += lib/libx10rt_mpi.a
 
-PROPERTIES += etc/libx10rt_mpi.properties
+PROPERTIES += etc/x10rt_mpi.properties
 
 %.mpi: %.cc lib/libx10rt_mpi.a
 	${MPICXX} ${CXXFLAGS} $< -o $@ ${LDFLAGS} -lx10rt_mpi
@@ -14,5 +14,10 @@ lib/libx10rt_mpi.a: mpi/x10rt_mpi.o
 	${AR} rcu $@ $^
 
 etc/x10rt_mpi.properties:
+	echo "CXX=$(MPICXX)" > $@
+	echo "CXXFLAGS=" >> $@
+	echo "LDFLAGS=-lx10rt_mpi" >> $@
+
+.PRECIOUS: etc/x10rt_mpi.properties
 
 # vim: ts=8:sw=8:noet
