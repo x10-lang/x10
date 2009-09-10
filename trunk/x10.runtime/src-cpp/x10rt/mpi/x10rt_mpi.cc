a0b339082b94f54cf8cf1e65912aab89f863cdd1
David Cunningham <dcunnin@us.ibm.com>
2009-09-10 17:48:47 +0000

    Remove 'quitting' flag hack
    git-svn-id: svn://svn.code.sf.net/p/x10/code@11184 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.runtime/src-cpp/x10rt/mpi/x10rt_mpi.cc
+++ trunk/x10.runtime/src-cpp/x10rt/mpi/x10rt_mpi.cc
@@ -756,11 +756,8 @@ void check_pending(x10rt_req_queue * q)
     }
 }
 
-bool quitting = false;
-
 void x10rt_probe (void)
 {
-    if (quitting) return;
     int arrived = 0;
     MPI_Status msg_status;
 
@@ -804,7 +801,6 @@ void x10rt_probe (void)
 
 void x10rt_finalize (void)
 {
-    quitting = true;
     if(MPI_SUCCESS != MPI_Barrier(MPI_COMM_WORLD)) {
         fprintf(stderr, "[%s:%d] Error in MPI_Barrier\n", __FILE__, __LINE__);
         exit(EXIT_FAILURE);
