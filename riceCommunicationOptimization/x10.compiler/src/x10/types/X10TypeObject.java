29bc4146e88f9785e8d3c624c0455ff7fee25105
David Grove <groved@us.ibm.com>
2009-09-23 21:53:42 +0000

    Create branch for use by Rice
    git-svn-id: svn://svn.code.sf.net/p/x10/code/branches@11291 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ riceCommunicationOptimization/x10.compiler/src/x10/types/X10TypeObject.java
@@ -0,0 +1,18 @@
+/*
+ *
+ * (C) Copyright IBM Corporation 2006-2008.
+ *
+ *  This file is part of X10 Language.
+ *
+ */
+
+package x10.types;
+
+import java.util.List;
+
+import polyglot.types.Type;
+import polyglot.types.TypeObject;
+
+public interface X10TypeObject extends TypeObject {
+	
+}
---
+++ riceCommunicationOptimization/x10.dist/samples/HelloWorld.x10
@@ -0,0 +1,14 @@
+import x10.io.Console;
+
+/**
+ * The classic hello world program, shows how to output to the console.
+ * @author ??
+ * @author vj 
+ */
+class HelloWorld {
+  public static def main(args:Rail[String]):void {
+     Console.OUT.println("Hello World");
+  }
+}
+
+
