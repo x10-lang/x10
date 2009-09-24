575dec784cf3b157422615758f10bb5d30cd9ec8
Beth Tibbitts <btibbitts@us.ibm.com>
2009-09-24 13:50:21 +0000

    branch to 1.7 from trunk
    git-svn-id: svn://svn.code.sf.net/p/x10/code@11292 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ x10dt/branches/x10-1.7/org.eclipse.imp.x10dt.ui.launch.core/src/org/eclipse/imp/x10dt/ui/launch/core/preferences/X10PlatformConfiguration.java
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2009 IBM Corporation.                                         *
+ * All rights reserved. This program and the accompanying materials            *
+ * are made available under the terms of the Eclipse Public License v1.0       *
+ * which accompanies this distribution, and is available at                    *
+ * http://www.eclipse.org/legal/epl-v10.html                                   *
+ *******************************************************************************/
+package org.eclipse.imp.x10dt.ui.launch.core.preferences;
+
+/**
+ * Defines the information about an X10 platform for compilation and (if needed) linking of X10 generated files.
+ * 
+ * @author egeay
+ */
+public final class X10PlatformConfiguration {
+  
+  // --- Public services
+
+}
