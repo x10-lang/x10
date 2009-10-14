498acf5ffcebfdbf9c8f4ad59fc5b47e04b9d955
Robert Fuhrer <rfuhrer@us.ibm.com>
2009-10-14 15:25:43 +0000

    Added a number of API calls to produce the set of memory effect pairs that interfere with one another.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/branches/x10-1.7@11463 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ x10.effects/src/x10/effects/constraints/Pair.java
@@ -0,0 +1,11 @@
+package x10.effects.constraints;
+
+public class Pair<T1,T2> {
+    public final T1 fst;
+    public final T2 snd;
+
+    public Pair(T1 v1, T2 v2) {
+        fst= v1;
+        snd= v2;
+    }
+}
