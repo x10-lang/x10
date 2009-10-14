58cc446d59ed970abc7357f7c14485abe9f398b9
Sayantan Sur <user@domain>
2009-10-14 17:54:19 +0000

    x10rt/Makefile should inherit these env vars from upper-level make
    git-svn-id: svn://svn.code.sf.net/p/x10/code@11467 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.runtime/src-cpp/x10rt/mpi/mpi.mk
+++ trunk/x10.runtime/src-cpp/x10rt/mpi/mpi.mk
@@ -1,17 +1,19 @@
 TESTS += $(patsubst test/%,test/%.mpi,$(BASE_TESTS))
 
+AR ?= ar
+
 LIBS += lib/libx10rt_mpi.a
 
 PROPERTIES += etc/x10rt_mpi.properties
 
 %.mpi: %.cc lib/libx10rt_mpi.a
-	${MPICXX} ${CXXFLAGS} $< -o $@ ${LDFLAGS} -lx10rt_mpi
+	${MPICXX} $(CXXFLAGS) $< -o $@ ${LDFLAGS} -lx10rt_mpi
 
 mpi/x10rt_mpi.o: mpi/x10rt_mpi.cc
-	${MPICXX} ${CXXFLAGS} -c $< -o $@
+	${MPICXX} $(CXXFLAGS) -c $< -o $@
 
 lib/libx10rt_mpi.a: mpi/x10rt_mpi.o
-	${AR} rcu $@ $^
+	${AR} $(ARFLAGS) $@ $^
 
 etc/x10rt_mpi.properties:
 	echo "CXX=$(MPICXX)" > $@
