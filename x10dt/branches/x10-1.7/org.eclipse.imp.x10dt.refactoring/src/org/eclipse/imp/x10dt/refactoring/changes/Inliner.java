de73814e3c877e1532d57f2626fbd81c77e06c37
Robert Fuhrer <rfuhrer@us.ibm.com>
2009-10-14 23:56:00 +0000

    Now handles return statements for void-producing methods. Now does some basic simplification using the actual method args.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@11474 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt/branches/x10-1.7/org.eclipse.imp.x10dt.refactoring/src/org/eclipse/imp/x10dt/refactoring/changes/Inliner.java
+++ x10dt/branches/x10-1.7/org.eclipse.imp.x10dt.refactoring/src/org/eclipse/imp/x10dt/refactoring/changes/Inliner.java
@@ -4,41 +4,139 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import polyglot.ast.Assign;
+import polyglot.ast.Binary;
 import polyglot.ast.Block;
 import polyglot.ast.Call;
 import polyglot.ast.CodeBlock;
 import polyglot.ast.Expr;
 import polyglot.ast.Formal;
+import polyglot.ast.Id;
+import polyglot.ast.If;
 import polyglot.ast.MethodDecl;
 import polyglot.ast.Node;
 import polyglot.ast.NodeFactory;
+import polyglot.ast.Return;
 import polyglot.ast.SourceFile;
+import polyglot.ast.Stmt;
 import polyglot.types.MethodInstance;
+import polyglot.types.Name;
 import polyglot.types.TypeSystem;
 import polyglot.types.VarDef;
 import polyglot.types.VarInstance;
+import polyglot.util.Position;
 import polyglot.visit.NodeVisitor;
 
 /**
  * Inlines a given method call.
  */
 public class Inliner {
+    /**
+     * Performs the necessary transformations on the inlined method body.
+     * Not yet prepared to handle value-producing methods in arbitrary expression contexts.
+     */
     private class InlinePerformer extends NodeVisitor {
         private final Block fBlock;
 
-        public InlinePerformer(Block block) {
+        private boolean fHasReturns;
+
+        private final Id fLabelID;
+
+        private final Expr fAssignTarget;
+
+        public InlinePerformer(Block block, Expr assignTarget) {
             fBlock= block;
+            fAssignTarget= assignTarget;
+            fLabelID= fNodeFactory.Id(fBlock.position(), Name.makeFresh("label"));
         }
 
         @Override
+        public NodeVisitor enter(Node n) {
+            return super.enter(n);
+        }
+        @Override
         public Node leave(Node parent, Node old, Node n, NodeVisitor v) {
-            // TODO Translate return stmts to assignments to the receptacle for the method return value
+            if (n instanceof Return) {
+                Return ret= (Return) n;
+                Expr expr= ret.expr();
+
+                if (expr != null) {
+                    // Translate return stmts to assignments to the receptacle for the method return value
+                    Assign ass= fNodeFactory.Assign(Position.COMPILER_GENERATED, fAssignTarget, Assign.ASSIGN, expr);
+                    return ass;
+                }
+                // Translate return stmts to break stmts to the appropriate label for the enclosing block
+                Stmt break_ = fNodeFactory.Break(Position.COMPILER_GENERATED, fLabelID);
+                fHasReturns= true;
+                return break_;
+            }
             return super.leave(parent, old, n, v);
         }
 
         public Node perform() {
-            Node result= fBlock.visit(this);
+            Stmt result= (Block) fBlock.visit(this);
+            if (fHasReturns) {
+                result= fNodeFactory.Labeled(Position.COMPILER_GENERATED, fLabelID, result);
+            }
             return result;
+        }
+    }
+
+    private class Simplifier extends NodeVisitor {
+        private Map<Expr,Object> fEvalResults= new HashMap<Expr, Object>();
+
+        @Override
+        public Node leave(Node parent, Node old, Node n, NodeVisitor v) {
+            if (n instanceof Binary) {
+                Binary b= (Binary) n;
+                Binary.Operator op= b.operator();
+                Expr left= b.left();
+                Expr right= b.right();
+                if (op == Binary.COND_AND || op == Binary.COND_OR) {
+                    if (fEvalResults.containsKey(left) && fEvalResults.containsKey(right)) {
+                        Boolean leftVal= (Boolean) fEvalResults.get(left);
+                        Boolean rightVal= (Boolean) fEvalResults.get(right);
+                        Boolean result= (op == Binary.COND_AND) ? leftVal && rightVal : leftVal || rightVal;
+
+                        fEvalResults.put(b, result);
+                    }
+                } else if (op == Binary.EQ || op == Binary.GE || op == Binary.GT || op == Binary.LE || op == Binary.LT || op == Binary.NE) {
+                    if (left.type().isInt() && right.type().isInt() && fEvalResults.containsKey(left) && fEvalResults.containsKey(right)) {
+                        Integer leftVal= (Integer) fEvalResults.get(left);
+                        Integer rightVal= (Integer) fEvalResults.get(right);
+                        boolean result= false;
+
+                        if (op == Binary.EQ) {
+                            result= leftVal == rightVal;
+                        } else if (op == Binary.GE) {
+                            result= leftVal >= rightVal;
+                        } else if (op == Binary.GT) {
+                            result= leftVal > rightVal;
+                        } else if (op == Binary.LE) {
+                            result= leftVal <= rightVal;
+                        } else if (op == Binary.LT) {
+                            result= leftVal < rightVal;
+                        } else if (op == Binary.NE) {
+                            result= leftVal != rightVal;
+                        }
+                        fEvalResults.put(b, result);
+                    }
+                }
+            } else if (n instanceof If) {
+                If ifStmt= (If) n;
+                Stmt thenStmt= ifStmt.consequent();
+                Stmt elseStmt= ifStmt.alternative();
+                Expr cond= ifStmt.cond();
+                if (fEvalResults.containsKey(cond)) {
+                    return (Boolean) fEvalResults.get(cond) ? thenStmt : elseStmt;
+                }
+            } else if (n instanceof Expr) {
+                Expr e= (Expr) n;
+                if (e.isConstant()) {
+                    fEvalResults.put(e, e.constantValue());
+                }
+            }
+            return super.leave(parent, old, n, v);
         }
     }
 
@@ -56,7 +154,7 @@ public class Inliner {
         fTypeSystem= ts;
     }
 
-    public Node perform() {
+    public Node perform(boolean simplify) {
         List<Expr> actuals= fCall.arguments();
         MethodInstance mi= fCall.methodInstance();
         MethodDecl md= findDecl(mi, fRoot);
@@ -69,21 +167,26 @@ public class Inliner {
         }
         List<Formal> formals= md.formals();
         Map<VarInstance<VarDef>,Node> subs= new HashMap<VarInstance<VarDef>, Node>(formals.size());
+
         for(int i=0; i < formals.size(); i++) {
             Formal formal= formals.get(i);
             Expr actual= actuals.get(i);
 
             subs.put((VarInstance) formal.localDef().asInstance(), actual);
         }
+        Expr assignTarget= null; // TODO Create a local var if needed
         SubstitutionPerformer subPerf= new SubstitutionPerformer(subs);
-
         Block blockSubbed= (Block) subPerf.perform(md.body(), fRoot);
+        InlinePerformer inPerf= new InlinePerformer(blockSubbed, mi.returnType().isVoid() ? null : assignTarget);
+        Node result= inPerf.perform();
 
-        InlinePerformer inPerf= new InlinePerformer(blockSubbed);
+        if (simplify) {
+            Simplifier simplifier= new Simplifier();
 
-        Node newBody= inPerf.perform();
+            result= result.visit(simplifier);
+        }
 
-        return newBody;
+        return result;
     }
 
     private MethodDecl findDecl(final MethodInstance mi, SourceFile root) {
