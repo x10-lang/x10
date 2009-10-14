4d0c0ad7fff1b029d2633480f9d24995a7b01273
Beth Tibbitts <btibbitts@us.ibm.com>
2009-10-14 21:18:16 +0000

    defaults for Mac platform
    git-svn-id: svn://svn.code.sf.net/p/x10/code@11470 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ x10dt/trunk/org.eclipse.imp.x10dt.ui.launch.core/src/org/eclipse/imp/x10dt/ui/launch/core/wizards/MacPlatform.java
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * Copyright (c) 2009 IBM Corporation.                                         *
+ * All rights reserved. This program and the accompanying materials            *
+ * are made available under the terms of the Eclipse Public License v1.0       *
+ * which accompanies this distribution, and is available at                    *
+ * http://www.eclipse.org/legal/epl-v10.html                                   *
+ *******************************************************************************/
+package org.eclipse.imp.x10dt.ui.launch.core.wizards;
+
+
+final class MacPlatform implements IDefaultX10Platform {
+  
+  // --- Interface methods implementation
+  
+  public String getArchiver() {
+    return "ar"; //$NON-NLS-1$
+  }
+
+  public String getArchivingOpts() {
+    return "r"; //$NON-NLS-1$
+  }
+
+  public String getCompiler() {
+    return "g++"; //$NON-NLS-1$
+  }
+
+  public String getCompilerOptions() {
+    return "-g -DTRANSPORT=sockets -Wno-long-long -Wno-unused-parameter -pthread  -msse2 -mfpmath=sse -DX10_USE_BDWGC "; //$NON-NLS-1$
+  }
+
+  public String getLinker() {
+    return getCompiler();
+  }
+
+  public String getLinkingLibraries() {
+    return "-lx10 -lgc  -lxlpgas_sockets -ldl -lm -lpthread "; //$NON-NLS-1$
+  }
+
+  public String getLinkingOptions() {
+    return "-g -DTRANSPORT=sockets -Wno-long-long -Wno-unused-parameter -msse2 -mfpmath=sse  -DX10_USE_BDWGC  "; //$NON-NLS-1$
+  }
+
+}
