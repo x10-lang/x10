2519338a705b04198bdc31e7cd55b79ca673f261
David Cunningham <dcunnin@us.ibm.com>
2009-10-17 14:52:03 +0000

    Add two new functions, implemented in pgas but not MPI / standalone
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@11511 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.runtime/src-cpp/x10rt_api.h
+++ x10.runtime/src-cpp/x10rt_api.h
@@ -25,6 +25,10 @@ void x10rt_send_get (x10rt_msg_params &, void *buf, unsigned long len);
 void *x10rt_put_realloc (void *old, size_t old_sz, size_t new_sz);
 void x10rt_send_put (x10rt_msg_params &, void *buf, unsigned long len);
 
+void x10rt_remote_xor (unsigned long place, unsigned long long addr, unsigned long long update);
+
+void x10rt_remote_op_fence (void);
+
 void x10rt_probe (void);
 
 void x10rt_finalize (void); 
