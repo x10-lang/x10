eeb70e45768f9d715bd696c0561de0f53d4bc012
Igor Peshansky <igorp@us.ibm.com>
2009-10-27 06:28:52 +0000

    Genericize. Better short-circuiting.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@11769 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.compiler/src/x10cpp/visit/X10SearchVisitor.java
+++ x10.compiler/src/x10cpp/visit/X10SearchVisitor.java
@@ -19,114 +19,114 @@ import polyglot.visit.NodeVisitor;
  * @author V. Krishna Nandivada
  * @author Igor Peshansky
  */
-public class X10SearchVisitor extends NodeVisitor {
+public class X10SearchVisitor<T extends Node> extends NodeVisitor {
 	public static boolean debug = false;
-	private ArrayList searchNames;
+	private ArrayList<Class<? extends T>> searchNames;
 	private boolean found = false;
-	private ArrayList results;
+	private ArrayList<T> results;
 	private final boolean oneMatch;
 
-	public X10SearchVisitor(Class z, boolean oneMatch) {
+	public X10SearchVisitor(Class<? extends T> z, boolean oneMatch) {
 		this.oneMatch = oneMatch;
 		reset(z);
 	}
 
-	public X10SearchVisitor(Class z, Class y, boolean oneMatch) {
+	public X10SearchVisitor(Class<? extends T> z, Class<? extends T> y, boolean oneMatch) {
 		this(z, oneMatch);
 		addNodeType(y);
 	}
 
-	public X10SearchVisitor(Class z, Class y, Class x, boolean oneMatch) {
+	public X10SearchVisitor(Class<? extends T> z, Class<? extends T> y, Class<? extends T> x, boolean oneMatch) {
 		this(z, y, oneMatch);
 		addNodeType(x);
 	}
 
-	public X10SearchVisitor(Class z, Class y, Class x, Class w, boolean oneMatch) {
+	public X10SearchVisitor(Class<? extends T> z, Class<? extends T> y, Class<? extends T> x, Class<? extends T> w, boolean oneMatch) {
 		this(z, y, x, oneMatch);
 		addNodeType(w);
 	}
 
-	public X10SearchVisitor(Class z, Class y, Class x, Class w, Class v, boolean oneMatch) {
+	public X10SearchVisitor(Class<? extends T> z, Class<? extends T> y, Class<? extends T> x, Class<? extends T> w, Class<? extends T> v, boolean oneMatch) {
 		this(z, y, x, w, oneMatch);
 		addNodeType(v);
 	}
 
-	public X10SearchVisitor(Class z, Class y, Class x, Class w, Class v, Class u, boolean oneMatch) {
+	public X10SearchVisitor(Class<? extends T> z, Class<? extends T> y, Class<? extends T> x, Class<? extends T> w, Class<? extends T> v, Class<? extends T> u, boolean oneMatch) {
 		this(z, y, x, w, v, oneMatch);
 		addNodeType(u);
 	}
 
-	public X10SearchVisitor(Class z, Class y, Class x, Class w, Class v, Class u, Class t, boolean oneMatch) {
+	public X10SearchVisitor(Class<? extends T> z, Class<? extends T> y, Class<? extends T> x, Class<? extends T> w, Class<? extends T> v, Class<? extends T> u, Class<? extends T> t, boolean oneMatch) {
 		this(z, y, x, w, v, u, oneMatch);
 		addNodeType(t);
 	}
 
-	public X10SearchVisitor(Class z) {
+	public X10SearchVisitor(Class<? extends T> z) {
 		this(z, false);
 	}
 
-	public X10SearchVisitor(Class z, Class y) {
+	public X10SearchVisitor(Class<? extends T> z, Class<? extends T> y) {
 		this(z, y, false);
 	}
 
-	public X10SearchVisitor(Class z, Class y, Class x) {
+	public X10SearchVisitor(Class<? extends T> z, Class<? extends T> y, Class<? extends T> x) {
 		this(z, y, x, false);
 	}
 
-	public X10SearchVisitor(Class z, Class y, Class x, Class w) {
+	public X10SearchVisitor(Class<? extends T> z, Class<? extends T> y, Class<? extends T> x, Class<? extends T> w) {
 		this(z, y, x, w, false);
 	}
 
-	public X10SearchVisitor(Class z, Class y, Class x, Class w, Class v) {
+	public X10SearchVisitor(Class<? extends T> z, Class<? extends T> y, Class<? extends T> x, Class<? extends T> w, Class<? extends T> v) {
 		this(z, y, x, w, v, false);
 	}
 
-	public X10SearchVisitor(Class z, Class y, Class x, Class w, Class v, Class u) {
+	public X10SearchVisitor(Class<? extends T> z, Class<? extends T> y, Class<? extends T> x, Class<? extends T> w, Class<? extends T> v, Class<? extends T> u) {
 		this(z, y, x, w, v, u, false);
 	}
 
-	public X10SearchVisitor(Class z, Class y, Class x, Class w, Class v, Class u, Class t) {
+	public X10SearchVisitor(Class<? extends T> z, Class<? extends T> y, Class<? extends T> x, Class<? extends T> w, Class<? extends T> v, Class<? extends T> u, Class<? extends T> t) {
 		this(z, y, x, w, v, u, t, false);
 	}
 
-	public void reset(Class x) {
-		searchNames = new ArrayList();
+	public void reset(Class<? extends T> x) {
+		searchNames = new ArrayList<Class<? extends T>>();
 		searchNames.add(x);
 		found = false;
-		results = new ArrayList();
+		results = new ArrayList<T>();
 	}
 
-	public void reset(Class z, Class y) {
+	public void reset(Class<? extends T> z, Class<? extends T> y) {
 		reset(z);
 		addNodeType(y);
 	}
 
-	public void reset(Class z, Class y, Class x) {
+	public void reset(Class<? extends T> z, Class<? extends T> y, Class<? extends T> x) {
 		reset(z, y);
 		addNodeType(x);
 	}
 
-	public void reset(Class z, Class y, Class x, Class w) {
+	public void reset(Class<? extends T> z, Class<? extends T> y, Class<? extends T> x, Class<? extends T> w) {
 		reset(z, y, x);
 		addNodeType(w);
 	}
 
-	public void reset(Class z, Class y, Class x, Class w, Class v) {
+	public void reset(Class<? extends T> z, Class<? extends T> y, Class<? extends T> x, Class<? extends T> w, Class<? extends T> v) {
 		reset(z, y, x, w);
 		addNodeType(v);
 	}
 
-	public void reset(Class z, Class y, Class x, Class w, Class v, Class u) {
+	public void reset(Class<? extends T> z, Class<? extends T> y, Class<? extends T> x, Class<? extends T> w, Class<? extends T> v, Class<? extends T> u) {
 		reset(z, y, x, w, v);
 		addNodeType(u);
 	}
 
-	public void reset(Class z, Class y, Class x, Class w, Class v, Class u, Class t) {
+	public void reset(Class<? extends T> z, Class<? extends T> y, Class<? extends T> x, Class<? extends T> w, Class<? extends T> v, Class<? extends T> u, Class<? extends T> t) {
 		reset(z, y, x, w, v, u);
 		addNodeType(t);
 	}
 
-	public void addNodeType(Class x) {
+	public void addNodeType(Class<? extends T> x) {
 		searchNames.add(x);
 	}
 
@@ -140,37 +140,37 @@ public class X10SearchVisitor extends NodeVisitor {
 		return (Node) results.get(0);
 	}
 
-	public ArrayList getMatches() {
+	public ArrayList<T> getMatches() {
 		assert(found);
 		return results;
 	}
 
-	private boolean isMatch(Class type) {
-		for (int i = 0; i < searchNames.size(); i++)
-			if (((Class) searchNames.get(i)).isAssignableFrom(type))
-				return true;
+	private boolean isMatch(Class<? extends Node> type) {
+		for (Class<? extends T> c : searchNames)
+		    if (c.isAssignableFrom(type))
+		        return true;
 		return false;
 	}
 
-	private void addMatch(Node n) {
+	private void addMatch(T n) {
 		found = true;
 		results.add(n);
 	}
 
 	private void checkNode(Node n) {
-		Class type = n.getClass();
+		Class<? extends Node> type = n.getClass();
 		if (debug) {
 			System.out.println("String = " + type);
 			System.out.println("Context = " + searchNames);
 		}
-		if (oneMatch && found)
-			return;
 		if (isMatch(type))
-			addMatch(n);
+			addMatch((T)n);
 		return;
 	}
 
 	public Node override(Node parent, Node child) {
+		if (oneMatch && found)
+		    return child;
 		checkNode(child);
 		return null;
 	}
