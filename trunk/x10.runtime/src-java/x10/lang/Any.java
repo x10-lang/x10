10a2c95d694ae81a57bdf1bfd254407dff156bcd
David Grove <groved@us.ibm.com>
2009-10-27 20:44:40 +0000

    forgot to do svn add...
    git-svn-id: svn://svn.code.sf.net/p/x10/code@11785 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ trunk/x10.runtime/src-java/x10/lang/Any.java
@@ -0,0 +1,14 @@
+/*
+ *
+ * (C) Copyright IBM Corporation 2006-2008.
+ *
+ *  This file is part of X10 Language.
+ *
+ */
+
+package x10.lang;
+
+// Base class of all X10 types
+public interface Any {
+
+}
