1877430cbaa5f4f605db9c7607ea718aa5daf7c0
David Grove <groved@us.ibm.com>
2009-11-05 15:57:37 +0000

    fix x10c++ version number.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@11935 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.compiler/src/x10cpp/Version.java
+++ x10.compiler/src/x10cpp/Version.java
@@ -12,7 +12,7 @@ package x10cpp;
 public class Version extends polyglot.main.Version {
 	public String name() { return "x10c++"; }
 
-	public int major() { return 1; }
-	public int minor() { return 8; }
+	public int major() { return 2; }
+	public int minor() { return 0; }
 	public int patch_level() { return 0; }
 }
