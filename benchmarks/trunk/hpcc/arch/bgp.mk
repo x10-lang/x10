c5baaf925cd1059ec7744d27446c16464c1f1289
Sayantan Sur <user@domain>
2009-11-06 20:43:20 +0000

    blas libs
    git-svn-id: svn://svn.code.sf.net/p/x10/code@11953 a326200e-df2c-42e0-81fa-597e909af41d

--- benchmarks/trunk/hpcc/arch/bgp.mk
+++ benchmarks/trunk/hpcc/arch/bgp.mk
@@ -16,19 +16,11 @@ export POSTCOMPILE_NATIVE_LIB_FLAGS ?= -O3 -DNDEBUG
 
 export POSTCOMPILE_INCLUDEOPT ?= -qinclude=
 
-export BLAS_LIB		= -lessl
+export BLAS_LIB		= -L/opt/ibmmath/lib -lesslbg -lm
 export EXTRA_LIBS	= 
 
 export USE_MEDIUM_PAGES ?= 0  # TODO: ask George
 export X10TRANSPORT = bgp     # Override default
-
-# AIX with G++
-#
-#export POSTCOMPILE_CXX		?= mpCC
-#export POSTCOMPILE_CXXFLAGS 	?= -w -q64 -DTRANSPORT=${X10TRANSPORT}
-
-#export POSTCOMPILE_OPTFLAGS	?= -O3 -qarch=pwr5 -qtune=pwr5 -qhot -qinline -qrtti=all -DNDEBUG -DNO_CHECKS
-#export POSTCOMPILE_DBGFLAGS	?= -g
 
 export POSTCOMPILE_AR		?= ar
 export POSTCOMPILE_ARFLAGS	?= -X64 -rv
