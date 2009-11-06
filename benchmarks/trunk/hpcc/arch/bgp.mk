80f863e716b83ab47c89317e2cc5b8e8d52b53e2
Sayantan Sur <user@domain>
2009-11-06 19:30:52 +0000

    add bgp.mk file to cross-compile for BG
    git-svn-id: svn://svn.code.sf.net/p/x10/code@11949 a326200e-df2c-42e0-81fa-597e909af41d

--- benchmarks/trunk/hpcc/arch/bgp.mk
+++ benchmarks/trunk/hpcc/arch/bgp.mk
@@ -2,12 +2,12 @@
 #
 
 
-# AIX with XLC
+# BGP specific settings
 #
-export POSTCOMPILE_CXX		?= mpCC_r
+export POSTCOMPILE_CXX		?= /bgsys/drivers/ppcfloor/gnu-linux/bin/powerpc-bgp-linux-g++
 export POSTCOMPILE_CXXFLAGS 	?=
-export POSTCOMPILE_LDFLAGS	?= 
-export POSTCOMPILE_LIBS		?= 
+export POSTCOMPILE_LDFLAGS	?= -L/bgsys/drivers/ppcfloor/comm/lib -L/bgsys/drivers/ppcfloor/runtime/SPI
+export POSTCOMPILE_LIBS		?= -ldcmf.cnk -ldcmfcoll.cnk -lSPI.cna -lpthread -lrt -lm
 
 export POSTCOMPILE_OPTFLAGS	?= -DNO_CHECKS
 export POSTCOMPILE_DBGFLAGS	?= -g
@@ -16,10 +16,11 @@ export POSTCOMPILE_NATIVE_LIB_FLAGS ?= -O3 -q64 -qarch=pwr5 -qtune=pwr5 -qhot -q
 
 export POSTCOMPILE_INCLUDEOPT ?= -qinclude=
 
-export BLAS_LIB		?= -lessl
-export EXTRA_LIBS	?= 
+export BLAS_LIB		= -lessl
+export EXTRA_LIBS	= 
 
-export USE_MEDIUM_PAGES ?= 1
+export USE_MEDIUM_PAGES ?= 0  # TODO: ask George
+export X10TRANSPORT = bgp     # Override default
 
 # AIX with G++
 #
