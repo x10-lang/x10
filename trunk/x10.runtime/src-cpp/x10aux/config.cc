e201e33e747f12ea60113bb3061f6b8f0b26ddea
David Cunningham <dcunnin@us.ibm.com>
2009-11-11 22:14:51 +0000

    add X10_TRACE_NET, which is an easier way of asking for both X10RT and SER Fix a deadlock in init code Do not use [[]] in makefile -- anek doesn't like it
    git-svn-id: svn://svn.code.sf.net/p/x10/code@12000 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.runtime/src-cpp/x10aux/config.cc
+++ trunk/x10.runtime/src-cpp/x10aux/config.cc
@@ -15,7 +15,7 @@ void x10aux::init_config_bools (void)
     use_ansi_colors_ = NULL==getenv("X10_NO_ANSI_COLORS");
     trace_alloc_ = getenv("X10_TRACE_ALLOC") || getenv("X10_TRACE_ALL");
     trace_init_ = getenv("X10_TRACE_INIT") || getenv("X10_TRACE_ALL");
-    trace_x10rt_ = getenv("X10_TRACE_X10RT") || getenv("X10_TRACE_ALL");
-    trace_ser_ = getenv("X10_TRACE_SER") || getenv("X10_TRACE_ALL");
+    trace_x10rt_ = getenv("X10_TRACE_X10RT") || getenv("X10_TRACE_NET") || getenv("X10_TRACE_ALL");
+    trace_ser_ = getenv("X10_TRACE_SER") || getenv("X10_TRACE_NET") || getenv("X10_TRACE_ALL");
     init_config_bools_done = true;
 }
