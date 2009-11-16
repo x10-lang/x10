2d42cc63011f47bac8a4acefa4bcd1369cdc5d08
Robert Fuhrer <rfuhrer@us.ibm.com>
2009-11-16 20:08:58 +0000

    Added UI controls to specify how to handle any "tail end" of the loop that isn't handled by an integral # of iterations of the unrolled loop.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@12084 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt/branches/x10-1.7/org.eclipse.imp.x10dt.refactoring/src/org/eclipse/imp/x10dt/refactoring/wizards/LoopUnrollInputPage.java
+++ x10dt/branches/x10-1.7/org.eclipse.imp.x10dt.refactoring/src/org/eclipse/imp/x10dt/refactoring/wizards/LoopUnrollInputPage.java
@@ -16,9 +16,13 @@ import org.eclipse.ltk.ui.refactoring.UserInputWizardPage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Text;
@@ -39,7 +43,7 @@ public class LoopUnrollInputPage extends UserInputWizardPage implements Listener
         Composite result = new Composite(parent, SWT.NONE);
         GridLayout layout = new GridLayout();
 
-        layout.numColumns = 4;
+        layout.numColumns = 1;
         result.setLayout(layout);
 
         final Label numTimesLabel= new Label(result, SWT.NONE);
@@ -52,11 +56,43 @@ public class LoopUnrollInputPage extends UserInputWizardPage implements Listener
                 if (newText.length() > 0) {
                     try {
                         int factor= Integer.parseInt(newText);
-                        getLoopUnrollRefactoring().setUnrollFactor(factor);
+                        // Apparently, in the literature, unrolling "twice" gives an unroll factor of 3,
+                        // i.e., a loop w/ 3 copies of the body.
+                        getLoopUnrollRefactoring().setUnrollFactor(factor+1);
                     } catch (NumberFormatException ex) {
                         LoopUnrollInputPage.this.setErrorMessage("Unroll count must be an integral number");
                     }
                 }
+            }
+        });
+
+        Group rbGroup= new Group(result, SWT.SHADOW_ETCHED_IN);
+        rbGroup.setText("Handling loops with non-integral # of unrollings:");
+        final Button addAssertRB= new Button(rbGroup, SWT.RADIO);
+        final Button genTailLoopRB= new Button(rbGroup, SWT.RADIO);
+        GridLayout rbgLayout= new GridLayout();
+        rbgLayout.numColumns= 1;
+        rbGroup.setLayout(rbgLayout);
+
+        addAssertRB.setText("Add assertion that loop bounds is multiple of unroll factor");
+        genTailLoopRB.setText("Generate additional loop to handle iterations beyond unroll boundary");
+
+        addAssertRB.setSelection(false);
+        genTailLoopRB.setSelection(true);
+        addAssertRB.addSelectionListener(new SelectionListener() {
+            public void widgetDefaultSelected(SelectionEvent e) { }
+
+            public void widgetSelected(SelectionEvent e) {
+                genTailLoopRB.setSelection(!addAssertRB.getSelection());
+                getLoopUnrollRefactoring().setAddAssertion();
+            }
+        });
+        genTailLoopRB.addSelectionListener(new SelectionListener() {
+            public void widgetDefaultSelected(SelectionEvent e) { }
+
+            public void widgetSelected(SelectionEvent e) {
+                addAssertRB.setSelection(!genTailLoopRB.getSelection());
+                getLoopUnrollRefactoring().setGenerateTailLoop();
             }
         });
         setControl(result);
