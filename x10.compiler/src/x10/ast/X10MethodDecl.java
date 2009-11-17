3547f9b0a466e66c23cf22153f22a03a1a6767ff
Robert Fuhrer <rfuhrer@us.ibm.com>
2009-11-17 23:35:24 +0000

    Added new interface X10ProcedureDecl, which gives access to the "method guard", and added it as a super-interface for X10MethodDecl and X10ConstructorDecl.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/branches/safety-analysis@12110 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.compiler/src/x10/ast/X10MethodDecl.java
+++ x10.compiler/src/x10/ast/X10MethodDecl.java
@@ -14,11 +14,9 @@ import polyglot.ast.Expr;
 import polyglot.ast.MethodDecl;
 import polyglot.ast.TypeNode;
 
-public interface X10MethodDecl extends MethodDecl {
+public interface X10MethodDecl extends X10ProcedureDecl, MethodDecl {
 	// The method's guard.
-	DepParameterExpr guard();
 	X10MethodDecl guard(DepParameterExpr e);
 	
-	List<TypeParamNode> typeParameters();
 	X10MethodDecl typeParameters(List<TypeParamNode> typeParams);
 }
