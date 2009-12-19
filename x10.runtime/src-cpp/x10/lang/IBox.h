13e08c845e5069e856b67fc0794971868c2f2110
David Grove <groved@us.ibm.com>
2009-12-19 04:18:40 +0000

    WIP on C++ codegen for XTENLANG-754
    C++ backend now supports boxing a struct "up" to an interface
    type and doing instanceof/checkcast operations on the boxed interface.
    
    Invocation of interface methods on the "boxed struct" will not work,
    yet as we need to augment to codegen for struct itables to generate
    an additional itable thunk class where the receiver parameter is 
    defined to be an IBox<S> instead of a S&. I'll work on this over the weekend
    (don't think it's too hard, but I may not finish it before calling it a night
    tonight).
    
    After interface method invocation on boxed structs works, I will merge the 
    trunk changes to the newEquals branch and start looking at C++ codegen issues
    with Any on the branch.
    
    
    
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@12336 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ x10.runtime/src-cpp/x10/lang/IBox.h
@@ -0,0 +1,69 @@
+#ifndef X10_LANG_IBOX_H
+#define X10_LANG_IBOX_H
+
+#include <x10/lang/Reference.h>
+
+namespace x10 {
+    namespace lang {
+
+        class String;
+        
+        template <class T> inline x10aux::itable_entry* getITablesForIBox(T value) { return value->_getITables(); } // TODO: Very soon will be changed to _getITablesForIBox()
+
+        // TODO: As soon as we change the basic numeric types to implement interfaces
+        //       at the X10 level, we will have to change this code to return to the
+        //       itables for IBox that we handwrite for the primitives.
+        inline x10aux::itable_entry *getITablesForIBox(x10_boolean) { return NULL; }
+        inline x10aux::itable_entry *getITablesForIBox(x10_byte) { return NULL; }
+        inline x10aux::itable_entry *getITablesForIBox(x10_ubyte) { return NULL; }
+        inline x10aux::itable_entry *getITablesForIBox(x10_char) { return NULL; }
+        inline x10aux::itable_entry *getITablesForIBox(x10_short) { return NULL; }
+        inline x10aux::itable_entry *getITablesForIBox(x10_ushort) { return NULL; }
+        inline x10aux::itable_entry *getITablesForIBox(x10_int) { return NULL; }
+        inline x10aux::itable_entry *getITablesForIBox(x10_uint) { return NULL; }
+        inline x10aux::itable_entry *getITablesForIBox(x10_long) { return NULL; }
+        inline x10aux::itable_entry *getITablesForIBox(x10_ulong) { return NULL; }
+        inline x10aux::itable_entry *getITablesForIBox(x10_float) { return NULL; }
+        inline x10aux::itable_entry *getITablesForIBox(x10_double) { return NULL; }
+
+        
+        /**
+         * This is a class that exists only at the C++ implementation level,
+         * not at the X10 language level.  Therefore it does not have an
+         * associated RTT.
+         * 
+         * The purpose of this class is to enable struct values to be boxed
+         * when they are assigned/casts to variables of interface types.
+         * When that happens, all we need to provide is a 
+         */
+        template <class T> class IBox : public Reference {
+        public:
+            T value;
+            
+            IBox(T val) : value(val) {
+                location = x10aux::here;
+            }
+
+            virtual x10aux::itable_entry* _getITables() { return getITablesForIBox(value); } 
+
+            virtual const x10aux::RuntimeType *_type() const { return x10aux::getRTT<T>(); }
+            static const x10aux::RuntimeType* getRTT() { return x10aux::getRTT<T>(); }
+
+            virtual x10aux::ref<String> toString() { return x10aux::to_string(value); }
+            
+            // TODO: Need to implement serialization/deserialization support for this class!
+
+            virtual x10aux::serialization_id_t _get_serialization_id() {
+                assert(false);
+                return -1000;
+            }
+
+            virtual void _serialize_body(x10aux::serialization_buffer &) {
+                assert(false);
+            }
+        };
+    }
+}
+
+
+#endif
