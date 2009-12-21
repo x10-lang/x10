869e6bd86105ab58130efd59664c084635a08e62
Emmanuel Geay <egeay@us.ibm.com>
2009-12-21 18:36:55 +0000

    Added visit of new AtStmt CAst node.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@12364 a326200e-df2c-42e0-81fa-597e909af41d

--- branches/polyglot3/com.ibm.wala.cast.x10/src/com/ibm/wala/cast/x10/translator/polyglot/X10TranslatorVisitor.java
+++ branches/polyglot3/com.ibm.wala.cast.x10/src/com/ibm/wala/cast/x10/translator/polyglot/X10TranslatorVisitor.java
@@ -3,18 +3,39 @@
  */
 package com.ibm.wala.cast.x10.translator.polyglot;
 
-import polyglot.ext.x10.ast.*;
+import x10.ast.Async;
+import x10.ast.AtEach;
+import x10.ast.AtStmt;
+import x10.ast.Atomic;
+import x10.ast.Await;
+import x10.ast.Clocked;
+import x10.ast.Closure;
+import x10.ast.ClosureCall;
+import x10.ast.Finish;
+import x10.ast.ForEach;
+import x10.ast.ForLoop;
+import x10.ast.Future;
+import x10.ast.Here;
+import x10.ast.Next;
+import x10.ast.ParExpr;
+import x10.ast.PlaceCast;
+import x10.ast.Point;
+import x10.ast.Range;
+import x10.ast.Region;
+import x10.ast.SettableAssign;
+import x10.ast.Tuple;
+import x10.ast.When;
+import x10.ast.X10Formal;
 
-import com.ibm.wala.cast.tree.CAstNode;
 import com.ibm.wala.cast.java.translator.polyglot.TranslatingVisitor;
-import com.ibm.wala.cast.java.translator.polyglot.PolyglotJava2CAstTranslator.*;
+import com.ibm.wala.cast.java.translator.polyglot.PolyglotJava2CAstTranslator.WalkContext;
+import com.ibm.wala.cast.tree.CAstNode;
 
 public interface X10TranslatorVisitor extends TranslatingVisitor {
-    // PORT1.7 ArrayConstructor in 1.5 was "new T[] { initialize-code }"; now this is represented as an array with a closure for an initializer(?)
-//    CAstNode visit(ArrayConstructor ac, WalkContext context);
     CAstNode visit(Async a, WalkContext context);
     CAstNode visit(AtEach a, WalkContext context);
     CAstNode visit(Atomic a, WalkContext context);
+    CAstNode visit(AtStmt atStmt, WalkContext context);
     CAstNode visit(Await a, WalkContext context);
     CAstNode visit(Clocked c, WalkContext context);
     CAstNode visit(Closure closure, WalkContext context);
@@ -23,8 +44,6 @@ public interface X10TranslatorVisitor extends TranslatingVisitor {
     CAstNode visit(ForLoop f, WalkContext context);
     CAstNode visit(ForEach f, WalkContext context);
     CAstNode visit(Future f, WalkContext context);
-    // PORT1.7 X10 1.7 now has proper parametric types
-//    CAstNode visit(GenParameterExpr gpe, WalkContext context);
     CAstNode visit(Here h, WalkContext context);
     CAstNode visit(Next n, WalkContext context);
     CAstNode visit(ParExpr expr, WalkContext wc);
@@ -34,9 +53,6 @@ public interface X10TranslatorVisitor extends TranslatingVisitor {
     CAstNode visit(Range r, WalkContext context);
     CAstNode visit(Tuple t, WalkContext context);
     CAstNode visit(When w, WalkContext context);
-    // PORT1.7 - Array accesses are now represented by ordinary method calls
-//    CAstNode visit(X10ArrayAccess aa, WalkContext context);
-//    CAstNode visit(X10ArrayAccess1 aa, WalkContext context);
     CAstNode visit(X10Formal f, WalkContext context);
     CAstNode visit(SettableAssign n, WalkContext wc);
 }
