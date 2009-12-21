7769d10979bfc7a5a3f3d0f5b2ea87b5ea27e1ad
Emmanuel Geay <egeay@us.ibm.com>
2009-12-21 18:25:23 +0000

    - Polyglot import update - Added visit of AtStmt CAst Node.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/branches@12359 a326200e-df2c-42e0-81fa-597e909af41d

--- polyglot3/com.ibm.wala.cast.x10/src/com/ibm/wala/cast/x10/translator/polyglot/X10ASTTraverser.java
+++ polyglot3/com.ibm.wala.cast.x10/src/com/ibm/wala/cast/x10/translator/polyglot/X10ASTTraverser.java
@@ -1,75 +1,72 @@
 package com.ibm.wala.cast.x10.translator.polyglot;
 
 import polyglot.ast.Node;
-import polyglot.ext.x10.ast.Async;
-import polyglot.ext.x10.ast.AtEach;
-import polyglot.ext.x10.ast.Atomic;
-import polyglot.ext.x10.ast.Await;
-import polyglot.ext.x10.ast.Closure;
-import polyglot.ext.x10.ast.ClosureCall;
-import polyglot.ext.x10.ast.Finish;
-import polyglot.ext.x10.ast.ForEach;
-import polyglot.ext.x10.ast.ForLoop;
-import polyglot.ext.x10.ast.Future;
-import polyglot.ext.x10.ast.Here;
-import polyglot.ext.x10.ast.Next;
-import polyglot.ext.x10.ast.ParExpr;
-import polyglot.ext.x10.ast.SettableAssign;
-import polyglot.ext.x10.ast.Tuple;
-import polyglot.ext.x10.ast.When;
-import polyglot.ext.x10.ast.X10Formal;
+import x10.ast.Async;
+import x10.ast.AtEach;
+import x10.ast.AtStmt;
+import x10.ast.Atomic;
+import x10.ast.Await;
+import x10.ast.Closure;
+import x10.ast.ClosureCall;
+import x10.ast.Finish;
+import x10.ast.ForEach;
+import x10.ast.ForLoop;
+import x10.ast.Future;
+import x10.ast.Here;
+import x10.ast.Next;
+import x10.ast.ParExpr;
+import x10.ast.SettableAssign;
+import x10.ast.Tuple;
+import x10.ast.When;
+import x10.ast.X10Formal;
 
 import com.ibm.wala.cast.java.translator.polyglot.ASTTraverser;
 import com.ibm.wala.cast.java.translator.polyglot.PolyglotJava2CAstTranslator.WalkContext;
 import com.ibm.wala.cast.tree.CAstNode;
 
 public class X10ASTTraverser extends ASTTraverser {
-    private X10ASTTraverser() { }
+  private X10ASTTraverser() {
+  }
 
-    public static CAstNode visit(Node n, X10TranslatorVisitor xtv, WalkContext wc) {
-        // PORT1.7 ArrayConstructor in 1.5 was "new T[] { initialize-code }"; now this is represented as an array with a closure for an initializer(?)
-//	if (n instanceof ArrayConstructor)
-//	    return xtv.visit((ArrayConstructor) n, wc);
-	if (n instanceof Async)
-	    return xtv.visit((Async) n, wc);
-	else if (n instanceof AtEach)
-	    return xtv.visit((AtEach) n, wc);
-	else if (n instanceof Atomic)
-	    return xtv.visit((Atomic) n, wc);
-	else if (n instanceof Await)
-	    return xtv.visit((Await) n, wc);
-	else if (n instanceof Closure)
-	    return xtv.visit((Closure) n, wc);
-	else if (n instanceof ClosureCall)
-	    return xtv.visit((ClosureCall) n, wc);
-	else if (n instanceof Finish)
-	    return xtv.visit((Finish) n, wc);
-	else if (n instanceof ForEach)
-	    return xtv.visit((ForEach) n, wc);
-	else if (n instanceof ForLoop)
-	    return xtv.visit((ForLoop) n, wc);
-	else if (n instanceof Future)
-	    return xtv.visit((Future) n, wc);
-	else if (n instanceof Here)
-	    return xtv.visit((Here) n, wc);
-	else if (n instanceof Next)
-	    return xtv.visit((Next) n, wc);
-	else if (n instanceof ParExpr)
-	    return xtv.visit((ParExpr) n, wc);
-	else if (n instanceof When)
-	    return xtv.visit((When) n, wc);
-	// PORT1.7 Array accesses now look like ordinary method calls
-//	else if (n instanceof X10ArrayAccess)
-//	    return xtv.visit((X10ArrayAccess) n, wc);
-//	else if (n instanceof X10ArrayAccess1)
-//	    return xtv.visit((X10ArrayAccess1) n, wc);
-	else if (n instanceof X10Formal)
-	    return xtv.visit((X10Formal) n, wc);
-	else if (n instanceof Tuple)
-	    return xtv.visit((Tuple) n, wc);
-	else if (n instanceof SettableAssign)
-	    return xtv.visit((SettableAssign) n, wc);
-	else
-	    return ASTTraverser.visit(n, xtv, wc);
-    }
+  public static CAstNode visit(Node n, X10TranslatorVisitor xtv, WalkContext wc) {
+    if (n instanceof Async)
+      return xtv.visit((Async) n, wc);
+    else if (n instanceof AtEach)
+      return xtv.visit((AtEach) n, wc);
+    else if (n instanceof Atomic)
+      return xtv.visit((Atomic) n, wc);
+    else if (n instanceof AtStmt)
+      return xtv.visit((AtStmt) n, wc);
+    else if (n instanceof Await)
+      return xtv.visit((Await) n, wc);
+    else if (n instanceof Closure)
+      return xtv.visit((Closure) n, wc);
+    else if (n instanceof ClosureCall)
+      return xtv.visit((ClosureCall) n, wc);
+    else if (n instanceof Finish)
+      return xtv.visit((Finish) n, wc);
+    else if (n instanceof ForEach)
+      return xtv.visit((ForEach) n, wc);
+    else if (n instanceof ForLoop)
+      return xtv.visit((ForLoop) n, wc);
+    else if (n instanceof Future)
+      return xtv.visit((Future) n, wc);
+    else if (n instanceof Here)
+      return xtv.visit((Here) n, wc);
+    else if (n instanceof Next)
+      return xtv.visit((Next) n, wc);
+    else if (n instanceof ParExpr)
+      return xtv.visit((ParExpr) n, wc);
+    else if (n instanceof When)
+      return xtv.visit((When) n, wc);
+    else if (n instanceof X10Formal)
+      return xtv.visit((X10Formal) n, wc);
+    else if (n instanceof Tuple)
+      return xtv.visit((Tuple) n, wc);
+    else if (n instanceof SettableAssign)
+      return xtv.visit((SettableAssign) n, wc);
+    else
+      return ASTTraverser.visit(n, xtv, wc);
+  }
+  
 }
