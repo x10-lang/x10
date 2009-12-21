21e277e75a4a841ce49056ed18583cebf51cb824
Emmanuel Geay <egeay@us.ibm.com>
2009-12-21 17:07:16 +0000

    Fixed bug in initialization of static variable.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/branches/polyglot3@12351 a326200e-df2c-42e0-81fa-597e909af41d

--- com.ibm.wala.cast.x10/src/com/ibm/wala/cast/x10/loader/X10PrimordialClassLoader.java
+++ com.ibm.wala.cast.x10/src/com/ibm/wala/cast/x10/loader/X10PrimordialClassLoader.java
@@ -1,6 +1,7 @@
 package com.ibm.wala.cast.x10.loader;
 
 import com.ibm.wala.cast.java.ipa.callgraph.JavaSourceAnalysisScope;
+import com.ibm.wala.cast.x10.translator.polyglot.X10SourceLoaderImpl;
 import com.ibm.wala.classLoader.ArrayClassLoader;
 import com.ibm.wala.classLoader.ClassLoaderImpl;
 import com.ibm.wala.classLoader.IClassLoader;
@@ -14,7 +15,7 @@ import com.ibm.wala.util.strings.Atom;
 public class X10PrimordialClassLoader extends ClassLoaderImpl {
     public static Atom X10PrimordialName= Atom.findOrCreateAsciiAtom("X10Primordial");
 
-    public static ClassLoaderReference X10Primordial= new ClassLoaderReference(X10PrimordialName, X10Language.X10, JavaSourceAnalysisScope.SOURCE);
+    public static ClassLoaderReference X10Primordial= new ClassLoaderReference(X10PrimordialName, X10SourceLoaderImpl.X10, JavaSourceAnalysisScope.SOURCE);
 
     public X10PrimordialClassLoader(ClassLoaderReference loader, ArrayClassLoader arrayClassLoader, IClassLoader parent, SetOfClasses exclusions, IClassHierarchy cha) {
 	super(loader, arrayClassLoader, parent, exclusions, cha);
