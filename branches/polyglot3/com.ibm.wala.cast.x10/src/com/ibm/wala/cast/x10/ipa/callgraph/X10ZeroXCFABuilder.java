deb6f8daf2098d80ff2df88e8f601003fc60195d
Emmanuel Geay <egeay@us.ibm.com>
2009-12-21 17:00:51 +0000

    Implemented makeFakeWorldClinitNode method
    git-svn-id: svn://svn.code.sf.net/p/x10/code@12348 a326200e-df2c-42e0-81fa-597e909af41d

--- branches/polyglot3/com.ibm.wala.cast.x10/src/com/ibm/wala/cast/x10/ipa/callgraph/X10ZeroXCFABuilder.java
+++ branches/polyglot3/com.ibm.wala.cast.x10/src/com/ibm/wala/cast/x10/ipa/callgraph/X10ZeroXCFABuilder.java
@@ -1,14 +1,15 @@
 package com.ibm.wala.cast.x10.ipa.callgraph;
 
-import sun.security.action.GetLongAction;
-
 import com.ibm.wala.cast.ipa.callgraph.AstCallGraph;
 import com.ibm.wala.cast.ipa.callgraph.MiscellaneousHacksContextSelector;
 import com.ibm.wala.cast.x10.loader.X10SyntheticLoaderImpl;
 import com.ibm.wala.classLoader.IClass;
 import com.ibm.wala.classLoader.IClassLoader;
-import com.ibm.wala.eclipse.util.CancelException;
-import com.ibm.wala.ipa.callgraph.*;
+import com.ibm.wala.ipa.callgraph.AnalysisCache;
+import com.ibm.wala.ipa.callgraph.AnalysisOptions;
+import com.ibm.wala.ipa.callgraph.CGNode;
+import com.ibm.wala.ipa.callgraph.ContextSelector;
+import com.ibm.wala.ipa.callgraph.ReflectionSpecification;
 import com.ibm.wala.ipa.callgraph.impl.DefaultContextSelector;
 import com.ibm.wala.ipa.callgraph.impl.DelegatingContextSelector;
 import com.ibm.wala.ipa.callgraph.impl.Everywhere;
@@ -16,12 +17,14 @@ import com.ibm.wala.ipa.callgraph.impl.ExplicitCallGraph;
 import com.ibm.wala.ipa.callgraph.impl.FakeRootClass;
 import com.ibm.wala.ipa.callgraph.impl.FakeRootMethod;
 import com.ibm.wala.ipa.callgraph.propagation.SSAContextInterpreter;
-import com.ibm.wala.ipa.callgraph.propagation.cfa.*;
+import com.ibm.wala.ipa.callgraph.propagation.cfa.ZeroXInstanceKeys;
+import com.ibm.wala.ipa.callgraph.propagation.cfa.nCFAContextSelector;
 import com.ibm.wala.ipa.cha.IClassHierarchy;
 import com.ibm.wala.types.Descriptor;
 import com.ibm.wala.types.MethodReference;
-import com.ibm.wala.types.Selector;
+import com.ibm.wala.types.TypeName;
 import com.ibm.wala.types.TypeReference;
+import com.ibm.wala.util.CancelException;
 import com.ibm.wala.util.strings.Atom;
 
 public class X10ZeroXCFABuilder extends X10CFABuilder {
@@ -115,7 +118,21 @@ public class X10ZeroXCFABuilder extends X10CFABuilder {
             }
             @Override
             protected CGNode makeFakeWorldClinitNode() throws CancelException {
-                return null;
+              final TypeReference fakeTypeRef = TypeReference.findOrCreate(X10SyntheticLoaderImpl.X10SyntheticLoader, FakeRootClass.FAKE_ROOT_CLASS.getName());
+              final Atom fakeMethodName = Atom.findOrCreateAsciiAtom("fakeWorldClinit");
+              final Descriptor fakeMethodDesc = Descriptor.findOrCreate(new TypeName[0], TypeReference.VoidName);
+              final MethodReference fakeMethodRef = MethodReference.findOrCreate(fakeTypeRef, fakeMethodName, fakeMethodDesc);
+              final IClass fakeClass = new FakeRootClass(fakeTypeRef, cha) {
+                  @Override
+                  public TypeReference getReference() {
+                      return fakeTypeRef;
+                  }
+                  @Override
+                  public IClassLoader getClassLoader() {
+                      return getClassHierarchy().getLoader(getReference().getClassLoader());
+                  }
+              };
+              return findOrCreateNode(new AstFakeRoot(fakeMethodRef, fakeClass, cha, options, getAnalysisCache()), Everywhere.EVERYWHERE);
             }
         };
     }
