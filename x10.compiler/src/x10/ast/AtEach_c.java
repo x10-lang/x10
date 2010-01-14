dbd531bcdfbde9ee7fe8755e1f8f07377683a318
Leena Unnikrishnan <user@domain>
2010-01-14 18:31:38 +0000

    Fix for XTENLANG-888. Added place checking for ateach.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@12608 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.compiler/src/x10/ast/AtEach_c.java
+++ x10.compiler/src/x10/ast/AtEach_c.java
@@ -16,13 +16,22 @@ import java.util.List;
 import polyglot.ast.Expr;
 import polyglot.ast.Formal;
 import polyglot.ast.Id_c;
+import polyglot.ast.Node;
 import polyglot.ast.Stmt;
 import polyglot.ast.Field_c;
+import polyglot.types.Context;
 import polyglot.types.Name;
 import polyglot.util.CodeWriter;
+import polyglot.util.InternalCompilerError;
 import polyglot.util.Position;
 import polyglot.visit.PrettyPrinter;
 import x10.ast.X10Loop.LoopKind;
+import x10.constraint.XConstrainedTerm;
+import x10.constraint.XConstraint;
+import x10.constraint.XConstraint_c;
+import x10.constraint.XFailure;
+import x10.constraint.XTerm;
+import x10.types.X10Context;
 
 /**
  * An immutable representation of the X10 statement: ateach (i : D) S
@@ -53,6 +62,28 @@ public class AtEach_c extends X10ClockedLoop_c implements AtEach, Clocked {
 
 	public Expr getDomain(Expr d) {
 		return new Field_c(position(), d, new Id_c(position(), Name.make("dist")));
+	}
+
+	@Override
+	public Context enterChildScope(Node child, Context c) {
+		X10Context xc = (X10Context) super.enterChildScope(child, c);
+		XConstraint d = new XConstraint_c();
+		XTerm term = XConstraint_c.genUQV();
+		try {
+			// FIXME: this creates a new place term; ideally, it should be the place associated with each
+			// point in the ateach distribution 
+			xc = (X10Context) xc.pushPlace(XConstrainedTerm.instantiate(d, term));
+		} 
+		catch (XFailure z) {
+			throw new InternalCompilerError("Cannot construct placeTerm from " + term + " and constraint " + d + ".");
+		}
+        return xc;
+	}
+
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		// TODO Auto-generated method stub
+		return super.clone();
 	}
 
 	public String toString() {
