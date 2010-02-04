68d1872beed1db6fd2e1fb291118c65dfac569cd
David Grove <groved@us.ibm.com>
2010-02-04 19:00:49 +0000

    Stub out implementation of x10.x10rt.* native methods.
    If JAVA_HOME is defined, then the x10rt build will 
    compile the jni/*.cc files and included them in the
    x10rt archives.  This is conditional on JAVA_HOME 
    being defined because we need to add JAVA_HOME/include
    to the CXXFLAGS to find jni.h.
    
    
    
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@12805 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ x10.runtime/x10rt/jni/jni_message.cc
@@ -0,0 +1,333 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <assert.h>
+
+#include <stdio.h>
+
+#include "x10_x10rt_ActiveMessage.h"
+
+#include <x10rt_front.h>
+
+/*
+ * Class:     x10_x10rt_ActiveMessage
+ * Method:    initializeMessageHandlers
+ * Signature: ()V
+ */
+JNIEXPORT void JNICALL Java_x10_x10rt_ActiveMessage_initializeMessageHandlers(JNIEnv *, jclass) {
+    fprintf(stderr, "Unimplemented native function\n");
+}
+
+/*
+ * Class:     x10_x10rt_ActiveMessage
+ * Method:    registerMethodImpl
+ * Signature: (Ljava/lang/reflect/Method;Ljava/lang/Class;I)V
+ */
+JNIEXPORT void JNICALL Java_x10_x10rt_ActiveMessage_registerMethodImpl(JNIEnv *, jclass, jobject, jclass, jint) {
+    fprintf(stderr, "Unimplemented native function\n");
+}    
+
+/*
+ * Class:     x10_x10rt_ActiveMessage
+ * Method:    sendRemote
+ * Signature: (II)V
+ */
+JNIEXPORT void JNICALL Java_x10_x10rt_ActiveMessage_sendRemote__II(JNIEnv *, jclass, jint, jint) {
+    fprintf(stderr, "Unimplemented native function\n");
+}    
+
+/*
+ * Class:     x10_x10rt_ActiveMessage
+ * Method:    sendRemote
+ * Signature: (III)V
+ */
+JNIEXPORT void JNICALL Java_x10_x10rt_ActiveMessage_sendRemote__III(JNIEnv *, jclass, jint, jint, jint) {
+    fprintf(stderr, "Unimplemented native function\n");
+}    
+
+/*
+ * Class:     x10_x10rt_ActiveMessage
+ * Method:    sendRemote
+ * Signature: (IIII)V
+ */
+JNIEXPORT void JNICALL Java_x10_x10rt_ActiveMessage_sendRemote__IIII(JNIEnv *, jclass, jint, jint, jint, jint) {
+    fprintf(stderr, "Unimplemented native function\n");
+}    
+
+/*
+ * Class:     x10_x10rt_ActiveMessage
+ * Method:    sendRemote
+ * Signature: (IIIII)V
+ */
+JNIEXPORT void JNICALL Java_x10_x10rt_ActiveMessage_sendRemote__IIIII(JNIEnv *, jclass, jint, jint, jint, jint, jint) {
+    fprintf(stderr, "Unimplemented native function\n");
+}    
+
+/*
+ * Class:     x10_x10rt_ActiveMessage
+ * Method:    sendRemote
+ * Signature: (IIIIII)V
+ */
+JNIEXPORT void JNICALL Java_x10_x10rt_ActiveMessage_sendRemote__IIIIII(JNIEnv *, jclass, jint, jint, jint, jint, jint, jint) {
+    fprintf(stderr, "Unimplemented native function\n");
+}
+
+/*
+ * Class:     x10_x10rt_ActiveMessage
+ * Method:    sendRemote
+ * Signature: (IIJ)V
+ */
+JNIEXPORT void JNICALL Java_x10_x10rt_ActiveMessage_sendRemote__IIJ(JNIEnv *, jclass, jint, jint, jlong) {
+    fprintf(stderr, "Unimplemented native function\n");
+}    
+
+/*
+ * Class:     x10_x10rt_ActiveMessage
+ * Method:    sendRemote
+ * Signature: (IIJJ)V
+ */
+JNIEXPORT void JNICALL Java_x10_x10rt_ActiveMessage_sendRemote__IIJJ(JNIEnv *, jclass, jint, jint, jlong, jlong) {
+    fprintf(stderr, "Unimplemented native function\n");
+}    
+
+/*
+ * Class:     x10_x10rt_ActiveMessage
+ * Method:    sendRemote
+ * Signature: (IIIJ)V
+ */
+JNIEXPORT void JNICALL Java_x10_x10rt_ActiveMessage_sendRemote__IIIJ(JNIEnv *, jclass, jint, jint, jint, jlong) {
+    fprintf(stderr, "Unimplemented native function\n");
+}    
+
+/*
+ * Class:     x10_x10rt_ActiveMessage
+ * Method:    sendRemote
+ * Signature: (IIJI)V
+ */
+JNIEXPORT void JNICALL Java_x10_x10rt_ActiveMessage_sendRemote__IIJI(JNIEnv *, jclass, jint, jint, jlong, jint) {
+    fprintf(stderr, "Unimplemented native function\n");
+}    
+
+/*
+ * Class:     x10_x10rt_ActiveMessage
+ * Method:    sendRemote
+ * Signature: (IIF)V
+ */
+JNIEXPORT void JNICALL Java_x10_x10rt_ActiveMessage_sendRemote__IIF(JNIEnv *, jclass, jint, jint, jfloat) {
+    fprintf(stderr, "Unimplemented native function\n");
+}    
+
+/*
+ * Class:     x10_x10rt_ActiveMessage
+ * Method:    sendRemote
+ * Signature: (IIFF)V
+ */
+JNIEXPORT void JNICALL Java_x10_x10rt_ActiveMessage_sendRemote__IIFF(JNIEnv *, jclass, jint, jint, jfloat, jfloat) {
+    fprintf(stderr, "Unimplemented native function\n");
+}    
+
+/*
+ * Class:     x10_x10rt_ActiveMessage
+ * Method:    sendRemote
+ * Signature: (IIIF)V
+ */
+JNIEXPORT void JNICALL Java_x10_x10rt_ActiveMessage_sendRemote__IIIF(JNIEnv *, jclass, jint, jint, jint, jfloat) {
+    fprintf(stderr, "Unimplemented native function\n");
+}    
+
+/*
+ * Class:     x10_x10rt_ActiveMessage
+ * Method:    sendRemote
+ * Signature: (IIJF)V
+ */
+JNIEXPORT void JNICALL Java_x10_x10rt_ActiveMessage_sendRemote__IIJF(JNIEnv *, jclass, jint, jint, jlong, jfloat) {
+    fprintf(stderr, "Unimplemented native function\n");
+}    
+
+/*
+ * Class:     x10_x10rt_ActiveMessage
+ * Method:    sendRemote
+ * Signature: (IIFI)V
+ */
+JNIEXPORT void JNICALL Java_x10_x10rt_ActiveMessage_sendRemote__IIFI(JNIEnv *, jclass, jint, jint, jfloat, jint) {
+    fprintf(stderr, "Unimplemented native function\n");
+}    
+
+/*
+ * Class:     x10_x10rt_ActiveMessage
+ * Method:    sendRemote
+ * Signature: (IIFJ)V
+ */
+JNIEXPORT void JNICALL Java_x10_x10rt_ActiveMessage_sendRemote__IIFJ(JNIEnv *, jclass, jint, jint, jfloat, jlong) {
+    fprintf(stderr, "Unimplemented native function\n");
+}
+
+/*
+ * Class:     x10_x10rt_ActiveMessage
+ * Method:    sendRemote
+ * Signature: (IID)V
+ */
+JNIEXPORT void JNICALL Java_x10_x10rt_ActiveMessage_sendRemote__IID(JNIEnv *, jclass, jint, jint, jdouble) {
+    fprintf(stderr, "Unimplemented native function\n");
+}    
+
+/*
+ * Class:     x10_x10rt_ActiveMessage
+ * Method:    sendRemote
+ * Signature: (IIDD)V
+ */
+JNIEXPORT void JNICALL Java_x10_x10rt_ActiveMessage_sendRemote__IIDD(JNIEnv *, jclass, jint, jint, jdouble, jdouble) {
+    fprintf(stderr, "Unimplemented native function\n");
+}    
+
+/*
+ * Class:     x10_x10rt_ActiveMessage
+ * Method:    sendRemote
+ * Signature: (IIID)V
+ */
+JNIEXPORT void JNICALL Java_x10_x10rt_ActiveMessage_sendRemote__IIID(JNIEnv *, jclass, jint, jint, jint, jdouble) {
+    fprintf(stderr, "Unimplemented native function\n");
+}    
+
+/*
+ * Class:     x10_x10rt_ActiveMessage
+ * Method:    sendRemote
+ * Signature: (IIFD)V
+ */
+JNIEXPORT void JNICALL Java_x10_x10rt_ActiveMessage_sendRemote__IIFD(JNIEnv *, jclass, jint, jint, jfloat, jdouble) {
+    fprintf(stderr, "Unimplemented native function\n");
+}    
+
+/*
+ * Class:     x10_x10rt_ActiveMessage
+ * Method:    sendRemote
+ * Signature: (IIJD)V
+ */
+JNIEXPORT void JNICALL Java_x10_x10rt_ActiveMessage_sendRemote__IIJD(JNIEnv *, jclass, jint, jint, jlong, jdouble) {
+    fprintf(stderr, "Unimplemented native function\n");
+}    
+
+/*
+ * Class:     x10_x10rt_ActiveMessage
+ * Method:    sendRemote
+ * Signature: (IIDI)V
+ */
+JNIEXPORT void JNICALL Java_x10_x10rt_ActiveMessage_sendRemote__IIDI(JNIEnv *, jclass, jint, jint, jdouble, jint) {
+    fprintf(stderr, "Unimplemented native function\n");
+}    
+
+/*
+ * Class:     x10_x10rt_ActiveMessage
+ * Method:    sendRemote
+ * Signature: (IIDJ)V
+ */
+JNIEXPORT void JNICALL Java_x10_x10rt_ActiveMessage_sendRemote__IIDJ(JNIEnv *, jclass, jint, jint, jdouble, jlong) {
+    fprintf(stderr, "Unimplemented native function\n");
+}    
+
+/*
+ * Class:     x10_x10rt_ActiveMessage
+ * Method:    sendRemote
+ * Signature: (IIDF)V
+ */
+JNIEXPORT void JNICALL Java_x10_x10rt_ActiveMessage_sendRemote__IIDF(JNIEnv *, jclass, jint, jint, jdouble, jfloat) {
+    fprintf(stderr, "Unimplemented native function\n");
+}    
+
+/*
+ * Class:     x10_x10rt_ActiveMessage
+ * Method:    sendArrayRemote
+ * Signature: (III[Z)V
+ */
+JNIEXPORT void JNICALL Java_x10_x10rt_ActiveMessage_sendArrayRemote__III_3Z(JNIEnv *, jclass, jint, jint, jint, jbooleanArray) {
+    fprintf(stderr, "Unimplemented native function\n");
+}    
+
+/*
+ * Class:     x10_x10rt_ActiveMessage
+ * Method:    sendArrayRemote
+ * Signature: (III[B)V
+ */
+JNIEXPORT void JNICALL Java_x10_x10rt_ActiveMessage_sendArrayRemote__III_3B(JNIEnv *, jclass, jint, jint, jint, jbyteArray) {
+    fprintf(stderr, "Unimplemented native function\n");
+}    
+
+/*
+ * Class:     x10_x10rt_ActiveMessage
+ * Method:    sendArrayRemote
+ * Signature: (III[S)V
+ */
+JNIEXPORT void JNICALL Java_x10_x10rt_ActiveMessage_sendArrayRemote__III_3S(JNIEnv *, jclass, jint, jint, jint, jshortArray) {
+    fprintf(stderr, "Unimplemented native function\n");
+}    
+
+/*
+ * Class:     x10_x10rt_ActiveMessage
+ * Method:    sendArrayRemote
+ * Signature: (III[C)V
+ */
+JNIEXPORT void JNICALL Java_x10_x10rt_ActiveMessage_sendArrayRemote__III_3C(JNIEnv *, jclass, jint, jint, jint, jcharArray) {
+    fprintf(stderr, "Unimplemented native function\n");
+}    
+
+/*
+ * Class:     x10_x10rt_ActiveMessage
+ * Method:    sendArrayRemote
+ * Signature: (III[I)V
+ */
+JNIEXPORT void JNICALL Java_x10_x10rt_ActiveMessage_sendArrayRemote__III_3I(JNIEnv *, jclass, jint, jint, jint, jintArray) {
+    fprintf(stderr, "Unimplemented native function\n");
+}    
+
+/*
+ * Class:     x10_x10rt_ActiveMessage
+ * Method:    sendArrayRemote
+ * Signature: (III[F)V
+ */
+JNIEXPORT void JNICALL Java_x10_x10rt_ActiveMessage_sendArrayRemote__III_3F(JNIEnv *, jclass, jint, jint, jint, jfloatArray) {
+    fprintf(stderr, "Unimplemented native function\n");
+}    
+
+/*
+ * Class:     x10_x10rt_ActiveMessage
+ * Method:    sendArrayRemote
+ * Signature: (III[J)V
+ */
+JNIEXPORT void JNICALL Java_x10_x10rt_ActiveMessage_sendArrayRemote__III_3J(JNIEnv *, jclass, jint, jint, jint, jlongArray) {
+    fprintf(stderr, "Unimplemented native function\n");
+}    
+
+/*
+ * Class:     x10_x10rt_ActiveMessage
+ * Method:    sendArrayRemote
+ * Signature: (III[D)V
+ */
+JNIEXPORT void JNICALL Java_x10_x10rt_ActiveMessage_sendArrayRemote__III_3D(JNIEnv *, jclass, jint, jint, jint, jdoubleArray) {
+    fprintf(stderr, "Unimplemented native function\n");
+}    
+
+/*
+ * Class:     x10_x10rt_ActiveMessage
+ * Method:    sendArrayRemote
+ * Signature: (IIII[I)V
+ */
+JNIEXPORT void JNICALL Java_x10_x10rt_ActiveMessage_sendArrayRemote__IIII_3I(JNIEnv *, jclass, jint, jint, jint, jint, jintArray) {
+    fprintf(stderr, "Unimplemented native function\n");
+}    
+
+/*
+ * Class:     x10_x10rt_ActiveMessage
+ * Method:    sendGeneralRemote
+ * Signature: (III[B)V
+ */
+JNIEXPORT void JNICALL Java_x10_x10rt_ActiveMessage_sendGeneralRemote(JNIEnv *, jclass, jint, jint, jint, jbyteArray) {
+    fprintf(stderr, "Unimplemented native function\n");
+}    
+
+/*
+ * Class:     x10_x10rt_ActiveMessage
+ * Method:    processQueue
+ * Signature: ()V
+ */
+JNIEXPORT void JNICALL Java_x10_x10rt_ActiveMessage_processQueue(JNIEnv *, jclass) {
+    fprintf(stderr, "Unimplemented native function\n");
+}    
---
+++ x10.runtime/x10rt/jni/jni_x10rt.cc
@@ -0,0 +1,76 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <assert.h>
+
+#include <stdio.h>
+
+#include "x10_x10rt_X10RT.h"
+
+#include <x10rt_front.h>
+
+/*
+ * Class:     x10_x10rt_X10RT
+ * Method:    initializeImpl
+ * Signature: ()I
+ */
+JNIEXPORT jint JNICALL Java_x10_x10rt_X10RT_initializeImpl(JNIEnv *, jclass) {
+    int argc = 0;
+    char **argv = NULL;
+    x10rt_init(argc, argv);
+    return 0;
+}
+
+/*
+ * Class:     x10_x10rt_X10RT
+ * Method:    hereImpl
+ * Signature: ()I
+ */
+JNIEXPORT jint JNICALL Java_x10_x10rt_X10RT_hereImpl(JNIEnv *, jclass) {
+    return (jint)x10rt_here();
+}
+
+/*
+ * Class:     x10_x10rt_X10RT
+ * Method:    finishImpl
+ * Signature: ()I
+ */
+JNIEXPORT jint JNICALL Java_x10_x10rt_X10RT_finishImpl(JNIEnv *, jclass) {
+    fprintf(stderr, "finishImpl called; doing nothing\n");
+    return 0;
+}
+
+/*
+ * Class:     x10_x10rt_X10RT
+ * Method:    numNodesImpl
+ * Signature: ()I
+ */
+JNIEXPORT jint JNICALL Java_x10_x10rt_X10RT_numNodesImpl(JNIEnv *, jclass) {
+    return (jint)x10rt_nplaces();
+}
+
+/*
+ * Class:     x10_x10rt_X10RT
+ * Method:    barrierImpl
+ * Signature: ()V
+ */
+JNIEXPORT void JNICALL Java_x10_x10rt_X10RT_barrierImpl(JNIEnv *, jclass) {
+    fprintf(stderr, "barrierImpl called; doing nothing\n");
+}
+
+/*
+ * Class:     x10_x10rt_X10RT
+ * Method:    fenceImpl
+ * Signature: ()V
+ */
+JNIEXPORT void JNICALL Java_x10_x10rt_X10RT_fenceImpl(JNIEnv *, jclass) {
+    fprintf(stderr, "fenceImpl called; doing nothing\n");
+}
+
+/*
+ * Class:     x10_x10rt_X10RT
+ * Method:    pollImpl
+ * Signature: ()V
+ */
+JNIEXPORT void JNICALL Java_x10_x10rt_X10RT_pollImpl(JNIEnv *, jclass) {
+    fprintf(stderr, "pollImpl called; doing nothing\n");
+}
