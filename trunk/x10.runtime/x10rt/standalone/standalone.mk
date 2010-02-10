4ff1b3ff15e6dbf9c4bb06d8c2764263192f99c8
David Grove <groved@us.ibm.com>
2010-02-10 22:59:55 +0000

    Bow to reality and be less aggrssive in the conversion of the build process over to producing static libs.
    By defining X10_STATIC_LIB=1 and LIBSUFFIX=.a
    in x10.runtime/Make.rules one can get the old behavior
    of building static libs (.a files) as the result
    of the intermediate x10rt and x10 builds.
    
    Will open JIRAs for converting the non-Linux
    platforms over to shared libs. People who know
    more about building on non-linux platforms can
    pick up the JIRAs and convert the platforms one at a time.
    
    
    
    git-svn-id: svn://svn.code.sf.net/p/x10/code@12853 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.runtime/x10rt/standalone/standalone.mk
+++ trunk/x10.runtime/x10rt/standalone/standalone.mk
@@ -8,8 +8,13 @@ PROPERTIES += etc/x10rt_standalone.properties
 %.standalone: %.cc $(STANDALONE_DYNLIB)
 	$(CXX) $(CXXFLAGS) $< -o $@ $(LDFLAGS) -lx10rt_standalone $(X10RT_TEST_LDFLAGS)
 
+ifdef X10_STATIC_LIB
+$(STANDALONE_DYNLIB): standalone/x10rt_standalone.o $(COMMON_OBJS)
+	$(AR) $(ARFLAGS) $@ $^
+else
 $(STANDALONE_DYNLIB): standalone/x10rt_standalone.o $(COMMON_OBJS)
 	$(CXX) $(CXXFLAGS) $(CXXFLAGS_SHARED) -o $@ $^
+endif
 
 etc/x10rt_standalone.properties:
 	@echo "CXX=$(CXX)" > $@
