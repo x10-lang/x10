990e2eb89f2220516559032cc15097cfde4ff454
David Grove <groved@us.ibm.com>
2010-02-11 21:46:39 +0000

    XTENLANG-930: Rail.toString() should print rail contents
    The C++ and Java native implementations of Rail had gotten out
    of synch.  The C++ version has printed something useful for a while,
    the Java version was just delegating to toString() on the backing
    Java array object, which does not print its elements.  
    
    So, this commit changes the Java implementation to match the C++ one.
    We could consider a slightly smarter implementation that only prints out
    the first few/last few elements if the array Rail is above a size threshold
    (for example print first 10, then "...n-20 elements..." then  last 10).
    
    
    
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@12866 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.runtime/src-java/x10/core/ValRail.java
+++ x10.runtime/src-java/x10/core/ValRail.java
@@ -146,11 +146,17 @@ public final class ValRail<T> implements AnyRail<T> {
     }
     
     public String toString() {
-    	return value.toString();
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (int i = 0; i < length; i++) {
+            if (i > 0)
+                sb.append(", ");
+            sb.append(apply(i));
+        }
+        sb.append("]");
+        return sb.toString();
     }
-
   
-    
     //
     // Runtime type information
     //
