82836367c86876cb08575393e58fdecdc94100ab
David Grove <groved@us.ibm.com>
2010-02-12 23:02:16 +0000

    WIP on restructuring railToString to reduce the volume of code that is duplicated via template expansion.
    Cuts the size of libx10.so by 500k (1.5%).  
    
    Working on one additional tweak that I will commit over 
    the weekend if it helps.
    
    
    
    git-svn-id: svn://svn.code.sf.net/p/x10/code@12877 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.runtime/src-cpp/x10aux/rail_utils.cc
+++ trunk/x10.runtime/src-cpp/x10aux/rail_utils.cc
@@ -10,6 +10,34 @@
 using namespace x10::lang;
 using namespace x10aux;
 
+ref<String> x10aux::emptyRailToString = String::Lit("[]");
+
+#ifndef NO_IOSTREAM
+std::stringstream* x10aux::allocStringStream() {
+    return new (x10aux::alloc<std::stringstream>()) std::stringstream();
+}
+
+void x10aux::freeStringStream (std::stringstream *ss) {
+    x10aux::dealloc<std::stringstream>(ss);
+}
+
+void x10aux::railToStringProcessElement(std::stringstream *ss, int i, ref<x10::lang::String> elemStr) {
+    if (i == 0) {
+        (*ss) << "[";
+    } else {
+        (*ss) << ",";
+    }
+    (*ss) << elemStr;
+}
+
+ref<x10::lang::String> x10aux::railToStringFinalize(std::stringstream *ss) {
+    (*ss) << "]";
+    ref<x10::lang::String> result = x10::lang::String::Lit(ss->str().c_str());
+    freeStringStream(ss);
+    return result;
+}
+#endif /* NO_IOSTREAM */
+
 void x10aux::throwArrayIndexOutOfBoundsException(x10_int index, x10_int length) {
 #ifndef NO_EXCEPTIONS
     char *msg = alloc_printf("%ld not in [0,%ld)", (long)index, (long)length);
