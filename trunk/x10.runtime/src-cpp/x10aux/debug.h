6a436c0d0eaa889b56b082d7121c4dab7de5b15a
David Grove <groved@us.ibm.com>
2010-02-17 23:18:08 +0000

    update file header comment
    git-svn-id: svn://svn.code.sf.net/p/x10/code@12928 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.runtime/src-cpp/x10aux/debug.h
+++ trunk/x10.runtime/src-cpp/x10aux/debug.h
@@ -1,3 +1,14 @@
+/*
+ *  This file is part of the X10 project (http://x10-lang.org).
+ *
+ *  This file is licensed to You under the Eclipse Public License (EPL);
+ *  You may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *      http://www.opensource.org/licenses/eclipse-1.0.php
+ *
+ *  (C) Copyright IBM Corporation 2006-2010.
+ */
+
 //
 // A list of interfaces agreed upon with the Toronto Parallel Debugger
 // team for passing source program information to the C++ debugger
