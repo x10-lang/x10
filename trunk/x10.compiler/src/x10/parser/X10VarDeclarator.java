7475b420f1b9a7a78b02961d0c65e533495f98a2
David Grove <groved@us.ibm.com>
2010-02-17 21:07:35 +0000

    update file header comment
    git-svn-id: svn://svn.code.sf.net/p/x10/code@12915 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.compiler/src/x10/parser/X10VarDeclarator.java
+++ trunk/x10.compiler/src/x10/parser/X10VarDeclarator.java
@@ -1,11 +1,14 @@
-//
-// Licensed Material 
-// (C) Copyright IBM Corp, 2006-2008
-//
-
 /*
- * Created by vj on Jan 23, 2005
+ *  This file is part of the X10 project (http://x10-lang.org).
+ *
+ *  This file is licensed to You under the Eclipse Public License (EPL);
+ *  You may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *      http://www.opensource.org/licenses/eclipse-1.0.php
+ *
+ *  (C) Copyright IBM Corporation 2006-2010.
  */
+
 package x10.parser;
 
 import java.util.ArrayList;
