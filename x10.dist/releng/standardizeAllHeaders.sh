2dca746c0ab1069cc4f02c597f97e89c3cbc291e
David Grove <groved@us.ibm.com>
2010-02-17 20:49:04 +0000

    avoid bashing the header on code contributed from other projects.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@12913 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.dist/releng/standardizeAllHeaders.sh
+++ x10.dist/releng/standardizeAllHeaders.sh
@@ -8,16 +8,18 @@ cdir="`pwd`"
 [ "$cdir" = "/" ] && cdir="$cdir."
 cd "$top"
 
-for proj in \
-	x10.common \
+# NOTE: x10.common/src to avoid including x10.common/contrib
+
+for srcdir in \
+	x10.common/src \
 	x10.compiler \
 	x10.constraints \
 	x10.dist \
 	x10.runtime \
 	x10.tests
 do
-  echo "Standardizing file headers in $proj"
-  cd $proj
+  echo "Standardizing file headers in $srcdir"
+  cd $srcdir
   find . -name .svn -prune -o -name "*.java" -exec $top/x10.dist/releng/standardizeHeader.pl {} \;
   find . -name .svn -prune -o -name "*.x10" -exec $top/x10.dist/releng/standardizeHeader.pl {} \;
   cd $top
