6e48e40595c6f283e9104ee5149250a8fcd0f36a
Mark Mergen <user@domain>
2010-02-17 16:17:11 +0000

    Additions to type mapping, calls, new, return, Main* classes, etc. Fix asm.jar in build.xml. x10.dist/samples/Hello.x10 now compiles and runs.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@12906 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.compiler/src/x10bc/X10BytecodeTranslator.java
+++ trunk/x10.compiler/src/x10bc/X10BytecodeTranslator.java
@@ -3,6 +3,7 @@ package x10bc;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -10,12 +11,25 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.objectweb.asm.AnnotationVisitor;
+import org.objectweb.asm.Attribute;
+import org.objectweb.asm.ClassAdapter;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.FieldVisitor;
+import org.objectweb.asm.Label;
+import org.objectweb.asm.MethodAdapter;
+import org.objectweb.asm.MethodVisitor;
+
 import polyglot.ast.Call;
 import polyglot.ast.ClassBody;
 import polyglot.ast.ClassDecl;
 import polyglot.ast.ConstructorCall;
 import polyglot.ast.Expr;
 import polyglot.ast.Expr_c;
+import polyglot.ast.MethodDecl;
+import polyglot.ast.New;
 import polyglot.ast.Node;
 import polyglot.ast.NodeFactory;
 import polyglot.ast.SourceFile;
@@ -37,6 +51,7 @@ import polyglot.dispatch.Dispatch;
 import polyglot.frontend.Job;
 import polyglot.types.ClassDef;
 import polyglot.types.ClassType;
+import polyglot.types.Flags;
 import polyglot.types.MethodInstance;
 import polyglot.types.Name;
 import polyglot.types.QName;
@@ -47,7 +62,10 @@ import polyglot.util.ErrorInfo;
 import polyglot.util.InternalCompilerError;
 import polyglot.util.Position;
 
+import x10.Configuration;
 import x10.ast.ParExpr_c;
+import x10.ast.X10New_c;
+import x10.emitter.Emitter;
 import x10.types.X10ClassDef;
 import x10.types.X10ClassType;
 import x10.types.X10TypeMixin;
@@ -58,6 +76,156 @@ import x10.visit.X10LocalClassRemover;
 public class X10BytecodeTranslator extends BytecodeTranslator {
 
     static public class X10ClassTranslator extends ClassTranslator {
+
+        class MethodNameAdapter extends MethodAdapter {
+            MethodNameAdapter(MethodVisitor mv) {
+                super(mv);
+            }
+            public AnnotationVisitor visitAnnotationDefault() {
+                System.out.println("visAnnDef");
+                return mv.visitAnnotationDefault();
+            }
+            public AnnotationVisitor visitAnnotation(String dsc, boolean vis) {
+                System.out.println("visAnn "+dsc+"|"+vis);
+                return mv.visitAnnotation(dsc, vis);
+            }
+            public AnnotationVisitor visitParameterAnnotation(int prm, String dsc, boolean vis) {
+                System.out.println("visPrmAnn "+prm+"|"+dsc+"|"+vis);
+                return mv.visitParameterAnnotation(prm, dsc, vis);
+            }
+            public void visitAttribute(Attribute att) {
+                System.out.println("visAtt "+att);
+                mv.visitAttribute(att);
+            }
+            public void visitCode() {
+                System.out.println("visCod");
+                mv.visitEnd();
+            }
+            public void visitFrame(int typ, int nLc, Object[] loc, int nSt, Object[] stk) {
+                System.out.println("visFrm "+typ+"|"+nLc+"|"+loc+"|"+nSt+"|"+stk);
+                mv.visitFrame(typ, nLc, loc, nSt, stk);
+            }
+            public void visitInsn(int opc) {
+                System.out.println("visIns "+opc);
+                mv.visitInsn(opc);
+            }
+            public void visitIntInsn(int opc, int opr) {
+                System.out.println("visIntIns "+opc+"|"+opr);
+                mv.visitIntInsn(opc, opr);
+            }
+            public void visitVarInsn(int opc, int var) {
+                System.out.println("visVarIns "+opc+"|"+var);
+                mv.visitVarInsn(opc, var);
+            }
+            public void visitTypeInsn(int opc, String dsc) {
+                System.out.println("visTypIns "+opc+"|"+dsc);
+                mv.visitTypeInsn(opc, dsc.replaceFirst("Test2", classNameOfSymbol(currentClass)));
+            }
+            public void visitFieldInsn(int opc, String own, String nam, String dsc) {
+                System.out.println("visFldIns "+opc+"|"+own+"|"+nam+"|"+dsc);
+                mv.visitFieldInsn(opc, own.replaceFirst("Test2", classNameOfSymbol(currentClass)),
+                                  nam, dsc.replaceFirst("Test2", classNameOfSymbol(currentClass)));
+            }
+            public void visitMethodInsn(int opc, String own, String nam, String dsc) {
+                System.out.println("visMthIns "+opc+"|"+own+"|"+nam+"|"+dsc);
+                mv.visitMethodInsn(opc, own.replaceFirst("Test2", classNameOfSymbol(currentClass)),
+                                   nam, dsc.replaceFirst("Test2", classNameOfSymbol(currentClass)));
+            }
+            public void visitJumpInsn(int opc, Label lab) {
+                System.out.println("visJmpIns "+opc+"|"+lab);
+                mv.visitJumpInsn(opc, lab);
+            }
+            public void visitLabel(Label lab) {
+                System.out.println("visLab "+lab);
+                mv.visitLabel(lab);
+            }
+            public void visitLdcInsn(Object cst) {
+                System.out.println("visLdcIns "+cst);
+                mv.visitLdcInsn(cst);
+            }
+            public void visitIincInsn(int var, int inc) {
+                System.out.println("visIncIns "+var+"|"+inc);
+                mv.visitIincInsn(var, inc);
+            }
+            public void visitTableSwitchInsn(int min, int max, Label dft, Label[] lbs) {
+                System.out.println("visTSwIns "+min+"|"+max+"|"+dft+"|"+lbs);
+                mv.visitTableSwitchInsn(min, max, dft, lbs);
+            }
+            public void visitLookupSwitchInsn(Label dft, int[] kys, Label[] lbs) {
+                System.out.println("visLSwIns "+dft+"|"+kys+"|"+lbs);
+                mv.visitLookupSwitchInsn(dft, kys, lbs);
+            }
+            public void visitMultiANewArrayInsn(String dsc, int dms) {
+                System.out.println("visMAAIns "+dsc+"|"+dms);
+                mv.visitMultiANewArrayInsn(dsc, dms);
+            }
+            public void visitTryCatchBlock(Label sta, Label end, Label hdl, String typ) {
+                System.out.println("visTrCBlk "+sta+"|"+end+"|"+hdl+"|"+typ);
+                mv.visitTryCatchBlock(sta, end, hdl, typ);
+            }
+            public void visitLocalVariable(String nam, String dsc, String sig, Label sta, Label end, int ndx) {
+                System.out.println("visLocVar "+nam+"|"+dsc+"|"+sig+"|"+sta+"|"+end+"|"+ndx);
+                mv.visitLocalVariable(nam, dsc, sig, sta, end, ndx);
+            }
+            public void visitLineNumber(int lin, Label sta) {
+                System.out.println("visLinNum "+lin+"|"+sta);
+                mv.visitLineNumber(lin, sta);
+            }
+            public void visitMaxs(int mxS, int mxL) {
+                System.out.println("visMxs "+mxS+"|"+mxL);
+                mv.visitMaxs(mxS, mxL);
+            }
+            public void visitEnd() {
+                System.out.println("visEnd");
+                mv.visitEnd();
+            }
+        }
+        class ClassNameAdapter extends ClassAdapter {
+            ClassNameAdapter(ClassVisitor cv) {
+                super(cv);
+            }
+            public void visit(int ver, int acc, String nam, String sig, String supNam, String[] intfas) {
+                System.out.println(ver+"|"+acc+"|"+nam+"|"+sig+"|"+supNam+"|"+intfas);
+                cv.visit(ver, acc, nam.replaceFirst("Test2", classNameOfSymbol(currentClass)), sig, supNam, intfas);
+            }
+            public void visitSource(String sor, String dbg) {
+                System.out.println("visSor "+sor+"|"+dbg);
+                cv.visitSource(fileName(currentClass.position()), dbg);
+            }
+            public void visitOuterClass(String own, String nam, String dsc) {
+                System.out.println("visOut "+own+"|"+nam+"|"+dsc);
+                cv.visitOuterClass(own.replaceFirst("Test2", classNameOfSymbol(currentClass)), nam, dsc);
+            }
+            public AnnotationVisitor visitAnnotation(String dsc, boolean vis) {
+                System.out.println("visAnn "+dsc+"|"+vis);
+                return cv.visitAnnotation(dsc, vis);
+            }
+            public void visitAttribute(Attribute att) {
+                System.out.println("visAtt "+att);
+                cv.visitAttribute(att);
+            }
+            public void visitInnerClass(String nam, String outNam, String innNam, int acc) {
+                System.out.println("visInn "+nam+"|"+outNam+"|"+innNam+"|"+acc);
+                cv.visitInnerClass(nam.replaceFirst("Test2", classNameOfSymbol(currentClass)),
+                                   outNam==null? outNam : outNam.replaceFirst("Test2", classNameOfSymbol(currentClass)),
+                                   innNam, acc);
+            }
+            public FieldVisitor visitField(int acc, String nam, String dsc, String sig, Object val) {
+                System.out.println("visFld "+acc+"|"+nam+"|"+dsc+"|"+sig+"|"+val);
+                return cv.visitField(acc, nam, dsc.replaceFirst("Test2", classNameOfSymbol(currentClass)), sig, val);
+            }
+            public MethodVisitor visitMethod(int acc, String nam, String dsc, String sig, String[] xcp) {
+                System.out.println("visMth "+acc+"|"+nam+"|"+dsc+"|"+sig+"|"+xcp);
+                MethodVisitor mv = cv.visitMethod(acc, nam, dsc.replaceFirst("Test2", classNameOfSymbol(currentClass)), sig, xcp);
+                if (mv != null) mv = new MethodNameAdapter(mv);
+                return mv;
+            }
+            public void visitEnd() {
+                System.out.println("visEnd");
+                cv.visitEnd();
+            }
+        }
+
         public X10ClassTranslator(Job job, TypeSystem ts, NodeFactory nf, BytecodeTranslator bc, ClassDef cd) {
             super(job, ts, nf, bc, cd);        	
         }
@@ -88,6 +256,60 @@ public class X10BytecodeTranslator extends BytecodeTranslator {
                 System.out.println(typeFromPolyglotTypeV(t));
                 return typeFromPolyglotTypeV(t);
             }
+        }
+
+        public void visit(final MethodDecl n) {
+            Flags flags = n.flags().flags();
+
+            System.out.println("MethodDecl " + n.name().id().toString());
+    		if (n.name().id().toString().equals("main")) {
+                System.out.println(flags.isPublic());
+                System.out.println(flags.isStatic());
+                System.out.println(n.returnType().type().isVoid());
+                System.out.println(n.formals().size() == 1);
+                System.out.println(n.formals().get(0).declType().isSubtype(((X10TypeSystem) ts).Rail(ts.String()), ts.emptyContext()));
+            if (flags.isPublic() && flags.isStatic() && n.returnType().type().isVoid() && n.formals().size() == 1 &&
+                n.formals().get(0).declType().isSubtype(((X10TypeSystem) ts).Rail(ts.String()), ts.emptyContext()))
+            {
+                try {
+                    String mname = Configuration.COMPILER_FRAGMENT_DATA_DIRECTORY + "Test2$Main.class";
+                    ClassReader cr = new ClassReader(X10BytecodeTranslator.class.getClassLoader().getResourceAsStream(mname));
+                    ClassWriter cw = new ClassWriter(cr, 0);
+                    ClassNameAdapter ca = new ClassNameAdapter(cw);
+                    cr.accept(ca, 0);
+                    FileOutputStream fw = new FileOutputStream(classNameOfSymbol(currentClass) + "$Main.class");
+                    fw.write(cw.toByteArray());
+                    fw.close();
+                } catch (IOException e) {
+                    System.out.println("Can't find/write $Main");
+                }
+                try {
+                    String mname = Configuration.COMPILER_FRAGMENT_DATA_DIRECTORY + "Test2$Main$1.class";
+                    ClassReader cr = new ClassReader(X10BytecodeTranslator.class.getClassLoader().getResourceAsStream(mname));
+                    ClassWriter cw = new ClassWriter(cr, 0);
+                    ClassNameAdapter ca = new ClassNameAdapter(cw);
+                    cr.accept(ca, 0);
+                    FileOutputStream fw = new FileOutputStream(classNameOfSymbol(currentClass) + "$Main$1.class");
+                    fw.write(cw.toByteArray());
+                    fw.close();
+                } catch (IOException e) {
+                    System.out.println("Can't find/write $Main$1");
+                }
+                try {
+                    String mname = Configuration.COMPILER_FRAGMENT_DATA_DIRECTORY + "Test2$Main$2.class";
+                    ClassReader cr = new ClassReader(X10BytecodeTranslator.class.getClassLoader().getResourceAsStream(mname));
+                    ClassWriter cw = new ClassWriter(cr, 0);
+                    ClassNameAdapter ca = new ClassNameAdapter(cw);
+                    cr.accept(ca, 0);
+                    FileOutputStream fw = new FileOutputStream(classNameOfSymbol(currentClass) + "$Main$2.class");
+                    fw.write(cw.toByteArray());
+                    fw.close();
+                } catch (IOException e) {
+                    System.out.println("Can't find/write $Main$2");
+                }
+            }}
+            genMethod(n.methodDef().flags(), n.methodDef(), n.methodDef().name().toString(), n.formals(),
+                      Types.get(n.methodDef().returnType()), n.throwTypes(), n.body());
         }
 
         public ClassTranslator newClassTranslator(BytecodeTranslator bc, ClassDef cd) {
@@ -177,9 +399,33 @@ public class X10BytecodeTranslator extends BytecodeTranslator {
 
 		public void visit(ParExpr_c n) { visitExpr(n.expr()); }
 
+        public void visit(final X10New_c n) {
+            if (n.body() != null) {
+                IClassGen cg = newClassTranslator(bc, n.anonType(), context).translateClass(n, n.body());
+	            context.cg().addInnerClass(cg);
+	        }
+
+	        alloc((ClassType) X10TypeMixin.baseType(n.type()), n.constructorInstance().formalTypes(), n.arguments(), n.position());
+	    }
+
+        protected void promote(Expr n, polyglot.types.Type t) {
+            visitExpr(n);
+            System.out.println(il.currentStack().top());
+            coerce(il.currentStack().top(), typeof(t), n.position());
+        }
+
+        protected void pushArguments(List<polyglot.types.Type> formalTypes, List<Expr> args) {
+            assert formalTypes.size() == args.size();
+            for (int i = 0; i < args.size(); i++) {
+                polyglot.types.Type t = formalTypes.get(i);
+                Expr arg = args.get(i);
+                promote(arg, t);
+            }
+        }
+
         void promoteToPrimitive(Expr n, Type t) {
             visitExpr(n);
-            coerce(typeof(n), t, n.position());
+            coerce(il.currentStack().top(), t, n.position());
         }
 
         public void visit(final Call n) {
@@ -208,7 +454,7 @@ public class X10BytecodeTranslator extends BytecodeTranslator {
             }
             else if (n.target() instanceof TypeNode) {
                 Type[] argTypes = typeofTypes(mi.formalTypes());
-                System.out.println(typeof(mi.container()) + " " + mi.name().toString() + " " + argTypes[0] + (argTypes.length>1?argTypes[1]:""));
+                System.out.println(typeof(mi.container()) + " " + mi.name().toString() + " " + (argTypes.length>0?argTypes[0]:"") + (argTypes.length>1?argTypes[1]:""));
                 if (typeof(mi.container()).equals(Type.STRING)) {
                     System.out.println("true1");
                     if (mi.name().toString().equals("valueOf")) {
@@ -242,7 +488,7 @@ public class X10BytecodeTranslator extends BytecodeTranslator {
                 MethodInstance mi = ts.findMethod(ts.String(), ts.MethodMatcher(ts.String(), Name.make("valueOf"), Collections.singletonList(n.type()), ts.emptyContext()));
                 coerce(typeof(n.type()), typeof(mi.formalTypes().get(0)), n.position());
                 Type[] argTypes = typeofTypes(mi.formalTypes());
-                System.out.println(typeof(mi.container()) + " " + mi.name().toString() + " " + argTypes[0] + (argTypes.length>1?argTypes[1]:""));
+                System.out.println(typeof(mi.container()) + " " + mi.name().toString() + " " + (argTypes.length>0?argTypes[0]:"") + (argTypes.length>1?argTypes[1]:""));
                 if (typeof(mi.container()).equals(Type.STRING)) {
                     System.out.println("true1");
                     if (mi.name().toString().equals("valueOf")) {
@@ -261,6 +507,7 @@ public class X10BytecodeTranslator extends BytecodeTranslator {
         }
 
         protected void coerce(final Type currentTop, final Type newTop, final Position pos) {
+            System.out.println("coerce " + currentTop + " to " + newTop);
             coerce(il, currentTop, newTop, pos);
         }
 
@@ -302,6 +549,7 @@ public class X10BytecodeTranslator extends BytecodeTranslator {
 
     private static Type typeFromPolyglotType(polyglot.types.Type t) {
         boolean boxPrimitives = true;
+        boolean printGenerics = false;
         if (t.isArray())
             return Type.array(typeFromPolyglotType(t.toArray().base()));
         t = X10TypeMixin.baseType(t);
@@ -320,6 +568,9 @@ public class X10BytecodeTranslator extends BytecodeTranslator {
                             break;
                         }
                     }
+                }
+                if (! printGenerics) {
+                    pat = pat.replaceAll("<.*>", "");
                 }
                 System.out.println(cd + " -> " + Type.typeFromClassName(pat));
                 return Type.typeFromClassName(pat);
