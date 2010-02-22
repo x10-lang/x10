c8387a42e13caf3de83212fe43b72859eb992dff
David Grove <groved@us.ibm.com>
2010-02-22 20:42:29 +0000

    Enhance script to examine the files to see if they already have a conformant file header comment before attempting to replace the header with a standardized header.  The motivation is to avoid accidental removal of additional Copyright statements in files that may have non-IBM contributions.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@13003 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.dist/releng/standardizeAllHeaders.sh
+++ x10.dist/releng/standardizeAllHeaders.sh
@@ -20,7 +20,18 @@ for srcdir in \
 do
   echo "Standardizing file headers in $srcdir"
   cd $srcdir
-  find . -name .svn -prune -o -name "*.java" -exec $top/x10.dist/releng/standardizeHeader.pl {} \;
-  find . -name .svn -prune -o -name "*.x10" -exec $top/x10.dist/releng/standardizeHeader.pl {} \;
+  JAVA_FILES=`find . -name .svn -prune -o -name "*.java" -print`
+  X10_FILES=`find . -name .svn -prune -o -name "*.x10" -print`
+  for file in $JAVA_FILES $X10_FILES
+  do
+      hasHeader=yes
+      `grep -q "This file is part of the X10 project (http://x10-lang.org)." $file` || hasHeader=no
+      `grep -q "This file is licensed to You under the Eclipse Public License (EPL)" $file` || hasHeader=no
+      `grep -q "(C) Copyright" $file` || hasHeader=no
+      if [[ "X$hasHeader" == "Xno" ]]; then
+	  echo "Invoking standardizeHeader.pl on $file"
+	  $top/x10.dist/releng/standardizeHeader.pl $file
+      fi
+  done
   cd $top
 done
