ff04abe1915702c9a343db28dea9dc87db562856
Igor Peshansky <igorp@us.ibm.com>
2010-02-26 19:31:50 +0000

    A rewrite of normalizeSVNProperties.sh:     Now can specify directories to normalize as arguments; still defaults to '.' if none     There is less output; only prints the summary information for each property     Improved performance on systems where process spawning is slow     Added comments
    git-svn-id: svn://svn.code.sf.net/p/x10/code@13071 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.dist/releng/normalizeSVNProperties.sh
+++ trunk/x10.dist/releng/normalizeSVNProperties.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 # Author: Dave Grove
+# Author: Igor Peshansky
 #
 # Simple script that can be run across a piece
 # of the X10 repository and ensure that 
 # svn properties are correctly set for a set of standard
 # file types based on their extension.
+#
+# Arguments: directory names to search; defaults to "." if no arguments.
 #
 
 # Source code files should have the following properties set:
@@ -12,12 +15,27 @@
 #   svn:mime-type : text/plain
 # Source code files should not be executable (svn:executable should not be set).
 #
-for extension in .java .c .h .cc .x10; do
-    find . -name .svn -prune -o -type f -name "*$extension" -exec  svn propset svn:eol-style native {} \;
-    find . -name .svn -prune -o -type f -name "*$extension" -exec svn propset svn:mime-type text/plain {} \;
-    find . -name .svn -prune -o -type f -name "*$extension" -exec svn propdel svn:executable {} \;
+
+# Default to current directory if no arguments
+[ "$#" -eq "0" ] && set -- .
+
+# The array of extensions to normalize properties for
+declare -a EXTENSIONS=( .java .c .h .cc .x10 )
+
+# Exit on interrupt
+trap "exit 2" INT
+
+# Compute 'find' arguments to match any of the EXTENSIONS
+declare -a MATCH
+for extension in "${EXTENSIONS[@]}"; do
+    MATCH=("${MATCH[@]}" ${MATCH[*]:+-o} -name "*$extension")
 done
 
-
-
-
+for prop_cmd in "propset -q svn:eol-style native" \
+                "propset -q svn:mime-type text/plain" \
+                "propdel -q svn:executable"
+do
+    echo "Executing 'svn $prop_cmd' on files in $@"
+    find "$@" -name .svn -prune -o -type f \( "${MATCH[@]}" \) -print | \
+        xargs svn $prop_cmd
+done
