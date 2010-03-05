ab87eb6f2eeda02af48ec80918ae69ac86adae0d
Yuki Makino <makino@jp.ibm.com>
2010-03-05 01:26:07 +0000

    Add java specific scheduler for sparating java specific passes from common scheduler, and add a goal to box shared variable for locating shared variable in heap to the sheduler. But x10c command in the mainstream doesn't uses the scheduler yet. I'm going to change to use the scheduler after local test.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@13197 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ trunk/x10.compiler/src/x10c/ExtensionInfo.java
@@ -0,0 +1,57 @@
+/*
+ *  This file is part of the X10 project (http://x10-lang.org).
+ *
+ *  This file is licensed to You under the Eclipse Public License (EPL);
+ *  You may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *      http://www.opensource.org/licenses/eclipse-1.0.php
+ *
+ *  (C) Copyright IBM Corporation 2006-2010.
+ */
+
+package x10c;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import polyglot.ast.NodeFactory;
+import polyglot.ast.SourceFile;
+import polyglot.frontend.AbstractGoal_c;
+import polyglot.frontend.Compiler;
+import polyglot.frontend.Goal;
+import polyglot.frontend.JLScheduler;
+import polyglot.frontend.Job;
+import polyglot.frontend.Scheduler;
+import polyglot.frontend.VisitorGoal;
+import polyglot.types.TypeSystem;
+import x10.optimizations.Optimizer;
+import x10.visit.SharedBoxer;
+import x10.visit.TestNodeVisitor;
+import x10.visit.TestVisitor;
+
+public class ExtensionInfo extends x10.ExtensionInfo {
+    @Override
+    protected Scheduler createScheduler() {
+        return new X10CScheduler(this);
+    }
+
+    static class X10CScheduler extends X10Scheduler {
+        public X10CScheduler(ExtensionInfo extInfo) {
+            super(extInfo);
+        }
+
+        @Override
+        public List<Goal> goals(Job job) {
+            List<Goal> goals = super.goals(job);
+            CodeGenerated(job).addPrereq(Desugarer(job));
+            CodeGenerated(job).addPrereq(SharedBoxed(job));
+            return goals;
+        }
+
+        private Goal SharedBoxed(Job job) {
+            TypeSystem ts = extInfo.typeSystem();
+            NodeFactory nf = extInfo.nodeFactory();
+            return new VisitorGoal("sharedBoxer", job, new SharedBoxer(job, ts, nf)).intern(this);
+        }
+    }
+}
