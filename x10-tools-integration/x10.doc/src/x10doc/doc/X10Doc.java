dfe30721adea6c70632c40e0af150b8b4cd56066
David Grove <groved@us.ibm.com>
2010-03-23 15:20:22 +0000

    Merge through 13503.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/branches@13504 a326200e-df2c-42e0-81fa-597e909af41d

--- x10-tools-integration/x10.doc/src/x10doc/doc/X10Doc.java
+++ x10-tools-integration/x10.doc/src/x10doc/doc/X10Doc.java
@@ -3,6 +3,7 @@ package x10doc.doc;
 import java.text.BreakIterator;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -19,20 +20,26 @@ public class X10Doc implements Doc {
 	private String rawComment;
 	private String comment;
 	X10Tag[] firstSentenceTags, inlineTags;
+	List<Tag> blockTags = new ArrayList<Tag>();
+	List<Tag> paramTags = new ArrayList<Tag>();
 	
-	public X10Doc(String rawComment) {
+	public void processComment(String rawComment) {
 		this.rawComment = rawComment;
-		setComment(rawCommentToText(rawComment));
+		procComment(rawCommentToText(rawComment));
 	}
+	
 
 	/**
 	 * Initializes fields comment, inlineTags of the object
 	 * @param commentText the processed comment text
 	 */
-	public void setComment(String commentText) {
+	private void procComment(String commentText) {
 		// initialize inlineTags
 		ArrayList<Tag> result = new ArrayList<Tag>();
-        Pattern p = Pattern.compile("\\{\\s*@[^\\}]*\\}"); // matches inline tags
+		String noInlineTags = replaceAtSigns(commentText);
+        
+		
+		/*Pattern p = Pattern.compile("\\{\\s*@[^}]*\\}"); // matches inline tags
         // Pattern p = Pattern.compile("\\{\\s*@([^\\s\\}]+)\\s*([^\\}]*)\\s*}"); // matches inline tags
         Matcher m = p.matcher(commentText);
         int start = 0, end = 0;
@@ -48,38 +55,97 @@ public class X10Doc implements Doc {
             String linkText = m.group();
             // System.out.print("String = \"" + commentText.substring(start, end));
             // System.out.println("\"; linkText = \"" + linkText + "\"");
-            result.add(new X10Tag("Text", commentText.substring(start, end), this));
+           // result.add(new X10Tag("Text", commentText.substring(start, end), this));
             result.add(X10Tag.processInlineTag(linkText, this));
+            //int index = commentText.indexOf(linkText);
+            //commentText = commentText.substring(0, index) + commentText.substring(index + linkText.length());
             // result.add(new X10SeeTag(true, linkText, this)); 
             // "true" signifies an @link tag, as opposed to an @see tag 
             start = m.end();
         }
-
-        // initialize comment
-        int blockTagStart = commentText.indexOf("@", start); // start of block tags within comment
-        blockTagStart = (blockTagStart == -1) ? commentText.length() : blockTagStart;
-        this.comment = commentText.substring(0, blockTagStart).trim();
-        result.add(new X10Tag("Text", commentText.substring(start, blockTagStart), this));
-        // System.out.println("X10Doc.initializeFields(): Rest = \"" + commentText.substring(start, blockTagStart) + "\"");
-        
+*/
+       if (!commentText.startsWith("@")){ //make sure that there is a beginning paragraph
+        	// initialize comment
+        	int blockTagStart = noInlineTags.indexOf("@"); // start of block tags within comment
+        	blockTagStart = (blockTagStart == -1) ? commentText.length() : blockTagStart;
+        	this.comment = commentText.substring(0, blockTagStart).trim();
+        	if (!comment.equals("")) {
+        		result.addAll(createInlineTags(comment, this));
+        	}
+        	
+        //}
         // add constraints, if any
         // String decl = declString();
         // if (decl != null) {
         //	result.add(new X10Tag(decl, this));
         // }
         
-        inlineTags = result.toArray(new X10Tag[0]);
+      
+        	// initialize firstSentenceTags
+        	BreakIterator b = BreakIterator.getSentenceInstance();
+        	b.setText(comment);
+        	int start = 0;
+        	int end = 0;
+        	start = b.first(); end = b.next();
+        	String firstSentence = ((start <= end) ? comment.substring(start, end).trim() : "");
+        	// System.out.println("X10Doc.initializeFields(): firstSentence = \"" + firstSentence + "\"");
+        	firstSentenceTags = createInlineTags(firstSentence, this).toArray(new X10Tag[0]);
+        	
+       } else {
+    	   firstSentenceTags = new X10Tag[0];
+       }
+       
+	    inlineTags = result.toArray(new X10Tag[0]);
 
-        // initialize firstSentenceTags
-		BreakIterator b = BreakIterator.getSentenceInstance();
-		b.setText(comment);
-		start = end = 0;
-		start = b.first(); end = b.next();
-		String firstSentence = ((start <= end) ? comment.substring(start, end).trim() : "");
-		// System.out.println("X10Doc.initializeFields(): firstSentence = \"" + firstSentence + "\"");
-		firstSentenceTags = createInlineTags(firstSentence);
-		
-		// TODO: creating Tag objects for block tags and storing them in a field of this object
+	 // TODO: creating Tag objects for block tags and storing them in a field of this object
+		Pattern blockTagPattern = Pattern.compile("\\s*@[^@]*");
+		Matcher blockTagMatcher = blockTagPattern.matcher(noInlineTags);
+	    while (blockTagMatcher.find()) {
+	        String tagText = blockTagMatcher.group();
+	        int start = blockTagMatcher.start();
+	        processBlockTag(commentText.substring(start, start + tagText.length()));
+	    }
+	}
+	
+	//replaces @ signs of inline tags in the comment with another char ("A")
+	private String replaceAtSigns(String comment){
+		String result = comment;
+		Pattern p = Pattern.compile("\\{\\s*@[^}]*\\}"); // matches inline tags
+	    Matcher m = p.matcher(comment);
+	    while (m.find()) {
+	         String linkText = m.group();
+	         int atIndex = linkText.indexOf("@") + m.start();
+	         result = result.substring(0,atIndex) + "A" + result.substring(atIndex+1);
+	    }
+	    return result;
+	}
+	
+	
+	private void processBlockTag(String tagText){
+		String kind = null;
+		String text = null;
+		Pattern p = Pattern.compile("@[^\\s]*");
+		Matcher m = p.matcher(tagText);
+		if (m.find()){
+			kind = m.group().substring(1);
+			text = tagText.substring(m.end()).trim();
+		}
+		if (kind.equals(X10Tag.PARAM)){
+			
+			
+			Pattern p1 = Pattern.compile("[^\\s]*");
+			Matcher m1 = p1.matcher(text);
+			if (m1.find()){
+				String name = m1.group();
+				String paramComment = text.substring(m1.end()).trim();
+				X10Tag[] inTags = createInlineTags(paramComment, this).toArray(new X10Tag[0]);
+				X10ParamTag t = new X10ParamTag(false, name, inTags, paramComment, text , this);
+				blockTags.add(t);
+				paramTags.add(t);
+			}
+		} else {
+			blockTags.add(new X10Tag(kind, text, this));
+		}
 	}
 	
 	// used to add comment lines displaying X10 class declarations, specifically the class constraints in 
@@ -89,7 +155,7 @@ public class X10Doc implements Doc {
 		if (declString == null) {
 			return;
 		}
-		X10Tag[] declTags = createInlineTags(declString);
+		X10Tag[] declTags = createInlineTags(declString,this).toArray(new X10Tag[0]);
 
 		// firstSentenceTags = concat(declTags, firstSentenceTags);
 
@@ -141,16 +207,16 @@ public class X10Doc implements Doc {
 		}
 		String newLine = "<PRE>\n</PRE>" + str;
 		this.rawComment = this.rawComment + newLine;
-		setComment(comment + newLine);
+		processComment(comment + newLine);
 	}
 	
 	/**
 	 * Creates an array of Tag objects of kind "Text" and "@link" for the specified string.
 	 * @param text processed comment text which may contain inline tags, but not block tags
 	 */
-	public X10Tag[] createInlineTags(String text) {
+	public static List<X10Tag> createInlineTags(String text, X10Doc holder) {
 		ArrayList<X10Tag> result = new ArrayList<X10Tag>();
-        Pattern p = Pattern.compile("\\{\\s*@[^\\}]*\\}");
+        Pattern p = Pattern.compile("\\{\\s*@[^}]*\\}");
         Matcher m = p.matcher(text);
         int start = 0, end = 0;
         while (m.find()) {
@@ -158,17 +224,20 @@ public class X10Doc implements Doc {
             String linkText = m.group();
             System.out.print("String = \"" + text.substring(start, end));
             System.out.println("\"; linkText = \"" + linkText + "\"");
-            result.add(new X10Tag("Text", text.substring(start, end), this));
-            result.add(X10Tag.processInlineTag(linkText, this));
+            result.add(new X10Tag("Text", text.substring(start, end), holder));
+            result.add(X10Tag.processInlineTag(linkText, holder));
             // result.add(new X10Tag("Text", text.substring(start, end), this));
             // result.add(new X10SeeTag(true, linkText, this)); 
               // "true" signifies an @link tag, as opposed to an @see tag 
             start = m.end();
+            result.add(new X10Tag("Text", text.substring(start, text.length()), holder));
         }
-        result.add(new X10Tag("Text", text.substring(start, text.length()), this));
-        // System.out.println("Rest = \"" + text.substring(start, text.length()) + "\"");
         
-        return result.toArray(new X10Tag[0]);
+        // System.out.println("Rest = \"" + text.substring(start, text.length()) + "\"");
+        if (result.isEmpty()){
+        	result.add(new X10Tag("Text", text, holder));
+        }
+        return result;
 	}
 
 	public String commentText() {
@@ -286,7 +355,7 @@ public class X10Doc implements Doc {
 		if (X10RootDoc.printSwitch)
 			System.out.println("Doc.setRawCommentText(String) called for "+name());
 		this.rawComment = arg0;
-		setComment(rawCommentToText(rawComment));
+		processComment(rawComment);
 	}
 	
 	/**
@@ -295,22 +364,36 @@ public class X10Doc implements Doc {
 	public Tag[] tags() {
 		if (X10RootDoc.printSwitch)
 			System.out.println("Doc.tags() called for "+name());
-		return inlineTags();
+		Tag[] result = new Tag[blockTags.size() + inlineTags.length];
+		if (blockTags.size() > 0)
+			System.arraycopy(blockTags.toArray(new Tag[0]), 0, result, 0, blockTags.size());
+		if (inlineTags.length > 0)
+			System.arraycopy(inlineTags, 0, result, blockTags.size(), inlineTags.length);
+		return result;
+		//return inlineTags();
 	}
 
 	/**
 	 * Return tags of the specified kind in this Doc item.
 	 */
-	public Tag[] tags(String arg0) {
+	public Tag[] tags(String kind) {
 		// TODO Auto-generated method stub
 		if (X10RootDoc.printSwitch)
-			System.out.println("Doc.tags(" + arg0 + ") called for "+name());
-		if (arg0.equals("Text")) { 
+			System.out.println("Doc.tags(" + kind + ") called for "+name());
+		if (kind.equals("Text")) { 
 			return inlineTags();
-		}
-		else return new Tag[0];
+		} else return getTags(kind); 
 	}
 
+	private Tag[] getTags(String kind){
+		List<Tag> result = new ArrayList<Tag>();
+		for(Tag t: blockTags){
+			if (t.kind().equals(kind)){
+				result.add(t);
+			}
+		}
+		return result.toArray(new X10Tag[0]);
+	}
 	public static boolean isIncluded(String accessModFilter, ProgramElementDoc pd) {
 		boolean isPublic = pd.isPublic();
 		if (accessModFilter.equals("-public")) {
@@ -364,4 +447,5 @@ public class X10Doc implements Doc {
 		}
 		return r;
 	}
+	
 }
