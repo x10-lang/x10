849d2d28b599517f12a8de911c86dd18d78d0963
Mark Stephenson <user@domain>
2010-03-30 23:48:18 +0000

    Added accessors to the CompilerDelegate from the ParseController and to the ExtensionInfo from the CompilerDelegate.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@13597 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt/branches/X10v15/org.eclipse.imp.x10dt.ui/src/org/eclipse/imp/x10dt/ui/parser/CompilerDelegate.java
+++ x10dt/branches/X10v15/org.eclipse.imp.x10dt.ui/src/org/eclipse/imp/x10dt/ui/parser/CompilerDelegate.java
@@ -1,14 +1,14 @@
 /*******************************************************************************
-* Copyright (c) 2008 IBM Corporation.
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Eclipse Public License v1.0
-* which accompanies this distribution, and is available at
-* http://www.eclipse.org/legal/epl-v10.html
-*
-* Contributors:
-*    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
+ * Copyright (c) 2008 IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
 
-*******************************************************************************/
+ *******************************************************************************/
 
 /*
  * (C) Copyright IBM Corporation 2007
@@ -52,141 +52,176 @@ import x10.parser.X10Lexer;
 import x10.parser.X10Parser;
 
 public class CompilerDelegate {
-    private ExtensionInfo extInfo;
-    private PolyglotFrontEnd fe;
+	private ExtensionInfo extInfo;
+	private PolyglotFrontEnd fe;
 
-    private final IJavaProject x10Project;
+	private final IJavaProject x10Project;
 
-    public X10Lexer getLexer() { return extInfo.getLexer(); }
-    public X10Parser getParser() { return extInfo.getParser(); }
-    public Job getJob(Source source) { return extInfo.getJob(source); }
-    public PolyglotFrontEnd getFrontEnd() { return fe; }
-
-    CompilerDelegate(Monitor monitor, IMessageHandler handler, IProject project) {
-	this.x10Project= (project != null) ? JavaCore.create(project) : null;
-
-        extInfo= new org.eclipse.imp.x10dt.ui.parser.ExtensionInfo(monitor, new MessageHandlerAdapter(handler)); // new ExtensionInfo(monitor);
-        buildOptions(extInfo);
-        ErrorQueue eq= new SilentErrorQueue(100, "stderr");
-        fe = new PolyglotFrontEnd(extInfo, eq);
-        Report.setQueue(eq);
-    }
-
-    /**
-     * @return a list of all project-relative CPE_SOURCE-type classpath entries.
-     * @throws JavaModelException
-     */
-    private List<IPath> getProjectSrcPath() throws JavaModelException {
-        List<IPath> srcPath= new ArrayList<IPath>();
-
-        if (this.x10Project == null)
-            return srcPath;
-
-        IClasspathEntry[] classPath= x10Project.getResolvedClasspath(true);
-
-        for(int i= 0; i < classPath.length; i++) {
-            IClasspathEntry e= classPath[i];
-
-            if (e.getEntryKind() == IClasspathEntry.CPE_SOURCE)
-                srcPath.add(e.getPath());
-        }
-        if (srcPath.size() == 0)
-            srcPath.add(x10Project.getProject().getLocation());
-        return srcPath;
-    }
-
-    private String pathListToPathString(List<IPath> pathList) {
-        StringBuffer buff= new StringBuffer();
-        IWorkspaceRoot wsRoot= ResourcesPlugin.getWorkspace().getRoot();
-        IPath wsPath= wsRoot.getLocation();
-
-        for(Iterator<IPath> iter= pathList.iterator(); iter.hasNext(); ) {
-            IPath path= iter.next();
-            IProject projectRef= wsRoot.getProject(path.segment(0));
-
-            if (projectRef != null && projectRef.exists()) {
-                // This is a workspace-relative path, but the project may not actually
-                // live inside the workspace, so use its actual location as the prefix
-                // for the rest of the specified path.
-                buff.append(projectRef.getLocation().append(path.removeFirstSegments(1)).toOSString());
-            } else if (x10Project.getProject().exists(path)) {
-                buff.append(x10Project.getProject().getLocation().append(path).toOSString());
-            } else {
-                buff.append(path.toOSString());
-            }
-            if (iter.hasNext())
-                buff.append(File.pathSeparatorChar);
-        }
-        return buff.toString();
-    }
-
-    private void buildOptions(ExtensionInfo extInfo) {
-	Options opts= extInfo.getOptions();
-
-	Options.global= opts;
-	try {
-            List<IPath> projectSrcLoc= getProjectSrcPath();
-            String projectSrcPath= pathListToPathString(projectSrcLoc);
-	    opts.parseCommandLine(new String[] { "-assert", "-noserial", "-cp", buildClassPathSpec(), "-sourcepath", projectSrcPath }, new HashSet());
-	} catch (UsageError e) {
-	    if (!e.getMessage().equals("must specify at least one source file"))
-		System.err.println(e.getMessage());
-	} catch (JavaModelException e) {
-            X10UIPlugin.getInstance().writeErrorMsg("Unable to obtain resolved class path: " + e.getMessage());
+	public polyglot.frontend.ExtensionInfo getExtensionInfo() {
+		return extInfo;
 	}
-//	X10UIPlugin.getInstance().maybeWriteInfoMsg("Source path = " + opts.source_path);
-//	X10UIPlugin.getInstance().maybeWriteInfoMsg("Class path = " + opts.classpath);
-    }
 
-    private String buildClassPathSpec() {
-        StringBuffer buff= new StringBuffer();
-        // RMF 8/2/2006 - Determine whether an X10 runtime is on the project classpath,
-        // and if not, silently add the default X10 runtime, so that various IDE services
-        // can still run (e.g. syntax highlighting, outlining, etc.).
-        boolean hasRuntime= false;
-        boolean runtimeValid= false;
+	public X10Lexer getLexer() {
+		return extInfo.getLexer();
+	}
 
-        try {
-            IClasspathEntry[] classPath= (x10Project != null) ? x10Project.getResolvedClasspath(true) : new IClasspathEntry[0];
+	public X10Parser getParser() {
+		return extInfo.getParser();
+	}
 
-            for(int i= 0; i < classPath.length; i++) {
-                IClasspathEntry entry= classPath[i];
-                final String entryPath= entry.getPath().toOSString();
+	public Job getJob(Source source) {
+		return extInfo.getJob(source);
+	}
 
-                if (i > 0)
-                    buff.append(File.pathSeparatorChar);
-                buff.append(entryPath);
+	public PolyglotFrontEnd getFrontEnd() {
+		return fe;
+	}
 
-                if (entryPath.contains("x10.runtime")) {
-                    hasRuntime= true;
-                    if (new File(entryPath).exists())
-                        runtimeValid= true;
-                }
-            }
-            if (!hasRuntime || !runtimeValid) {
-                if (buff.length() > 0)
-                    buff.append(File.pathSeparatorChar);
-                buff.append(getRuntimePath());
-            }
-            // if (X10Preferences.autoAddRuntime) {
-            //    String commonPath= X10Plugin.x10CommonPath;
-            //    String runtimePath= commonPath.substring(0, commonPath.lastIndexOf(File.separator) + 1) + "x10.runtime" + File.separator + "classes";
-            //
-            //    if (classPath.length > 0)
-            //        buff.append(';');
-            //    buff.append(runtimePath);
-            // }
-        } catch (JavaModelException e) {
-            X10Plugin.getInstance().writeErrorMsg("Error resolving class path: " + e.getMessage());
-        }
-        return buff.toString();
-    }
+	CompilerDelegate(Monitor monitor, IMessageHandler handler, IProject project) {
+		this.x10Project = (project != null) ? JavaCore.create(project) : null;
 
-    private String getRuntimePath() {
-        Bundle x10RuntimeBundle= Platform.getBundle("x10.runtime");
-        String bundleVersion= (String) x10RuntimeBundle.getHeaders().get("Bundle-Version");
-        String x10RuntimePath= Platform.getInstallLocation().getURL().getPath() + "plugins/x10.runtime_" + bundleVersion + ".jar";
+		extInfo = new org.eclipse.imp.x10dt.ui.parser.ExtensionInfo(monitor,
+				new MessageHandlerAdapter(handler)); // new
+														// ExtensionInfo(monitor);
+		buildOptions(extInfo);
+		ErrorQueue eq = new SilentErrorQueue(100, "stderr");
+		fe = new PolyglotFrontEnd(extInfo, eq);
+		Report.setQueue(eq);
+	}
 
-        return x10RuntimePath;
-    }
+	/**
+	 * @return a list of all project-relative CPE_SOURCE-type classpath entries.
+	 * @throws JavaModelException
+	 */
+	private List<IPath> getProjectSrcPath() throws JavaModelException {
+		List<IPath> srcPath = new ArrayList<IPath>();
+
+		if (this.x10Project == null)
+			return srcPath;
+
+		IClasspathEntry[] classPath = x10Project.getResolvedClasspath(true);
+
+		for (int i = 0; i < classPath.length; i++) {
+			IClasspathEntry e = classPath[i];
+
+			if (e.getEntryKind() == IClasspathEntry.CPE_SOURCE)
+				srcPath.add(e.getPath());
+		}
+		if (srcPath.size() == 0)
+			srcPath.add(x10Project.getProject().getLocation());
+		return srcPath;
+	}
+
+	private String pathListToPathString(List<IPath> pathList) {
+		StringBuffer buff = new StringBuffer();
+		IWorkspaceRoot wsRoot = ResourcesPlugin.getWorkspace().getRoot();
+		IPath wsPath = wsRoot.getLocation();
+
+		for (Iterator<IPath> iter = pathList.iterator(); iter.hasNext();) {
+			IPath path = iter.next();
+			IProject projectRef = wsRoot.getProject(path.segment(0));
+
+			if (projectRef != null && projectRef.exists()) {
+				// This is a workspace-relative path, but the project may not
+				// actually
+				// live inside the workspace, so use its actual location as the
+				// prefix
+				// for the rest of the specified path.
+				buff.append(projectRef.getLocation().append(
+						path.removeFirstSegments(1)).toOSString());
+			} else if (x10Project.getProject().exists(path)) {
+				buff.append(x10Project.getProject().getLocation().append(path)
+						.toOSString());
+			} else {
+				buff.append(path.toOSString());
+			}
+			if (iter.hasNext())
+				buff.append(File.pathSeparatorChar);
+		}
+		return buff.toString();
+	}
+
+	private void buildOptions(ExtensionInfo extInfo) {
+		Options opts = extInfo.getOptions();
+
+		Options.global = opts;
+		try {
+			List<IPath> projectSrcLoc = getProjectSrcPath();
+			String projectSrcPath = pathListToPathString(projectSrcLoc);
+			opts.parseCommandLine(new String[] { "-assert", "-noserial", "-cp",
+					buildClassPathSpec(), "-sourcepath", projectSrcPath },
+					new HashSet());
+		} catch (UsageError e) {
+			if (!e.getMessage().equals("must specify at least one source file"))
+				System.err.println(e.getMessage());
+		} catch (JavaModelException e) {
+			X10UIPlugin.getInstance().writeErrorMsg(
+					"Unable to obtain resolved class path: " + e.getMessage());
+		}
+		// X10UIPlugin.getInstance().maybeWriteInfoMsg("Source path = " +
+		// opts.source_path);
+		// X10UIPlugin.getInstance().maybeWriteInfoMsg("Class path = " +
+		// opts.classpath);
+	}
+
+	private String buildClassPathSpec() {
+		StringBuffer buff = new StringBuffer();
+		// RMF 8/2/2006 - Determine whether an X10 runtime is on the project
+		// classpath,
+		// and if not, silently add the default X10 runtime, so that various IDE
+		// services
+		// can still run (e.g. syntax highlighting, outlining, etc.).
+		boolean hasRuntime = false;
+		boolean runtimeValid = false;
+
+		try {
+			IClasspathEntry[] classPath = (x10Project != null) ? x10Project
+					.getResolvedClasspath(true) : new IClasspathEntry[0];
+
+			for (int i = 0; i < classPath.length; i++) {
+				IClasspathEntry entry = classPath[i];
+				final String entryPath = entry.getPath().toOSString();
+
+				if (i > 0)
+					buff.append(File.pathSeparatorChar);
+				buff.append(entryPath);
+
+				if (entryPath.contains("x10.runtime")) {
+					hasRuntime = true;
+					if (new File(entryPath).exists())
+						runtimeValid = true;
+				}
+			}
+			if (!hasRuntime || !runtimeValid) {
+				if (buff.length() > 0)
+					buff.append(File.pathSeparatorChar);
+				buff.append(getRuntimePath());
+			}
+			// if (X10Preferences.autoAddRuntime) {
+			// String commonPath= X10Plugin.x10CommonPath;
+			// String runtimePath= commonPath.substring(0,
+			// commonPath.lastIndexOf(File.separator) + 1) + "x10.runtime" +
+			// File.separator + "classes";
+			//
+			// if (classPath.length > 0)
+			// buff.append(';');
+			// buff.append(runtimePath);
+			// }
+		} catch (JavaModelException e) {
+			X10Plugin.getInstance().writeErrorMsg(
+					"Error resolving class path: " + e.getMessage());
+		}
+		return buff.toString();
+	}
+
+	private String getRuntimePath() {
+		Bundle x10RuntimeBundle = Platform.getBundle("x10.runtime");
+		String bundleVersion = (String) x10RuntimeBundle.getHeaders().get(
+				"Bundle-Version");
+		String x10RuntimePath = Platform.getInstallLocation().getURL()
+				.getPath()
+				+ "plugins/x10.runtime_" + bundleVersion + ".jar";
+
+		return x10RuntimePath;
+	}
 }
--- x10dt/branches/X10v15/org.eclipse.imp.x10dt.ui/src/org/eclipse/imp/x10dt/ui/parser/ParseController.java
+++ x10dt/branches/X10v15/org.eclipse.imp.x10dt.ui/src/org/eclipse/imp/x10dt/ui/parser/ParseController.java
@@ -1,14 +1,14 @@
 /*******************************************************************************
-* Copyright (c) 2008 IBM Corporation.
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Eclipse Public License v1.0
-* which accompanies this distribution, and is available at
-* http://www.eclipse.org/legal/epl-v10.html
-*
-* Contributors:
-*    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
+ * Copyright (c) 2008 IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
 
-*******************************************************************************/
+ *******************************************************************************/
 
 /*
  * (C) Copyright IBM Corporation 2007
@@ -31,80 +31,92 @@ import org.eclipse.imp.parser.IMessageHandler;
 import org.eclipse.imp.parser.IParser;
 import org.eclipse.imp.parser.ISourcePositionLocator;
 import org.eclipse.imp.parser.SimpleLPGParseController;
-import org.eclipse.imp.services.IAnnotationTypeInfo;
 import org.eclipse.imp.services.ILanguageSyntaxProperties;
 import org.eclipse.imp.x10dt.core.X10Plugin;
-import org.eclipse.imp.x10dt.core.builder.StreamSource;
-import org.eclipse.imp.x10dt.ui.X10UIPlugin;
 
 import polyglot.ast.Node;
 import polyglot.frontend.FileSource;
 import polyglot.frontend.Job;
 
 public class ParseController extends SimpleLPGParseController {
-    private CompilerDelegate fCompiler;
-    private PMMonitor fMonitor;
+	private CompilerDelegate fCompiler;
+	private PMMonitor fMonitor;
 
-    public ParseController() {
-	super(X10Plugin.kLanguageName);
-    }
+	public ParseController() {
+		super(X10Plugin.kLanguageName);
+	}
 
-    public IParser getParser() {
-	return new ParserDelegate(fCompiler.getParser());
-    }
+	public CompilerDelegate getCompilerDelegate() {
+		return fCompiler;
+	}
 
-    public ILexer getLexer() {
-	return new LexerDelegate(fCompiler.getLexer());
-    }
+	public IParser getParser() {
+		return new ParserDelegate(fCompiler.getParser());
+	}
 
-    public ISourcePositionLocator getSourcePositionLocator() {
-	return new PolyglotNodeLocator(fProject, null /*getLexer().getLexStream()*/);
-    }
+	public ILexer getLexer() {
+		return new LexerDelegate(fCompiler.getLexer());
+	}
 
-    public ILanguageSyntaxProperties getSyntaxProperties() {
-        return new X10SyntaxProperties();
-    }
+	public ISourcePositionLocator getSourcePositionLocator() {
+		return new PolyglotNodeLocator(fProject, null /* getLexer().getLexStream() */);
+	}
 
-    @Override
-    public void initialize(IPath filePath, ISourceProject project, IMessageHandler handler) {
-        super.initialize(filePath, project, handler);
-        fMonitor= new PMMonitor(null);
-    }
+	public ILanguageSyntaxProperties getSyntaxProperties() {
+		return new X10SyntaxProperties();
+	}
 
-    public Object parse(String contents, IProgressMonitor monitor) {
-        FileSource fileSource= null;
-        try {
-            fMonitor.setMonitor(monitor);
-            
-            String path= fFilePath.toOSString();
-            File file= new File(fProject != null ? fProject.getRawProject().getLocation().append(fFilePath).toString() : path);
+	@Override
+	public void initialize(IPath filePath, ISourceProject project,
+			IMessageHandler handler) {
+		super.initialize(filePath, project, handler);
+		fMonitor = new PMMonitor(null);
+	}
 
-            fileSource= new StringSource(contents, file, path);
-            
-            List<FileSource> streams= new ArrayList<FileSource>();
-            // Bug 526: NPE when opening a file outside the workspace due to null fProject.
-            IProject proj= (fProject != null) ? fProject.getRawProject() : null;
-            
-            streams.add(fileSource); // PC: just to test...
-            fCompiler= new CompilerDelegate(fMonitor, handler, proj); // Create the compiler
-            fCompiler.getFrontEnd().compile(streams);
-        } catch (IOException e) {
-            throw new Error(e);
-        } finally {
-            // RMF 8/2/2006 - retrieve the AST if there is one; some later phase of compilation
-            // may fail, even though the AST is well-formed enough to provide an outline.
-            if (fileSource != null) {
-                Job job= fCompiler.getJob(fileSource);
-                if (job != null) {
-                    fCurrentAst= (Node) job.ast();
-                }
-            }
-            // RMF 8/2/2006 - cacheKeywordsOnce() must have been run for syntax highlighting to work.
-            // Must do this after attempting parsing (even though that might fail), since it depends
-            // on the parser/lexer being set in the ExtensionInfo, which only happens as a result of
-            // ExtensionInfo.parser(). Ugghh.
-            cacheKeywordsOnce();
-        }
-        return fCurrentAst;
-    }
+	public Object parse(String contents, IProgressMonitor monitor) {
+		FileSource fileSource = null;
+		try {
+			fMonitor.setMonitor(monitor);
+
+			String path = fFilePath.toOSString();
+			File file = new File(fProject != null ? fProject.getRawProject()
+					.getLocation().append(fFilePath).toString() : path);
+
+			fileSource = new StringSource(contents, file, path);
+
+			List<FileSource> streams = new ArrayList<FileSource>();
+			// Bug 526: NPE when opening a file outside the workspace due to
+			// null fProject.
+			IProject proj = (fProject != null) ? fProject.getRawProject()
+					: null;
+
+			streams.add(fileSource); // PC: just to test...
+			fCompiler = new CompilerDelegate(fMonitor, handler, proj); // Create
+																		// the
+																		// compiler
+			fCompiler.getFrontEnd().compile(streams);
+		} catch (IOException e) {
+			throw new Error(e);
+		} finally {
+			// RMF 8/2/2006 - retrieve the AST if there is one; some later phase
+			// of compilation
+			// may fail, even though the AST is well-formed enough to provide an
+			// outline.
+			if (fileSource != null) {
+				Job job = fCompiler.getJob(fileSource);
+				if (job != null) {
+					fCurrentAst = (Node) job.ast();
+				}
+			}
+			// RMF 8/2/2006 - cacheKeywordsOnce() must have been run for syntax
+			// highlighting to work.
+			// Must do this after attempting parsing (even though that might
+			// fail), since it depends
+			// on the parser/lexer being set in the ExtensionInfo, which only
+			// happens as a result of
+			// ExtensionInfo.parser(). Ugghh.
+			cacheKeywordsOnce();
+		}
+		return fCurrentAst;
+	}
 }
