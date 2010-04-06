7dcba0fe24da9b5e2c532e6752d16691ec908afd
ANJU KAMBADUR <pkambadu@us.ibm.com>
2010-04-06 15:12:41 +0000

    rng_nextrand() is almost useless. The way RNG is used is that everytime a new child node is created, its random number generator is spawned from its parent's value --- using rng_spawn()
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@13671 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.tests/examples/Benchmarks/UTS/Sha1Rand.cpp
+++ x10.tests/examples/Benchmarks/UTS/Sha1Rand.cpp
@@ -1,33 +1,46 @@
 #include "sha1.h"
 
 struct Sha1Rand {
-  private:
-  RNG_state internal_state; 
-  struct true_type {};
-  struct false_type {};
+  RNG_state internal_state; /* internal state for the RNG */
 
-  int get_rand (false_type) { return rng_rand (&internal_state); }
-
-  int get_rand (true_type) { return rng_nextrand (&internal_state); }
-
-  public:
+  /*
+   * Constructor --- takes in the initial seed value
+   */
   Sha1Rand (const int& seed) { rng_init (&internal_state, seed); }
 
-  int operator()(const bool reset=false) {
-    return ((false==reset) ? get_rand (false_type()) :
-                             get_rand (true_type()));
+  /*
+   * *Almost* a copy constructor --- takes in a spawn number in addition 
+   * to modifying the original Sha1Rand parameter.
+   */
+  Sha1Rand (Sha1Rand& other, const int& spawn_number) : internal_state (0) { 
+    rng_spawn (&(other.internal_state), &internal_state, spawn_number); 
   }
+
+  /*
+   * Return the random number stored in the current object
+   */
+  int operator()(void) { return rng_rand (&internal_state); }
 };
 
 #ifdef SHA1_TEST
 #include <iostream>
 int main (int argc, char** argv) {
   Sha1Rand rgen (0);
-  for (int i=0; i<10; ++i) std::cout << rgen(true) << std::endl;
+  std::cout << "Initial value: " << rgen() << std::endl;
+  for (int i=0; i<10; ++i) {
+    Sha1Rand ngen (rgen, i);
+    std::cout << i << ":" << rgen() << ", " << ngen() << std::endl;
+  }
 
   RNG_state mystate;
   rng_init (&mystate, 0);
-  for (int i=0; i<10; ++i) std::cout << rng_nextrand(&mystate) << std::endl;
+  std::cout << "Initial value: " << rng_rand(&mystate) << std::endl;
+  for (int i=0; i<10; ++i) {
+    RNG_state newstate;
+    rng_spawn (&mystate, &newstate, i);
+    std::cout << i << ":" << rng_rand(&mystate) << ", "
+              << rng_rand(&newstate) << std::endl;
+  }
 
   return 0;
 }
