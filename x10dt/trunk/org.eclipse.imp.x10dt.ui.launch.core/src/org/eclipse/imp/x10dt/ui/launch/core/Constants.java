b4ba471cf0f99685d64c3ad143f395d745729363
Emmanuel Geay <egeay@us.ibm.com>
2010-04-07 15:03:57 +0000

    First improvement into the compilation/launch mechanism in X10 with C++ back-end by introducing SWT forms.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@13688 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt/trunk/org.eclipse.imp.x10dt.ui.launch.core/src/org/eclipse/imp/x10dt/ui/launch/core/Constants.java
+++ x10dt/trunk/org.eclipse.imp.x10dt.ui.launch.core/src/org/eclipse/imp/x10dt/ui/launch/core/Constants.java
@@ -16,20 +16,21 @@ import org.eclipse.core.runtime.QualifiedName;
  */
 @SuppressWarnings("all")
 public final class Constants {
+  
+  public static final String EMPTY_STR = ""; //$NON-NLS-1$
 
   // --- Constants for Launch Configuration
   
   public static final String ATTR_SHOULD_LINK_APP = LaunchCore.PLUGIN_ID + ".ShouldLinkApp"; //$NON-NLS-1$
   
-  public static final String ATTR_MAIN_TYPE_PATH = LaunchCore.PLUGIN_ID + ".MainTypePath"; //$NON-NLS-1$
+  public static final String ATTR_X10_MAIN_CLASS = LaunchCore.PLUGIN_ID + ".X10MainClass"; //$NON-NLS-1$
   
-  // --- Constants for connection info for a build
+  public static final String ATTR_MAIN_CPP_FILE_PATH = LaunchCore.PLUGIN_ID + ".MainCppFilePath"; //$NON-NLS-1$
   
-  public static final QualifiedName RES_MANAGER_ID = new QualifiedName(LaunchCore.PLUGIN_ID, "resource.manager"); //$NON-NLS-1$
-  public static final QualifiedName WORKSPACE_DIR = new QualifiedName(LaunchCore.PLUGIN_ID, "target.workspace-dir"); //$NON-NLS-1$
-  public static final QualifiedName X10_PLATFORM_CONF = new QualifiedName(LaunchCore.PLUGIN_ID, "x10.platform.conf"); //$NON-NLS-1$
+  // --- Constant for X10 builder
+  
   public static final QualifiedName EXEC_PATH = new QualifiedName(LaunchCore.PLUGIN_ID, "exec.path"); //$NON-NLS-1$
-
+  
   // --- Plugin ID
   
   public static final String X10_DIST_PLUGIN_ID = "x10.dist.host"; //$NON-NLS-1$
