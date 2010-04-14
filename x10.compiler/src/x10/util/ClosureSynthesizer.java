799988548e1036149aae75710818b6884b8fc52a
Vijay Saraswat <vsaraswa@us.ibm.com>
2010-04-14 02:54:54 +0000

    Roll back some changes introduced in r13830 so that regressions can pass.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@13854 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.compiler/src/x10/util/ClosureSynthesizer.java
+++ x10.compiler/src/x10/util/ClosureSynthesizer.java
@@ -195,10 +195,6 @@ public class ClosureSynthesizer {
             return ct.x10Def();
         }
 
-        // We are going to construct our own list of formal names, using the 
-        // name, flags and position of arguments in formalNames, but supplying our own
-        // formal types.
-        List<LocalDef> myFormalNames = new ArrayList<LocalDef>(formalNames.size());
         X10ClassDef cd = (X10ClassDef) new X10ClassDef_c(xts, null) {
         	@Override
         	public boolean isFunction() { 
@@ -239,10 +235,7 @@ public class ClosureSynthesizer {
 
         for (int i = 0; i < numValueParams; i++) {
             ParameterType t = new ParameterType_c(xts, pos, Name.make("Z" + (i + 1)), Types.ref(cd));
-            Ref<ParameterType> ref = Types.ref(t);
-            argTypes.add(ref);
-            LocalDef formal = formalNames.get(i);
-            myFormalNames.add(xts.localDef(formal.position(), formal.flags(), ref, formal.name()));
+            argTypes.add(Types.ref(t));
             cd.addTypeParameter(t, ParameterType.Variance.CONTRAVARIANT);
         }
 
@@ -260,7 +253,7 @@ public class ClosureSynthesizer {
         // NOTE: don't call cd.asType() until after the type parameters are
         // added.
         FunctionType ct = (FunctionType) cd.asType();
-        xts.systemResolver().install(fullName, (FunctionType) ct.copy());
+        xts.systemResolver().install(fullName, ct);
 
         String fullNameWithThis = fullName + "#this";
         //String fullNameWithThis = "this";
@@ -276,7 +269,7 @@ public class ClosureSynthesizer {
         		typeParams, 
         		argTypes, 
         		thisVar,
-        		myFormalNames, 
+        		formalNames, 
         		guard, 
         		null, 
         		Collections.EMPTY_LIST, 
