b2862890e8c90fe41506363ac4b8cfbbda0c0854
Olivier Tardieu <tardieu@us.ibm.com>
2010-05-18 15:23:21 +0000

    -qarch=auto -qtune=auto for aix
    git-svn-id: svn://svn.code.sf.net/p/x10/code@14197 a326200e-df2c-42e0-81fa-597e909af41d

--- benchmarks/trunk/hpcc/arch/aix.mk
+++ benchmarks/trunk/hpcc/arch/aix.mk
@@ -4,7 +4,7 @@
 
 # AIX with XLC
 #
-export POSTCOMPILE_NATIVE_LIB_FLAGS ?= -O3 -q64 -qarch=pwr5 -qtune=pwr5 -qhot -qinline -qrtti=all -DNDEBUG
+export POSTCOMPILE_NATIVE_LIB_FLAGS ?= -O3 -q64 -qarch=auto -qtune=auto -qhot -qinline -qrtti=all -DNDEBUG
 
 export BLAS_LIB		?= -lessl
 
@@ -15,7 +15,7 @@ export USE_MEDIUM_PAGES ?= 1
 #export POSTCOMPILE_CXX		?= mpCC
 #export POSTCOMPILE_CXXFLAGS 	?= -w -q64 -DTRANSPORT=${PGASTRANSPORT}
 
-#export POSTCOMPILE_OPTFLAGS	?= -O3 -qarch=pwr5 -qtune=pwr5 -qhot -qinline -qrtti=all -DNDEBUG -DNO_CHECKS
+#export POSTCOMPILE_OPTFLAGS	?= -O3 -qarch=auto -qtune=auto -qhot -qinline -qrtti=all -DNDEBUG -DNO_CHECKS
 
 export POSTCOMPILE_AR		?= ar
 export POSTCOMPILE_ARFLAGS	?= -X64 -rv
