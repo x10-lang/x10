c2b1fc8a1fb16600ef6d4ab46572eaef7ab4e345
Olivier Tardieu <tardieu@us.ibm.com>
2010-05-21 21:28:15 +0000

    replace loop nest with blas routine
    git-svn-id: svn://svn.code.sf.net/p/x10/code@14253 a326200e-df2c-42e0-81fa-597e909af41d

--- benchmarks/trunk/hpcc/LU/rc7/essl_natives.cc
+++ benchmarks/trunk/hpcc/LU/rc7/essl_natives.cc
@@ -11,7 +11,10 @@ extern "C"
 void dgemm_(const char *, const char *, int *, int *, int *, double *, void *,
 	   int *, void *, int *, double *, void *, int *);
 
-
+#ifdef __cplusplus
+extern "C"
+#endif
+void dger_(int *, int *, double *, double *, int *, double *, int *, double *, int *);
 
 extern "C" {
 
@@ -57,6 +60,33 @@ void blockMulSubRow (double     * me,
     {
       double div0 = 1.0 / me[B*j+j];
       for (i = j+1; i < B; i++)
+        {
+          me[i*B+j] *= div0;
+          for (k=j+1; k<B; k++) me[i*B+k] -= me[i*B+j]*me[j*B+k];
+        }
+    }
+  else
+    {
+      int one = 1;
+      double div0 = 1.0 / diag[j];
+      for (i=0; i<B; i++) me[i*B+j] *= div0;
+      double alpha = -1.0;
+      int n = B-j-1;
+      int m = B;
+      double * a = me + j + 1;
+      double * x = me + j;
+      double * d = diag + j + 1;
+      dger_ (&n, &m, &alpha, d, &one, x, &B, a, &B);
+    }
+}
+/*
+{
+  int i, k;
+
+  if (cond)
+    {
+      double div0 = 1.0 / me[B*j+j];
+      for (i = j+1; i < B; i++)
 	{
 	  me[i*B+j] *= div0;
 	  for (k=j+1; k<B; k++) me[i*B+k] -= me[i*B+j]*me[j*B+k];
@@ -72,7 +102,7 @@ void blockMulSubRow (double     * me,
 	}
     }
 }
-
+*/
 #if TRANSPORT == bgp
 void _xliltrm() { }
 void flush_ () { }
