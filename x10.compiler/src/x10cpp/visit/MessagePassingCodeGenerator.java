f2e2b492ff96f0c17e6fb070941a4dda3c7d2db4
David Grove <groved@us.ibm.com>
2010-06-02 22:09:22 +0000

    Workaround problem discovered by Vijay where the C++ backend failed to initialize blank instance fields of X10 struct types that don't correspond to Java built-in primitives.  The workaround is to explicitly call memset in the _make method of classes to zero the storage allocated by x10aux::alloc.
    Intentionally didn't insert the memset inside of x10aux::alloc because
    in most cases we don't need to zero initialize the storage, so we don't
    want to pay for a redundant initialization.
    
    Opened XTENLANG-1407 to remind us to revisit this code once we
    have specified the language such that we can get a definite
    assignment (or a definite default value) for all fields in the 
    object. The combination of that and proto should allow us to
    enable the memset as unnecessary.
    
    
    
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@14375 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.compiler/src/x10cpp/visit/MessagePassingCodeGenerator.java
+++ x10.compiler/src/x10cpp/visit/MessagePassingCodeGenerator.java
@@ -2061,8 +2061,11 @@ public class MessagePassingCodeGenerator extends X10DelegatingVisitor {
 	            b.write(CONSTRUCTOR+"(this_");
 	            if (!dec.formals().isEmpty()) b.write(", ");
 	        } else {
+	            // XTENLANG-1407: Remove this memset call once we finish the default value specification/implementation.
+	            //                Expect it to be more efficient to explicitly initialize all of the object fields instead
+	            //                of first calling memset, then storing into most of the fields a second time.
 	            b.write(make_ref(typeName)+" this_ = "+
-	                    "new (x10aux::alloc"+chevrons(typeName)+"()) "+typeName+"();"); b.newline();
+	                    "new (memset(x10aux::alloc"+chevrons(typeName)+"(), 0, sizeof("+typeName+"))) "+typeName+"();"); b.newline();
 	                    b.write("this_->"+CONSTRUCTOR+"(");
 	        }
 	        for (Iterator<Formal> i = dec.formals().iterator(); i.hasNext(); ) {
