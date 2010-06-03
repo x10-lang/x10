41ea8db74d844ade1e9fc0f1c2337ac27b1a8ecf
Baolin Shao <blshao@us.ibm.com>
2010-06-03 15:14:29 +0000

    analysis code added
    git-svn-id: svn://svn.code.sf.net/p/x10/code/branches@14389 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ scalable-finish/finish-analysis/src/x10/finish/analysis/Test.java
@@ -0,0 +1,41 @@
+package x10.finish.analysis;
+
+import java.io.*;
+
+public class Test {
+
+	public static void main(String[] args) throws Exception {
+	    
+		/* ***********************************************************************************
+		 * /home/blshao/workspace/wala-bridge-1.0/x10.runtime/src-x10/x10/lang/
+		 * /Users/blshao/workspace/wala-bridge-1.0/x10.runtime/src-x10/x10/lang/Future.x10
+		 * /home/blshao/workspace/wala-bridge-1.0/test.x10/Mytest.x10
+		 * /home/blshao/workspace/wala-bridge-1.0/x10.runtime/src-x10/x10/array/PolyScanner.x10
+		 * "/blshao/workspace/wala-bridge-1.0/test.x10/RuntimeTest.x10"
+	    	 ***************************************************************************************/
+	    String os = System.getProperty("os.name");
+	    String user = "Users";
+	    if(os.contains("Linux")){
+		user = "home";
+		
+	    }
+
+	    File f = new File("/"+user+"/blshao/workspace/wala-bridge-1.0/test.x10/TrivialTest2.x10");
+	    X10FinishAsyncAnalysis x10fa = new X10FinishAsyncAnalysis();
+	 
+			/* *********************************
+			 *  
+			 * examples of methods' signatures
+			 *  
+			 *  "","main","x10/lang/Rail;"
+			 *  "x10/lang/","run",""
+			 *  "x10/array/", "foo", "()V"
+			 *  "x10/lang/","foo","()V"
+			 ************************************/
+		
+		//compile(file, package, entrymethod, methodsig)
+		x10fa.compile(f,"x10/lang/","foo","()V" );
+
+
+	}
+}
---
+++ scalable-finish/finish-analysis/src/x10/finish/table/CallTableVal.java
@@ -0,0 +1,88 @@
+package x10.finish.table;
+import java.io.Serializable;
+
+
+
+public abstract class CallTableVal  implements Serializable {
+	/**
+	 * automatically added for serialization
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * An object of CallTableVal represents an instance of method invocation in an x10 program, and 
+	 * the arity of an invocation means whether this invocation is in any loops or if-else 
+	 * One: this invocation can be and only be called once, thus not in any loop or if-else
+	 * ZeOrOne: this invocation is within in at least one if-else branch, but not in any loops
+	 * Unbounded: this invocation is within in at least one loop, possibly in a if-else
+	 * @author blshao
+	 *
+	 */
+	public enum Arity {One, ZerOrOne, Unbounded}
+	public final String scope;
+	public Arity a;
+	public int blk;
+	public CallTableKey aslast;
+	public Arity getArity(){
+	    return a;
+	}
+	
+	public CallTableVal(String s, int b){
+	    scope = renameMethod(s);
+	    blk = b;
+	    aslast = null;
+	    a = Arity.One;
+	}
+	public CallTableVal(String s, int b, Arity a){
+	    scope = renameMethod(s);
+	    blk = b;
+	    aslast = null;
+	    this.a = a;
+	}
+	/*
+	 * when set a new arity to this object, it must check its
+	 * current arity. For example, if current invocation is within
+	 * an loop and you want to set it to ZerOrOne, because you find 
+	 * this invocation is also in a branch. The final arity will still be
+	 * unbounded. 
+	 */
+	public void setArity(Arity tmparity){
+	    /* One -> lowest priority
+	     * Unbounded -> highest priority
+	     */
+	    switch(a){
+	    case One:	
+		a = tmparity;
+		break;
+	    case ZerOrOne: 
+		switch(tmparity){
+		case One: 
+		   break;
+		case ZerOrOne:
+		    break;
+		case Unbounded:
+		    a = tmparity;
+		}
+		break;
+	    case Unbounded:
+		break;
+	    }
+	}
+	
+	public String renameMethod(String name) {
+	    if (name.contains("activity") && (name.contains(">"))) {
+		int first_colon = name.indexOf(':');
+		int second_colon = name.indexOf(':', first_colon + 1);
+		String file_name = name.substring(first_colon + 1, second_colon);
+		int last = name.indexOf('>');
+		String line = name.substring(second_colon + 1, last);
+		int hashed_name = file_name.hashCode();
+		return "activity"+file_name+":"+line;
+		//return "activity" + String.valueOf(hashed_name) + ":" + line;
+	    }
+	    return name;
+	}
+	public abstract int hashCode();
+	public abstract String toString();
+	public abstract boolean equals(Object o);
+}
