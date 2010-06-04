fdff1722970fe8f63c5da1430f2f2e599df736bd
Baolin Shao <blshao@us.ibm.com>
2010-06-04 15:35:10 +0000

    git-svn-id: svn://svn.code.sf.net/p/x10/code@14418 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ branches/scalable-finish/x10.compiler/src/x10/ast/RegionMaker.java
@@ -0,0 +1,21 @@
+/*
+ *
+ * (C) Copyright IBM Corporation 2006-2008
+ *
+ *  This file is part of X10 Language.
+ *
+ */
+/**
+ * 
+ */
+package x10.ast;
+
+import polyglot.ast.Call;
+
+/** An immutable representation of int:int region constructor.
+ * @author vj
+ *
+ */
+public interface RegionMaker extends Call {
+
+}
---
+++ branches/scalable-finish/x10.compiler/src/x10cpp/postcompiler/Linux_CXXCommandBuilder.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package x10cpp.postcompiler;
+
+import java.util.ArrayList;
+
+import polyglot.main.Options;
+import polyglot.util.ErrorQueue;
+
+public class Linux_CXXCommandBuilder extends CXXCommandBuilder {
+    protected static final boolean USE_X86 = CXXCommandBuilder.PLATFORM.endsWith("_x86");
+    protected static final boolean USE_BFD = System.getenv("USE_BFD")!=null;
+ 
+    public Linux_CXXCommandBuilder(Options options, ErrorQueue eq) {
+        super(options, eq);
+        assert (CXXCommandBuilder.PLATFORM.startsWith("linux_"));
+    }
+
+    protected boolean gcEnabled() { return true; }
+
+    protected void addPreArgs(ArrayList<String> cxxCmd) {
+        super.addPreArgs(cxxCmd);
+        cxxCmd.add("-Wno-long-long");
+        cxxCmd.add("-Wno-unused-parameter");
+        cxxCmd.add("-pthread");
+        if (USE_X86) {
+            cxxCmd.add("-msse2");
+            cxxCmd.add("-mfpmath=sse");
+        }
+    }
+
+    protected void addPostArgs(ArrayList<String> cxxCmd) {
+        super.addPostArgs(cxxCmd);
+        cxxCmd.add("-Wl,-export-dynamic");
+        cxxCmd.add("-lrt");
+        if (USE_BFD) {
+            cxxCmd.add("-lbfd");
+            cxxCmd.add("-liberty");
+        }
+    }
+}
