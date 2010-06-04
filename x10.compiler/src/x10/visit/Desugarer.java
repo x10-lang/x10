46f726f36b4e1dcdc3ce4c1f035d5c820671b715
Yan Li <liyancrl@cn.ibm.com>
2010-06-04 11:08:07 +0000

    Code gen of collecting Finish
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@14410 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.compiler/src/x10/visit/Desugarer.java
+++ x10.compiler/src/x10/visit/Desugarer.java
@@ -68,6 +68,7 @@ import x10.ast.Closure;
 import x10.ast.Closure_c;
 import x10.ast.DepParameterExpr;
 import x10.ast.Finish;
+import x10.ast.FinishExpr;
 import x10.ast.ForEach;
 import x10.ast.FunctionTypeNode;
 import x10.ast.Future;
@@ -94,10 +95,12 @@ import x10.constraint.XConstraint;
 import x10.constraint.XVar;
 import x10.emitter.Emitter;
 import x10.types.ClosureDef;
+import x10.types.ConstrainedType;
 import x10.types.X10ClassType;
 import x10.types.X10ConstructorInstance;
 import x10.types.X10Context;
 import x10.types.X10MethodInstance;
+import x10.types.X10ParsedClassType;
 import x10.types.X10TypeMixin;
 import x10.types.X10TypeSystem;
 import x10.types.X10TypeSystem_c;
@@ -203,6 +206,8 @@ public class Desugarer extends ContextVisitor {
             return visitCast((X10Cast_c) n);
         if (n instanceof X10Instanceof_c)
             return visitInstanceof((X10Instanceof_c) n);
+        if (n instanceof LocalDecl)
+            return visitLocalDecl((LocalDecl) n);    
         return n;
     }
 
@@ -569,7 +574,114 @@ public class Desugarer extends ContextVisitor {
 
         return xnf.Try(pos, tryBlock, Collections.singletonList(catchBlock), finallyBlock);
     }
+    // finish (R)S; ->
+    //    val finishva1 = new x10.lang.Runtime.CollectingFinish(new test.TotalsReducer1());
+    //    try { S; }
+    //    catch (t:Throwable) { Runtime.pushException(t); }
+    //    finally { left = finishva1.stopFinish(); }
+    
+    private Stmt visitFinishExpr(Assign n, LocalDecl l) throws SemanticException {
+    	FinishExpr f = null;
+    	if((l==null)&&(n!=null)){
+    		f = (FinishExpr)n.right();
+    	}
+    	if((n==null)&&(l!=null)){
+    		f = (FinishExpr)l.init();
+    	}
+    	        	
+        Position pos = f.position();
+        Expr reducer = f.reducer();
+        
+        //Begin Try Block Code
+        Type reducerType = reducer.type();                          
+        if (reducerType instanceof ConstrainedType) {
+    		ConstrainedType ct = (ConstrainedType) reducerType;
+    		reducerType = X10TypeMixin.baseType(Types.get(ct.baseType()));    		
+        }         
+                
+        CanonicalTypeNode CCE = xnf.CanonicalTypeNode(pos, reducerType);        
+        X10ConstructorInstance ni = xts.findConstructor(reducerType, xts.ConstructorMatcher(reducerType, Collections.EMPTY_LIST, context));        
+        Expr newReducer = xnf.New(pos, CCE, Collections.EMPTY_LIST).constructorInstance(ni).type(reducerType);
 
+        
+        //Parse out T
+        Type reducerTarget = null;
+        for (Type t : xts.interfaces(reducerType)) {     	
+        	for (Type tt : ((X10ParsedClassType)t).typeArguments()) {
+        		reducerTarget = tt;
+        	}        	
+        }
+              
+        Type coFinish =  xts.load("x10.lang.Runtime.CollectingFinish");
+        Type coFinishT = (((X10ParsedClassType)coFinish).typeArguments(Collections.singletonList(reducerTarget)));
+        CanonicalTypeNode TTE = xnf.CanonicalTypeNode(pos, coFinishT);
+        X10ConstructorInstance ti = xts.findConstructor(coFinishT, xts.ConstructorMatcher(coFinishT, Collections.singletonList(reducerType), context));        
+        Expr newCF = xnf.New(pos, TTE, Collections.singletonList(newReducer)).constructorInstance(ti).type(coFinishT);
+
+        Name tmp1 = Name.make("finishR");  
+        LocalDef lDef1 = xts.localDef(pos, xts.NoFlags(), Types.ref(coFinishT), tmp1);        
+        Expr local1 = xnf.Local(pos, xnf.Id(pos, tmp1)).localInstance(lDef1.asInstance()).type(coFinishT);       
+        LocalDecl localDecl = xnf.LocalDecl(pos, xnf.FlagsNode(pos, xts.NoFlags()), xnf.CanonicalTypeNode(pos, coFinishT), xnf.Id(pos, tmp1)).localDef(lDef1);
+        
+        Expr a = xnf.Assign(pos, local1, Assign.ASSIGN, newCF).type(coFinishT);        
+        Stmt s1 = xnf.Eval(pos, a);         
+ 
+        //Traverse the body 
+        LocalOfferVisitor ldVisitor = new LocalOfferVisitor(local1,xContext(),xts,xnf,synth);
+        Block fbody = xnf.Block(pos, f.body());             
+        Block newfbody = (Block)fbody.visit(ldVisitor);                      
+        Block tryBlock = xnf.Block(pos,newfbody);                  
+        
+
+        //Begin catch block 
+        Name tmp2 = Name.make("Throwvar");
+        MethodInstance mi = xts.findMethod(xts.Runtime(),
+                xts.MethodMatcher(xts.Runtime(), PUSH_EXCEPTION, Collections.singletonList(xts.Throwable()), context));
+        LocalDef lDef = xts.localDef(pos, xts.NoFlags(), Types.ref(xts.Throwable()), tmp2);
+        Formal formal = xnf.Formal(pos, xnf.FlagsNode(pos, xts.NoFlags()),
+                xnf.CanonicalTypeNode(pos, xts.Throwable()), xnf.Id(pos, tmp2)).localDef(lDef);
+        Expr local = xnf.Local(pos, xnf.Id(pos, tmp2)).localInstance(lDef.asInstance()).type(xts.Throwable());
+        Expr call = xnf.X10Call(pos, xnf.CanonicalTypeNode(pos, xts.Runtime()),
+                xnf.Id(pos, PUSH_EXCEPTION), Collections.EMPTY_LIST,
+                Collections.singletonList(local)).methodInstance(mi).type(xts.Void());
+        Catch catchBlock = xnf.Catch(pos, formal, xnf.Block(pos, xnf.Eval(pos, call)));
+          
+        
+        //Begin finally block
+        Name STOPFINISHEXPR = Name.make("stopFinishExpr"); 
+        Stmt returnS = null;
+        if((l==null)&&(n!=null)){
+        	Expr left = n.left(xnf).type(reducerTarget);              
+            Call instanceCall = synth.makeInstanceCall(pos, local1, STOPFINISHEXPR, Collections.EMPTY_LIST, Collections.EMPTY_LIST, reducerTarget, Collections.EMPTY_LIST, xContext());
+            Expr b = xnf.Assign(pos, left, Assign.ASSIGN, instanceCall).type(reducerTarget);  
+            returnS = xnf.Eval(pos, b);                      
+      	}
+        if((n==null)&&(l!=null)){                         	
+            Expr local2 = xnf.Local(pos, l.name()).localInstance(l.localDef().asInstance()).type(coFinishT);       
+        	Call instanceCall = synth.makeInstanceCall(pos, local1, STOPFINISHEXPR, Collections.EMPTY_LIST, Collections.EMPTY_LIST, reducerTarget, Collections.EMPTY_LIST, xContext());
+         	Expr b = xnf.Assign(pos, local2, Assign.ASSIGN, instanceCall).type(reducerTarget);  
+            returnS = xnf.Eval(pos, b);                	                            
+         }
+        
+        Block finalBlock = xnf.Block(pos, returnS);
+        return xnf.Block(pos, localDecl,s1, xnf.Try(pos, tryBlock, Collections.singletonList(catchBlock), finalBlock));
+    }
+    
+    private Stmt visitLocalDecl(LocalDecl n) throws SemanticException {
+        if (n.init() instanceof FinishExpr) {
+      	    Assign x = null;
+      	    Position pos = n.position();
+      	    ArrayList<Stmt> sList = new ArrayList<Stmt>();
+      	    LocalDecl localDec = xnf.LocalDecl(pos, n.flags(),n.type() , n.name()).localDef(n.localDef());
+      	    sList.add(localDec);
+      	    Stmt s = visitFinishExpr(x,n);
+      	    sList.add(s);
+      		return  xnf.StmtSeq(pos, sList);    		
+      	}
+      	   
+      	return n;
+      	
+      }
     // foreach (p in D) S; -> for (p in D) async S;
     private Stmt visitForEach(ForEach f) throws SemanticException {
         Position pos = f.position();
@@ -751,6 +863,14 @@ public class Desugarer extends ContextVisitor {
     // x++; -> ++x; or x--; -> --x; (to avoid creating an extra closure)
     private Stmt visitEval(Eval n) throws SemanticException {
         Position pos = n.position();
+        if (n.expr() instanceof Assign) {
+        	Assign f = (Assign) n.expr();
+        	Expr right = f.right();
+        	Expr left = f.left(xnf);
+        	LocalDecl l = null;
+        	if (right instanceof FinishExpr)
+        		return visitFinishExpr(f,l);        		
+        }
         if (n.expr() instanceof X10Unary_c) {
             X10Unary_c e = (X10Unary_c) n.expr();
             Position ePos = e.position();
