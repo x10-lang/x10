1af331ab56dfdef61d54e968aa77480c0d10ce9c
David Grove <groved@us.ibm.com>
2010-06-04 04:00:21 +0000

    XTENLANG-1398: Implement Byte,toString() and parse()
    patch, including test case, from Josh.
    
    
    
    git-svn-id: svn://svn.code.sf.net/p/x10/code@14404 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.runtime/src-cpp/x10aux/byte_utils.cc
+++ trunk/x10.runtime/src-cpp/x10aux/byte_utils.cc
@@ -7,21 +7,40 @@
  *      http://www.opensource.org/licenses/eclipse-1.0.php
  *
  *  (C) Copyright IBM Corporation 2006-2010.
+ *  (C) Copyright Australian National University 2010.
  */
 
 #include <x10aux/byte_utils.h>
 #include <x10aux/basic_functions.h>
 
-#include <x10/lang/String.h>                    \
+#include <x10/lang/String.h>
 
 using namespace x10::lang;
 using namespace std;
 using namespace x10aux;
 
 const ref<String> x10aux::byte_utils::toString(x10_byte value, x10_int radix) {
-    (void) value; (void) radix;
-    assert(false); /* FIXME: STUBBED NATIVE */
-    return null;
+    assert(radix>=2);
+    assert(radix<=16);
+    static char numerals[] = { '0', '1', '2', '3', '4', '5', '6', '7',  
+                               '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+    // worst case is binary -- needs 8 digits and a '\0'
+    char buf[9] = ""; //zeroes entire buffer (S6.7.8.21)
+    x10_int value2;
+    if (value < 0) {
+        value2 = 0x80 - (value & 0x7F);
+    } else {
+        value2 = value;
+    }
+    char *b;
+    // start on the '\0', will predecrement so will not clobber it
+    for (b=&buf[8] ; value2>0 ; value2/=radix) {
+        *(--b) = numerals[value2 % radix];
+    }
+    if (value < 0) {
+        *(--b) = '-';
+    }
+    return String::Steal(alloc_printf("%s",b));
 }
 
 const ref<String> x10aux::byte_utils::toHexString(x10_byte value) {
@@ -41,15 +60,15 @@ const ref<String> x10aux::byte_utils::toString(x10_byte value) {
 }
 
 x10_byte x10aux::byte_utils::parseByte(const ref<String>& s, x10_int radix) {
-    (void) s;
-    assert(false); /* FIXME: STUBBED NATIVE */
-    return radix; /* Bogus, but use radix to avoid warning about unused parameter */
+    nullCheck(s);
+    char* dummy;
+    return strtol(s.operator->()->c_str(), &dummy, radix);
 }
 
 x10_byte x10aux::byte_utils::parseByte(const ref<String>& s) {
-    (void) s;
-    assert(false); /* FIXME: STUBBED NATIVE */
-    return 0; 
+    nullCheck(s);
+    // FIXME: NumberFormatException?
+    return atoi(nullCheck(s)->c_str());
 }
 
 // vim:tabstop=4:shiftwidth=4:expandtab
