5eccbd812d5b5894f107596ea2d6459cfc077ae5
David Grove <groved@us.ibm.com>
2010-06-07 21:47:04 +0000

    bug fixes.  now actually works...
    git-svn-id: svn://svn.code.sf.net/p/x10/code@14473 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.dist/releng/masterBuildRelease.sh
+++ trunk/x10.dist/releng/masterBuildRelease.sh
@@ -46,10 +46,10 @@ do
     ssh $host "(cd /tmp; ./buildRelease.sh -dir /tmp/x10-rc-$USER -version $version -tag $tag)"
     ssh $host "(cd /tmp; rm ./buildRelease.sh)"
     echo "transfering file from $host to localhost"
-    scp "$host:/tmp/x10-rc-$USER/x10/x10.dist/x10-rc*.tgz" .
+    scp "$host:/tmp/x10-rc-$USER/x10-$version/x10.dist/x10-$version*.tgz" .
     echo "transfering from localhost to orquesta"
-    scp x10-rc_*.tgz orquesta.watson.ibm.com:/var/www/localhost/htdocs/x10dt/x10-rc-builds/$version
-    rm x10-rc_*.tgz
+    scp x10-$version*.tgz orquesta.watson.ibm.com:/var/www/localhost/htdocs/x10dt/x10-rc-builds/$version
+    rm x10-$version*.tgz
     #ssh $host rm -rf /tmp/x10-rc-$USER 
 done
 
