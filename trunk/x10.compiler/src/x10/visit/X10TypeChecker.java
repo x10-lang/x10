6991d8a61618663270a8cce073ad3931af8b48a8
Igor Peshansky <igorp@us.ibm.com>
2010-06-12 01:59:00 +0000

    Fix for XTENLANG-1416 and XTENLANG-1383/XTENLANG-1375 WIP:    The typechecker occasionally uses the pattern "typecheck a synthesized node, and try something else if it    fails".  This pattern depends on exceptions being thrown by the typechecker.  The cases where this pattern    is used now have to explicitly set the "throwExceptions" bit in the typechecker used to typecheck the    synthetic nodes.  In all other cases the typechecker will report the error and proceed with fake type    information.    Thus, in normal typechecking situations, the typechecker will not throw any exceptions, and the    TypeChecked goal will not fail.  This allows compilation to proceed, both for the current file and for    the other, possibly independent files.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@14569 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.compiler/src/x10/visit/X10TypeChecker.java
+++ trunk/x10.compiler/src/x10/visit/X10TypeChecker.java
@@ -1,8 +1,17 @@
-/**
- * 
+/*
+ *  This file is part of the X10 project (http://x10-lang.org).
+ *
+ *  This file is licensed to You under the Eclipse Public License (EPL);
+ *  You may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *      http://www.opensource.org/licenses/eclipse-1.0.php
+ *
+ *  (C) Copyright IBM Corporation 2006-2010.
  */
+
 package x10.visit;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import polyglot.ast.Node;
@@ -13,6 +22,7 @@ import polyglot.types.SemanticException;
 import polyglot.types.TypeSystem;
 import polyglot.util.ErrorInfo;
 import polyglot.util.Position;
+import polyglot.visit.ContextVisitor;
 import polyglot.visit.NodeVisitor;
 import polyglot.visit.TypeChecker;
 import x10.errors.Errors;
@@ -23,17 +33,24 @@ import x10.errors.Errors;
  */
 public class X10TypeChecker extends TypeChecker {
 
-	  Map<Node,Node> memo;
+	Map<Node,Node> memo;
+	/**
+	 * @param job
+	 * @param ts
+	 * @param nf
+	 */
+	private X10TypeChecker(Job job, TypeSystem ts, NodeFactory nf) {
+	    this(job, ts, nf, new HashMap<Node, Node>());
+	}
 	/**
 	 * @param job
 	 * @param ts
 	 * @param nf
 	 * @param memo
 	 */
-	  public X10TypeChecker(Job job, TypeSystem ts, NodeFactory nf,
-				Map<Node, Node> memo) {
-		  this(job, ts, nf, memo, false);
-	  }
+	public X10TypeChecker(Job job, TypeSystem ts, NodeFactory nf, Map<Node, Node> memo) {
+	    this(job, ts, nf, memo, false);
+	}
 	public X10TypeChecker(Job job, TypeSystem ts, NodeFactory nf,
 			Map<Node, Node> memo, boolean isFragmentChecker) {
 		super(job, ts, nf, memo);
@@ -74,6 +91,16 @@ public class X10TypeChecker extends TypeChecker {
 	    }
 	}
 
+	private boolean throwExceptions = false;
+	public boolean throwExceptions() {
+	    return throwExceptions;
+	}
+	public X10TypeChecker throwExceptions(boolean val) {
+	    X10TypeChecker tc = (X10TypeChecker) copy();
+	    tc.throwExceptions = val;
+	    return tc;
+	}
+
 	protected NodeVisitor enterCall(Node n) throws SemanticException {
 	    try {
 	        return super.enterCall(n);
@@ -95,5 +122,11 @@ public class X10TypeChecker extends TypeChecker {
 	    }
 	    // continue, errors have been reported, maybe you will find more errors.
 	    return old;
+	}
+
+	public static X10TypeChecker getTypeChecker(ContextVisitor tc) {
+	    return (X10TypeChecker)
+	        (tc instanceof X10TypeChecker ? tc :
+	            new X10TypeChecker(tc.job(), tc.typeSystem(), tc.nodeFactory()).context(tc.context()));
 	}
 }
