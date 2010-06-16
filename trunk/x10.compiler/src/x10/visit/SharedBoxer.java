38bd70ce4c08f65662ae62b46b48086a30f5a855
Yuki Makino <makino@jp.ibm.com>
2010-06-16 06:34:25 +0000

    Fix for handling shared variable in Java backend
    git-svn-id: svn://svn.code.sf.net/p/x10/code@14597 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.compiler/src/x10/visit/SharedBoxer.java
+++ trunk/x10.compiler/src/x10/visit/SharedBoxer.java
@@ -10,9 +10,7 @@
  */
 package x10.visit;
 
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
 
 import polyglot.ast.Call;
@@ -34,21 +32,14 @@ import polyglot.util.InternalCompilerError;
 import polyglot.util.Position;
 import polyglot.visit.ContextVisitor;
 import polyglot.visit.NodeVisitor;
-import x10.ast.X10Binary_c;
-import x10.ast.X10Call_c;
-import x10.ast.X10Conditional_c;
-import x10.ast.X10If_c;
 import x10.ast.X10LocalAssign_c;
 import x10.ast.X10LocalDecl_c;
 import x10.ast.X10Local_c;
-import x10.ast.X10New_c;
 import x10.ast.X10NodeFactory;
-import x10.ast.X10Return_c;
 import x10.types.X10ClassType;
 import x10.types.X10Flags;
 import x10.types.X10MethodInstance;
 import x10.types.X10TypeSystem;
-import x10.util.Synthesizer;
 
 public class SharedBoxer extends ContextVisitor {
     private static final QName SHARED = QName.make("x10.compiler.Shared");
@@ -62,6 +53,7 @@ public class SharedBoxer extends ContextVisitor {
         xnf = (X10NodeFactory) nf;
     }
     
+    @Override
     protected Node leaveCall(Node parent, Node old, Node n, NodeVisitor v) throws SemanticException {
         
         // shared var *** = +++ -> val Share[typeof***] *** = new Shared[typeof***](+++)
@@ -73,13 +65,14 @@ public class SharedBoxer extends ContextVisitor {
             return visitLocalAssign((X10LocalAssign_c) n);
         
         // *** not above case && (*** = ) -> ***.get()
-        if (!(parent instanceof X10LocalAssign_c) && n instanceof X10Local_c) {
-            X10Local_c local = (X10Local_c) n;
-            if(X10Flags.toX10Flags(local.flags()).isShared()) {
-                return createWrappingCall(local);
+        if (n instanceof X10Local_c) {
+            if (!(parent instanceof X10LocalAssign_c && X10Flags.toX10Flags(((X10LocalAssign_c) parent).local().flags()).isShared())) {
+                X10Local_c local = (X10Local_c) n;
+                if(X10Flags.toX10Flags(local.flags()).isShared()) {
+                    return createWrappingCall(local);
+                }
             }
         }
-        
         return n;
     }
     
@@ -114,22 +107,23 @@ public class SharedBoxer extends ContextVisitor {
             // if b is also shared, a += b -> a += b.get()
             if (right instanceof X10Local_c) {
                 X10Local_c local2 = (X10Local_c) right;
-                if (X10Flags.toX10Flags(local2.flags()).isShared())
-                right = createWrappingCall(local2);
+                if (X10Flags.toX10Flags(local2.flags()).isShared()) {
+                    right = createWrappingCall(local2);
+                }
             }
             
             X10MethodInstance mi;
             try {
                 Type type = xts.typeForName(SHARED);
                 List<Type> typeArguments = ((X10ClassType) type).typeArguments();
-                mi = (X10MethodInstance) xts.findMethod(type, xts.MethodMatcher(right.type(), Name.make("set"), typeArguments, context));
+                mi = xts.findMethod(type, xts.MethodMatcher(right.type(), Name.make("set"), typeArguments, context));
             } catch (SemanticException e) {
                 throw new InternalCompilerError("Something is terribly wrong", e);
             }
             if (bo == null) { // i.e. =         a = b -> a.set(b)
-                return (Call) xnf.Call(position, local, xnf.Id(position, "set"), right).methodInstance(mi).type(right.type());
+                return xnf.Call(position, local, xnf.Id(position, "set"), right).methodInstance(mi).type(right.type());
             } else {          // i.e. += *= ... a += b -> a.set(a.get() + b)
-                return (Call) xnf.Call(position, local, xnf.Id(position, "set"), xnf.Binary(position, createWrappingCall(local), bo, right))
+                return xnf.Call(position, local, xnf.Id(position, "set"), xnf.Binary(position, createWrappingCall(local), bo, right).type(right.type()))
                 .methodInstance(mi).type(right.type());
             }
         }
@@ -140,7 +134,7 @@ public class SharedBoxer extends ContextVisitor {
         X10MethodInstance mi;
         try {
             Type type = xts.typeForName(SHARED);
-            mi = (X10MethodInstance) xts.findMethod(type, xts.MethodMatcher(local.type(), Name.make("get"), Collections.EMPTY_LIST, context));
+            mi = xts.findMethod(type, xts.MethodMatcher(local.type(), Name.make("get"), Collections.EMPTY_LIST, context));
         } catch (SemanticException e) {
             throw new InternalCompilerError("Something is terribly wrong", e);
         }
