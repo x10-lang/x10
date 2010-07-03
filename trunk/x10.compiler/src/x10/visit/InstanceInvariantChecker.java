d5c5c425e5305d0d936b287ddbe25a224ea8aed3
Mandana Vaziri <mvaziri@us.ibm.com>
2010-07-03 22:34:27 +0000

    x10.compiler: Added a check in InstanceInvariantChecker to check that ASTs have no ambiguous nodes after type checking. Modified both InstanceInvariantChecker and PositionInvariantChecker to add invariant violations as "Internal_Error" rather than "Warning" to the error queue.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@14745 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.compiler/src/x10/visit/InstanceInvariantChecker.java
+++ trunk/x10.compiler/src/x10/visit/InstanceInvariantChecker.java
@@ -1,5 +1,8 @@
 package x10.visit;
 
+import polyglot.ast.AmbAssign;
+import polyglot.ast.Ambiguous;
+import polyglot.ast.Assign_c;
 import polyglot.ast.FlagsNode_c;
 import polyglot.ast.Node;
 import polyglot.ast.LocalDecl;
@@ -16,6 +19,7 @@ import polyglot.visit.NodeVisitor;
 import polyglot.frontend.Job;
 import polyglot.main.Report;
 import x10.ast.AnnotationNode_c;
+import x10.ast.DepParameterExpr;
 import x10.ast.X10Formal_c;
 import x10.ast.Closure;
 import x10.ast.AssignPropertyBody;
@@ -40,13 +44,25 @@ public class InstanceInvariantChecker extends NodeVisitor
         } catch (AssertionError e) {
             String msg = e.getMessage()+("!")+
                     (" n=")+(n).toString();
-            job.compiler().errorQueue().enqueue(ErrorInfo.WARNING,msg,n.position());
+            job.compiler().errorQueue().enqueue(ErrorInfo.INTERNAL_ERROR,msg,n.position());
         }
         return this;
     }
+    
+    private boolean isAmbiguous(Node n){
+    	if (n instanceof Ambiguous && !(n instanceof DepParameterExpr)
+    			&&(!(n instanceof Assign_c) || n instanceof AmbAssign)) {
+    			return true;
+    	}
+    	return false;
+    }
+    
     private void checkInvariants(Node n) {
         assert (n != null) : "Cannot visit null";
 
+        if (isAmbiguous(n))
+        	assert false : "Ambiguous node found in AST";
+        
         if (n instanceof Typed) {
             assert ((Typed)n).type()!=null;
         }
