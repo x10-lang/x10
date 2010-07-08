31b91f84b168b27c7f2fa0b29bcbddecbc476665
Yuki Makino <makino@jp.ibm.com>
2010-07-08 05:46:12 +0000

    Fix NPE
    git-svn-id: svn://svn.code.sf.net/p/x10/code@14773 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.compiler/src/x10c/visit/RailInLoopOptimizer.java
+++ trunk/x10.compiler/src/x10c/visit/RailInLoopOptimizer.java
@@ -121,7 +121,7 @@ public class RailInLoopOptimizer extends ContextVisitor {
 
             // targets to privatize
             final List<Pair<BackingArray, Boolean>> targetAndIsFinals = new ArrayList<Pair<BackingArray, Boolean>>();
-            final Map<BackingArray, Id> backingArrayToId = new HashMap<BackingArray, Id>(); 
+            final Map<BackingArray, Id> backingArrayToId = new HashMap<BackingArray, Id>();
 
             // to merge privatization statement
             Stmt visited1 = (Stmt) loop.body().visit(new NodeVisitor() {
@@ -243,7 +243,7 @@ public class RailInLoopOptimizer extends ContextVisitor {
                                 return xnf.BackingArrayAccess(pos, xnf.Local(pos, id).localInstance(ldef.asInstance()).type(target.type()), index, type);
                             }
                             LocalDef ldef = xts.localDef(n.position(), xts.NoFlags(), Types.ref(type), id.id());
-                            return xnf.BackingArrayAccessAssign(pos, xnf.Local(pos, id).localInstance(ldef.asInstance()), index, Assign.ASSIGN, elem);
+                            return xnf.BackingArrayAccessAssign(pos, xnf.Local(pos, id).localInstance(ldef.asInstance()), index, Assign.ASSIGN, elem).type(type);
                         }
                     }
                     if (n instanceof SettableAssign_c) {
@@ -294,7 +294,7 @@ public class RailInLoopOptimizer extends ContextVisitor {
                                 ba = xnf.BackingArray(n.position(), id, createArrayType(type), array);
                             }
                             LocalDef ldef = xts.localDef(n.position(), xts.NoFlags(), Types.ref(type), id.id());
-                            return xnf.BackingArrayAccessAssign(n.position(), xnf.Local(n.position(), id).localInstance(ldef.asInstance()), ((SettableAssign_c) n).index().get(0), ((SettableAssign_c) n).operator(), ((SettableAssign_c) n).right());
+                            return xnf.BackingArrayAccessAssign(n.position(), xnf.Local(n.position(), id).localInstance(ldef.asInstance()), ((SettableAssign_c) n).index().get(0), ((SettableAssign_c) n).operator(), ((SettableAssign_c) n).right()).type(type);
                         }
                     }
                     // rail = Rail.make(10) -> rail = Rail.make(10); railvaluexxx = (int[]) rail.value;
@@ -528,7 +528,7 @@ public class RailInLoopOptimizer extends ContextVisitor {
                                     targetAndIsFinals.add(new Pair<BackingArray, Boolean>(jna, true));
                                 }
                                 LocalDef ldef = xts.localDef(n.position(), xts.NoFlags(), Types.ref(type), id.id());
-                                return xnf.BackingArrayAccessAssign(n.position(), xnf.Local(n.position(), id).localInstance(ldef.asInstance()).type(type), ((SettableAssign_c) n).index().get(0), ((SettableAssign_c) n).operator(), ((SettableAssign_c) n).right());
+                                return xnf.BackingArrayAccessAssign(n.position(), xnf.Local(n.position(), id).localInstance(ldef.asInstance()).type(type), ((SettableAssign_c) n).index().get(0), ((SettableAssign_c) n).operator(), ((SettableAssign_c) n).right()).type(type);
                             }
                         }
                         return n;
