c54d3c06aebfa63dcb904e2777a1c5b3136b6532
Robert Fuhrer <rfuhrer@us.ibm.com>
2010-07-12 15:32:38 +0000

    Build scripts now check exit codes in a few key places. packageCPPRelease.sh now has platform-specific lists of things that belong in lib/, to avoid errors from the tar command. buildToolIntegration.sh now talks in terms of platforms that map onto hosts, rather than talking directly about hosts. masterBuildToolIntegration.sh now runs the remote builds in parallel.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@14807 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.dist/releng/masterBuildToolIntegration.sh
+++ x10.dist/releng/masterBuildToolIntegration.sh
@@ -1,44 +1,160 @@
 #!/bin/bash
 
 # Dave Grove
+# Robert M. Fuhrer
 
-hosts="condor triloka3 nashira"
+# Following are the platforms for which we need to build a platform-specific tarball
+# for use with the X10DT. These tarballs are only used for "local" builds, i.e., on
+# the same host as the one running the X10DT. So we only need such tarballs for
+# platforms that the X10DT supports.
+# For more info on what machines can be used to build for each platform, see
+#  http://xj.watson.ibm.com/twiki/bin/view/Main/XtenTestMachines
 
-# TODO: we should get svn info by parsing svn info URL and extracting revision from there.
+# Mac OS/x86 + x86_64:
+# This host needs to have Xcode and must be configured so that the default g++ is
+# the right version (currently, 4.2 or higher). For now, it should be a 32-bit
+# Leopard host, so that the build works on both 32- and 64-bit Macs running
+# Leopard (10.5) or Snow Leopard (10.6).
+macX86Host=condor
+
+# Linux/x86_64:
+# This host needs to have g++ version 4.2 or higher.
+linuxX86_64Host=triloka3
+
+# Linux/x86 (32-bit):
+# This host needs to have g++ version 4.2 or higher.
+linuxX86Host="" # FIXME
+
+# Cygwin/x86:
+# This host needs to have cygwin 1.7 (for gcc to work right).
+cygwinHost=nashira
+
+# We don't presently support Linux/Power as a development host for the X10DT
+# Linux/Power:
+linuxPowerHost=""
+
+# We don't presently support AIX as a development host for the X10DT
+# AIX/power:
+aixPowerHost=""
+
+hosts="$macX86Host $linuxX86_64Host $linuxX86Host $cygwinHost $linuxPowerHost $aixPowerHost"
+
+noClean="true"
+userID="$USER"
+
+usageMsg="Usage: $0 [-noclean] [-platforms platformList] [-hosts hostList] [-user remoteUserID] [-showhosts] [-showplatforms]"
+
 while [ $# != 0 ]; do
   case $1 in
-    -rev)
-        rev=$2
-	shift
-    ;;
-
     -hosts)
         hosts=$2
 	shift
     ;;
 
-   esac
-   shift
+    -user)
+        userID=$2
+	shift
+        ;;
+
+    -platforms)
+        platforms="$2"
+        hosts=""
+        for platform in $platforms; do
+            case $platform in
+                macosx)
+                    host="$macX86Host"
+                    ;;
+                linux-x86)
+                    host="$linuxX86Host"
+                    ;;
+                linux-x86_64)
+                    host="$linuxX86_64Host"
+                    ;;
+                cygwin)
+                    host="$cygwinHost"
+                    ;;
+                *)
+                    echo "No such platform: $platform"
+                    exit 1
+                    ;;
+            esac
+            hosts="$hosts $host"
+        done
+        shift
+        ;;
+
+    -show-platforms)
+        echo "macosx linux-x86 linux-x86_64 cygwin"
+        exit 0
+        ;;
+
+    -show-hosts)
+        echo "Host list: $hosts"
+        echo "Mac OS x86 host:   $macX86Host"
+        echo "Linux x86 host:    $linuxX86Host"
+        echo "Linux x86_64 host: $linuxX86_64Host"
+        echo "Cygwin host:       $cygwinHost"
+        echo "Linux/Power:       unsupported"
+        echo "AIX/Power:         unsupported"
+        exit 0
+        ;;
+
+    -noclean)
+        noClean="-noclean"
+        ;;
+
+    -h)
+        echo "$usageMsg"
+        exit 0
+        ;;
+
+    -*)
+        echo "$usageMsg"
+        exit 1
+        ;;
+    esac
+    shift
 done
 
+branchName="x10-tools-integration"
+tibURL="http://x10.svn.sourceforge.net/svnroot/x10/branches/${branchName}/"
+tarballDest=/var/www/localhost/htdocs/x10dt/x10-builds
+remoteTmpDir=/tmp/x10-tib-$userID
+
+echo "Determining current revision of ${branchName}..."
+
+rev=$(svn info $tibURL | grep 'Last Changed Rev' | sed -E 's/Last Changed Rev: ([0-9]+)/\1/')
+
 if [[ -z "$rev" ]]; then
-    echo "usage: $0 must give revision -rev <rev>"
+    echo "Error: Unable to determine revision from 'svn info' on URL $tibURL"
     exit 1
 fi
 
-ssh orquesta.watson.ibm.com "mkdir -p /var/www/localhost/htdocs/x10dt/x10-builds/$rev"
+echo "Using revision $rev"
+echo "Building on hosts: $hosts"
+echo "Using user ID $userID"
+
+ssh ${userID}@orquesta.watson.ibm.com "mkdir -p $tarballDest/$rev"
 
 for host in $hosts
 do
-    echo "Launching buildToolIntegration.sh on $host"
-    scp buildToolIntegration.sh $host:/tmp 
-    ssh $host "(cd /tmp; ./buildToolIntegration.sh -d /tmp/x10-tib-$USER -rev $rev)"
-    ssh $host "(cd /tmp; rm ./buildToolIntegration.sh)"
-    echo "transfering file from $host to localhost"
-    scp "$host:/tmp/x10-tib-$USER/x10/x10.dist/x10-tib*.tgz" .
-    echo "transfering from localhost to orquesta"
-    scp x10-tib_*.tgz orquesta.watson.ibm.com:/var/www/localhost/htdocs/x10dt/x10-builds/$rev
-    rm x10-tib_*.tgz
-    #ssh $host rm -rf /tmp/x10-tib-$USER 
+    logFile=/tmp/x10-tib-$host.log
+    echo "=================================================="
+    echo "=== Launching buildToolIntegration.sh on $host ==="
+    echo "=== Log is in ${logFile} ==="
+    echo "=================================================="
+    (
+    scp buildToolIntegration.sh ${userID}@$host:/tmp 
+    ssh ${userID}@$host "(cd /tmp; ./buildToolIntegration.sh ${noClean} -d $remoteTmpDir -rev $rev)"
+    rc=$?
+    ssh ${userID}@$host "(cd /tmp; rm ./buildToolIntegration.sh)"
+    if [ $rc == 0 ]; then
+        echo "Transferring file from $host to localhost"
+        scp "${userID}@$host:$remoteTmpDir/x10/x10.dist/x10-tib*.tgz" .
+        echo "Transferring from localhost to orquesta"
+        scp x10-tib_*.tgz ${userID}@orquesta.watson.ibm.com:$tarballDest/$rev
+        rm x10-tib_*.tgz
+        #ssh $host rm -rf $remoteTmpDir
+    fi ) > ${logFile} 2>&1 &
 done
-
+wait
