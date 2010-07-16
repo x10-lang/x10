7fb90b9ffca1308f75b35fecb5d312550a5c4d38
Bowen Alpern <alpern@us.ibm.com>
2010-07-16 19:47:50 +0000

    allow explicit expression flattening
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@14957 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.compiler/src/x10/ExtensionInfo.java
+++ x10.compiler/src/x10/ExtensionInfo.java
@@ -411,6 +411,9 @@ public class ExtensionInfo extends polyglot.frontend.ParserlessJLExtensionInfo {
            goals.add(InnerClassRemover(job));
            goals.addAll(Optimizer.goals(this, job));
            goals.add(Desugarer(job));
+           if (x10.Configuration.FLATTEN_EXPRESSIONS) {
+               goals.add(ExpressionFlattener(job));
+           }
            goals.add(CodeGenerated(job));
            goals.add(End(job));
            
@@ -801,7 +804,7 @@ public class ExtensionInfo extends polyglot.frontend.ParserlessJLExtensionInfo {
        public Goal ExpressionFlattener(Job job) {
            TypeSystem ts = extInfo.typeSystem();
            NodeFactory nf = extInfo.nodeFactory();
-           VisitorGoal ef = new VisitorGoal("WorkStealing ExpressionFlattener", job, new ExpressionFlattener(job, ts, nf));
+           VisitorGoal ef = new VisitorGoal("ExpressionFlattener", job, new ExpressionFlattener(job, ts, nf));
            Goal ef2 = ef.intern(this);
            if (ef == ef2) {
                ef.addPrereq(Desugarer(job));
