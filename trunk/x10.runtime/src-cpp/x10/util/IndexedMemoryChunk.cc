01cd28ae46fd949f7ea528c824d7dbc3b85b49dd
David Grove <groved@us.ibm.com>
2010-07-16 18:35:33 +0000

    support for uncounted copyTo/copyFrom on IndexedMemoryChunk and Array.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@14951 a326200e-df2c-42e0-81fa-597e909af41d

--- trunk/x10.runtime/src-cpp/x10/util/IndexedMemoryChunk.cc
+++ trunk/x10.runtime/src-cpp/x10/util/IndexedMemoryChunk.cc
@@ -5,6 +5,7 @@
 x10aux::RuntimeType x10::util::IndexedMemoryChunk<void>::rtt;
 
 using namespace x10aux;
+using namespace x10::lang;
 
 namespace x10 {
     namespace util {
@@ -15,41 +16,43 @@ namespace x10 {
                                                        NULL,
                                                        NULL);
 
+        const serialization_id_t IMC_uncounted_copy_to_serialization_id =
+            DeserializationDispatcher::addPutFunctions(IMC_buffer_finder,
+                                                       IMC_uncounted_notifier,
+                                                       NULL,
+                                                       NULL);
+
         const serialization_id_t IMC_copy_from_serialization_id =
             DeserializationDispatcher::addGetFunctions(IMC_buffer_finder,
                                                        IMC_notifier,
                                                        NULL,
                                                        NULL);
 
+        const serialization_id_t IMC_uncounted_copy_from_serialization_id =
+            DeserializationDispatcher::addGetFunctions(IMC_buffer_finder,
+                                                       IMC_uncounted_notifier,
+                                                       NULL,
+                                                       NULL);
+
         
-        void* IMC_buffer_finder(deserialization_buffer& buf, x10_int ) {
-            void *dstAddr = (void*)(size_t)buf.read<x10_long>();
-            return dstAddr;
-        }
-
-        void IMC_notifier(deserialization_buffer &buf, x10_int) {
-            buf.read<x10_long>();  // Read and discard data used by IMC_copy_to_buffer_finder
-            x10::lang::Rail_notifyEnclosingFinish(buf);
-        }
-
         void IMC_notifyEnclosingFinish(deserialization_buffer& buf) {
-            ref<x10::lang::Reference> fs = buf.read<ref<x10::lang::Reference> >();
-            ref<x10::lang::Runtime> rt = x10::lang::PlaceLocalHandle_methods<ref<x10::lang::Runtime> >::apply(x10::lang::Runtime::FMGL(runtime));
+            ref<Reference> fs = buf.read<ref<Reference> >();
+            ref<Runtime> rt = PlaceLocalHandle_methods<ref<Runtime> >::apply(Runtime::FMGL(runtime));
             // olivier says the incr should be just after the notifySubActivitySpawn
-            (fs.operator->()->*(findITable<x10::lang::Runtime__FinishState>(fs->_getITables())->notifyActivityCreation))();
-            (fs.operator->()->*(findITable<x10::lang::Runtime__FinishState>(fs->_getITables())->notifyActivityTermination))();
+            (fs.operator->()->*(findITable<Runtime__FinishState>(fs->_getITables())->notifyActivityCreation))();
+            (fs.operator->()->*(findITable<Runtime__FinishState>(fs->_getITables())->notifyActivityTermination))();
         }
 
         void IMC_serialize_finish_state(place dst, serialization_buffer &buf) {
             // dst is the place where the finish update will occur, i.e. where the notifier runs
             dst = parent(dst);
-            ref<x10::lang::Runtime> rt = x10::lang::PlaceLocalHandle_methods<ref<x10::lang::Runtime> >::apply(x10::lang::Runtime::FMGL(runtime));
-            ref<x10::lang::Reference> fs = rt->currentState();
-            (fs.operator->()->*(findITable<x10::lang::Runtime__FinishState>(fs->_getITables())->notifySubActivitySpawn))(x10::lang::Place_methods::_make(dst));
+            ref<Runtime> rt = PlaceLocalHandle_methods<ref<Runtime> >::apply(Runtime::FMGL(runtime));
+            ref<Reference> fs = rt->currentState();
+            (fs.operator->()->*(findITable<Runtime__FinishState>(fs->_getITables())->notifySubActivitySpawn))(Place_methods::_make(dst));
             buf.write(fs);
         }
 
-        void IMC_copyToBody(void *srcAddr, void *dstAddr, x10_int numBytes, x10::lang::Place dstPlace, bool overlap) {
+        void IMC_copyToBody(void *srcAddr, void *dstAddr, x10_int numBytes, Place dstPlace, bool overlap, bool uncounted) {
             if (dstPlace->FMGL(id) == x10aux::here) {
                 if (overlap) {
                     // potentially overlapping, use memmove
@@ -61,12 +64,16 @@ namespace x10 {
                 x10aux::place dst_place = dstPlace->FMGL(id);
                 x10aux::serialization_buffer buf;
                 buf.write((x10_long)(size_t)(dstAddr));
-                IMC_serialize_finish_state(dst_place, buf);
-                x10aux::send_put(dst_place, IMC_copy_to_serialization_id, buf, srcAddr, numBytes);
+                if (uncounted) {
+                    x10aux::send_put(dst_place, IMC_uncounted_copy_to_serialization_id, buf, srcAddr, numBytes);
+                } else {
+                    IMC_serialize_finish_state(dst_place, buf);
+                    x10aux::send_put(dst_place, IMC_copy_to_serialization_id, buf, srcAddr, numBytes);
+                }
             }
         }
 
-        void IMC_copyFromBody(void *srcAddr, void *dstAddr, x10_int numBytes, x10::lang::Place srcPlace, bool overlap) {
+        void IMC_copyFromBody(void *srcAddr, void *dstAddr, x10_int numBytes, Place srcPlace, bool overlap, bool uncounted) {
             if (srcPlace->FMGL(id) == x10aux::here) {
                 if (overlap) {
                     // potentially overlapping, use memmove
@@ -78,11 +85,29 @@ namespace x10 {
                 x10aux::place src_place = srcPlace->FMGL(id);
                 x10aux::serialization_buffer buf;
                 buf.write((x10_long)(size_t)(srcAddr));
-                IMC_serialize_finish_state(x10aux::here, buf);
-                x10aux::send_get(src_place, IMC_copy_from_serialization_id, buf, dstAddr, numBytes);
+                if (uncounted) {
+                    x10aux::send_get(src_place, IMC_uncounted_copy_from_serialization_id, buf, dstAddr, numBytes);
+                } else {
+                    IMC_serialize_finish_state(x10aux::here, buf);
+                    x10aux::send_get(src_place, IMC_copy_from_serialization_id, buf, dstAddr, numBytes);
+                }
             }
         }
-        
+
+        void* IMC_buffer_finder(deserialization_buffer& buf, x10_int ) {
+            void *dstAddr = (void*)(size_t)buf.read<x10_long>();
+            return dstAddr;
+        }
+
+        void IMC_notifier(deserialization_buffer &buf, x10_int) {
+            buf.read<x10_long>();  // Read and discard data used by IMC_copy_to_buffer_finder
+            IMC_notifyEnclosingFinish(buf);
+        }
+
+        void IMC_uncounted_notifier(deserialization_buffer &buf, x10_int) {
+            // do nothing.
+            // TODO: would maybe be nice if we could just register a NULL notifier callback.  Does X10RT support this?
+        }
     }
 }
 
