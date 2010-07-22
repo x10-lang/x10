c825eccb8e2d0d6ef610384639643f1fa2960291
Baolin Shao <blshao@us.ibm.com>
2010-07-22 19:00:17 +0000

    git-svn-id: svn://svn.code.sf.net/p/x10/code@15090 a326200e-df2c-42e0-81fa-597e909af41d

--- branches/scalable-finish/x10.compiler/src/x10/ExtensionInfo.java
+++ branches/scalable-finish/x10.compiler/src/x10/ExtensionInfo.java
@@ -394,6 +394,15 @@ public class ExtensionInfo extends polyglot.frontend.ParserlessJLExtensionInfo {
                VisitorGoal vg = (VisitorGoal) new VisitorGoal("FinishAnnot",job,av).intern(this);
 
            }*/
+           if(x10.Configuration.FINISH_ANNOTS){
+        	   TypeSystem ts = extInfo.typeSystem();
+               NodeFactory nf = extInfo.nodeFactory();
+               FinishAnnotationVisitor av = new FinishAnnotationVisitor(job,ts,nf,"java");    
+               VisitorGoal vg = (VisitorGoal) new VisitorGoal("FinishAnnot",job,av).intern(this);	   
+               goals.add(vg);
+                
+
+           }
 
            goals.add(NativeClassVisitor(job));
 
