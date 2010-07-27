6bb6c88463aa8edf1d99bc1d8aa01b04c4845261
Robert Fuhrer <rfuhrer@us.ibm.com>
2010-07-27 13:21:16 +0000

    Memory/stability fixes: changed compiler initialization done in parse() method so that (a) the IPrsStream and ILexStream are retrieved inside a finally block, and so nearly always happens, which is important for various other clients, and (b) to avoid holding onto the Compiler instance in between parses, and instead holds onto only the AST and the token stream.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@15180 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt/trunk/org.eclipse.imp.x10dt.ui/src/org/eclipse/imp/x10dt/ui/parser/ParseController.java
+++ x10dt/trunk/org.eclipse.imp.x10dt.ui/src/org/eclipse/imp/x10dt/ui/parser/ParseController.java
@@ -17,12 +17,15 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import lpg.runtime.ILexStream;
+import lpg.runtime.IPrsStream;
 import lpg.runtime.IToken;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.Platform;
+import org.eclipse.imp.core.ErrorHandler;
 import org.eclipse.imp.model.ISourceProject;
 import org.eclipse.imp.parser.ILexer;
 import org.eclipse.imp.parser.IMessageHandler;
@@ -36,10 +39,14 @@ import org.eclipse.jface.text.IRegion;
 import polyglot.frontend.FileSource;
 import polyglot.frontend.Globals;
 import polyglot.frontend.Source;
+import x10.parser.X10Lexer;
+import x10.parser.X10Parser;
 
 public class ParseController extends SimpleLPGParseController {
     private CompilerDelegate fCompiler;
     private PMMonitor fMonitor;
+    private IPrsStream fPrsStream;
+    private ILexStream fLexStream;
 
     public ParseController() {
     	super(X10DTCorePlugin.kLanguageName);
@@ -90,28 +97,43 @@ public class ParseController extends SimpleLPGParseController {
             IPath sourcePath = (fProject != null) ? Platform.getLocation().append(fProject.getName()).append(fFilePath) : fFilePath;
     		
             fCompiler= new CompilerDelegate(fMonitor, handler, proj, sourcePath); // Create the compiler
-            // RMF 5/11/2009 - Make sure to create new parser/lexer delegates, so that no one
-            // gets stale information (e.g. prev token stream) if they go through the delegates.
             fCompiler.compile(streams);
-            fParser= new ParserDelegate(fCompiler.getParserFor(fileSource));
-            fLexer= new LexerDelegate(fCompiler.getLexerFor(fileSource));
         } catch (IOException e) {
             throw new Error(e);
         } finally {
-            // RMF 8/2/2006 - retrieve the AST if there is one; some later phase of compilation
-            // may fail, even though the AST is well-formed enough to provide an outline.
-            if (fileSource != null) {
-                fCurrentAst= fCompiler.getASTFor(fileSource);
+            // RMF 8/2/2006 - retrieve the AST, token stream and lex stream, if they exist; front-end semantic
+            // checks may fail, even though the AST/token-stream are well-formed enough to support various IDE
+        	// services, like syntax highlighting and the outline view's contents.
+            if (fileSource != null && fCompiler != null) {
+            	final X10Parser parser= fCompiler.getParserFor(fileSource);
+            	final X10Lexer lexer= fCompiler.getLexerFor(fileSource);
+            	fPrsStream = parser.getIPrsStream();
+            	fLexStream = lexer.getILexStream();
+            	fParser = new ParserDelegate(parser); // HACK - SimpleLPGParseController.cacheKeywordsOnce() needs an IParser and an ILexer, so create them here. Luckily, they're just lightweight wrappers...
+            	fLexer = new LexerDelegate(lexer);
+            	fCurrentAst= fCompiler.getASTFor(fileSource);
             }
             // RMF 8/2/2006 - cacheKeywordsOnce() must have been run for syntax highlighting to work.
             // Must do this after attempting parsing (even though that might fail), since it depends
             // on the parser/lexer being set in the ExtensionInfo, which only happens as a result of
             // ExtensionInfo.parser(). Ugghh.
-            if (fParser != null) { //PORT1.7
+            if (fParser != null) {
             	cacheKeywordsOnce();
             }
+            fCompiler = null;
+            fParser = null;
+            fLexer = null;
+            Globals.initialize(null);
         }
         return fCurrentAst;
+    }
+
+    public ILexStream getLexStream() {
+    	return fLexStream;
+    }
+
+    public IPrsStream getParseStream() {
+    	return fPrsStream;
     }
 
     /**
@@ -130,15 +152,193 @@ public class ParseController extends SimpleLPGParseController {
 
     @Override
     public Object getCurrentAst() {
-    	if (fCompiler == null)
-    		return null;
-        initializeGlobalsIfNeeded();
+//    	if (fCompiler == null)
+//    		return null;
+//        initializeGlobalsIfNeeded();
         return super.getCurrentAst();
     }
 
     @Override
     public Iterator<IToken> getTokenIterator(IRegion region) {
-        initializeGlobalsIfNeeded();
-        return super.getTokenIterator(region);
+      final int regionOffset= region.getOffset();
+      final int regionLength= region.getLength();
+      final int regionEnd= regionOffset + regionLength - 1;
+
+      if (fPrsStream == null) {
+    	  return new Iterator<IToken>() {
+			public boolean hasNext() {
+				return false;
+			}
+
+			public IToken next() {
+				return null;
+			}
+
+			public void remove() {
+			} };
+      }
+      return new Iterator<IToken>() {
+          final IPrsStream stream= fPrsStream;
+          final int firstTokIdx= getTokenIndexAtCharacter(regionOffset);
+          final int lastTokIdx;
+          {
+              int endIdx= getTokenIndexAtCharacter(regionEnd);
+              char[] streamChars= stream.getInputChars();
+              int streamLen= streamChars.length;
+              try {
+                  if (regionEnd >= 1 && regionEnd < streamLen
+                          && streamChars[regionEnd] == IToken.EOF) {
+                      // skip EOF token (assume LPG puts one at end of input
+                      // character stream, since it does)
+                      endIdx--;
+                  }
+              } catch (ArrayIndexOutOfBoundsException e) {
+                  ErrorHandler.logError("ParseController.getTokenIterator(IRegion): error initializing lastTokIdx",
+                          e);
+                  // System.err.println("getTokenIterator: new Iterator(..)<init>: ArrayIndexOutOfBoundsException");
+                  // System.err.println(" regionEnd = " + regionEnd + ", endIdx = " + endIdx + ", streamLen = " + streamLen + ",
+                  // inputChars.length = " + streamChars.length);
+              }
+              lastTokIdx= endIdx;
+          }
+          int curTokIdx= Math.max(1, firstTokIdx); // skip bogus initial token
+
+          private int getTokenIndexAtCharacter(int offset) {
+              int result= stream.getTokenIndexAtCharacter(offset);
+              // getTokenIndexAtCharacter() answers the negative of the index of the
+              // preceding token if the given offset is not actually within a token.
+              if (result < 0) {
+                  result= -result + 1;
+              }
+
+              // The above may leave result set to a value that is one more than the
+              // last token index, so return the last token index if that's the case
+              // (This can happen if the end of the file contains some text that
+              // does not correspond to a token--e.g., if the text represents an adjunct
+              // or something unrecognized)
+              if (result >= stream.getTokens().size())
+                  result= stream.getTokens().size() - 1;
+
+              return result;
+          }
+
+          // The following declarations cover the whole input stream, which
+          // may be a proper superset of the range of the given region.
+          // For now, that's a simple way to collect the information, and
+          // most often the given region corresponds to the whole input anyway.
+          // In any case, iteration is based on the range of the given region.
+
+          // The preceding adjuncts for each token
+          IToken[][] precedingAdjuncts= new IToken[lastTokIdx + 1][];
+          {
+              stream.setStreamLength();
+              for(int i= 0; i < precedingAdjuncts.length; i++) {
+                  precedingAdjuncts[i]= stream.getPrecedingAdjuncts(i);
+              }
+          }
+
+          // The current indices for each array of preceding adjuncts
+          int[] nextPrecedingAdjunct= new int[lastTokIdx + 1];
+          {
+              for(int i= 0; i < nextPrecedingAdjunct.length; i++) {
+                  if (precedingAdjuncts[i].length == 0)
+                      nextPrecedingAdjunct[i]= -1;
+                  else
+                      nextPrecedingAdjunct[i]= 0;
+              }
+          }
+
+          // The following adjuncts (for the last token only)
+          IToken[] followingAdjuncts;
+          {
+              if (lastTokIdx <= 0)
+                  followingAdjuncts= new IToken[0];
+              else
+                  followingAdjuncts= stream.getFollowingAdjuncts(lastTokIdx);
+          }
+
+          // The current index for the array of following adjuncts
+          int nextFollowingAdjunct;
+          {
+              if (followingAdjuncts.length == 0)
+                  nextFollowingAdjunct= -1;
+              else
+                  nextFollowingAdjunct= 0;
+          }
+
+          // To support hasNext(); initial values may be reset if appropriate
+          private boolean finalTokenReturned= regionEnd < 1 || lastTokIdx <= 0;
+          private boolean finalAdjunctsReturned= !(followingAdjuncts.length > 0);
+
+          /**
+           * Tests whether the iterator has any unreturned tokens. These may
+           * include "regular" tokens and "adjunct" tokens (e.g., representing
+           * comments).
+           * 
+           * @return True if there is another token available, false otherwise
+           */
+          public boolean hasNext() {
+              return !(finalTokenReturned && finalAdjunctsReturned);
+          }
+
+          /**
+           * Returns the next available token in the iterator (or null if
+           * there is none)
+           * 
+           * Will return a valid token under conditions that would cause
+           * hasNext() to to return true; conversely, will return null under
+           * conditions that would cause hasNext() to return false.
+           * 
+           * As a side effect, updates the flags that are used to compute the
+           * value returned by hasNext().
+           * 
+           * The returned token may be a "regular" token (which will have a
+           * corresponding AST node) or an "adjunct" token (which will
+           * represent a comment). The tokens are returned in the order in
+           * which they occur in the text, regardless of their kind.
+           * 
+           */
+          public IToken next() {
+              int next= -1; // for convenience
+
+              // If we're not all the way through the tokens
+              if (curTokIdx <= lastTokIdx) {
+
+                  // First check for any remaining preceding adjuncts
+                  // of the current token
+                  next= nextPrecedingAdjunct[curTokIdx];
+                  // If the current token has any unreturned preceding
+                  // adjuncts
+                  if (next >= 0 && next < precedingAdjuncts[curTokIdx].length) {
+                      // Return the next preceding adjunct, incrementing the
+                      // adjunct index afterwards
+                      return precedingAdjuncts[curTokIdx][nextPrecedingAdjunct[curTokIdx]++];
+                  }
+
+                  // Flag whether the current token is the last one
+                  finalTokenReturned= curTokIdx >= lastTokIdx;
+
+                  // Return the current token, incrementing the token index
+                  // afterwards
+                  return stream.getIToken(curTokIdx++);
+              }
+
+              // If there are any adjuncts following the last token
+              if (nextFollowingAdjunct >= 0 && nextFollowingAdjunct < followingAdjuncts.length) {
+                  // Flag whether the current adjunct is the last one
+                  finalAdjunctsReturned= (nextFollowingAdjunct + 1) >= followingAdjuncts.length;
+
+                  // Return the current adjunct, incrementing the adjunct
+                  // index afterwards
+                  return followingAdjuncts[nextFollowingAdjunct++];
+              }
+
+              return null;
+          }
+
+          public void remove() {
+              throw new UnsupportedOperationException("Unimplemented");
+          }
+      };
     }
 }
