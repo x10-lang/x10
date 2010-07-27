22a53a7ac15c7d262cf5c65969f59f8aa8133491
Igor Peshansky <igorp@us.ibm.com>
2010-07-27 22:56:48 +0000

    Use the same mechanism to generate labels for inlined closures in the C++ backend and in the Inliner (fixes label clashes).
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@15206 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.compiler/src/x10cpp/visit/MessagePassingCodeGenerator.java
+++ x10.compiler/src/x10cpp/visit/MessagePassingCodeGenerator.java
@@ -4266,7 +4266,7 @@ public class MessagePassingCodeGenerator extends X10DelegatingVisitor {
 	            Name rn = Name.make(getId());
 	            Type rt = cd.asInstance().returnType();
 	            this.ret = rt.isVoid() ? null : xts.localDef(closure.position(), xts.NoFlags(), Types.ref(rt), rn);
-	            this.label = Name.make("__ret"+getUniqueId_());
+	            this.label = Name.makeFresh("__ret");
 	        }
 	    }
 	    // TODO: use override to short-circuit the traversal
