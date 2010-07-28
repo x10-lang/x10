116f004c9bdf55c3616954348eb57b8cbb392e82
Baolin Shao <blshao@us.ibm.com>
2010-07-28 20:16:34 +0000

    git-svn-id: svn://svn.code.sf.net/p/x10/code/branches@15222 a326200e-df2c-42e0-81fa-597e909af41d

--- scalable-finish/finish-analysis/src/x10/finish/util/HprofParser.java
+++ scalable-finish/finish-analysis/src/x10/finish/util/HprofParser.java
@@ -6,6 +6,8 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.StringTokenizer;
@@ -20,23 +22,27 @@ public class HprofParser {
 	public long allocatedBytes = 0;
 	public long allocatedObj = 0;
 	public String name;
+	public int debug = 0;
 
 	public ExprStat(double p, long l, long b, long o, String n) {
-	    name = n;
+	    // System.out.println("original name:"+n);
+	    name = abstractName(n);
+	    // System.out.println("processed name:"+name);
 	    percentage = p;
 	    liveBytes = l;
 	    allocatedBytes = b;
 	    allocatedObj = o;
 	}
-	
+
 	public void add(ExprStat r) {
-	    assert(r.name == this.name);
+	    assert (r.name == this.name);
 	    percentage += r.percentage;
 	    liveBytes += r.liveBytes;
 	    allocatedBytes += r.allocatedBytes;
 	    allocatedObj += r.allocatedObj;
+	    debug++;
 	}
-	
+
 	public int hashCode() {
 	    return name.hashCode();
 	}
@@ -48,14 +54,74 @@ public class HprofParser {
 	    }
 	    return result;
 	}
+
+	public String toString() {
+	    return name + ":(" + percentage + "%," + liveBytes / 1000 + "KB,"
+		    + allocatedBytes / 1000 + "KB," + allocatedObj + ")---"
+		    + debug;
+	}
+
+	private String abstractName(String s) {
+	    int i;
+	    for (i = s.length() - 1; i >= 0; i--) {
+		int c = s.charAt(i);
+		boolean f1 = (c == '$');
+		boolean f2 = Character.isDigit(c);
+		if (!(f1 || f2)) {
+		    break;
+		}
+	    }
+	    String tmp = s.substring(0, i + 1);
+	    return tmp;
+	}
     }
 
     public final ReverseFileReader file;
     private Vector<ExprStat> results;
+    private long root = 0;
+    private long remote = 0;
+    private long all = 0;
 
     public HprofParser(String name) throws Exception {
 	file = new ReverseFileReader(name);
 	results = new Vector<ExprStat>();
+    }
+
+    public long getRootStat() {
+	if (root == 0)
+	    add();
+	return root / 1000;
+    }
+
+    public long getRemoteStat() {
+	if (remote == 0)
+	    add();
+	return remote / 1000;
+    }
+
+    public long getAllStat() {
+	if (all == 0)
+	    add();
+	return all / 1000;
+    }
+
+    private void add() {
+	if (all == 0 && root == 0 && remote == 0) {
+	    for (int i = 0; i < results.size(); i++) {
+		ExprStat tmp = results.get(i);
+		if (tmp.name.contains("Root")) {
+		    root += tmp.allocatedBytes;
+		    all += tmp.allocatedBytes;
+		    // System.err.println("find root");
+		} else if (tmp.name.contains("Remote")) {
+		    remote += tmp.allocatedBytes;
+		    all += tmp.allocatedBytes;
+		    // System.err.println("find remote");
+		} else if (tmp.name.contains("FinishStates")) {
+		    all += tmp.allocatedBytes;
+		}
+	    }
+	}
     }
 
     public void parse() throws Exception {
@@ -63,21 +129,31 @@ public class HprofParser {
 	String record = null;
 	// the last two lines are not statistics, skip them
 	record = file.readLine();
+	//System.err.println(record);
 	record = file.readLine();
+	//System.err.println(record);
 	while ((record = file.readLine()) != null) {
+	    //System.err.println(record);
 	    if (record.contains("rank")) {
 		break;
 	    }
-	    //TODO: test it
+	    // TODO: test it
+	    
 	    ExprStat r = parseRecord(record);
+	    //System.out.println("record: "+r.toString());
+	    //Thread.sleep(200);
 	    int pos = results.indexOf(r);
-	    if(pos==-1){
-		//not found
+	    if (pos == -1) {
+		// not found
+		//System.out.println("record: "+r.toString()+" first added");
 		results.add(r);
-	    }
-	    else{
+	    } else {
 		ExprStat pre = results.get(pos);
+		//System.out.println("record found: "+pre.toString());
+		//System.out.println("record added: "+r.toString());
 		pre.add(r);
+
+		//System.out.println("record updated: "+pre.toString());
 		results.remove(pos);
 		results.add(pre);
 	    }
@@ -126,7 +202,62 @@ public class HprofParser {
 	return new ExprStat(perc, live, allocated, obj, name);
     }
 
-    private void dump() {
+    class PercentageComparer implements Comparator {
+	public int compare(Object obj1, Object obj2) {
+	    double i1 = ((ExprStat) obj1).percentage;
+	    double i2 = ((ExprStat) obj2).percentage;
+	    return (int) (i1 - i2);
+	}
+    }
+
+    class LiveBytesComparer implements Comparator {
+	public int compare(Object obj1, Object obj2) {
+	    long i1 = ((ExprStat) obj1).liveBytes;
+	    long i2 = ((ExprStat) obj2).liveBytes;
+	    return (int) (i1 - i2);
+	}
+    }
+
+    class AllocatedBytesComparer implements Comparator {
+	public int compare(Object obj1, Object obj2) {
+	    long i1 = ((ExprStat) obj1).allocatedBytes;
+	    long i2 = ((ExprStat) obj2).allocatedBytes;
+	    return (int) (i1 - i2);
+	}
+    }
+
+    class AllocatedObjsComparer implements Comparator {
+	public int compare(Object obj1, Object obj2) {
+	    long i1 = ((ExprStat) obj1).allocatedObj;
+	    long i2 = ((ExprStat) obj2).allocatedObj;
+	    return (int) (i1 - i2);
+	}
+    }
+
+    public void dump(int sel) {
+	Comparator c;
+	switch (sel) {
+	case 1:
+	    c = new PercentageComparer();
+	    break;
+	case 2:
+	    c = new LiveBytesComparer();
+	    break;
+	case 3:
+	    c = new AllocatedBytesComparer();
+	    break;
+	case 4:
+	    c = new AllocatedObjsComparer();
+	    break;
+	default:
+	    c = new AllocatedBytesComparer();
+	}
+	Collections.sort(results, c);
+	for (int i = results.size() - 1; i >= 0; i--) {
+	    ExprStat es = results.get(i);
+	    if (es.toString().contains("Finish"))
+		System.out.println(es);
+	}
 
     }
 
