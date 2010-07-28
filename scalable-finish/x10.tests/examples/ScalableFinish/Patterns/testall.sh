04a1f7fce20f9d6b4b01609d99a87cce54fdab5b
Baolin Shao <blshao@us.ibm.com>
2010-07-28 18:35:45 +0000

    redo experiments
    git-svn-id: svn://svn.code.sf.net/p/x10/code/branches@15221 a326200e-df2c-42e0-81fa-597e909af41d

--- scalable-finish/x10.tests/examples/ScalableFinish/Patterns/testall.sh
+++ scalable-finish/x10.tests/examples/ScalableFinish/Patterns/testall.sh
@@ -1,78 +1,178 @@
-#!/bin/sh
- ./run.sh ManyLocalFinish1_old 1
- ./run.sh ManyLocalFinish1_old 2
- ./run.sh ManyLocalFinish1_old 4
- ./run.sh ManyLocalFinish1_old 8
- ./run.sh ManyLocalFinish1_old 16
- ./run.sh ManyLocalFinish1 1
- ./run.sh ManyLocalFinish1 2
- ./run.sh ManyLocalFinish1 4
- ./run.sh ManyLocalFinish1 8
- ./run.sh ManyLocalFinish1 16
+  #!/bin/sh
+  ./run.sh ManyLocalFinish1_old 3
+  ./run.sh ManyLocalFinish1_old 5
+  ./run.sh ManyLocalFinish1_old 6
+  ./run.sh ManyLocalFinish1_old 7
+  ./run.sh ManyLocalFinish1_old 9
+  ./run.sh ManyLocalFinish1_old 10
+  ./run.sh ManyLocalFinish1_old 11
+  ./run.sh ManyLocalFinish1_old 12
+  ./run.sh ManyLocalFinish1_old 13
+  ./run.sh ManyLocalFinish1_old 14
+  ./run.sh ManyLocalFinish1_old 15
 
- ./run.sh ManyLocalFinish2_old 1
- ./run.sh ManyLocalFinish2_old 2
- ./run.sh ManyLocalFinish2_old 4
- ./run.sh ManyLocalFinish2_old 8
- ./run.sh ManyLocalFinish2_old 16
- ./run.sh ManyLocalFinish2 1
- ./run.sh ManyLocalFinish2 2
- ./run.sh ManyLocalFinish2 4
- ./run.sh ManyLocalFinish2 8
- ./run.sh ManyLocalFinish2 16
+  ./run.sh ManyLocalFinish1 3
+  ./run.sh ManyLocalFinish1 5
+  ./run.sh ManyLocalFinish1 6
+ ./run.sh ManyLocalFinish1 7
+  ./run.sh ManyLocalFinish1 9
+  ./run.sh ManyLocalFinish1 10
+  ./run.sh ManyLocalFinish1 11
+  ./run.sh ManyLocalFinish1 12
+ ./run.sh ManyLocalFinish1 13
+ ./run.sh ManyLocalFinish1 14
+ ./run.sh ManyLocalFinish1 15
 
-#./run.sh SimpleFinish1_old 1
-#./run.sh SimpleFinish1_old 2
-#./run.sh SimpleFinish1_old 4
-#./run.sh SimpleFinish1_old 8
-#./run.sh SimpleFinish1_old 16
-#./run.sh SimpleFinish1 1
-#./run.sh SimpleFinish1 2
-#./run.sh SimpleFinish1 4
-#./run.sh SimpleFinish1 8
-#./run.sh SimpleFinish1 16
+ ./run.sh ManyLocalFinish2_old 3
+ ./run.sh ManyLocalFinish2_old 5
+ ./run.sh ManyLocalFinish2_old 6
+ ./run.sh ManyLocalFinish2_old 7
+ ./run.sh ManyLocalFinish2_old 9
+ ./run.sh ManyLocalFinish2_old 10
+ ./run.sh ManyLocalFinish2_old 11
+ ./run.sh ManyLocalFinish2_old 12
+ ./run.sh ManyLocalFinish2_old 13
+ ./run.sh ManyLocalFinish2_old 14
+ ./run.sh ManyLocalFinish2_old 15
 
-#./run.sh SimpleFinish2_old 1
-#./run.sh SimpleFinish2_old 2
-#./run.sh SimpleFinish2_old 4
-#./run.sh SimpleFinish2_old 8
-#./run.sh SimpleFinish2_old 16
-#./run.sh SimpleFinish2 1
-#./run.sh SimpleFinish2 2
-#./run.sh SimpleFinish2 4
-#./run.sh SimpleFinish2 8
-#./run.sh SimpleFinish2 16
+ ./run.sh ManyLocalFinish2 3
+ ./run.sh ManyLocalFinish2 5
+ ./run.sh ManyLocalFinish2 6
+./run.sh ManyLocalFinish2 7
+ ./run.sh ManyLocalFinish2 9
+ ./run.sh ManyLocalFinish2 10
+ ./run.sh ManyLocalFinish2 11
+ ./run.sh ManyLocalFinish2 12
+ ./run.sh ManyLocalFinish2 13
+ ./run.sh ManyLocalFinish2 14
+ ./run.sh ManyLocalFinish2 15
 
-#./run.sh SimpleFinish3_old 1
-#./run.sh SimpleFinish3_old 2
-#./run.sh SimpleFinish3_old 4
-#./run.sh SimpleFinish3_old 8
-#./run.sh SimpleFinish3_old 16
-#./run.sh SimpleFinish3 1
-#./run.sh SimpleFinish3 2
-#./run.sh SimpleFinish3 4
-#./run.sh SimpleFinish3 8
-#./run.sh SimpleFinish3 16
 
-#./run.sh SimpleFinish4_old 1
-#./run.sh SimpleFinish4_old 2
-#./run.sh SimpleFinish4_old 4
-#./run.sh SimpleFinish4_old 8
-#./run.sh SimpleFinish4_old 16
-#./run.sh SimpleFinish4 1
-#./run.sh SimpleFinish4 2
-#./run.sh SimpleFinish4 4
-#./run.sh SimpleFinish4 8
-#./run.sh SimpleFinish4 16
+ ./run.sh SimpleFinish1_old 3
+ ./run.sh SimpleFinish1_old 5
+ ./run.sh SimpleFinish1_old 6
+ ./run.sh SimpleFinish1_old 7
+ ./run.sh SimpleFinish1_old 9
+ ./run.sh SimpleFinish1_old 10
+ ./run.sh SimpleFinish1_old 11
+ ./run.sh SimpleFinish1_old 12
+ ./run.sh SimpleFinish1_old 13
+ ./run.sh SimpleFinish1_old 14
+ ./run.sh SimpleFinish1_old 15
 
-#./run.sh SimpleFinish5_old 1
-#./run.sh SimpleFinish5_old 2
-#./run.sh SimpleFinish5_old 4
-#./run.sh SimpleFinish5_old 8
-#./run.sh SimpleFinish5_old 16
-#./run.sh SimpleFinish5 1
-#./run.sh SimpleFinish5 2
-#./run.sh SimpleFinish5 4
-#./run.sh SimpleFinish5 8
-#./run.sh SimpleFinish5 16
+ ./run.sh SimpleFinish1 3
+ ./run.sh SimpleFinish1 5
+ ./run.sh SimpleFinish1 6
+./run.sh SimpleFinish1 7
+ ./run.sh SimpleFinish1 9
+ ./run.sh SimpleFinish1 10
+ ./run.sh SimpleFinish1 11
+ ./run.sh SimpleFinish1 12
+ ./run.sh SimpleFinish1 13
+ ./run.sh SimpleFinish1 14
+ ./run.sh SimpleFinish1 15
+
+
+
+ ./run.sh SimpleFinish2_old 3
+ ./run.sh SimpleFinish2_old 5
+ ./run.sh SimpleFinish2_old 6
+ ./run.sh SimpleFinish2_old 7
+ ./run.sh SimpleFinish2_old 9
+ ./run.sh SimpleFinish2_old 10
+ ./run.sh SimpleFinish2_old 11
+ ./run.sh SimpleFinish2_old 12
+ ./run.sh SimpleFinish2_old 13
+ ./run.sh SimpleFinish2_old 14
+ ./run.sh SimpleFinish2_old 15
+
+ ./run.sh SimpleFinish2 3
+ ./run.sh SimpleFinish2 5
+ ./run.sh SimpleFinish2 6
+./run.sh SimpleFinish2 7
+ ./run.sh SimpleFinish2 9
+ ./run.sh SimpleFinish2 10
+ ./run.sh SimpleFinish2 11
+ ./run.sh SimpleFinish2 12
+ ./run.sh SimpleFinish2 13
+ ./run.sh SimpleFinish2 14
+ ./run.sh SimpleFinish2 15
+
+ ./run.sh SimpleFinish3_old 3
+ ./run.sh SimpleFinish3_old 5
+ ./run.sh SimpleFinish3_old 6
+ ./run.sh SimpleFinish3_old 7
+ ./run.sh SimpleFinish3_old 9
+ ./run.sh SimpleFinish3_old 10
+ ./run.sh SimpleFinish3_old 11
+ ./run.sh SimpleFinish3_old 12
+ ./run.sh SimpleFinish3_old 13
+ ./run.sh SimpleFinish3_old 14
+ ./run.sh SimpleFinish3_old 15
+
+ ./run.sh SimpleFinish3 3
+ ./run.sh SimpleFinish3 5
+ ./run.sh SimpleFinish3 6
+./run.sh SimpleFinish3 7
+ ./run.sh SimpleFinish3 9
+ ./run.sh SimpleFinish3 10
+ ./run.sh SimpleFinish3 11
+ ./run.sh SimpleFinish3 12
+ ./run.sh SimpleFinish3 13
+ ./run.sh SimpleFinish3 14
+ ./run.sh SimpleFinish3 15
+
+
+ ./run.sh SimpleFinish4_old 3
+ ./run.sh SimpleFinish4_old 5
+ ./run.sh SimpleFinish4_old 6
+ ./run.sh SimpleFinish4_old 7
+ ./run.sh SimpleFinish4_old 9
+ ./run.sh SimpleFinish4_old 10
+ ./run.sh SimpleFinish4_old 11
+ ./run.sh SimpleFinish4_old 12
+ ./run.sh SimpleFinish4_old 13
+ ./run.sh SimpleFinish4_old 14
+ ./run.sh SimpleFinish4_old 15
+
+ ./run.sh SimpleFinish4 3
+ ./run.sh SimpleFinish4 5
+ ./run.sh SimpleFinish4 6
+./run.sh SimpleFinish4 7
+ ./run.sh SimpleFinish4 9
+ ./run.sh SimpleFinish4 10
+ ./run.sh SimpleFinish4 11
+ ./run.sh SimpleFinish4 12
+ ./run.sh SimpleFinish4 13
+ ./run.sh SimpleFinish4 14
+ ./run.sh SimpleFinish4 15
+
+
+
+
+ ./run.sh SimpleFinish5_old 3
+ ./run.sh SimpleFinish5_old 5
+ ./run.sh SimpleFinish5_old 6
+ ./run.sh SimpleFinish5_old 7
+ ./run.sh SimpleFinish5_old 9
+ ./run.sh SimpleFinish5_old 10
+ ./run.sh SimpleFinish5_old 11
+ ./run.sh SimpleFinish5_old 12
+ ./run.sh SimpleFinish5_old 13
+ ./run.sh SimpleFinish5_old 14
+ ./run.sh SimpleFinish5_old 15
+
+ ./run.sh SimpleFinish5 3
+ ./run.sh SimpleFinish5 5
+ ./run.sh SimpleFinish5 6
+./run.sh SimpleFinish5 7
+./run.sh SimpleFinish5 9
+./run.sh SimpleFinish5 10
+./run.sh SimpleFinish5 11
+./run.sh SimpleFinish5 12
+./run.sh SimpleFinish5 13
+./run.sh SimpleFinish5 14
+./run.sh SimpleFinish5 15
+
+
 
