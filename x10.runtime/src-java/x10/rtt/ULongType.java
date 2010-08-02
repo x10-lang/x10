5af0c2c33d20af5153ec345dde1c1a550ec515cb
Toshio Suganuma <SUGANUMA@jp.ibm.com>
2010-08-02 04:41:32 +0000

    Fix of XTENLANG-1295 and 1413
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@15311 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.runtime/src-java/x10/rtt/ULongType.java
+++ x10.runtime/src-java/x10/rtt/ULongType.java
@@ -17,7 +17,7 @@ import x10.core.fun.Fun_0_2;
 
 public class ULongType extends RuntimeType<Long> {
     public ULongType() {
-        super(long.class);
+        super(x10.lang.ULong.class);
     }
     
     @Override
@@ -27,7 +27,7 @@ public class ULongType extends RuntimeType<Long> {
 
     @Override
     public boolean instanceof$(Object o) {
-        return o instanceof java.lang.Long;
+        return o instanceof x10.lang.ULong;
     }
 
     @Override
