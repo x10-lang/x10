4f798ce418e7527897119ca34ec70126079f2168
Emmanuel Geay <egeay@us.ibm.com>
2010-08-20 20:19:20 +0000

    Test Suite for SWTBot tests of this plugin
    git-svn-id: svn://svn.code.sf.net/p/x10/code@15663 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ x10dt/trunk/org.eclipse.imp.x10dt.ui.launch.cpp.tests/src/org/eclipse/imp/x10dt/ui/launch/cpp/tests/SWTBotTestSuite.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2010 IBM Corporation.                                         *
+ * All rights reserved. This program and the accompanying materials            *
+ * are made available under the terms of the Eclipse Public License v1.0       *
+ * which accompanies this distribution, and is available at                    *
+ * http://www.eclipse.org/legal/epl-v10.html                                   *
+ *******************************************************************************/
+package org.eclipse.imp.x10dt.ui.launch.cpp.tests;
+
+import org.eclipse.imp.x10dt.ui.launch.cpp.LocalAndRemoteLaunchTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * All SWTBot tests for this plugin.
+ * 
+ * @author egeay
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ LocalAndRemoteLaunchTest.class })
+public final class SWTBotTestSuite {
+}
