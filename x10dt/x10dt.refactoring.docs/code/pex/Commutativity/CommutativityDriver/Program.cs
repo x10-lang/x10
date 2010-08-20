3b1c401de43fba4a22b93d8b610edab36d0ee4e3
Johnathan Brezin <jbrezin@us.ibm.com>
2010-08-20 17:00:38 +0000

    Added the sample programs to examin the suitability of Pex to reduction analysis.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@15650 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ x10dt/x10dt.refactoring.docs/code/pex/Commutativity/CommutativityDriver/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CommutativityTests;
+
+namespace CommutativityDriver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SwapNode swapNode = new SwapNode();
+            //swapNode.SwapNodeShouldNotBeCommutativeOnCounterExample();
+        }
+    }
+}
