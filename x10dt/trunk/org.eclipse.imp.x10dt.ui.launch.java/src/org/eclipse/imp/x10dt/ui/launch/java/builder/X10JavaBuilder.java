9732ae4a8ff1306e38dc377d7291bc9d460561c8
Mandana Vaziri <mvaziri@us.ibm.com>
2010-08-30 21:54:18 +0000

    X10DT builders: bug fixes, added collect bookmarks and check package declaration, added workspace runnables.
    git-svn-id: svn://svn.code.sf.net/p/x10/code@15896 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt/trunk/org.eclipse.imp.x10dt.ui.launch.java/src/org/eclipse/imp/x10dt/ui/launch/java/builder/X10JavaBuilder.java
+++ x10dt/trunk/org.eclipse.imp.x10dt.ui.launch.java/src/org/eclipse/imp/x10dt/ui/launch/java/builder/X10JavaBuilder.java
@@ -1,35 +1,17 @@
 package org.eclipse.imp.x10dt.ui.launch.java.builder;
 
 import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.imp.preferences.IPreferencesService;
-import org.eclipse.imp.x10dt.core.X10DTCorePlugin;
-import org.eclipse.imp.x10dt.core.builder.BuilderExtensionInfo;
-import org.eclipse.imp.x10dt.core.preferences.generated.X10Constants;
 import org.eclipse.imp.x10dt.ui.launch.core.Constants;
-import org.eclipse.imp.x10dt.ui.launch.core.LaunchCore;
 import org.eclipse.imp.x10dt.ui.launch.core.builder.AbstractX10Builder;
 import org.eclipse.imp.x10dt.ui.launch.core.builder.target_op.IX10BuilderFileOp;
 import org.eclipse.imp.x10dt.ui.launch.core.utils.IFilter;
-import org.eclipse.imp.x10dt.ui.launch.core.utils.UIUtils;
-import org.eclipse.jdt.core.JavaModelException;
-import org.eclipse.ui.console.MessageConsole;
-import org.eclipse.ui.console.MessageConsoleStream;
 
-import polyglot.main.Options;
-import polyglot.main.UsageError;
-import x10.Configuration;
 import x10.ExtensionInfo;
-import x10.config.ConfigurationError;
-import x10.config.OptionError;
 
 public class X10JavaBuilder extends AbstractX10Builder {
 	
