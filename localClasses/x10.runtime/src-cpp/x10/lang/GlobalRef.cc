719f98ac5fb37b2cd6a87631f09554b685051069
David Grove <groved@us.ibm.com>
2010-09-07 20:57:47 +0000

    fixes to ReferenceLogging code
    git-svn-id: svn://svn.code.sf.net/p/x10/code/branches@16106 a326200e-df2c-42e0-81fa-597e909af41d

--- localClasses/x10.runtime/src-cpp/x10/lang/GlobalRef.cc
+++ localClasses/x10.runtime/src-cpp/x10/lang/GlobalRef.cc
@@ -1,4 +1,5 @@
 #include <x10/lang/GlobalRef.h>
+#include <x10aux/reference_logger.h>
 
 x10aux::RuntimeType x10::lang::GlobalRef<void>::rtt;
 
@@ -7,8 +8,8 @@ namespace x10 {
         
         void logGlobalReference(x10aux::ref<x10::lang::Object> obj) {
             #if defined(X10_USE_BDWGC) || defined(X10_DEBUG_REFERENCE_LOGGER)
-            if (!obj_->_isMortal()) {
-                ReferenceLogger::log(obj_.operator->());
+            if (!obj->_isMortal()) {
+                x10aux::ReferenceLogger::log(obj.operator->());
             }
             #endif
         }
