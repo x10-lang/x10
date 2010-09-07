85cd66bd6c879d9cbdc7461f4e3279301c45601a
David Grove <groved@us.ibm.com>
2010-09-07 18:25:54 +0000

    log references for GC when creating GlobalRef
    git-svn-id: svn://svn.code.sf.net/p/x10/code@16104 a326200e-df2c-42e0-81fa-597e909af41d

--- branches/localClasses/x10.runtime/src-cpp/x10/lang/GlobalRef.cc
+++ branches/localClasses/x10.runtime/src-cpp/x10/lang/GlobalRef.cc
@@ -2,3 +2,17 @@
 
 x10aux::RuntimeType x10::lang::GlobalRef<void>::rtt;
 
+namespace x10 {
+    namespace lang {
+        
+        void logGlobalReference(x10aux::ref<x10::lang::Object> obj) {
+            #if defined(X10_USE_BDWGC) || defined(X10_DEBUG_REFERENCE_LOGGER)
+            if (!obj_->_isMortal()) {
+                ReferenceLogger::log(obj_.operator->());
+            }
+            #endif
+        }
+    }
+}
+
+
