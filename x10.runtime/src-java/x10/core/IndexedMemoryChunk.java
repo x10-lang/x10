e923b75a0d609cc6822a4c2a39deb1d575db3978
David Cunningham <dcunnin@us.ibm.com>
2010-10-07 20:43:17 +0000

    Add a String constructor that takes Array[Char]
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@17175 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.runtime/src-java/x10/core/IndexedMemoryChunk.java
+++ x10.runtime/src-java/x10/core/IndexedMemoryChunk.java
@@ -75,4 +75,62 @@ public final class IndexedMemoryChunk<T> extends x10.core.Struct {
         return i == 0 ? type : null;
     }
 
+
+    // Methods to get the backing array.   May be called by generated code.
+    public Object getBackingArray() { return value; }
+
+    public boolean[] getBooleanArray() { return (boolean[]) value; }
+    public byte[] getByteArray() { return (byte[]) value; }
+    public short[] getShortArray() { return (short[]) value; }
+    public char[] getCharArray() { return (char[]) value; }
+    public int[] getIntArray() { return (int[]) value; }
+    public long[] getLongArray() { return (long[]) value; }
+    public float[] getFloatArray() { return (float[]) value; }
+    public double[] getDoubleArray() { return (double[]) value; }
+    public Object[] getObjectArray() { return (Object[]) value; }
+
+    public Object[] getBoxedArray() {
+        if (value instanceof boolean[]) {
+            boolean[] a = (boolean[]) value;
+            Boolean[] b = new Boolean[a.length];
+            for (int i = 0; i < a.length; i++) b[i] = a[i];
+        }
+        if (value instanceof byte[]) {
+            byte[] a = (byte[]) value;
+            Byte[] b = new Byte[a.length];
+            for (int i = 0; i < a.length; i++) b[i] = a[i];
+        }
+        if (value instanceof char[]) {
+            char[] a = (char[]) value;
+            Character[] b = new Character[a.length];
+            for (int i = 0; i < a.length; i++) b[i] = a[i];
+        }
+        if (value instanceof short[]) {
+            short[] a = (short[]) value;
+            Short[] b = new Short[a.length];
+            for (int i = 0; i < a.length; i++) b[i] = a[i];
+        }
+        if (value instanceof int[]) {
+            int[] a = (int[]) value;
+            Integer[] b = new Integer[a.length];
+            for (int i = 0; i < a.length; i++) b[i] = a[i];
+        }
+        if (value instanceof long[]) {
+            long[] a = (long[]) value;
+            Long[] b = new Long[a.length];
+            for (int i = 0; i < a.length; i++) b[i] = a[i];
+        }
+        if (value instanceof float[]) {
+            float[] a = (float[]) value;
+            Float[] b = new Float[a.length];
+            for (int i = 0; i < a.length; i++) b[i] = a[i];
+        }
+        if (value instanceof double[]) {
+            double[] a = (double[]) value;
+            Double[] b = new Double[a.length];
+            for (int i = 0; i < a.length; i++) b[i] = a[i];
+        }
+        return (Object[]) value;
+    }
+
 }
--- x10.runtime/src-x10/x10/lang/String.x10
+++ x10.runtime/src-x10/x10/lang/String.x10
@@ -43,10 +43,17 @@ public final class String implements (Int) => Char/*TODO, (Range) => String*//*T
 
 
     /**
+     * Construct a String from an Array[Char].
+     */
+    @Native("java", "new java.lang.String((#1).raw().getCharArray(),#2,#3)")
+    public native def this(r:Array[Char], offset:Int, length:Int): String;
+
+    /**
      * Construct a String from a Rail[Char].
      */
     @Native("java", "new java.lang.String((#1).getCharArray(),#2,#3)")
     public native def this(r:Rail[Char], offset:Int, length:Int): String;
+
 
     /**
      * Return true if the given entity is a String, and this String is equal
