47ea55489e71ea7bc07bde8213805b6fa274c2fa
Igor Peshansky <igorp@us.ibm.com>
2010-11-18 23:06:55 +0000

    Factor out the common functionality for class member defs.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@18372 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.compiler/src/polyglot/types/ProcedureDef_c.java
+++ x10.compiler/src/polyglot/types/ProcedureDef_c.java
@@ -16,12 +16,10 @@ import polyglot.util.TypedList;
  * A <code>ProcedureInstance_c</code> contains the type information for a Java
  * procedure (either a method or a constructor).
  */
-public abstract class ProcedureDef_c extends Def_c implements ProcedureDef
+public abstract class ProcedureDef_c extends MemberDef_c implements ProcedureDef
 {
     private static final long serialVersionUID = 7146402627770404357L;
 
-    protected Ref<? extends StructType> container;
-    protected Flags flags;
     protected List<Ref<? extends Type>> formalTypes;
 
     /** Used for deserializing types. */
@@ -30,45 +28,18 @@ public abstract class ProcedureDef_c extends Def_c implements ProcedureDef
     public ProcedureDef_c(TypeSystem ts, Position pos,
             Ref<? extends StructType> container,
 			       Flags flags, List<Ref<? extends Type>> formalTypes) {
-        super(ts, pos);
-	this.container = container;
-	this.flags = flags;
-	this.formalTypes = TypedList.copyAndCheck(formalTypes, Ref.class, true);
+        super(ts, pos, container, flags);
+        this.formalTypes = TypedList.copyAndCheck(formalTypes, Ref.class, true);
     }
     
-    public Ref<? extends StructType> container() {
-        return container;
-    }
-
-    public Flags flags() {
-        return flags;
-    }
-
     public List<Ref<? extends Type>> formalTypes() {
         return Collections.unmodifiableList(formalTypes);
     }
 
-
-    /**
-     * @param container The container to set.
-     */
-    public void setContainer(Ref<? extends StructType> container) {
-        this.container = container;
-    }
-    
-    /**
-     * @param flags The flags to set.
-     */
-    public void setFlags(Flags flags) {
-        this.flags = flags;
-    }
-    
     /**
      * @param formalTypes The formalTypes to set.
      */
     public void setFormalTypes(List<Ref<? extends Type>> formalTypes) {
         this.formalTypes = TypedList.copyAndCheck(formalTypes, Ref.class, true);
     }
-    
-  
 }
