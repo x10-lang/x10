ac197a83d970441fa27c4407ca864bb7547b6dca
Igor Peshansky <igorp@us.ibm.com>
2010-11-29 05:02:21 +0000

    Comment and whitespace fixes.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@18558 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.compiler/src/x10/util/HierarchyUtils.java
+++ x10.compiler/src/x10/util/HierarchyUtils.java
@@ -1,3 +1,14 @@
+/*
+ *  This file is part of the X10 project (http://x10-lang.org).
+ *
+ *  This file is licensed to You under the Eclipse Public License (EPL);
+ *  You may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *      http://www.opensource.org/licenses/eclipse-1.0.php
+ *
+ *  (C) Copyright IBM Corporation 2006-2010.
+ */
+
 package x10.util;
 
 import java.util.HashSet;
@@ -10,98 +21,80 @@ import polyglot.types.MethodInstance;
 import polyglot.types.Type;
 
 public class HierarchyUtils {
-	
-	public static Set<ClassType> getSuperTypes(ClassType startingClass)
-	{
+
+	public static Set<ClassType> getSuperTypes(ClassType startingClass) {
 		Set<ClassType> superTypes = new LinkedHashSet<ClassType>();
 		ClassType previousType = startingClass;
 		ClassType superType = (ClassType)startingClass.superClass();
-		
-		while(superType != null)
-		{
+
+		while (superType != null) {
 			superTypes.add(superType);
 			addInterfaces(previousType, superTypes);
 			previousType = superType;
 			superType = (ClassType)superType.superClass();
 		}
-		
+
 		addInterfaces(previousType, superTypes);
-		
+
 		return superTypes;
 	}
-	
-	public static Set<ClassType> getSuperClasses(ClassType startingClass)
-	{
+
+	public static Set<ClassType> getSuperClasses(ClassType startingClass) {
 		Set<ClassType> superTypes = new HashSet<ClassType>();
 		ClassType superType = (ClassType)startingClass.superClass();
-		
-		while(superType != null)
-		{
+
+		while (superType != null) {
 			superTypes.add(superType);
 			superType = (ClassType)superType.superClass();
 		}
-		
+
 		return superTypes;
 	}
 	
-	private static void addInterfaces(ClassType startingClass, Set<ClassType> interfaces)
-	{
-		for(Type type : startingClass.interfaces())
-		{
+	private static void addInterfaces(ClassType startingClass, Set<ClassType> interfaces) {
+		for (Type type : startingClass.interfaces()) {
 			interfaces.add((ClassType)type);
 			addInterfaces((ClassType)type, interfaces);
 		}
 	}
 	
-	public static Set<ClassType> getInterfaces(Set<ClassType> classes)
-	{
+	public static Set<ClassType> getInterfaces(Set<ClassType> classes) {
 		Set<ClassType> interfaces = new HashSet<ClassType>();
-		for(ClassType ct : classes)
-		{
+		for (ClassType ct : classes) {
 			addInterfaces(ct, interfaces);
 		}
-		
+
 		return interfaces;
 	}
-	
-	public static Set<MethodInstance> getMethods(Set<ClassType> classes)
-	{
+
+	public static Set<MethodInstance> getMethods(Set<ClassType> classes) {
 		return getMethods(classes, Flags.NONE);
 	}
-	
-	public static Set<MethodInstance> getMethods(Set<ClassType> classes, Flags flags)
-	{
+
+	public static Set<MethodInstance> getMethods(Set<ClassType> classes, Flags flags) {
 		Set<MethodInstance> methods = new HashSet<MethodInstance>();
-		for(ClassType ct : classes)
-		{
-			for(MethodInstance mi : ct.methods())
-			{
-				if(mi.flags().contains(flags))
+		for (ClassType ct : classes) {
+			for (MethodInstance mi : ct.methods()) {
+				if (mi.flags().contains(flags))
 				{
 					methods.add(mi);
 				}
 			}
 		}
-		
+
 		return methods;
 	}
-	
-	public static Set<MethodInstance> getImplementedMethods(Set<ClassType> classes)
-	{
+
+	public static Set<MethodInstance> getImplementedMethods(Set<ClassType> classes) {
 		Set<MethodInstance> methods = new HashSet<MethodInstance>();
-		for(ClassType ct : classes)
-		{
-			for(MethodInstance mi : ct.methods())
-			{
-				if(!mi.flags().isAbstract())
-				{
+		for (ClassType ct : classes) {
+			for (MethodInstance mi : ct.methods()) {
+				if (!mi.flags().isAbstract()) {
 					methods.add(mi);
 				}
 			}
 		}
-		
+
 		return methods;
 	}
-	
-	
 }
