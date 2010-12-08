0926615c85832f1b72b36c19cc26dca080f3d961
Igor Peshansky <igorp@us.ibm.com>
2010-12-08 14:37:42 +0000

    Fix Cygwin build.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@18814 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.runtime/src-cpp/x10/lang/String.cc
+++ x10.runtime/src-cpp/x10/lang/String.cc
@@ -336,7 +336,7 @@ ref<String> String::format(ref<String> format, ref<x10::array::Array<ref<Any> > 
     nullCheck(format);
     nullCheck(parms);
     //size_t len = format->FMGL(content_length);
-    char* orig = ::strdup(format->c_str());
+    char* orig = string_utils::strdup(format->c_str());
     char* fmt = orig;
     char* next = NULL;
     for (x10_int i = 0; fmt != NULL; ++i, fmt = next) {
@@ -353,7 +353,7 @@ ref<String> String::format(ref<String> format, ref<x10::array::Array<ref<Any> > 
         if (next != NULL)
             *next = '%';
     }
-    ::free(orig);
+    dealloc(orig);
     return String::Lit(ss.str().c_str());
 }
 
