8fc5e991d70954b0106d3e125381777e055baa10
Igor Peshansky <igorp@us.ibm.com>
2011-01-20 18:44:37 +0000

    Add an initial version of the Cygwin-specific Java launcher, to allow loading Cygwin-built DLLs from Java. Change the x10 script to use this launcher on Cygwin.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/branches/multivm@19598 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ x10.runtime/x10rt/jni/cyglaunch/H.cc
@@ -0,0 +1,67 @@
+#ifdef __CYGWIN__
+#include <windows.h>
+#endif
+
+//#undef __stdcall
+//#define __stdcall
+
+#include <sys/types.h>
+#include "H.h"
+#include <stdio.h>
+#include <x10rt.h>
+
+//#ifndef __WIN32__
+//#ifndef __stdcall
+//#define __stdcall
+//#endif
+//#ifndef __declspec
+//#define __declspec(X)
+//#endif
+//#endif
+
+#ifdef __cplusplus
+#define EXTERN extern "C"
+#else
+#define EXTERN
+#endif
+
+#ifdef __CYGWIN__
+static void init() {
+  char exe[MAX_PATH];
+  long res = GetModuleFileName(NULL, exe, MAX_PATH);
+  if (res == 0 || res == MAX_PATH) {
+    strcpy(exe, "java");
+  }
+  char* av_0 = exe;
+  int ac = 1;
+  char** av = &av_0;
+  x10rt_init(&ac, &av);
+}
+#else
+static void init() {
+  char* exe = "java";
+  char* av_0 = exe;
+  int ac = 1;
+  char **av = &av_0;
+  x10rt_init(&ac, &av);
+}
+#endif
+
+JNIEXPORT void JNICALL Java_H_nat(JNIEnv *e, jobject o) {
+  printf("here2\n");
+  init();
+  printf("Nplaces=%d\n", x10rt_nplaces());
+  printf("I am place=%d\n", x10rt_here());
+} 
+
+JNIEXPORT void JNICALL Java_H_x10_1init(JNIEnv *e, jobject o) {
+  init();
+}
+
+JNIEXPORT jint JNICALL Java_H_x10_1nplaces(JNIEnv *e, jobject o) {
+  return (jint)x10rt_nplaces();
+}
+
+JNIEXPORT jint JNICALL Java_H_x10_1here(JNIEnv *e, jobject o) {
+  return (jint)x10rt_here();
+}
---
+++ x10.runtime/x10rt/jni/cyglaunch/cyglaunch.cc
@@ -0,0 +1,176 @@
+#include <sys/types.h>
+#include <jni.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dlfcn.h>
+#include <sys/cygwin.h>
+#include <windows.h>
+
+//#define TRACE_JNI
+
+#define JRE_BIN_PATH "/jre/bin"
+#define JRE_CLIENT_PATH JRE_BIN_PATH "/client/"
+#define JRE_SERVER_PATH JRE_BIN_PATH "/server/"
+#define JRE_J9VM_PATH JRE_BIN_PATH "/j9vm/"
+#define JRE_CLASSIC_PATH JRE_BIN_PATH "/classic/"
+#define JVM_DLL "jvm.dll"
+void* try_load_jvm_dll(const char* JAVA_HOME, const char* JAVA_HOME_REL_PATH) {
+  int JAVA_HOME_length = strlen(JAVA_HOME);
+  char dll_name[JAVA_HOME_length+strlen(JAVA_HOME_REL_PATH)+strlen(JVM_DLL)+10];
+  strcpy(dll_name, JAVA_HOME);
+  strcpy(dll_name+JAVA_HOME_length, JAVA_HOME_REL_PATH);
+  strcat(dll_name+JAVA_HOME_length, JVM_DLL);
+  int len = cygwin_conv_path(CCP_POSIX_TO_WIN_A|CCP_RELATIVE, dll_name, NULL, 0);
+  char* dll_name_w = (char*) malloc((len+1) * sizeof(char));
+  cygwin_conv_path(CCP_POSIX_TO_WIN_A, dll_name, dll_name_w, len);
+#ifdef TRACE_JNI
+    fprintf(stderr, "Loading %s\n", dll_name_w);
+#endif
+  void* jvm_dll = LoadLibrary(dll_name_w);
+  return jvm_dll;
+}
+void* load_jvm_dll() {
+  char* JAVA_HOME = getenv("JAVA_HOME");
+  if (JAVA_HOME == NULL) {
+    // JAVA_HOME is not set -- try the PATH
+#ifdef TRACE_JNI
+    fprintf(stderr, "Loading %s\n", JVM_DLL);
+#endif
+    void* jvm_dll = LoadLibrary(JVM_DLL);
+    if (jvm_dll == NULL) {
+      fprintf(stderr, "JAVA_HOME is not set.  Unable to load '%s': %s\n", JVM_DLL, dlerror());
+      exit(2);
+    }
+    return jvm_dll;
+  }
+  void* jvm_dll = try_load_jvm_dll(JAVA_HOME, JRE_SERVER_PATH);
+  if (jvm_dll != NULL) return jvm_dll;
+  jvm_dll = try_load_jvm_dll(JAVA_HOME, JRE_CLIENT_PATH);
+  if (jvm_dll != NULL) return jvm_dll;
+  jvm_dll = try_load_jvm_dll(JAVA_HOME, JRE_J9VM_PATH);
+  if (jvm_dll != NULL) return jvm_dll;
+  jvm_dll = try_load_jvm_dll(JAVA_HOME, JRE_CLASSIC_PATH);
+  if (jvm_dll != NULL) return jvm_dll;
+  fprintf(stderr, "JAVA_HOME ('%s') may not point to a JRE.  Unable to load '%s': %s\n", JAVA_HOME, JVM_DLL, dlerror());
+  exit(2);
+}
+void release_jvm_dll(void* jvm_dll) {
+  if (dlclose(jvm_dll) != 0) {
+    fprintf(stderr, "Unable to unload '%s': %s\n", JVM_DLL, dlerror());
+    exit(2);
+  }
+}
+void* load_jni_symbol(void* jvm_dll, const char* symbol) {
+  void* ptr = dlsym(jvm_dll, symbol);
+  if (ptr == NULL) {
+    fprintf(stderr, "Unable to load symbol %s from %s: %s\n", symbol, JVM_DLL, dlerror());
+    exit(2);
+  }
+  return ptr;
+}
+
+int main(int ac, char** av) {
+#ifdef TRACE_JNI
+  fprintf(stderr, "Started %s\n", av[0]);
+#endif
+  void* jvm_dll = load_jvm_dll();
+  jint JNICALL (*JNI_CreateJavaVM)(JavaVM **pvm, void **penv, void *args);
+  JNI_CreateJavaVM = (jint JNICALL (*)(JavaVM**,void**,void*))load_jni_symbol(jvm_dll, "JNI_CreateJavaVM");
+
+  JavaVM* jvm;
+  JNIEnv* env;
+  JavaVMInitArgs args;
+  args.version = JNI_VERSION_1_2;
+  int c;
+  int i;
+  for (c = 1; c < ac; c++) {
+    if (!strcmp(av[c], "-classpath") || !strcmp(av[c], "-cp")) {
+      c++;
+      continue;
+    }
+    if (*av[c] != '-')
+      break;
+  }
+  args.nOptions = c - 1;
+  args.options = (JavaVMOption*) malloc((c - 1) * sizeof(JavaVMOption));
+  int j = 0;
+  for (i = 1; i < c; i++, j++) {
+    if (!strcmp(av[i], "-classpath") || !strcmp(av[i], "-cp")) {
+      if (i == c-1) {
+        fprintf(stderr, "Option %s requires an argument\n", av[i]);
+        exit(2);
+      }
+      i++;
+      const char* prefix = "-Djava.class.path=";
+      int sz = strlen(prefix) + strlen(av[i]) + 1;
+      char* buf = (char*) malloc(sz * sizeof(char));
+      strcpy(buf, prefix);
+      strcat(buf, av[i]);
+      args.options[j].optionString = buf;
+      args.options[j].extraInfo = NULL;
+      args.nOptions--;
+      continue;
+    }
+    args.options[j].optionString = av[i];
+    args.options[j].extraInfo = NULL;
+  }
+  args.ignoreUnrecognized = JNI_TRUE;
+  char* className = av[c];
+  if (c == ac) {
+    fprintf(stderr, "Usage: %s [jvm_args] <classname> [prog_args]\n", av[0]);
+    exit(2);
+  }
+  jint r1 = JNI_CreateJavaVM(&jvm, (void**)&env, &args);
+#ifdef TRACE_JNI
+  fprintf(stderr, "JNI_CreateJavaVM returned %d\n", r1);
+#endif
+  jclass cls = env->FindClass(className); 
+#ifdef TRACE_JNI
+  fprintf(stderr, "\tFound class %s: %p\n", className, cls);
+#endif
+  if (env->ExceptionOccurred()) {
+    env->ExceptionDescribe();
+  }
+  jmethodID mid = env->GetStaticMethodID(cls, "main", "([Ljava/lang/String;)V"); 
+#ifdef TRACE_JNI
+  fprintf(stderr, "\tFound method main([Ljava/lang/String;)V: %p\n", mid);
+#endif
+  if (env->ExceptionOccurred()) {
+    env->ExceptionDescribe();
+  }
+  const char* STRING = "java/lang/String";
+  jclass java_lang_String = env->FindClass(STRING); 
+#ifdef TRACE_JNI
+  fprintf(stderr, "\tFound class %s: %p\n", STRING, java_lang_String);
+#endif
+  if (env->ExceptionOccurred()) {
+    env->ExceptionDescribe();
+  }
+  jobjectArray applicationArgs = env->NewObjectArray(ac - c - 1, java_lang_String, NULL);
+#ifdef TRACE_JNI
+  fprintf(stderr, "\tCreated args array: %p\n", applicationArgs);
+#endif
+  if (env->ExceptionOccurred()) {
+    env->ExceptionDescribe();
+  }
+  for (i = c + 1; i < ac; i++) {
+    jstring applicationArg = env->NewStringUTF(av[i]);
+    env->SetObjectArrayElement(applicationArgs, i - c - 1, applicationArg);
+  }
+  if (env->ExceptionOccurred()) {
+    env->ExceptionDescribe();
+  }
+  env->CallStaticVoidMethod(cls, mid, applicationArgs); 
+#ifdef TRACE_JNI
+  fprintf(stderr, "\tInvoked main\n");
+#endif
+  if (env->ExceptionOccurred()) {
+    env->ExceptionDescribe();
+  }
+  jint r2 = jvm->DestroyJavaVM();
+#ifdef TRACE_JNI
+  fprintf(stderr, "DestroyJavaVM returned %d\n", r2);
+#endif
+  release_jvm_dll(jvm_dll);
+}
