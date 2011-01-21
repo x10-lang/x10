b43d05e8f5f4072c8fbe8df860bd51ca6a630c1b
David Grove <groved@us.ibm.com>
2011-01-21 15:49:18 +0000

    delete multivm-test dir.  No longer needed.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@19636 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.dist/samples/multivm-test/hello-launch.sh
+++
@@ -1,9 +0,0 @@
-# export x10LAUNCHER_NPROCS=4
-#export JAVA_HOME=/usr/lib/jvm/java-1.6.0-openjdk-1.6.0.0/jre
-export X10_HOME=/home/suzumura/workspace/x10-multivm
-export HOST_LIST="knoxville"
-export NUM_PLACES=2
-export SCRIPT=hello-socket.sh
-
-echo "${X10_HOME}/x10.runtime/x10rt/sockets/X10Launcher -np ${NUM_PLACES} -hostlist ${HOST_LIST} sh ${SCRIPT}"
-${X10_HOME}/x10.runtime/x10rt/sockets/X10Launcher -np ${NUM_PLACES} -hostlist ${HOST_LIST} sh ${SCRIPT}
--- x10.dist/samples/multivm-test/hello-socket.sh
+++
@@ -1,8 +0,0 @@
-# export x10LAUNCHER_NPROCS=4
-export JAVA_HOME=/usr/lib/jvm/java-1.6.0-openjdk-1.6.0.0/jre
-export X10_HOME=/home/suzumura/workspace/x10-multivm
-export X10_DIST=${X10_HOME}/x10.dist
-
-echo "${JAVA_HOME}/bin/java  -Djava.library.path=\".:${X10_DIST}/lib/x10.jar:${X10_DIST}/lib\" -Dx10.LOAD=\"x10rt_sockets\" -ea -classpath \".:${X10_DIST}/lib/x10.jar\" 'HelloWholeWorld$Main'"
-
-${JAVA_HOME}/bin/java  -Djava.library.path=".:${X10_DIST}/lib/x10.jar:${X10_DIST}/lib" -Dx10.LOAD="x10rt_sockets" -ea -classpath ".:${X10_DIST}/lib/x10.jar" 'HelloWholeWorld$Main'
