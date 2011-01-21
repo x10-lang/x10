9bf722f884e7ccdaccafbc773022af4ae87317a5
David Grove <groved@us.ibm.com>
2011-01-21 18:19:58 +0000

    quick and dirty fix to makefile to get cygwin build to actually complete in a clean tree
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@19639 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.runtime/x10rt/jni/jni.mk
+++ x10.runtime/x10rt/jni/jni.mk
@@ -1,6 +1,14 @@
 ifeq ($(X10RT_PLATFORM), cygwin)
-JNI_EXECUTABLES = bin/cyglaunch.exe
-EXECUTABLES += $(JNI_EXECUTABLES)
+
+# DAVE G: Disabled because the rule doesn't work in
+#         in a "clean" source tree.  You can't attempt to
+#         build an exe here that is going to link against
+#         x10.dist/lib/x10rt_sockets.so because the dist rule
+#         for the overall x10rt rule hasn't run yet to cause
+#         the library to be copied to x10.dist/lib
+#
+#JNI_EXECUTABLES = bin/cyglaunch.exe
+#EXECUTABLES += $(JNI_EXECUTABLES)
 
 ${JNI_EXECUTABLES:bin/%=jni/cyglaunch/%}:
 	-$(MAKE) -C jni/cyglaunch $^
