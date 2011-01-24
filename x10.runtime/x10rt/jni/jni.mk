c7a385a2ed7ae468b08c4d6c5104582b24d4698f
Igor Peshansky <igorp@us.ibm.com>
2011-01-24 22:25:37 +0000

    Update clean rule to include the Cygwin launcher if available.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@19703 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.runtime/x10rt/jni/jni.mk
+++ x10.runtime/x10rt/jni/jni.mk
@@ -8,4 +8,8 @@ ${JNI_EXECUTABLES:bin/%=jni/cyglaunch/%}:
 
 ${JNI_EXECUTABLES}: ${JNI_EXECUTABLES:bin/%=jni/cyglaunch/%}
 	$(CP) $^ bin/
+
+clean: jni_clean
+jni_clean:
+	$(MAKE) X10_HOME="$(X10_HOME)" -C jni/cyglaunch clean
 endif
