68986b1ba4abd7ae5ef104102f840679425ece85
Mikio Takeuchi <mtake@jp.ibm.com>
2011-01-28 08:47:57 +0000

    Fix for XTENLANG-2381
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@19781 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.runtime/src-java/x10/rtt/StaticFunType.java
+++ x10.runtime/src-java/x10/rtt/StaticFunType.java
@@ -34,7 +34,15 @@ public class StaticFunType<T> extends RuntimeType<T> {
     public String typeName(Object o) {
         // Note: assume that the first parent in this RuntimeType is the parameterized type which corresponds to the function type
         assert o instanceof x10.core.fun.Fun;
-        return ((x10.rtt.ParameterizedType<?>) getParents()[0]).typeNameForFun(o);
+        Type<?> parent = getParents()[0];
+        String typeName;
+        if (parent instanceof ParameterizedType<?>) {
+        	typeName = ((ParameterizedType<?>) parent).typeNameForFun(o);
+        } else {
+        	assert parent instanceof RuntimeType<?>;
+        	typeName = ((RuntimeType<?>) parent).typeNameForFun(o);
+        }
+        return typeName;
     }
 
 }
