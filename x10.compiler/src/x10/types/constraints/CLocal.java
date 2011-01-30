913a384378fbac7bcbbabc42299115381ca1fa2c
Vijay Saraswat <vsaraswa@us.ibm.com>
2011-01-30 22:22:17 +0000

    stragglers -- new files
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@19818 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ x10.compiler/src/x10/types/constraints/CLocal.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package x10.types.constraints;
+
+import polyglot.types.Type;
+import polyglot.types.Types;
+import x10.constraint.XLocal;
+import x10.types.X10LocalDef;
+
+
+/**
+ * A representation of a local variable, with its associated type information.
+ * <p>In essence, a CLocal is a serializable representation of an X10LocalInstance.
+ * <p> Note: X10LocalDef instead of LocalDef because place information is useful in
+ * some cases.
+ * @author vj
+ *
+ */
+public class CLocal extends XLocal<X10LocalDef> {
+    
+    String s;
+    public CLocal(X10LocalDef ld) {
+        super(ld);
+        s=ld.name().toString();
+    }
+    
+    public CLocal(X10LocalDef ld, String s) {
+        super(ld);
+        this.s=s;
+    }
+    public X10LocalDef localDef() {
+        return name;
+    }
+    public Type type() {
+        return Types.get(name.type());
+    }
+
+}
