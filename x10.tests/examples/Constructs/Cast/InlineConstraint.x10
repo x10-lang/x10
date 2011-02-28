x10-lang/x10

commit ea34e568228be3c25a63af093b6492643639078f

Add self!=null constraint to the coercion from IntRange to Region. Fix some tests that expected to treat 0..5 as a Region. Fix MultiDimensionalJavaArray because of the API change from Region to IntRange. Revert change in InlineConstraint that replaced inline constraints with typedefs. Add -STATIC_CALLS to HasTypeClosure_MustFailCompile. Remove obsolete ERR marker.
git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@20385 a326200e-df2c-42e0-81fa-597e909af41d

x10.tests/examples/Constructs/Cast/InlineConstraint.x10
@@ -15,26 +15,26 @@ import harness.x10Test;
  */
 public class InlineConstraint extends x10Test {
     public def run()  {
-	val v:Object = 0..5;
+	val v:Object = 0..5 as Region;
 	var result:boolean=true;
-   val c = v instanceof Region(1);
+	val c = v instanceof Region{self.rank==1};
 	if (!c) {
 	    Console.OUT.println("v instanceof Region{self.rank==1} failed.");
 	    result=false;
 	}
-   val d = v instanceof Region(2);
+	val d = v instanceof Region{self.rank==2};
 	if (d) {
 	    Console.OUT.println("WTF, it's a two-dimensional region?!");
 	    result=false;
 	}
 	try {
-       val e = v as Region(1);
+	    val e = v as Region{self.rank==1};
 	} catch (ClassCastException) {
 	    Console.OUT.println("WTF, cast to a single-dimensional region failed?!");
 	    result=false;
 	}
 	try {
-       val f = v as Region(2);
+	    val f = v as Region{self.rank==2};
 	    Console.OUT.println("WTF, cast to a two-dimensional region succeeded?!");
 	    result = false;
 	} catch (ClassCastException) {

x10.tests/examples/Constructs/Cast/MacroConstraint.x10
@@ -16,7 +16,7 @@ import harness.x10Test;
  */
 public class MacroConstraint extends x10Test {
 	public def run()  {
-	    val v:Object = 0..5;
+	    val v:Object = 0..5 as Region;
 	    var result:boolean=true;
 	    val c = v instanceof Region(1);
 	    if (!c) {

x10.tests/examples/Constructs/For/ForLoop.x10
@@ -22,7 +22,7 @@ public class ForLoop extends x10Test {
 	var nActivities: int = 0;
 
 	public def run(): boolean = {
-		val r = 0..(N-1);
+		val r = 0..(N-1) as Region;
 		val P0 = here;
 		val d = r->P0;
 

x10.runtime/src-x10/x10/array/Region.x10
@@ -377,7 +377,7 @@ public abstract class Region(
     //
     public static operator (a:Array[Region(1){self.rect}](1)):Region(a.size){self.rect} = make(a);
 
-    public static operator (r:IntRange):Region(1){rect} {
+    public static operator (r:IntRange):Region(1){rect&&self!=null} {
         return new RectRegion(r.min, r.max);
     }
 

x10.tests/examples/Constructs/Array/MultiDimensionalJavaArray.x10
@@ -30,9 +30,9 @@ public class MultiDimensionalJavaArray extends x10Test {
         for (val [i,j]: Point(2) in MIN*MAJ)
             a(i)(j) = (i * j / PI);
 
-        val d = a(MIN.max(0)/2);
-        for (val [j]: Point in MAJ) 
-            chk(d(j) == (MIN.max(0)/2 * j / PI));
+        val d = a(MIN.max/2);
+        for (val j in MAJ) 
+            chk(d(j) == (MIN.max/2 * j / PI));
 
         return true;
     }

x10.tests/examples/Constructs/Types/HasTypeClosure_MustFailCompile.x10
@@ -9,6 +9,8 @@
  *  (C) Copyright IBM Corporation 2006-2010.
  */
 
+// OPTIONS: -STATIC_CALLS
+
 import harness.x10Test;
 
 /**

x10.tests/examples/Misc/x10/frontend/tests/FrontEndTests_MustFailCompile.x10
@@ -4717,7 +4717,7 @@ class RegionCastTests {
 }
 
 class TestOverridingAndHasType {
-	val i <: Double = 2; // ERR
+	val i <: Double = 2;
 	class A {
 		def m():Int = 1;
 		def t():Int = 1;

commit 0a382f9a2b269a87dea533deeabf45058fff1971

Fix silly precedence bugs introduced in r20385.
git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@20398 a326200e-df2c-42e0-81fa-597e909af41d

x10.tests/examples/Constructs/Cast/InlineConstraint.x10
@@ -15,7 +15,7 @@ import harness.x10Test;
  */
 public class InlineConstraint extends x10Test {
     public def run()  {
-	val v:Object = 0..5 as Region;
+	val v:Object = (0..5) as Region;
 	var result:boolean=true;
 	val c = v instanceof Region{self.rank==1};
 	if (!c) {

x10.tests/examples/Constructs/Cast/MacroConstraint.x10
@@ -16,7 +16,7 @@ import harness.x10Test;
  */
 public class MacroConstraint extends x10Test {
 	public def run()  {
-	    val v:Object = 0..5 as Region;
+	    val v:Object = (0..5) as Region;
 	    var result:boolean=true;
 	    val c = v instanceof Region(1);
 	    if (!c) {

x10.tests/examples/Constructs/For/ForLoop.x10
@@ -22,7 +22,7 @@ public class ForLoop extends x10Test {
 	var nActivities: int = 0;
 
 	public def run(): boolean = {
-		val r = 0..(N-1) as Region;
+		val r = (0..(N-1)) as Region;
 		val P0 = here;
 		val d = r->P0;
 