2e24e8a69df57f2e92bfb0ab3d4856cc3c8f759d
David Grove <groved@us.ibm.com>
2011-04-11 16:03:44 +0000

    XTENLANG-2653 : Include the Empty AST in the list of harmless ASTs   in the StructMethodAnalyzer.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@21236 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.compiler/src/x10cpp/visit/StructMethodAnalyzer.java
+++ x10.compiler/src/x10cpp/visit/StructMethodAnalyzer.java
@@ -17,6 +17,7 @@ import polyglot.ast.Block_c;
 import polyglot.ast.BooleanLit;
 import polyglot.ast.Branch;
 import polyglot.ast.Conditional;
+import polyglot.ast.Empty_c;
 import polyglot.ast.Eval_c;
 import polyglot.ast.FlagsNode_c;
 import polyglot.ast.FloatLit;
@@ -108,7 +109,7 @@ public class StructMethodAnalyzer extends ContextVisitor {
         // Trivial nodes that will never by themselves prevent us from putting the body in the struct method class.
         if (n instanceof FlagsNode_c || n instanceof Id_c || 
                 n instanceof X10Local_c || n instanceof LocalAssign_c || 
-                n instanceof X10CanonicalTypeNode_c || n instanceof X10Special_c) {
+                n instanceof X10CanonicalTypeNode_c || n instanceof X10Special_c || n instanceof Empty_c) {
             return n;
         }
         
