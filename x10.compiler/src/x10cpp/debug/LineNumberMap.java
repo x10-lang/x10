384058f1c632ade7377378b127c2817d34e11a01
Benjamin Herta <bherta@us.ibm.com>
2011-04-13 14:45:11 +0000

    Modified the loop variable redirection that was requested by the debugger team in RTC entry 5641 so that it only applies to base types (int, long, etc), and not classes, as requested in RTC entry 6179.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@21266 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.compiler/src/x10cpp/debug/LineNumberMap.java
+++ x10.compiler/src/x10cpp/debug/LineNumberMap.java
@@ -597,7 +597,7 @@ public class LineNumberMap extends StringTable {
 			}
 		}
 		// convert loop indexes
-		if (loopVariables != null && loopVariables.containsKey(name))
+		if (v._x10type < 100 && loopVariables != null && loopVariables.containsKey(name))
 		{
 			ArrayList<LoopVariable> list = loopVariables.get(name);
 			for (LoopVariable lv : list)
@@ -1364,7 +1364,7 @@ public class LineNumberMap extends StringTable {
         w.newline(4); w.begin(0);
         w.writeln("sizeof(struct _MetaDebugInfo_t),");
         w.writeln("X10_META_LANG,");
-        w.writeln("0x0B040C0C, // 2011-04-12, 12:00"); // Format: "YYMMDDHH". One byte for year, month, day, hour.
+        w.writeln("0x0B040D0B, // 2011-04-13, 11:00"); // Format: "YYMMDDHH". One byte for year, month, day, hour.
         w.writeln("sizeof(_X10strings),");
         if (!m.isEmpty()) {
             w.writeln("sizeof(_X10sourceList),");
