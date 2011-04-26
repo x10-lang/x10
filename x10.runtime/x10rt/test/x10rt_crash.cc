4ec2825c7a926f4d94c31c79ec1a4b39031969bb
David Cunningham <dcunnin@us.ibm.com>
2011-04-26 23:14:34 +0000

    Adding an x10rt test that simulates segfaults and other crashes to see how the launching mechanisms handle it
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@21449 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ x10.runtime/x10rt/test/x10rt_crash.cc
@@ -0,0 +1,47 @@
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <iomanip>
+
+#include <strings.h>
+#include <unistd.h>
+
+#include <x10rt_front.h>
+
+int *null_ptr = 0;
+
+int main(int argc, char **argv)
+{
+    x10rt_init(&argc, &argv);
+    x10rt_registration_complete();
+
+    if (x10rt_here()==0) {
+        std::cout << "This test simulates a crash to ensure x10rt implementations shut down cleanly." << std::endl;
+    }
+
+    sleep(x10rt_here());
+    
+    const char *crash_mode = getenv("CRASH_MODE")==NULL ? "segfault" : getenv("CRASH_MODE");
+
+    if (x10rt_here() == x10rt_nhosts()-1) {
+        if (!strcasecmp(crash_mode, "segfault")) {
+            std::cerr << "Dereferencing null at place " << x10rt_nhosts()-1 << "..." << std::endl;
+            *null_ptr = 42;
+        } else if (!strcasecmp(crash_mode, "exit")) {
+            std::cerr << "Calling exit() before x10rt_finalize() at place " << x10rt_nhosts()-1 << "..." << std::endl;
+            exit(EXIT_FAILURE);
+        } else if (!strcasecmp(crash_mode, "abort")) {
+            std::cerr << "Calling abort() at place " << x10rt_nhosts()-1 << "..." << std::endl;
+            abort();
+        } else {
+            std::cerr << "CRASH_MODE is unknown: \"" << crash_mode << "\"" << std::endl;
+            std::cerr << "Not simulating a crash." << std::endl;
+        }
+    }
+
+    x10rt_finalize();
+    return EXIT_SUCCESS;
+}
+
+// vim: shiftwidth=4:tabstop=4:expandtab:textwidth=100
+
