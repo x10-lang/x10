894f509d4a2cd77edaeb9cc92c6923758100a964
David Grove <groved@us.ibm.com>
2011-04-28 22:04:49 +0000

    generate the x10doc as part of building a release
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@21486 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.dist/releng/buildRelease.sh
+++ x10.dist/releng/buildRelease.sh
@@ -96,6 +96,7 @@ if [[ -z "$SKIP_X10_BUILD" ]]; then
     ant -Doptimize=true -Dtar.version=$X10_VERSION testtar
     ant -Doptimize=true -Dtar.version=$X10_VERSION srctar
     ant dist -Doptimize=true
+    ant xrx-xdoc
     $distdir/x10.dist/releng/packageCPPRelease.sh -version $X10_VERSION -platform $X10_PLATFORM
     echo "Platform specific distribuiton tarball created"
 fi
