26208ffd437060967cd0cce7888bb13c8a4ff3d8
Igor Peshansky <igorp@us.ibm.com>
2011-05-10 13:20:00 +0000

    Fix XTENLANG-2710: mark as non-constant all casts of numeric constants to all non-numeric types (not just Any).
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@21639 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.compiler/src/x10/ast/X10Cast_c.java
+++ x10.compiler/src/x10/ast/X10Cast_c.java
@@ -246,7 +246,7 @@ public class X10Cast_c extends Cast_c implements X10Cast, X10CastInfo {
     public boolean isConstant() {
         if (!expr.isConstant()) return false;
         if (castType.type().isNumeric()) return true;
-        if (castType.type().typeSystem().isAny(Types.baseType(castType.type()))) return false; // FIXME: because constantValue method below doesn't know how to correctly handle this case
+        if (expr.type().isNumeric()) return false; // FIXME: because constantValue method below doesn't know how to correctly handle this case
         return expr.type().isSubtype(castType.type(), expr.type().typeSystem().emptyContext());
     }
         
