153dd024ac725f0fcfff8f7123ea061558f4fe36
David Grove <groved@us.ibm.com>
2011-05-12 20:21:06 +0000

    Committing improvments on behalf of Janice (mailed to me several weeks ago).
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@21685 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.compiler/src/x10/visit/CodeCleanUp.java
+++ x10.compiler/src/x10/visit/CodeCleanUp.java
@@ -15,11 +15,15 @@ package x10.visit;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.Stack;
 
 import polyglot.ast.Block;
 import polyglot.ast.Branch;
 import polyglot.ast.Labeled;
+import polyglot.ast.Local;
+import polyglot.ast.LocalDecl;
 import polyglot.ast.Node;
 import polyglot.ast.NodeFactory;
 import polyglot.ast.Return;
@@ -28,12 +32,14 @@ import polyglot.ast.SwitchBlock;
 import polyglot.ast.Throw;
 import polyglot.frontend.Job;
 import polyglot.main.Reporter;
+import polyglot.types.LocalDef;
 import polyglot.types.Name;
 import polyglot.types.TypeSystem;
 import polyglot.util.CollectionUtil;
-import polyglot.visit.AlphaRenamer;
+import polyglot.util.InternalCompilerError;
 import polyglot.visit.ContextVisitor;
 import polyglot.visit.NodeVisitor;
+import x10.ast.AtStmt;
 import x10.ast.StmtExpr;
 import x10.ast.StmtSeq;
 import x10.extension.X10Ext;
@@ -47,7 +53,7 @@ public class CodeCleanUp extends ContextVisitor {
 
     private TypeSystem xts;
     private NodeFactory xnf;
-    protected AlphaRenamer alphaRen;
+    protected OneScopeRenamer oneScopeRen;
     protected boolean report;
     protected boolean reportStats;
     static protected long blockCount;
@@ -62,9 +68,180 @@ public class CodeCleanUp extends ContextVisitor {
         super(job, ts, nf);
         xts = ts;
         xnf = nf;
-        this.alphaRen = new AlphaRenamer();
+        this.oneScopeRen = new OneScopeRenamer();
         this.report = reporter.should_report("CodeCleanUp", 1);
         this.reportStats = reporter.should_report("CodeCleanUpStats", 1);
+    }
+
+    private class OneScopeRenamer extends NodeVisitor {
+        
+        protected Map<Name,Name> renamingMap;
+
+        protected Map<LocalDef, Name> oldNamesMap;
+        
+        // Tracks the set of variables known to be fresh.
+        protected Set<Name> freshVars;
+
+        protected Map<Name,Name> labelMap;
+        
+        public int scopeLevel;
+
+        /**
+         * Creates a visitor for alpha-renaming locals.
+         *
+         * @param nf  The node factory to be used when generating new nodes.
+         **/
+        public OneScopeRenamer() {
+
+            this.oldNamesMap = CollectionFactory.newHashMap();
+            this.renamingMap = CollectionFactory.newHashMap();
+            this.labelMap = CollectionFactory.newHashMap();
+            this.freshVars = CollectionFactory.newHashSet();
+            this.scopeLevel = 0;
+        }
+
+        /** Map from local def to old names. */
+        public Map<LocalDef, Name> getMap() {
+            return oldNamesMap;
+        }
+
+        public static final String LABEL_PREFIX = "label ";
+
+        public NodeVisitor enter(Node n) {
+            if (n instanceof Block) {
+                if (!((n instanceof StmtSeq) || n instanceof StmtExpr)) {
+                    // Bump the scope level
+                    scopeLevel++;
+                }
+            }
+
+            if (scopeLevel == 1) {
+                if (n instanceof LocalDecl) {
+                    LocalDecl l = (LocalDecl) n;
+                    Name name = l.name().id();
+
+                    if (!freshVars.contains(name)) {
+                        // Add a new entry to the current renaming map.
+                        Name name_ = Name.makeFresh(name);
+
+                        freshVars.add(name_);
+
+                        renamingMap.put(name, name_);
+                    }
+                }
+
+                if (n instanceof Labeled) {
+                    Labeled l = (Labeled) n;
+                    Name name = l.labelNode().id();
+                    Name key = Name.make(LABEL_PREFIX + name.toString());
+                    if (!freshVars.contains(key)) {
+                        Name name_ = Name.makeFresh(name);
+                        Name key_ = Name.make(LABEL_PREFIX + name_.toString());
+
+                        freshVars.add(key_);
+
+                        labelMap.put(key, name_);
+                    }
+                }
+            }
+            return this;
+        }
+
+        public Node leave(Node old, Node n, NodeVisitor v) {
+            if (n instanceof Block) {
+                if (!((n instanceof StmtSeq) || n instanceof StmtExpr)) {
+                    scopeLevel--;
+                    if (scopeLevel == 0) {
+                        // Pop the current name set off the stack and remove the
+                        // corresponding
+                        // entries from the renaming map.
+                        renamingMap.keySet().clear();
+                        labelMap.keySet().clear();
+                    }
+                }
+                assert (scopeLevel >= 0); 
+                return n;
+            }
+
+            if (n instanceof Local) {
+                // Rename the local if its name is in the renaming map.
+                Local l = (Local) n;
+                Name name = l.name().id();
+
+                if (!renamingMap.containsKey(name)) {
+                    return n;
+                }
+
+                // Update the local instance as necessary.
+                Name newName = renamingMap.get(name);
+                // LocalType li = l.localInstance();
+                // if (li != null) li.setName(newName);
+
+                return l.name(l.name().id(newName));
+            }
+
+            if (n instanceof LocalDecl) {
+                // Rename the local decl.
+                LocalDecl l = (LocalDecl) n;
+                Name name = l.name().id();
+
+                if (freshVars.contains(name)) {
+                    return n;
+                }
+
+                if (!renamingMap.containsKey(name)) {
+                    return n;
+                }
+
+                // Update the local instance as necessary.
+                Name newName = renamingMap.get(name);
+                LocalDef li = l.localDef();
+                if (li != null) {
+                    oldNamesMap.put(li, li.name());
+                    li.setName(newName);
+                }
+                return l.name(l.name().id(newName));
+            }
+
+            if (n instanceof Branch) {
+                // Rename the label if its name is in the renaming map.
+                Branch b = (Branch) n;
+
+                if (b.labelNode() == null) {
+                    return n;
+                }
+
+                Name name = b.labelNode().id();
+                Name key = Name.make(LABEL_PREFIX + name.toString());
+
+                if (!labelMap.containsKey(key)) {
+                    return n;
+                }
+
+                Name newName = labelMap.get(key);
+
+                return b.labelNode(b.labelNode().id(newName));
+            }
+
+            if (n instanceof Labeled) {
+                Labeled l = (Labeled) n;
+                Name name = l.labelNode().id();
+                Name key = Name.make(LABEL_PREFIX + name.toString());
+
+                if (freshVars.contains(key)) {
+                    return n;
+                }
+
+                if (!labelMap.containsKey(key)) {
+                    return n;
+                }
+
+                Name newName = labelMap.get(key);
+                return l.labelNode(l.labelNode().id(newName));
+            }
+
+            return n;
+        }
     }
 
     @Override
@@ -72,7 +249,10 @@ public class CodeCleanUp extends ContextVisitor {
         if (!(n instanceof Block) || n instanceof StmtExpr || n instanceof SwitchBlock) {
             return n;
         }
-
+        if (parent instanceof AtStmt) {
+            return n;
+        }
+        
         // We can flatten labeled blocks when there is no reference to the label
         // within the block. If we have a labeled block consisting of just one
         // statement and there is a reference to the label, then we might be
@@ -129,7 +309,8 @@ public class CodeCleanUp extends ContextVisitor {
                     // flattening.
                     if (report) System.out.println("Cleaning up a block" + inner.position());
                     if (reportStats) blockCount++;
-                    if (!innerIsStmtSeq) inner = (Block) inner.visit(alphaRen);
+                    if (!innerIsStmtSeq) inner = (Block) inner.visit(oneScopeRen);
+                    // Could add a check here that scopeLevel is back to 0???
                     stmtList.addAll(inner.statements());
                     changeMade = true;
                 } else {
