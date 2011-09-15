2f922e4dae48121db0f8bf2422a613d306f31d17
David Grove <groved@us.ibm.com>
2011-09-15 22:13:54 +0000

    Add flag to indicate we are using gcc 4.1 as the postcompiler, if flag is set then attempt to workaround stmt expr bug by using operator->() hack. Set flag by default for BG/P.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@22907 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.compiler/src/x10cpp/visit/MessagePassingCodeGenerator.java
+++ x10.compiler/src/x10cpp/visit/MessagePassingCodeGenerator.java
@@ -2297,10 +2297,6 @@ public class MessagePassingCodeGenerator extends X10DelegatingVisitor {
 
 	public void visit(StmtExpr_c n) {
 	    X10CPPCompilerOptions opts = (X10CPPCompilerOptions) tr.job().extensionInfo().getOptions();
-	    if (!opts.x10_config.ALLOW_STATEMENT_EXPRESSIONS) {
-	        tr.job().compiler().errorQueue().enqueue(ErrorInfo.SEMANTIC_ERROR,
-	                "Statement expression node encountered, but statement expressions are disabled: ", n.position());
-	    }
         boolean oldSemiColon = tr.appendSemicolon(true);
 	    sw.write("(__extension__ ({");
 	    sw.newline(4); sw.begin(0);
@@ -2313,7 +2309,14 @@ public class MessagePassingCodeGenerator extends X10DelegatingVisitor {
 	    tr.printType(oldPrintType);
 	    Expr e = n.result();
 	    if (e != null) {
+	        boolean gccHack = opts.x10_config.STATEMENT_EXPR_GCC_WORKAROUND && e.type().isReference();
+	        if (gccHack) {
+	            sw.write("(");
+	        }
 	        n.print(e, sw, tr);
+	        if (gccHack) {
+	            sw.write(").operator->()");
+	        }
 	        sw.write(";");
 	    }
 	    sw.end(); sw.newline();
@@ -4053,8 +4056,7 @@ public class MessagePassingCodeGenerator extends X10DelegatingVisitor {
 		}
 		sw.write(tmp);
 		X10CPPCompilerOptions opts = (X10CPPCompilerOptions) tr.job().extensionInfo().getOptions();
-		if (!opts.x10_config.ALLOW_STATEMENT_EXPRESSIONS) {
-		    // FIXME: HACK around a compiler bug in GCC 4.1
+		if (opts.x10_config.STATEMENT_EXPR_GCC_WORKAROUND) {
 		    sw.write(".operator->()");
 		}
 		sw.write(";");
