x10-lang/x10

commit 5363e1230bc23137be4e892f3c8362d08850f88a

Big fixes in lexical capture recomputation code in the constant propagator.   (1) we need to recompute environments even if we are not propagating closures       because constant propagation of locals can reduce the captured env.   (2) when recomputing the environment for at, we need to compute it only       for the actual body, not for the place expression.
git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@23297 a326200e-df2c-42e0-81fa-597e909af41d

x10.compiler/src/x10/visit/ConstantPropagator.java
@@ -48,6 +48,8 @@ import polyglot.visit.ErrorHandlingVisitor;
 import polyglot.visit.NodeVisitor;
 import x10.ast.Async;
 import x10.ast.Async_c;
+import x10.ast.AtEach;
+import x10.ast.AtExpr;
 import x10.ast.AtStmt;
 import x10.ast.Closure;
 import x10.ast.Closure_c;
@@ -96,17 +98,22 @@ public class ConstantPropagator extends ContextVisitor {
     protected Node leaveCall(Node parent, Node old, Node n, NodeVisitor v) throws SemanticException {
         Position pos = n.position();
         
-        // If we are propagating closures, then AST nodes that capture lexical
-        // environments need to have the captured environment recomputed for their def 
-        // because we may have propagated one or more closure literals into their bodies.
-        if (propClosures) {
-            if (n instanceof Async) {
-                n.visit(new ClosureCaptureVisitor(this.context(), ((Async)n).asyncDef()));
-            } else if (n instanceof Closure) {
-                n.visit(new ClosureCaptureVisitor(this.context(), ((Closure)n).closureDef()));                
-            } else if (n instanceof AtStmt) {
-                n.visit(new ClosureCaptureVisitor(this.context(), ((AtStmt)n).atDef()));                
-            }
+        // AST nodes that capture lexical environments need to have the captured environment
+        // recomputed in leaveCall because we may have done one of two things:
+        //   (a) Constant propagated away uses of a lexically captured variable, thus 
+        //       reducing the captured environment.
+        //   (b) Propagated one or more closure literals into the body of the construct,
+        //       which can increase the captured environment. 
+        if (n instanceof Async) {
+        	n.visit(new ClosureCaptureVisitor(this.context(), ((Async)n).asyncDef()));
+        } else if (n instanceof AtStmt) {
+        	((AtStmt)n).body().visit(new ClosureCaptureVisitor(this.context(), ((AtStmt)n).atDef()));                
+        } else if (n instanceof AtEach) {
+        	((AtEach)n).body().visit(new ClosureCaptureVisitor(this.context(), ((AtEach)n).atDef()));
+        } else if (n instanceof AtExpr) {
+        	((AtExpr)n).body().visit(new ClosureCaptureVisitor(this.context(), ((AtExpr)n).closureDef()));                
+        } else if (n instanceof Closure) {
+        	n.visit(new ClosureCaptureVisitor(this.context(), ((Closure)n).closureDef()));                
         }
 
         if (!(n instanceof Expr || n instanceof Stmt)) return n;

commit 305ee094560df399daa7583a929b230504a9cf24

More correct recomputation of environment capture for AtEach construct. Should fix test cases broken by r23297 in optimized test runs.
git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@23302 a326200e-df2c-42e0-81fa-597e909af41d

x10.compiler/src/x10/visit/ConstantPropagator.java
@@ -33,6 +33,8 @@ import polyglot.ast.Stmt;
 import polyglot.ast.Throw;
 import polyglot.ast.VarDecl;
 import polyglot.frontend.Job;
+import polyglot.types.Context;
+import polyglot.types.LocalDef;
 import polyglot.types.Name;
 import polyglot.types.QName;
 import polyglot.types.SemanticException;
@@ -54,6 +56,7 @@ import x10.ast.AtStmt;
 import x10.ast.Closure;
 import x10.ast.Closure_c;
 import x10.ast.StmtExpr;
+import x10.ast.X10Formal;
 import x10.constraint.XLit;
 import x10.constraint.XTerm;
 import x10.constraint.XVar;
@@ -105,15 +108,20 @@ public class ConstantPropagator extends ContextVisitor {
         //   (b) Propagated one or more closure literals into the body of the construct,
         //       which can increase the captured environment. 
         if (n instanceof Async) {
-        	n.visit(new ClosureCaptureVisitor(this.context(), ((Async)n).asyncDef()));
+            n.visit(new ClosureCaptureVisitor(this.context(), ((Async)n).asyncDef()));
         } else if (n instanceof AtStmt) {
-        	((AtStmt)n).body().visit(new ClosureCaptureVisitor(this.context(), ((AtStmt)n).atDef()));                
+            ((AtStmt)n).body().visit(new ClosureCaptureVisitor(this.context(), ((AtStmt)n).atDef()));                
         } else if (n instanceof AtEach) {
-        	((AtEach)n).body().visit(new ClosureCaptureVisitor(this.context(), ((AtEach)n).atDef()));
+            // Only want to visit the body to recompute capture, but need to simulate 
+            // visiting the formal so that it gets in scope for the body so env capture can find it.
+            AtEach ateach = (AtEach)n;
+            Context tmpContext = this.context().pushBlock();
+            ateach.formal().addDecls(tmpContext);
+            ateach.body().visit(new ClosureCaptureVisitor(tmpContext, ateach.atDef()));
         } else if (n instanceof AtExpr) {
-        	((AtExpr)n).body().visit(new ClosureCaptureVisitor(this.context(), ((AtExpr)n).closureDef()));                
+            ((AtExpr)n).body().visit(new ClosureCaptureVisitor(this.context(), ((AtExpr)n).closureDef()));                
         } else if (n instanceof Closure) {
-        	n.visit(new ClosureCaptureVisitor(this.context(), ((Closure)n).closureDef()));                
+            n.visit(new ClosureCaptureVisitor(this.context(), ((Closure)n).closureDef()));                
         }
 
         if (!(n instanceof Expr || n instanceof Stmt)) return n;