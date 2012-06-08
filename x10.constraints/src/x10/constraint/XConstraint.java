b15e3a508456c3b0624b0dd061ad37ef36edd243
David Cunningham <dcunnin@us.ibm.com>
2012-06-08 18:10:23 +0000

    Fix html in javadoc
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@23863 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.constraints/src/x10/constraint/XConstraint.java
+++ x10.constraints/src/x10/constraint/XConstraint.java
@@ -30,7 +30,7 @@ import java.util.Set;
  * 
  *  A constraint solver for the following constraint system. Note terms in this 
  *  constraint system are untyped.
- * <verbatim>
+ * <pre>
  * t ::= x                  -- variable
  *       | t.f              -- field access
  *       | g(t1,..., tn)    -- uninterpreted function symbol
@@ -39,7 +39,7 @@ import java.util.Set;
  *       | t != t           -- dis-equality
  *       | c,c              -- conjunction
  *       | p(t1,..., tn)    -- atomic formula
- * </verbatim>  
+ * </pre>  
  * 
  * The constraint system implements the usual congruence rules for equality. 
  * That is, if <code>s1,...,sn</code> and 
