6de2df06e45dc9c897c22eb07d3e1ba885b52e7f
David Grove <groved@us.ibm.com>
2012-07-11 21:08:15 +0000

    XTENLANG-2143 : merge Polyglot and X10 standard headers where appropriate.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@24058 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.compiler/src/polyglot/main/Report.java
+++ x10.compiler/src/polyglot/main/Report.java
@@ -1,9 +1,15 @@
 /*
- * This file is part of the Polyglot extensible compiler framework.
+ *  This file is part of the X10 project (http://x10-lang.org).
  *
- * Copyright (c) 2000-2006 Polyglot project group, Cornell University
- * Copyright (c) 2006 IBM Corporation
- * 
+ *  This file is licensed to You under the Eclipse Public License (EPL);
+ *  You may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *      http://www.opensource.org/licenses/eclipse-1.0.php
+ *
+ * This file was originally derived from the Polyglot extensible compiler framework.
+ *
+ *  (C) Copyright 2000-2007 Polyglot project group, Cornell University
+ *  (C) Copyright IBM Corporation 2007-2012.
  */
 
 package polyglot.main;
