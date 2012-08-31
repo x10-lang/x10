0fc27642f9734ad0ebb4f7b8cf7b596440ad09de
David Grove <groved@us.ibm.com>
2012-08-31 22:21:57 +0000

    undo my changes from r24474 that attempted to get the dynamic and static type of the throw helper methods to line up.  Since we aren't really going to use C++'s exception dispatch mechanism, we don't need to templatize throw quite as heavily.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@24480 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.runtime/src-cpp/x10aux/throw.h
+++ x10.runtime/src-cpp/x10aux/throw.h
@@ -22,19 +22,16 @@ namespace x10aux {
 
     template<class T> void throwException() X10_PRAGMA_NORETURN;
 
-    template<class T> void throwException(T* e) X10_PRAGMA_NORETURN;
+    void throwException(x10::lang::CheckedThrowable* e) X10_PRAGMA_NORETURN;
 
     void throwArithmeticException() X10_PRAGMA_NORETURN;
 
-    template<class T> inline void throwException(T* e) {
-        e->fillInStackTrace();
-        throw e;
+    inline void throwException(x10::lang::CheckedThrowable* e) {
+        throw e->fillInStackTrace();
     }
 
     template<class T> void throwException() {
-        T* e = T::_make();
-        e->fillInStackTrace();
-        throw e;
+        throwException(T::_make());
     }
 
     inline x10_byte zeroCheck(x10_byte val) {
