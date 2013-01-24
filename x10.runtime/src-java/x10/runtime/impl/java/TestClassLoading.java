0fab7cb0cb8f5fda4df3b69450508005a4913ea9
David Grove <groved@us.ibm.com>
2013-01-24 21:10:38 +0000

    whoops. restore TestClassLoading; it is used by the x10 script error checking code...
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@24885 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ x10.runtime/src-java/x10/runtime/impl/java/TestClassLoading.java
@@ -0,0 +1,24 @@
+/*
+ *  This file is part of the X10 project (http://x10-lang.org).
+ *
+ *  This file is licensed to You under the Eclipse Public License (EPL);
+ *  You may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *      http://www.opensource.org/licenses/eclipse-1.0.php
+ *
+ *  (C) Copyright IBM Corporation 2006-2012.
+ */
+
+package x10.runtime.impl.java;
+
+public class TestClassLoading {
+    public static void main(java.lang.String[] args) {
+        try {
+            java.lang.String name = args[0];
+            java.lang.Class.forName(name);
+        } catch (java.lang.Throwable e) {
+            java.lang.System.exit(1);
+        }
+        java.lang.System.exit(0);
+    }
+}
