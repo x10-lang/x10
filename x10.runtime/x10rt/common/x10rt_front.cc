5e07067446e8bbfedb3488a2e45d4dae806a9879
Mikio Takeuchi <mtake@jp.ibm.com>
2013-02-17 07:07:16 +0000

    just added standard copyright header. no other changes.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@24985 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.runtime/x10rt/common/x10rt_front.cc
+++ x10.runtime/x10rt/common/x10rt_front.cc
@@ -1,3 +1,14 @@
+/*
+ *  This file is part of the X10 project (http://x10-lang.org).
+ *
+ *  This file is licensed to You under the Eclipse Public License (EPL);
+ *  You may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *      http://www.opensource.org/licenses/eclipse-1.0.php
+ *
+ *  (C) Copyright IBM Corporation 2006-2013.
+ */
+
 #if defined(__CYGWIN__) || defined(__FreeBSD__)
 #undef __STRICT_ANSI__ // Strict ANSI mode is too strict in Cygwin and FreeBSD
 #endif
