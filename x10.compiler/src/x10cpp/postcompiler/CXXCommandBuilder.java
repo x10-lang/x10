a666a09af733300bf83b680921fab9c0b1b3c605
David Grove <groved@us.ibm.com>
2013-02-20 22:04:24 +0000

    XTENLANG-3178 : Need additional linker flags for gperftools on modern Ubuntu system
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@25013 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.compiler/src/x10cpp/postcompiler/CXXCommandBuilder.java
+++ x10.compiler/src/x10cpp/postcompiler/CXXCommandBuilder.java
@@ -226,7 +226,9 @@ public class CXXCommandBuilder {
         cxxCmd.addAll(x10rt.libs);
 
         if (options.gpt) {
+            cxxCmd.add("-Wl,--no-as-needed");
             cxxCmd.add("-lprofiler");
+            cxxCmd.add("-Wl,--as-needed");
         }
         
         if (options.buildX10Lib != null) {
