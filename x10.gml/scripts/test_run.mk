31db0021b32feeae8083b864ab9e0cb2a793060d
Mikio Takeuchi <mtake@jp.ibm.com>
2013-03-06 06:13:56 +0000

    use X10_NPLACES instead of -np for x10 command
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@25122 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.gml/scripts/test_run.mk
+++ x10.gml/scripts/test_run.mk
@@ -43,7 +43,8 @@ Xjvm  = $(shell which x10)
 # run tests short-keys
 
 run_java	: java
-		$(Xjvm) -np $(numplaces) -classpath $(build_path):$(gml_lib)/managed_gml.jar -libpath $(build_path):$(gml_lib) $(target) $(test_args)
+		X10_NPLACES=$(numplaces) $(Xjvm) -classpath $(build_path):$(gml_lib)/managed_gml.jar -libpath $(build_path):$(gml_lib) $(target) $(test_args)
+#		$(Xjvm) -np $(numplaces) -classpath $(build_path):$(gml_lib)/managed_gml.jar -libpath $(build_path):$(gml_lib) $(target) $(test_args)
 
 run_mpi		: mpi
 			$(Srun) -n $(numplaces) ./$(target)_mpi $(test_args)
