bcdd02382cc8cc2c94bc7632eacc3fa5959641c4
David Cunningham <dcunnin@us.ibm.com>
2013-03-30 00:15:44 +0000

    WIP on rose integration in X10
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@25310 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.compiler/src/x10rose/visit/RoseTranslator.java
+++ x10.compiler/src/x10rose/visit/RoseTranslator.java
@@ -146,6 +146,8 @@ public class RoseTranslator extends Translator {
 		ErrorQueue eq = job.compiler().errorQueue();
 		Compiler compiler = job.compiler();
 		Options options = job.extensionInfo().getOptions();
+
+        JNI.cactionTest();
 		
 		Source src = sfn.source();
 		String in_file_name = sfn.source().path();
