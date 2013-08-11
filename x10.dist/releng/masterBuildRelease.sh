60afa85af50d60242cde0401a21be889c4f08286
David Grove <groved@us.ibm.com>
2013-08-11 01:17:39 +0000

    improve copying of tar balls from build machines to orquesta by going directly from one to the other instead of shuttling through the 'master' machine where the script is being run.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@25971 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.dist/releng/masterBuildRelease.sh
+++ x10.dist/releng/masterBuildRelease.sh
@@ -59,23 +59,16 @@ do
     scp buildRelease.sh $host:/tmp 
     ssh $host "bash -l -c 'cd /tmp; ./buildRelease.sh -dir /tmp/x10-rc-$USER -version $version -tag $tag $debug_arg'"
     ssh $host "(cd /tmp; rm ./buildRelease.sh)"
-    echo "transfering binary build from $host to localhost"
-    scp "$host:/tmp/x10-rc-$USER/x10-$version/x10.dist/x10-$version*.tgz" .
-    echo "transfering from localhost to orquesta"
-    scp x10-$version*.tgz orquesta.pok.ibm.com:/var/www/localhost/htdocs/x10dt/x10-rc-builds/$version
-    rm x10-$version*.tgz
+    echo "transfering binary build from $host to orquesta"
+    scp "$host:/tmp/x10-rc-$USER/x10-$version/x10.dist/x10-$version*.tgz" "orquesta.pok.ibm.com:/var/www/localhost/htdocs/x10dt/x10-rc-builds/$version"
 
     if [[ -z "$pushed_source" ]]; then
-	echo "transfering source build and testsuite from $host to localhost"
-	scp "$host:/tmp/x10-rc-$USER/x10-$version/x10-$version*.tar.bz2" .
-	echo "transfering from localhost to orquesta"
-	scp x10-$version*.tar.bz2 orquesta.pok.ibm.com:/var/www/localhost/htdocs/x10dt/x10-rc-builds/$version
-	rm x10-$version*.tar.bz2
-
+	echo "transfering source build and testsuite from $host to orquesta"
+	scp "$host:/tmp/x10-rc-$USER/x10-$version/x10-$version*.tar.bz2" "orquesta.pok.ibm.com:/var/www/localhost/htdocs/x10dt/x10-rc-builds/$version"
 	echo "Packaging benchmarks"
 	./packageBenchmarks.sh -dir /tmp/x10-bench-$USER -version $version -tag $tag
 	echo "transfering benchmarks tar to orquesta"
-	scp /tmp/x10-bench-$USER/x10-benchmarks-$version.tar.bz2 orquesta.pok.ibm.com:/var/www/localhost/htdocs/x10dt/x10-rc-builds/$version 
+	scp "/tmp/x10-bench-$USER/x10-benchmarks-$version.tar.bz2" "orquesta.pok.ibm.com:/var/www/localhost/htdocs/x10dt/x10-rc-builds/$version"
 
 	export pushed_source="done"
     fi
