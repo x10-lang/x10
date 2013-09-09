0e1ed3d6866929d90707f1ad59c78d295169c2d9
David Grove <groved@us.ibm.com>
2013-09-09 12:10:39 +0000

    delete cyglaunch (attempt to use jni-based sockets for Managed X10 on cygwin/windows); made obsolete by JavaSockets implementaton of x10rt
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@26188 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.runtime/x10rt/jni/jni.mk
+++ x10.runtime/x10rt/jni/jni.mk
@@ -9,18 +9,3 @@
 #  (C) Copyright IBM Corporation 2006-2010.
 #
 
-ifeq ($(X10RT_PLATFORM), cygwin)
-
-JNI_EXECUTABLES = bin/cyglaunch.exe
-EXECUTABLES += $(JNI_EXECUTABLES)
-
-jni/cyglaunch/cyglaunch.exe: jni/cyglaunch/cyglaunch.cc
-	$(MAKE) X10_HOME="$(X10_HOME)" -C jni/cyglaunch all
-
-${JNI_EXECUTABLES}: ${JNI_EXECUTABLES:bin/%=jni/cyglaunch/%}
-	$(CP) $^ bin/
-
-clean: jni_clean
-jni_clean:
-	$(MAKE) X10_HOME="$(X10_HOME)" -C jni/cyglaunch clean
-endif
