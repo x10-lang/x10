39abd5662ebbb93b143bc97d8135ff5c7ade06c9
Mikio Takeuchi <mtake@jp.ibm.com>
2013-09-11 08:02:11 +0000

    merge r26205 (fix for XTENLANG-1011) from trunk to x10-2.4 branch
    git-svn-id: svn://svn.code.sf.net/p/x10/code/branches/x10-2.4@26206 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.runtime/src-java/x10/runtime/impl/java/TestClassLoading.java
+++ x10.runtime/src-java/x10/runtime/impl/java/TestClassLoading.java
@@ -12,13 +12,27 @@
 package x10.runtime.impl.java;
 
 public class TestClassLoading {
-    public static void main(java.lang.String[] args) {
-        try {
-            java.lang.String name = args[0];
-            java.lang.Class.forName(name);
-        } catch (java.lang.Throwable e) {
-            java.lang.System.exit(1);
+    public static void main(String[] args) {
+        /* check java version */
+        String javaversionstring = System.getProperty("java.specification.version");
+        if (javaversionstring == null) {
+            System.out.println("Error: Unknow Java version.  Use Java 6 or newer.");
+            System.exit(1);
         }
-        java.lang.System.exit(0);
+        double javaversion = Double.parseDouble(javaversionstring);
+        if (javaversion < 1.6) {
+            System.out.println("Error: Unsupported Java version.  Use Java 6 or newer.");
+            System.exit(1);
+        }
+
+        /* check application class */
+        try {
+            String name = args[0];
+            Class.forName(name);
+        } catch (Throwable e) {
+            System.exit(1);
+        }
+
+        System.exit(0);
     }
 }
