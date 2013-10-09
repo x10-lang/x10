02199390b69d9d7803234a93d2a2bdd90313719a
Benjamin Herta <bherta@us.ibm.com>
2013-10-09 15:41:15 +0000

    Better implementation of previous
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@26504 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.runtime/src-java/x10/x10rt/SocketTransport.java
+++ x10.runtime/src-java/x10/x10rt/SocketTransport.java
@@ -223,7 +223,7 @@ public class SocketTransport {
     	if (place < 0 || place >= channels.length)
     		return true;
     	
-    	return (channels[place] == null || channels[place].sc == null);
+    	return (channels[place] != null && channels[place].sc == null);
     }
     
     public int shutdown() {
@@ -705,7 +705,7 @@ public class SocketTransport {
     private boolean flushBufferedBytes(SelectionKey key, int placeid) {
     	if (DEBUG) System.out.println("Flush called for place "+placeid);
     	
-    	if (channels[placeid].writeLock.tryLock()) {
+    	if (channels[placeid] != null && channels[placeid].writeLock.tryLock()) {
     		try {
     			if (channels[placeid].pendingWrites == null) return false;
     	    	
