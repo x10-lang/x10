11d6ef37f12fbb30611e2929bac736e464aea17a
David Grove <groved@us.ibm.com>
2014-01-21 20:34:04 +0000

    enable parallel C++ compilation when building a release tar ball
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@27227 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.dist/releng/buildRelease.sh
+++ x10.dist/releng/buildRelease.sh
@@ -128,9 +128,9 @@ if [[ -z "$SKIP_X10_BUILD" ]]; then
     cd $distdir/x10.dist
     ant $EXTRA_X10RT_BUILD_ARG -Doptimize=true -Dx10.version=$X10_VERSION testtar
     ant $EXTRA_X10RT_BUILD_ARG -Doptimize=true -Dx10.version=$X10_VERSION srctar
-    ant $EXTRA_X10RT_BUILD_ARG -Doptimize=true dist
+    ant $EXTRA_X10RT_BUILD_ARG -Doptimize=true -Davailable.procs=4 dist
     if [[ -z "$SKIP_DEBUG_BUILD" ]]; then
-        ant $EXTRA_X10RT_BUILD_ARG -Ddebug=true dist-cpp
+        ant $EXTRA_X10RT_BUILD_ARG -Ddebug=true -Davailable.procs=4 dist-cpp
     fi 
     ant xrx-xdoc
     $distdir/x10.dist/releng/packageRelease.sh -version $X10_VERSION -platform $X10_PLATFORM
