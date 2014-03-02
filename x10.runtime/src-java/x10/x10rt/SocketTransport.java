a67998ab08671a755c4c188c5cc343d331ef9e0c
David Grove <groved@us.ibm.com>
2014-03-02 04:49:52 +0000

    Merge 27462 (add catch block for deserialization exception to JavaSockets) from trunk to 2.4.2 branch
    git-svn-id: svn://svn.code.sf.net/p/x10/code/branches/x10-2.4.2@27463 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.runtime/src-java/x10/x10rt/SocketTransport.java
+++ x10.runtime/src-java/x10/x10rt/SocketTransport.java
@@ -802,7 +802,15 @@ public class SocketTransport {
     	X10JavaDeserializer deserializer = new X10JavaDeserializer(new DataInputStream(new ByteArrayInputStream(input.array())));
     	FinishState finishState = (FinishState) deserializer.readObject();
     	Place src = (Place) deserializer.readObject();
-    	VoidFun_0_0 actObj = (VoidFun_0_0) deserializer.readObject();
+    	VoidFun_0_0 actObj;
+    	try {
+    	    actObj = (VoidFun_0_0) deserializer.readObject();
+    	} catch (Throwable e) {
+            finishState.notifyActivityCreation$O(src);
+            finishState.pushException(new x10.io.SerializationException(e));
+            finishState.notifyActivityTermination();
+            return;
+    	}
     	x10.lang.Runtime.execute(actObj, src, finishState);
     }
 }
