7a1a66f31192b12d2ce544134c49ca389a5c9e18
David Grove <groved@us.ibm.com>
2014-03-13 13:40:43 +0000

    Skip generating Fun_* and VoidFun_* .java file if it already exists   (WIP on attempting to avoid recompilation of Java files when nothing    has changed).
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@27536 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.compiler/src/x10c/util/MakeFun.java
+++ x10.compiler/src/x10c/util/MakeFun.java
@@ -52,6 +52,7 @@ public class MakeFun {
 	private static final int MAX_METHOD_PARAMS = 127;
 
 	public static void main(String[] args) throws FileNotFoundException {
+	    int numGenerated = 0;
 		int min = MIN_METHOD_PARAMS;
 		int max = MAX_METHOD_PARAMS;
 		if (args.length > 0) {
@@ -74,112 +75,121 @@ public class MakeFun {
 			 * Making Fun_0_n.java
 			 */
 			String fun_name = X10PrettyPrinterVisitor.X10_FUN_CLASS_NAME_PREFIX + "_0_" + n;
-			PrintStream fun_ps = new PrintStream(new FileOutputStream(new File(dir, fun_name + ".java")));
+			File fun_f = new File(dir, fun_name + ".java");
+			if (!fun_f.exists()) {
+			    PrintStream fun_ps = new PrintStream(new FileOutputStream(fun_f));
 
-			fun_ps.println(copyright);
-			fun_ps.println(dont_modify);
-			fun_ps.println(fun_imports);
+			    fun_ps.println(copyright);
+			    fun_ps.println(dont_modify);
+			    fun_ps.println(fun_imports);
 
-			// public interface Fun_0_2<T1,T2,U> extends Fun {
-			fun_ps.print("public interface " + fun_name + "<");
-			for (int i = 1; i <= n; ++i) {
-				fun_ps.print("T" + i + ",");
+			    // public interface Fun_0_2<T1,T2,U> extends Fun {
+			    fun_ps.print("public interface " + fun_name + "<");
+			    for (int i = 1; i <= n; ++i) {
+			        fun_ps.print("T" + i + ",");
+			    }
+			    fun_ps.println("U> extends " + X10PrettyPrinterVisitor.X10_FUN_CLASS_NAME_PREFIX + " {");
+
+			    //     U $apply(T1 o1, Type t1, T2 o2, Type t2);
+			    fun_ps.print("    U $apply(");
+			    for (int i = 1; i <= n; ++i) {
+			        fun_ps.print("T" + i + " o" + i + ", Type t" + i);
+			        if (i < n)
+			            fun_ps.print(", ");
+			    }
+			    fun_ps.println(");");
+
+			    fun_ps.println();
+
+			    //     public static final RuntimeType<Fun_0_2<?,?,?>> $RTT = FunType.<Fun_0_2<?,?,?>> make(
+			    String fun_unknowntypes = "<";
+			    for (int i = 1; i <= n + 1; ++i) {
+			        fun_unknowntypes += "?";
+			        if (i < n + 1)
+			            fun_unknowntypes += ",";
+			    }
+			    fun_unknowntypes += ">";
+			    fun_ps.print("    public static final RuntimeType<" + fun_name + fun_unknowntypes);
+			    fun_ps.println("> " + X10PrettyPrinterVisitor.RTT_NAME + " = " + X10PrettyPrinterVisitor.X10_FUN_CLASS_NAME_PREFIX + "Type.<" + fun_name + fun_unknowntypes + "> make(");
+
+			    //         Fun_0_2.class,
+			    fun_ps.println("        " + fun_name + ".class,");
+
+			    // 3
+			    fun_ps.println("        " + (n + 1));
+
+			    //     );
+			    // }
+			    fun_ps.println("    );");
+			    fun_ps.println("}");
+
+			    fun_ps.close();
+			    numGenerated++;
 			}
-			fun_ps.println("U> extends " + X10PrettyPrinterVisitor.X10_FUN_CLASS_NAME_PREFIX + " {");
-
-			//     U $apply(T1 o1, Type t1, T2 o2, Type t2);
-			fun_ps.print("    U $apply(");
-			for (int i = 1; i <= n; ++i) {
-				fun_ps.print("T" + i + " o" + i + ", Type t" + i);
-				if (i < n)
-					fun_ps.print(", ");
-			}
-			fun_ps.println(");");
-
-			fun_ps.println();
-
-			//     public static final RuntimeType<Fun_0_2<?,?,?>> $RTT = FunType.<Fun_0_2<?,?,?>> make(
-			String fun_unknowntypes = "<";
-			for (int i = 1; i <= n + 1; ++i) {
-				fun_unknowntypes += "?";
-				if (i < n + 1)
-					fun_unknowntypes += ",";
-			}
-			fun_unknowntypes += ">";
-			fun_ps.print("    public static final RuntimeType<" + fun_name + fun_unknowntypes);
-                        fun_ps.println("> " + X10PrettyPrinterVisitor.RTT_NAME + " = " + X10PrettyPrinterVisitor.X10_FUN_CLASS_NAME_PREFIX + "Type.<" + fun_name + fun_unknowntypes + "> make(");
-
-			//         Fun_0_2.class,
-			fun_ps.println("        " + fun_name + ".class,");
-
-			// 3
-			fun_ps.println("        " + (n + 1));
-
-			//     );
-			// }
-			fun_ps.println("    );");
-			fun_ps.println("}");
-
-			fun_ps.close();
-			
 			
 
 			/*
 			 * Making VoidFun_0_n.java
 			 */
 			String voidfun_name = X10PrettyPrinterVisitor.X10_VOIDFUN_CLASS_NAME_PREFIX + "_0_" + n;
-			PrintStream voidfun_ps = new PrintStream(new FileOutputStream(new File(dir, voidfun_name + ".java")));
+			File voidfun_f = new File(dir, voidfun_name + ".java");
+			if (!voidfun_f.exists()) {
+			    PrintStream voidfun_ps = new PrintStream(new FileOutputStream(voidfun_f));
 
-			voidfun_ps.println(copyright);
-			voidfun_ps.println(dont_modify);
-			voidfun_ps.println(voidfun_imports);
+			    voidfun_ps.println(copyright);
+			    voidfun_ps.println(dont_modify);
+			    voidfun_ps.println(voidfun_imports);
 
-			// public interface VoidFun_0_2<T1,T2> extends VoidFun {
-			voidfun_ps.print("public interface " + voidfun_name + "<");
-			for (int i = 1; i <= n; ++i) {
-				voidfun_ps.print("T" + i);
-				if (i < n)
-					voidfun_ps.print(",");
+			    // public interface VoidFun_0_2<T1,T2> extends VoidFun {
+			    voidfun_ps.print("public interface " + voidfun_name + "<");
+			    for (int i = 1; i <= n; ++i) {
+			        voidfun_ps.print("T" + i);
+			        if (i < n)
+			            voidfun_ps.print(",");
+			    }
+			    voidfun_ps.println("> extends " + X10PrettyPrinterVisitor.X10_VOIDFUN_CLASS_NAME_PREFIX + " {");
+
+			    //     Object $apply(T1 o1, Type t1, T2 o2, Type t2);
+			    voidfun_ps.print("    Object $apply(");
+			    for (int i = 1; i <= n; ++i) {
+			        voidfun_ps.print("T" + i + " o" + i + ", Type t" + i);
+			        if (i < n)
+			            voidfun_ps.print(", ");
+			    }
+			    voidfun_ps.println(");");
+
+			    voidfun_ps.println();
+
+			    //     public static final RuntimeType<VoidFun_0_2<?,?>> $RTT = VoidFunType.<VoidFun_0_2<?,?>> make(
+			    String voidfun_unknowntypes = "<";
+			    for (int i = 1; i <= n; ++i) {
+			        voidfun_unknowntypes += "?";
+			        if (i < n)
+			            voidfun_unknowntypes += ",";
+			    }
+			    voidfun_unknowntypes += ">";
+			    voidfun_ps.print("    public static final RuntimeType<" + voidfun_name + voidfun_unknowntypes);
+			    voidfun_ps.println("> " + X10PrettyPrinterVisitor.RTT_NAME + " = " + X10PrettyPrinterVisitor.X10_VOIDFUN_CLASS_NAME_PREFIX + "Type.<" + voidfun_name + voidfun_unknowntypes + "> make(");
+
+			    //         VoidFun_0_2.class,
+			    voidfun_ps.println("        " + voidfun_name + ".class,");
+
+			    // 2
+			    voidfun_ps.println("        " + n);
+
+			    //     );
+			    // }
+			    voidfun_ps.println("    );");
+			    voidfun_ps.println("}");
+
+			    voidfun_ps.close();
+			    numGenerated++;
 			}
-			voidfun_ps.println("> extends " + X10PrettyPrinterVisitor.X10_VOIDFUN_CLASS_NAME_PREFIX + " {");
-
-			//     Object $apply(T1 o1, Type t1, T2 o2, Type t2);
-			voidfun_ps.print("    Object $apply(");
-			for (int i = 1; i <= n; ++i) {
-				voidfun_ps.print("T" + i + " o" + i + ", Type t" + i);
-				if (i < n)
-					voidfun_ps.print(", ");
-			}
-			voidfun_ps.println(");");
-
-			voidfun_ps.println();
-
-			//     public static final RuntimeType<VoidFun_0_2<?,?>> $RTT = VoidFunType.<VoidFun_0_2<?,?>> make(
-			String voidfun_unknowntypes = "<";
-			for (int i = 1; i <= n; ++i) {
-				voidfun_unknowntypes += "?";
-				if (i < n)
-					voidfun_unknowntypes += ",";
-			}
-			voidfun_unknowntypes += ">";
-			voidfun_ps.print("    public static final RuntimeType<" + voidfun_name + voidfun_unknowntypes);
-                        voidfun_ps.println("> " + X10PrettyPrinterVisitor.RTT_NAME + " = " + X10PrettyPrinterVisitor.X10_VOIDFUN_CLASS_NAME_PREFIX + "Type.<" + voidfun_name + voidfun_unknowntypes + "> make(");
-			
-			//         VoidFun_0_2.class,
-			voidfun_ps.println("        " + voidfun_name + ".class,");
-			
-			// 2
-			voidfun_ps.println("        " + n);
-			
-			//     );
-			// }
-			voidfun_ps.println("    );");
-			voidfun_ps.println("}");
-
-			voidfun_ps.close();
 			
 		}
-
+		if (numGenerated > 0) {
+		    System.out.println("Generated "+numGenerated+" files.");
+		}
 	}
 
 }
