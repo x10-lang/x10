2559453b13ef453ff6a44259fde9e84a0a1dddfe
Mikio Takeuchi <mtake@jp.ibm.com>
2014-03-18 14:32:11 +0000

    merge r27558 (revert platform name to linux_ppc64 and change target for rpmbuild to ppc64) from trunk to x10-2.4.2 branch
    git-svn-id: svn://svn.code.sf.net/p/x10/code/branches/x10-2.4.2@27559 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.dist/releng/buildRelease.sh
+++ x10.dist/releng/buildRelease.sh
@@ -70,7 +70,7 @@ case "$UNAME" in
       X10_PLATFORM='linux_x86_64'
       ;;
   Linux,*86*,*) X10_PLATFORM='linux_x86';;
-  Linux,ppc64*,*) X10_PLATFORM='ppc64'
+  Linux,ppc64*,*) X10_PLATFORM='linux_ppc64'
       SHORT_HOSTNAME=`hostname -s`
       if [[ "$SHORT_HOSTNAME" == "f01c08n02-hf0" ]]; then 
           EXTRA_X10RT_BUILD_ARG="-DX10RT_PAMI=true -DX10RT_PAMI_IS_DEFAULT=true"
