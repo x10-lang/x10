83356ccdc034dc5fff705d31080c6724189cd13a
David Grove <groved@us.ibm.com>
2014-04-05 02:31:37 +0000

    Fix bug in Native X10 sleep implementation reported by Danny Munera. The calculated nanosecond wakeup time could have been larger than 1e9 since it includes a piece of the current time.  This results in an EINVAL return from pthread_cond_timedwait. The fix is to carry the excess nanos into the second field...
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@27605 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.runtime/src-cpp/x10/lang/Thread.cc
+++ x10.runtime/src-cpp/x10/lang/Thread.cc
@@ -376,9 +376,10 @@ Thread::sleep(x10_long millis, x10_int nanos)
     pthread_cleanup_push(thread_sleep_cleanup, (void *)cmp);
     gettimeofday(&tval, NULL);
     tout.tv_sec = tval.tv_sec + (millis/1000);
-    tout.tv_nsec = ((tval.tv_usec + ((millis%1000) * 1000)) * 1000) + nanos;
-    sleep_usec = (tout.tv_sec * 1000 * 1000) +
-                (tout.tv_nsec / 1000);
+    x10_long tout_nanos = ((tval.tv_usec + ((millis%1000) * 1000)) * 1000) + nanos;
+    tout.tv_sec += tout_nanos / 1000000000UL;
+    tout.tv_nsec = tout_nanos % 1000000000UL;
+    sleep_usec = (tout.tv_sec * 1000 * 1000) + (tout.tv_nsec / 1000);
     while (!done) {
         rc = pthread_cond_timedwait(&(cmp->cond), &(cmp->mutex), &tout);
         if (rc == ETIMEDOUT) {
