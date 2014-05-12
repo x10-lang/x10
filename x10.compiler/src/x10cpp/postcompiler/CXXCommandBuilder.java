630ff03d78bfe7752b98ae36411c72cac35bec0b
Mikio Takeuchi <mtake@jp.ibm.com>
2014-05-12 13:35:06 +0000

    add a comment
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@27688 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.compiler/src/x10cpp/postcompiler/CXXCommandBuilder.java
+++ x10.compiler/src/x10cpp/postcompiler/CXXCommandBuilder.java
@@ -416,6 +416,7 @@ public class CXXCommandBuilder {
     	ans.add("sm_21");
     	ans.add("sm_30");
     	ans.add("sm_35"); // requires CUDA Toolkit 5.0 Version 1.1 or newer
+//        ans.add("sm_50"); // requires CUDA Toolkit 6.0 or newer
     	return ans;
     }
 
