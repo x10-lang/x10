8cd843f073af1ab27981a645df8fbf195c2256b2
Josh Milthorpe <josh.milthorpe@anu.edu.au>
2014-06-20 13:28:59 +0000

    GML: restructure build scripts to add correct postargs to X10LIB properties file; progress on building for BG/Q (still can't run executable)
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@27844 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.gml/scripts/system_setting.mk
+++ x10.gml/scripts/system_setting.mk
@@ -62,23 +62,30 @@ ADD_LAPACK	= yes
 
 # BLAS and LAPACK compiler options
 ifdef ADD_BLAS
-    X10CXXFLAGS += -cxx-prearg -DENABLE_BLAS
+    X10CXX_PREARGS += -cxx-prearg -DENABLE_BLAS
 	add_jblas	= chk_jblas
     ifdef ADD_LAPACK
-        X10CXXFLAGS += -cxx-prearg -DENABLE_LAPACK
-		add_jlapack	= chk_jlapack
+        X10CXX_PREARGS += -cxx-prearg -DENABLE_LAPACK
+	add_jlapack	= chk_jlapack
     endif
 endif
 
 # BLAS and LAPACK linker options
 ifdef BGQ
-# Blue Gene/Q compiler settings
-    BLASLIB = ESSL
+    # Blue Gene/Q compiler settings
     CXX = /bgsys/drivers/ppcfloor/gnu-linux/bin/powerpc64-bgq-linux-g++
+    # IBM ESSL on Blue Gene/Q
+    BLASLIB = ESSL
     ifdef ADD_BLAS
-        XLSMP_LIB_PATH = /opt/ibmcmp/xlsmp/bg/1.7/lib
-        XLF_LIB_PATH = /opt/ibmcmp/xlf/bg/11.1/lib
-        POST_LDFLAGS += -L/opt/ibmmath/lib -L/opt/ibmcmp/vac/bg/9.0/lib
+        IBMCMP_ROOT = /opt/ibmcmp
+        XLSMP_LIB_PATH = $(IBMCMP_ROOT)/xlsmp/bg/3.1/bglib64
+        XLMASS_LIB_PATH = $(IBMCMP_ROOT)/xlmass/bg/7.3/bglib64
+        XLF_LIB_PATH = $(IBMCMP_ROOT)/xlf/bg/14.1/bglib64
+        ESSL_LIB_PATH = /opt/ibmmath/essl/5.1/lib64
+        ESSL_LIB = esslsmpbg
+        # need mass lib on BG/Q
+        X10CXX_POSTARGS += -cxx-postarg -Wl,--allow-multiple-definition -cxx-postarg -L$(XLMASS_LIB_PATH) -cxx-postarg -lmassv -cxx-postarg -lmass
+        #X10CXXFLAGS += -cxx-postarg -L/opt/ibmcmp/vac/bg/9.0/lib
     endif
 endif
 
@@ -87,37 +94,41 @@ BLASLIB ?=
 
 ifeq ($(BLASLIB),ESSL)
     # IBM ESSL
-    X10CXXFLAGS += -cxx-prearg -D__essl__
+    X10CXX_PREARGS += -cxx-prearg -D__essl__
     ifdef ADD_BLAS
-        XLSMP_LIB_PATH ?= /opt/ibmcmp/xlsmp/3.1/lib64
-        XLF_LIB_PATH ?= /opt/ibmcmp/xlf/14.1/lib64
-        POST_LDFLAGS += -L/usr/lib64 -L$(XLSMP_LIB_PATH) -L$(XLF_LIB_PATH) -lesslsmp6464 -lxlf90_r -lxl -lxlsmp -lxlfmath
+        IBMCMP_ROOT = /opt/ibmcmp
+        XLSMP_LIB_PATH ?= $(IBMCMP_ROOT)/xlsmp/3.1/lib64
+        XLF_LIB_PATH ?= $(IBMCMP_ROOT)/xlf/14.1/lib64
+        ESSL_LIB_PATH ?= /usr/lib64
+        ESSL_LIB ?= esslsmp6464
         ifdef ADD_LAPACK
-            POST_LDFLAGS += -llapack
+            X10CXX_POSTARGS += -cxx-postarg -llapack
         endif
+        X10CXX_POSTARGS += -cxx-postarg -L$(ESSL_LIB_PATH) -cxx-postarg -l$(ESSL_LIB) -cxx-postarg -L$(XLF_LIB_PATH) -cxx-postarg -lxlf90_r -cxx-postarg -L$(XLSMP_LIB_PATH) -cxx-postarg -lxlsmp -cxx-postarg -lxlopt -cxx-postarg -lxlfmath -cxx-postarg -lxl
+
     endif
 else
 ifeq ($(BLASLIB),GotoBLAS2)
     # GotoBLAS2
     ifdef ADD_BLAS
-        POST_LDFLAGS += -L$(HOME)/GotoBLAS2 -lgoto2
+        X10CXX_POSTARGS += -cxx-postarg -L$(HOME)/GotoBLAS2 -cxx-postarg -lgoto2
         ifdef ADD_LAPACK
-            POST_LDFLAGS += -llapack
+            X10CXX_POSTARGS += -cxx-postarg -llapack
         endif
     endif
 else
 ifeq ($(BLASLIB),MKL)
     # Intel Math Kernel Library (LP64, 64-bit, libgomp)
     ifdef ADD_BLAS
-        POST_LDFLAGS += -L$(MKLROOT)/lib/intel64 -lmkl_intel_lp64 -lmkl_core -lmkl_gnu_thread -ldl
-        POST_CXXFLAGS += -fopenmp -m64 -I$(MKLROOT)/include
+        X10CXX_POSTARGS += -cxx-postarg -L$(MKLROOT)/lib/intel64 -cxx-postarg -lmkl_intel_lp64 -cxx-postarg -lmkl_core -cxx-postarg -lmkl_gnu_thread -cxx-postarg -ldl
+        X10CXX_PREARGS += -cxx-prearg -fopenmp -cxx-prearg -m64 -cxx-prearg -I$(MKLROOT)/include
     endif
 else
     # assume NetLib reference BLAS/LAPACK
     ifdef ADD_BLAS
-        POST_LDFLAGS += -L/usr/lib64 -lblas
+        X10CXX_POSTARGS += -cxx-postarg -L/usr/lib64 -cxx-postarg -lblas
         ifdef ADD_LAPACK
-            POST_LDFLAGS += -llapack
+            X10CXX_POSTARGS += -cxx-postarg -llapack
         endif
     endif
 endif
