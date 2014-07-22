e09108451a3ea5a1e46cb7fbd4b9c7beaf3fb877
David Grove <groved@us.ibm.com>
2014-07-22 13:56:48 +0000

    Add logic so that x10rt-level emulated collectives will only be used if X10RT_FORCE_NATIVE_COLLECTIVES is set.  This makes the X10 emulated collectives in Team.x10 the default for x10rt_sockets for both Native and Managed X10.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@27980 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.runtime/x10rt/common/x10rt_logical.cc
+++ x10.runtime/x10rt/common/x10rt_logical.cc
@@ -957,8 +957,8 @@ void x10rt_lgl_finalize (void)
 
 x10rt_coll_type x10rt_lgl_coll_support () {
 	x10rt_coll_type v = x10rt_net_coll_support();
-	if (v == X10RT_COLL_NOCOLLECTIVES)
-		return X10RT_COLL_ALLNONBLOCKINGCOLLECTIVES; // use local implementation
+	if (v == X10RT_COLL_NOCOLLECTIVES && checkBoolEnvVar(getenv("X10RT_FORCE_NATIVE_COLLECTIVES")))
+        return X10RT_COLL_ALLNONBLOCKINGCOLLECTIVES; // use local implementation
 	else
 		return v;
 }
