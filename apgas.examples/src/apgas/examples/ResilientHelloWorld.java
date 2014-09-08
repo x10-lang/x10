3b3e3b2039f64ca2d97933899bc06f3d4257d1ef
Olivier Tardieu <tardieu@us.ibm.com>
2014-09-08 18:29:23 +0000

    apgas: checking exception in ResilientHelloWorld
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@28182 a326200e-df2c-42e0-81fa-597e909af41d

--- apgas.examples/src/apgas/examples/ResilientHelloWorld.java
+++ apgas.examples/src/apgas/examples/ResilientHelloWorld.java
@@ -21,6 +21,21 @@ import apgas.NoSuchPlaceException;
 import apgas.Place;
 
 final class ResilientHelloWorld {
+  public static boolean bad(Throwable e) {
+    if (e instanceof NoSuchPlaceException) {
+      return false;
+    }
+    if (e instanceof MultipleException) {
+      for (final Throwable t : e.getSuppressed()) {
+        if (bad(t)) {
+          return true;
+        }
+      }
+      return false;
+    }
+    return true;
+  }
+
   public static void main(String[] args) {
     System.setProperty(Configuration.APGAS_RESILIENT, "true");
     System.setProperty(Configuration.APGAS_SERIALIZATION_EXCEPTION, "true");
@@ -43,7 +58,11 @@ final class ResilientHelloWorld {
           }
         });
       } catch (final MultipleException | NoSuchPlaceException e) {
-        System.err.println("Ignoring Exception");
+        if (bad(e)) {
+          e.printStackTrace();
+        } else {
+          System.err.println("Ignoring NoSuchPlaceException");
+        }
         try {
           Thread.sleep(2000);
         } catch (final InterruptedException x) {
