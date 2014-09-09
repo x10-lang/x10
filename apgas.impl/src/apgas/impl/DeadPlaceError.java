4a7959556cd2c3ff3473c4db4f81fd17c746cdc5
Olivier Tardieu <tardieu@us.ibm.com>
2014-09-09 02:21:07 +0000

    apgas: added missing file
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@28186 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ apgas.impl/src/apgas/impl/DeadPlaceError.java
@@ -0,0 +1,26 @@
+/*
+ *  This file is part of the X10 project (http://x10-lang.org).
+ *
+ *  This file is licensed to You under the Eclipse Public License (EPL);
+ *  You may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *      http://www.opensource.org/licenses/eclipse-1.0.php
+ *
+ *  (C) Copyright IBM Corporation 2006-2014.
+ */
+
+package apgas.impl;
+
+/**
+ * A {@link DeadPlaceError} is thrown when attempting a finish state change
+ * from a place that is considered dead by the finish.
+ */
+public class DeadPlaceError extends RuntimeException {
+  private static final long serialVersionUID = -6291716310951978192L;
+
+  /**
+   * Constructs a new {@link DeadPlaceError}.
+   */
+  public DeadPlaceError() {
+  }
+}
