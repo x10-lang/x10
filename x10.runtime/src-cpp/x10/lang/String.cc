e754e240c195700c6b6a051c9b9cf11d42e6451c
Josh Milthorpe <josh.milthorpe@anu.edu.au>
2014-09-23 15:35:48 +0000

    remove space after backslash to suppress g++ warning
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@28263 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.runtime/src-cpp/x10/lang/String.cc
+++ x10.runtime/src-cpp/x10/lang/String.cc
@@ -533,7 +533,7 @@ String* String::__plus(x10_boolean p1, String* p2) {
 String* String::__plus(String* p1, T p2) { \
     char buf[SZ]; \
     size_t used = (size_t)(::snprintf(buf, SZ, FMT, (C)p2)); \
-    if (NULL == p1) { p1 = makeStringLit("null"); } \ 
+    if (NULL == p1) { p1 = makeStringLit("null"); } \
     std::size_t newLength = p1->FMGL(content_length) + used; \
     char *newChars = x10aux::alloc<char>(newLength+1, false); \
     memcpy(newChars, p1->FMGL(content), p1->FMGL(content_length)); \
@@ -544,7 +544,7 @@ String* String::__plus(String* p1, T p2) { \
 String* String::__plus(T p1, String* p2) { \
     char buf[SZ]; \
     size_t used = (size_t)(::snprintf(buf, SZ, FMT, (C)p1)); \
-    if (NULL == p2) { p2 = makeStringLit("null"); } \ 
+    if (NULL == p2) { p2 = makeStringLit("null"); } \
     std::size_t newLength = used + p2->FMGL(content_length); \
     char *newChars = x10aux::alloc<char>(newLength+1, false); \
     memcpy(newChars, buf, used);                              \
