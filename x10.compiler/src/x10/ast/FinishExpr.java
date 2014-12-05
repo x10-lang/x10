88dd0559b837ff5c9305274b140598563bf014ed
Mikio Takeuchi <mtake@jp.ibm.com>
2014-12-05 11:59:00 +0000

    add standard copyright header
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@28674 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.compiler/src/x10/ast/FinishExpr.java
+++ x10.compiler/src/x10/ast/FinishExpr.java
@@ -1,6 +1,14 @@
-/**
- * 
+/*
+ *  This file is part of the X10 project (http://x10-lang.org).
+ *
+ *  This file is licensed to You under the Eclipse Public License (EPL);
+ *  You may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *      http://www.opensource.org/licenses/eclipse-1.0.php
+ *
+ *  (C) Copyright IBM Corporation 2006-2014.
  */
+
 package x10.ast;
 
 import java.util.List;
@@ -15,11 +23,8 @@ import polyglot.visit.CFGBuilder;
 /**
  * An implementation of collecting finish.
  * @author vj
- *
  */
-public interface FinishExpr extends Expr{
-
-
+public interface FinishExpr extends Expr {
 	Expr reducer();
 	Stmt body();
 }
--- x10.compiler/src/x10/types/constraints/ConstraintManager.java
+++ x10.compiler/src/x10/types/constraints/ConstraintManager.java
@@ -1,8 +1,18 @@
+/*
+ *  This file is part of the X10 project (http://x10-lang.org).
+ *
+ *  This file is licensed to You under the Eclipse Public License (EPL);
+ *  You may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *      http://www.opensource.org/licenses/eclipse-1.0.php
+ *
+ *  (C) Copyright IBM Corporation 2006-2014.
+ */
+
 package x10.types.constraints;
 
 import x10.constraint.XConstraintSystem;
 import x10.types.constraints.xnative.CNativeConstraintSystem;
-
 
 
 public class ConstraintManager {
