2276aec8fa3af806ba5d0bdf89a868d5df3fa600
Benjamin Herta <bherta@us.ibm.com>
2015-01-15 21:45:21 +0000

    Look for any immediate threads before enabling lockless context usage in PAMI
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@28847 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.runtime/x10rt/pami/x10rt_pami.cc
+++ x10.runtime/x10rt/pami/x10rt_pami.cc
@@ -952,7 +952,8 @@ x10rt_error x10rt_net_init (int *argc, char ***argv, x10rt_msg_type *counter)
 	// determine the level of parallelism we need to support
 	char* value = getenv("X10_STATIC_THREADS");
 	char* nthreads = getenv("X10_NTHREADS");
-	if (nthreads && checkBoolEnvVar(value) && state.numParallelContexts >= atoi(nthreads))
+	char* ithreads = getenv("X10_NUM_IMMEDIATE_THREADS");
+	if (nthreads && ithreads && checkBoolEnvVar(value) && state.numParallelContexts >= atoi(nthreads) && 0 == atoi(nthreads))
 	{
 		state.numParallelContexts = atoi(nthreads); // use specified nthreads, not possible nthreads
 		// We have as many endpoints as we have X10_NTHREADS
