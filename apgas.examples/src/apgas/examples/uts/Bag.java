8579a38f24197e57edc18edb7f1661b9256fbd66
Olivier Tardieu <tardieu@us.ibm.com>
2015-04-01 20:38:02 +0000

    [APGAS] uts cleanup
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@29279 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ apgas.examples/src/apgas/examples/uts/Bag.java
@@ -0,0 +1,205 @@
+/*
+ *  This file is part of the X10 project (http://x10-lang.org).
+ *
+ *  This file is licensed to You under the Eclipse Public License (EPL);
+ *  You may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *      http://www.opensource.org/licenses/eclipse-1.0.php
+ *
+ *  (C) Copyright IBM Corporation 2006-2014.
+ */
+
+package apgas.examples.uts;
+
+import java.io.Serializable;
+import java.security.DigestException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+final class Bag implements Serializable {
+  private static final long serialVersionUID = 2200935927036145803L;
+
+  // branching factor: 4
+  private static final double den = Math.log(4.0 / (1.0 + 4.0));
+
+  static MessageDigest encoder() {
+    try {
+      return MessageDigest.getInstance("SHA-1");
+    } catch (final NoSuchAlgorithmException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private byte[] hash;
+  private int[] depth;
+  private int[] lower;
+  private int[] upper;
+  int size; // number of nodes in the bag
+  long count; // number of nodes processed so far
+
+  Bag(int n) {
+    hash = new byte[n * 20 + 4]; // slack for in-place SHA1 computation
+    depth = new int[n];
+    lower = new int[n];
+    upper = new int[n];
+  }
+
+  private void digest(MessageDigest md, int d) throws DigestException {
+    if (size >= depth.length) {
+      grow();
+    }
+    ++count;
+    final int offset = size * 20;
+    md.digest(hash, offset, 20);
+    final int v = ((0x7f & hash[offset + 16]) << 24)
+        | ((0xff & hash[offset + 17]) << 16)
+        | ((0xff & hash[offset + 18]) << 8) | (0xff & hash[offset + 19]);
+    final int n = (int) (Math.log(1.0 - v / 2147483648.0) / den);
+    if (n > 0) {
+      if (d > 1) {
+        depth[size] = d - 1;
+        lower[size] = 0;
+        upper[size++] = n;
+      } else {
+        count += n;
+      }
+    }
+  }
+
+  void seed(MessageDigest md, int s, int d) {
+    try {
+      hash[16] = (byte) (s >> 24);
+      hash[17] = (byte) (s >> 16);
+      hash[18] = (byte) (s >> 8);
+      hash[19] = (byte) s;
+      md.update(hash, 0, 20);
+      digest(md, d);
+    } catch (final DigestException e) {
+    }
+  }
+
+  void expand(MessageDigest md) throws DigestException {
+    final int top = size - 1;
+    final int d = depth[top];
+    final int l = lower[top];
+    final int u = upper[top] - 1;
+    if (u == l) {
+      --size;
+    } else {
+      upper[top] = u;
+    }
+    final int offset = top * 20;
+    hash[offset + 20] = (byte) (u >> 24);
+    hash[offset + 21] = (byte) (u >> 16);
+    hash[offset + 22] = (byte) (u >> 8);
+    hash[offset + 23] = (byte) u;
+    md.update(hash, offset, 24);
+    digest(md, d);
+  }
+
+  void run(MessageDigest md) {
+    try {
+      while (size > 0) {
+        expand(md);
+      }
+    } catch (final DigestException e) {
+    }
+  }
+
+  Bag trim() {
+    final Bag b = new Bag(size);
+    System.arraycopy(hash, 0, b.hash, 0, size * 20);
+    System.arraycopy(depth, 0, b.depth, 0, size);
+    System.arraycopy(lower, 0, b.lower, 0, size);
+    System.arraycopy(upper, 0, b.upper, 0, size);
+    b.size = size;
+    b.count = count;
+    return b;
+  }
+
+  Bag split() {
+    int s = 0;
+    for (int i = 0; i < size; ++i) {
+      if ((upper[i] - lower[i]) >= 2) {
+        ++s;
+      }
+    }
+    if (s == 0) {
+      return null;
+    }
+    final Bag b = new Bag(s);
+    for (int i = 0; i < size; ++i) {
+      final int p = upper[i] - lower[i];
+      if (p >= 2) {
+        System.arraycopy(hash, i * 20, b.hash, b.size * 20, 20);
+        b.depth[b.size] = depth[i];
+        b.upper[b.size] = upper[i];
+        upper[i] -= p / 2;
+        b.lower[b.size++] = upper[i];
+      }
+    }
+    return b;
+  }
+
+  void merge(Bag b) {
+    while (size + b.size > depth.length) {
+      grow();
+    }
+    System.arraycopy(b.hash, 0, hash, size * 20, b.size * 20);
+    System.arraycopy(b.depth, 0, depth, size, b.size);
+    System.arraycopy(b.lower, 0, lower, size, b.size);
+    System.arraycopy(b.upper, 0, upper, size, b.size);
+    size += b.size;
+  }
+
+  private void grow() {
+    final int n = depth.length * 2;
+    final byte[] h = new byte[n * 20 + 4];
+    final int[] d = new int[n];
+    final int[] l = new int[n];
+    final int[] u = new int[n];
+    System.arraycopy(hash, 0, h, 0, size * 20);
+    System.arraycopy(depth, 0, d, 0, size);
+    System.arraycopy(lower, 0, l, 0, size);
+    System.arraycopy(upper, 0, u, 0, size);
+    hash = h;
+    depth = d;
+    lower = l;
+    upper = u;
+  }
+
+  static String sub(String str, int start, int end) {
+    return str.substring(start, Math.min(end, str.length()));
+  }
+
+  public static void main(String[] args) {
+    int depth = 13;
+    try {
+      depth = Integer.parseInt(args[0]);
+    } catch (final Exception e) {
+    }
+
+    final MessageDigest md = encoder();
+    Bag bag = new Bag(5);
+
+    System.out.println("Warmup...");
+    bag.seed(md, 19, depth - 2);
+    bag.run(md);
+
+    bag = new Bag(5);
+
+    System.out.println("Starting...");
+    long time = System.nanoTime();
+
+    bag.seed(md, 19, depth);
+    bag.run(md);
+    final long count = bag.count;
+
+    time = System.nanoTime() - time;
+    System.out.println("Finished.");
+
+    System.out.println("Depth: " + depth + ", Performance: " + count + "/"
+        + sub("" + time / 1e9, 0, 6) + " = "
+        + sub("" + (count / (time / 1e3)), 0, 6) + "M nodes/s");
+  }
+}
---
+++ apgas.examples/src/apgas/examples/uts/ResilientUTS.java
@@ -0,0 +1,320 @@
+/*
+ *  This file is part of the X10 project (http://x10-lang.org).
+ *
+ *  This file is licensed to You under the Eclipse Public License (EPL);
+ *  You may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *      http://www.opensource.org/licenses/eclipse-1.0.php
+ *
+ *  (C) Copyright IBM Corporation 2006-2014.
+ */
+
+package apgas.examples.uts;
+
+import static apgas.Constructs.*;
+
+import java.security.DigestException;
+import java.security.MessageDigest;
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import apgas.Configuration;
+import apgas.DeadPlaceException;
+import apgas.GlobalRuntime;
+import apgas.MultipleException;
+import apgas.Place;
+
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
+import com.hazelcast.core.TransactionalMap;
+import com.hazelcast.transaction.TransactionException;
+import com.hazelcast.transaction.TransactionalTask;
+import com.hazelcast.transaction.TransactionalTaskContext;
+
+final class ResilientUTS {
+  static ResilientUTS uts;
+
+  synchronized void handle(Place p) {
+    // p is dead, unblock if waiting on p
+    if (state == p.id) {
+      // attempt to extract loot from store
+      final Bag b = map.get(home);
+      if (b.size != 0) {
+        bag.merge(b);
+      }
+      state = -1;
+      notifyAll();
+    }
+  }
+
+  final HazelcastInstance hz = Hazelcast.getHazelcastInstanceByName("apgas");
+  final IMap<Place, Bag> map = hz.getMap("map");
+  final Place home = here();
+  final int places = places().size();
+
+  final Random random = new Random();
+  final MessageDigest md = Bag.encoder();
+  Bag bag = new Bag(64);
+  long transfers;
+
+  final ConcurrentLinkedQueue<Place> thieves = new ConcurrentLinkedQueue<Place>();
+  AtomicBoolean lifeline = new AtomicBoolean(home.id != places - 1);
+  int state = -2; // -2: inactive, -1: running, p: stealing from p
+
+  void init(int s, int d) {
+    bag.seed(md, s, d);
+    map.set(home, bag.trim());
+  }
+
+  void run() throws DigestException {
+    System.err.println(here() + " starting");
+    synchronized (this) {
+      state = -1;
+    }
+    while (bag.size > 0) {
+      while (bag.size > 0) {
+        for (int n = 500; (n > 0) && (bag.size > 0); --n) {
+          bag.expand(md);
+        }
+        distribute();
+      }
+      map.set(home, bag.trim());
+      steal();
+    }
+    synchronized (this) {
+      state = -2;
+    }
+    distribute();
+    lifelinesteal();
+    System.err.println(here() + " stopping");
+  }
+
+  void lifelinesteal() {
+    if (places == 1) {
+      return;
+    }
+    try {
+      asyncat(place((here().id + places - 1) % places), () -> {
+        uts.lifeline.set(true);
+      });
+    } catch (final DeadPlaceException e) {
+      // TODO should go to next lifeline, but correct as is
+    }
+  }
+
+  void steal() {
+    if (places == 1) {
+      return;
+    }
+    final Place from = home;
+    int p = random.nextInt(places - 1);
+    if (p >= from.id) {
+      p++;
+    }
+    if (!places().contains(place(p))) {
+      // TODO should try other place, but ok as is
+      return;
+    }
+    synchronized (this) {
+      state = p;
+    }
+    try {
+      uncountedasyncat(place(p), () -> {
+        uts.request(from);
+      });
+    } catch (final DeadPlaceException e) {
+      // pretend stealing failed
+      // TODO should try other place, but ok as is
+      synchronized (this) {
+        state = -1;
+      }
+    }
+    synchronized (this) {
+      while (state >= 0) {
+        try {
+          wait();
+        } catch (final InterruptedException e) {
+        }
+      }
+    }
+  }
+
+  void request(Place p) {
+    synchronized (this) {
+      if (state == -1) {
+        thieves.add(p);
+        return;
+      }
+    }
+    try {
+      final Place h = home;
+      uncountedasyncat(p, () -> {
+        uts.deal(h, null);
+      });
+    } catch (final DeadPlaceException e) {
+      // place is dead, nothing to do
+    }
+  }
+
+  void lifelinedeal(Bag b) throws DigestException {
+    bag.merge(b);
+    run();
+  }
+
+  synchronized void deal(Place p, Bag b) {
+    if (state != p.id) {
+      // victim is dead, ignore late distribution
+      return;
+    }
+    if (b != null) {
+      bag.merge(b);
+    }
+    state = -1;
+    notifyAll();
+  }
+
+  void transfer(Place p, Bag b) {
+    transfers++;
+    while (true) {
+      try {
+        hz.executeTransaction(new TransactionalTask<Object>() {
+          @Override
+          public Object execute(TransactionalTaskContext context)
+              throws TransactionException {
+            final TransactionalMap<Place, Bag> map = context.getMap("map");
+            map.set(home, bag.trim());
+            final Bag old = map.getForUpdate(p);
+            b.count = old == null ? 0 : old.count;
+            map.set(p, b);
+            return null;
+          }
+        });
+        return;
+      } catch (final Throwable t) {
+        System.err.println("Exception in transaction at " + home
+            + "... retrying");
+        t.printStackTrace();
+      }
+    }
+  }
+
+  void distribute() {
+    Place p;
+    if (lifeline.get()) {
+      final Bag b = bag.split();
+      if (b != null) {
+        p = place((here().id + 1) % places);
+        lifeline.set(false);
+        transfer(p, b);
+        try {
+          asyncat(p, () -> {
+            uts.lifelinedeal(b);
+          });
+        } catch (final DeadPlaceException e) {
+          // thief died, nothing to do
+        }
+      }
+    }
+    while ((p = thieves.poll()) != null) {
+      final Bag b = bag.split();
+      if (b != null) {
+        transfer(p, b);
+      }
+      try {
+        final Place h = home;
+        uncountedasyncat(p, () -> {
+          uts.deal(h, b);
+        });
+      } catch (final DeadPlaceException e) {
+        // thief died, nothing to do
+      }
+    }
+  }
+
+  static String sub(String str, int start, int end) {
+    return str.substring(start, Math.min(end, str.length()));
+  }
+
+  public static void init() {
+    GlobalRuntime.getRuntime();
+    uts = new ResilientUTS();
+    GlobalRuntime.getRuntime().setPlaceFailureHandler(place -> {
+      if (place.id > 0) {
+        System.err.println(here() + " observes that " + place + " failed!");
+      }
+      uts.handle(place);
+    });
+  }
+
+  public static void main(String[] args) {
+    int depth = 13;
+    try {
+      depth = Integer.parseInt(args[0]);
+    } catch (final Exception e) {
+    }
+
+    if (System.getProperty(Configuration.APGAS_PLACES) == null) {
+      System.setProperty(Configuration.APGAS_PLACES, "4");
+    }
+    System.setProperty(Configuration.APGAS_SERIALIZATION_EXCEPTION, "true");
+    System.setProperty(Configuration.APGAS_RESILIENT, "true");
+
+    // initialize worker and handler in each place
+    finish(() -> {
+      for (final Place p : places()) {
+        asyncat(p, () -> {
+          init();
+        });
+      }
+    });
+
+    System.out.println("Starting...");
+    long time = System.nanoTime();
+
+    try {
+      uts.init(19, depth); // seed: 19
+      finish(() -> {
+        uts.run();
+      });
+    } catch (final MultipleException e) {
+      if (!e.isDeadPlaceException()) {
+        throw e;
+      }
+    }
+
+    // if places have died, process remaning nodes seqentially at place 0
+    for (final Map.Entry<Place, Bag> e : uts.map.entrySet()) {
+      final Bag b = e.getValue();
+      if (b != null && b.size != 0) {
+        System.err.println("Recovering " + e.getKey());
+        b.run(uts.md);
+        uts.map.set(e.getKey(), b.trim());
+      }
+    }
+
+    long count = 0;
+    // collect all counts
+    for (final Bag b : uts.map.values()) {
+      count += b.count;
+    }
+
+    time = System.nanoTime() - time;
+    System.out.println("Finished.");
+
+    long transfers = 0;
+    // collect all counts
+    for (final Place p : places()) {
+      transfers += at(p, () -> {
+        return uts.transfers;
+      });
+    }
+
+    System.out.println("Performance: " + count + "/"
+        + sub("" + time / 1e9, 0, 6) + " = "
+        + sub("" + (count / (time / 1e3)), 0, 6) + "M nodes/s using "
+        + transfers + " transactions");
+  }
+}
