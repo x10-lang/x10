945185aa5da8ae4fa06e3704739f1b9449a290d8
Olivier Tardieu <tardieu@us.ibm.com>
2015-06-08 03:11:22 +0000

    [APGAS] invoking ant from maven
    git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@29560 a326200e-df2c-42e0-81fa-597e909af41d

--- x10.dist/releng/buildApgas.sh
+++ x10.dist/releng/buildApgas.sh
@@ -1,19 +1,18 @@
 #!/bin/sh
 
 cd /tmp
-rm -Rf eclipse.jdt.core
+rm -Rf apgas-build 
+mkdir apgas-build
 
-git clone -b R4_4_2 https://git.eclipse.org/r/jdt/eclipse.jdt.core
-svn co --depth=empty https://svn.code.sf.net/p/x10/code/x10dt/trunk eclipse.jdt.core
-cd eclipse.jdt.core
+git clone -b R4_4_2 https://git.eclipse.org/r/jdt/eclipse.jdt.core apgas-build
+svn co --depth=empty https://svn.code.sf.net/p/x10/code/x10dt/trunk apgas-build
+cd apgas-build
 svn update apgas.compiler apgas.runtime apgas.ui apgas.tools apgas.site apgas.parent
 svn co https://svn.code.sf.net/p/x10/code/trunk/apgas
 svn co https://svn.code.sf.net/p/x10/code/trunk/apgas.examples
 patch -p1 < apgas.compiler/R4_4_2.patch
-cd apgas
-ant
-cd ../apgas.parent
+cd apgas.parent
 mvn -P build-individual-bundles package
-cd /tmp
-cp eclipse.jdt.core/apgas/apgas.zip .
-cp eclipse.jdt.core/apgas.site/target/site_assembly.zip apgas-eclipse.zip
+cd ..
+cp apgas/apgas.zip .
+cp apgas.site/target/site_assembly.zip apgas-eclipse.zip
