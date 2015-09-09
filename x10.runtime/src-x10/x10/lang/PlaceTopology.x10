x10-lang/x10

commit 7442dbc3a447b61c2058a32c37fe3779cbbdaae2

Eliminate duplicate copies of the number of places and number of hosts from Native X10, changing them to calls down to the x10rt layer.  This is a necessary step on the path to elastic Native X10.
git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@29243 a326200e-df2c-42e0-81fa-597e909af41d

x10.runtime/src-x10/x10/lang/PlaceTopology.x10
@@ -106,11 +106,11 @@ public abstract class PlaceTopology {
 
 
     @Native("java", "((long)x10.x10rt.X10RT.numPlaces())")
-    @Native("c++", "((x10_long)::x10aux::num_places)")
+    @Native("c++", "((x10_long)x10rt_nplaces())")
     protected static native def num_all_places():Long;
 
     @Native("java", "((long)x10.x10rt.X10RT.numPlaces())")
-    @Native("c++", "((x10_long)::x10aux::num_hosts)")
+    @Native("c++", "((x10_long)x10rt_nplaces())")
     protected static native def num_primary_places():Long;
 
     /**

x10.runtime/src-cpp/x10aux/network.cc
@@ -38,8 +38,6 @@ using namespace x10::lang;
 using namespace x10aux;
 
 // caches to avoid repeatedly calling into x10rt for trivial things
-x10aux::place x10aux::num_places = 0;
-x10aux::place x10aux::num_hosts = 0;
 x10aux::place x10aux::here = -1;
 bool x10aux::x10rt_initialized = false;
 x10_int x10aux::num_local_cores = 1; // this will be set in template_main
@@ -163,8 +161,6 @@ void x10aux::network_init (int ac, char **av) {
         abort();
     }
     x10aux::here = x10rt_here();
-    x10aux::num_places = x10rt_nplaces();
-    x10aux::num_hosts = x10rt_nhosts();
     remote_op_batch = get_remote_op_batch();
     opv = (x10rt_remote_op_params*)malloc(remote_op_batch * sizeof(*opv));
 }
@@ -255,7 +251,7 @@ void x10aux::run_closure_at(x10aux::place p, x10::lang::VoidFun_0_0* body_fun,
         <<" nid "<<net_id<<" to place: "<<p);
 
     assert(p!=here); // this case should be handled earlier
-    assert(p<num_places); // this is ensured by XRX runtime
+    assert(p<x10rt_nplaces()); // this is ensured by XRX runtime
 
     assert(!is_cuda(p));
 

x10.runtime/src-cpp/x10aux/network.h
@@ -51,8 +51,6 @@ namespace x10aux {
     extern msg_type kernel_put;
 
     // caches to avoid repeatedly calling into x10rt for trivial things
-    extern place num_places;
-    extern place num_hosts;
     extern place here;
 
     // can be used to test whether the above caches contain valid data

x10.runtime/src-x10/x10/lang/Place.x10
@@ -45,14 +45,14 @@ public final struct Place(
      * Invariant: Place.numPlaces() == Place.places().numPlaces().
      */
     @Native("java", "((long)x10.x10rt.X10RT.numPlaces())")
-    @Native("c++", "((x10_long)::x10aux::num_hosts)")
+    @Native("c++", "((x10_long)x10rt_nplaces())")
     public static native def numPlaces():Long;
     
     /** 
      * The total number of all kinds of places (both primary and children/accelerators).
      */
     @Native("java", "((long)x10.x10rt.X10RT.numPlaces())")
-    @Native("c++", "((x10_long)::x10aux::num_places)")
+    @Native("c++", "((x10_long)x10rt_nplaces())")
     public static native def numAllPlaces():Long;
 
     /** 

commit 6cce17f84961d0cce710a5ca517808646d95934d

Fix bug in hosts vs. accelerator identification in PlaceTopology
Fix bug introduced in 7442dbc when as part of resilient/elastic
X10RT work the caching of x10rt_nplaces and x10rt_nhosts was
eliminated. The @Native annotation for PlaceTopology.num_primary_places()
was changed to call x10rt_nplaces() when it should have been calling
x10rt_nhosts().  As a result, all of the CUDA sample programs
were not actually using GPUs, because they rely on the PlaceTopology
functions to determine whether or not GPUs are present.

x10.runtime/src-x10/x10/lang/PlaceTopology.x10
@@ -110,7 +110,7 @@ public abstract class PlaceTopology {
     protected static native def num_all_places():Long;
 
     @Native("java", "((long)x10.x10rt.X10RT.numPlaces())")
-    @Native("c++", "((x10_long)x10rt_nplaces())")
+    @Native("c++", "((x10_long)x10rt_nhosts())")
     protected static native def num_primary_places():Long;
 
     /**