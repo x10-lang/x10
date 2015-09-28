b9f307e65c561e431425df4c6e594b7d3c083557
Olivier Tardieu <tardieu@us.ibm.com>
2015-09-28 11:42:06 -0400

    [APGAS] updating script to build release from github

--- x10.dist/releng/buildApgas.sh
+++ x10.dist/releng/buildApgas.sh
@@ -3,16 +3,15 @@
 cd /tmp
 rm -Rf apgas-build 
 mkdir apgas-build
-
-git clone -b R4_4_2 https://git.eclipse.org/r/jdt/eclipse.jdt.core apgas-build
-svn co --depth=empty https://svn.code.sf.net/p/x10/code/x10dt/trunk apgas-build
 cd apgas-build
-svn update apgas.compiler apgas.runtime apgas.ui apgas.tools apgas.site apgas.parent
-svn co https://svn.code.sf.net/p/x10/code/trunk/apgas
-svn co https://svn.code.sf.net/p/x10/code/trunk/apgas.examples
-patch -p1 < apgas.compiler/R4_4_2.patch
-cd apgas.parent
+
+git clone -b R4_4_2 https://git.eclipse.org/r/jdt/eclipse.jdt.core
+git clone https://github.com/x10-lang/x10.git
+git clone https://github.com/x10-lang/x10dt.git
+cd eclipse.jdt.core
+patch -p1 < ../x10dt/apgas.compiler/R4_4_2.patch
+cd ../x10dt/apgas.parent
 mvn -P build-individual-bundles package
-cd ..
-cp apgas/apgas.zip .
-cp apgas.site/target/site_assembly.zip apgas-eclipse.zip
+cd ../..
+cp x10/apgas/apgas.zip .
+cp x10dt/apgas.site/target/site_assembly.zip apgas-eclipse.zip
